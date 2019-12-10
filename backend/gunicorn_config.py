b52d7997f896f68463591c40e9361a565702759f
Mikołaj Kosmal <mikolaj.kosmal@tivix.com>
2019-12-10 15:02:11 -0800

    Gunicorn config

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,15 +1,62 @@
-import os
+bind = '0.0.0.0:8000'
+backlog = 2048
 
-ENV = os.getenv('ENV')
-IS_DEV = ENV == 'dev'
-STDOUT_DESTINATION = '-'
 
-reload = IS_DEV
-errorlog = STDOUT_DESTINATION
-accesslog = STDOUT_DESTINATION
-capture_output = IS_DEV
-bind = ':8000'
-loglevel = 'debug' if IS_DEV else 'info'
-workers = 1
-disable_redirect_access_to_syslog = False
+workers = 2
 worker_class = 'gevent'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+
+proc_name = None
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+
+errorlog = '-'
+loglevel = 'info'
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+
+def pre_fork(server, worker):
+    pass
+
+
+def pre_exec(server):
+    server.log.info("Forked child, re-executing.")
+
+
+def when_ready(server):
+    server.log.info("Server is ready. Spawning workers")
+
+
+def worker_int(worker):
+    worker.log.info("Worker received INT or QUIT signal")
+
+    # get traceback info
+    import threading
+    import sys
+    import traceback
+    id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
+    code = []
+    for threadId, stack in sys._current_frames().items():
+        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId, ""), threadId))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    worker.log.debug("\n".join(code))
+
+
+def worker_abort(worker):
+    worker.log.info("worker received SIGABRT signal")
--- backend/hct_mis_api/wsgi.py
+++ backend/hct_mis_api/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hct_mis_api.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "hct_mis_api.settings.{}".format(
+        os.getenv('ENV', 'dev')
+        )
+    )
 
 application = get_wsgi_application()
