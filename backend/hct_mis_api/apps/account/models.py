8bff93890abdf15cfea5ec80de78761bf7608093
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-11 17:00:46 +0100

    added models (without makemigrations)

--- backend/hct_mis_api/apps/account/models.py
+++ backend/hct_mis_api/apps/account/models.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from model_utils.models import UUIDModel
 
 
-class User(AbstractUser):
+class User(AbstractUser, UUIDModel):
     first_name = models.CharField(max_length=64, default="")
     last_name = models.CharField(max_length=64, default="")
     phone_number = models.CharField(max_length=16, default="")
--- backend/hct_mis_api/apps/grievance/models.py
+++ backend/hct_mis_api/apps/grievance/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from model_utils.models import UUIDModel
 
-# Create your models here.
+
+class Grievance(UUIDModel):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    opened_date = models.DateTimeField()
+    household = models.ForeignKey('household.Household', related_name='grievances')
+    assigned_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='grievances')
+
+    def __str__(self):
+        return self.name
