8781a54e57dbb9472ddcd467d69c074a1aa955ff
Mikołaj Kosmal <mikolaj.kosmal@tivix.com>
2019-12-10 17:33:51 -0800

    Added staging settings

--- backend/hct_mis_api/settings/staging.py
+++ backend/hct_mis_api/settings/staging.py
@@ -5,3 +5,19 @@ from .base import *  # noqa: ignore=F403
 # dev overrides
 DEBUG = False
 IS_STAGING = True
+
+# domains/hosts etc.
+DOMAIN_NAME = 'localhost:8000'
+WWW_ROOT = 'http://%s/' % DOMAIN_NAME
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "10.0.2.2", os.getenv("DOMAIN", "")]
+
+# other
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# CACHE
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 1800,
+    }
+}
