11fa4b3050c5b1da7a7433e425c7654769a75404
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-11 17:00:46 +0100

    added models (without makemigrations)

--- backend/hct_mis_api/apps/core/models.py
+++ backend/hct_mis_api/apps/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django_countries.fields import CountryField
+from model_utils.models import UUIDModel
 
-# Create your models here.
+
+class Location(UUIDModel):
+    name = models.CharField(max_length=256)
+    country = CountryField()
+
+    def __str__(self):
+        return self.name
