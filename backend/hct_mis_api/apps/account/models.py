56b92188a7d8c7bc99c13c161163d4a4484f801b
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-12 15:43:21 +0100

    added migrations

--- backend/hct_mis_api/apps/account/models.py
+++ backend/hct_mis_api/apps/account/models.py
@@ -1,14 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
-# Create your models here.
 from model_utils.models import UUIDModel
 
 
 class User(AbstractUser, UUIDModel):
-    first_name = models.CharField(max_length=64, default="")
-    last_name = models.CharField(max_length=64, default="")
-    phone_number = models.CharField(max_length=16, default="")
-
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
