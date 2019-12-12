2417080465a1b06aa5f48866fdcf8cff06b2ba2c
Mikołaj Kosmal <mikolaj.kosmal@tivix.com>
2019-12-12 10:29:08 -0800

    Added AzureStorage

--- backend/hct_mis_api/settings/staging.py
+++ backend/hct_mis_api/settings/staging.py
@@ -21,3 +21,17 @@ CACHES = {
         'TIMEOUT': 1800,
     }
 }
+
+## STORAGE
+STATIC_LOCATION = "static"
+MEDIA_LOCATION = "media"
+
+AZURE_ACCOUNT_NAME = os.getenv("STORAGE_AZURE_ACCOUNT_NAME", "")
+AZURE_ACCOUNT_KEY = os.getenv("STORAGE_AZURE_ACCOUNT_KEY", "")
+
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
+
+DEFAULT_FILE_STORAGE = 'core.storage.AzureMediaStorage'
+STATICFILES_STORAGE = 'core.storage.AzureStaticStorage'
