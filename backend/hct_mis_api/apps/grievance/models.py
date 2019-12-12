dc02c3b925c25af3c0ad138f507c75a379e55d77
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-12 14:11:05 +0100

    updated models, added programme model, payment model

--- backend/hct_mis_api/apps/grievance/models.py
+++ backend/hct_mis_api/apps/grievance/models.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from django.db import models
-from model_utils.models import UUIDModel
+
+from hct_mis_api.apps.utils.models import TimeStampedUUIDModel
 
 
-class Grievance(UUIDModel):
+class Grievance(TimeStampedUUIDModel):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     opened_date = models.DateTimeField()
-    household = models.ForeignKey('household.Household', related_name='grievances')
-    assigned_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='grievances')
+    household = models.ForeignKey('household.Household', related_name='grievances', on_delete=models.CASCADE)
+    assigned_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='grievances', on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
