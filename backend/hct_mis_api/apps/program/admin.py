4e91ccd65f1fe48131b55b6477d7860a289cb8ff
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-17 22:09:08 +0100

    add graphql queries (relay) for program, location, cash_plans, payment_records

--- backend/hct_mis_api/apps/program/admin.py
+++ backend/hct_mis_api/apps/program/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Program, CashPlan
+
+
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'start_date', 'end_date', 'location')
+
+
+admin.site.register(Program, ProgramAdmin)
+admin.site.register(CashPlan)
