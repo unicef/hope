3b98a38cb6378b5daee9fab083cbbf09decd8470
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-18 14:18:42 +0100

    fixes in code style

--- backend/hct_mis_api/apps/program/admin.py
+++ backend/hct_mis_api/apps/program/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import Program, CashPlan
 
 
+@admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'start_date', 'end_date', 'location')
 
 
-admin.site.register(Program, ProgramAdmin)
-admin.site.register(CashPlan)
+@admin.register(CashPlan)
+class CashPlanAdmin(admin.ModelAdmin):
+    pass
