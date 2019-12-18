6b1c1e1eb41ed6eac0299ef6c0193c3be6a1823a
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-18 14:05:55 +0100

    tsconfig

--- frontend/src/relay/enviroment.ts
+++ frontend/src/relay/enviroment.ts
@@ -1,30 +1,21 @@
-import {
-    Environment,
-    Network,
-    RecordSource,
-    Store,
-} from 'relay-runtime';
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
 
-function fetchQuery(
-    operation,
-    variables,
-) {
-    return fetch('/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: operation.text,
-            variables,
-        }),
-    }).then(response => {
-        return response.json();
-    });
+function fetchQuery(operation, variables) {
+  return fetch("/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables
+    })
+  }).then(response => {
+    return response.json();
+  });
 }
 
 export const environment = new Environment({
-    network: Network.create(fetchQuery),
-    store: new Store(new RecordSource()),
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource())
 });
-
