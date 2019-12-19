fda81078fba9b3855e811743ba96185daddf4fd5
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-19 14:16:28 +0100

    relay full init

--- backend/hct_mis_api/urls.py
+++ backend/hct_mis_api/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from core.views import homepage, schema
@@ -17,7 +18,7 @@ def test_raise_exception_view(request):
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('', homepage),
-    path('api/graphql', GraphQLView.as_view(graphiql=True)),
+    path('api/graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('api/graphql/schema.graphql', schema),
     path('api/', include('social_django.urls', namespace='social')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
