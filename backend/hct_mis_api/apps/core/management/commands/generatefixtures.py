e1eb685b5c1210b55cf054c537419ab6301f1097
Wojtek Nosal <wojciech.nosal@tivix.com>
2019-12-19 15:43:28 +0100

    [feat 45935] Introduce fake data generation using factory boy

---
+++ backend/hct_mis_api/apps/core/management/commands/generatefixtures.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+
+from program.fixtures import CashPlanFactory
+
+
+class Command(BaseCommand):
+    help = 'Generate fixtures data for project'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--objects-num',
+            dest='amount',
+            const=10,
+            default=10,
+            action='store',
+            nargs='?',
+            type=int,
+            help='Creates provided amount of fixture objects.',
+        )
+
+    def handle(self, *args, **options):
+        # TODO: should be changed in future to allow more options
+        CashPlanFactory.create_batch(options['amount'])
