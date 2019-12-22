a2000d175e050c898b1a8b49f2a7bc4e9a52248b
Jan Romaniak <jan.romaniak@tivix.com>
2019-12-22 22:14:06 +0100

    programme page

--- frontend/src/containers/HomeRouter.tsx
+++ frontend/src/containers/HomeRouter.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import {makeStyles} from '@material-ui/core';
-import {Drawer} from '../components/Drawer/Drawer';
-import {MiśTheme} from '../theme';
-import {AppBar} from "../components/AppBar";
-import {LoginPage} from "./LoginPage";
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import {DashboardPage} from "./DashboardPage";
-import {ProgramsPage} from "./ProgramsPage";
-
+import { makeStyles } from '@material-ui/core';
+import { Drawer } from '../components/Drawer/Drawer';
+import { MiśTheme } from '../theme';
+import { AppBar } from '../components/AppBar';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { DashboardPage } from './DashboardPage';
+import { ProgramsPage } from './ProgramsPage';
 
 const useStyles = makeStyles((theme: MiśTheme) => ({
   root: {
@@ -42,30 +39,33 @@ export function HomeRouter(): React.ReactElement {
   const isAuthenticated = false;
   const classes = useStyles({});
   const [open, setOpen] = React.useState(true);
+  const location = useLocation();
   const handleDrawerOpen = () => {
     setOpen(true);
   };
+  console.log('location', location.pathname);
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar open={open} handleDrawerOpen={handleDrawerOpen}/>
-      <Drawer open={open} handleDrawerClose={handleDrawerClose} />
+      <AppBar open={open} handleDrawerOpen={handleDrawerOpen} />
+      <Drawer
+        open={open}
+        handleDrawerClose={handleDrawerClose}
+        currentLocation={location.pathname}
+      />
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <Container maxWidth='lg' className={classes.container}>
-          <Switch>
-            <Route path='/'>
-              <DashboardPage />
-            </Route>
-            <Route path='/programs'>
-              <ProgramsPage />
-            </Route>
-          </Switch>
-        </Container>
+        <Switch>
+          <Route path='/programs'>
+            <ProgramsPage />
+          </Route>
+          <Route path='/'>
+            <DashboardPage />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
