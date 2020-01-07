6f4155b4b7ad841612fae7a90a097a894854cc14
Jan Romaniak <jan.romaniak@tivix.com>
2020-01-07 10:42:14 +0100

    styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { RelayEnvironmentProvider } from 'relay-hooks';
 import { environment } from './relay/enviroment';
 import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { ThemeProvider as StyledJssThemeProvider } from 'styled-jss';
 import {
   Route,
   Switch,
@@ -17,16 +19,20 @@ export const App: React.FC = () => {
   return (
     <RelayEnvironmentProvider environment={environment}>
       <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Route path='/login'>
-              <LoginPage />
-            </Route>
-            <Route path='/'>
-              {!authenticated ? <Redirect to='/login' /> : <HomeRouter />}
-            </Route>
-          </Switch>
-        </Router>
+        <StyledThemeProvider theme={theme}>
+          <StyledJssThemeProvider theme={theme}>
+            <Router>
+              <Switch>
+                <Route path='/login'>
+                  <LoginPage />
+                </Route>
+                <Route path='/'>
+                  {!authenticated ? <Redirect to='/login' /> : <HomeRouter />}
+                </Route>
+              </Switch>
+            </Router>
+          </StyledJssThemeProvider>
+        </StyledThemeProvider>
       </ThemeProvider>
     </RelayEnvironmentProvider>
   );
--- frontend/src/containers/HomeRouter.tsx
+++ frontend/src/containers/HomeRouter.tsx
@@ -1,54 +1,38 @@
 import React from 'react';
-import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core';
 import { Drawer } from '../components/Drawer/Drawer';
-import { MiśTheme } from '../theme';
 import { AppBar } from '../components/AppBar';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import { DashboardPage } from './pages/DashboardPage';
 import { ProgramsPage } from './pages/ProgramsPage';
+import { ProgramDetailsPage } from './pages/ProgramDetailsPage';
+import styled from 'styled-components';
+import styledJSS from 'styled-jss';
 
-const useStyles = makeStyles((theme: MiśTheme) => ({
-  root: {
-    display: 'flex',
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
+
+const Root = styled.div`
+  display: flex;
+`;
+const MainContent = styled.div`
+  flex-grow: 1;
+  height: 100vh;
+  overflow: auto;
+`;
+const AppBarSpacer = styledJSS('div')(({ theme }) => ({
+  ...theme.mixins.toolbar,
 }));
 
 export function HomeRouter(): React.ReactElement {
-  const isAuthenticated = false;
-  const classes = useStyles({});
   const [open, setOpen] = React.useState(true);
   const location = useLocation();
   const handleDrawerOpen = () => {
     setOpen(true);
   };
-  console.log('location', location.pathname);
   const handleDrawerClose = () => {
     setOpen(false);
   };
   return (
-    <div className={classes.root}>
+    <Root>
       <CssBaseline />
       <AppBar open={open} handleDrawerOpen={handleDrawerOpen} />
       <Drawer
@@ -56,9 +40,12 @@ export function HomeRouter(): React.ReactElement {
         handleDrawerClose={handleDrawerClose}
         currentLocation={location.pathname}
       />
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
+      <MainContent>
+        <AppBarSpacer/>
         <Switch>
+          <Route path='/programs/:id'>
+            <ProgramDetailsPage />
+          </Route>
           <Route path='/programs'>
             <ProgramsPage />
           </Route>
@@ -66,7 +53,7 @@ export function HomeRouter(): React.ReactElement {
             <DashboardPage />
           </Route>
         </Switch>
-      </main>
-    </div>
+      </MainContent>
+    </Root>
   );
 }
