84e40b112dc8ac8467ffc23e3042effe219e86c5
Jan Romaniak <jan.romaniak@tivix.com>
2020-01-09 15:11:10 +0100

    fix

--- frontend/src/components/programs/ProgramCard.tsx
+++ frontend/src/components/programs/ProgramCard.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 import { theme as themeObj } from '../../theme';
