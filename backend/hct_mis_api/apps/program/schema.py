81e9ed98a87608bb26a017ba40b1f5e66ff78a30
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-01-15 10:58:53 +0100

    renamed CashPlan in schema to CashPlanNode

--- backend/hct_mis_api/apps/program/schema.py
+++ backend/hct_mis_api/apps/program/schema.py
@@ -3,14 +3,14 @@ from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-import program.models as models
+from program.models import Program, CashPlan
 
 
 class ProgramNode(DjangoObjectType):
     total_number_of_households = graphene.Int()
 
     class Meta:
-        model = models.Program
+        model = Program
         filter_fields = []
         interfaces = (relay.Node,)
 
@@ -18,9 +18,9 @@ class ProgramNode(DjangoObjectType):
         return self.total_number_of_households
 
 
-class CashPlan(DjangoObjectType):
+class CashPlanNode(DjangoObjectType):
     class Meta:
-        model = models.CashPlan
+        model = CashPlan
         filter_fields = ['program']
         interfaces = (relay.Node,)
 
@@ -28,8 +28,8 @@ class CashPlan(DjangoObjectType):
 class Query(graphene.ObjectType):
     program = relay.Node.Field(ProgramNode)
     all_programs = DjangoFilterConnectionField(ProgramNode)
-    cash_plan = relay.Node.Field(CashPlan)
-    all_cash_plans = DjangoFilterConnectionField(CashPlan)
+    cash_plan = relay.Node.Field(CashPlanNode)
+    all_cash_plans = DjangoFilterConnectionField(CashPlanNode)
     program_status_choices = graphene.List(graphene.List(graphene.String))
     program_frequency_of_payments_choices = graphene.List(
         graphene.List(graphene.String)
@@ -39,16 +39,16 @@ class Query(graphene.ObjectType):
     cash_plan_status_choices = graphene.List(graphene.List(graphene.String))
 
     def resolve_program_status_choices(self, info, **kwargs):
-        return models.Program.STATUS_CHOICE
+        return Program.STATUS_CHOICE
 
     def resolve_program_frequency_of_payments_choices(self, info, **kwargs):
-        return models.Program.FREQUENCY_OF_PAYMENTS_CHOICE
+        return Program.FREQUENCY_OF_PAYMENTS_CHOICE
 
     def resolve_program_sector_choices(self, info, **kwargs):
-        return models.Program.SECTOR_CHOICE
+        return Program.SECTOR_CHOICE
 
     def resolve_program_scope_choices(self, info, **kwargs):
-        return models.Program.SCOPE_CHOICE
+        return Program.SCOPE_CHOICE
 
     def resolve_cash_plan_status_choices(self, info, **kwargs):
-        return models.CashPlan.STATUS_CHOICE
+        return CashPlan.STATUS_CHOICE
