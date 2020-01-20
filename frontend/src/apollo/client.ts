69150358c479b6a3e9f40b6382e7d496530fe7a4
Jan Romaniak <jan.romaniak@tivix.com>
2020-01-20 12:57:16 +0100

    no message

--- frontend/src/apollo/client.ts
+++ frontend/src/apollo/client.ts
@@ -1,6 +1,16 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
 import { GRAPHQL_URL } from '../config';
+import {createUploadLink} from  'apollo-upload-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = ApolloLink.from([
+    new HttpLink({ uri: GRAPHQL_URL }),
+    createUploadLink()
+    ]);
 
 export const client = new ApolloClient({
-    uri: GRAPHQL_URL,
+    cache: new InMemoryCache(),
+    link,
 });
