209566441068a7879a408619311678ee98320587
Jan Romaniak <romaniakjan@gmail.com>
2020-01-21 18:17:25 +0100

    fixed pagination, added generated types to repository

---
+++ frontend/src/__generated__/introspection-result.ts
@@ -0,0 +1,47 @@
+
+      export interface IntrospectionResultData {
+        __schema: {
+          types: {
+            kind: string;
+            name: string;
+            possibleTypes: {
+              name: string;
+            }[];
+          }[];
+        };
+      }
+      const result: IntrospectionResultData = {
+  "__schema": {
+    "types": [
+      {
+        "kind": "INTERFACE",
+        "name": "Node",
+        "possibleTypes": [
+          {
+            "name": "PaymentRecordNode"
+          },
+          {
+            "name": "CashPlanNode"
+          },
+          {
+            "name": "ProgramNode"
+          },
+          {
+            "name": "LocationNode"
+          },
+          {
+            "name": "HouseholdNode"
+          },
+          {
+            "name": "RegistrationDataImportNode"
+          },
+          {
+            "name": "TargetPopulationNode"
+          }
+        ]
+      }
+    ]
+  }
+};
+      export default result;
+    
