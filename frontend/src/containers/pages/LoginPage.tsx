567abbf3f2bfd880cd5ac970a4729582c4269026
Jan Romaniak <jan.romaniak@tivix.com>
2020-01-23 13:44:20 +0100

    active directory login

--- frontend/src/containers/pages/LoginPage.tsx
+++ frontend/src/containers/pages/LoginPage.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { useMeQuery } from '../../__generated__/graphql';
+import { setAuthenticated } from '../../utils/utils';
+import { LOGIN_URL } from '../../config';
 
 export function LoginPage(): React.ReactElement {
-  return <div>Login</div>;
+  const { data, error } = useMeQuery();
+  useEffect(() => {
+    if (error) {
+      window.location.replace(LOGIN_URL);
+    }
+  });
+  if (error) {
+    return null;
+  }
+  setAuthenticated(true);
+  return <Redirect to='/' />;
 }
