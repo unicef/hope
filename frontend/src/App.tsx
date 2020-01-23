46d71a61309c2e5bdfff761753883e6955bbfe9c
Jan Romaniak <jan.romaniak@tivix.com>
2020-01-23 17:11:02 +0100

    fixed login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { theme } from './theme';
 import { HomeRouter } from './containers/HomeRouter';
@@ -9,7 +9,6 @@ import { ProfilePage } from './containers/pages/ProfilePage';
 import { client } from './apollo/client';
 import { LoginPage } from './containers/pages/LoginPage';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
 
 export const App: React.FC = () => {
   return (
@@ -26,7 +25,7 @@ export const App: React.FC = () => {
                 <ProfilePage />
               </Route>
               <Route path='/'>
-                  <HomeRouter />
+                <HomeRouter />
               </Route>
             </Switch>
           </Router>
--- frontend/src/containers/pages/LoginPage.tsx
+++ frontend/src/containers/pages/LoginPage.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from 'react-router-dom';
+import React from 'react';
 import styled from 'styled-components';
-import { setAuthenticated } from '../../utils/utils';
 import { LOGIN_URL } from '../../config';
-import { Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 
 const Container = styled.div`
   background-color: #eee;
@@ -22,7 +15,7 @@ const LoginBox = styled.div`
   height: 384px;
   width: 533px;
   border-radius: 4px;
-  background-color: #023e90;
+  background-color: ${({ theme }) => theme.palette.primary.main};
   box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.12), 0 2px 2px 0 rgba(0, 0, 0, 0.24);
   padding: 50px;
 `;
@@ -39,13 +32,37 @@ const SubTitle = styled(Typography)`
   font-weight: 300;
   line-height: 32px;
 `;
+const Separator = styled.div`
+  height: 1px;
+  width: 433px;
+  opacity: 0.22;
+  background-color: #d8d8d8;
+  margin-top: ${({ theme }) => theme.spacing(8)}px;
+  width: 100%;
+`;
+const LoginButton = styled(Button)`
+  margin-top: ${({ theme }) => theme.spacing(16)}px !important;
+  width: 100%;
+  height: 64px;
+  margin-left: ${({ theme }) => theme.spacing(11)}px;
+  margin-right: ${({ theme }) => theme.spacing(11)}px;
+`;
 
 export function LoginPage(): React.ReactElement {
   return (
     <Container>
       <LoginBox>
-        <Title >HCT-MIS Portal</Title>
+        <Title>HCT-MIS Portal</Title>
         <SubTitle>Login via Active Directory</SubTitle>
+        <Separator />
+        <LoginButton
+          variant='contained'
+          size='large'
+          component='a'
+          href={LOGIN_URL}
+        >
+          Sign in
+        </LoginButton>
       </LoginBox>
     </Container>
   );
