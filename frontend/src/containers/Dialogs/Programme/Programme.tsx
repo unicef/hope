0535d9565a7bf1fcb9968e9aae024bc669971ace
Marcin <skorekmarcin@gmail.com>
2020-01-23 17:04:01 +0100

    Change administrative fieldname

--- frontend/src/containers/Dialogs/Programme/Programme.tsx
+++ frontend/src/containers/Dialogs/Programme/Programme.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   TextField,
   Dialog,
+  DialogTitle,
   DialogContent,
+  DialogActions,
   Typography,
   FormControl,
   FormLabel,
@@ -11,27 +13,31 @@ import {
   Radio,
   RadioGroup,
 } from '@material-ui/core';
+import * as Yup from 'yup';
 import styled from 'styled-components';
+import moment from 'moment';
 import { Formik, Form, Field } from 'formik';
 import { FormikTextField } from '../../../shared/Formik/FormikTextField';
 import { FormikSelectField } from '../../../shared/Formik/FormikSelectField';
-import { useProgrammeChoiceDataQuery } from '../../../__generated__/graphql';
+import { FormikSwitchField } from '../../../shared/Formik/FormikSwitchField';
+import {
+  useProgrammeChoiceDataQuery,
+  useCreateProgramMutation,
+} from '../../../__generated__/graphql';
 
-const DialogTitle = styled.div`
-  padding: 16px;
-  margin: 0;
+const DialogTitleWrapper = styled.div`
   border-bottom: 1px solid #e4e4e4;
 `;
 
 const DialogFooter = styled.div`
-  padding: 16px;
+  padding: 12px 16px;
   margin: 0;
   border-top: 1px solid #e4e4e4;
   text-align: right;
 `;
 
 const DialogDescription = styled.div`
-  margin: 0 0 20px 0;
+  margin: 20px 0;
   font-size: 14px;
   color: rgba(0, 0, 0, 0.54);
 `;
@@ -43,12 +49,27 @@ const MediumLabel = styled.div`
 
 const DateFields = styled.div`
   display: flex;
+  justify-content: space-between;
+  margin: 12px 0;
 `;
 
-export function Programme() {
-  const [open, setOpen] = React.useState(false);
-  const { data } = useProgrammeChoiceDataQuery();
+const DateField = styled.div`
+  width: 48%;
+`;
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Programme name is required'),
+  scope: Yup.string().required('CashAssist Scope is required'),
+  startDate: Yup.string().required('Start Date is required'),
+  endDate: Yup.string().required('End Date is required'),
+  sector: Yup.string().required('Sector is required'),
+});
+
+export function Programme() {
+  const [open, setOpen] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const { data } = useProgrammeChoiceDataQuery();
+  const [mutate] = useCreateProgramMutation();
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -57,8 +78,25 @@ export function Programme() {
     setOpen(false);
   };
 
-  const submitForm = (values) => {
+  const handleToggle = () => {
+    return toggle ? setToggle(false) : setToggle(true);
+  };
+
+  const submitFormHandler = (values) => {
     console.log('blah blah submit', values);
+    mutate({
+      variables: {
+        programData: {
+          ...values,
+          startDate: moment(values.startDate).toISOString(),
+          endDate: moment(values.endDate).toISOString(),
+          locationId:
+            'TG9jYXRpb25Ob2RlOmNiMjkzZmY0LTM4MzEtNDFmNy1iYWEzLTMxZjQzMGIwMDUzMQ==',
+        },
+      },
+    });
+    //eslint-disable-next-line
+    debugger;
   };
 
   return data ? (
@@ -66,168 +104,199 @@ export function Programme() {
       <Button variant='contained' color='primary' onClick={handleClickOpen}>
         new programme
       </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='form-dialog-title'
+      <Formik
+        initialValues={{
+          name: '',
+          scope: '',
+          startDate: '',
+          endDate: '',
+          description: '',
+          budget: 0,
+          administrativeAreasOfImplementation: '',
+          populationGoal: 0,
+          sector: '',
+          cashPlus: false,
+        }}
+        onSubmit={(values) => {
+          return submitFormHandler(values);
+        }}
+        validationSchema={validationSchema}
       >
-        <DialogTitle>
-          <Typography variant='h6'>Set-up a new Programme</Typography>
-        </DialogTitle>
-        <Formik
-          initialValues={{}}
-          onSubmit={(values) => {
-            return submitForm(values);
-          }}
-        >
+        {({ submitForm }) => (
           <Form>
-            <DialogContent>
-              <DialogDescription>
-                To subscribe to this website, please enter your email address
-                here. We will send updates occasionally.
-              </DialogDescription>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              scroll='paper'
+              aria-labelledby='form-dialog-title'
+            >
+              <DialogTitleWrapper>
+                <DialogTitle id='scroll-dialog-title'>
+                  <Typography variant='h6'>Set-up a new Programme</Typography>
+                </DialogTitle>
+              </DialogTitleWrapper>
+              <DialogContent>
+                <DialogDescription>
+                  To subscribe to this website, please enter your email address
+                  here. We will send updates occasionally.
+                </DialogDescription>
 
-              <Field
-                name='programmeName'
-                label='Programme Name'
-                type='text'
-                fullWidth
-                required
-                component={FormikTextField}
-              />
-
-              <MediumLabel>
                 <Field
-                  name='cashAssisst'
-                  label='CashAssist Scope'
+                  name='name'
+                  label='Programme Name'
+                  type='text'
                   fullWidth
-                  choices={data.programScopeChoices}
-                  component={FormikSelectField}
+                  required
+                  component={FormikTextField}
                 />
-              </MediumLabel>
 
-              <DateFields>
-                <Field name='dateFrom'>
-                  {({ field, form, meta }) => (
-                    <TextField
-                      {...field}
-                      id='from'
+                <MediumLabel>
+                  <Field
+                    name='scope'
+                    label='CashAssist Scope'
+                    fullWidth
+                    required
+                    choices={data.programScopeChoices}
+                    component={FormikSelectField}
+                  />
+                </MediumLabel>
+                <DateFields>
+                  <DateField>
+                    <Field
+                      name='startDate'
                       label='Start Date'
-                      type='date'
-                      margin='dense'
-                      variant='filled'
-                      name='dateFrom'
                       required
                       fullWidth
-                      value={form.values.dateFrom}
+                      component={FormikTextField}
+                      type='date'
                       InputLabelProps={{
                         shrink: true,
                       }}
                     />
-                  )}
-                </Field>
-
-                <Field name='dateTo'>
-                  {({ field, form, meta }) => {
-                    return (<TextField
-                      {...field}
-                      id='to'
-                      label='End Date'
-                      type='date'
-                      margin='dense'
-                      variant='filled'
-                      name='dateTo'
-                      required
-                      fullWidth
-                      value={form.values.dateTo}
-                      InputLabelProps={{
-                        shrink: true,
+                  </DateField>
+                  <DateField>
+                    <Field name='endDate'>
+                      {({ field, form, meta }) => {
+                        return (
+                          <TextField
+                            {...field}
+                            id='to'
+                            label='End Date'
+                            type='date'
+                            margin='dense'
+                            variant='filled'
+                            name='endDate'
+                            required
+                            fullWidth
+                            disabled={!form.values.startDate}
+                            value={form.values.endDate}
+                            InputLabelProps={{
+                              shrink: true,
+                            }}
+                            inputProps={{
+                              min: form.values.startDate,
+                            }}
+                          />
+                        );
                       }}
-                    />)
-                  }}
-                </Field>
-              </DateFields>
-
-              <Field
-                name='description'
-                label='Description'
-                type='text'
-                fullWidth
-                component={FormikTextField}
-              />
-
-              <MediumLabel>
+                    </Field>
+                  </DateField>
+                </DateFields>
                 <Field
-                  name='budget'
-                  label='Budget'
-                  type='number'
-                  required
+                  name='description'
+                  label='Description'
+                  type='text'
                   fullWidth
                   component={FormikTextField}
                 />
-              </MediumLabel>
-              <Field name='frequencyOfPayment'>
-                {({ field, form, meta }) => (
-                  <FormControl component='fieldset'>
-                    <FormLabel component='legend'>
-                      Frequency of Payment
-                    </FormLabel>
-                    <RadioGroup {...field} aria-label='gender' name='gender1'>
-                      {data.programFrequencyOfPaymentsChoices.map(
-                        (each) => (
+                <MediumLabel>
+                  <Field
+                    name='budget'
+                    label='Budget'
+                    type='number'
+                    fullWidth
+                    component={FormikTextField}
+                  />
+                </MediumLabel>
+                <Field name='frequencyOfPayment'>
+                  {({ field }) => (
+                    <FormControl component='fieldset'>
+                      <FormLabel component='legend'>
+                        Frequency of Payment
+                      </FormLabel>
+                      <RadioGroup
+                        {...field}
+                        aria-label='gender'
+                        name='frequencyOfPayments'
+                      >
+                        {data.programFrequencyOfPaymentsChoices.map((each) => (
                           <FormControlLabel
-                            value={each[0]}
-                            label={each[1]}
+                            key={each.value}
+                            value={each.value}
+                            label={each.name}
                             control={<Radio />}
                           />
-                        ),
-                      )}
-                    </RadioGroup>
-                  </FormControl>
-                )}
-              </Field>
-
-              <Field
-                name='areasOfImplementation'
-                label='Administrative Areas of Implementation'
-                type='text'
-                required
-                fullWidth
-                component={FormikTextField}
-              />
-              <MediumLabel>
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                  )}
+                </Field>
                 <Field
-                  name='populationGoal'
-                  label='Population goal'
-                  type='number'
-                  required
+                  name='administrativeAreasOfImplementation'
+                  label='Administrative Areas of Implementation'
+                  type='text'
                   fullWidth
                   component={FormikTextField}
                 />
-              </MediumLabel>
-
-              <MediumLabel>
-                <Field
-                  name='sector'
-                  label='Sector'
-                  fullWidth
-                  choices={data.programSectorChoices}
-                  component={FormikSelectField}
-                />
-              </MediumLabel>
-            </DialogContent>
-
-            <DialogFooter>
-              <Button onClick={handleClose} color='primary'>
-                Cancel
-              </Button>
-              <Button type='submit' color='primary' variant='contained'>
-                Save
-              </Button>
-            </DialogFooter>
+                <MediumLabel>
+                  <Field
+                    name='populationGoal'
+                    label='Population goal'
+                    type='number'
+                    fullWidth
+                    component={FormikTextField}
+                  />
+                </MediumLabel>
+                <MediumLabel>
+                  <Field
+                    name='sector'
+                    label='Sector'
+                    fullWidth
+                    required
+                    choices={data.programSectorChoices}
+                    component={FormikSelectField}
+                  />
+                </MediumLabel>
+                <MediumLabel>
+                  <Field
+                    name='cashPlus'
+                    label='Cash+'
+                    color="primary"
+                    component={FormikSwitchField}
+                    checked={toggle}
+                    onChange={handleToggle}
+                  />
+                </MediumLabel>
+              </DialogContent>
+              <DialogFooter>
+                <DialogActions>
+                  <Button onClick={handleClose} color='primary'>
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={submitForm}
+                    type='submit'
+                    color='primary'
+                    variant='contained'
+                  >
+                    Save
+                  </Button>
+                </DialogActions>
+              </DialogFooter>
+            </Dialog>
           </Form>
-        </Formik>
-      </Dialog>
+        )}
+      </Formik>
     </div>
   ) : (
     <div> Loading ... </div>
