490765041a8a97d2f358069c93ccf16d5601cf10
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-02-03 15:21:53 +0100

    added PaymentRecord query ordering

--- backend/hct_mis_api/apps/payment/schema.py
+++ backend/hct_mis_api/apps/payment/schema.py
@@ -1,10 +1,32 @@
 import graphene
+from django_filters import FilterSet, OrderingFilter
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from core.schema import ExtendedConnection, ChoiceObject
 from payment.models import PaymentRecord
+
+
+class PaymentRecordFilter(FilterSet):
+    class Meta:
+        fields = (
+            "cash_plan",
+            "household",
+        )
+        model = PaymentRecord
+
+    order_by = OrderingFilter(
+        fields=(
+            "status",
+            "name",
+            "status_date",
+            "cash_assist_id",
+            "head_of_household",
+            "total_person_covered",
+            "distribution_modality",
+        )
+    )
 
 
 class PaymentRecordNode(DjangoObjectType):
@@ -17,7 +39,9 @@ class PaymentRecordNode(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     payment_record = relay.Node.Field(PaymentRecordNode)
-    all_payment_records = DjangoFilterConnectionField(PaymentRecordNode)
+    all_payment_records = DjangoFilterConnectionField(
+        PaymentRecordNode, filterset_class=PaymentRecordFilter,
+    )
     payment_delivery_type_choices = graphene.List(ChoiceObject)
 
     def resolve_payment_delivery_type_choices(self, info, **kwargs):
