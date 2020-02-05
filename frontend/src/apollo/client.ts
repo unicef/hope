e1eed74fea3b580480214e4d9387b0d7932c699e
Jan Romaniak <romaniakjan@gmail.com>
2020-02-05 15:40:58 +0100

    added link error to login page (#52)

--- frontend/src/apollo/client.ts
+++ frontend/src/apollo/client.ts
@@ -2,8 +2,25 @@ import ApolloClient from 'apollo-client';
 import { createUploadLink } from 'apollo-upload-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { GRAPHQL_URL } from '../config';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      if (message.toLowerCase().includes('user is not authenticated')) {
+        window.location.replace('/login');
+      }
+    });
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+const link = ApolloLink.from([
+  errorLink,
+  createUploadLink({ uri: GRAPHQL_URL }),
+]);
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: createUploadLink({ uri: GRAPHQL_URL }),
+  link,
 });
