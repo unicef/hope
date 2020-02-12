6d8a54fa9dc5ec6146aa263c9347510f35dfe39c
Wojciech Nosal <wojciech.nosal@tivix.com>
2020-02-12 13:42:23 +0100

    Factories fix and orderBy for household (#67)
    * Factories fix and orderBy for household
    
    * household filters
    
    * filters fix

--- backend/hct_mis_api/apps/household/schema.py
+++ backend/hct_mis_api/apps/household/schema.py
@@ -1,10 +1,40 @@
 import graphene
+from django_filters import FilterSet, OrderingFilter, CharFilter, NumberFilter
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from core.schema import ExtendedConnection
 from household.models import Household, RegistrationDataImport
+
+
+class HouseholdFilter(FilterSet):
+    business_area = CharFilter(field_name="location__business_area__slug")
+    family_size_greater = NumberFilter(
+        field_name="family_size", lookup_expr="gte"
+    )
+    family_size_lower = NumberFilter(
+        field_name="family_size", lookup_expr="lte"
+    )
+
+    class Meta:
+        model = Household
+        fields = (
+            "business_area",
+            "family_size_greater",
+            "family_size_lower",
+        )
+
+    order_by = OrderingFilter(
+        fields=(
+            "household_ca_id",
+            "residence_status",
+            "nationality",
+            "family_size",
+            "representative__full_name",
+            "registration_data_import_id__name",
+        )
+    )
 
 
 class HouseholdNode(DjangoObjectType):
@@ -25,7 +55,9 @@ class RegistrationDataImportNode(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     household = relay.Node.Field(HouseholdNode)
-    all_households = DjangoFilterConnectionField(HouseholdNode)
+    all_households = DjangoFilterConnectionField(
+        HouseholdNode, filterset_class=HouseholdFilter
+    )
     registration_data_import = relay.Node.Field(RegistrationDataImportNode)
     all_registration_data_imports = DjangoFilterConnectionField(
         RegistrationDataImportNode
