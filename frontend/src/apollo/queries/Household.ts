e776cfbcacbe48188aa19b7931eace1061d7dc0e
Otavio Sonda Alenski <otavio.alenski@tivix.com>
2020-02-13 16:58:08 +0000

    first commit

---
+++ frontend/src/apollo/queries/Household.ts
@@ -0,0 +1,33 @@
+import { gql } from 'apollo-boost';
+
+export const HOUSEHOLD_QUERY = gql`
+  query Household($id: ID!) {
+    household(id: $id) {
+      id
+      createdAt
+      familySize
+      location {
+        id
+        title
+      }
+      residenceStatus
+      paymentRecords {
+        edges {
+          node {
+            id
+            headOfHousehold
+            cashPlan {
+              id
+              program {
+                id
+                name
+              }
+              totalDeliveredQuantity
+              currency
+            }
+          }
+        }
+      }
+    }
+  }
+`;
