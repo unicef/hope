768c9403ed4960a6002780a83406430ae7177428
Marcin <skorekmarcin@gmail.com>
2020-02-17 08:43:17 +0100

    Add templates

---
+++ frontend/src/containers/pages/TargetPopulationDetailsPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PageHeader } from '../../components/PageHeader';
+
+export function TargetPopulationDetailsPage() {
+  return (
+    <div>
+      <PageHeader title='Helping people in {businessArea}' />
+    </div>
+  );
+}
