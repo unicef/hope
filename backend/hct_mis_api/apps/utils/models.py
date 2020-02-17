91e948ecf34067c981475dccbb6baff530dee877
Wojciech Nosal <wojciech.nosal@tivix.com>
2020-02-17 12:52:42 +0100

    New version of XLS Import of Flex Fields (#73)
    * New version of XLS Import of Flex Fields
    
    * Test added, some fixes, displaying user friendly errors
    
    * fix for reimporting soft deleted objects

--- backend/hct_mis_api/apps/utils/models.py
+++ backend/hct_mis_api/apps/utils/models.py
@@ -1,6 +1,8 @@
 # Create your models here.
 from django.db import models
 from model_utils.models import UUIDModel
+from mptt.managers import TreeManager
+from mptt.models import MPTTModel
 
 
 class TimeStampedUUIDModel(UUIDModel):
@@ -9,3 +11,36 @@ class TimeStampedUUIDModel(UUIDModel):
 
     class Meta:
         abstract = True
+
+
+class SoftDeletionTreeManager(TreeManager):
+    def get_queryset(self, *args, **kwargs):
+        """
+        Return queryset limited to not removed entries.
+        """
+        return (
+            super(TreeManager, self)
+            .get_queryset(*args, **kwargs).filter(is_removed=False)
+            .order_by(self.tree_id_attr, self.left_attr)
+        )
+
+
+class SoftDeletionTreeModel(MPTTModel, TimeStampedUUIDModel):
+    is_removed = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    objects = SoftDeletionTreeManager()
+    all_objects = models.Manager()
+
+    def delete(self, using=None, soft=True, *args, **kwargs):
+        """
+        Soft delete object (set its ``is_removed`` field to True).
+        Actually delete object if setting ``soft`` to False.
+        """
+        if soft:
+            self.is_removed = True
+            self.save(using=using)
+        else:
+            return super().delete(using=using, *args, **kwargs)
