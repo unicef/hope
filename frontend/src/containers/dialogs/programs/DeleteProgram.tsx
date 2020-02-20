091fa445a7fe80c5901d60d680ea7055b06a1740
Marcin <skorekmarcin@gmail.com>
2020-02-20 17:19:45 +0100

    Change toast duration

--- frontend/src/containers/dialogs/programs/DeleteProgram.tsx
+++ frontend/src/containers/dialogs/programs/DeleteProgram.tsx
@@ -144,7 +144,7 @@ export function DeleteProgram({
       {snackBar.show && (
         <Snackbar
           open={snackBar.show}
-          autoHideDuration={2000}
+          autoHideDuration={5000}
           onClose={() => snackBar.setShow(false)}
         >
           <SnackbarContent message={snackBar.message} />
