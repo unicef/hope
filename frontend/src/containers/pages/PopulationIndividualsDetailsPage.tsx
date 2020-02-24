8c98d09cf8f948752c720fb45f4179da976a3eee
Otavio Sonda Alenski <otavio.alenski@tivix.com>
2020-02-24 10:25:52 +0000

    final changes to population individual details components

--- frontend/src/containers/pages/PopulationIndividualsDetailsPage.tsx
+++ frontend/src/containers/pages/PopulationIndividualsDetailsPage.tsx
@@ -4,11 +4,14 @@ import { useParams } from 'react-router-dom';
 import { PageHeader } from '../../components/PageHeader';
 import { BreadCrumbsItem } from '../../components/BreadCrumbs';
 import { useBusinessArea } from '../../hooks/useBusinessArea';
-import { IndividualsBioData } from '../../components/population/IndividualsBioData';
+import { IndividualsBioData } from '../../components/population/IndividualBioData';
 import {
   useIndividualQuery,
   IndividualNode,
 } from '../../__generated__/graphql';
+import { IndividualContactDetails } from '../../components/population/IndividualContactDetails';
+import { IndividualVulnerabilities } from '../../components/population/IndividualVunerabilities';
+import { IndividualActivityTable } from '../IndividualActivityTable';
 
 const Container = styled.div`
 padding 20px;
@@ -46,6 +49,9 @@ export function PopulationIndividualsDetailsPage(): React.ReactElement {
       />
       <Container>
         <IndividualsBioData individual={individual as IndividualNode} />
+        <IndividualContactDetails individual={individual as IndividualNode} />
+        <IndividualVulnerabilities individual={individual as IndividualNode} />
+        <IndividualActivityTable individual={individual as IndividualNode} />
       </Container>
     </div>
   );
