3849e9d0d09b621a12dc04512b6ee8d774707714
Marcin <skorekmarcin@gmail.com>
2020-02-25 17:20:06 +0100

    Add paper containers

--- frontend/src/containers/pages/CreateTargetPopulation.tsx
+++ frontend/src/containers/pages/CreateTargetPopulation.tsx
@@ -1,16 +1,22 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { Button } from '@material-ui/core';
+import { Button, Typography, Paper } from '@material-ui/core';
 import { PageHeader } from '../../components/PageHeader';
 
-const Container = styled.div`
-  && {
-    display: flex;
-    flex-direction: column;
-    min-width: 100%;
-  }
+const PaperContainer = styled(Paper)`
+  display: flex;
+  padding: ${({ theme }) => theme.spacing(3)}px
+    ${({ theme }) => theme.spacing(4)}px;
+  margin: ${({ theme }) => theme.spacing(5)}px;
+  flex-direction: column;
+  border-bottom: 1px solid rgba(224, 224, 224, 1);
 `;
+
+const Title = styled.div`
+  padding-bottom: ${({ theme }) => theme.spacing(8)}px;
+`;
+
 
 export function CreateTargetPopulation() {
   const { t } = useTranslation();
@@ -22,9 +28,24 @@ export function CreateTargetPopulation() {
           Create new
         </Button>
       </PageHeader>
-      <Container>
-        dupa
-      </Container>
+      <PaperContainer>
+        <Title>
+          <Typography variant='h6'>Targeting Criteria</Typography>
+        </Title>
+        <Button variant='outlined' color='primary'>
+          Add Criteria
+        </Button>
+      </PaperContainer>
+      <PaperContainer>
+        <Title>
+          <Typography variant='h6'>Results</Typography>
+        </Title>
+      </PaperContainer>
+      <PaperContainer>
+        <Title>
+          <Typography variant='h6'>Target Population Entries (Households)</Typography>
+        </Title>
+      </PaperContainer>
     </div>
   );
 }
