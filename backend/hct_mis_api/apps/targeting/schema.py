481d8c72063221de12bb97932b200168641c3f7f
Akul Mathur <akul.mathur@tivix.com>
2020-02-26 23:01:11 +0530

    added migration for allTargetPopulation.

--- backend/hct_mis_api/apps/targeting/schema.py
+++ backend/hct_mis_api/apps/targeting/schema.py
@@ -1,6 +1,6 @@
 import django_filters
 import graphene
-import targeting.models as models
+import targeting.models as target_models
 from core.schema import ExtendedConnection
 from graphene import relay
 from graphene_django import DjangoObjectType
@@ -8,17 +8,42 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 
 class TargetPopulationFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name="name",
+                                     lookup_expr="icontains")
+    created_by_name = django_filters.CharFilter(
+        field_name="created_by", method='filter_created_by_name')
+    min_num_individuals = django_filters.NumericRangeFilter(
+        field_name="num_individuals_household", lookup_expr="gte")
+    max_num_individuals = django_filters.NumericRangeFilter(
+        field_name="num_individuals_household", lookup_expr="lte")
+
+    @staticmethod
+    def filter_created_by_name(self, queryset, model_field, value):
+        """Gets full name of the associated user from query."""
+        return queryset.filter(
+            **{
+                f'{model_field}__first_name__contains': value,
+                f'{model_field}_last_name__contains': value,
+            })
+
     class Meta:
-        model = models.TargetPopulation
+        model = target_models.TargetPopulation
         fields = (
-            "created_by",
+            "name",
             "status",
+            "created_by_name",
+            "created_at",
+            "last_edited_at",
             "num_individuals_household",
+            "households",
         )
         filter_overrides = {
-            models.IntegerRangeField: {
+            target_models.IntegerRangeField: {
                 'filter_class': django_filters.NumericRangeFilter,
-            }
+            },
+            target_models.models.DateTimeField: {
+                'filter_class': django_filters.DateTimeFilter,
+            },
         }
 
     order_by = django_filters.OrderingFilter(fields=("created_by",))
@@ -26,12 +51,12 @@ class TargetPopulationFilter(django_filters.FilterSet):
 
 class TargetPopulationNode(DjangoObjectType):
     class Meta:
-        model = models.TargetPopulation
+        model = target_models.TargetPopulation
         interfaces = (relay.Node,)
         connection_class = ExtendedConnection
+        filterset_class = TargetPopulationFilter
 
 
 class Query(graphene.ObjectType):
     target_population = relay.Node.Field(TargetPopulationNode)
-    all_target_population = DjangoFilterConnectionField(
-        TargetPopulationNode, filterset_class=TargetPopulationFilter)
+    all_target_population = DjangoFilterConnectionField(TargetPopulationNode)
