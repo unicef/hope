138b5c647f385b847bdf6ea7c8e0bf31a5028d51
Wojciech Nosal <wojciech.nosal@tivix.com>
2020-02-26 12:35:27 +0100

    50796 - [backend] GraphQL query for registration data import list screen with filters (#92)
    * Registration Data Hub and household/individual/registration_data models changes, added queries, filters (both for imported and hct individuals and households) + tests, added imported fixtures to generate in generatefixtures
    
    * implementing suggestion from review comment

--- backend/hct_mis_api/apps/registration_datahub/tests/snapshots/snap_test_imported_household_query.py
+++ backend/hct_mis_api/apps/registration_datahub/tests/snapshots/snap_test_imported_household_query.py
@@ -7,9 +7,9 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestHouseholdQuery::test_household_query_all 1'] = {
+snapshots['TestImportedHouseholdQuery::test_imported_household_query_all 1'] = {
     'data': {
-        'allHouseholds': {
+        'allImportedHouseholds': {
             'edges': [
                 {
                     'node': {
@@ -72,144 +72,144 @@ snapshots['TestHouseholdQuery::test_household_query_all 1'] = {
     }
 }
 
-snapshots['TestHouseholdQuery::test_household_query_single 1'] = {
+snapshots['TestImportedHouseholdQuery::test_imported_household_query_all_max 1'] = {
     'data': {
-        'household': {
+        'allImportedHouseholds': {
+            'edges': [
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 2,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 4,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 5,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 1,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 3,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                }
+            ]
+        }
+    }
+}
+
+snapshots['TestImportedHouseholdQuery::test_imported_household_query_all_min 1'] = {
+    'data': {
+        'allImportedHouseholds': {
+            'edges': [
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 4,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 5,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 3,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 11,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 14,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                }
+            ]
+        }
+    }
+}
+
+snapshots['TestImportedHouseholdQuery::test_imported_household_query_all_range 1'] = {
+    'data': {
+        'allImportedHouseholds': {
+            'edges': [
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 4,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 5,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                },
+                {
+                    'node': {
+                        'address': 'Lorem Ipsum',
+                        'familySize': 3,
+                        'householdCaId': '123-123-123',
+                        'nationality': 'PL'
+                    }
+                }
+            ]
+        }
+    }
+}
+
+snapshots['TestImportedHouseholdQuery::test_imported_household_query_single 1'] = {
+    'data': {
+        'importedHousehold': {
             'address': 'Lorem Ipsum',
             'familySize': 2,
             'householdCaId': '123-123-123',
             'nationality': 'PL'
-        }
-    }
-}
-
-snapshots['TestHouseholdQuery::test_household_query_all_max 1'] = {
-    'data': {
-        'allHouseholds': {
-            'edges': [
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 2,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 4,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 5,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 1,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 3,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestHouseholdQuery::test_household_query_all_min 1'] = {
-    'data': {
-        'allHouseholds': {
-            'edges': [
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 4,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 5,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 3,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 11,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 14,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestHouseholdQuery::test_household_query_all_range 1'] = {
-    'data': {
-        'allHouseholds': {
-            'edges': [
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 4,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 5,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'Lorem Ipsum',
-                        'familySize': 3,
-                        'householdCaId': '123-123-123',
-                        'nationality': 'PL'
-                    }
-                }
-            ]
         }
     }
 }
