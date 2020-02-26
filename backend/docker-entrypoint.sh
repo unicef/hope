488ca717e9093d77fcd6ced46eeec779d47a55d9
Akul Mathur <akul.mathur@tivix.com>
2020-02-26 16:03:44 +0530

    fixed to run on local and bind docker db_1 instance.

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]; then
 else
     case "$1" in
         "dev")
-        until pg_isready -h db -p 5432;
+        until pg_isready -h db -p 5432:5432;
           do echo "waiting for database"; sleep 2; done;
         python manage.py collectstatic --no-input
         python manage.py migrate
