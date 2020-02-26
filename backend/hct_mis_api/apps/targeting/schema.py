4711d07bd9e5901852c3b254a7f123c6eff9c4ce
Akul Mathur <akul.mathur@tivix.com>
2020-02-26 16:07:02 +0530

    target population model and gql initial skeletal.

--- backend/hct_mis_api/apps/targeting/schema.py
+++ backend/hct_mis_api/apps/targeting/schema.py
@@ -1,20 +1,37 @@
+import django_filters
 import graphene
+import targeting.models as models
+from core.schema import ExtendedConnection
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from core.schema import ExtendedConnection
-from targeting.models import TargetPopulation
+
+class TargetPopulationFilter(django_filters.FilterSet):
+    class Meta:
+        model = models.TargetPopulation
+        fields = (
+            "created_by",
+            "status",
+            "num_individuals_household",
+        )
+        filter_overrides = {
+            models.IntegerRangeField: {
+                'filter_class': django_filters.NumericRangeFilter,
+            }
+        }
+
+    order_by = django_filters.OrderingFilter(fields=("created_by",))
 
 
 class TargetPopulationNode(DjangoObjectType):
     class Meta:
-        model = TargetPopulation
-        filter_fields = []
+        model = models.TargetPopulation
         interfaces = (relay.Node,)
         connection_class = ExtendedConnection
 
 
 class Query(graphene.ObjectType):
     target_population = relay.Node.Field(TargetPopulationNode)
-    all_target_population = DjangoFilterConnectionField(TargetPopulationNode)
+    all_target_population = DjangoFilterConnectionField(
+        TargetPopulationNode, filterset_class=TargetPopulationFilter)
