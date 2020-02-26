78069db44d6f9fb6b41d1db59ef0454c57bd7aac
Akul Mathur <akul.mathur@tivix.com>
2020-02-26 16:07:02 +0530

    target population model and gql initial skeletal.

--- backend/hct_mis_api/apps/targeting/models.py
+++ backend/hct_mis_api/apps/targeting/models.py
@@ -1,22 +1,38 @@
+import enum
+from typing import List
+
 from django.conf import settings
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import IntegerRangeField
 from django.db import models
 from model_utils.models import UUIDModel
 
 
+class TargetStatus(enum.Enum):
+    IN_PROGRESS = 'In Progress'
+    FINALIZED = 'Finalized'
+
+    @classmethod
+    def get_choices(cls) -> List[tuple]:
+        return [(field.name, field.value) for field in cls]
+
+
 class TargetPopulation(UUIDModel):
-    name = models.CharField(max_length=255)
+    _STATE_CHOICES = TargetStatus.get_choices()
+    # fields
+    name = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    last_edited_at = models.DateTimeField(auto_now=False)
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         related_name="target_populations",
         null=True,
     )
-    rules = JSONField()
-    households = models.ManyToManyField(
-        "household.Household", related_name="target_populations"
-    )
-
-    def __str__(self):
-        return self.name
+    households = models.ManyToManyField("household.Household",
+                                        related_name="target_populations")
+    num_individuals_household = IntegerRangeField()
+    status = models.CharField(max_length=120,
+                              blank=False,
+                              choices=_STATE_CHOICES,
+                              null=False,
+                              default=TargetStatus.IN_PROGRESS)
