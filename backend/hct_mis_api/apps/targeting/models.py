33255e2d51a065e2c23f1087c24486a9022a5182
Akul Mathur <akul.mathur@tivix.com>
2020-02-28 16:42:31 +0530

    added target filters models and create mutations. modifications to query.

--- backend/hct_mis_api/apps/targeting/models.py
+++ backend/hct_mis_api/apps/targeting/models.py
@@ -1,4 +1,3 @@
-import datetime as dt
 import enum
 from typing import List
 
@@ -6,21 +5,34 @@ from django.conf import settings
 from django.contrib.postgres.fields import IntegerRangeField
 from django.contrib.postgres.validators import RangeMinValueValidator, RangeMaxValueValidator
 from django.db import models
+from household.models import Household
 from model_utils.models import UUIDModel
 from psycopg2.extras import NumericRange
 
-
-def get_serialized_range():
-    return NumericRange(1, 101)
+_MAX_LEN = 256
 
 
-class TargetStatus(enum.Enum):
-    IN_PROGRESS = 'In Progress'
-    FINALIZED = 'Finalized'
+def get_serialized_range(min_range=None, max_range=None):
+    return NumericRange(min_range or 1, max_range or 101)
+
+
+class EnumGetChoices(enum.Enum):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
 
     @classmethod
     def get_choices(cls) -> List[tuple]:
         return [(field.name, field.value) for field in cls]
+
+
+class TargetStatus(EnumGetChoices):
+    IN_PROGRESS = 'In Progress'
+    FINALIZED = 'Finalized'
+
+
+class Gender(EnumGetChoices):
+    M = 'M'
+    F = 'F'
 
 
 class TargetPopulation(UUIDModel):
@@ -28,9 +40,7 @@ class TargetPopulation(UUIDModel):
     # fields
     name = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    last_edited_at = models.DateTimeField(auto_now=False,
-                                          null=True,
-                                          default=dt.datetime.today())
+    last_edited_at = models.DateTimeField(auto_now=True, null=True)
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
@@ -38,15 +48,41 @@ class TargetPopulation(UUIDModel):
         null=True,
     )
     households = models.ManyToManyField("household.Household",
-                                        related_name="target_populations")
+                                        related_name="target_populations",
+                                        blank=False)
     # saves (min, max) individuals from households.
     num_individuals_household = IntegerRangeField(
         default=get_serialized_range,
         blank=True,
         validators=[RangeMinValueValidator(1),
-                    RangeMaxValueValidator(50)])
-    status = models.CharField(max_length=120,
+                    RangeMaxValueValidator(50000)])
+    status = models.CharField(max_length=_MAX_LEN,
                               blank=False,
                               choices=_STATE_CHOICES,
                               null=False,
                               default=TargetStatus.IN_PROGRESS)
+
+    @classmethod
+    def bulk_add_households(cls, household_ca_ids):
+        # add households
+        households = [
+            Household.objects.get(
+                household_ca_id=target_household["household_ca_id"])
+            for target_household in household_ca_ids
+        ]
+
+
+class TargetFilter(models.Model):
+    _GENDER_CHOICES = Gender.get_choices()
+
+    intake_group = models.CharField(max_length=_MAX_LEN, null=False)
+    sex = models.CharField(max_length=2, choices=_GENDER_CHOICES)
+    age = models.IntegerField()
+    school_distance_min = models.IntegerField()
+    school_distance_max = models.IntegerField()
+    num_individuals_household_min = models.IntegerField()
+    num_individuals_household_max = models.IntegerField()
+    target_population = models.ForeignKey("TargetPopulation",
+                                          related_name="target_filters",
+                                          on_delete=models.PROTECT,
+                                          null=False)
