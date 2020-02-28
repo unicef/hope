87840af827899c875e4263766ef47dd350ecd2f6
Marcin <skorekmarcin@gmail.com>
2020-02-28 14:34:55 +0100

    Add button for detailed page

--- frontend/src/containers/pages/TargetPopulationDetailsPage.tsx
+++ frontend/src/containers/pages/TargetPopulationDetailsPage.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { Button, Typography, Paper } from '@material-ui/core';
+import { EditRounded } from '@material-ui/icons';
 import { PageHeader } from '../../components/PageHeader';
 import { TargetingCriteria } from '../../components/TargetPopulation/TargetingCriteria';
+import { useBusinessArea } from '../../hooks/useBusinessArea';
+import { BreadCrumbsItem } from '../../components/BreadCrumbs';
 
 const PaperContainer = styled(Paper)`
   display: flex;
@@ -18,16 +21,34 @@ const Title = styled.div`
   padding-bottom: ${({ theme }) => theme.spacing(8)}px;
 `;
 
+const ButtonContainer = styled.span`
+  margin: 0 ${({ theme }) => theme.spacing(2)}px;
+`;
 
 export function TargetPopulationDetailsPage() {
   const { t } = useTranslation();
-
+  const businessArea = useBusinessArea();
+  const breadCrumbsItems: BreadCrumbsItem[] = [
+    {
+      title: 'Target Population',
+      to: `/${businessArea}/target-population/`,
+    },
+  ];
   return (
     <div>
-      <PageHeader title={t('Population')}>
-        <Button variant='contained' color='primary' disabled>
-          Save
-        </Button>
+      <PageHeader title={t('Population')} breadCrumbs={breadCrumbsItems}>
+        <>
+          <ButtonContainer>
+            <Button variant='outlined' color='primary' startIcon={<EditRounded />}>
+              Edit
+            </Button>
+          </ButtonContainer>
+          <ButtonContainer>
+            <Button variant='contained' color='primary'>
+              Finalize
+            </Button>
+          </ButtonContainer>
+        </>
       </PageHeader>
       <TargetingCriteria />
       <PaperContainer>
@@ -37,7 +58,9 @@ export function TargetPopulationDetailsPage() {
       </PaperContainer>
       <PaperContainer>
         <Title>
-          <Typography variant='h6'>Target Population Entries (Households)</Typography>
+          <Typography variant='h6'>
+            Target Population Entries (Households)
+          </Typography>
         </Title>
       </PaperContainer>
     </div>
