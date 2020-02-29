8425868264b77c0b563531cc5991284799dd3315
Marcin <skorekmarcin@gmail.com>
2020-02-29 11:22:45 +0100

    Add field to filter target population by person who created

--- frontend/src/containers/tables/TargetPopulationTable/TargetPopulationTable.tsx
+++ frontend/src/containers/tables/TargetPopulationTable/TargetPopulationTable.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import styled from 'styled-components';
 import {
   TargetPopulationNode,
   useAllTargetPopulationsQuery,
@@ -8,16 +9,34 @@ import { UniversalTable } from '../UniversalTable';
 import { headCells } from './TargetPopulationTableHeadCells';
 import { TargetPopulationTableRow } from './TargetPopulationTableRow';
 
-export function TargetPopulationTable(): ReactElement {
-  const initialVariables = {};
-  return (
-    <UniversalTable<TargetPopulationNode, AllTargetPopulationsQuery>
-      title='Payment Records'
-      headCells={headCells}
-      query={useAllTargetPopulationsQuery}
-      queriedObjectName='allTargetPopulation'
-      initialVariables={null}
-      renderRow={(row) => <TargetPopulationTableRow targetPopulation={row} />}
-    />
-  );
+const TableWrapper = styled.div`
+  padding: 20px;
+`;
+
+interface TargetPopulationProps {
+  businessArea: string;
+  sizeFilter: { min: number | undefined; max: number | undefined };
+  textFilter: string;
 }
+
+export const TargetPopulationTable = ({
+  businessArea,
+  sizeFilter,
+}: TargetPopulationProps): ReactElement => {
+  const initialVariables = {
+    businessArea,
+    familySize: JSON.stringify(sizeFilter),
+  };
+  return (
+    <TableWrapper>
+      <UniversalTable<TargetPopulationNode, AllTargetPopulationsQuery>
+        title='Payment Records'
+        headCells={headCells}
+        query={useAllTargetPopulationsQuery}
+        queriedObjectName='allTargetPopulation'
+        initialVariables={null}
+        renderRow={(row) => <TargetPopulationTableRow targetPopulation={row} />}
+      />
+    </TableWrapper>
+  );
+};
