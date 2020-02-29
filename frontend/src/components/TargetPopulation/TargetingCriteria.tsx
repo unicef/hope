87ec2113133c08072e51b75a634e69a4c0521a20
Marcin <skorekmarcin@gmail.com>
2020-02-29 11:51:13 +0100

    Move criteria component to seperate file

--- frontend/src/components/TargetPopulation/TargetingCriteria.tsx
+++ frontend/src/components/TargetPopulation/TargetingCriteria.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import { Typography, Paper } from '@material-ui/core';
+import { Criteria } from './Criteria';
 
 const data = [
   {
@@ -35,21 +36,6 @@ const ContentWrapper = styled.div`
   display: flex;
 `;
 
-const Criteria = styled.div`
-  width: 380px;
-  border: 2px solid #033f91;
-  border-radius: 3px;
-  font-size: 16px;
-  background-color: #f7faff;
-  padding: ${({ theme }) => theme.spacing(3)}px;
-  p {
-    margin: ${({ theme }) => theme.spacing(2)}px 0;
-    span {
-      color: #003c8f;
-    }
-  }
-`;
-//eslint-disable-next-line
 export function TargetingCriteria() {
   const { t } = useTranslation();
 
@@ -62,36 +48,9 @@ export function TargetingCriteria() {
         <ContentWrapper>
           {data.map((criteria) => {
             return (
-              <Criteria>
-                {criteria.intakeGroup && (
-                  <p>
-                    Intake group: <span>{criteria.intakeGroup}</span>
-                  </p>
-                )}
-                {criteria.sex && (
-                  <p>
-                    Sex: <span>{criteria.sex}</span>
-                  </p>
-                )}
-                {criteria.age && (
-                  <p>
-                    Age: <span>{criteria.age}</span>
-                  </p>
-                )}
-                {criteria.distanceToSchool && (
-                  <p>
-                    Distance to school: <span>{criteria.distanceToSchool}</span>
-                  </p>
-                )}
-                {criteria.household && (
-                  <p>
-                    Hoousehold size: <span>{criteria.household}</span>
-                  </p>
-                )}
-              </Criteria>
+              <Criteria criteria={criteria} />
             );
           })}
-          
         </ContentWrapper>
         {/* <Criteria>Add Criteria</Criteria> */}
       </PaperContainer>
