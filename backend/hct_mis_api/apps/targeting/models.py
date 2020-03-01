60a45e1978fe3be29ff5cb576fd4ee3ca2ddf826
Akul Mathur <akul.mathur@tivix.com>
2020-03-01 20:28:49 +0530

    added fields for mutations, status and last_edited_at

--- backend/hct_mis_api/apps/targeting/models.py
+++ backend/hct_mis_api/apps/targeting/models.py
@@ -5,15 +5,29 @@ from django.conf import settings
 from django.contrib.postgres.fields import IntegerRangeField
 from django.contrib.postgres.validators import RangeMinValueValidator, RangeMaxValueValidator
 from django.db import models
+from django.db.models import Q
 from household.models import Household
 from model_utils.models import UUIDModel
 from psycopg2.extras import NumericRange
 
 _MAX_LEN = 256
+_MIN_RANGE = 1
+_MAX_RANGE = 200
 
 
 def get_serialized_range(min_range=None, max_range=None):
-    return NumericRange(min_range or 1, max_range or 101)
+    return NumericRange(min_range or _MIN_RANGE, max_range or _MAX_RANGE)
+
+
+def get_integer_range(min_range=None, max_range=None):
+    min_range = min_range or _MIN_RANGE
+    max_range = max_range or _MAX_RANGE
+    return IntegerRangeField(default=get_serialized_range,
+                             blank=True,
+                             validators=[
+                                 RangeMinValueValidator(min_range),
+                                 RangeMaxValueValidator(max_range)
+                             ])
 
 
 class EnumGetChoices(enum.Enum):
@@ -47,29 +61,22 @@ class TargetPopulation(UUIDModel):
         related_name="target_populations",
         null=True,
     )
-    households = models.ManyToManyField("household.Household",
-                                        related_name="target_populations",
-                                        blank=False)
-    # saves (min, max) individuals from households.
-    num_individuals_household = IntegerRangeField(
-        default=get_serialized_range,
-        blank=True,
-        validators=[RangeMinValueValidator(1),
-                    RangeMaxValueValidator(50000)])
     status = models.CharField(max_length=_MAX_LEN,
                               blank=False,
                               choices=_STATE_CHOICES,
                               null=False,
                               default=TargetStatus.IN_PROGRESS)
+    households = models.ManyToManyField("household.Household",
+                                        related_name="target_populations",
+                                        blank=False)
 
     @classmethod
-    def bulk_add_households(cls, household_ca_ids):
-        # add households
-        households = [
-            Household.objects.get(
-                household_ca_id=target_household["household_ca_id"])
-            for target_household in household_ca_ids
-        ]
+    def bulk_get_households(cls, household_list):
+        """Get associated households."""
+        household_entries = (household["household_ca_id"]
+                             for household in household_list)
+        return Household.objects.filter(
+            Q(household_ca_id__in=household_entries)).all()
 
 
 class TargetFilter(models.Model):
@@ -77,11 +84,13 @@ class TargetFilter(models.Model):
 
     intake_group = models.CharField(max_length=_MAX_LEN, null=False)
     sex = models.CharField(max_length=2, choices=_GENDER_CHOICES)
-    age = models.IntegerField()
-    school_distance_min = models.IntegerField()
-    school_distance_max = models.IntegerField()
-    num_individuals_household_min = models.IntegerField()
-    num_individuals_household_max = models.IntegerField()
+    age_min = models.IntegerField(max_length=_MAX_LEN, null=True)
+    age_max = models.IntegerField(max_length=_MAX_LEN, null=True)
+    school_distance_min = models.IntegerField(max_length=_MAX_LEN, null=True)
+    school_distance_max = models.IntegerField(max_length=_MAX_LEN, null=True)
+    num_individuals_min = models.IntegerField(max_length=_MAX_LEN, null=True)
+    num_individuals_max = models.IntegerField(max_length=_MAX_LEN, null=True)
+    school_distance_range = get_integer_range()
     target_population = models.ForeignKey("TargetPopulation",
                                           related_name="target_filters",
                                           on_delete=models.PROTECT,
