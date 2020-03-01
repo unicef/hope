e03d6dcdb4fad2e47a3308ed9a29b51d7e15b2a9
Akul Mathur <akul.mathur@tivix.com>
2020-03-01 20:28:49 +0530

    added fields for mutations, status and last_edited_at

--- backend/hct_mis_api/apps/targeting/mutations.py
+++ backend/hct_mis_api/apps/targeting/mutations.py
@@ -9,11 +9,12 @@ class TargetFilterInput(graphene.InputObjectType):
     """Defines filtering attributes."""
     intake_group = graphene.String()
     sex = graphene.String()
-    age = graphene.Int()
+    age_min = graphene.Int()
+    age_max = graphene.Int()
     school_distance_min = graphene.Int()
     school_distance_max = graphene.Int()
-    num_individuals_household_min = graphene.Int()
-    num_individuals_household_max = graphene.Int()
+    num_individuals_min = graphene.Int()
+    num_individuals_max = graphene.Int()
 
 
 class TargetFilterListInput(graphene.InputObjectType):
@@ -21,17 +22,25 @@ class TargetFilterListInput(graphene.InputObjectType):
     target_filters = graphene.List(TargetFilterInput, required=True)
 
 
-class TargetHouseHoldInput(graphene.InputObjectType):
+class TargetHouseholdInput(graphene.InputObjectType):
     """Target Household Entries attributes."""
     household_ca_id = graphene.String()
+    # TODO(codecakes): modify based on what admin level is.
+    # admin_level = graphene.String()
+
+
+class TargetHouseHoldList(graphene.InputObjectType):
+    """List of TargetHouseholdInput entries."""
+    household_list = graphene.List(TargetHouseholdInput, required=True)
 
 
 class CreateTargetPopulationInput(graphene.InputObjectType):
     """All attribute inputs to create a new entry."""
     target_population_name = graphene.String()
-    target_filter_rules = graphene.InputField(TargetFilterListInput,
-                                              required=True)
-    target_household = graphene.InputField(TargetHouseHoldInput, required=True)
+    last_edited_at = graphene.DateTime()
+    status = graphene.String()
+    target_filter_rules = graphene.InputField(TargetFilterListInput)
+    target_households = graphene.InputField(TargetHouseHoldList)
 
 
 class CreateTarget(graphene.relay.ClientIDMutation):
@@ -43,36 +52,28 @@ class CreateTarget(graphene.relay.ClientIDMutation):
     @staticmethod
     @is_authenticated
     def mutate_and_get_payload(self, root, info, **target_population_data):
-        num_individuals_household_min = float('inf')
-        num_individuals_household_max = float('-inf')
 
-        households = TargetPopulation.bulk_add_households(
-            target_population_data["target_household"])
+        # get all household models by conditional attributes.
+        households = TargetPopulation.bulk_get_households(
+            target_population_data["target_households"])
+        # create a new target population entry.
         target = TargetPopulation.objects.create(
             name=target_population_data["target_population_name"],
+            status = target_population_data["status"],
+            last_edited_at = target_population_data["last_edited_at"],
             created_by=info.context.user)
+        # add associated household results to target entry.
         target.households.add(*households)
 
-        for target_filer in (target_population_data["target_filter_rules"]
-                             ["target_filters"]):
+        for target_filer_rule in (
+                target_population_data["target_filter_rules"]):
+            target_filer = target_filer_rule["target_filters"]
             # add TargetFilter models to a target.
             target_filter_model = target_models.TargetFilter.objects.create(
                 **target_filer)
             target_filter_model.target_population = target
             target_filter_model.save()
 
-            # get min and max family size from all households.
-            num_individuals_household_min = min(
-                num_individuals_household_min,
-                target_filer['num_individuals_household_min'])
-            num_individuals_household_max = max(
-                num_individuals_household_max,
-                target_filer['num_individuals_household_max'])
-
-        num_individuals_household = target_models.get_serialized_range(
-            min_range=num_individuals_household_min,
-            max_range=num_individuals_household_max)
-        target.num_individuals_household = num_individuals_household
         target.save()
 
         return CreateTarget(target)
