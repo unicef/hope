920960a18891a701332cac55510fe09e21da3e99
Akul Mathur <akul.mathur@tivix.com>
2020-03-03 19:33:23 +0530

    fixtures for models and api tests query for filter. removed

--- backend/hct_mis_api/apps/targeting/fixtures.py
+++ backend/hct_mis_api/apps/targeting/fixtures.py
@@ -1,9 +1,10 @@
-import json
+import datetime as dt
 
 import factory
-
 from account.fixtures import UserFactory
+from factory import fuzzy
 from household.fixtures import HouseholdFactory
+from targeting.models import TargetFilter
 from targeting.models import TargetPopulation
 
 
@@ -11,9 +12,29 @@ class TargetPopulationFactory(factory.DjangoModelFactory):
     class Meta:
         model = TargetPopulation
 
-    name = factory.Faker(
-        "sentence", nb_words=6, variable_nb_words=True, ext_word_list=None,
-    )
+    name = factory.LazyAttribute(lambda: "sentence")
     created_by = factory.SubFactory(UserFactory)
-    rules = json.loads('{"example": 123}')
+    last_edited_at = fuzzy.FuzzyDateTime(dt.datetime.utcnow().astimezone())
+    status = factory.fuzzy.FuzzyChoice(
+        TargetPopulation.STATE_CHOICES,
+        getter=lambda x: x[0],
+    )
     households = factory.RelatedFactory(HouseholdFactory)
+
+
+class TargetFilterFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = TargetFilter
+
+    intake_group = factory.LazyAttribute(lambda: "sentence")
+    sex = factory.fuzzy.FuzzyChoice(
+        TargetFilter.GENDER_CHOICES,
+        getter=lambda x: x[0],
+    )
+    age_min = factory.LazyAttribute(lambda: 1)
+    age_max = factory.LazyAttribute(lambda: 200)
+    school_distance_min = factory.LazyAttribute(lambda: 1)
+    school_distance_max = factory.LazyAttribute(lambda: 20)
+    num_individuals_min = factory.LazyAttribute(lambda: 1)
+    num_individuals_max = factory.LazyAttribute(lambda: 50)
+    target_population = factory.SubFactory(TargetPopulationFactory)
