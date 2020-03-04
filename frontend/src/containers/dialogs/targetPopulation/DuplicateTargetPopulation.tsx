c01fd543a872018e04c65354339761a66ef373aa
Marcin <skorekmarcin@gmail.com>
2020-03-04 14:04:20 +0100

    Add duplicate and delete modal

---
+++ frontend/src/containers/dialogs/targetPopulation/DuplicateTargetPopulation.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from '@material-ui/core';
+import styled from 'styled-components';
+
+export interface FinalizeTargetPopulationPropTypes {
+  open: boolean;
+  setOpen: Function;
+};
+
+const DialogTitleWrapper = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+`;
+
+const DialogFooter = styled.div`
+  padding: 12px 16px;
+  margin: 0;
+  border-top: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+  text-align: right;
+`;
+
+const DialogDescription = styled.div`
+  margin: 20px 0;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.54);
+`;
+
+export function DuplicateTargetPopulation({ open, setOpen }) {
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      scroll='paper'
+      aria-labelledby='form-dialog-title'
+    >
+      <DialogTitleWrapper>
+        <DialogTitle id='scroll-dialog-title'>
+          <Typography variant='h6'>Duplicate Target Population</Typography>
+        </DialogTitle>
+      </DialogTitleWrapper>
+      <DialogContent>
+        <DialogDescription>
+          Are you sure you want to duplicate this Target Population?
+        </DialogDescription>
+      </DialogContent>
+      <DialogFooter>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>CANCEL</Button>
+          <Button type='submit' color='primary' variant='contained'>
+            Duplicate
+          </Button>
+        </DialogActions>
+      </DialogFooter>
+    </Dialog>
+  );
+}
