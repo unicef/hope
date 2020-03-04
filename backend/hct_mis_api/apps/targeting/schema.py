7b6a6855a9541913e027133cb7b2fd3711a6f724
akurnya <akulmat@protonmail.com>
2020-03-04 16:52:28 +0530

    no mutations for this feature. refactoring/styling based on black and PR reviews. changes to how Q filtering is done.

--- backend/hct_mis_api/apps/targeting/schema.py
+++ backend/hct_mis_api/apps/targeting/schema.py
@@ -1,11 +1,13 @@
 import datetime as dt
 import functools
 import json
+import operator
 
 import django_filters
 import graphene
 import targeting.models as target_models
 from core.schema import ExtendedConnection
+from django.db.models import Q
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
@@ -16,7 +18,7 @@ from registration_data.models import RegistrationDataImport
 class IntakeGroupType(DjangoObjectType):
     class Meta:
         model = RegistrationDataImport
-        fields = ("name", )
+        fields = ("name",)
 
 
 class HouseHoldType(DjangoObjectType):
@@ -37,27 +39,35 @@ class TargetPopulationFilter(django_filters.FilterSet):
 
     Loads associated entries for Households and TargetFilters.
     """
-    name = django_filters.CharFilter(field_name="name",
-                                     lookup_expr="icontains")
+
+    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
     created_by_name = django_filters.CharFilter(
-        field_name="created_by", method='filter_created_by_name')
+        field_name="created_by", method="filter_created_by_name"
+    )
     num_individuals_min = django_filters.NumberFilter(
-        field_name="target_filters__num_individuals_min", lookup_expr="gte")
+        field_name="target_filters__num_individuals_min", lookup_expr="gte"
+    )
     num_individuals_max = django_filters.NumberFilter(
-        field_name="target_filters__num_individuals_max", lookup_expr="lte")
+        field_name="target_filters__num_individuals_max", lookup_expr="lte"
+    )
 
     # TODO(codecakes): waiting on dist to school and adminlevel clarification.
 
     @staticmethod
     def filter_created_by_name(queryset, model_field, value):
         """Gets full name of the associated user from query."""
-        return queryset.filter(**{
-            f'{model_field}__first_name__icontains': value,
-        }).filter(**{
-            f'{model_field}__last_name__icontains': value,
-        }).filter(**{
-            f'{model_field}__full_name__icontains': value,
-        })
+        first_name_query = {
+            f"{model_field}__first_name__icontains": value,
+        }
+        last_name_query = {
+            f"{model_field}__last_name__icontains": value,
+        }
+        full_name_query = {
+            f"{model_field}__full_name__icontains": value,
+        }
+        return queryset.filter(
+            Q(**first_name_query) | Q(**last_name_query) | Q(full_name_query)
+        )
 
     class Meta:
         model = target_models.TargetPopulation
@@ -73,22 +83,23 @@ class TargetPopulationFilter(django_filters.FilterSet):
 
         filter_overrides = {
             target_models.IntegerRangeField: {
-                'filter_class': django_filters.NumericRangeFilter,
+                "filter_class": django_filters.NumericRangeFilter,
             },
             target_models.models.DateTimeField: {
-                'filter_class': django_filters.DateTimeFilter,
+                "filter_class": django_filters.DateTimeFilter,
             },
         }
 
     # TODO(codecakes): how to order?
-    order_by = django_filters.OrderingFilter(fields=("created_at", ))
+    order_by = django_filters.OrderingFilter(fields=("created_at",))
 
 
 class TargetPopulationNode(DjangoObjectType):
     """Defines an individual target population record."""
+
     class Meta:
         model = target_models.TargetPopulation
-        interfaces = (relay.Node, )
+        interfaces = (relay.Node,)
         connection_class = ExtendedConnection
         filterset_class = TargetPopulationFilter
 
@@ -100,7 +111,8 @@ class Query(graphene.ObjectType):
     target_filters = graphene.List(
         HouseHoldType,
         serialized_list=graphene.String(),
-        description="json dump of filters containing key value pairs.")
+        description="json dump of filters containing key value pairs.",
+    )
 
     def resolve_target_filters(self, info, serialized_list):
         """Resolver for target_filters.
@@ -111,10 +123,9 @@ class Query(graphene.ObjectType):
         Arguments in serialized_list are of type.
         """
         filter_lists = json.loads(serialized_list)
-        queryset = functools.reduce(
-            lambda f_1, f_2: f_1 | f_2,
-            Query.get_households_from_filter(filter_lists))
-        return queryset.all()
+        return functools.reduce(
+            operator.or_, Query.get_households_from_filter(filter_lists),
+        )
 
     @staticmethod
     def get_households_from_filter(filter_lists):
@@ -125,18 +136,14 @@ class Query(graphene.ObjectType):
             year_max = today.year - filter_list["age_max"]
             dob_min = dt.date(year_min, 1, 1)
             dob_max = dt.date(year_max, 1, 1)
-            yield Household.objects.filter(
-                **{
-                    "head_of_household__sex":
-                        filter_list["sex"],
-                    "family_size__gte":
-                        filter_list["num_individuals_min"],
-                    "family_size__lte":
-                        filter_list["num_individuals_max"],
-                    "head_of_household__dob__gte":
-                        dob_min,
-                    "registration_data_import_id__name":
-                        filter_list["intake_group"],
-                }).filter(**{
+            search_filters = {
+                "head_of_household__sex": filter_list["sex"],
+                "family_size__gte": filter_list["num_individuals_min"],
+                "family_size__lte": filter_list["num_individuals_max"],
+                "head_of_household__dob__gte": dob_min,
                 "head_of_household__dob__lte": dob_max,
-            })
+                "registration_data_import_id__name": filter_list[
+                    "intake_group"
+                ],
+            }
+            yield Household.objects.filter(**search_filters)
