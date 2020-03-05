a2154a1b691f0b82670c13170c30b6c057a82f25
Wojciech Nosal <wojciech.nosal@tivix.com>
2020-03-05 12:49:39 +0100

    Filtering Household and Individual by Programme (#105)

--- backend/hct_mis_api/apps/household/tests/test_household_query.py
+++ backend/hct_mis_api/apps/household/tests/test_household_query.py
@@ -1,6 +1,10 @@
+from django.core.management import call_command
+
 from account.fixtures import UserFactory
 from core.base_test_case import APITestCase
+from core.models import BusinessArea
 from household.fixtures import HouseholdFactory
+from program.fixtures import ProgramFactory
 
 
 class TestHouseholdQuery(APITestCase):
@@ -60,6 +64,27 @@ class TestHouseholdQuery(APITestCase):
       }
     }
     """
+    ALL_HOUSEHOLD_FILTER_PROGRAMME_QUERY = """
+    query AllHouseholds{
+      allHouseholds(programme: "Test program TWO") {
+        edges {
+          node {
+            familySize
+            nationality
+            householdCaId
+            address
+            programs { 
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """
     HOUSEHOLD_QUERY = """
     query Household($id: ID!) {
       household(id: $id) {
@@ -73,17 +98,30 @@ class TestHouseholdQuery(APITestCase):
 
     def setUp(self):
         super().setUp()
+        call_command("loadbusinessareas")
         self.user = UserFactory.create()
         family_sizes_list = (2, 4, 5, 1, 3, 11, 14)
-        self.households = [
-            HouseholdFactory(
+        program_one = ProgramFactory(
+            name="Test program ONE", business_area=BusinessArea.objects.first(),
+        )
+        program_two = ProgramFactory(
+            name="Test program TWO", business_area=BusinessArea.objects.first(),
+        )
+
+        self.households = []
+        for index, family_size in enumerate(family_sizes_list):
+            household = HouseholdFactory(
                 family_size=family_size,
                 address="Lorem Ipsum",
                 nationality="PL",
                 household_ca_id="123-123-123",
             )
-            for family_size in family_sizes_list
-        ]
+            if index % 2:
+                household.programs.add(program_one)
+            else:
+                household.programs.add(program_two)
+
+            self.households.append(household)
 
     def test_household_query_all(self):
         self.snapshot_graphql_request(
@@ -106,6 +144,12 @@ class TestHouseholdQuery(APITestCase):
     def test_household_query_all_max(self):
         self.snapshot_graphql_request(
             request_string=self.ALL_HOUSEHOLD_QUERY_MAX,
+            context={"user": self.user},
+        )
+
+    def test_household_filter_by_programme(self):
+        self.snapshot_graphql_request(
+            request_string=self.ALL_HOUSEHOLD_FILTER_PROGRAMME_QUERY,
             context={"user": self.user},
         )
 
