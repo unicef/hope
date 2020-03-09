3c9fa3576685309cea116b750c50691668e92d57
codecakes <akul.mathur@tivix.com>
2020-03-09 20:29:32 +0530

    changes to get fake data instead of fixed value in fixture. updated schema changes.

--- backend/hct_mis_api/apps/targeting/fixtures.py
+++ backend/hct_mis_api/apps/targeting/fixtures.py
@@ -8,19 +8,40 @@ from household.fixtures import HouseholdFactory
 from targeting.models import TargetPopulation
 from targeting.models import TargetRule
 
+_NOW_DT = dt.datetime.utcnow().astimezone()
+
+
+def get_fuzzy_dates_from_now(delta_days, last_edited=0):
+    """Creates a datetime from Fuzzy Date Time.
+    args:
+        delta_days: int, how many days to rewind from current timestamp.
+
+    returns:
+        FuzzyDateTime.
+    """
+    past_createstamp = _NOW_DT - dt.timedelta(days=delta_days)
+    last_edited = past_createstamp + dt.timedelta(days=last_edited)
+    return fuzzy.FuzzyDateTime(start_dt=past_createstamp, end_dt=last_edited)
+
 
 class TargetPopulationFactory(factory.DjangoModelFactory):
+    _DELTA_DAYS = 365
+    _LAST_EDITED = 10
+
     class Meta:
         model = TargetPopulation
 
-    name = "sentence"
+    name = factory.Faker(
+        "sentence", nb_words=6, variable_nb_words=True, ext_word_list=None,
+    )
     created_by = factory.SubFactory(UserFactory)
-    last_edited_at = fuzzy.FuzzyDateTime(dt.datetime.utcnow().astimezone())
+    created_at = get_fuzzy_dates_from_now(_DELTA_DAYS, _LAST_EDITED)
+    last_edited_at = get_fuzzy_dates_from_now(_DELTA_DAYS, _LAST_EDITED)
     status = factory.fuzzy.FuzzyChoice(
         TargetPopulation.STATE_CHOICES, getter=lambda x: x[0],
     )
-    total_households = 10
-    total_family_size = 20
+    total_households = factory.fuzzy.FuzzyInteger(5, 500)
+    total_family_size = factory.fuzzy.FuzzyInteger(2, 20)
 
     @factory.post_generation
     def households(self, create, extracted, **kwargs):
