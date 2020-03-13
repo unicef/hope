748033dea9987dcd4a56f928acd40f3db56d6f62
Marcin <skorekmarcin@gmail.com>
2020-03-13 13:10:48 +0100

    Add target populatio details

--- frontend/src/containers/pages/CreateTargetPopulation.tsx
+++ frontend/src/containers/pages/CreateTargetPopulation.tsx
@@ -7,6 +7,7 @@ import { PageHeader } from '../../components/PageHeader';
 import { TargetingCriteria } from '../../components/TargetPopulation/TargetingCriteria';
 import { FormikTextField } from '../../shared/Formik/FormikTextField';
 import { Results } from '../../components/TargetPopulation/Results';
+import { EditTargetPopulation } from '../../components/TargetPopulation/EditTargetPopulation';
 
 const PaperContainer = styled(Paper)`
   display: flex;
@@ -45,55 +46,6 @@ export function CreateTargetPopulation() {
   const { t } = useTranslation();
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ submitForm, values }) => (
-        <Form>
-          <PageHeader
-            title={
-              <Field
-                name='name'
-                label='Programme Name'
-                type='text'
-                fullWidth
-                required
-                component={FormikTextField}
-              />
-            }
-          >
-            <Button
-              variant='contained'
-              color='primary'
-              type='submit'
-              onClick={submitForm}
-            >
-              Save
-            </Button>
-          </PageHeader>
-          <FieldArray
-            name='criterias'
-            render={(arrayHelpers) => (
-              <TargetingCriteria
-                helpers={arrayHelpers}
-                criterias={values.criterias}
-                isEdit
-              />
-            )}
-          />
-          <Results />
-          <PaperContainer>
-            <Title>
-              <Typography variant='h6'>
-                Target Population Entries (Households)
-              </Typography>
-            </Title>
-          </PaperContainer>
-        </Form>
-      )}
-    </Formik>
+    <EditTargetPopulation />
   );
 }
