af6fbc7353a2d61e8c6ffb3c0eec637d48beb01c
Jan Romaniak <romaniakjan@gmail.com>
2020-03-16 15:28:16 +0100

    autocomplete and fixes

--- frontend/src/containers/registration/list/RegistrationFilter.tsx
+++ frontend/src/containers/registration/list/RegistrationFilter.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import {
-  TextField,
-  InputAdornment,
-  Select,
-  Menu,
-  MenuItem,
   FormControl,
+  InputAdornment,
   InputLabel,
-  createStyles,
-  makeStyles,
+  MenuItem,
+  Select,
+  TextField,
 } from '@material-ui/core';
+
+import get from 'lodash/get';
 import SearchIcon from '@material-ui/icons/Search';
-import GroupIcon from '@material-ui/icons/Group';
-import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
+import PersonIcon from '@material-ui/icons/Person';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { DatePicker } from '@material-ui/pickers';
 import CalendarTodayRoundedIcon from '@material-ui/icons/CalendarTodayRounded';
-import WcIcon from '@material-ui/icons/Wc';
 import {
+  AllUsersQuery,
   useAllUsersQuery,
   useRegistrationChoicesQuery,
 } from '../../../__generated__/graphql';
-import { decodeIdString } from '../../../utils/utils';
+import { useDebounce } from '../../../hooks/useDebounce';
 
 const Container = styled.div`
   display: flex;
@@ -149,7 +149,76 @@ const StyledFormControl = styled(FormControl)`
   border-bottom: 0;
 `;
 
+const StyledAutocomplete = styled(Autocomplete)`
+  width: 232px;
+  .MuiFormControl-marginDense {
+    margin-top: 4px;
+  }
+`;
 
+function AutoComplete({ value, onChange, onInputTextChange, inputValue }) {
+  const [open, setOpen] = React.useState(false);
+  const debouncedInputText = useDebounce(inputValue, 500);
+  const [newValue, setNewValue] = useState();
+  const { data, loading } = useAllUsersQuery({
+    variables: {
+      first: 100,
+      fullName: debouncedInputText,
+    },
+  });
+  useEffect(() => setNewValue(value), [data, value]);
+  return (
+    <StyledAutocomplete<AllUsersQuery['allUsers']['edges'][number]>
+      open={open}
+      filterOptions={(options1) => options1}
+      onChange={onChange}
+      value={newValue}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      getOptionSelected={(option, value1) => {
+        return value1 === option.node.id;
+      }}
+      getOptionLabel={(option) => {
+        if (!option.node) {
+          return '';
+        }
+        return `${option.node.firstName} ${option.node.lastName}`;
+      }}
+      options={get(data, 'allUsers.edges', [])}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label='Imported By'
+          variant='outlined'
+          margin='dense'
+          value={inputValue}
+          onChange={(e) => onInputTextChange(e.target.value)}
+          InputProps={{
+            ...params.InputProps,
+            startAdornment: (
+              <InputAdornment position='start'>
+                <PersonIcon style={{ color: '#5f6368' }} />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <>
+                {loading ? (
+                  <CircularProgress color='inherit' size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}
 
 interface RegistrationFiltersProps {
   onFilterChange;
@@ -162,19 +231,16 @@ export function RegistrationFilters({
   const handleFilterChange = (e, name) =>
     onFilterChange({ ...filter, [name]: e.target.value });
   const importedByLabel = React.useRef<HTMLLabelElement>(null);
-  const [importedByLabelWidth, setImportedByLabelWidth] = React.useState(0);
   const statusLabel = React.useRef<HTMLLabelElement>(null);
   const [statusLabelWidth, setStatusLabelWidth] = React.useState(0);
-  const { data: allUsersData } = useAllUsersQuery();
   const { data: registrationChoicesData } = useRegistrationChoicesQuery();
   React.useEffect(() => {
-    if (!importedByLabel.current) {
+    if (!statusLabel.current) {
       return;
     }
-    setImportedByLabelWidth(importedByLabel.current.offsetWidth);
     setStatusLabelWidth(statusLabel.current.offsetWidth);
-  }, [importedByLabel, statusLabel, allUsersData]);
-  if (!allUsersData || !registrationChoicesData) {
+  }, [importedByLabel, statusLabel]);
+  if (!registrationChoicesData) {
     return null;
   }
   return (
@@ -209,25 +275,39 @@ export function RegistrationFilters({
           ),
         }}
       />
-      <StyledFormControl variant='outlined' margin='dense'>
-        <InputLabel ref={importedByLabel}>Imported By</InputLabel>
-        <Select
-          value={filter.importedBy || ''}
-          labelWidth={importedByLabelWidth}
-          onChange={(e) => handleFilterChange(e, 'importedBy')}
-        >
-          <MenuItem value=''>
-            <em>None</em>
-          </MenuItem>
-          {allUsersData.allUsers.edges.map((edge) => {
-            return (
-              <MenuItem key={edge.node.id} value={decodeIdString(edge.node.id)}>
-                {edge.node.firstName} {edge.node.lastName}
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </StyledFormControl>
+      <AutoComplete
+        onInputTextChange={(value) =>
+          onFilterChange({ ...filter, userInputValue: value })
+        }
+        inputValue={filter.userInputValue}
+        onChange={(e, option) => {
+          if (!option ) {
+            onFilterChange({ ...filter, importedBy: undefined });
+            return;
+          }
+          onFilterChange({ ...filter, importedBy: option.node.id });
+        }}
+        value={filter.importedBy}
+      />
+      {/*<StyledFormControl variant='outlined' margin='dense'>*/}
+      {/*  <InputLabel ref={importedByLabel}>Imported By</InputLabel>*/}
+      {/*  <Select*/}
+      {/*    value={filter.importedBy || ''}*/}
+      {/*    labelWidth={importedByLabelWidth}*/}
+      {/*    onChange={(e) => handleFilterChange(e, 'importedBy')}*/}
+      {/*  >*/}
+      {/*    <MenuItem value=''>*/}
+      {/*      <em>None</em>*/}
+      {/*    </MenuItem>*/}
+      {/*    {allUsersData.allUsers.edges.map((edge) => {*/}
+      {/*      return (*/}
+      {/*        <MenuItem key={edge.node.id} value={decodeIdString(edge.node.id)}>*/}
+      {/*          {edge.node.firstName} {edge.node.lastName}*/}
+      {/*        </MenuItem>*/}
+      {/*      );*/}
+      {/*    })}*/}
+      {/*  </Select>*/}
+      {/*</StyledFormControl>*/}
       <StyledFormControl variant='outlined' margin='dense'>
         <InputLabel ref={statusLabel}>Status</InputLabel>
         <Select
