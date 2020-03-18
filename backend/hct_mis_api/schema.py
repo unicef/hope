853caa56328b27e3904a1b6009425de95de49e6e
codecakes <akul.mathur@tivix.com>
2020-03-18 23:46:22 +0530

    GraphQL mutation for Change Target population state (in progress -> finalized) Once Finalized, throws error.

--- backend/hct_mis_api/schema.py
+++ backend/hct_mis_api/schema.py
@@ -1,17 +1,13 @@
-import graphene
-from graphene_django.debug import DjangoDebug
-
 import account.schema
 import core.schema
+import graphene
 import household.schema
 import payment.schema
-import program.mutations
 import program.schema
 import registration_data.schema
-import registration_datahub.schema
 import registration_datahub.mutations
-import targeting.schema
 import targeting.mutations
+from graphene_django.debug import DjangoDebug
 
 
 class Query(
@@ -31,6 +27,7 @@ class Query(
 class Mutations(
     registration_datahub.mutations.Mutations,
     program.mutations.Mutations,
+    targeting.mutations.Mutations,
     graphene.ObjectType,
 ):
     pass
