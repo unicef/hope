50d7661a4694211ee6c6f9c8b5c6e083941ac181
Wojciech Nosal <wojciech.nosal@tivix.com>
2020-03-19 14:32:25 +0100

    Approve + Unapprove mutation, Merge into population in progress (#125)

--- backend/hct_mis_api/apps/registration_datahub/tests/snapshots/snap_test_registration_data_import_datahub_mutations.py
+++ backend/hct_mis_api/apps/registration_datahub/tests/snapshots/snap_test_registration_data_import_datahub_mutations.py
@@ -12,9 +12,175 @@ snapshots['TestRegistrationDataImportDatahubQuery::test_registration_data_import
         'uploadImportDataXlsxFile': {
             'importData': {
                 'numberOfHouseholds': 0,
-                'numberOfIndividuals': 0,
-                'xlsxFile': 'Registration_Data_Import_XLS_Template.xlsx'
+                'numberOfIndividuals': 0
             }
         }
     }
+}
+
+snapshots['TestRegistrationDataImportDatahubQuery::test_registration_data_import_create 1'] = {
+    'data': {
+        'createRegistrationDataImport': {
+            'registrationDataImport': {
+                'name': 'New Import of Data 123',
+                'numberOfHouseholds': 500,
+                'numberOfIndividuals': 1000,
+                'status': 'IN_REVIEW'
+            }
+        }
+    }
+}
+
+snapshots['TestRegistrationDataImportDatahubQuery::test_approve_registration_data_import 1'] = {
+    'data': {
+        'approveRegistrationDataImport': {
+            'registrationDataImport': {
+                'status': 'APPROVED'
+            }
+        }
+    }
+}
+
+snapshots['TestRegistrationDataImportDatahubQuery::test_approve_registration_data_import_wrong_initial_status 1'] = {
+    'data': {
+        'approveRegistrationDataImport': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 7,
+                    'line': 3
+                }
+            ],
+            'message': 'Only In Review Registration Data Import can be Approved',
+            'path': [
+                'approveRegistrationDataImport'
+            ]
+        }
+    ]
+}
+
+snapshots['TestRegistrationDataImportDatahubQuery::test_merge_registration_data_import 1'] = {
+    'data': {
+        'mergeRegistrationDataImport': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 7,
+                    'line': 3
+                }
+            ],
+            'message': 'Cannot assign "\'Lorem Ipsum\'": "Household.location" must be a "Location" instance.',
+            'path': [
+                'mergeRegistrationDataImport'
+            ]
+        }
+    ]
+}
+
+snapshots['TestRegistrationDataImportDatahubMutations::test_merge_registration_data_import 1'] = {
+    'data': {
+        'mergeRegistrationDataImport': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 7,
+                    'line': 3
+                }
+            ],
+            'message': '',
+            'path': [
+                'mergeRegistrationDataImport'
+            ]
+        }
+    ]
+}
+
+snapshots['TestRegistrationDataImportDatahubMutations::test_approve_registration_data_import 1'] = {
+    'data': {
+        'approveRegistrationDataImport': {
+            'registrationDataImport': {
+                'status': 'APPROVED'
+            }
+        }
+    }
+}
+
+snapshots['TestRegistrationDataImportDatahubMutations::test_approve_registration_data_import_wrong_initial_status 1'] = {
+    'data': {
+        'approveRegistrationDataImport': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 7,
+                    'line': 3
+                }
+            ],
+            'message': 'Only In Review Registration Data Import can be Approved',
+            'path': [
+                'approveRegistrationDataImport'
+            ]
+        }
+    ]
+}
+
+snapshots['TestRegistrationDataImportDatahubMutations::test_registration_data_import_create 1'] = {
+    'data': {
+        'createRegistrationDataImport': {
+            'registrationDataImport': {
+                'name': 'New Import of Data 123',
+                'numberOfHouseholds': 500,
+                'numberOfIndividuals': 1000,
+                'status': 'IN_REVIEW'
+            }
+        }
+    }
+}
+
+snapshots['TestRegistrationDataImportDatahubMutations::test_registration_data_import_datahub_upload 1'] = {
+    'data': {
+        'uploadImportDataXlsxFile': {
+            'importData': {
+                'numberOfHouseholds': 0,
+                'numberOfIndividuals': 0
+            }
+        }
+    }
+}
+
+snapshots['TestRegistrationDataImportDatahubMutations::test_unapprove_registration_data_import 1'] = {
+    'data': {
+        'unapproveRegistrationDataImport': {
+            'registrationDataImport': {
+                'status': 'IN_REVIEW'
+            }
+        }
+    }
+}
+
+snapshots['TestRegistrationDataImportDatahubMutations::test_unapprove_registration_data_import_wrong_initial_status 1'] = {
+    'data': {
+        'unapproveRegistrationDataImport': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 7,
+                    'line': 3
+                }
+            ],
+            'message': 'Only Approved Registration Data Import can be Unapproved',
+            'path': [
+                'unapproveRegistrationDataImport'
+            ]
+        }
+    ]
 }
