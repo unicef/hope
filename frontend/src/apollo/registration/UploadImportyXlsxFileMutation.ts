2d1d7d777b4f4d025dd5de692d39489157408634
Jan Romaniak <romaniakjan@gmail.com>
2020-03-24 11:33:58 +0100

    tags for rdi

--- frontend/src/apollo/registration/UploadImportyXlsxFileMutation.ts
+++ frontend/src/apollo/registration/UploadImportyXlsxFileMutation.ts
@@ -3,16 +3,17 @@ import { gql } from 'apollo-boost';
 export const UPLOAD_IMPORT_XSLSX_MUTATION = gql`
   mutation UploadImportDataXlsxFile($file: Upload!) {
     uploadImportDataXlsxFile(file: $file) {
+      errors {
+        header
+        message
+        rowNumber
+      }
       importData {
         id
         numberOfIndividuals
         numberOfHouseholds
         registrationDataImport {
-          edges {
-            node {
-              id
-            }
-          }
+          id
         }
       }
     }
