f3717c11ac63f1f37bea341aa18fbcc327ac669e
Jan Romaniak <romaniakjan@gmail.com>
2020-04-01 14:57:46 +0200

    signals tested and fixed

--- backend/hct_mis_api/apps/targeting/tests/test_target_query.py
+++ backend/hct_mis_api/apps/targeting/tests/test_target_query.py
@@ -2,161 +2,167 @@ import datetime as dt
 
 from account.fixtures import UserFactory
 from core.base_test_case import APITestCase
-from household.fixtures import HouseholdFactory
+from household.fixtures import HouseholdFactory, IndividualFactory
 from targeting.fixtures import TargetPopulationFactory
+from targeting.models import (
+    TargetingCriteria,
+    TargetingCriteriaRule,
+    TargetingCriteriaRuleFilter,
+    TargetPopulation,
+    HouseholdSelection,
+)
 
 
 class TestTargetPopulationQuery(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.ALL_TARGET_POPULATION_QUERY = """
-        query AllTargetPopulation {
-            allTargetPopulation {
-                edges {
-                    node {
-                         name
-                         status
-                         createdBy {
-                            firstName
-                            lastName
-                         }
-                         createdAt
-                         lastEditedAt
-                         candidateListTargetingCriteria{
-                          rules{
-                            id
-                            filters{
-                              id
-                                            comparisionMethod
-                              isFlexField
-                              fieldName
-                              arguments
-                            }
-                          }
-                        }
-                    }
-                }
-            }
-        }
-        """
-
-        cls.ALL_TARGET_POPULATION_NUM_INDIVIDUALS_QUERY = """
-        query AllTargetPopulation($numIndividualsMin: Int, $numIndividualsMax: Int) {
-            allTargetPopulation(numIndividualsMin: $numIndividualsMin, numIndividualsMax: $numIndividualsMax) {
-                edges {
-                    cursor
-                    node {
-                         name
-                         status
-                         createdBy {
-                            firstName
-                            lastName
-                         }
-                         createdAt
-                         lastEditedAt
-                         
-                    }
-                }
-            }
-        }
-        """
-
-        cls.TARGET_POPULATION_QUERY = """
-        query TargetPopulation($id: ID!) {
-            targetPopulation(id: $id) {
-                name
-                status
-                createdBy {
-                    firstName
-                    lastName
-                 }
-                createdAt
-                lastEditedAt
-                households {
+    ALL_TARGET_POPULATION_QUERY = """
+            query AllTargetPopulation {
+                allTargetPopulation {
                     edges {
                         node {
-                            householdCaId
-                            familySize
-                            address
-                            location {
-                                title
-                            }
-                            registrationDataImportId {
-                                name
-                            }
-                            headOfHousehold {
-                                fullName
+                             name
+                             status
+                            candidateListTotalHouseholds
+                            candidateListTotalIndividuals
+                            finalListTotalHouseholds
+                            finalListTotalIndividuals
+                             createdBy {
                                 firstName
                                 lastName
-                            }
+                             }
+                             createdAt
+                             updatedAt
                         }
                     }
                 }
             }
-        }
-        """
-        cls.user = UserFactory.create()
-        cls.last_edited = dt.datetime.utcnow().astimezone()
-        cls.households = HouseholdFactory.create_batch(5)
-        # cls.target_rules = TargetRuleFactory.create_batch(3)
-        targets_to_create = [
-            {
-                "name": "target_1",
-                "created_by": cls.user,
-                "last_edited_at": cls.last_edited,
-                "households": cls.households,
-                # "target_rules": cls.target_rules,
-            },
-            {
-                "name": "target_2",
-                "created_by": cls.user,
-                "last_edited_at": cls.last_edited,
-                "households": cls.households,
-                # "target_rules": cls.target_rules,
-            },
-        ]
-        # populate mock factory model
-        cls.targets = [
-            TargetPopulationFactory(**data) for data in targets_to_create
-        ]
-
-    def test_all_target_population_query(self):
-        self.snapshot_graphql_request(
-            request_string=self.ALL_TARGET_POPULATION_QUERY,
-            context={"user": self.user},
-        )
-
-    def test_all_target_population_num_individuals_query(self):
-        self.snapshot_graphql_request(
-            request_string=self.ALL_TARGET_POPULATION_NUM_INDIVIDUALS_QUERY,
-            context={"user": self.user},
-            variables={"numIndividualsMin": 1, "numIndividualsMax": 100},
-        )
-
-    def test_target_population_query(self):
-        self.snapshot_graphql_request(
-            request_string=self.TARGET_POPULATION_QUERY,
-            context={"user": self.user},
-            variables={
-                "id": self.id_to_base64(self.targets[0].id, "TargetPopulation")
-            },
-        )
-
-class TestTargetStatusTypesQuery(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.TARGET_STATUS_TYPES_QUERY = """
-        query TargetStatusTypes {
-          targetStatusTypes {
-            key
-            value
+            """
+    TARGET_POPULATION_QUERY = """
+       query TargetPopulation($id:ID!) {
+          targetPopulation(id:$id){
+            id
+            name
+            status
+            candidateListTotalHouseholds
+            candidateListTotalIndividuals
+            finalListTotalHouseholds
+            finalListTotalIndividuals
+            candidateListTargetingCriteria{
+              rules{
+                filters{
+                  id
+                  comparisionMethod
+                  fieldName
+                  isFlexField
+                  arguments
+                }
+              }
+            }
+            finalListTargetingCriteria{
+              rules{
+                filters{
+                  id
+                  comparisionMethod
+                  fieldName
+                  isFlexField
+                  arguments
+                }
+              }
+            }
           }
         }
-        """
-        cls.user = UserFactory.create()
+                """
 
-    def test_target_status_types_query(self):
+    @classmethod
+    def setUpTestData(cls):
+        cls.households = []
+        HouseholdFactory(
+            family_size=1, residence_status="CITIZEN",
+        )
+        household = HouseholdFactory(family_size=1, residence_status="CITIZEN",)
+        cls.households.append(household)
+        cls.household_family_size_1 = HouseholdFactory(
+            family_size=1, residence_status="IDP",
+        )
+        cls.households.append(cls.household_family_size_1)
+        cls.household_residence_status_citizen = cls.household_family_size_1
+        IndividualFactory(household=cls.household_family_size_1)
+        cls.household_residence_status_refugee = HouseholdFactory(
+            family_size=2, residence_status="REFUGEE",
+        )
+        cls.households.append(cls.household_residence_status_refugee)
+        cls.household_family_size_2 = cls.household_residence_status_refugee
+        IndividualFactory(household=cls.household_residence_status_refugee)
+        IndividualFactory(household=cls.household_residence_status_refugee)
+        cls.user = UserFactory.create()
+        targeting_criteria = cls.get_targeting_criteria_for_rule(
+            {
+                "field_name": "family_size",
+                "arguments": [2],
+                "comparision_method": "EQUALS",
+            }
+        )
+        cls.target_population_family_size_2 = TargetPopulation(
+            name="target_population_family_size_2",
+            created_by=cls.user,
+            final_list_targeting_criteria=targeting_criteria,
+            status="APPROVED",
+        )
+        cls.target_population_family_size_2.households.set(cls.households)
+        cls.target_population_family_size_2.save()
+        targeting_criteria = cls.get_targeting_criteria_for_rule(
+            {
+                "field_name": "residence_status",
+                "arguments": ["CITIZEN"],
+                "comparision_method": "EQUALS",
+            }
+        )
+        cls.target_population_residence_status = TargetPopulation(
+            name="target_population_residence_status",
+            created_by=cls.user,
+            final_list_targeting_criteria=targeting_criteria,
+            status="APPROVED",
+        )
+        cls.target_population_residence_status.households.set(cls.households)
+        cls.target_population_residence_status.save()
+        targeting_criteria = cls.get_targeting_criteria_for_rule(
+            {
+                "field_name": "family_size",
+                "arguments": [1],
+                "comparision_method": "EQUALS",
+            }
+        )
+        cls.target_population_family_size_1_finalized = TargetPopulation(
+            name="target_population_family_size_1_finalized",
+            created_by=cls.user,
+            final_list_targeting_criteria=targeting_criteria,
+            status="FINALIZED",
+        )
+        cls.target_population_family_size_1_finalized.save()
+        HouseholdSelection.objects.create(
+            household=cls.household_family_size_1,
+            final=True,
+            target_population=cls.target_population_family_size_1_finalized,
+        )
+
+    @staticmethod
+    def get_targeting_criteria_for_rule(rule_filter):
+        targeting_criteria = TargetingCriteria()
+        targeting_criteria.save()
+        rule = TargetingCriteriaRule(targeting_criteria=targeting_criteria)
+        rule.save()
+        rule_filter = TargetingCriteriaRuleFilter(
+            **rule_filter, targeting_criteria_rule=rule
+        )
+        rule_filter.save()
+        return targeting_criteria
+
+    def test_simple_all_targets_query(self):
         self.snapshot_graphql_request(
-            request_string=self.TARGET_STATUS_TYPES_QUERY,
-            context={"user": self.user},
+            request_string=TestTargetPopulationQuery.ALL_TARGET_POPULATION_QUERY,
+        )
+
+    def test_simple_target_query(self):
+        self.snapshot_graphql_request(
+            request_string=TestTargetPopulationQuery.TARGET_POPULATION_QUERY,
+            variables={"id": self.id_to_base64(self.target_population_family_size_1_finalized.id,'TargetPopulation')},
         )
