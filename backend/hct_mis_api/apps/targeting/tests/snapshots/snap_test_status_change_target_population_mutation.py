37015404a31365665e45b4a785b23ffa667175d3
Jan Romaniak <romaniakjan@gmail.com>
2020-04-02 13:11:39 +0200

    mutations for changing status tests

---
+++ backend/hct_mis_api/apps/targeting/tests/snapshots/snap_test_status_change_target_population_mutation.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestStatusChangeTargetPopulationMutation::test_unapprove_fail_target_population 1'] = {
+    'data': None,
+    'errors': [
+        {
+            'message': 'cannot unpack non-iterable NoneType object'
+        }
+    ]
+}
+
+snapshots['TestStatusChangeTargetPopulationMutation::test_approve_fail_target_population 1'] = {
+    'data': None,
+    'errors': [
+        {
+            'message': 'cannot unpack non-iterable NoneType object'
+        }
+    ]
+}
+
+snapshots['TestStatusChangeTargetPopulationMutation::test_approve_target_population 1'] = {
+    'data': {
+        'approveTargetPopulation': {
+            'targetPopulation': {
+                'households': {
+                    'edges': [
+                        {
+                            'node': {
+                                'familySize': 1,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        },
+                        {
+                            'node': {
+                                'familySize': 2,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        }
+                    ],
+                    'totalCount': 2
+                },
+                'status': 'APPROVED'
+            }
+        }
+    }
+}
+
+snapshots['TestStatusChangeTargetPopulationMutation::test_finalize_fail_target_population 1'] = {
+    'data': None,
+    'errors': [
+        {
+            'message': 'cannot unpack non-iterable NoneType object'
+        }
+    ]
+}
+
+snapshots['TestStatusChangeTargetPopulationMutation::test_finalize_target_population 1'] = {
+    'data': {
+        'finalizeTargetPopulation': {
+            'targetPopulation': {
+                'finalList': {
+                    'edges': [
+                        {
+                            'node': {
+                                'familySize': 1,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        },
+                        {
+                            'node': {
+                                'familySize': 2,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        }
+                    ]
+                },
+                'households': {
+                    'edges': [
+                        {
+                            'node': {
+                                'familySize': 1,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        },
+                        {
+                            'node': {
+                                'familySize': 2,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        }
+                    ],
+                    'totalCount': 2
+                },
+                'status': 'FINALIZED'
+            }
+        }
+    }
+}
+
+snapshots['TestStatusChangeTargetPopulationMutation::test_finalize_target_population_with_final_criteria 1'] = {
+    'data': {
+        'finalizeTargetPopulation': {
+            'targetPopulation': {
+                'finalList': {
+                    'edges': [
+                        {
+                            'node': {
+                                'familySize': 2,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        }
+                    ]
+                },
+                'households': {
+                    'edges': [
+                        {
+                            'node': {
+                                'familySize': 1,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        },
+                        {
+                            'node': {
+                                'familySize': 2,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        }
+                    ],
+                    'totalCount': 2
+                },
+                'status': 'FINALIZED'
+            }
+        }
+    }
+}
+
+snapshots['TestStatusChangeTargetPopulationMutation::test_unapprove_target_population 1'] = {
+    'data': {
+        'unapproveTargetPopulation': {
+            'targetPopulation': {
+                'households': {
+                    'edges': [
+                        {
+                            'node': {
+                                'familySize': 1,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        },
+                        {
+                            'node': {
+                                'familySize': 2,
+                                'residenceStatus': 'CITIZEN'
+                            }
+                        }
+                    ],
+                    'totalCount': 2
+                },
+                'status': 'DRAFT'
+            }
+        }
+    }
+}
