de9c5bc509b32c05355d736073a20504e82d0b22
Marcin <skorekmarcin@gmail.com>
2020-04-08 20:46:19 +0200

    Reduce fields in approve target population

--- frontend/src/apollo/mutations/ApproveTargetPopulation.ts
+++ frontend/src/apollo/mutations/ApproveTargetPopulation.ts
@@ -2,82 +2,8 @@ import { gql } from 'apollo-boost';
 export const ApproveTargetPopulation = gql`
   mutation ApproveTP($id: ID!, $programId: ID!) {
     approveTargetPopulation(id: $id, programId: $programId) {
-      id
-      name
-      status
-      households {
-        edges {
-          node {
-            id
-            headOfHousehold {
-              fullName
-            }
-            householdCaId
-            location {
-              title
-            }
-          }
-        }
-      }
-      candidateListTargetingCriteria {
-        targetPopulationCandidate {
-          createdBy {
-            firstName
-            lastName
-          }
-          program {
-            id
-            name
-          }
-        }
-        rules {
-          id
-          filters {
-            fieldName
-            isFlexField
-            arguments
-            comparisionMethod
-            fieldAttribute {
-              name
-              labelEn
-              type
-              choices {
-                value
-                labelEn
-              }
-            }
-          }
-        }
-      }
-      finalListTargetingCriteria {
-        targetPopulationFinal {
-          createdBy {
-            firstName
-            lastName
-          }
-          program {
-            id
-            name
-          }
-        }
-        rules {
-          id
-          filters {
-            fieldName
-            isFlexField
-            arguments
-            comparisionMethod
-            fieldAttribute {
-              name
-              labelEn
-              type
-              choices {
-                value
-                labelEn
-              }
-            }
-          }
-        }
+      targetPopulation {
+        status
       }
     }
   }
