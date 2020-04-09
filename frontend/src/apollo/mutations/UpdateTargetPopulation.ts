ca152ed527a908b13d71a2ecbbe782fb08894d23
Marcin <skorekmarcin@gmail.com>
2020-04-09 12:52:56 +0200

    Fix query refetching

--- frontend/src/apollo/mutations/UpdateTargetPopulation.ts
+++ frontend/src/apollo/mutations/UpdateTargetPopulation.ts
@@ -5,7 +5,68 @@ export const UpdateTP = gql`
     updateTargetPopulation(input: $input) {
       targetPopulation {
         id
+        name
         status
+        candidateListTargetingCriteria {
+          targetPopulationCandidate {
+            createdBy {
+              firstName
+              lastName
+            }
+            program {
+              id
+              name
+            }
+          }
+          rules {
+            id
+            filters {
+              fieldName
+              isFlexField
+              arguments
+              comparisionMethod
+              fieldAttribute {
+                name
+                labelEn
+                type
+                choices {
+                  value
+                  labelEn
+                }
+              }
+            }
+          }
+        }
+        finalListTargetingCriteria {
+          targetPopulationFinal {
+            createdBy {
+              firstName
+              lastName
+            }
+            program {
+              id
+              name
+            }
+          }
+          rules {
+            id
+            filters {
+              fieldName
+              isFlexField
+              arguments
+              comparisionMethod
+              fieldAttribute {
+                name
+                labelEn
+                type
+                choices {
+                  value
+                  labelEn
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
