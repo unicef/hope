c51248d7939c81c6aefa6719cb033f99531c45ed
Jan Romaniak <romaniakjan@gmail.com>
2020-04-10 13:41:33 +0200

    Remove all Target Populations on 4th mutation because of duplications

--- backend/hct_mis_api/apps/targeting/migrations/0004_migration.py
+++ backend/hct_mis_api/apps/targeting/migrations/0004_migration.py
@@ -6,13 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('targeting', '0003_migration'),
+        ("targeting", "0003_migration"),
     ]
 
+    def clear_target_populations(apps, schema):
+        TargetPopulation = apps.get_model("targeting", "TargetPopulation")
+        TargetPopulation.objects.all().delete()
+
     operations = [
+        migrations.RunPython(clear_target_populations),
         migrations.AlterField(
-            model_name='targetpopulation',
-            name='name',
+            model_name="targetpopulation",
+            name="name",
             field=models.TextField(unique=True),
         ),
     ]
