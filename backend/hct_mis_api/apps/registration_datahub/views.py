6c33438c632e61f6474820924e817fbca277b25a
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-04-10 20:12:13 +0200

    xlsx template file generator and download endpoint

--- backend/hct_mis_api/apps/registration_datahub/views.py
+++ backend/hct_mis_api/apps/registration_datahub/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.http import HttpResponse
+from openpyxl.writer.excel import save_virtual_workbook
+
+from registration_datahub.template_generator import TemplateFileGenerator
+
+
+def download_template(request):
+    mimetype = (
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+    filename = "registration_data_import_template.xlsx"
+    response = HttpResponse(content_type=mimetype)
+    response["Content-Disposition"] = f"attachment; filename={filename}"
+    response.write(
+        save_virtual_workbook(TemplateFileGenerator.get_template_file())
+    )
+
+    return response
