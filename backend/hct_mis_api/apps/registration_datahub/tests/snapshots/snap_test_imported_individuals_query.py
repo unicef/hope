26f48083b9eacdbcd409ad2b8a1d8674fb7df412
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-04-23 13:58:30 +0200

    registration_datahub tests fix

--- backend/hct_mis_api/apps/registration_datahub/tests/snapshots/snap_test_imported_individuals_query.py
+++ backend/hct_mis_api/apps/registration_datahub/tests/snapshots/snap_test_imported_individuals_query.py
@@ -7,114 +7,6 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestImportedIndividualQuery::test_imported_individual_query_age_max 1'] = {
-    'data': {
-        'allImportedIndividuals': {
-            'edges': [
-                {
-                    'node': {
-                        'birthDate': '1983-12-21',
-                        'familyName': 'Perry',
-                        'fullName': 'Timothy Perry',
-                        'givenName': 'Timothy',
-                        'phoneNo': '(548)313-1700-902'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1973-03-23',
-                        'familyName': 'Torres',
-                        'fullName': 'Eric Torres',
-                        'givenName': 'Eric',
-                        'phoneNo': '+12282315473'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1969-11-29',
-                        'familyName': 'Franklin',
-                        'fullName': 'Jenna Franklin',
-                        'givenName': 'Jenna',
-                        'phoneNo': '001-296-358-5428-607'
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestImportedIndividualQuery::test_imported_individual_query_age_min 1'] = {
-    'data': {
-        'allImportedIndividuals': {
-            'edges': [
-                {
-                    'node': {
-                        'birthDate': '1943-07-30',
-                        'familyName': 'Butler',
-                        'fullName': 'Benjamin Butler',
-                        'givenName': 'Benjamin',
-                        'phoneNo': '(953)682-4596'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1946-02-15',
-                        'familyName': 'Ford',
-                        'fullName': 'Robin Ford',
-                        'givenName': 'Robin',
-                        'phoneNo': '+18663567905'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1973-03-23',
-                        'familyName': 'Torres',
-                        'fullName': 'Eric Torres',
-                        'givenName': 'Eric',
-                        'phoneNo': '+12282315473'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1969-11-29',
-                        'familyName': 'Franklin',
-                        'fullName': 'Jenna Franklin',
-                        'givenName': 'Jenna',
-                        'phoneNo': '001-296-358-5428-607'
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestImportedIndividualQuery::test_imported_individual_query_age_range 1'] = {
-    'data': {
-        'allImportedIndividuals': {
-            'edges': [
-                {
-                    'node': {
-                        'birthDate': '1973-03-23',
-                        'familyName': 'Torres',
-                        'fullName': 'Eric Torres',
-                        'givenName': 'Eric',
-                        'phoneNo': '+12282315473'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1969-11-29',
-                        'familyName': 'Franklin',
-                        'fullName': 'Jenna Franklin',
-                        'givenName': 'Jenna',
-                        'phoneNo': '001-296-358-5428-607'
-                    }
-                }
-            ]
-        }
-    }
-}
-
 snapshots['TestImportedIndividualQuery::test_imported_individual_query_all 1'] = {
     'data': {
         'allImportedIndividuals': {
@@ -169,24 +61,6 @@ snapshots['TestImportedIndividualQuery::test_imported_individual_query_all 1'] =
     }
 }
 
-snapshots['TestImportedIndividualQuery::test_imported_individual_query_sex 1'] = {
-    'data': {
-        'allImportedIndividuals': {
-            'edges': [
-                {
-                    'node': {
-                        'birthDate': '1969-11-29',
-                        'familyName': 'Franklin',
-                        'fullName': 'Jenna Franklin',
-                        'givenName': 'Jenna',
-                        'phoneNo': '001-296-358-5428-607'
-                    }
-                }
-            ]
-        }
-    }
-}
-
 snapshots['TestImportedIndividualQuery::test_imported_individual_query_single 1'] = {
     'data': {
         'importedIndividual': {
@@ -195,6 +69,114 @@ snapshots['TestImportedIndividualQuery::test_imported_individual_query_single 1'
             'fullName': 'Benjamin Butler',
             'givenName': 'Benjamin',
             'phoneNo': '(953)682-4596'
+        }
+    }
+}
+
+snapshots['TestImportedIndividualQuery::test_imported_individual_query_order_by_dob_a_all 1'] = {
+    'data': {
+        'allImportedIndividuals': {
+            'edges': [
+                {
+                    'node': {
+                        'birthDate': '1943-07-30',
+                        'familyName': 'Butler',
+                        'fullName': 'Benjamin Butler',
+                        'givenName': 'Benjamin',
+                        'phoneNo': '(953)682-4596'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1946-02-15',
+                        'familyName': 'Ford',
+                        'fullName': 'Robin Ford',
+                        'givenName': 'Robin',
+                        'phoneNo': '+18663567905'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1969-11-29',
+                        'familyName': 'Franklin',
+                        'fullName': 'Jenna Franklin',
+                        'givenName': 'Jenna',
+                        'phoneNo': '001-296-358-5428-607'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1973-03-23',
+                        'familyName': 'Torres',
+                        'fullName': 'Eric Torres',
+                        'givenName': 'Eric',
+                        'phoneNo': '+12282315473'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1983-12-21',
+                        'familyName': 'Perry',
+                        'fullName': 'Timothy Perry',
+                        'givenName': 'Timothy',
+                        'phoneNo': '(548)313-1700-902'
+                    }
+                }
+            ]
+        }
+    }
+}
+
+snapshots['TestImportedIndividualQuery::test_imported_individual_query_order_by_dob_d_all 1'] = {
+    'data': {
+        'allImportedIndividuals': {
+            'edges': [
+                {
+                    'node': {
+                        'birthDate': '1983-12-21',
+                        'familyName': 'Perry',
+                        'fullName': 'Timothy Perry',
+                        'givenName': 'Timothy',
+                        'phoneNo': '(548)313-1700-902'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1973-03-23',
+                        'familyName': 'Torres',
+                        'fullName': 'Eric Torres',
+                        'givenName': 'Eric',
+                        'phoneNo': '+12282315473'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1969-11-29',
+                        'familyName': 'Franklin',
+                        'fullName': 'Jenna Franklin',
+                        'givenName': 'Jenna',
+                        'phoneNo': '001-296-358-5428-607'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1946-02-15',
+                        'familyName': 'Ford',
+                        'fullName': 'Robin Ford',
+                        'givenName': 'Robin',
+                        'phoneNo': '+18663567905'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1943-07-30',
+                        'familyName': 'Butler',
+                        'fullName': 'Benjamin Butler',
+                        'givenName': 'Benjamin',
+                        'phoneNo': '(953)682-4596'
+                    }
+                }
+            ]
         }
     }
 }
