e5ef832f1ef697f2a29523a2f1d76a485989df1a
Jan Romaniak <romaniakjan@gmail.com>
2020-04-29 13:33:15 +0200

    user profile

---
+++ frontend/src/containers/UserProfileMenu.tsx
@@ -0,0 +1,102 @@
+import {
+  Avatar,
+  Button,
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+import { MeQuery } from '../__generated__/graphql';
+
+const UserProfileButton = styled(Button)`
+  && {
+    color: #e3e6e7;
+  }
+`;
+const MenuButtonText = styled.span`
+  margin-left: ${({ theme }) => theme.spacing(2)}px;
+`;
+interface UserProfileMenuProps {
+  meData: MeQuery;
+}
+export function UserProfileMenu({ meData }: UserProfileMenuProps) {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: React.MouseEvent<EventTarget>) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+  const handleLogout = (event: React.MouseEvent<EventTarget>) => {
+    window.location.assign('/api/logout');
+    handleClose(event);
+  };
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+  return (
+    <>
+      <UserProfileButton ref={anchorRef} onClick={handleToggle}>
+        <Avatar alt={meData.me.email} src='/static/images/avatar/1.jpg' />
+        <MenuButtonText> {meData.me.email}</MenuButtonText>
+      </UserProfileButton>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  autoFocusItem={open}
+                  id='menu-list-grow'
+                  onKeyDown={handleListKeyDown}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+}
