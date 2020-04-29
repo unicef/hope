a05f1e3aaace0af7e70ef97a4b89dadb116e2cc8
Jan Romaniak <romaniakjan@gmail.com>
2020-04-29 10:16:24 +0200

    fixed other tests

--- backend/hct_mis_api/apps/household/tests/snapshots/snap_test_individual_query.py
+++ backend/hct_mis_api/apps/household/tests/snapshots/snap_test_individual_query.py
@@ -67,6 +67,101 @@ snapshots['TestIndividualQuery::test_individual_programme_filter 1'] = {
                         },
                         'phoneNo': '(953)682-4596'
                     }
+                },
+                {
+                    'node': {
+                        'birthDate': '1962-08-07',
+                        'familyName': 'Gibbs',
+                        'givenName': 'Janet',
+                        'household': {
+                            'programs': {
+                                'edges': [
+                                    {
+                                        'node': {
+                                            'name': 'Test program TWO'
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        'phoneNo': '158-147-0749x340'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1939-12-16',
+                        'familyName': 'Gomez',
+                        'givenName': 'Roger',
+                        'household': {
+                            'programs': {
+                                'edges': [
+                                    {
+                                        'node': {
+                                            'name': 'Test program TWO'
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        'phoneNo': '+19542331272'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1977-08-06',
+                        'familyName': 'Michael',
+                        'givenName': 'Danielle',
+                        'household': {
+                            'programs': {
+                                'edges': [
+                                    {
+                                        'node': {
+                                            'name': 'Test program TWO'
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        'phoneNo': '001-291-293-0741x813'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1956-01-03',
+                        'familyName': 'White',
+                        'givenName': 'Travis',
+                        'household': {
+                            'programs': {
+                                'edges': [
+                                    {
+                                        'node': {
+                                            'name': 'Test program TWO'
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        'phoneNo': '488-733-5557x06643'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1933-07-10',
+                        'familyName': 'French',
+                        'givenName': 'Catherine',
+                        'household': {
+                            'programs': {
+                                'edges': [
+                                    {
+                                        'node': {
+                                            'name': 'Test program TWO'
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        'phoneNo': '001-321-086-1627x329'
+                    }
                 }
             ]
         }
@@ -77,6 +172,69 @@ snapshots['TestIndividualQuery::test_individual_query_all 1'] = {
     'data': {
         'allIndividuals': {
             'edges': [
+                {
+                    'node': {
+                        'birthDate': '1973-03-23',
+                        'familyName': 'Torres',
+                        'fullName': 'Eric Torres',
+                        'givenName': 'Eric',
+                        'phoneNo': '+12282315473'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1967-10-19',
+                        'familyName': 'Barry',
+                        'fullName': 'Katie Kristen Barry',
+                        'givenName': 'Katie',
+                        'phoneNo': '+12064930450'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1967-07-12',
+                        'familyName': 'Davidson',
+                        'fullName': 'Tara Lauren Davidson',
+                        'givenName': 'Tara',
+                        'phoneNo': '+18179547965'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '2001-12-30',
+                        'familyName': 'Livingston',
+                        'fullName': 'Jesse Christopher Livingston',
+                        'givenName': 'Jesse',
+                        'phoneNo': '960-534-4074x3203'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1937-01-19',
+                        'familyName': 'Sloan',
+                        'fullName': 'Laura Carol Sloan',
+                        'givenName': 'Laura',
+                        'phoneNo': '+15394566371'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1945-04-30',
+                        'familyName': 'Smith',
+                        'fullName': 'Laura Elizabeth Smith',
+                        'givenName': 'Laura',
+                        'phoneNo': '(824)628-3298x64013'
+                    }
+                },
+                {
+                    'node': {
+                        'birthDate': '1934-10-04',
+                        'familyName': 'Sanford',
+                        'fullName': 'Angela Christina Sanford',
+                        'givenName': 'Angela',
+                        'phoneNo': '496-383-7465x901'
+                    }
+                },
                 {
                     'node': {
                         'birthDate': '1943-07-30',
@@ -102,15 +260,6 @@ snapshots['TestIndividualQuery::test_individual_query_all 1'] = {
                         'fullName': 'Timothy Perry',
                         'givenName': 'Timothy',
                         'phoneNo': '(548)313-1700-902'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1973-03-23',
-                        'familyName': 'Torres',
-                        'fullName': 'Eric Torres',
-                        'givenName': 'Eric',
-                        'phoneNo': '+12282315473'
                     }
                 },
                 {
