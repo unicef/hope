2c79c3f906fcecf8760812faea7db95816f638fb
Sumit Chachra <sumit@tivix.com>
2020-04-29 22:15:04 -0700

    Added id's for logout link and nav item in header with user email address. Added programme_management feature and spec stub, added test for logging out.

--- cypress_testing_service/cypress/integration/auth/auth.spec.js
+++ cypress_testing_service/cypress/integration/auth/auth.spec.js
@@ -5,6 +5,12 @@ Given('I visit {word}', (path) => {
     cy.visit(path)
 })
 
+When('I click Logout', (path) => {
+    cy.get('#logged_in_user_header_button').click();
+    cy.get('#logout_link').click();
+})
+
+
 Then('I should get redirected to login', () => {
     cy.location('pathname').should('eq', '/login')
     cy.get('a').contains('Sign in')
@@ -12,4 +18,8 @@ Then('I should get redirected to login', () => {
 
 Then('I should see the Dashboard', () => {
     cy.get('.MuiList-root').contains('Dashboard')
+})
+
+Then('I see my email address in the header', () => {
+    cy.get('header').contains(Cypress.env('logged_in_user_username'))
 })
