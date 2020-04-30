4813eabd08e446bbe1f2379769d89305098fabb2
Jan Romaniak <romaniakjan@gmail.com>
2020-04-30 18:52:38 +0200

    Ariflow Registration Data Import Merge

--- backend/hct_mis_api/apps/targeting/fixtures.py
+++ backend/hct_mis_api/apps/targeting/fixtures.py
@@ -5,7 +5,7 @@ import factory
 from factory import fuzzy
 
 from account.fixtures import UserFactory
-from core.core_fields_attributes import CORE_FIELDS_ATTRIBUTES
+from core.core_fields_attributes import CORE_FIELDS_ATTRIBUTES, FILTERABLE_CORE_FIELDS_ATTRIBUTES
 from household.fixtures import HouseholdFactory
 from household.models import RESIDENCE_STATUS_CHOICE
 from targeting.models import (
@@ -36,6 +36,10 @@ def comparision_method_resolver(obj):
 
     if core_field_attr.get("type") == "SELECT_ONE":
         return random.choice(["EQUALS", "NOT_EQUALS",])
+    if core_field_attr.get("type") == "STRING":
+        return "CONTAINS"
+    print("**************************************")
+    print(core_field_attr.get("type") )
 
 
 def arguments_resolver(obj):
@@ -48,9 +52,7 @@ def arguments_resolver(obj):
         min = random.randint(1, 5)
         max = random.randint(min, random.randint(min + 1, 10))
     if obj.field_name == "residence_status":
-        return [
-            random.choice([x[0] for x in RESIDENCE_STATUS_CHOICE])
-        ]
+        return [random.choice([x[0] for x in RESIDENCE_STATUS_CHOICE])]
     if (
         obj.comparision_method == "RANGE"
         or obj.comparision_method == "NOT_IN_RANGE"
@@ -61,8 +63,7 @@ def arguments_resolver(obj):
 
 class TargetingCriteriaRuleFilterFactory(factory.DjangoModelFactory):
     field_name = factory.fuzzy.FuzzyChoice(
-        CORE_FIELDS_ATTRIBUTES,
-        getter=lambda x: x.get("name"),
+        ['age','size','residence_status'],
     )
     comparision_method = factory.LazyAttribute(comparision_method_resolver)
     arguments = factory.LazyAttribute(arguments_resolver)
@@ -82,7 +83,6 @@ class TargetingCriteriaFactory(factory.DjangoModelFactory):
 
 
 class TargetPopulationFactory(factory.DjangoModelFactory):
-
     class Meta:
         model = TargetPopulation
 
