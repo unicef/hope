b2e9350e1a29a2a1c1d9dab9a533cb18f34f9e91
Grzegorz Duda <grzegorz.duda@tivix.com>
2020-05-03 01:07:28 +0200

    chore: add prettier to cypress

--- cypress_testing_service/.eslintrc.js
+++ cypress_testing_service/.eslintrc.js
@@ -1,12 +1,17 @@
 const indent = 4;
 
 module.exports = {
-  extends: ['airbnb-typescript/base', 'plugin:cypress/recommended'],
-  rules: {
-    indent: ['error', indent],
-    '@typescript-eslint/indent': ['error', indent]
-  },
-  parserOptions: {
-    project: './tsconfig.json',
-  }
+    extends: [
+        'airbnb-typescript/base',
+        'plugin:cypress/recommended',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    rules: {
+        indent: ['error', indent],
+        '@typescript-eslint/indent': ['error', indent],
+    },
+    parserOptions: {
+        project: './tsconfig.json',
+    },
 };
---
+++ cypress_testing_service/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    $schema: 'http://json.schemastore.org/prettierrc',
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: true,
+    tabWidth: 4,
+};
