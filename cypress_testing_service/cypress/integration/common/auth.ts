fe358e4bf95a3a92bded667e51b0ec48d088ad17
Grzegorz Duda <grzegorz.duda@tivix.com>
2020-05-03 00:43:11 +0200

    chore: add eslint for cypress

--- cypress_testing_service/cypress/integration/common/auth.ts
+++ cypress_testing_service/cypress/integration/common/auth.ts
@@ -1,14 +1,13 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps';
 
-
-Given('I login to AD as {word}', (user_role) => {
-    cy.log('Signing in user to A as ${user_rule}')
+Given('I login to AD as {word}', (userRole) => {
+    cy.log(`Signing in user to A as ${userRole}`);
     cy.loginToAD(
-        Cypress.env(user_role).ad_username, 
-        Cypress.env(user_role).ad_password, 
-        Cypress.env('loginUrl')
-    )
+        Cypress.env(userRole).ad_username,
+        Cypress.env(userRole).ad_password,
+        Cypress.env('loginUrl'),
+    );
     // TODO: see if there is a better way to do this?
-    Cypress.env('logged_in_user_username', Cypress.env(user_role).ad_username)
-    cy.visit(Cypress.env('loginUrl'))
-})
+    Cypress.env('logged_in_user_username', Cypress.env(userRole).ad_username);
+    cy.visit(Cypress.env('loginUrl'));
+});
