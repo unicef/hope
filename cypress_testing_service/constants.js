71de9851c4d9fe085262559c1c76d2f3a35d5f6d
Grzegorz Duda <grzegorz.duda@tivix.com>
2020-05-03 01:16:12 +0200

    chore: pretty cypress files with indent set to 2

---
+++ cypress_testing_service/constants.js
@@ -0,0 +1,2 @@
+export const INDENT_SIZE = 2;
+
--- cypress_testing_service/cypress/integration/common/assertions.ts
+++ cypress_testing_service/cypress/integration/common/assertions.ts
@@ -2,8 +2,11 @@ import { Given } from 'cypress-cucumber-preprocessor/steps';
 
 // assert path within the current business area
 Given('I get taken to {word} in current business area', (pathToCheckFor) => {
-    cy.location('pathname').then((currentPath) => {
-        const businessAreaSlug = currentPath.split('/')[1];
-        cy.location('pathname').should('eq', '/'.concat(businessAreaSlug, pathToCheckFor));
-    });
+  cy.location('pathname').then((currentPath) => {
+    const businessAreaSlug = currentPath.split('/')[1];
+    cy.location('pathname').should(
+      'eq',
+      '/'.concat(businessAreaSlug, pathToCheckFor),
+    );
+  });
 });
