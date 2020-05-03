29630ccb180024743b7285aaa46f553267ca414a
Grzegorz Duda <grzegorz.duda@tivix.com>
2020-05-03 23:28:25 +0200

    chore: add cucumber autocomplete vsc extension w/ strict gherkin

--- cypress_testing_service/cypress/integration/common/assertions.ts
+++ cypress_testing_service/cypress/integration/common/assertions.ts
@@ -1,7 +1,7 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 
 // assert path within the current business area
-Given('I get taken to {word} in current business area', (pathToCheckFor) => {
+Then('I get taken to {word} in current business area', (pathToCheckFor) => {
   cy.location('pathname').then((currentPath) => {
     const businessAreaSlug = currentPath.split('/')[1];
     cy.location('pathname').should(
--- cypress_testing_service/cypress/integration/common/selectors.ts
+++ cypress_testing_service/cypress/integration/common/selectors.ts
@@ -1,6 +1,6 @@
-import { Given } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 
 // Detect a string on the page
-Given('I see {string} on the page', (words) => {
+Then('I see {string} on the page', (words) => {
   cy.contains(words);
 });
