5bbbbb4284846814b34e0fddfad0e564eeacbb7e
Grzegorz Duda <grzegorz.duda@tivix.com>
2020-05-04 20:05:08 +0200

    chore: create programme management in draft

--- cypress_testing_service/cypress/support/commands.js
+++ cypress_testing_service/cypress/support/commands.js
@@ -27,12 +27,28 @@
 // This is an example of how you might use the plugin in your tests
 // https://gist.github.com/csuzw/845b589549b61d3a5fe18e49592e166f
 
+const login = (cookies) => {
+  cy.clearCookies();
+
+  cookies.forEach((cookie) => {
+    cy.setCookie(cookie.name, cookie.value, {
+      domain: cookie.domain,
+      expiry: cookie.expires,
+      httpOnly: cookie.httpOnly,
+      path: cookie.path,
+      secure: cookie.secure,
+      sameSite: cookie.sameSite,
+    });
+    Cypress.Cookies.preserveOnce(cookie.name);
+  });
+};
+
 Cypress.Commands.add('loginToAD', (username, password, loginUrl) => {
   const options = {
     username,
     password,
     loginUrl,
-    postLoginSelector: '.MuiList-root',
+    postLoginSelector: '[data-cy=side-nav]',
     headless: true,
     logs: false,
     getAllBrowserCookies: true,
@@ -42,22 +58,30 @@ Cypress.Commands.add('loginToAD', (username, password, loginUrl) => {
   // https://github.com/cypress-io/cypress/issues/1342
   // https://github.com/cypress-io/cypress/issues/944
   cy.task('AzureAdSingleSignOn', options).then((result) => {
-    cy.clearCookies();
-
-    result.cookies.forEach((cookie) => {
-      cy.setCookie(cookie.name, cookie.value, {
-        domain: cookie.domain,
-        expiry: cookie.expires,
-        httpOnly: cookie.httpOnly,
-        path: cookie.path,
-        secure: cookie.secure,
-        sameSite: cookie.sameSite,
-      });
-      Cypress.Cookies.preserveOnce(cookie.name);
-    });
+    login(result.cookies);
   });
+});
+
+Cypress.Commands.add('loginWithMock', () => {
+  // eslint-disable-next-line global-require
+  const mockAuthCookies = []; // require('./mockAuthCookies.json');
+  login(mockAuthCookies);
 });
 
 Cypress.Commands.add('getByTestId', (value) => {
   return cy.get(`[data-cy=${value}]`);
+});
+
+Cypress.Commands.add('navigateTo', (newPath) => {
+  return cy.location('pathname').then((pathname) => {
+    const businessAreaSlug = pathname.split('/')[1];
+    cy.visit('/'.concat(businessAreaSlug, newPath));
+  });
+});
+
+Cypress.Commands.add('pickDayOfTheMonth', (day, inputName) => {
+  cy.getByTestId(`date-input-${inputName}`).click();
+  cy.get('.MuiPickersBasePicker-pickerView button')
+    .contains(new RegExp(`^${day}$`, 'g'))
+    .click();
 });
