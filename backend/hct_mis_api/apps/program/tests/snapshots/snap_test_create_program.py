3fbc6572fe4e656caa3f806d215b7ff43047ef64
Jan Romaniak <romaniakjan@gmail.com>
2020-05-06 10:46:52 +0200

    fixed snap tests

--- backend/hct_mis_api/apps/program/tests/snapshots/snap_test_create_program.py
+++ backend/hct_mis_api/apps/program/tests/snapshots/snap_test_create_program.py
@@ -28,3 +28,45 @@ snapshots['TestCreateProgram::test_create_program_authenticated 1'] = {
         }
     }
 }
+
+snapshots['TestCreateProgram::test_create_program_invalid_dates 1'] = {
+    'data': {
+        'createProgram': {
+            'program': {
+                'administrativeAreasOfImplementation': 'Lorem Ipsum',
+                'budget': '20000000',
+                'cashPlus': True,
+                'description': 'my description of program',
+                'endDate': '2021-12-20',
+                'frequencyOfPayments': 'REGULAR',
+                'name': 'Test',
+                'populationGoal': 150000,
+                'programCaId': '5e0a38c6-7bcb-4b4a-b8e0-311e8c694ae3',
+                'scope': 'FULL',
+                'sector': 'EDUCATION',
+                'startDate': '2019-12-20',
+                'status': 'DRAFT'
+            }
+        }
+    }
+}
+
+snapshots['TestCreateProgram::test_create_program_not_authenticated 1'] = {
+    'data': {
+        'createProgram': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 7,
+                    'line': 3
+                }
+            ],
+            'message': 'Permission Denied: User is not authenticated.',
+            'path': [
+                'createProgram'
+            ]
+        }
+    ]
+}
