23ceb9f01421d6a2f4298494fe4454671f175c00
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-05-06 16:35:35 +0200

    simple tests for execute, resolve high cpu usage for airflow scheduler

--- backend/hct_mis_api/apps/registration_datahub/tests/test_rdi_create.py
+++ backend/hct_mis_api/apps/registration_datahub/tests/test_rdi_create.py
@@ -1,9 +1,15 @@
+from django.core.files import File
 from django.core.management import call_command
 from django.test import TestCase
 
 from core.models import BusinessArea
 from registration_datahub.fixtures import RegistrationDataImportDatahubFactory
-from registration_datahub.models import ImportData
+from registration_datahub.models import (
+    ImportData,
+    ImportedHousehold,
+    ImportedIndividual,
+)
+from registration_datahub.tasks.rdi_create import RdiCreateTask
 
 
 class TestRdiCreateTask(TestCase):
@@ -13,19 +19,35 @@ class TestRdiCreateTask(TestCase):
     def setUpTestData(cls):
         call_command("loadbusinessareas")
 
-        cls.import_data = ImportData.objects.create(
-            xlsx_file="hct_mis_api/apps/registration_datahub/tests"
+        with open(
+            "hct_mis_api/apps/registration_datahub/tests"
             "/test_file/new_reg_data_import.xlsx",
-            number_of_households=3,
-            number_of_individuals=6,
-        )
+            "rb",
+        ) as excel_file:
+            file = File(excel_file)
+            cls.import_data = ImportData.objects.create(
+                xlsx_file=file,
+                number_of_households=3,
+                number_of_individuals=6,
+            )
         cls.registration_data_import = RegistrationDataImportDatahubFactory(
             import_data=cls.import_data
         )
         cls.business_area = BusinessArea.objects.first()
 
     def test_execute(self):
-        pass
+        task = RdiCreateTask()
+        task.execute(
+            self.registration_data_import.id,
+            self.import_data.id,
+            self.business_area.id,
+        )
+
+        households_count = ImportedHousehold.objects.count()
+        individuals_count = ImportedIndividual.objects.count()
+
+        self.assertEqual(3, households_count)
+        self.assertEqual(6, individuals_count)
 
     def test_handle_document_fields(self):
         pass
