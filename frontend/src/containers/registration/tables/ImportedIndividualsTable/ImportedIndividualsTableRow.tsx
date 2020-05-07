7370a63ad65cfd17859aa3805d29cf7401aca9bb
Jan Romaniak <romaniakjan@gmail.com>
2020-05-07 15:12:26 +0200

    multiple fixes

--- frontend/src/containers/registration/tables/ImportedIndividualsTable/ImportedIndividualsTableRow.tsx
+++ frontend/src/containers/registration/tables/ImportedIndividualsTable/ImportedIndividualsTableRow.tsx
@@ -2,22 +2,30 @@ import TableCell from '@material-ui/core/TableCell';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import moment from 'moment';
-import { ImportedIndividualMinimalFragment } from '../../../../__generated__/graphql';
+import {
+  HouseholdChoiceDataQuery,
+  ImportedIndividualMinimalFragment,
+  useHouseholdChoiceDataQuery,
+} from '../../../../__generated__/graphql';
 import { useBusinessArea } from '../../../../hooks/useBusinessArea';
 import { ClickableTableRow } from '../../../../components/table/ClickableTableRow';
-import { decodeIdString } from '../../../../utils/utils';
+import { choicesToDict, decodeIdString } from '../../../../utils/utils';
 import { Missing } from '../../../../components/Missing';
 
 interface ImportedIndividualsTableRowProps {
   individual: ImportedIndividualMinimalFragment;
+  choices: HouseholdChoiceDataQuery;
 }
 
 export function ImportedIndividualsTableRow({
   individual,
+  choices,
 }: ImportedIndividualsTableRowProps): React.ReactElement {
   const history = useHistory();
   const businessArea = useBusinessArea();
 
+  const relationshipChoicesDict = choicesToDict(choices.relationshipChoices);
+  const roleChoicesDict = choicesToDict(choices.roleChoices);
   const handleClick = (): void => {
     const path = `/${businessArea}/registration-data-import/individual/${individual.id}`;
     history.push(path);
@@ -26,8 +34,9 @@ export function ImportedIndividualsTableRow({
     <ClickableTableRow hover onClick={handleClick} key={individual.id}>
       <TableCell align='left'>{decodeIdString(individual.id)}</TableCell>
       <TableCell align='left'>{individual.fullName}</TableCell>
+      <TableCell align='left'>{roleChoicesDict[individual.role]}</TableCell>
       <TableCell align='left'>
-        <Missing />
+        {relationshipChoicesDict[individual.relationship]}
       </TableCell>
       <TableCell align='left'>
         {moment(individual.birthDate).format('DD MMM YYYY')}
