314f1d5f969be705cb9680027540595a650af0b8
Grzegorz Duda <grzegorz.duda@tivix.com>
2020-05-08 20:07:12 +0200

    chore: population filter households

--- cypress_testing_service/cypress/integration/population/population.spec.ts
+++ cypress_testing_service/cypress/integration/population/population.spec.ts
@@ -16,7 +16,7 @@ When('the User enters alphanumeric string in search field', () => {
   const searchQuery = 'Julia';
   cy.wrap(searchQuery).as('searchQuery');
 
-  // TODO using force b/c input is covered by some other element
+  // TODO using force b/c filter is covered by some other element in the UI, investigate
   cy.getByTestId('population-household-filters-search')
     .find('input')
     .type(searchQuery, {
@@ -29,6 +29,10 @@ When('the User enters alphanumeric string in search field', () => {
 });
 
 Then('a list of the Housholds that meet the text in search is shown', () => {
+  cy.getByTestId('population-household-details-container')
+    .find('table tbody tr')
+    .should('have.length.greaterThan', 1);
+
   cy.get('@searchQuery').then((searchQuery) => {
     cy.getByTestId('population-household-details-container')
       .find('table tbody tr')
@@ -37,3 +41,42 @@ Then('a list of the Housholds that meet the text in search is shown', () => {
       });
   });
 });
+
+When('the user makes at least one selection of filters available', () => {
+  const filterQuery = 'refugee';
+  cy.wrap(filterQuery).as('filterQuery');
+
+  cy.getByTestId('main-content').scrollTo('top');
+
+  // TODO using force b/c filter is covered by some other element in the UI, investigate
+  cy.getByTestId('population-household-filters-residence-status').click({
+    force: true,
+  });
+
+  // TODO using force b/c filter is covered by some other element in the UI, investigate
+  cy.getByTestId('population-household-filters-residence-status-options')
+    .find('ul')
+    .contains(filterQuery, { matchCase: false })
+    .click({ force: true });
+
+  cy.getByTestId('population-household-details-container')
+    .getByTestId('loading-container')
+    .as('loadingContainer');
+});
+
+Then(
+  'a list of the Housholds the criteria selected in the filters is shown',
+  () => {
+    cy.getByTestId('population-household-details-container')
+      .find('table tbody tr')
+      .should('have.length.greaterThan', 1);
+
+    cy.get<string>('@filterQuery').then((filterQuery) => {
+      cy.getByTestId('population-household-details-container')
+        .find('table tbody tr')
+        .should(($row) => {
+          expect($row.text().toLowerCase()).contain(filterQuery.toLowerCase());
+        });
+    });
+  },
+);
