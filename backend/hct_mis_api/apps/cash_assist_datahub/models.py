e59d8e07e601d73a54f12e13b7bb132aedeb9efd
Jan Romaniak <romaniakjan@gmail.com>
2020-05-13 15:21:42 +0200

    first model CA hub changes

--- backend/hct_mis_api/apps/cash_assist_datahub/models.py
+++ backend/hct_mis_api/apps/cash_assist_datahub/models.py
@@ -5,77 +5,149 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django_countries.fields import CountryField
 from model_utils import Choices
+from model_utils.fields import UUIDField
+from model_utils.models import UUIDModel
 
-from hct_mis_api.apps.utils.models import TimeStampedUUIDModel
+from household.models import (
+    RELATIONSHIP_CHOICE,
+    ROLE_CHOICE,
+    MARITAL_STATUS_CHOICE,
+)
+from utils.models import TimeStampedUUIDModel
 
 
-# TODO THIS MODELS MUST BE CHANGED
-
-class Household(TimeStampedUUIDModel):
-    # TODO Get correct RECEPTION_TYPE_CHOICE
-    RECEPTION_TYPE_CHOICE = (
-        ("REFUGEE", _("Refugee")),
-        ("MIGRANT", _("Migrant")),
-        ("CITIZEN", _("Citizen")),
-        ("IDP", _("IDP")),
-        ("OTHER", _("Other")),
+class Session(models.Model):
+    timestamp = models.DateTimeField()
+    source = models.CharField(
+        max_length=3, choices=(("MIS", "HCT-MIS"), ("CA", "Cash Assist")),
     )
-
-    household_ca_id = models.CharField(max_length=255)
-    reception_type = models.CharField(
-        max_length=255, choices=RECEPTION_TYPE_CHOICE,
+    status = models.CharField(
+        max_length=11,
+        choices=(
+            ("NEW", "New"),
+            ("READY", "Ready"),
+            ("PROCESSING", "Processing"),
+            ("COMPLETED", "Completed"),
+            ("FAILED", "Failed"),
+        ),
     )
-    family_name = models.CharField(max_length=255)
-    household_size = models.PositiveIntegerField(blank=True, null=True)
-    has_demographic_breakdown = models.BooleanField()
-
-    def __str__(self):
-        return self.family_name
+    last_modified_date = models.DateTimeField(auto_now=True)
 
 
-class Individual(TimeStampedUUIDModel):
+class SessionModel(models.Model):
+    last_sync_on = models.DateTimeField(null=True)
+    session_id = models.ForeignKey("cash_assist_datahub.Session",on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Household(SessionModel):
+    household_id = UUIDField(primary_key=True, version=4,)
+    status = models.CharField(
+        max_length=50, choices=(("INACTIVE", "Inactive"), ("ACTIVE", "Active")),
+    )
+    household_size = models.PositiveIntegerField()
+    # head of household document id
+    government_form_number = models.CharField(max_length=255, blank=True)
+    # registration household id
+    form_number = models.CharField(max_length=255, blank=True)
+    agency_id = models.CharField(max_length=255, blank=True)
+    #  individual_id head of household
+    focal_point = models.ForeignKey(
+        "cash_assist_datahub.Individual",
+        db_column="focal_point",
+        on_delete=models.CASCADE,
+    )
+    address = models.CharField(max_length=255, blank=True)
+    admin1 = models.CharField(max_length=255, blank=True)
+    admin2 = models.CharField(max_length=255, blank=True)
+    country = CountryField(blank=True)
+
+
+class Individual(SessionModel):
+    individual_id = UUIDField(primary_key=True, version=4,)
+    household = models.ForeignKey(
+        "cash_assist_datahub.Household",
+        db_column="household_id",
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=(("INACTIVE", "Inactive"), ("ACTIVE", "Active")),
+        blank=True,
+    )
     SEX_CHOICE = (
         ("MALE", _("Male")),
         ("FEMALE", _("Female")),
-        ("OTHER", _("Other")),
     )
-
-    household = models.ForeignKey(
-        "Household", related_name="individuals", on_delete=models.CASCADE
-    )
-    individual_ca_id = models.CharField(max_length=255)
-    family_name = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255)
+    family_name = models.CharField(max_length=255, blank=True)
+    given_name = models.CharField(max_length=255, blank=True)
+    middle_name = models.CharField(max_length=255, blank=True)
     sex = models.CharField(max_length=255, choices=SEX_CHOICE,)
-    date_of_birth = models.DateField(blank=True, null=True)
-    estimated_date_of_birth = models.DateField(blank=True, null=True)
-    country_of_origin = CountryField()
+    date_of_birth = models.DateField()
+    estimated_date_of_birth = models.BooleanField()
+    relationship = models.CharField(
+        max_length=255, blank=True, choices=RELATIONSHIP_CHOICE,
+    )
+    role = models.CharField(max_length=255, blank=True, choices=ROLE_CHOICE,)
+    marital_status = models.CharField(
+        max_length=255, choices=MARITAL_STATUS_CHOICE,
+    )
+    phone_number = models.CharField(max_length=14, blank=True)
 
     def __str__(self):
         return self.family_name
 
 
-class TargetPopulation(TimeStampedUUIDModel):
+class TargetPopulation(SessionModel):
+    tp_unicef_id = UUIDField(primary_key=True, version=4,)
     name = models.CharField(max_length=255)
-    population_type = models.PositiveIntegerField()
-    description = models.CharField(max_length=255, blank=True)
-    target_type = models.CharField(max_length=255)
-    households = models.ManyToManyField(
-        "Household", related_name="target_populations"
+    population_type = models.CharField(default="HOUSEHOLD")
+    targeting_criteria = models.TextField()
+
+    active_households = models.PositiveIntegerField(default=0)
+    inactive_households = models.PositiveIntegerField(default=0)
+    program = models.ForeignKey(
+        "cash_assist_datahub.Program",
+        db_column="program_id",
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
         return self.name
 
 
-class Program(TimeStampedUUIDModel):
-    ACTIVE = "ACTIVE"
-    COMPLETED = "COMPLETED"
-    STATUS_CHOICE = (
-        (ACTIVE, _("In progress")),
-        (COMPLETED, _("Done")),
+class TargetPopulationEntries(SessionModel):
+    target_population = models.ForeignKey(
+        "cash_assist_datahub.TargetPopulation", on_delete=models.CASCADE,
     )
-    program_ca_id = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
+    household = models.ForeignKey(
+        "cash_assist_datahub.Household", on_delete=models.CASCADE, null=True
+    )
+    individual = models.ForeignKey(
+        "cash_assist_datahub.Individual", on_delete=models.CASCADE, null=True
+    )
+    unhcr_household_id = models.CharField(max_length=255)
+
+
+class Program(SessionModel):
+    program_id = UUIDField(primary_key=True, version=4,)
+    business_area = models.CharField(max_length=20)
+    STATUS_CHOICE = (
+        ("NOT_STARTED", _("NOT_STARTED")),
+        ("STARTED", _("STARTED")),
+        ("COMPLETE", _("COMPLETE")),
+    )
+    SCOPE_CHOICE = (
+        ("FULL", _("Full")),
+        ("PARTIAL", _("Partial")),
+        ("NO_INTEGRATION", _("No Integration")),
+    )
+    program_unhcr_id = models.CharField(max_length=255)
+    program_hash_id = models.CharField(max_length=255)
+    programme_name = models.CharField(max_length=255)
     scope = models.PositiveIntegerField()
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
