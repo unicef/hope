feb548181d53aaf98fd5f282669b90d426cdd526
Jan Romaniak <romaniakjan@gmail.com>
2020-05-15 16:03:02 +0200

    changes in our models

--- backend/hct_mis_api/apps/payment/models.py
+++ backend/hct_mis_api/apps/payment/models.py
@@ -2,67 +2,59 @@ from decimal import Decimal
 
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.db.models import Sum
+from django.db.models import Sum, UUIDField
 from django.utils.translation import ugettext_lazy as _
 from model_utils import Choices
 
 from hct_mis_api.apps.utils.models import TimeStampedUUIDModel
 
 
-class PaymentRecord(TimeStampedUUIDModel):
+class PaymentRecord(SessionModel):
     STATUS_CHOICE = (
         ("SUCCESS", _("Sucess")),
         ("PENDING", _("Pending")),
         ("ERROR", _("Error")),
     )
-    status = models.CharField(max_length=255, choices=STATUS_CHOICE,)
-    name = models.CharField(max_length=255)
-    status_date = models.DateTimeField()
-    cash_assist_id = models.CharField(max_length=255)
-    cash_plan = models.ForeignKey(
-        "program.CashPlan",
-        on_delete=models.CASCADE,
-        related_name="payment_records",
+    ENTITLEMENT_CARD_STATUS_CHOICE = Choices(
+        ("ACTIVE", _("Active")), ("INACTIVE", _("Inactive")),
     )
-
-    household = models.ForeignKey(
-        "household.Household",
-        on_delete=models.CASCADE,
-        related_name="payment_records",
-    )
-    head_of_household = models.CharField(max_length=255)
-    total_person_covered = models.PositiveIntegerField()
-
-    distribution_modality = models.CharField(max_length=255,)
-
-    target_population = models.ForeignKey(
-        "targeting.TargetPopulation",
-        related_name="payment_records",
-        on_delete=models.CASCADE,
-    )
-    entitlement = models.OneToOneField(
-        "PaymentEntitlement",
-        on_delete=models.SET_NULL,
-        related_name="payment_record",
-        null=True,
-    )
-
-    payment_record_verification = models.OneToOneField(
-        "payment.PaymentRecordVerification",
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-
-
-class PaymentEntitlement(TimeStampedUUIDModel):
     DELIVERY_TYPE_CHOICE = (
         ("CASH", _("Cash")),
         ("DEPOSIT_TO_CARD", _("Deposit to Card")),
         ("TRANSFER", _("Transfer")),
     )
-    delivery_type = models.CharField(
-        max_length=255, choices=DELIVERY_TYPE_CHOICE,
+    business_area = models.CharField(max_length=20)
+    status = models.CharField(max_length=255, choices=STATUS_CHOICE,)
+    status_date = models.DateTimeField()
+    cash_plan = models.ForeignKey(
+        "CashPlan", on_delete=models.CASCADE, related_name="payment_records",
     )
+    unhcr_registration_id = models.CharField(max_length=255)
+    household = models.ForeignKey(
+        "Household", on_delete=models.CASCADE, related_name="payment_records",
+    )
+    # head of household
+    focal_point = models.ForeignKey(
+        "Individual", on_delete=models.CASCADE, related_name="payment_records",
+    )
+    full_name = models.CharField(max_length=255)
+    total_persons_covered = models.IntegerField()
+    distribution_modality = models.CharField(max_length=255,)
+    target_population = models.ForeignKey(
+        "targeting.TargetPopulation",
+        on_delete=models.CASCADE,
+        related_name="payment_records",
+    )
+    target_population_cash_assist_id = models.CharField(max_length=255)
+    entitlement_card_number = models.CharField(max_length=255,)
+    entitlement_card_status = models.CharField(
+        choices=STATUS_CHOICE, default="ACTIVE", max_length=20,
+    )
+    entitlement_card_issue_date = models.DateField()
+    delivery_type = models.CharField(
+        choices=DELIVERY_TYPE_CHOICE, default="ACTIVE", max_length=20,
+    )
+    currency = models.CharField(max_length=4,)
     entitlement_quantity = models.DecimalField(
         decimal_places=2,
         max_digits=12,
@@ -71,54 +63,22 @@ class PaymentEntitlement(TimeStampedUUIDModel):
     delivered_quantity = models.DecimalField(
         decimal_places=2,
         max_digits=12,
-        blank=True,
-        null=True,
         validators=[MinValueValidator(Decimal("0.01"))],
     )
-    entitlement_card_issue_date = models.DateField(blank=True, null=True)
-    entitlement_card_number = models.CharField(max_length=255)
-    currency = models.CharField(max_length=255)
-    delivery_date = models.DateTimeField(blank=True, null=True)
-    transaction_reference_id = models.CharField(max_length=255)
-    fsp = models.CharField(max_length=255)
-
-
-class VerificationProcess(TimeStampedUUIDModel):
-    VERIFICATION_TYPE_CHOICE = Choices(
-        ("RAPIDPRO", _("RapidPro")),
-        ("MANUAL", _("Manual")),
-        ("OTHER", _("Other")),
-    )
-    STATUS_CHOICE = Choices(
-        ("PENDING", _("Pending")),
-        ("ERROR", _("Error")),
-        ("SUCCESS", _("Success")),
-    )
-    payment_records_to_verify = models.PositiveIntegerField()
-    verification_type = models.CharField(
-        choices=VERIFICATION_TYPE_CHOICE, max_length=10,
-    )
-    status = models.CharField(choices=STATUS_CHOICE, max_length=10,)
-
-
-class PaymentRecordVerification(TimeStampedUUIDModel):
-    STATUS_CHOICE = Choices(("STARTED", _("Started")), ("ENDED", _("Ended")),)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    sample_size = models.PositiveIntegerField()
-    responded = models.PositiveIntegerField()
-    received = models.PositiveIntegerField()
-    not_received = models.PositiveIntegerField()
-    received_correct_amount = models.PositiveIntegerField()
-    received_wrong_amount = models.PositiveIntegerField()
-    verification_process = models.ForeignKey(
-        "payment.VerificationProcess",
-        related_name="verification_processes",
+    delivery_date = models.DateTimeField()
+    service_provider = models.ForeignKey(
+        "ServiceProvider",
         on_delete=models.CASCADE,
+        related_name="payment_records",
     )
 
-    @property
-    def total_number(self):
-        return self.paymentrecord.aggregate(total_number=Sum("household"),)[
-            "total_number"
-        ]
+
+class ServiceProvider(TimeStampedUUIDModel):
+    business_area = models.ForeignKey(
+        "core.BusinessArea", on_delete=models.CASCADE
+    )
+    cash_assist_id = models.CharField(max_length=255, primary_key=True)
+    full_name = models.CharField(max_length=255, primary_key=True)
+    short_name = models.CharField(max_length=4, primary_key=True)
+    country = models.CharField(max_length=3, primary_key=True)
+    vision_id = models.CharField(max_length=255, primary_key=True)
