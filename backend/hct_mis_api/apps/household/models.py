3155e4f6dc372454995d317ed3c7af6b0aae16c6
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-05-25 12:01:04 +0200

    Cash Plus fields for Individual

--- backend/hct_mis_api/apps/household/models.py
+++ backend/hct_mis_api/apps/household/models.py
@@ -259,6 +259,8 @@ class Individual(TimeStampedUUIDModel):
     )
     disability = models.BooleanField(default=False,)
     flex_fields = JSONField(default=dict)
+    enrolled_in_nutrition_programme = models.BooleanField(default=False)
+    administration_of_rutf = models.BooleanField(default=False)
 
     @property
     def age(self):
