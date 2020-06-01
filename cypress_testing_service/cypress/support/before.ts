be25f38e1656150c3fdffb54367e8f4ef6ef1b3c
Grzegorz Duda <grzegorz.duda@tivix.com>
2020-06-01 14:39:44 +0200

    chore: ability to authenticate with cookies & storage

--- cypress_testing_service/cypress/support/before.ts
+++ cypress_testing_service/cypress/support/before.ts
@@ -1,10 +1,5 @@
 before(() => {
   cy.clearLocalStorage();
+  cy.clearCookiesWhitelist();
   cy.clearCookies();
-
-  cy.login({ role: 'countryAdmin' });
-
-  cy.get<{ username: string }>('@loggedUser').then(({ username }) => {
-    cy.get('header').should('be.visible').contains(username);
-  });
 });
--- cypress_testing_service/cypress/support/localStorage.ts
+++ cypress_testing_service/cypress/support/localStorage.ts
@@ -4,15 +4,19 @@ const { clear } = Cypress.LocalStorage;
 // Workaround allowing to whitelist localStorage entries,
 // so that specific items wouldn't be cleared between tests.
 // Ref. to https://github.com/cypress-io/cypress/issues/461.
-(function (root: Window) {
-  Cypress.LocalStorage.clear = function (keys) {
-    const updatedKeys =
-      (keys || []).length > 0
-        ? keys
-        : Object.keys(root.localStorage || {}).filter(
-          (key) => Cypress.env('whitelist').localStorage.indexOf(key) === -1,
-        );
+Cypress.LocalStorage.clear = (keys) => {
+  const getWhitelistedKeys = () => {
+    const { role } = Cypress.env('currentUser') || {};
+    const whitelist = Cypress._.get(
+      Cypress.env(role),
+      'whitelist.localstorage',
+      [],
+    );
 
-    return clear.apply(root, updatedKeys);
+    return Object.keys(localStorage || {}).filter(
+      (key) => whitelist.indexOf(key) === -1,
+    );
   };
-})(window);
+
+  return clear.apply((keys || []).length > 0 ? keys : getWhitelistedKeys());
+};
