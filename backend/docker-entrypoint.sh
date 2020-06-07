aacace49ede0c272c2c5e9fa9919aa8ce4f16476
Mikolaj Kosmal <mikolaj.kosmal@tivix.com>
2020-06-07 19:44:45 +0200

    [OPS] Updated backend entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -8,6 +8,10 @@ else
         "dev")
         until pg_isready -h db -p 5432;
           do echo "waiting for database"; sleep 2; done;
+        until pg_isready -h $POSTGRES_CASHASSIST_DATAHUB_HOST -p 5432;
+          do echo "waiting for database"; sleep 2; done;
+        until pg_isready -h $POSTGRES_REGISTRATION_DATAHUB_HOST -p 5432;
+          do echo "waiting for database"; sleep 2; done;
         sh ./create_schemas.sh
         python manage.py collectstatic --no-input
         python manage.py migrate
