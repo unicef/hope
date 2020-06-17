bde356b1ac43eb11dfbe0584c921da2dc73f2e9e
Jan Romaniak <romaniakjan@gmail.com>
2020-06-17 11:52:05 +0200

    visions models

---
+++ backend/hct_mis_api/apps/erp_datahub/models.py
@@ -0,0 +1,85 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
+
+from utils.models import AbstractSession
+from django.db import models
+
+
+class Session(AbstractSession):
+    pass
+
+
+class SessionModel(models.Model):
+    session = models.ForeignKey("Session", on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class FundsCommitment(SessionModel):
+    business_area = models.CharField(max_length=20)
+    funds_commitment_number = models.CharField(max_length=255, primary_key=True)
+    document_type = models.CharField(max_length=10)
+    document_text = models.TextField()
+    currency_code = models.CharField(max_length=4, null=True)
+    g_l_account = models.IntegerField(null=True)
+    commitment_amount_local = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+        null=True,
+    )
+    commitment_amount_usd = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+        null=True,
+    )
+    total_open_amount_local = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+    )
+    total_open_amount_usd = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+        null=True,
+    )
+    exchange_rate = models.DecimalField(
+        decimal_places=6,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+        null=True,
+    )
+    vendor_id = models.CharField(max_length=255, null=True)
+    posting_date = models.DateField(null=True)
+    vision_approval = models.DateField(null=True)
+
+
+class DownPayment(SessionModel):
+    business_area = models.CharField(max_length=20)
+    down_payment_number = models.CharField(max_length=255, primary_key=True)
+    document_type = models.CharField(max_length=10)
+    consumer_fc_number = models.CharField(max_length=255)
+    total_down_payment_amount_local = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+    )
+    total_down_payment_amount_usd = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+        null=True,
+    )
+    currency_code = models.CharField(max_length=4, null=True)
+    exchange_rate = models.DecimalField(
+        decimal_places=6,
+        max_digits=12,
+        validators=[MinValueValidator(Decimal("0.01"))],
+        null=True,
+    )
+    vendor_id = models.CharField(max_length=255, null=True)
+    posting_date = models.DateField(null=True)
