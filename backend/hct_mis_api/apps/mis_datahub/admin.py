b6e4cf9837030327772282e33535904c7132997f
Jan Romaniak <romaniakjan@gmail.com>
2020-06-17 18:10:23 +0200

    admin pages

--- backend/hct_mis_api/apps/mis_datahub/admin.py
+++ backend/hct_mis_api/apps/mis_datahub/admin.py
@@ -1,7 +1,13 @@
 # Register your models here.
 from django.contrib import admin
 
-from mis_datahub.models import Household, Individual
+from mis_datahub.models import (
+    Household,
+    Individual,
+    Session,
+    TargetPopulation,
+    Program,
+)
 
 
 @admin.register(Household)
@@ -11,4 +17,19 @@ class HouseholdAdmin(admin.ModelAdmin):
 
 @admin.register(Individual)
 class IndividualAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TargetPopulation)
+class SessionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
     pass
