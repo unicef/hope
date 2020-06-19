3a62d8ab1b928f457e83c82a974bb8bbe61a86fb
Jan Romaniak <romaniakjan@gmail.com>
2020-06-19 12:15:48 +0200

    fix

--- backend/hct_mis_api/urls.py
+++ backend/hct_mis_api/urls.py
@@ -9,21 +9,29 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
 
 import registration_datahub.views
-from core.views import homepage, schema, trigger_error, logout_view
+from core.views import (
+    homepage,
+    schema,
+    trigger_error,
+    logout_view,
+    call_command_view,
+)
+
 
 urlpatterns = [
-    path("api/admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls ),
+    path("api/admin/call-command", call_command_view),
     path("", homepage),
     path("_health", homepage),
     path("api/_health", homepage),
-    path('api/graphql/schema.graphql', schema),
+    path("api/graphql/schema.graphql", schema),
     path(
         "api/graphql", csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))
     ),
     path("api/", include("social_django.urls", namespace="social")),
-    path("api/logout",logout_view),
-    path('api/sentry-debug/', trigger_error),
-    path("api/download-template", registration_datahub.views.download_template)
+    path("api/logout", logout_view),
+    path("api/sentry-debug/", trigger_error),
+    path("api/download-template", registration_datahub.views.download_template),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
