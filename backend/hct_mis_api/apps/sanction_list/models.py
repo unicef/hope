3ac9b5bc43858b5659e25fa1a2e4e8ed250e1a2f
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-07-01 12:32:53 +0200

    Sanction List - part 2 - airflow task to load xml - airflow task to check and send results - models changes - mutation to upload file and trigger check

--- backend/hct_mis_api/apps/sanction_list/models.py
+++ backend/hct_mis_api/apps/sanction_list/models.py
@@ -51,14 +51,17 @@ class SanctionListIndividual(TimeStampedUUIDModel):
     designation = models.TextField(blank=True, default="")
     list_type = models.CharField(max_length=50)
     quality = models.CharField(max_length=50, blank=True, default="")
+    # TODO: don't know if we need alias name, if yes then this should be moved
+    #  to another model, because there can be multiple alias names
     alias_name = models.CharField(max_length=255, blank=True, default="")
     street = models.CharField(max_length=255, blank=True, default="")
     city = models.CharField(max_length=255, blank=True, default="")
     state_province = models.CharField(max_length=255, blank=True, default="")
     address_note = models.CharField(max_length=255, blank=True, default="")
     date_of_birth = models.DateField(null=True, blank=True, default=None)
+    # for handling only year and years between
     year_of_birth = models.PositiveIntegerField(null=True, default=None)
-    exact_date = models.BooleanField(default=True)
+    second_year_of_birth = models.PositiveIntegerField(null=True, default=None)
     country_of_birth = CountryField(blank=True, default="")
     active = models.BooleanField(default=True)
     history = AuditlogHistoryField(pk_indexable=False)
@@ -103,6 +106,11 @@ class SanctionListIndividualCountries(TimeStampedUUIDModel):
         on_delete=models.CASCADE,
         related_name="countries",
     )
+
+
+class UploadedXLSXFile(TimeStampedUUIDModel):
+    file = models.FileField()
+    associated_email = models.EmailField()
 
 
 auditlog.register(SanctionListIndividual)
