3b2ebbce49cf8553ce6a86e19c1109892189318f
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-07-06 10:35:50 +0200

    add FormikCheckbox, FormikMultiSelect, FormikSlider components

--- frontend/src/components/payments/NewPaymentVerificationDialog.tsx
+++ frontend/src/components/payments/NewPaymentVerificationDialog.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
 import styled from 'styled-components';
 import {
   Button,
@@ -7,6 +8,7 @@ import {
   Tabs,
   Tab,
   Typography,
+  Box,
 } from '@material-ui/core';
 import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
 
@@ -14,6 +16,9 @@ import { useSnackbar } from '../../hooks/useSnackBar';
 import { Dialog } from '../../containers/dialogs/Dialog';
 import { DialogActions } from '../../containers/dialogs/DialogActions';
 import { TabPanel } from '../TabPanel';
+import { FormikSliderField } from '../../shared/Formik/FormikSliderField';
+import { FormikCheckboxField } from '../../shared/Formik/FormikCheckboxField';
+import { FormikMultiSelectField } from '../../shared/Formik/FormikMultiSelectField/FormikMultiSelectField';
 
 const DialogTitleWrapper = styled.div`
   border-bottom: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
@@ -40,6 +45,9 @@ const StyledTabs = styled(Tabs)`
 const TabsContainer = styled.div`
   border-bottom: 1px solid #e8e8e8;
 `;
+const DialogContainer = styled.div`
+  width: 700px;
+`;
 
 export function NewPaymentVerificationDialog(): React.ReactElement {
   const [open, setOpen] = useState(false);
@@ -57,71 +65,171 @@ export function NewPaymentVerificationDialog(): React.ReactElement {
   //   showMessage('New verification plan created.');
   // };
 
-  const submit = () => console.log('submit');
+  const options = [
+    {
+      value: 1,
+      name: 'Oliver Hansen',
+    },
+    {
+      value: 2,
+      name: 'Van Henry',
+    },
+    {
+      value: 3,
+      name: 'April Tucker',
+    },
+    {
+      value: 4,
+      name: 'John James',
+    },
+    {
+      value: 5,
+      name: 'Jimmy Choo',
+    },
+    {
+      value: 6,
+      name: 'John Polasky',
+    },
+    {
+      value: 7,
+      name: 'Chris Cross',
+    },
+    {
+      value: 8,
+      name: 'Arthur Schwartz',
+    },
+    {
+      value: 9,
+      name: 'Bridget Hansen',
+    },
+    {
+      value: 10,
+      name: 'CJ Will',
+    },
+  ];
+
+  const initialValues = {
+    confidenceInterval: 8,
+    marginOfError: 24,
+    admin: false,
+    age: true,
+    sex: false,
+    filterAdminAreas: [],
+  };
 
   return (
-    <span>
-      <Button
-        color='primary'
-        variant='contained'
-        onClick={() => setOpen(true)}
-        data-cy='button-new-plan'
-      >
-        NEW VERIFICATION PLAN
-      </Button>
-      <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        scroll='paper'
-        aria-labelledby='form-dialog-title'
-      >
-        <DialogTitleWrapper>
-          <DialogTitle id='scroll-dialog-title'>
-            <Typography variant='h6'>Create Verification Plan</Typography>
-          </DialogTitle>
-        </DialogTitleWrapper>
-        <DialogContent>
-          {/* <DialogDescription>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ submitForm, values }) => (
+        <Form>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={() => setOpen(true)}
+            data-cy='button-new-plan'
+          >
+            NEW VERIFICATION PLAN
+          </Button>
+          <Dialog
+            open={open}
+            onClose={() => setOpen(false)}
+            scroll='paper'
+            aria-labelledby='form-dialog-title'
+          >
+            <DialogTitleWrapper>
+              <DialogTitle id='scroll-dialog-title'>
+                Create Verification Plan
+              </DialogTitle>
+            </DialogTitleWrapper>
+            <DialogContent>
+              <DialogContainer>
+                {/* <DialogDescription>
             <div>description part</div>
           </DialogDescription> */}
-          <TabsContainer>
-            <StyledTabs
-              value={selectedTab}
-              onChange={(event: React.ChangeEvent<{}>, newValue: number) =>
-                setSelectedTab(newValue)
-              }
-              indicatorColor='primary'
-              textColor='primary'
-              variant='fullWidth'
-              aria-label='full width tabs example'
-            >
-              <Tab label='FULL LIST' />
-              <Tab label='RANDOM SAMPLING' />
-            </StyledTabs>
-          </TabsContainer>
-          <TabPanel value={selectedTab} index={0}>
-            <div>full list here</div>
-          </TabPanel>
-          <TabPanel value={selectedTab} index={1}>
-            <div>random sampling</div>
-          </TabPanel>
-        </DialogContent>
-        <DialogFooter>
-          <DialogActions>
-            <Button onClick={() => setOpen(false)}>CANCEL</Button>
-            <Button
-              startIcon={<CheckRoundedIcon />}
-              type='submit'
-              color='primary'
-              variant='contained'
-              onClick={submit}
-              data-cy='button-submit'
-            >
-              SAVE
-            </Button>
-          </DialogActions>
-        </DialogFooter>
-      </Dialog>
-    </span>
+                <TabsContainer>
+                  <StyledTabs
+                    value={selectedTab}
+                    onChange={(
+                      event: React.ChangeEvent<{}>,
+                      newValue: number,
+                    ) => setSelectedTab(newValue)}
+                    indicatorColor='primary'
+                    textColor='primary'
+                    variant='fullWidth'
+                    aria-label='full width tabs example'
+                  >
+                    <Tab label='FULL LIST' />
+                    <Tab label='RANDOM SAMPLING' />
+                  </StyledTabs>
+                </TabsContainer>
+                <TabPanel value={selectedTab} index={0}>
+                  <div>full list here</div>
+                </TabPanel>
+                <TabPanel value={selectedTab} index={1}>
+                  <Box pt={3}>
+                    <Field
+                      name='confidenceInterval'
+                      label='Confidence Interval'
+                      component={FormikSliderField}
+                    />
+                    <Field
+                      name='marginOfError'
+                      label='Margin of Error'
+                      component={FormikSliderField}
+                    />
+                    <Typography variant='caption'>Cluster Filters</Typography>
+                    <Box display='flex'>
+                      <Field
+                        name='admin'
+                        label='Admin'
+                        color='primary'
+                        component={FormikCheckboxField}
+                      />
+                      <Field
+                        name='age'
+                        label='Age'
+                        color='primary'
+                        component={FormikCheckboxField}
+                      />
+                      <Field
+                        name='sex'
+                        label='Sex'
+                        color='primary'
+                        component={FormikCheckboxField}
+                      />
+                    </Box>
+                    <Field
+                      name='filterAdminAreas'
+                      options={options}
+                      label='Filter Out Admin Areas'
+                      component={FormikMultiSelectField}
+                    />
+                  </Box>
+                </TabPanel>
+              </DialogContainer>
+            </DialogContent>
+            <DialogFooter>
+              <DialogActions>
+                <Button onClick={() => setOpen(false)}>CANCEL</Button>
+                <Button
+                  startIcon={<CheckRoundedIcon />}
+                  type='submit'
+                  color='primary'
+                  variant='contained'
+                  onClick={submitForm}
+                  data-cy='button-submit'
+                >
+                  SAVE
+                </Button>
+              </DialogActions>
+            </DialogFooter>
+          </Dialog>
+        </Form>
+      )}
+    </Formik>
   );
 }
