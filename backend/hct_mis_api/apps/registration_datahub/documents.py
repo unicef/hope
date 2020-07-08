e6a8132d07db40a52c3af4fba3ef2c907b0aacf7
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-07-08 16:08:05 +0200

    phonetic analyzer and filter

--- backend/hct_mis_api/apps/registration_datahub/documents.py
+++ backend/hct_mis_api/apps/registration_datahub/documents.py
@@ -1,21 +1,28 @@
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
 
+from core.es_analyzers import phonetic_analyzer
 from .models import ImportedIndividual
 
 
 @registry.register_document
 class ImportedIndividualDocument(Document):
+    given_name = fields.TextField(analyzer=phonetic_analyzer)
+    middle_name = fields.TextField(analyzer=phonetic_analyzer)
+    family_name = fields.TextField(analyzer=phonetic_analyzer)
+    full_name = fields.TextField(analyzer=phonetic_analyzer)
     phone_no = fields.TextField("phone_no.__str__")
     phone_no_alternative = fields.TextField("phone_no_alternative.__str__")
-    household = fields.ObjectField(properties={
-        'size': fields.IntegerField(),
-        'address': fields.TextField(),
-        'created_at': fields.DateField(),
-        'updated_at': fields.DateField(),
-        "country_origin": fields.TextField(attr="country_origin.__str__"),
-        "country": fields.TextField(attr="country.__str__"),
-    })
+    household = fields.ObjectField(
+        properties={
+            "size": fields.IntegerField(),
+            "address": fields.TextField(analyzer=phonetic_analyzer),
+            "created_at": fields.DateField(),
+            "updated_at": fields.DateField(),
+            "country_origin": fields.TextField(attr="country_origin.__str__"),
+            "country": fields.TextField(attr="country.__str__"),
+        }
+    )
 
     class Index:
         name = "importedindividuals"
@@ -25,10 +32,6 @@ class ImportedIndividualDocument(Document):
         model = ImportedIndividual
 
         fields = [
-            "full_name",
-            "given_name",
-            "middle_name",
-            "family_name",
             "relationship",
             "role",
             "sex",
