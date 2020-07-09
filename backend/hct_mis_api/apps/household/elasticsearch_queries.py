4e9f3566f85d5e83f8bb16e1c2cf38a3bc3628d9
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-07-09 11:53:16 +0200

    moved dockerfile, simple function for MLT

--- backend/hct_mis_api/apps/household/elasticsearch_queries.py
+++ backend/hct_mis_api/apps/household/elasticsearch_queries.py
@@ -1,7 +1,9 @@
+from typing import List, Union
+
 from elasticsearch_dsl.query import MoreLikeThis
 
 from household.documents import IndividualDocument
-
+from registration_datahub.documents import ImportedIndividualDocument
 
 """
 This file contains some test queries to play with ElasticSearch,
@@ -25,3 +27,34 @@ query2 = IndividualDocument.search().query(
     )
 )
 
+query3 = ImportedIndividualDocument.search().query(
+    MoreLikeThis(
+        like={
+            "_id": "170fa724-51f2-44df-a5be-df83dcf624da",
+            "_index": "importedindividuals",
+        },
+        min_term_freq=1,
+        fields=["given_name", "full_name"],
+        analyzer="phonetic",
+        minimum_should_match="75%",
+    )
+)
+
+
+def get_similar_objects(
+    document: Union[ImportedIndividualDocument, IndividualDocument],
+    object_id: str,
+    fields: List[str],
+    threshold: str = "75%",
+):
+    query = document.search().query(
+        MoreLikeThis(
+            like={"_id": object_id, "_index": document._index._name,},
+            min_term_freq=1,
+            fields=fields,
+            analyzer="phonetic",
+            minimum_should_match=threshold,
+        )
+    )
+
+    return query.execute()
