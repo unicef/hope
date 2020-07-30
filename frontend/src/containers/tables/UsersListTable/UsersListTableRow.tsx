d618e5b1d14e6bd30be73020babfda53908ec3fb
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-07-30 12:12:51 +0200

    add missing labels

--- frontend/src/containers/tables/UsersListTable/UsersListTableRow.tsx
+++ frontend/src/containers/tables/UsersListTable/UsersListTableRow.tsx
@@ -5,6 +5,7 @@ import TableCell from '@material-ui/core/TableCell';
 import { UserNode } from '../../../__generated__/graphql';
 
 import { TableRow } from '@material-ui/core';
+import { Missing } from '../../../components/Missing';
 
 const StatusContainer = styled.div`
   min-width: 120px;
@@ -19,8 +20,12 @@ export function UsersListTableRow({ user }: UsersListTableRowProps) {
   return (
     <TableRow key={user.id}>
       <TableCell align='left'>{`${user.firstName} ${user.lastName}`}</TableCell>
-      <TableCell align='left'>status</TableCell>
-      <TableCell align='left'>partner</TableCell>
+      <TableCell align='left'>
+        <Missing />
+      </TableCell>
+      <TableCell align='left'>
+        <Missing />
+      </TableCell>
       <TableCell align='left'>{user.email}</TableCell>
       <TableCell align='left'>
         <Moment format='DD/MM/YYYY'>{user.lastLogin}</Moment>
