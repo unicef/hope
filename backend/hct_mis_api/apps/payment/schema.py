3cd450441fc06804020ecfa671fef6c71ef3c509
Jan Romaniak <jan.romaniak@tivix.com>
2020-08-07 15:17:28 +0200

    szampling szize

--- backend/hct_mis_api/apps/payment/schema.py
+++ backend/hct_mis_api/apps/payment/schema.py
@@ -1,12 +1,16 @@
 import graphene
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from django_filters import FilterSet, OrderingFilter
 from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from core.extended_connection import ExtendedConnection
+from core.filters import filter_age
 from core.schema import ChoiceObject
-from core.utils import to_choice_object
+from core.utils import to_choice_object, decode_id_string
+from payment.inputs import GetCashplanVerificationSampleSizeInput
 from payment.models import (
     PaymentRecord,
     ServiceProvider,
@@ -14,6 +18,8 @@ from payment.models import (
     PaymentVerification,
 )
 from payment.rapid_pro.api import RapidProAPI
+from payment.utils import get_number_of_samples
+from program.models import CashPlan
 
 
 class PaymentRecordFilter(FilterSet):
@@ -120,6 +126,11 @@ class PaymentVerificationNode(DjangoObjectType):
         connection_class = ExtendedConnection
 
 
+class GetCashplanVerificationSampleSizeObject(graphene.ObjectType):
+    payment_record_count = graphene.Int()
+    sample_size = graphene.Int()
+
+
 class Query(graphene.ObjectType):
     payment_record = relay.Node.Field(PaymentRecordNode)
     payment_record_verification = relay.Node.Field(PaymentVerificationNode)
@@ -142,6 +153,60 @@ class Query(graphene.ObjectType):
     all_rapid_pro_flows = graphene.List(
         RapidProFlow, business_area_slug=graphene.String(required=True),
     )
+    sample_size = graphene.Field(
+        GetCashplanVerificationSampleSizeObject,
+        input=GetCashplanVerificationSampleSizeInput(),
+    )
+
+    def resolve_sample_size(self, info, **kwargs):
+        arg = lambda name: input.get(name)
+        cash_plan_id = decode_id_string(arg("cash_plan_id"))
+        cash_plan = get_object_or_404(CashPlan, id=cash_plan_id)
+        sampling = arg("sampling")
+        excluded_admin_areas = []
+        sex = None
+        age = None
+        confidence_interval = None
+        margin_of_error = None
+        payment_records = cash_plan.payment_records
+        if sampling == CashPlanPaymentVerification.SAMPLING_FULL_LIST:
+            excluded_admin_areas = arg("full_list_arguments").get(
+                "excluded_admin_areas", []
+            )
+        elif sampling == CashPlanPaymentVerification.SAMPLING_RANDOM:
+            random_sampling_arguments = arg("random_sampling_arguments")
+            confidence_interval = random_sampling_arguments.get(
+                "confidence_interval"
+            )
+            margin_of_error = random_sampling_arguments.get("margin_of_error")
+            sex = random_sampling_arguments.get("sex")
+            age = random_sampling_arguments.get("random_sampling_arguments")
+
+        payment_records = payment_records.filter(
+            ~(Q(household__admin_area__title__in=excluded_admin_areas))
+        )
+        if sex is not None:
+            payment_records = payment_records.filter(
+                household__head_of_household__sex=sex
+            )
+        if age is not None:
+            payment_records = filter_age(
+                "household__head_of_household__birth_date",
+                payment_records,
+                age.get(min),
+                age.get("max"),
+            )
+        payment_records_sample_count = payment_records.count()
+        if sampling == CashPlanPaymentVerification.SAMPLING_RANDOM:
+            payment_records_sample_count = get_number_of_samples(
+                payment_records_sample_count,
+                confidence_interval,
+                margin_of_error,
+            )
+        return {
+            "payment_record_count": cash_plan.payment_records.count(),
+            "sample_size": payment_records_sample_count,
+        }
 
     def resolve_all_rapid_pro_flows(self, info, business_area_slug, **kwargs):
         api = RapidProAPI(business_area_slug)
