155698f7dea8c630045c9ee7065eb42b9730714e
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-08-07 15:42:09 +0200

    Drop documents constraints

--- backend/hct_mis_api/apps/mis_datahub/models.py
+++ backend/hct_mis_api/apps/mis_datahub/models.py
@@ -186,6 +186,3 @@ class Document(SessionModel):
     number = models.CharField(max_length=255, null=True)
     individual_mis_id = models.UUIDField(null=True)
     type = models.CharField(max_length=50, choices=IDENTIFICATION_TYPE_CHOICE)
-
-    class Meta:
-        unique_together = ("type", "number")
