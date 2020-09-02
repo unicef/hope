1e12c838a09a7e80639048688e14c844dccc5866
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-09-02 18:20:27 +0200

    add deduperesults modals

---
+++ frontend/src/containers/registration/details/DedupeResults.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@material-ui/core';
+import {
+  ImportedIndividualMinimalFragment,
+  DeduplicationResultNode,
+} from '../../../__generated__/graphql';
+import { useHistory } from 'react-router-dom';
+import { MiśTheme } from '../../../theme';
+import { decodeIdString } from '../../../utils/utils';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { useBusinessArea } from '../../../hooks/useBusinessArea';
+
+const DialogTitleWrapper = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+`;
+
+const DialogFooter = styled.div`
+  padding: 12px 16px;
+  margin: 0;
+  border-top: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+  text-align: right;
+`;
+
+const DialogDescription = styled.div`
+  margin: 20px 0;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.54);
+`;
+const Error = styled.span`
+  color: ${({ theme }: { theme: MiśTheme }) => theme.hctPalette.red};
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+const Bold = styled.span`
+  font-weight: bold;
+  font-size: 16px;
+`;
+const Pointer = styled.span`
+  cursor: pointer;
+`;
+
+interface DedupeResultsProps {
+  individual: ImportedIndividualMinimalFragment;
+  status: string;
+  results: Array<DeduplicationResultNode>;
+}
+
+export function DedupeResults({
+  individual,
+  status,
+  results,
+}: DedupeResultsProps): React.ReactElement {
+  const [open, setOpen] = useState(false);
+  const history = useHistory();
+  const businessArea = useBusinessArea();
+  function createData(hitId, score, proximityToScore) {
+    return { hitId, score, proximityToScore };
+  }
+
+  const rows = results.map((result) => {
+    return createData(result.hitId, result.score, result.proximityToScore);
+  });
+  const handleClick = (id): void => {
+    const path = `/${businessArea}/population/individuals/${id}`;
+    history.push(path);
+  };
+  return (
+    <>
+      <Error onClick={() => setOpen(true)}>
+        {status} ({results.length})
+      </Error>
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        scroll='paper'
+        aria-labelledby='form-dialog-title'
+      >
+        <DialogTitleWrapper>
+          <DialogTitle id='scroll-dialog-title'>Duplicates</DialogTitle>
+        </DialogTitleWrapper>
+        <DialogContent>
+          <DialogDescription>
+            <div>
+              Duplicates of{' '}
+              <Bold>
+                {decodeIdString(individual.id)} {individual.fullName}
+              </Bold>{' '}
+              are listed below.
+            </div>
+          </DialogDescription>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ width: 100 }}>Individual ID</TableCell>
+                <TableCell style={{ width: 100 }} align='right'>
+                  Similarity Score
+                </TableCell>
+                <TableCell style={{ width: 100 }} align='right'>
+                  Proximity to the Score
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.hitId}>
+                  <TableCell
+                    onClick={() => handleClick(row.hitId)}
+                    component='th'
+                    scope='row'
+                  >
+                    <Pointer>{row.hitId}</Pointer>
+                  </TableCell>
+                  <TableCell align='right'>{row.score.toFixed(2)}</TableCell>
+                  <TableCell align='right'>
+                    {row.proximityToScore.toFixed(2)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </DialogContent>
+        <DialogFooter>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)}>CLOSE</Button>
+          </DialogActions>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
