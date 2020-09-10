fcedb856071312a22f7014ba7635d31b9d21f449
Jan Romaniak <romaniakjan@gmail.com>
2020-09-10 15:09:54 +0200

    discarding

---
+++ backend/hct_mis_api/apps/payment/tests/snapshots/snap_test_discard_verification_mutation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestDiscardVerificationMutation::test_discard_active 1'] = {
+    'data': {
+        'discardCashPlanPaymentVerification': {
+            'cashPlan': {
+                'id': 'Q2FzaFBsYW5Ob2RlOmQ5NTBiYTQ5LTc0OGUtNDBjNy05ZTI4LThhNTdkM2Y1MzFlZg=='
+            }
+        }
+    }
+}
