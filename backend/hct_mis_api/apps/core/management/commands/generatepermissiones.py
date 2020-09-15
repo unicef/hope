e0b170b880b1b6e8278a64d8f694b7b4e6dc9fa2
Jan Romaniak <romaniakjan@gmail.com>
2020-09-15 16:55:03 +0200

    changes to models and django admin

--- backend/hct_mis_api/apps/core/management/commands/generatepermissiones.py
+++ backend/hct_mis_api/apps/core/management/commands/generatepermissiones.py
@@ -15,17 +15,19 @@ from registration_datahub.models import (
 
 
 class Command(BaseCommand):
-    help = "Generate document types for all countries"
+    help = "Generate permissions"
 
     @transaction.atomic
     def handle(self, *args, **options):
-        self.stdout.write(self.style.WARNING("Generate document types for all countries"))
+        self.stdout.write(self.style.WARNING("Generate permissions"))
         self._generate_document_types_for_all_countries()
 
     def _generate_document_types_for_all_countries(self):
+        UserPermission.objects.all().delete()
         permissions_to_create = []
         for (permission_name, label) in UserPermission.PERMISSIONS_CHOICES:
             read_permission = UserPermission(name=permission_name)
             write_permission = UserPermission(name=permission_name, write=True)
             permissions_to_create.append(read_permission)
             permissions_to_create.append(write_permission)
+        UserPermission.objects.bulk_create(permissions_to_create)
