74988c81bd57c6c8052b651f688479eaae0d457f
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-09-15 09:58:55 +0200

    Add universal moment and replace all Moment instances.

---
+++ frontend/src/components/UniversalMoment.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Moment from 'react-moment';
+import { DATE_FORMAT } from '../config';
+
+export interface Props {
+  children: string;
+}
+
+export function UniversalMoment({ children }: Props): React.ReactElement {
+  return <Moment format={DATE_FORMAT}>{children}</Moment>;
+}
