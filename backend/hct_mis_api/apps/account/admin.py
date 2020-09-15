f6080177191316609d98bfb7b12be7f9b9a8d5b5
Jan Romaniak <romaniakjan@gmail.com>
2020-09-15 16:55:03 +0200

    changes to models and django admin

--- backend/hct_mis_api/apps/account/admin.py
+++ backend/hct_mis_api/apps/account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from account.models import User
+from account.models import User, UserRole
 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext, gettext_lazy as _
@@ -12,7 +12,25 @@ class UserAdmin(BaseUserAdmin):
     list_display = ("username", "email", "first_name")
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "business_areas")},),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions",),},),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "email", "business_areas")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "roles",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
+
+
+@admin.register(UserRole)
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display = ("name", "business_area")
--- backend/hct_mis_api/apps/account/models.py
+++ backend/hct_mis_api/apps/account/models.py
@@ -8,6 +8,7 @@ from utils.models import TimeStampedUUIDModel
 
 class User(AbstractUser, UUIDModel):
     business_areas = models.ManyToManyField("core.BusinessArea", blank=True)
+    roles = models.ManyToManyField("account.UserRole", blank=True, related_name="users")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -15,41 +16,34 @@ class User(AbstractUser, UUIDModel):
 
 class UserRole(TimeStampedUUIDModel):
     name = models.CharField(max_length=250)
-    business_areas = models.ForeignKey("core.BusinessArea", blank=True)
-    user = models.ManyToManyField("account.User", blank=True)
+    business_area = models.ForeignKey("core.BusinessArea", on_delete=models.CASCADE)
     permissions = models.ManyToManyField("account.UserPermission", related_name="roles")
+
+    def __str__(self):
+        return f"{self.name} in {self.business_area}"
 
 
 class UserPermission(TimeStampedUUIDModel):
     PERMISSION_DASHBOARD = 1
-    PERMISSION_RDI = 1
-    PERMISSION_POPULATION = 1
-    PERMISSION_PROGRAM_MANAGEMENT = 1
-    PERMISSION_TARGETING = 1
-    PERMISSION_PAYMENT_VERIFICATION = 1
-    PERMISSION_GRIEVANCES = 1
-    PERMISSION_REPORTING = 1
-    PERMISSION_USER_MANAGEMENT = 1
-    PERMISSION_SETTINGS = 1
+    PERMISSION_RDI = 2
+    PERMISSION_POPULATION = 3
+    PERMISSION_PROGRAM_MANAGEMENT = 4
+    PERMISSION_TARGETING = 5
+    PERMISSION_PAYMENT_VERIFICATION = 6
+    PERMISSION_GRIEVANCES = 7
+    PERMISSION_REPORTING = 8
+    PERMISSION_USER_MANAGEMENT = 9
+    PERMISSION_SETTINGS = 10
     PERMISSIONS_CHOICES = (
-        PERMISSION_DASHBOARD,
-        _("Dashboard"),
-        PERMISSION_RDI,
-        _("Registration Data Import"),
-        PERMISSION_POPULATION,
-        _("Population"),
-        PERMISSION_PROGRAM_MANAGEMENT,
-        _("Program Management"),
-        PERMISSION_TARGETING,
-        _("Targeting"),
-        PERMISSION_PAYMENT_VERIFICATION,
-        _("Payment Verification"),
-        PERMISSION_REPORTING,
-        _("Reporting"),
-        PERMISSION_USER_MANAGEMENT,
-        _("User Management"),
-        PERMISSION_SETTINGS,
-        _("Settings"),
+        (PERMISSION_DASHBOARD, _("Dashboard")),
+        (PERMISSION_RDI, _("Registration Data Import")),
+        (PERMISSION_POPULATION, _("Population")),
+        (PERMISSION_PROGRAM_MANAGEMENT, _("Program Management")),
+        (PERMISSION_TARGETING, _("Targeting")),
+        (PERMISSION_PAYMENT_VERIFICATION, _("Payment Verification")),
+        (PERMISSION_REPORTING, _("Reporting")),
+        (PERMISSION_USER_MANAGEMENT, _("User Management")),
+        (PERMISSION_SETTINGS, _("Settings")),
     )
     PERMISSIONS_CHOICES_DICT = dict(PERMISSIONS_CHOICES)
     name = models.CharField(max_length=250, choices=PERMISSIONS_CHOICES)
@@ -59,4 +53,4 @@ class UserPermission(TimeStampedUUIDModel):
         type_name = _("Read")
         if self.write:
             type_name = _("Write")
-        return f"{UserPermission.PERMISSIONS_CHOICES_DICT[self.name]} {type_name} Permission"
+        return f"{UserPermission.PERMISSIONS_CHOICES_DICT.get(int(self.name))} {type_name} Permission"
