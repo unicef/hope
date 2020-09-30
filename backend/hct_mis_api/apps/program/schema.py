73330ea5024c7bc09036491b88afc2a9c306c592
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-09-30 15:24:08 +0200

    Programme removed unused choices, filters and ordering, schema changes Sanction list last sync property for individual

--- backend/hct_mis_api/apps/program/schema.py
+++ backend/hct_mis_api/apps/program/schema.py
@@ -1,19 +1,18 @@
 import graphene
-from django.db.models import Case, When, Value, IntegerField, Q
-from django_filters import FilterSet, OrderingFilter, CharFilter
+from django.db.models import Case, When, Value, IntegerField, Q, Sum
+from django.db.models.functions import Coalesce
+from django_filters import FilterSet, OrderingFilter, CharFilter, MultipleChoiceFilter, ModelMultipleChoiceFilter
 from graphene import relay, ConnectionField
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from account.schema import LogEntryObjectConnection
 from account.permissions import (
     BaseNodePermissionMixin,
-    hopePermissionClass,
-    PERMISSION_PROGRAM,
-    PERMISSION_READ,
     DjangoPermissionFilterConnectionField,
 )
+from account.schema import LogEntryObjectConnection
 from core.extended_connection import ExtendedConnection
+from core.filters import IntegerRangeFilter, DecimalRangeFilter
 from core.schema import ChoiceObject
 from core.utils import to_choice_object
 from payment.models import CashPlanPaymentVerification
@@ -21,11 +20,23 @@ from program.models import Program, CashPlan
 
 
 class ProgramFilter(FilterSet):
-    business_area = CharFilter(field_name="business_area__slug")
+    business_area = CharFilter(field_name="business_area__slug", required=True)
+    search = CharFilter(method="search_filter")
+    status = MultipleChoiceFilter(field_name="status", choices=Program.STATUS_CHOICE)
+    sector = MultipleChoiceFilter(field_name="sector", choices=Program.STATUS_CHOICE)
+    number_of_households = IntegerRangeFilter(field_name="households_count")
+    budget = DecimalRangeFilter(field_name="budget")
 
     class Meta:
-        fields = ("id", "status")
+        fields = ("id", "status", "sector", "business_area", "search")
         model = Program
+
+    def search_filter(self, qs, name, value):
+        values = value.split(" ")
+        q_obj = Q()
+        for value in values:
+            q_obj |= Q(name__icontains=value)
+        return qs.filter(q_obj)
 
 
 class ProgramNode(BaseNodePermissionMixin, DjangoObjectType):
@@ -107,7 +118,8 @@ class CashPlanNode(DjangoObjectType):
 class Query(graphene.ObjectType):
     program = relay.Node.Field(ProgramNode)
     all_programs = DjangoPermissionFilterConnectionField(
-        ProgramNode, filterset_class=ProgramFilter,
+        ProgramNode,
+        filterset_class=ProgramFilter,
         # TODO Enable permissions below
         # permission_classes=(hopePermissionClass("PERMISSION_PROGRAM.LIST"),)
     )
@@ -120,14 +132,18 @@ class Query(graphene.ObjectType):
     cash_plan_status_choices = graphene.List(ChoiceObject)
 
     def resolve_all_programs(self, info, **kwargs):
-        return Program.objects.annotate(
-            custom_order=Case(
-                When(status=Program.DRAFT, then=Value(1)),
-                When(status=Program.ACTIVE, then=Value(2)),
-                When(status=Program.FINISHED, then=Value(3)),
-                output_field=IntegerField(),
+        return (
+            Program.objects.annotate(
+                custom_order=Case(
+                    When(status=Program.DRAFT, then=Value(1)),
+                    When(status=Program.ACTIVE, then=Value(2)),
+                    When(status=Program.FINISHED, then=Value(3)),
+                    output_field=IntegerField(),
+                )
             )
-        ).order_by("custom_order")
+            .annotate(households_count=Coalesce(Sum("cash_plans__total_persons_covered"), 0))
+            .order_by("custom_order", "start_date")
+        )
 
     def resolve_program_status_choices(self, info, **kwargs):
         return to_choice_object(Program.STATUS_CHOICE)
