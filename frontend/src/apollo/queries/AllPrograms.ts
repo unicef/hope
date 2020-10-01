ad3722067441f9d71ab8b76c689768faec63a6ca
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-10-01 16:14:38 +0200

    Population Individual details view - reorganize sanction list last screening date

--- frontend/src/apollo/queries/AllPrograms.ts
+++ frontend/src/apollo/queries/AllPrograms.ts
@@ -1,7 +1,7 @@
 import { gql } from 'apollo-boost';
 
 export const ALL_PROGRAMS_QUERY = gql`
-  query AllPrograms($businessArea: String, $status: String) {
+  query AllPrograms($businessArea: String!, $status: [String]) {
     allPrograms(businessArea: $businessArea, status: $status) {
       pageInfo {
         hasNextPage
