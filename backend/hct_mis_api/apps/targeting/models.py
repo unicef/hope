85b09848f2b340460811c78d0a2c93dbb9173a74
Jan Romaniak <jan.romaniak@tivix.com>
2020-10-09 15:44:43 +0200

    Targeting changes

--- backend/hct_mis_api/apps/targeting/models.py
+++ backend/hct_mis_api/apps/targeting/models.py
@@ -371,9 +371,135 @@ class TargetingCriteriaRuleFilter(TimeStampedUUIDModel):
         flex_field_attr = FlexibleAttribute.objects.get(name=self.field_name)
         if not flex_field_attr:
             raise ValidationError(
-                f"There are no Core Field Attributes associated with this fieldName {self.field_name}"
+                f"There are no Flex Field Attributes associated with this fieldName {self.field_name}"
             )
         lookup = f"{'individuals__' if flex_field_attr.associated_with else ''}flex_fields__{flex_field_attr.name}"
+        return self.get_query_for_lookup(lookup, select_many=flex_field_attr.type == "SELECT_MANY",)
+
+    def get_query(self):
+        if not self.is_flex_field:
+            return self.get_query_for_core_field()
+        return self.get_query_for_flex_field()
+
+
+class TargetingIndividualSubcriteriaRuleFilter(TimeStampedUUIDModel):
+    """
+    This is one explicit filter like:
+        :Age <> 10-20
+        :Residential Status = Refugee
+        :Residential Status != Refugee
+    """
+
+    COMPARISION_ATTRIBUTES = {
+        "EQUALS": {
+            "arguments": 1,
+            "lookup": "",
+            "negative": False,
+            "supported_types": ["INTEGER", "SELECT_ONE", "STRING"],
+        },
+        "NOT_EQUALS": {"arguments": 1, "lookup": "", "negative": True, "supported_types": ["INTEGER", "SELECT_ONE"]},
+        "CONTAINS": {
+            "min_arguments": 1,
+            "arguments": 1,
+            "lookup": "__icontains",
+            "negative": False,
+            "supported_types": ["SELECT_MANY", "STRING"],
+        },
+        "NOT_CONTAINS": {"arguments": 1, "lookup": "__icontains", "negative": True, "supported_types": ["STRING"]},
+        "RANGE": {"arguments": 2, "lookup": "__range", "negative": False, "supported_types": ["INTEGER"]},
+        "NOT_IN_RANGE": {"arguments": 2, "lookup": "__range", "negative": True, "supported_types": ["INTEGER"]},
+        "GREATER_THAN": {"arguments": 1, "lookup": "__gt", "negative": False, "supported_types": ["INTEGER"]},
+        "LESS_THAN": {"arguments": 1, "lookup": "__lt", "negative": False, "supported_types": ["INTEGER"]},
+    }
+
+    COMPARISON_CHOICES = Choices(
+        ("EQUALS", _("Equals")),
+        ("NOT_EQUALS", _("Not Equals")),
+        ("CONTAINS", _("Contains")),
+        ("NOT_CONTAINS", _("Does not contain")),
+        ("RANGE", _("In between <>")),
+        ("NOT_IN_RANGE", _("Not in between <>")),
+        ("GREATER_THAN", _("Greater than")),
+        ("LESS_THAN", _("Less than")),
+    )
+    comparision_method = models.CharField(max_length=20, choices=COMPARISON_CHOICES,)
+    targeting_criteria_rule = models.ForeignKey(
+        "TargetingCriteriaRule", related_name="individual_subcriteria_filters", on_delete=models.CASCADE,
+    )
+    is_flex_field = models.BooleanField(default=False)
+    field_name = models.CharField(max_length=50)
+    arguments = JSONField(
+        help_text="""
+            Array of arguments
+            """
+    )
+
+    def get_query_for_lookup(self, lookup, select_many=False):
+        comparision_attribute = TargetingCriteriaRuleFilter.COMPARISION_ATTRIBUTES.get(self.comparision_method)
+        args_count = comparision_attribute.get("arguments")
+        if self.arguments is None:
+            raise ValidationError(
+                f"{self.field_name} {self.comparision_method} filter query expect {args_count} " f"arguments"
+            )
+        args_input_count = len(self.arguments)
+        if select_many:
+            if args_input_count < 1:
+                raise ValidationError(
+                    f"{self.field_name} SELECT MULTIPLE CONTAINS filter query expect at least 1 argument"
+                )
+        elif args_count != args_input_count:
+            raise ValidationError(
+                f"{self.field_name} {self.comparision_method} filter query expect {args_count} "
+                f"arguments gets {args_input_count}"
+            )
+        argument = self.arguments if args_input_count > 1 else self.arguments[0]
+
+        if select_many:
+            query = Q(**{f"{lookup}__contains": argument})
+        else:
+            query = Q(**{f"{lookup}{comparision_attribute.get('lookup')}": argument})
+        if comparision_attribute.get("negative"):
+            return ~query
+        return query
+
+    def get_query_for_core_field(self):
+        core_fields = CORE_FIELDS_ATTRIBUTES
+        core_field_attrs = [attr for attr in core_fields if attr.get("name") == self.field_name]
+        if len(core_field_attrs) != 1:
+            raise ValidationError(
+                f"There are no Core Field Attributes associated with this fieldName {self.field_name}"
+            )
+        core_field_attr = core_field_attrs[0]
+        if core_field_attr['associated_with'] != _INDIVIDUAL:
+            raise ValidationError(
+                f"Only Core Fields associated with individuals can be used inside Individual Subcriteria"
+            )
+        # TODO Johniak implement age filter
+        get_individual_subcriteria_query = core_field_attr.get("get_individual_subcriteria_query")
+        if get_individual_subcriteria_query:
+            return get_individual_subcriteria_query(self.comparision_method, self.arguments)
+        lookup = core_field_attr.get("lookup")
+        if not lookup:
+            raise ValidationError(
+                f"Core Field Attributes associated with this fieldName {self.field_name}"
+                f" doesn't have get_query method or lookup field"
+            )
+        return self.get_query_for_lookup(
+            f"{lookup}",
+            select_many=core_field_attr.get("type") == "SELECT_MANY",
+        )
+
+    def get_query_for_flex_field(self):
+        flex_field_attr = FlexibleAttribute.objects.get(name=self.field_name)
+        if not flex_field_attr:
+            raise ValidationError(
+                f"There are no Flex Field Attributes associated with this fieldName {self.field_name}"
+            )
+        if flex_field_attr['associated_with'] != _INDIVIDUAL:
+            raise ValidationError(
+                f"Only Flex Fields associated with individuals can be used inside Individual Subcriteria"
+            )
+        lookup = f"flex_fields__{flex_field_attr.name}"
         return self.get_query_for_lookup(lookup, select_many=flex_field_attr.type == "SELECT_MANY",)
 
     def get_query(self):
---
+++ backend/hct_mis_api/apps/targeting/tests/test_individual_subcriteria.py
@@ -0,0 +1,38 @@
+from django.core.exceptions import ValidationError
+from django.core.management import call_command
+from django.test import TestCase
+
+from core.models import BusinessArea
+from household.fixtures import create_household
+from household.models import Household, Individual, FEMALE, MALE
+from targeting.models import TargetingCriteriaRuleFilter
+
+
+class TestIndividualSubcriteria(TestCase):
+    def setUp(self):
+        call_command("loadflexfieldsattributes")
+        call_command("loadbusinessareas")
+        business_area = BusinessArea.objects.first()
+        (household, individuals) = create_household(
+            {
+                "size": 2,
+                "business_area": business_area,
+            }
+        )
+        individuals[0].sex = FEMALE
+        individuals[1].sex = MALE
+        individuals[1].marital_status = "MARRIED"
+        self.household_targeted = household
+        (household, individuals) = create_household(
+            {
+                "size": 1,
+                "business_area": business_area,
+            },
+            {"sex": FEMALE},
+        )
+        self.not_targeted_household = household
+
+    def test_all_individuals_are_female(self):
+        query = Household.objects.all()
+        self.assertEqual(query.count(), 1)
+        self.assertEqual(query.first().id, self.household_targeted.id)
