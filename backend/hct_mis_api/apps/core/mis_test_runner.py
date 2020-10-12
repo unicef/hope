d67d04a9a6d898d866ae5e6ac5a9c087ddba6465
Jan Romaniak <romaniakjan@gmail.com>
2020-10-12 19:07:39 +0200

    Fix using snapshot runner

--- backend/hct_mis_api/apps/core/mis_test_runner.py
+++ backend/hct_mis_api/apps/core/mis_test_runner.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 
+import os
+
+import xmlrunner
 from django.conf import settings
 from django.db import connections
 from snapshottest.django import TestRunner
@@ -139,7 +142,50 @@ def _setup_schema_database(verbosity, interactive, keepdb=False, debug_sql=False
     return [old_name]
 
 
-class PostgresTestRunner(XMLTestRunner):
+class PostgresTestRunner(TestRunner):
+    test_runner = xmlrunner.XMLTestRunner
+
+    def get_resultclass(self):
+        # Django provides `DebugSQLTextTestResult` if `debug_sql` argument is True
+        # To use `xmlrunner.result._XMLTestResult` we supress default behavior
+        return None
+
+    def get_test_runner_kwargs(self):
+        # We use separate verbosity setting for our runner
+        verbosity = getattr(settings, 'TEST_OUTPUT_VERBOSE', 1)
+        if isinstance(verbosity, bool):
+            verbosity = (1, 2)[verbosity]
+        verbosity = verbosity  # not self.verbosity
+
+        output_dir = getattr(settings, 'TEST_OUTPUT_DIR', '.')
+        single_file = getattr(settings, 'TEST_OUTPUT_FILE_NAME', None)
+
+        # For single file case we are able to create file here
+        # But for multiple files case files will be created inside runner/results
+        if single_file is None:  # output will be a path (folder)
+            output = output_dir
+        else:  # output will be a stream
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            file_path = os.path.join(output_dir, single_file)
+            output = open(file_path, 'wb')
+
+        return dict(
+            verbosity=verbosity,
+            descriptions=getattr(settings, 'TEST_OUTPUT_DESCRIPTIONS', False),
+            failfast=self.failfast,
+            resultclass=self.get_resultclass(),
+            output=output,
+        )
+
+    def run_suite(self, suite, **kwargs):
+        runner_kwargs = self.get_test_runner_kwargs()
+        runner = self.test_runner(**runner_kwargs)
+        results = runner.run(suite)
+        if hasattr(runner_kwargs['output'], 'close'):
+            runner_kwargs['output'].close()
+        return results
+
     def teardown_databases(self, old_config, **kwargs):
         connections["cash_assist_datahub_ca"].close()
         connections["cash_assist_datahub_erp"].close()
@@ -173,7 +219,6 @@ class PostgresTestRunner(XMLTestRunner):
                         keepdb=self.keepdb,
                         serialize=connection.settings_dict.get("TEST", {}).get("SERIALIZE", True),
                     )
-        print(aliases)
         old_names.extend(super().setup_databases(**kwargs))
 
         return old_names
