a9c21c9ef447fefc1714ead77d945bc1ea320273
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-10-13 09:34:52 +0200

    fix invalid date in programmes filters

--- frontend/src/containers/tables/ProgrammesTable/ProgrammesFilter.tsx
+++ frontend/src/containers/tables/ProgrammesTable/ProgrammesFilter.tsx
@@ -128,7 +128,7 @@ export function ProgrammesFilters({
                 onChange={(date) =>
                   onFilterChange({
                     ...filter,
-                    startDate: moment(date).format('YYYY-MM-DD'),
+                    startDate: date ? moment(date).format('YYYY-MM-DD') : null,
                   })
                 }
                 value={filter.startDate || null}
@@ -147,7 +147,7 @@ export function ProgrammesFilters({
                 onChange={(date) =>
                   onFilterChange({
                     ...filter,
-                    endDate: moment(date).format('YYYY-MM-DD'),
+                    endDate: date ? moment(date).format('YYYY-MM-DD') : null,
                   })
                 }
                 value={filter.endDate || null}
