1aac74a18d5c058e9771740afba9cc3a2699558a
Jan Romaniak <romaniakjan@gmail.com>
2020-10-13 16:59:59 +0200

    Added sets of targeting criteria

--- frontend/src/components/TargetPopulation/FieldChooser.tsx
+++ frontend/src/components/TargetPopulation/FieldChooser.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Field } from 'formik';
-import { CriteriaAutocomplete } from './TargetingCriteria/CriteriaAutocomplete';
 import { IconButton } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
+import { CriteriaAutocomplete } from './TargetingCriteria/CriteriaAutocomplete';
 
 const FlexWrapper = styled.div`
   display: flex;
@@ -11,37 +11,39 @@ const FlexWrapper = styled.div`
 `;
 
 export function FieldChooser({
-                               onChange,
-                               fieldName,
-                               onClick,
-                               filters,
-                               index,
-                               choices,
-                             }: {
+  onChange,
+  fieldName,
+  onClick,
+  index,
+  choices,
+  baseName,
+  showDelete,
+}: {
   index: number;
   choices;
   fieldName: string;
   onChange: (e, object) => void;
-  filters;
   onClick: () => void;
+  baseName: string;
+  showDelete: boolean;
 }): React.ReactElement {
   return (
-      <FlexWrapper>
-        <Field
-            name={`filters[${index}].fieldName`}
-            label='Choose field type'
-            required
-            choices={choices}
-            index={index}
-            value={fieldName || null}
-            onChange={onChange}
-            component={CriteriaAutocomplete}
-        />
-        {filters.length > 1 && (
-            <IconButton onClick={onClick}>
-              <Delete />
-            </IconButton>
-        )}
-      </FlexWrapper>
+    <FlexWrapper>
+      <Field
+        name={`${baseName}.fieldName`}
+        label='Choose field type'
+        required
+        choices={choices}
+        index={index}
+        value={fieldName || null}
+        onChange={onChange}
+        component={CriteriaAutocomplete}
+      />
+      {showDelete && (
+        <IconButton onClick={onClick}>
+          <Delete />
+        </IconButton>
+      )}
+    </FlexWrapper>
   );
 }
