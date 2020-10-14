7e07c7938f33c0e058f2afeae072c8f81318cba0
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-10-14 14:05:00 +0200

    add hoh checkbox for each filter

--- frontend/src/containers/forms/TargetCriteriaFilter.tsx
+++ frontend/src/containers/forms/TargetCriteriaFilter.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { SubField } from '../../components/TargetPopulation/SubField';
 import { ImportedIndividualFieldsQuery } from '../../__generated__/graphql';
 import { FieldChooser } from '../../components/TargetPopulation/FieldChooser';
+import { Field } from 'formik';
+import { FormikCheckboxField } from '../../shared/Formik/FormikCheckboxField';
 
 const Divider = styled.div`
   border-top: 1px solid #b1b1b5;
@@ -60,6 +62,11 @@ export function TargetingCriteriaFilter({
       {each.fieldName && (
         <div data-cy='autocomplete-target-criteria-values'>
           <SubField field={each} index={index} baseName={`filters[${index}]`} />
+          <Field
+            name={`filters[${index}].onlyForHoH`}
+            label='Head of Household'
+            component={FormikCheckboxField}
+          />
         </div>
       )}
       {shouldShowDivider && (
