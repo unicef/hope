72d7c253f45d1322773953226a39d201277cd4d2
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-10-16 15:55:28 +0200

    wrapping fixture arg name in LazyFunction

--- backend/hct_mis_api/apps/registration_data/fixtures.py
+++ backend/hct_mis_api/apps/registration_data/fixtures.py
@@ -17,11 +17,20 @@ class RegistrationDataImportFactory(factory.DjangoModelFactory):
         model = RegistrationDataImport
         django_get_or_create = ("name",)
 
-    name = f"{faker.sentence(nb_words=3, variable_nb_words=True, ext_word_list=None)} - {time.time_ns()}"
+    name = factory.LazyFunction(
+        lambda: f"{faker.sentence(nb_words=3, variable_nb_words=True, ext_word_list=None)} - {time.time_ns()}"
+    )
     status = "IN_REVIEW"
-    import_date = factory.Faker("date_time_this_decade", before_now=True, tzinfo=utc,)
+    import_date = factory.Faker(
+        "date_time_this_decade",
+        before_now=True,
+        tzinfo=utc,
+    )
     imported_by = factory.SubFactory(UserFactory)
-    data_source = factory.fuzzy.FuzzyChoice(RegistrationDataImport.DATA_SOURCE_CHOICE, getter=lambda c: c[0],)
+    data_source = factory.fuzzy.FuzzyChoice(
+        RegistrationDataImport.DATA_SOURCE_CHOICE,
+        getter=lambda c: c[0],
+    )
     number_of_individuals = factory.fuzzy.FuzzyInteger(100, 10000)
     number_of_households = factory.fuzzy.FuzzyInteger(3, 50)
     datahub_id = factory.Faker("uuid4")
