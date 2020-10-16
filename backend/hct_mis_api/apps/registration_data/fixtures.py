db5a52ce3b9572922d9d335208261f8f44333937
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-10-16 15:09:53 +0200

    Fixtures fix

--- backend/hct_mis_api/apps/registration_data/fixtures.py
+++ backend/hct_mis_api/apps/registration_data/fixtures.py
@@ -1,10 +1,15 @@
+import time
+
 import factory
 from factory import fuzzy
+from faker import Faker
 from pytz import utc
 
 from account.fixtures import UserFactory
 from core.models import BusinessArea
 from registration_data.models import RegistrationDataImport
+
+faker = Faker()
 
 
 class RegistrationDataImportFactory(factory.DjangoModelFactory):
@@ -12,7 +17,7 @@ class RegistrationDataImportFactory(factory.DjangoModelFactory):
         model = RegistrationDataImport
         django_get_or_create = ("name",)
 
-    name = factory.Faker("sentence", nb_words=3, variable_nb_words=True, ext_word_list=None,)
+    name = f"{faker.sentence(nb_words=3, variable_nb_words=True, ext_word_list=None)} - {time.time_ns()}"
     status = "IN_REVIEW"
     import_date = factory.Faker("date_time_this_decade", before_now=True, tzinfo=utc,)
     imported_by = factory.SubFactory(UserFactory)
