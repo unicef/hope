41ee730604bfbee8555e71bcf31bda002b49b55b
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-10-30 11:44:37 +0100

    Add correct lookup household list filters and columns

--- frontend/src/components/Grievances/LookUpHouseholdTable/LookUpHouseholdTableRow.tsx
+++ frontend/src/components/Grievances/LookUpHouseholdTable/LookUpHouseholdTableRow.tsx
@@ -8,31 +8,36 @@ import {
   HouseholdChoiceDataQuery,
   HouseholdNode,
 } from '../../../__generated__/graphql';
+import { Missing } from '../../Missing';
 import { Pointer } from '../../Pointer';
 import { ClickableTableRow } from '../../table/ClickableTableRow';
 import { UniversalMoment } from '../../UniversalMoment';
 
 interface LookUpHouseholdTableRowProps {
   household: HouseholdNode;
-  choicesData: HouseholdChoiceDataQuery;
   radioChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
   selectedHousehold: string;
+  choicesData: HouseholdChoiceDataQuery;
 }
 
 export function LookUpHouseholdTableRow({
   household,
-  choicesData,
   radioChangeHandler,
   selectedHousehold,
+  choicesData,
 }: LookUpHouseholdTableRowProps): React.ReactElement {
   const history = useHistory();
   const businessArea = useBusinessArea();
-  const residenceStatusChoiceDict = choicesToDict(
-    choicesData.residenceStatusChoices,
-  );
+
   const handleClick = (): void => {
     const path = `/${businessArea}/population/household/${household.id}`;
     history.push(path);
+  };
+  const renderPrograms = (): string => {
+    const programNames = household.programs?.edges?.map(
+      (edge) => edge.node.name,
+    );
+    return programNames?.length ? programNames.join(', ') : '-';
   };
   return (
     <ClickableTableRow hover role='checkbox' key={household.unicefId}>
@@ -51,15 +56,12 @@ export function LookUpHouseholdTableRow({
       </TableCell>
       <TableCell align='left'>{household.headOfHousehold.fullName}</TableCell>
       <TableCell align='left'>{household.size}</TableCell>
-      <TableCell align='left'>{household.adminArea?.title || '-'}</TableCell>
       <TableCell align='left'>
-        {residenceStatusChoiceDict[household.residenceStatus]}
+        <Missing />
       </TableCell>
+      <TableCell align='left'>{renderPrograms()}</TableCell>
       <TableCell align='right'>
-        {formatCurrency(household.totalCashReceived)}
-      </TableCell>
-      <TableCell align='right'>
-        <UniversalMoment>{household.firstRegistrationDate}</UniversalMoment>
+        <UniversalMoment>{household.lastRegistrationDate}</UniversalMoment>
       </TableCell>
     </ClickableTableRow>
   );
