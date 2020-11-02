3b1233dff3d7b76d0b0b27636313e9de07dfda48
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-11-02 17:21:41 +0100

    Add sorting by last registration date to Look Up Household

--- frontend/src/components/Grievances/LookUpHouseholdTable/LookUpHouseholdTableHeadCells.tsx
+++ frontend/src/components/Grievances/LookUpHouseholdTable/LookUpHouseholdTableHeadCells.tsx
@@ -47,7 +47,7 @@ export const headCells: HeadCell<HouseholdNode>[] = [
   {
     disablePadding: false,
     label: 'Registration Date',
-    id: 'last-registration-date',
+    id: 'last_registration_date',
     numeric: false,
     dataCy: 'household-last-registration-date',
   },
