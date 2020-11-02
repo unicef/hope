6e9a9ebbd69137e31200f2485cb7bce3d411be31
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-11-02 13:01:41 +0100

    add grievance filters. Some are missing

--- frontend/src/components/Grievances/GrievancesTablePage.tsx
+++ frontend/src/components/Grievances/GrievancesTablePage.tsx
@@ -1,12 +1,23 @@
-import { Button, Typography } from '@material-ui/core';
-import React from 'react';
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useBusinessArea } from '../../hooks/useBusinessArea';
+import { useDebounce } from '../../hooks/useDebounce';
 import { PageHeader } from '../PageHeader';
+import { GrievancesFilters } from './GrievancesTable/GrievancesFilters';
 import { GrievancesTable } from './GrievancesTable/GrievancesTable';
 
 export function GrievancesTablePage(): React.ReactElement {
   const businessArea = useBusinessArea();
+
+  const [filter, setFilter] = useState({
+    search: '',
+    status: [],
+    fsp: [],
+    createdAtRange: '',
+    admin: '',
+  });
+  const debouncedFilter = useDebounce(filter, 500);
 
   return (
     <>
@@ -22,6 +33,7 @@ export function GrievancesTablePage(): React.ReactElement {
           </Button>
         </>
       </PageHeader>
+      <GrievancesFilters filter={debouncedFilter} onFilterChange={setFilter} />
       <GrievancesTable businessArea={businessArea} />
     </>
   );
