6936b4eb0cc406c61487e208075100a0e5eb643f
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-11-02 19:43:50 +0100

    fix assigned spelling

--- frontend/src/containers/DefaultRoute.tsx
+++ frontend/src/containers/DefaultRoute.tsx
@@ -8,7 +8,7 @@ export function DefaultRoute(): React.ReactElement {
     return null;
   }
   if (data.me.businessAreas.edges.length < 1) {
-    return <div>You dont have any business area asigned to your account</div>;
+    return <div>You dont have any business area assigned to your account</div>;
   }
   return <Redirect to={`/${data.me.businessAreas.edges[0].node.slug}/`} />;
 }
