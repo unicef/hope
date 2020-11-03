e7bc9e9fd4a5516a34ce1b4ebf7ff3458f912737
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-11-03 17:35:42 +0100

    Open Household/individual link in new tab

--- frontend/src/components/Grievances/LookUpHouseholdTable/LookUpHouseholdTableRow.tsx
+++ frontend/src/components/Grievances/LookUpHouseholdTable/LookUpHouseholdTableRow.tsx
@@ -1,13 +1,11 @@
 import { Radio } from '@material-ui/core';
 import TableCell from '@material-ui/core/TableCell';
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { useBusinessArea } from '../../../hooks/useBusinessArea';
 import {
   HouseholdChoiceDataQuery,
   HouseholdNode,
 } from '../../../__generated__/graphql';
-import { Missing } from '../../Missing';
 import { Pointer } from '../../Pointer';
 import { ClickableTableRow } from '../../table/ClickableTableRow';
 import { UniversalMoment } from '../../UniversalMoment';
@@ -24,12 +22,14 @@ export function LookUpHouseholdTableRow({
   radioChangeHandler,
   selectedHousehold,
 }: LookUpHouseholdTableRowProps): React.ReactElement {
-  const history = useHistory();
   const businessArea = useBusinessArea();
 
   const handleClick = (): void => {
     const path = `/${businessArea}/population/household/${household.id}`;
-    history.push(path);
+    const win = window.open(path, '_blank');
+    if (win != null) {
+      win.focus();
+    }
   };
   const renderPrograms = (): string => {
     const programNames = household.programs?.edges?.map(
