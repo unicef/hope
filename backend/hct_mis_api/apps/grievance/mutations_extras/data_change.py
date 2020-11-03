086e5bf3c3095f24a4719fdbf0db6877ef6bd1d5
Jan Romaniak <romaniakjan@gmail.com>
2020-11-03 21:29:42 +0100

    Add individual data change

--- backend/hct_mis_api/apps/grievance/mutations_extras/data_change.py
+++ backend/hct_mis_api/apps/grievance/mutations_extras/data_change.py
@@ -2,8 +2,8 @@ import graphene
 from django.shortcuts import get_object_or_404
 
 from core.utils import decode_id_string
-from grievance.models import GrievanceTicket, TicketIndividualDataUpdateDetails
-from household.models import Individual
+from grievance.models import GrievanceTicket, TicketIndividualDataUpdateDetails, TicketAddIndividualDetails
+from household.models import Individual, Household
 from household.schema import HouseholdNode, IndividualNode
 
 
@@ -36,6 +36,34 @@ class IndividualUpdateDataObjectType(graphene.InputObjectType):
     who_answers_alt_phone = graphene.String()
 
 
+class AddIndividualDataObjectType(graphene.InputObjectType):
+    full_name = graphene.String(required=True)
+    given_name = graphene.String(required=True)
+    middle_name = graphene.String()
+    family_name = graphene.String(required=True)
+    sex = graphene.String(required=True)
+    birth_date = graphene.Date()
+    estimated_birth_date = graphene.Boolean()
+    marital_status = graphene.String()
+    phone_no = graphene.String()
+    phone_no_alternative = graphene.String()
+    relationship = graphene.String()
+    disability = graphene.Boolean()
+    work_status = graphene.String()
+    enrolled_in_nutrition_programme = graphene.Boolean()
+    administration_of_rutf = graphene.Boolean()
+    pregnant = graphene.Boolean()
+    observed_disability = graphene.List(graphene.String)
+    seeing_disability = graphene.String()
+    hearing_disability = graphene.String()
+    physical_disability = graphene.String()
+    memory_disability = graphene.String()
+    selfcare_disability = graphene.String()
+    comms_disability = graphene.String()
+    who_answers_phone = graphene.String()
+    who_answers_alt_phone = graphene.String()
+
+
 class HouseholdDataUpdateIssueTypeExtras(graphene.InputObjectType):
     household = graphene.GlobalID(node=HouseholdNode, required=True)
     individual = graphene.GlobalID(node=IndividualNode)
@@ -46,19 +74,23 @@ class IndividualDataUpdateIssueTypeExtras(graphene.InputObjectType):
     individual_data = IndividualUpdateDataObjectType(required=True)
 
 
+class AddIndividualIssueTypeExtras(graphene.InputObjectType):
+    household = graphene.GlobalID(node=HouseholdNode, required=True)
+    individual_data = AddIndividualDataObjectType(required=True)
+
+
 class IndividualDeleteIssueTypeExtras(graphene.InputObjectType):
     individual = graphene.GlobalID(node=IndividualNode, required=True)
-    household = graphene.GlobalID(node=HouseholdNode)
 
 
-class AddIndividualIssueTypeExtras(graphene.InputObjectType):
-    pass
 
 
 def save_data_change_extras(root, info, input, grievance_ticket, extras, **kwargs):
     issue_type = input.get("issue_type")
     if issue_type == GrievanceTicket.ISSUE_TYPE_INDIVIDUAL_DATA_CHANGE_DATA_UPDATE:
         return save_individual_data_update_extras(root, info, input, grievance_ticket, extras, **kwargs)
+    if issue_type == GrievanceTicket.ISSUE_TYPE_DATA_CHANGE_ADD_INDIVIDUAL:
+        return save_add_individual_extras(root, info, input, grievance_ticket, extras, **kwargs)
 
 
 def save_individual_data_update_extras(root, info, input, grievance_ticket, extras, **kwargs):
@@ -84,4 +116,18 @@ def save_individual_delete_extras(root, info, input, grievance_ticket, extras, *
 
 
 def save_add_individual_extras(root, info, input, grievance_ticket, extras, **kwargs):
-    pass
+    data_change_extras = extras.get("issue_type")
+    add_individual_issue_type_extras = data_change_extras.get("add_individual_issue_type_extras")
+
+    household_encoded_id = add_individual_issue_type_extras.get("household")
+    household_id = decode_id_string(household_encoded_id)
+    household = get_object_or_404(Household, id=household_id)
+    individual_data = add_individual_issue_type_extras.get("individual_data")
+    ticket_add_individual_details = TicketAddIndividualDetails(
+        individual_data=individual_data,
+        household=household,
+        ticket=grievance_ticket,
+    )
+    ticket_add_individual_details.save()
+    grievance_ticket.refresh_from_db()
+    return [grievance_ticket]
