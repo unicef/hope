4e828b3b009d5feb4ff9cb105cdca5faa3863008
Jan Romaniak <romaniakjan@gmail.com>
2020-11-04 09:41:24 +0100

    tests added

---
+++ backend/hct_mis_api/apps/grievance/tests/snapshots/snap_test_grievance_data_change_mutations.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestGrievanceCreateDataChangeMutation::test_grievance_create_individual_data_change 1'] = {
+    'data': {
+        'createGrievanceTicket': {
+            'grievanceTickets': [
+                {
+                    'addIndividualTicketDetails': {
+                        'household': {
+                            'id': 'SG91c2Vob2xkTm9kZTowN2E5MDFlZC1kMmE1LTQyMmEtYjk2Mi0zNTcwZGExZDVkMDc='
+                        },
+                        'individualData': {
+                            'family_name': 'Romaniak',
+                            'full_name': 'Test Test',
+                            'given_name': 'Test',
+                            'sex': 'MALE'
+                        }
+                    },
+                    'category': 2,
+                    'description': 'Test',
+                    'individualDataUpdateTicketDetails': None,
+                    'issueType': 16,
+                    'sensitiveTicketDetails': None
+                }
+            ]
+        }
+    }
+}
+
+snapshots['TestGrievanceCreateDataChangeMutation::test_grievance_update_individual_data_change 1'] = {
+    'data': {
+        'createGrievanceTicket': {
+            'grievanceTickets': [
+                {
+                    'addIndividualTicketDetails': None,
+                    'category': 2,
+                    'description': 'Test',
+                    'individualDataUpdateTicketDetails': {
+                        'individual': {
+                            'fullName': 'Benjamin Butler'
+                        },
+                        'individualData': {
+                            'full_name': 'Test Test',
+                            'given_name': 'Test',
+                            'sex': 'MALE'
+                        }
+                    },
+                    'issueType': 14,
+                    'sensitiveTicketDetails': None
+                }
+            ]
+        }
+    }
+}
