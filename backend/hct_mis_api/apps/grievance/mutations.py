29c3226105959121289db566387bc4700c903a4b
Wojtek Nosal <wojciech.nosal@tivix.com>
2020-11-26 17:59:35 +0100

    Approve Delete Individual Grievance, Reassing Role Mutation, Tests fix

--- backend/hct_mis_api/apps/grievance/mutations.py
+++ backend/hct_mis_api/apps/grievance/mutations.py
@@ -26,8 +26,11 @@ from grievance.mutations_extras.main import (
 )
 from grievance.mutations_extras.payment_verification import save_payment_verification_extras
 from grievance.mutations_extras.sensitive_grievance import save_sensitive_grievance_extras
+from grievance.mutations_extras.utils import get_role_data_key
 from grievance.schema import GrievanceTicketNode, TicketNoteNode
 from grievance.validators import DataChangeValidator
+from household.models import Household, Individual, HEAD, ROLE_ALTERNATE, ROLE_PRIMARY, IndividualRoleInHousehold
+from household.schema import HouseholdNode, IndividualNode
 
 
 class CreateGrievanceTicketInput(graphene.InputObjectType):
@@ -108,17 +111,11 @@ class CreateGrievanceTicketMutation(graphene.Mutation):
     ISSUE_TYPE_OPTIONS = {
         GrievanceTicket.ISSUE_TYPE_HOUSEHOLD_DATA_CHANGE_DATA_UPDATE: {
             "required": ["extras.issue_type.household_data_update_issue_type_extras"],
-            "not_allowed": [
-                "individual_data_update_issue_type_extras",
-                "individual_delete_issue_type_extras",
-            ],
+            "not_allowed": ["individual_data_update_issue_type_extras", "individual_delete_issue_type_extras",],
         },
         GrievanceTicket.ISSUE_TYPE_INDIVIDUAL_DATA_CHANGE_DATA_UPDATE: {
             "required": ["extras.issue_type.individual_data_update_issue_type_extras"],
-            "not_allowed": [
-                "household_data_update_issue_type_extras",
-                "individual_delete_issue_type_extras",
-            ],
+            "not_allowed": ["household_data_update_issue_type_extras", "individual_delete_issue_type_extras",],
         },
         GrievanceTicket.ISSUE_TYPE_DATA_CHANGE_ADD_INDIVIDUAL: {
             "required": ["extras.issue_type.add_individual_issue_type_extras"],
@@ -130,10 +127,7 @@ class CreateGrievanceTicketMutation(graphene.Mutation):
         },
         GrievanceTicket.ISSUE_TYPE_DATA_CHANGE_DELETE_INDIVIDUAL: {
             "required": ["extras.issue_type.individual_delete_issue_type_extras"],
-            "not_allowed": [
-                "household_data_update_issue_type_extras",
-                "individual_data_update_issue_type_extras",
-            ],
+            "not_allowed": ["household_data_update_issue_type_extras", "individual_data_update_issue_type_extras",],
         },
         GrievanceTicket.ISSUE_TYPE_DATA_BREACH: {"required": [], "not_allowed": []},
         GrievanceTicket.ISSUE_TYPE_BRIBERY_CORRUPTION_KICKBACK: {"required": [], "not_allowed": []},
@@ -442,6 +436,76 @@ class AddIndividualApproveMutation(graphene.Mutation):
         return cls(grievance_ticket=grievance_ticket)
 
 
+class DeleteIndividualApproveMutation(graphene.Mutation):
+    grievance_ticket = graphene.Field(GrievanceTicketNode)
+
+    class Arguments:
+        grievance_ticket_id = graphene.Argument(graphene.ID, required=True)
+        approve_status = graphene.Boolean(required=True)
+
+    @classmethod
+    @is_authenticated
+    @transaction.atomic
+    def mutate(cls, root, info, grievance_ticket_id, approve_status, **kwargs):
+        grievance_ticket_id = decode_id_string(grievance_ticket_id)
+        grievance_ticket = get_object_or_404(GrievanceTicket, id=grievance_ticket_id)
+        individual_details = grievance_ticket.delete_individual_ticket_details
+        individual_details.approve_status = approve_status
+        individual_details.save()
+        grievance_ticket.refresh_from_db()
+
+        return cls(grievance_ticket=grievance_ticket)
+
+
+class ReassignRoleMutation(graphene.Mutation):
+    household = graphene.Field(HouseholdNode)
+    individual = graphene.Field(IndividualNode)
+
+    class Arguments:
+        grievance_ticket_id = graphene.Argument(graphene.ID, required=True)
+        household_id = graphene.Argument(graphene.ID, required=True)
+        individual_id = graphene.Argument(graphene.ID, required=True)
+        role = graphene.String(required=True)
+
+    @classmethod
+    def verify_role_choices(cls, role):
+        if role not in [ROLE_PRIMARY, ROLE_ALTERNATE, HEAD]:
+            raise GraphQLError("Provided role is invalid! Please provide one of those: PRIMARY, ALTERNATE, HEAD")
+
+    @classmethod
+    def verify_if_role_exists(cls, household, current_individual, role):
+        if role == HEAD:
+            if household.head_of_household.id != current_individual.id:
+                raise GraphQLError("This individual is not a head of provided household")
+        else:
+            get_object_or_404(IndividualRoleInHousehold, individual=current_individual, household=household, role=role)
+
+    @classmethod
+    @is_authenticated
+    @transaction.atomic
+    def mutate(cls, root, info, household_id, individual_id, grievance_ticket_id, role, **kwargs):
+        cls.verify_role_choices(role)
+        decoded_household_id = decode_id_string(household_id)
+        decoded_individual_id = decode_id_string(individual_id)
+        grievance_ticket_id = decode_id_string(grievance_ticket_id)
+
+        grievance_ticket = get_object_or_404(GrievanceTicket, id=grievance_ticket_id)
+        household = get_object_or_404(Household, id=decoded_household_id)
+        individual = get_object_or_404(Individual, id=decoded_individual_id)
+        ticket_details = grievance_ticket.delete_individual_ticket_details
+        cls.verify_if_role_exists(household, ticket_details.individual, role)
+
+        role_data_key = get_role_data_key(household.id, individual.id, role)
+        ticket_details.role_reassign_data[role_data_key] = {
+            "role": role,
+            "household": str(household.id),
+            "individual": str(individual.id),
+        }
+        ticket_details.save()
+
+        return cls(household=household, individual=individual)
+
+
 class Mutations(graphene.ObjectType):
     create_grievance_ticket = CreateGrievanceTicketMutation.Field()
     grievance_status_change = GrievanceStatusChangeMutation.Field()
@@ -449,3 +513,4 @@ class Mutations(graphene.ObjectType):
     approve_individual_data_change = IndividualDataChangeApproveMutation.Field()
     approve_household_data_change = HouseholdDataChangeApproveMutation.Field()
     approve_add_individual = AddIndividualApproveMutation.Field()
+    reassign_role = ReassignRoleMutation.Field()
