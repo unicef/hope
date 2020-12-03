c98897e3257b888575cb069ac063192236f4a23c
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-12-03 16:25:35 +0100

    fixed approving

--- frontend/src/components/Grievances/RequestedHouseholdDataChange.tsx
+++ frontend/src/components/Grievances/RequestedHouseholdDataChange.tsx
@@ -75,7 +75,7 @@ export function RequestedHouseholdDataChange({
             },
           });
           showMessage('Changes Approved');
-          setEdit(false);
+          setEdit(values.selected.length === 0);
         } catch (e) {
           e.graphQLErrors.map((x) => showMessage(x.message));
         }
