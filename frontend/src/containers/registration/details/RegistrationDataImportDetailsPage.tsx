a906e6dda6d60451227435f1122511dce57d08cf
Egle Libby <eglelibby@gmail.com>
2020-12-08 11:30:37 -0800

    move rdi details container to const

--- frontend/src/containers/registration/details/RegistrationDataImportDetailsPage.tsx
+++ frontend/src/containers/registration/details/RegistrationDataImportDetailsPage.tsx
@@ -11,11 +11,11 @@ import {
 import { LoadingComponent } from '../../../components/LoadingComponent';
 import { ImportedHouseholdTable } from '../tables/ImportedHouseholdsTable';
 import { ImportedIndividualsTable } from '../tables/ImportedIndividualsTable';
-import { RegistrationDetails } from './RegistrationDetails';
-import { RegistrationDataImportDetailsPageHeader } from './RegistrationDataImportDetailsPageHeader';
 import { usePermissions } from '../../../hooks/usePermissions';
 import { hasPermissions, PERMISSIONS } from '../../../config/permissions';
 import { PermissionDenied } from '../../../components/PermissionDenied';
+import { RegistrationDetails } from './RegistrationDetails';
+import { RegistrationDataImportDetailsPageHeader } from './RegistrationDataImportDetailsPageHeader';
 
 const Container = styled.div`
   && {
@@ -68,7 +68,7 @@ export function RegistrationDataImportDetailsPage(): React.ReactElement {
     data: choices,
     loading: choicesLoading,
   } = useProgrammeChoiceDataQuery();
-  
+
   if (loading || choicesLoading) {
     return <LoadingComponent />;
   }
@@ -76,45 +76,62 @@ export function RegistrationDataImportDetailsPage(): React.ReactElement {
   if (!data || !choices || permissions === null) {
     return null;
   }
-  if (!hasPermissions([PERMISSIONS.RDI_VIEW_DETAILS, PERMISSIONS.RDI_MERGE_IMPORT, PERMISSIONS.RDI_RERUN_DEDUPE], permissions)) {
+  if (
+    !hasPermissions(
+      [
+        PERMISSIONS.RDI_VIEW_DETAILS,
+        PERMISSIONS.RDI_MERGE_IMPORT,
+        PERMISSIONS.RDI_RERUN_DEDUPE,
+      ],
+      permissions,
+    )
+  ) {
     return <PermissionDenied />;
   }
+
+  const rdiDetails = (
+    <Container>
+      <RegistrationDetails registration={data.registrationDataImport} />
+      <TableWrapper>
+        <Paper>
+          <Title variant='h6'>Import Preview</Title>
+          <TabsContainer>
+            <StyledTabs
+              value={selectedTab}
+              onChange={(event: React.ChangeEvent<{}>, newValue: number) =>
+                setSelectedTab(newValue)
+              }
+              indicatorColor='primary'
+              textColor='primary'
+              variant='fullWidth'
+              aria-label='full width tabs example'
+            >
+              <Tab label='Households' />
+              <Tab label='Individuals' />
+            </StyledTabs>
+          </TabsContainer>
+          <TabPanel value={selectedTab} index={0}>
+            <ImportedHouseholdTable rdiId={id} />
+          </TabPanel>
+          <TabPanel value={selectedTab} index={1}>
+            <ImportedIndividualsTable showCheckbox rdiId={id} />
+          </TabPanel>
+        </Paper>
+      </TableWrapper>
+    </Container>
+  );
+
   return (
     <div>
       <RegistrationDataImportDetailsPageHeader
         registration={data.registrationDataImport}
         canMerge={hasPermissions(PERMISSIONS.RDI_MERGE_IMPORT, permissions)}
-        canRerunDedupe={hasPermissions(PERMISSIONS.RDI_RERUN_DEDUPE, permissions)}
+        canRerunDedupe={hasPermissions(
+          PERMISSIONS.RDI_RERUN_DEDUPE,
+          permissions,
+        )}
       />
-      {hasPermissions(PERMISSIONS.RDI_VIEW_DETAILS, permissions) && <Container>
-        <RegistrationDetails registration={data.registrationDataImport} />
-        <TableWrapper>
-          <Paper>
-            <Title variant='h6'>Import Preview</Title>
-            <TabsContainer>
-              <StyledTabs
-                value={selectedTab}
-                onChange={(event: React.ChangeEvent<{}>, newValue: number) =>
-                  setSelectedTab(newValue)
-                }
-                indicatorColor='primary'
-                textColor='primary'
-                variant='fullWidth'
-                aria-label='full width tabs example'
-              >
-                <Tab label='Households' />
-                <Tab label='Individuals' />
-              </StyledTabs>
-            </TabsContainer>
-            <TabPanel value={selectedTab} index={0}>
-              <ImportedHouseholdTable rdiId={id} />
-            </TabPanel>
-            <TabPanel value={selectedTab} index={1}>
-              <ImportedIndividualsTable showCheckbox rdiId={id} />
-            </TabPanel>
-          </Paper>
-        </TableWrapper>
-      </Container>}
+      {hasPermissions(PERMISSIONS.RDI_VIEW_DETAILS, permissions) && rdiDetails}
     </div>
   );
 }
