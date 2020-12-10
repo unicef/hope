5a02aee5f7b1b0dd90a0e4c8e1e95da196f0fd57
Egle Libby <eglelibby@gmail.com>
2020-12-09 16:48:33 -0800

    updated test cases for changing programe status

--- backend/hct_mis_api/apps/program/tests/test_change_program_status.py
+++ backend/hct_mis_api/apps/program/tests/test_change_program_status.py
@@ -1,6 +1,8 @@
 from django.core.management import call_command
+from parameterized import parameterized
 
 from account.fixtures import UserFactory
+from account.permissions import Permissions
 from core.base_test_case import APITestCase
 from core.fixtures import AdminAreaFactory, AdminAreaTypeFactory
 from core.models import BusinessArea
@@ -23,60 +25,44 @@ class TestChangeProgramStatus(APITestCase):
         call_command("loadbusinessareas")
         self.user = UserFactory.create()
 
-        business_area = BusinessArea.objects.first()
-        state_area_type = AdminAreaTypeFactory(name="State", business_area=business_area, admin_level=1)
+        self.business_area = BusinessArea.objects.get(slug="afghanistan")
+        state_area_type = AdminAreaTypeFactory(name="State", business_area=self.business_area, admin_level=1)
         self.admin_area = AdminAreaFactory(admin_area_type=state_area_type)
 
-    def test_draft_to_active(self):
-        program = ProgramFactory.create(status="DRAFT", business_area=BusinessArea.objects.order_by("?").first(),)
-
-        self.snapshot_graphql_request(
-            request_string=self.UPDATE_PROGRAM_MUTATION,
-            context={"user": self.user},
-            variables={"programData": {"id": self.id_to_base64(program.id, "ProgramNode"), "status": "ACTIVE"}},
+    @parameterized.expand(
+        [
+            ("draft_to_active_with_permission", [Permissions.PROGRAMME_ACTIVATE], "DRAFT", "ACTIVE"),
+            ("draft_to_acive_without_permission", [Permissions.PROGRAMME_FINISH], "DRAFT", "ACTIVE"),
+            ("finish_to_active_with_permission", [Permissions.PROGRAMME_ACTIVATE], "FINISHED", "ACTIVE"),
+            ("finish_to_active_without_permission", [], "FINISHED", "ACTIVE"),
+            ("draft_to_finished_with_permission", [Permissions.PROGRAMME_FINISH], "DRAFT", "FINISHED"),
+            ("draft_to_finished_without_permission", [], "DRAFT", "FINISHED"),
+            ("active_to_finished_with_permission", [Permissions.PROGRAMME_FINISH], "ACTIVE", "FINISHED"),
+            ("active_to_finished_without_permission", [Permissions.PROGRAMME_ACTIVATE], "ACTIVE", "FINISHED"),
+            (
+                "active_to_draft",
+                [Permissions.PROGRAMME_ACTIVATE, Permissions.PROGRAMME_FINISH, Permissions.PROGRAMME_UPDATE],
+                "ACTIVE",
+                "DRAFT",
+            ),
+            (
+                "finished_to_draft",
+                [Permissions.PROGRAMME_ACTIVATE, Permissions.PROGRAMME_FINISH, Permissions.PROGRAMME_UPDATE],
+                "FINISHED",
+                "DRAFT",
+            ),
+        ]
+    )
+    def test_status_change(self, _, permissions, initial_status, target_status):
+        program = ProgramFactory.create(
+            status=initial_status,
+            business_area=self.business_area,
         )
 
-    def test_active_to_finished(self):
-        program = ProgramFactory.create(status="ACTIVE", business_area=BusinessArea.objects.order_by("?").first(),)
+        self.create_user_role_with_permissions(self.user, permissions, self.business_area)
 
         self.snapshot_graphql_request(
             request_string=self.UPDATE_PROGRAM_MUTATION,
             context={"user": self.user},
-            variables={"programData": {"id": self.id_to_base64(program.id, "ProgramNode"), "status": "FINISHED"}},
-        )
-
-    def test_finished_to_active(self):
-        program = ProgramFactory.create(status="FINISHED", business_area=BusinessArea.objects.order_by("?").first(),)
-
-        self.snapshot_graphql_request(
-            request_string=self.UPDATE_PROGRAM_MUTATION,
-            context={"user": self.user},
-            variables={"programData": {"id": self.id_to_base64(program.id, "ProgramNode"), "status": "ACTIVE"}},
-        )
-
-    def test_draft_to_finished(self):
-        program = ProgramFactory.create(status="DRAFT", business_area=BusinessArea.objects.order_by("?").first(),)
-
-        self.snapshot_graphql_request(
-            request_string=self.UPDATE_PROGRAM_MUTATION,
-            context={"user": self.user},
-            variables={"programData": {"id": self.id_to_base64(program.id, "ProgramNode"), "status": "FINISHED"}},
-        )
-
-    def test_active_to_draft(self):
-        program = ProgramFactory.create(status="ACTIVE", business_area=BusinessArea.objects.order_by("?").first(),)
-
-        self.snapshot_graphql_request(
-            request_string=self.UPDATE_PROGRAM_MUTATION,
-            context={"user": self.user},
-            variables={"programData": {"id": self.id_to_base64(program.id, "ProgramNode"), "status": "DRAFT"}},
-        )
-
-    def test_finished_to_draft(self):
-        program = ProgramFactory.create(status="FINISHED", business_area=BusinessArea.objects.order_by("?").first(),)
-
-        self.snapshot_graphql_request(
-            request_string=self.UPDATE_PROGRAM_MUTATION,
-            context={"user": self.user},
-            variables={"programData": {"id": self.id_to_base64(program.id, "ProgramNode"), "status": "DRAFT"}},
+            variables={"programData": {"id": self.id_to_base64(program.id, "ProgramNode"), "status": target_status}},
         )
