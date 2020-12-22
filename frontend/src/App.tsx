599b36f213375388d020074b292d1bac77e1588b
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-12-22 10:59:02 +0100

    Add react-idle timer and implement Auto-logout after 15 mins, fix user missing email.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { ThemeProvider } from '@material-ui/core';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import IdleTimer from 'react-idle-timer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import moment from 'moment';
@@ -17,12 +18,58 @@ import { SanctionList } from './containers/pages/SanctionList';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
 export const App: React.FC = () => {
+  let countdownInterval;
+  let timeout;
+  const fifteenMinutes = 1000 * 60 * 15;
+  //eslint-disable-next-line
+  const [timeoutCountdown, setTimeoutCountdown] = useState(0);
+  const idleTimer = useRef(null);
+  const clearSessionTimeout = (): void => {
+    clearTimeout(timeout);
+  };
+  const clearSessionInterval = (): void => {
+    clearInterval(countdownInterval);
+  };
+  const handleLogout = (): void => {
+    clearSessionInterval();
+    clearSessionTimeout();
+    window.location.assign('/api/logout');
+    localStorage.removeItem('AUTHENTICATED');
+  };
+  const onActive = (): void => {
+    clearSessionInterval();
+    clearSessionTimeout();
+  };
+  const onIdle = (): void => {
+    const delay = 1000 * 1;
+    if (localStorage.getItem('AUTHENTICATED') !== null) {
+      timeout = setTimeout(() => {
+        let countDown = 10;
+        setTimeoutCountdown(countDown);
+        countdownInterval = setInterval(() => {
+          if (countDown > 0) {
+            //eslint-disable-next-line
+            setTimeoutCountdown(--countDown);
+          } else {
+            handleLogout();
+          }
+        }, 1000);
+      }, delay);
+    }
+  };
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <StyledThemeProvider theme={theme}>
           <MuiPickersUtilsProvider libInstance={moment} utils={MomentUtils}>
             <CssBaseline />
+            <IdleTimer
+              ref={idleTimer}
+              onActive={onActive}
+              onIdle={onIdle}
+              debounce={250}
+              timeout={fifteenMinutes}
+            />
             <Router>
               <Switch>
                 <Route path='/login'>
