be436eef613a5c34980973a09a6ffb68eed28e34
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-12-23 12:30:37 +0100

    reporting branch

---
+++ frontend/src/containers/pages/ReportingDetailsPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ReportingDetailsPage = () => {
+  return <div>Reporting Details Page</div>;
+};
---
+++ frontend/src/containers/pages/ReportingPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const ReportingPage = () => {
+  return <div>Reporting Page</div>;
+};
