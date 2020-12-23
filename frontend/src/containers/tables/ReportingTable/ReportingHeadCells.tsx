45a24a9902be293cabd336d1baeea4c096d9727d
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2020-12-23 14:54:37 +0100

    add files templates for Reporting page

---
+++ frontend/src/containers/tables/ReportingTable/ReportingHeadCells.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { HeadCell } from '../../../components/table/EnhancedTableHead';
+
+export const headCells = [
+  {
+    disablePadding: false,
+    label: 'Report Type',
+    id: '',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Timeframe',
+    id: '',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Status',
+    id: '',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Creation Date',
+    id: '',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Created By',
+    id: '',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: '',
+    id: '',
+    numeric: false,
+  },
+];
