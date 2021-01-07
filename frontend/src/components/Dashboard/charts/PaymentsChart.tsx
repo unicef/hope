ce34039bae43c72c25e0f9a9217f5539f2c74522
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-01-07 15:59:14 +0100

    Add all charts to Dashboard page

---
+++ frontend/src/components/Dashboard/charts/PaymentsChart.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+
+export const PaymentsChart = (): React.ReactElement => {
+  const data = {
+    labels: ['Successful Payments', 'Unsuccessful Payments'],
+    datasets: [
+      {
+        backgroundColor: ['#3363A5', '#FFAA1D'],
+        data: [80, 20],
+      },
+    ],
+  };
+  const options = {
+    maintainAspectRatio: false,
+    cutoutPercentage: 65,
+    legend: {
+      position: 'bottom',
+    },
+  };
+
+  return <Doughnut data={data} options={options} />;
+};
