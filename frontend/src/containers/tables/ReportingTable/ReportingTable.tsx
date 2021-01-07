baa3741c8decae6fab59b3891a6c8d5829beb120
Egle Libby <eglelibby@gmail.com>
2021-01-07 15:41:09 -0800

    added queries to get all reports, filter parameters, choices data

--- frontend/src/containers/tables/ReportingTable/ReportingTable.tsx
+++ frontend/src/containers/tables/ReportingTable/ReportingTable.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
-// import { UniversalTable } from '../UniversalTable';
-//import { headCells } from './ReportingHeadCells';
-//import { ReportingTableRow } from './ReportingTableRow';
+import {
+  ReportChoiceDataQuery,
+  ReportNode,
+  AllReportsQueryVariables,
+  useAllReportsQuery,
+} from '../../../__generated__/graphql';
+import { UniversalTable } from '../UniversalTable';
+import { headCells } from './ReportingHeadCells';
+import { ReportingTableRow } from './ReportingTableRow';
 
 const TableWrapper = styled.div`
   padding: 20px;
 `;
-// interface ReportingTableProps {
-//   businessArea: string;
-//   filter;
-// }
-export const ReportingTable = (): React.ReactElement => {
-  // const initialVariables = {
-  //   businessArea,
-  // };
-  return <TableWrapper>REPORTING TABLE</TableWrapper>;
+interface ReportingTableProps {
+  businessArea: string;
+  filter;
+  choicesData: ReportChoiceDataQuery;
+}
+export const ReportingTable = ({
+  businessArea,
+  filter,
+  choicesData,
+}: ReportingTableProps): React.ReactElement => {
+  const initialVariables = {
+    businessArea,
+    createdFrom: filter.createdFrom,
+    createdTo: filter.createdTo,
+    reportType: filter.type,
+    status: filter.status,
+  };
+  return (
+    <TableWrapper>
+      <UniversalTable<ReportNode, AllReportsQueryVariables>
+        // title='Repor'
+        headCells={headCells}
+        query={useAllReportsQuery}
+        queriedObjectName='allReports'
+        initialVariables={initialVariables}
+        renderRow={(row) => (
+          <ReportingTableRow
+            key={row.id}
+            report={row}
+            // choicesData={choicesData}
+          />
+        )}
+      />
+    </TableWrapper>
+  );
 };
