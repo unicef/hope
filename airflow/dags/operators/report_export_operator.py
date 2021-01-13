36231ae0c017dc644423d092164ed76f6672ef27
Egle Libby <eglelibby@gmail.com>
2021-01-13 15:10:25 -0800

    fix imports in operator

--- airflow/dags/operators/report_export_operator.py
+++ airflow/dags/operators/report_export_operator.py
@@ -3,8 +3,10 @@ from .base import DjangoOperator
 
 class ReportExportOperator(DjangoOperator):
     def execute(self, context, **kwargs):
-        from reporting.generate_report_service import GenerateReportService
-        from reporting.models import Report
+        from hct_mis_api.apps.reporting.generate_report_service import (
+            GenerateReportService,
+        )
+        from hct_mis_api.apps.reporting.models import Report
 
         dag_run = context["dag_run"]
         config_vars = dag_run.conf
