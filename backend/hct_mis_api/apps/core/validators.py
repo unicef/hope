1d24735f2ed24883c2ec801c7d9abb13dcc7e349
Wojtek Nosal <wojciech.nosal@tivix.com>
2021-01-21 12:02:49 +0100

    refactor

--- backend/hct_mis_api/apps/core/validators.py
+++ backend/hct_mis_api/apps/core/validators.py
@@ -1,5 +1,21 @@
 from django.core.exceptions import ValidationError
 
+from hct_mis_api.apps.core.core_fields_attributes import (
+    CORE_FIELDS_ATTRIBUTES,
+    KOBO_ONLY_HOUSEHOLD_FIELDS,
+    KOBO_ONLY_INDIVIDUAL_FIELDS,
+    TYPE_GEOPOINT,
+    TYPE_SELECT_MANY,
+    TYPE_DATE,
+    TYPE_DECIMAL,
+    TYPE_INTEGER,
+    TYPE_STRING,
+    TYPE_SELECT_ONE,
+    TYPE_IMAGE,
+    TYPE_BOOL,
+)
+from hct_mis_api.apps.core.utils import xlrd_rows_iterator
+
 
 class BaseValidator:
     """
@@ -89,23 +105,7 @@ def prepare_choices_for_validation(choices_sheet):
     return choices_mapping
 
 
-def validate_kobo_template(survey_sheet, choices_sheet):
-    from hct_mis_api.apps.core.core_fields_attributes import (
-        CORE_FIELDS_ATTRIBUTES,
-        KOBO_ONLY_HOUSEHOLD_FIELDS,
-        KOBO_ONLY_INDIVIDUAL_FIELDS,
-        TYPE_GEOPOINT,
-        TYPE_SELECT_MANY,
-        TYPE_DATE,
-        TYPE_DECIMAL,
-        TYPE_INTEGER,
-        TYPE_STRING,
-        TYPE_SELECT_ONE,
-        TYPE_IMAGE,
-        TYPE_BOOL,
-    )
-    from hct_mis_api.apps.core.utils import xlrd_rows_iterator
-
+class KoboTemplateValidator:
     CHOICE_MAP = {
         "note": TYPE_STRING,
         "image": TYPE_IMAGE,
@@ -118,161 +118,149 @@ def validate_kobo_template(survey_sheet, choices_sheet):
         "select_multiple": TYPE_SELECT_MANY,
         "geopoint": TYPE_GEOPOINT,
     }
-
-    choices_mapping = prepare_choices_for_validation(choices_sheet)
-
-    columns_names_and_numbers_mapping = {
-        "type": None,
-        "name": None,
-        "required": None,
-    }
-    first_row = survey_sheet.row(0)
-    for index, cell in enumerate(first_row):
-        column_name = cell.value
-        if column_name in columns_names_and_numbers_mapping.keys():
-            columns_names_and_numbers_mapping[column_name] = index
-
-    if None in columns_names_and_numbers_mapping.values():
-        raise ValueError("Survey sheet does not contain all required columns")
-
-    core_fields_in_file = {}
-    for row in xlrd_rows_iterator(survey_sheet):
-        field_name = row[columns_names_and_numbers_mapping["name"]].value
-        if field_name.endswith("_c") is False:
-            continue
-
-        field_type = row[columns_names_and_numbers_mapping["type"]].value
-
-        choices_list_name = None
-        if field_type.startswith("select_"):
-            field_type, choices_list_name, *_ = field_type.split(" ")
-
-        if field_type not in CHOICE_MAP.keys():
-            continue
-
-        required_value = str(row[columns_names_and_numbers_mapping["required"]].value)
-        if required_value.lower() not in ("true", "false"):
-            is_field_required = False
-        else:
-            is_field_required = True if required_value == "true" else False
-
-        field_type = CHOICE_MAP[field_type] if field_type != "calculate" else "CALCULATE"
-
-        core_fields_in_file[field_name] = {
-            "type": field_type,
-            "required": is_field_required,
-            "choices": choices_mapping[choices_list_name] if choices_list_name is not None else [],
-        }
-
-    all_core_fields = (
+    EXPECTED_REQUIRED_FIELDS = (
+        "country_h_c"
+        "size_h_c"
+        "relationship_i_c"
+        "role_i_c"
+        "full_name_i_c"
+        "gender_i_c"
+        "birth_date_i_c"
+        "estimated_birth_date_i_c"
+    )
+    FIELDS_EXCLUDED_FROM_CHOICE_CHECK = (
+        "country_h_c",
+        "country_origin_h_c",
+        "birth_certificate_issuer_i_c",
+        "drivers_license_issuer_i_c",
+        "electoral_card_issuer_i_c",
+        "unhcr_id_issuer_i_c",
+        "national_passport_issuer_i_c",
+        "national_id_issuer_i_c",
+        "scope_id_issuer_i_c",
+        "other_id_issuer_i_c",
+        "currency_h_c",
+        # temporarily disabled from checking
+        "admin1_h_c",
+        "admin2_h_c",
+    )
+    CHOICES_EXCLUDED_FROM_CHECKING = (
+        {"label": {"English(EN)": "None"}, "value": ""},
+        {"label": {"English(EN)": "Not provided"}, "value": "NOT_PROVIDED"},
+        {"label": {"English(EN)": "Unknown"}, "value": "UNKNOWN"},
+    )
+    ALL_CORE_FIELDS = (
         CORE_FIELDS_ATTRIBUTES + list(KOBO_ONLY_HOUSEHOLD_FIELDS.values()) + list(KOBO_ONLY_INDIVIDUAL_FIELDS.values())
     )
-    core_fields_in_db = {
-        core_field_data["xlsx_field"]: {
-            "type": core_field_data["type"],
-            "required": core_field_data["required"],
-            "choices": core_field_data["choices"],
+
+    @classmethod
+    def _map_columns_numbers(cls, first_row):
+        columns_names_and_numbers_mapping = {
+            "type": None,
+            "name": None,
+            "required": None,
         }
-        for core_field_data in all_core_fields
-        if core_field_data["xlsx_field"].endswith("_c")
-    }
 
-    validation_errors = []
-    for core_field, field_data in core_fields_in_db.items():
-        field_type = field_data["type"]
-        field_required = field_data["required"]
-        field_choices = field_data["choices"]
-        core_field_from_file = core_fields_in_file.get(core_field)
+        for index, cell in enumerate(first_row):
+            column_name = cell.value
+            if column_name in columns_names_and_numbers_mapping.keys():
+                columns_names_and_numbers_mapping[column_name] = index
 
-        # check if field exists
+        if None in columns_names_and_numbers_mapping.values():
+            raise ValueError("Survey sheet does not contain all required columns")
+
+        return columns_names_and_numbers_mapping
+
+    @classmethod
+    def _get_core_fields_from_file(cls, survey_sheet, choices_mapping, columns_names_and_numbers_mapping):
+        core_fields_in_file = {}
+        for row in xlrd_rows_iterator(survey_sheet):
+            field_name = row[columns_names_and_numbers_mapping["name"]].value
+            if field_name.endswith("_c") is False:
+                continue
+
+            field_type = row[columns_names_and_numbers_mapping["type"]].value
+
+            choices_list_name = None
+            if field_type.startswith("select_"):
+                field_type, choices_list_name, *_ = field_type.split(" ")
+
+            if field_type not in cls.CHOICE_MAP.keys():
+                continue
+
+            required_value = str(row[columns_names_and_numbers_mapping["required"]].value)
+            if required_value.lower() not in ("true", "false"):
+                is_field_required = False
+            else:
+                is_field_required = True if required_value == "true" else False
+
+            field_type = cls.CHOICE_MAP[field_type] if field_type != "calculate" else "CALCULATE"
+
+            core_fields_in_file[field_name] = {
+                "type": field_type,
+                "required": is_field_required,
+                "choices": choices_mapping[choices_list_name] if choices_list_name is not None else [],
+            }
+
+        return core_fields_in_file
+
+    @classmethod
+    def _get_core_fields_from_db(cls):
+        return {
+            core_field_data["xlsx_field"]: {
+                "type": core_field_data["type"],
+                "required": core_field_data["required"],
+                "choices": core_field_data["choices"],
+            }
+            for core_field_data in cls.ALL_CORE_FIELDS
+            if core_field_data["xlsx_field"].endswith("_c")
+        }
+
+    @classmethod
+    def _check_if_field_exists(cls, core_field, core_field_from_file):
         if core_field_from_file is None:
-            validation_errors.append(
-                {
-                    "field": core_field,
-                    "message": "Field is missing",
-                }
-            )
-            continue
+            return {
+                "field": core_field,
+                "message": "Field is missing",
+            }
+        return
 
-        # check types
+    @classmethod
+    def _check_field_type(cls, core_field, core_field_from_file, field_type):
         if field_type != core_field_from_file["type"] and core_field_from_file["type"] != "CALCULATE":
             if field_type == TYPE_BOOL and core_field_from_file["type"] == TYPE_SELECT_ONE:
-                continue
-            validation_errors.append(
-                {
-                    "field": core_field,
-                    "message": f"Expected type: {field_type}, actual type: {core_field_from_file['type']}",
-                }
-            )
+                return
+            return {
+                "field": core_field,
+                "message": f"Expected type: {field_type}, actual type: {core_field_from_file['type']}",
+            }
+        return
 
-        # check required
+    @classmethod
+    def _check_is_field_required(cls, core_field, core_field_from_file):
         field_from_file_required = str(core_field_from_file["required"])
-        expected_required_fields = (
-            "country_h_c"
-            "size_h_c"
-            "relationship_i_c"
-            "role_i_c"
-            "full_name_i_c"
-            "gender_i_c"
-            "birth_date_i_c"
-            "estimated_birth_date_i_c"
-        )
-        if core_field in expected_required_fields and field_from_file_required.lower() != "true":
-            validation_errors.append(
-                {
-                    "field": core_field,
-                    "message": "Field must be required",
-                }
-            )
-        # if field_required != field_from_file_required:
-        #     if field_required is True and field_from_file_required is False:
-        #         validation_errors.append(
-        #             {
-        #                 "field": core_field,
-        #                 "message": "Field must be required",
-        #             }
-        #         )
-        #     else:
-        #         validation_errors.append(
-        #             {
-        #                 "field": core_field,
-        #                 "message": "Field must be not required",
-        #             }
-        #         )
 
-        # check choices
-        fields_excluded_from_choice_check = (
-            "country_h_c",
-            "country_origin_h_c",
-            "birth_certificate_issuer_i_c",
-            "drivers_license_issuer_i_c",
-            "electoral_card_issuer_i_c",
-            "unhcr_id_issuer_i_c",
-            "national_passport_issuer_i_c",
-            "national_id_issuer_i_c",
-            "scope_id_issuer_i_c",
-            "other_id_issuer_i_c",
-            "currency_h_c",
-            # temporarily disabled from checking
-            "admin1_h_c",
-            "admin2_h_c",
-        )
-        if core_field in fields_excluded_from_choice_check:
-            continue
+        if core_field in cls.EXPECTED_REQUIRED_FIELDS and field_from_file_required.lower() != "true":
+            return {
+                "field": core_field,
+                "message": "Field must be required",
+            }
+        return
+
+    @classmethod
+    def _check_field_choices(cls, core_field, core_field_from_file, field_choices):
+        if core_field in cls.FIELDS_EXCLUDED_FROM_CHOICE_CHECK:
+            return
 
         from_file_choices = core_field_from_file["choices"]
+        errors = []
 
-        choices_excluded_from_checking = (
-            {"label": {"English(EN)": "None"}, "value": ""},
-            {"label": {"English(EN)": "Not provided"}, "value": "NOT_PROVIDED"},
-            {"label": {"English(EN)": "Unknown"}, "value": "UNKNOWN"},
-        )
         for internal_choice in field_choices:
-            if internal_choice in choices_excluded_from_checking:
+            if internal_choice in cls.CHOICES_EXCLUDED_FROM_CHECKING:
                 continue
 
             if internal_choice not in from_file_choices:
-                validation_errors.append(
+                errors.append(
                     {
                         "field": core_field,
                         "message": f"Choice: {internal_choice} is not present in the file",
@@ -281,11 +269,48 @@ def validate_kobo_template(survey_sheet, choices_sheet):
 
         for file_choice in from_file_choices:
             if file_choice not in from_file_choices:
-                validation_errors.append(
+                errors.append(
                     {
                         "field": core_field,
                         "message": f"Choice: {file_choice} is not present in HOPE",
                     }
                 )
 
-    return validation_errors
+        return errors
+
+    @classmethod
+    def validate_kobo_template(cls, survey_sheet, choices_sheet):
+        choices_mapping = prepare_choices_for_validation(choices_sheet)
+
+        first_row = survey_sheet.row(0)
+        columns_names_and_numbers_mapping = cls._map_columns_numbers(first_row)
+
+        core_fields_in_file = cls._get_core_fields_from_file(
+            survey_sheet, choices_mapping, columns_names_and_numbers_mapping
+        )
+        core_fields_in_db = cls._get_core_fields_from_db()
+
+        validation_errors = []
+        for core_field, field_data in core_fields_in_db.items():
+            field_type = field_data["type"]
+            field_choices = field_data["choices"]
+            core_field_from_file = core_fields_in_file.get(core_field)
+
+            field_exists_error = cls._check_if_field_exists(core_field, core_field_from_file)
+            if field_exists_error:
+                validation_errors.append(field_exists_error)
+                continue
+
+            field_type_error = cls._check_field_type(core_field, core_field_from_file, field_type)
+            if field_type_error:
+                validation_errors.append(field_type_error)
+
+            field_required_error = cls._check_is_field_required(core_field, core_field_from_file)
+            if field_required_error:
+                validation_errors.append(field_required_error)
+
+            field_choices_errors = cls._check_field_choices(core_field, core_field_from_file, field_choices)
+            if field_choices_errors:
+                validation_errors.extend(field_choices_errors)
+
+        return validation_errors
