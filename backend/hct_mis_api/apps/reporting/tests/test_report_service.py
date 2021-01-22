8b81b1c61bb18cd75e188a8f41a5f3204faf21a8
Egle Libby <eglelibby@gmail.com>
2021-01-21 16:29:33 -0800

    added test cases for the rest of report types

--- backend/hct_mis_api/apps/reporting/tests/test_report_service.py
+++ backend/hct_mis_api/apps/reporting/tests/test_report_service.py
@@ -9,7 +9,11 @@ from hct_mis_api.apps.reporting.models import Report
 from hct_mis_api.apps.household.fixtures import create_household_and_individuals
 from hct_mis_api.apps.core.fixtures import AdminAreaTypeFactory, AdminAreaFactory
 from hct_mis_api.apps.program.fixtures import CashPlanFactory, ProgramFactory
-from hct_mis_api.apps.payment.fixtures import CashPlanPaymentVerificationFactory, PaymentRecordFactory
+from hct_mis_api.apps.payment.fixtures import (
+    CashPlanPaymentVerificationFactory,
+    PaymentRecordFactory,
+    PaymentVerificationFactory,
+)
 
 
 class TestGenerateReportService(TestCase):
@@ -50,16 +54,33 @@ class TestGenerateReportService(TestCase):
             self.households.append(household)
             self.individuals.extend(individuals)
 
-        self.program = ProgramFactory(business_area=self.business_area)
-        self.cash_plan = CashPlanFactory(business_area=self.business_area, program=self.program)
-        self.cash_plan_verification = CashPlanPaymentVerificationFactory(
-            cash_plan=self.cash_plan, completion_date="2020-01-01"
+        self.program_1 = ProgramFactory(business_area=self.business_area, end_date="2020-01-01")
+        self.program_2 = ProgramFactory(business_area=self.business_area, end_date="2022-01-01")
+
+        self.cash_plan_1 = CashPlanFactory(
+            business_area=self.business_area, program=self.program_1, end_date="2020-01-01"
         )
-        CashPlanPaymentVerificationFactory(
-            cash_plan=CashPlanFactory(business_area=self.business_area), completion_date="2020-01-01"
+        self.cash_plan_2 = CashPlanFactory(business_area=self.business_area, end_date="2020-01-01")
+        self.cash_plan_verification_1 = CashPlanPaymentVerificationFactory(
+            cash_plan=self.cash_plan_1, completion_date="2020-01-01"
         )
-        PaymentRecordFactory(household=self.households[0], business_area=self.business_area, delivery_date="2020-01-01")
-        PaymentRecordFactory(household=self.households[1], business_area=self.business_area, delivery_date="2020-01-01")
+        self.cash_plan_verification_2 = CashPlanPaymentVerificationFactory(
+            cash_plan=self.cash_plan_2, completion_date="2020-01-01"
+        )
+        PaymentRecordFactory(
+            household=self.households[0],
+            business_area=self.business_area,
+            delivery_date="2020-01-01",
+            cash_plan=self.cash_plan_1,
+        )
+        PaymentRecordFactory(
+            household=self.households[1],
+            business_area=self.business_area,
+            delivery_date="2020-01-01",
+            cash_plan=self.cash_plan_2,
+        )
+        PaymentVerificationFactory(cash_plan_payment_verification=self.cash_plan_verification_1)
+        PaymentVerificationFactory(cash_plan_payment_verification=self.cash_plan_verification_2)
 
     @parameterized.expand(
         [
@@ -71,6 +92,15 @@ class TestGenerateReportService(TestCase):
             ("cash_plan_verifications_program", Report.CASH_PLAN_VERIFICATION, False, True, 1),
             ("payments_no_filter", Report.PAYMENTS, False, False, 2),
             ("payments_filter_admin_area", Report.PAYMENTS, True, False, 1),
+            ("payment_verifications_no_filter", Report.PAYMENT_VERIFICATION, False, False, 2),
+            ("payment_verifications_program", Report.PAYMENT_VERIFICATION, False, True, 1),
+            ("cash_plans_no_filter", Report.CASH_PLAN, False, False, 2),
+            ("cash_plans_program", Report.CASH_PLAN, False, True, 1),
+            ("programs_no_filter", Report.PROGRAM, False, False, 1),
+            ("individuals_payments_no_filter", Report.INDIVIDUALS_AND_PAYMENT, False, False, 4),
+            ("individuals_payments_admin_area", Report.INDIVIDUALS_AND_PAYMENT, True, False, 2),
+            ("individuals_payments_program", Report.INDIVIDUALS_AND_PAYMENT, False, True, 2),
+            ("individuals_payments_admin_area_and_program", Report.INDIVIDUALS_AND_PAYMENT, True, True, 2),
         ]
     )
     def test_report_types(self, _, report_type, should_set_admin_area, should_set_program, number_of_records):
@@ -89,7 +119,7 @@ class TestGenerateReportService(TestCase):
             report.admin_area.set([self.admin_area_1])
 
         if should_set_program:
-            report.program = self.program
+            report.program = self.program_1
             report.save()
 
         report_service = self.GenerateReportService(report)
