8db6f78eb9c849cb72195eabbc2930b62751ef8b
Egle Libby <eglelibby@gmail.com>
2021-01-25 14:06:41 -0800

    fix: custom resolver for relationship field to handle cases where relationship is empty string

--- frontend/src/__generated__/graphql.tsx
+++ frontend/src/__generated__/graphql.tsx
@@ -545,7 +545,6 @@ export type CopyTargetPopulationInput = {
 
 export type CopyTargetPopulationMutationInput = {
   targetPopulationData?: Maybe<CopyTargetPopulationInput>,
-  version?: Maybe<Scalars['BigInt']>,
   clientMutationId?: Maybe<Scalars['String']>,
 };
 
