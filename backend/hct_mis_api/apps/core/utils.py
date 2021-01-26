54abb9a7fbc1dfd163618529a4d0986144800e8e
Dominik Grafik <dominik.grafik@tivix.com>
2021-01-26 20:31:07 +0100

    restore removed utils functions

--- backend/hct_mis_api/apps/core/utils.py
+++ backend/hct_mis_api/apps/core/utils.py
@@ -562,3 +562,33 @@ def xlrd_rows_iterator(sheet):
             continue
 
         yield row
+
+
+def chart_map_choices(choices):
+    return dict(choices)
+
+
+def chart_get_filtered_qs(obj, year, business_area_slug_filter=None, additional_filters=None):
+    if additional_filters is None:
+        additional_filters = {}
+    if business_area_slug_filter is None:
+        business_area_slug_filter = {}
+    return obj.objects.filter(
+        created_at__year=year,
+        **business_area_slug_filter,
+        **additional_filters
+    )
+
+
+def parse_list_values_to_int(list_to_parse):
+    return list(map(lambda x: int(x or 0), list_to_parse))
+
+
+def sum_lists(qs_values, list_len):
+    data = [0] * list_len
+    for values in qs_values:
+        parsed_values = parse_list_values_to_int(values)
+        for i, value in enumerate(parsed_values):
+            data[i] += value
+
+    return data
