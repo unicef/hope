ff7111ab149cfb960a2d8b6c04720371f0086020
Dominik Grafik <dominik.grafik@tivix.com>
2021-01-26 21:53:42 +0100

    Handle array form select many flex field on fronted

--- frontend/src/components/Grievances/AddIndividualGrievanceDetails.tsx
+++ frontend/src/components/Grievances/AddIndividualGrievanceDetails.tsx
@@ -67,20 +67,16 @@ export function AddIndividualGrievanceDetails({
       );
     }) || [];
   const flexFieldLabes =
-    Object.entries(flexFields || {}).map(([key, value]) => {
+    Object.entries(flexFields || {}).map(([key, value]: [string, string|string[]]) => {
       let textValue = value;
       const fieldAttribute = fieldsDict[key];
       if (fieldAttribute.type === 'SELECT_ONE') {
         textValue = fieldAttribute.choices.find((item) => item.value === value)
           .labelEn;
-        console.log('---', textValue)
       }
-      console.log(fieldAttribute.type)
-      console.log(fieldAttribute.choices)
-      console.log(value)
       if (fieldAttribute.type === 'SELECT_MANY') {
           const values = fieldAttribute.choices.filter((item) => value.includes(item.value))
-          textValue = values.map((item) => item.labelEn).join()
+          textValue = values.map((item) => item.labelEn).join(", ")
       }
       return (
         <Grid key={key} item xs={6}>
