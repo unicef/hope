98ddcffeede47fda527976746636ab8a9a50e711
Jan Romaniak <jan.romaniak@tivix.com>
2021-01-26 21:24:06 +0100

    added synonym trhough django made docker smaller

--- backend/hct_mis_api/apps/payment/utils.py
+++ backend/hct_mis_api/apps/payment/utils.py
@@ -1,15 +1,14 @@
 from decimal import Decimal
 from math import ceil
-
+from statistics import NormalDist
 from django.db.models import Q
-from scipy.special import ndtri
 
 from hct_mis_api.apps.payment.models import PaymentVerification
 
 
 def get_number_of_samples(payment_records_sample_count, confidence_interval, margin_of_error):
     variable = 0.5
-    z_score = ndtri(confidence_interval + (1 - confidence_interval) / 2)
+    z_score = NormalDist().inv_cdf(0.01)(confidence_interval + (1 - confidence_interval) / 2)
     theoretical_sample = (z_score ** 2) * variable * (1 - variable) / margin_of_error ** 2
     actual_sample = ceil(
         (payment_records_sample_count * theoretical_sample / (theoretical_sample + payment_records_sample_count)) * 1.5
