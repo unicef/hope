7c58bf5b61107fe303c4e9e44855734f51743702
sax <s.apostolico@gmail.com>
2021-01-28 00:13:51 +0100

    add inspect capability to CA datahub section

--- backend/hct_mis_api/apps/cash_assist_datahub/admin.py
+++ backend/hct_mis_api/apps/cash_assist_datahub/admin.py
@@ -3,6 +3,9 @@ from admin_extra_urls.api import action
 from admin_extra_urls.mixins import ExtraUrlMixin
 from adminfilters.filters import TextFieldFilter, AllValuesComboFilter, AllValuesRadioFilter
 from django.contrib import admin
+from django.contrib.admin.templatetags.admin_urls import admin_urlname
+from django.template.response import TemplateResponse
+from django.urls import reverse
 
 from hct_mis_api.apps.cash_assist_datahub.models import (
     CashPlan,
@@ -21,9 +24,27 @@ class SessionAdmin(ExtraUrlMixin, admin.ModelAdmin):
     list_filter = ('status', 'source', TextFieldFilter.factory('business_area'))
     ordering = 'timestamp',
 
-    @action
-    def inspect(self, pk):
-        pass
+    @action()
+    def inspect(self, request, pk):
+        opts = self.model._meta
+        ctx = {
+            "opts": opts,
+            "app_label": "account",
+            "change": True,
+            "is_popup": False,
+            "save_as": False,
+            "has_delete_permission": False,
+            "has_add_permission": False,
+            "has_change_permission": True,
+            "original": Session.objects.get(pk=pk),
+            "data": {}
+        }
+        for model in [PaymentRecord, CashPlan, Programme, ServiceProvider, TargetPopulation]:
+            ctx["data"][model] = {"count": model.objects.filter(session=pk).count(),
+                                  "meta": model._meta
+                                  }
+
+        return TemplateResponse(request, "admin/cash_assist_datahub/session/inspect.html", ctx)
 
 
 @admin.register(CashPlan)
@@ -37,7 +58,7 @@ class CashPlanAdmin(admin.ModelAdmin):
 
 
 @admin.register(PaymentRecord)
-class PaymentRecordAdmin(admin.ModelAdmin):
+class PaymentRecordAdmin(ExtraUrlMixin, admin.ModelAdmin):
     list_display = ('session', 'business_area', 'status', 'full_name')
     raw_id_fields = ('session',)
     date_hierarchy = 'session__timestamp'
@@ -45,6 +66,45 @@ class PaymentRecordAdmin(admin.ModelAdmin):
                    'delivery_type',
                    TextFieldFilter.factory('session__id'),
                    TextFieldFilter.factory('business_area'))
+
+    @action()
+    def inspect(self, request, pk):
+        opts = self.model._meta
+        payment_record: PaymentRecord = PaymentRecord.objects.get(pk=pk)
+        ctx = {
+            "opts": opts,
+            "app_label": "account",
+            "change": True,
+            "is_popup": False,
+            "save_as": False,
+            "has_delete_permission": False,
+            "has_add_permission": False,
+            "has_change_permission": True,
+            "original": payment_record,
+            "data": {}
+        }
+        from hct_mis_api.apps.core.models import BusinessArea
+        from hct_mis_api.apps.household.models import Individual, Household
+        # ba = BusinessArea.objects.get(code=payment_record.business_area)
+        for field_name, model, rk in [
+                               ('business_area', BusinessArea, 'code'),
+                               ('household_mis_id', Household, 'pk'),
+                               ('head_of_household_mis_id', Individual, 'pk'),
+                               ('target_population_mis_id', TargetPopulation, 'pk'),
+                               ]:
+            instance = model.objects.filter(**{rk:getattr(payment_record, field_name)}).first()
+            details = None
+            if instance:
+                details = reverse(admin_urlname(model._meta, "change"), args=[instance.pk])
+            ctx["data"][model] = {
+                "instance":instance,
+                "details": details,
+                "meta": model._meta
+                }
+
+        return TemplateResponse(request,
+                                "admin/cash_assist_datahub/payment_record/inspect.html",
+                                ctx)
 
 
 @admin.register(ServiceProvider)
