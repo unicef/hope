453f33b0af6d9ec586bb9da6fef423300dff1311
Dominik Grafik <dominik.grafik@tivix.com>
2021-01-28 18:06:13 +0100

    handle int and float for decimal type (flex field)

--- backend/hct_mis_api/apps/core/core_fields_attributes.py
+++ backend/hct_mis_api/apps/core/core_fields_attributes.py
@@ -46,7 +46,7 @@ FIELD_TYPES_TO_INTERNAL_TYPE = {
     TYPE_SELECT_ONE: str,
     TYPE_SELECT_MANY: list,
     TYPE_GEOPOINT: str,
-    TYPE_DECIMAL: int,
+    TYPE_DECIMAL: (int, float),
 }
 
 _INDIVIDUAL = "Individual"
