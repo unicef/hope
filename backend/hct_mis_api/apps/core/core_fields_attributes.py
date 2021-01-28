39c877373b267b635293a18821108c1d122bec1f
Dominik Grafik <dominik.grafik@tivix.com>
2021-01-28 14:03:12 +0100

    fix decimal type - flex field

--- backend/hct_mis_api/apps/core/core_fields_attributes.py
+++ backend/hct_mis_api/apps/core/core_fields_attributes.py
@@ -46,7 +46,7 @@ FIELD_TYPES_TO_INTERNAL_TYPE = {
     TYPE_SELECT_ONE: str,
     TYPE_SELECT_MANY: list,
     TYPE_GEOPOINT: str,
-    TYPE_DECIMAL: str,
+    TYPE_DECIMAL: int,
 }
 
 _INDIVIDUAL = "Individual"
