89ec6160aa226f4108e6b1e8732d4907167faa8f
Jan Romaniak <romaniakjan@gmail.com>
2021-01-29 23:00:13 +0100

    admin area is working fine

--- frontend/src/components/Grievances/LookUpIndividualTable/LookUpIndividualTableRow.tsx
+++ frontend/src/components/Grievances/LookUpIndividualTable/LookUpIndividualTableRow.tsx
@@ -39,12 +39,6 @@ export function LookUpIndividualTableRow({
     );
     return programNames?.length ? programNames.join(', ') : '-';
   };
-  const renderAdminLevel2 = (): string => {
-    if (individual?.household?.adminArea?.adminAreaType?.adminLevel === 2) {
-      return individual?.household?.adminArea?.title;
-    }
-    return '-';
-  };
   return (
     <ClickableTableRow hover role='checkbox' key={individual.id}>
       <TableCell padding='checkbox'>
@@ -68,7 +62,7 @@ export function LookUpIndividualTableRow({
       </TableCell>
       <TableCell align='right'>{age}</TableCell>
       <TableCell align='left'>{sexToCapitalize(individual.sex)}</TableCell>
-      <TableCell align='left'>{renderAdminLevel2()}</TableCell>
+      <TableCell align='left'>{individual?.household?.admin2?.title||'-'}</TableCell>
       <TableCell align='left'>{renderPrograms()}</TableCell>
       <TableCell align='left'>
         <UniversalMoment>{individual.lastRegistrationDate}</UniversalMoment>
