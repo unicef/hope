2617adc3699c3e54803cdb4b74513817a16d23f5
sax <s.apostolico@gmail.com>
2021-02-01 14:59:20 +0100

    admin enhancement

--- backend/hct_mis_api/apps/registration_data/admin.py
+++ backend/hct_mis_api/apps/registration_data/admin.py
@@ -1,8 +1,13 @@
+from adminfilters.filters import ChoicesFieldComboFilter
 from django.contrib import admin
 
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
+from hct_mis_api.apps.utils.admin import HOPEModelAdminBase
 
 
 @admin.register(RegistrationDataImport)
-class RegistrationDataImportAdmin(admin.ModelAdmin):
-    pass
+class RegistrationDataImportAdmin(HOPEModelAdminBase):
+    list_filter = (('status', ChoicesFieldComboFilter),
+                   ('data_source', ChoicesFieldComboFilter),
+                   )
+    date_hierarchy = 'updated_at'
