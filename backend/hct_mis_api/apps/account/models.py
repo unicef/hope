4f57baafd1e340c032eac557d6857e7019ee5ef0
sax <s.apostolico@gmail.com>
2021-02-01 16:43:06 +0100

    removes default orderind for Role

--- backend/hct_mis_api/apps/account/models.py
+++ backend/hct_mis_api/apps/account/models.py
@@ -78,8 +78,6 @@ class Role(TimeStampedUUIDModel):
     permissions = ChoiceArrayField(
         models.CharField(choices=Permissions.choices(), max_length=255), null=True, blank=True
     )
-    class Meta:
-        ordering = ('name', )
 
     def __str__(self):
         return self.name
