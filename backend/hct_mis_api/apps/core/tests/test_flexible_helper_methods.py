70bc9deb21adf8d30292dac7c2f8c41d8bc402cb
Wojtek Nosal <wojciech.nosal@tivix.com>
2021-02-04 14:53:04 +0100

    fixes and changes

--- backend/hct_mis_api/apps/core/tests/test_flexible_helper_methods.py
+++ backend/hct_mis_api/apps/core/tests/test_flexible_helper_methods.py
@@ -10,11 +10,10 @@ from hct_mis_api.apps.core.flex_fields_importer import FlexibleAttributeImporter
 class TestFlexibleHelperMethods(TestCase):
     def setUp(self):
         self.importer = FlexibleAttributeImporter()
-        with open(f"{settings.PROJECT_ROOT}/apps/core/tests/test_files/flex_init.xls", "rb") as f:
-            wb = xlrd.open_workbook(file_contents=f)
-            self.survey_sheet = wb.sheet_by_name("survey")
-            self.choices_sheet = wb.sheet_by_name("choices")
-            self.importer._reset_model_fields_variables()
+        wb = xlrd.open_workbook(filename=f"{settings.PROJECT_ROOT}/apps/core/tests/test_files/flex_init.xls")
+        self.survey_sheet = wb.sheet_by_name("survey")
+        self.choices_sheet = wb.sheet_by_name("choices")
+        self.importer._reset_model_fields_variables()
 
     def test_get_model_fields(self):
         args = ("attribute", "group", "choice", "Not Correct Arg")
