2fb58925303cca7303fa5ccbcce9e634c243e967
sax <s.apostolico@gmail.com>
2021-02-05 21:55:04 +0100

    add user info so Sentry scope

--- backend/hct_mis_api/settings/base.py
+++ backend/hct_mis_api/settings/base.py
@@ -159,6 +159,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "hct_mis_api.middlewares.sentry.SentryScopeMiddleware",
 ]
 
 TEMPLATES = [
