88ccdd0872208ae3fecfe8958fe4aa13d969f85e
Egle Libby <eglelibby@gmail.com>
2021-02-08 12:03:29 -0800

    initial setup for rapidpro test connection form

--- backend/hct_mis_api/apps/core/admin.py
+++ backend/hct_mis_api/apps/core/admin.py
@@ -1,9 +1,10 @@
 import xlrd
-from admin_extra_urls.extras import link, ExtraUrlMixin
+from admin_extra_urls.extras import link, ExtraUrlMixin, action
 from django.contrib import admin, messages
+from django.conf.urls import url
 from django.contrib.messages import ERROR
 from django.core.exceptions import ValidationError, PermissionDenied
-from django.forms import forms
+from django import forms
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
 from django.utils.html import format_html
@@ -28,21 +29,92 @@ class XLSImportForm(forms.Form):
     xls_file = forms.FileField()
 
 
-@admin.register(BusinessArea)
-class BusinessAreaAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug")
-    change_form_template = "business_area_changeform.html"
+class TestRapidproForm(forms.Form):
+    phone_number = forms.CharField(
+        label="Phone number",
+        required=True,
+    )
+    flow_name = forms.CharField(label="Name of the test flow", initial="Test", required=True)
 
-    def response_change(self, request, obj):
-        if "_test_rapidpro_connection" in request.POST:
-            api = RapidProAPI(obj.slug)
-            error = api.test_connection_flow()
-            if error:
-                messages.error(request, error)
-            else:
-                messages.success(request, "Connection successful")
-            return HttpResponseRedirect(".")
-        return super().response_change(request, obj)
+
+@admin.register(BusinessArea)
+class BusinessAreaAdmin(ExtraUrlMixin, admin.ModelAdmin):
+    list_display = ("name", "slug")
+    # change_form_template = "core/business_area_changeform.html"
+
+    def get_context(self, request, pk=None, **kwargs):
+        opts = self.model._meta
+        app_label = opts.app_label
+        self.object = None
+
+        context = {
+            **self.admin_site.each_context(request),
+            **kwargs,
+            "opts": opts,
+            "app_label": app_label,
+        }
+        if pk:
+            self.object = self.get_object(request, pk)
+            context["business_area"] = self.object
+            context["title"] = f"Test `{self.object.name}` RapidPRO connection"
+
+        return context
+
+    @action(label="Test RapidPro Connection")
+    def _test_rapidpro_connection(self, request, pk):
+        context = self.get_context(request, pk)
+
+        if request.method == "GET":
+            context["form"] = TestRapidproForm()
+
+            return TemplateResponse(request, "core/test_rapidpro.html", context)
+        else:
+            form = TestRapidproForm(request.POST)
+            if form.is_valid():
+                phone_number = form.cleaned_data["phone_number"]
+                flow_name = form.cleaned_data["flow_name"]
+                context["phone_number"] = phone_number
+                context["flow_name"] = flow_name
+                print(phone_number, flow_name)
+                api = RapidProAPI(context["business_area"].slug)
+                error = api.test_connection_flow(flow_name, phone_number)
+                if error:
+                    messages.error(request, error)
+                else:
+                    messages.success(request, "Connection successful")
+            #     target_population = form.cleaned_data["target_population"]
+            #     context["title"] = f"Test results of `{self.object.name}` against `{target_population}`"
+            #     context["target_population"] = target_population
+            #     elements = []
+            #     context["elements"] = elements
+            #     entries = context["target_population"].selections.all()[:100]
+            #     if entries:
+            #         for tp in entries:
+            #             value = context["rule"].execute(hh=tp.household)
+            #             tp.vulnerability_score = value
+            #             elements.append(tp)
+            #         self.message_user(request, "%s scores calculated" % len(elements))
+            #     else:
+            #         self.message_user(request, "No records found", messages.WARNING)
+            return TemplateResponse(request, "core/test_rapidpro.html", context)
+
+    # def response_change(self, request, obj):
+    #     if "_test_rapidpro_connection" in request.POST:
+    #         api = RapidProAPI(obj.slug)
+    #         error = api.test_connection_flow()
+    #         if error:
+    #             messages.error(request, error)
+    #         else:
+    #             messages.success(request, "Connection successful")
+    #         return HttpResponseRedirect(".")
+    #     return super().response_change(request, obj)
+
+
+# class BusinessAreaTestRapidproAdmin(admin.AdminSite):
+#     def get_urls(self):
+#         urls = super(BusinessAreaTestRapidproAdmin, self).get_urls()
+#         custom_urls = [url(r"test-rapidpro$", test_rapidpro_view, name="test_rapidpro")]
+#         return urls + custom_urls
 
 
 @admin.register(AdminArea)
