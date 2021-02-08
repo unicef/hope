94d3818b78963ce306d9e19e3931b832d66ae8d6
Wojtek Nosal <wojciech.nosal@tivix.com>
2021-02-08 15:49:44 +0100

    Global Business Area

--- frontend/src/components/Dashboard/charts/PlannedBudget.tsx
+++ frontend/src/components/Dashboard/charts/PlannedBudget.tsx
@@ -10,7 +10,7 @@ export const PlannedBudget = ({
   data,
 }: PlannedBudgetProps): React.ReactElement => {
   const chartdata = {
-    labels: data.labels,
+    labels: data?.labels,
     datasets: [
       {
         barPercentage: 0.4,
