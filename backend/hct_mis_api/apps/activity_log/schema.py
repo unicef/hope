01ae969c64192355c63d1e237fac0da285cddaca
Jan Romaniak <jan.romaniak@tivix.com>
2021-02-16 15:21:11 +0100

    fix typo

--- backend/hct_mis_api/apps/activity_log/schema.py
+++ backend/hct_mis_api/apps/activity_log/schema.py
@@ -29,14 +29,14 @@ class LogEntryFilter(FilterSet):
         for value in values:
             if value.lower() == "system":
                 q_obj |= Q(user__isnull=True)
-            q_obj |= Q(content_type__model__starts_with=value)
-            q_obj |= Q(object_id__starts_with=value)
-            q_obj |= Q(action__starts_with=value)
-            q_obj |= Q(object_repr__starts_with=value)
-            q_obj |= Q(user__first_name__starts_with=value)
-            q_obj |= Q(user__last_name__starts_with=value)
-            q_obj |= Q(user__email__starts_with=value)
-            q_obj |= Q(timestamp__starts_with=value)
+            q_obj |= Q(content_type__model__startswith=value)
+            q_obj |= Q(object_id__startswith=value)
+            q_obj |= Q(action__startswith=value)
+            q_obj |= Q(object_repr__startswith=value)
+            q_obj |= Q(user__first_name__startswith=value)
+            q_obj |= Q(user__last_name__startswith=value)
+            q_obj |= Q(user__email__startswith=value)
+            q_obj |= Q(timestamp__startswith=value)
         return qs.filter(q_obj)
 
 
