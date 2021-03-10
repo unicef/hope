ee502ff23422b7380a635abd94b9d9e0c3f217fb
Jan Romaniak <jan.romaniak@tivix.com>
2021-03-10 15:05:34 +0100

    set expirations secs to 30 create correct view for non expiring report link

--- backend/hct_mis_api/apps/core/storage.py
+++ backend/hct_mis_api/apps/core/storage.py
@@ -6,7 +6,7 @@ class AzureMediaStorage(AzureStorage):
     account_name = settings.AZURE_ACCOUNT_NAME
     account_key = settings.AZURE_ACCOUNT_KEY
     azure_container = "media"
-
+    expiration_secs = 30
 
 class AzureStaticStorage(AzureStorage):
     account_name = settings.AZURE_ACCOUNT_NAME
