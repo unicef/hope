2d5d11c7bab1ec3fb656bd1f701e4d338c6a0a19
Jan Romaniak <jan.romaniak@tivix.com>
2021-03-18 15:15:42 +0100

    added retry task, fixed test, added retry button

--- backend/hct_mis_api/apps/core/admin.py
+++ backend/hct_mis_api/apps/core/admin.py
@@ -4,7 +4,7 @@ from django import forms
 from django.contrib import admin, messages
 from django.contrib.messages import ERROR
 from django.core.exceptions import ValidationError, PermissionDenied
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.template.response import TemplateResponse
 from django.utils.html import format_html
 from xlrd import XLRDError
@@ -158,6 +158,14 @@ class XLSXKoboTemplateAdmin(ExtraUrlMixin, admin.ModelAdmin):
             level=ERROR,
         )
 
+    @action(label="Rerun KOBO Import", visible=lambda o: o is not None and o.status != XLSXKoboTemplate.SUCCESSFUL)
+    def rerun_kobo_import(self, request, pk):
+        xlsx_kobo_template_object = get_object_or_404(XLSXKoboTemplate, pk=pk)
+        upload_new_kobo_template_and_update_flex_fields_task.run(
+            xlsx_kobo_template_id=str(xlsx_kobo_template_object.id)
+        )
+        return redirect(".")
+
     def add_view(self, request, form_url="", extra_context=None):
         if not self.has_add_permission(request):
             raise PermissionDenied
@@ -204,8 +212,7 @@ class XLSXKoboTemplateAdmin(ExtraUrlMixin, admin.ModelAdmin):
                 self.message_user(
                     request,
                     "Core field validation successful, running KoBo Template upload task..., "
-                    "Import stat"
-                    "us will change after task completion",
+                    "Import status will change after task completion",
                 )
                 upload_new_kobo_template_and_update_flex_fields_task.run(
                     xlsx_kobo_template_id=str(xlsx_kobo_template_object.id)
--- backend/hct_mis_api/apps/core/celery_tasks.py
+++ backend/hct_mis_api/apps/core/celery_tasks.py
@@ -1,4 +1,23 @@
 from hct_mis_api.apps.core.celery import app
+from hct_mis_api.apps.core.models import XLSXKoboTemplate
+from hct_mis_api.apps.core.tasks.upload_new_template_and_update_flex_fields import KoboRetriableError
+
+
+@app.task(bind=True, default_retry_delay=60)
+def upload_new_kobo_template_and_update_flex_fields_task_with_retry(self, xlsx_kobo_template_id):
+    from hct_mis_api.apps.core.tasks.upload_new_template_and_update_flex_fields import (
+        UploadNewKoboTemplateAndUpdateFlexFieldsTask,
+    )
+    from datetime import datetime, timedelta
+
+    try:
+        UploadNewKoboTemplateAndUpdateFlexFieldsTask().execute(xlsx_kobo_template_id=xlsx_kobo_template_id)
+    except KoboRetriableError as exc:
+        one_day_earlier_time = datetime.now() - timedelta(days=1)
+        if exc.xlsx_kobo_template_object.first_connection_failed_time > one_day_earlier_time:
+            raise self.retry(exc=exc)
+        else:
+            exc.xlsx_kobo_template_object.status = XLSXKoboTemplate.UNSUCCESSFUL
 
 
 @app.task
@@ -7,27 +26,7 @@ def upload_new_kobo_template_and_update_flex_fields_task(xlsx_kobo_template_id):
         UploadNewKoboTemplateAndUpdateFlexFieldsTask,
     )
 
-    UploadNewKoboTemplateAndUpdateFlexFieldsTask().execute(xlsx_kobo_template_id=xlsx_kobo_template_id)
-
-
-@app.task
-def check_periodically_connection_failed_tasks():
-    from hct_mis_api.apps.core.tasks.upload_new_template_and_update_flex_fields import (
-        UploadNewKoboTemplateAndUpdateFlexFieldsTask,
-    )
-    from datetime import datetime, timedelta
-    from hct_mis_api.apps.core.models import XLSXKoboTemplate
-
-    one_day_earlier_time = datetime.now() - timedelta(days=1)
-    XLSXKoboTemplate.objects.filter(
-        first_connection_failed_time__lte=one_day_earlier_time, status=XLSXKoboTemplate.CONNECTION_FAILED
-    ).update(status=XLSXKoboTemplate.UNSUCCESSFUL)
-    template = (
-        XLSXKoboTemplate.objects.filter(
-            first_connection_failed_time__gt=one_day_earlier_time, status=XLSXKoboTemplate.CONNECTION_FAILED
-        )
-        .order_by("-created_at")
-        .first()
-    )
-    if template is not None:
-        UploadNewKoboTemplateAndUpdateFlexFieldsTask().execute(xlsx_kobo_template_id=template.id)
+    try:
+        UploadNewKoboTemplateAndUpdateFlexFieldsTask().execute(xlsx_kobo_template_id=xlsx_kobo_template_id)
+    except KoboRetriableError:
+        upload_new_kobo_template_and_update_flex_fields_task_with_retry.delay(xlsx_kobo_template_id)
