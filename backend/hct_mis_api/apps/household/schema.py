684c1920ffb73808fab9e357979dee4d970cc9a8
Wojtek Nosal <wojciech.nosal@tivix.com>
2021-03-19 15:54:19 +0100

    Resolving flex fields image urls

--- backend/hct_mis_api/apps/household/schema.py
+++ backend/hct_mis_api/apps/household/schema.py
@@ -1,6 +1,3 @@
-import base64
-import os
-
 import graphene
 from django.db.models import Prefetch, Q, Sum
 from django.db.models.functions import Lower
@@ -61,7 +58,12 @@ from hct_mis_api.apps.payment.utils import get_payment_records_for_dashboard
 from hct_mis_api.apps.program.models import Program
 from hct_mis_api.apps.registration_datahub.schema import DeduplicationResultNode
 from hct_mis_api.apps.targeting.models import HouseholdSelection
-from hct_mis_api.apps.utils.schema import ChartDatasetNode, ChartDetailedDatasetsNode, SectionTotalNode
+from hct_mis_api.apps.utils.schema import (
+    ChartDatasetNode,
+    ChartDetailedDatasetsNode,
+    SectionTotalNode,
+    FlexFieldsScalar,
+)
 
 INDIVIDUALS_CHART_LABELS = [
     "Females 0-5",
@@ -222,27 +224,6 @@ class DocumentNode(DjangoObjectType):
         filter_fields = []
         interfaces = (relay.Node,)
         connection_class = ExtendedConnection
-
-
-class FlexFieldsScalar(graphene.Scalar):
-    """
-    Allows use of a JSON String for input / output from the GraphQL schema.
-
-    Use of this type is *not recommended* as you lose the benefits of having a defined, static
-    schema (one of the key benefits of GraphQL).
-    """
-
-    @staticmethod
-    def serialize(dt):
-        return dt
-
-    @staticmethod
-    def parse_literal(node):
-        return node
-
-    @staticmethod
-    def parse_value(value):
-        return value
 
 
 class ExtendedHouseHoldConnection(graphene.Connection):
