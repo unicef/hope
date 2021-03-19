681d99adfb7bdab1c94d0560e4b9c015f767446d
Wojtek Nosal <wojciech.nosal@tivix.com>
2021-03-19 16:43:56 +0100

    displaying flex fields images in vulnerabilities

--- frontend/src/components/population/HouseholdVulnerabilities.tsx
+++ frontend/src/components/population/HouseholdVulnerabilities.tsx
@@ -4,7 +4,12 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import { LabelizedField } from '../LabelizedField';
-import { HouseholdDetailedFragment } from '../../__generated__/graphql';
+import {
+  HouseholdDetailedFragment,
+  useAllHouseholdsFlexFieldsAttributesQuery,
+} from '../../__generated__/graphql';
+import { useArrayToDict } from '../../hooks/useArrayToDict';
+import { LoadingComponent } from '../LoadingComponent';
 
 const Overview = styled(Paper)`
   padding: ${({ theme }) => theme.spacing(8)}px
@@ -20,6 +25,10 @@ const Title = styled.div`
   padding-bottom: ${({ theme }) => theme.spacing(8)}px;
 `;
 
+const Image = styled.img`
+  max-width: 150px;
+`;
+
 interface HouseholdVulnerabilitiesProps {
   household: HouseholdDetailedFragment;
 }
@@ -27,8 +36,32 @@ interface HouseholdVulnerabilitiesProps {
 export function HouseholdVulnerabilities({
   household,
 }: HouseholdVulnerabilitiesProps): React.ReactElement {
+  const { data, loading } = useAllHouseholdsFlexFieldsAttributesQuery();
+  const flexAttributesDict = useArrayToDict(
+    data?.allHouseholdsFlexFieldsAttributes,
+    'name',
+    '*',
+  );
+
+  if (loading) {
+    return <LoadingComponent />;
+  }
+
+  if (!data || !flexAttributesDict) {
+    return null;
+  }
   const fields = Object.entries(household.flexFields || {}).map(
     ([key, value]: [string, string | string[]]) => {
+      if (flexAttributesDict[key].type === 'IMAGE') {
+        return (
+          <LabelizedField
+            key={key}
+            label={key.replaceAll('_i_f', '').replace(/_/g, ' ')}
+          >
+            <Image src={value} />
+          </LabelizedField>
+        );
+      }
       return (
         <LabelizedField
           key={key}
