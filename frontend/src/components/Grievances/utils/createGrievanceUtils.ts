2f7bf0fc7f0e0b9fc54fe9f5e0483e820b2b32de
Jan Romaniak <jan.romaniak@tivix.com>
2021-04-09 13:26:15 +0200

    #80949 Grivance tickets are not showing and are blocking system

--- frontend/src/components/Grievances/utils/createGrievanceUtils.ts
+++ frontend/src/components/Grievances/utils/createGrievanceUtils.ts
@@ -62,6 +62,15 @@ function prepareSesitiveVariables(requiredVariables, values) {
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function prepareAddIndividualVariables(requiredVariables, values) {
+  let { flexFields } = values.individualData;
+  if (flexFields) {
+    flexFields = { ...flexFields };
+    for (const [key, value] of Object.entries(flexFields)) {
+      if (value === '') {
+        delete flexFields[key];
+      }
+    }
+  }
   return {
     variables: {
       input: {
@@ -72,7 +81,7 @@ function prepareAddIndividualVariables(requiredVariables, values) {
           issueType: {
             addIndividualIssueTypeExtras: {
               household: values.selectedHousehold?.id,
-              individualData: values.individualData,
+              individualData: { ...values.individualData, flexFields },
             },
           },
         },
