b7f2d411c19face77ad68a0fb9e4bf62786a67be
Jan Romaniak <jan.romaniak@tivix.com>
2021-04-29 12:07:48 +0200

    only acitve check box pull pictures check box duplicated keys fix

--- frontend/src/apollo/registration/SaveKoboImportData.ts
+++ frontend/src/apollo/registration/SaveKoboImportData.ts
@@ -1,14 +1,22 @@
 import { gql } from 'apollo-boost';
 
 export const SAVE_KOBO_IMPORT_DATA = gql`
-  mutation SaveKoboImportData($businessAreaSlug: String!, $projectId: Upload!) {
-    saveKoboImportData(businessAreaSlug: $businessAreaSlug, uid: $projectId) {
+  mutation SaveKoboImportData(
+    $businessAreaSlug: String!
+    $projectId: Upload!
+    $onlyActiveSubmissions: Boolean!
+  ) {
+    saveKoboImportData(
+      businessAreaSlug: $businessAreaSlug
+      uid: $projectId
+      onlyActiveSubmissions: $onlyActiveSubmissions
+    ) {
       importData {
         id
         numberOfHouseholds
         numberOfIndividuals
       }
-      errors{
+      errors {
         header
         message
       }
