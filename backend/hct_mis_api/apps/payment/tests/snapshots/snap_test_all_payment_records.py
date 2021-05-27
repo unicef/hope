4188aaa1482cbc798131f81d10dfe1fa21a1c08a
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2021-05-27 14:33:15 +0200

    Fix problem with payment records filtering

--- backend/hct_mis_api/apps/payment/tests/snapshots/snap_test_all_payment_records.py
+++ backend/hct_mis_api/apps/payment/tests/snapshots/snap_test_all_payment_records.py
@@ -4,10 +4,9 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
-
 snapshots = Snapshot()
 
-snapshots['TestAllPaymentRecords::test_fetch_payment_records_only_for_single_household 1'] = {
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_household 1'] = {
     'data': {
         'allPaymentRecords': {
             'edgeCount': 3,
@@ -16,7 +15,124 @@ snapshots['TestAllPaymentRecords::test_fetch_payment_records_only_for_single_hou
     }
 }
 
-snapshots['TestAllPaymentRecords::test_household_without_payment_records_should_return_empty_list 1'] = {
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_household 2'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 4,
+            'totalCount': 4
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_household 3'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 0,
+            'totalCount': 0
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan 1'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 4,
+            'totalCount': 4
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan 2'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 1,
+            'totalCount': 1
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan 3'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 2,
+            'totalCount': 2
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 1'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 2,
+            'totalCount': 2
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 2'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 1,
+            'totalCount': 1
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 3'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 0,
+            'totalCount': 0
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 4'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 2,
+            'totalCount': 2
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 5'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 0,
+            'totalCount': 0
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 6'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 2,
+            'totalCount': 2
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 7'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 0,
+            'totalCount': 0
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 8'] = {
+    'data': {
+        'allPaymentRecords': {
+            'edgeCount': 0,
+            'totalCount': 0
+        }
+    }
+}
+
+snapshots['TestAllPaymentRecords::test_fetch_payment_records_filter_by_cash_plan_and_household 9'] = {
     'data': {
         'allPaymentRecords': {
             'edgeCount': 0,
