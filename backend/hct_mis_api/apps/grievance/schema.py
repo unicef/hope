636f9cdaea1e4925cf3b7a084fdc430c830f3016
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2021-07-07 14:13:21 +0200

    Add ability to link household and individual in positive feedback ticket

--- backend/hct_mis_api/apps/grievance/schema.py
+++ backend/hct_mis_api/apps/grievance/schema.py
@@ -55,6 +55,7 @@ from hct_mis_api.apps.grievance.models import (
     TicketNeedsAdjudicationDetails,
     TicketNote,
     TicketPaymentVerificationDetails,
+    TicketPositiveFeedbackDetails,
     TicketSensitiveDetails,
     TicketSystemFlaggingDetails,
 )
@@ -322,11 +323,9 @@ class GrievanceTicketNode(BaseNodePermissionMixin, DjangoObjectType):
     @staticmethod
     def _search_for_lookup(grievance_ticket_obj, lookup_name):
         for field, lookups in GrievanceTicket.FIELD_TICKET_TYPES_LOOKUPS.items():
-            # print(grievance_ticket_obj.status, lookup_name)
             extras_field = getattr(grievance_ticket_obj, field, None)
             if extras_field is None:
                 continue
-            # print(extras_field)
             real_lookup = lookup_name
             for lookup in lookups:
                 if isinstance(lookup, dict):
@@ -471,6 +470,14 @@ class TicketSystemFlaggingDetailsNode(DjangoObjectType):
 class TicketPaymentVerificationDetailsNode(DjangoObjectType):
     class Meta:
         model = TicketPaymentVerificationDetails
+        exclude = ("ticket",)
+        interfaces = (relay.Node,)
+        connection_class = ExtendedConnection
+
+
+class TicketPositiveFeedbackDetailsNode(DjangoObjectType):
+    class Meta:
+        model = TicketPositiveFeedbackDetails
         exclude = ("ticket",)
         interfaces = (relay.Node,)
         connection_class = ExtendedConnection
