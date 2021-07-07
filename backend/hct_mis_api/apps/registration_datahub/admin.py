7d259f81af54b1c7546c5b4afb8f70d64d5b3325
sax <s.apostolico@gmail.com>
2021-07-07 15:13:10 +0200

    updates

--- backend/hct_mis_api/apps/registration_datahub/admin.py
+++ backend/hct_mis_api/apps/registration_datahub/admin.py
@@ -1,23 +1,46 @@
-from adminfilters.filters import TextFieldFilter
 from django.contrib import admin
+from django.template.response import TemplateResponse
+
+from admin_extra_urls.decorators import button
+from admin_extra_urls.mixins import ExtraUrlMixin
+from adminfilters.filters import ChoicesFieldComboFilter, TextFieldFilter
 
 from hct_mis_api.apps.registration_datahub.models import (
-    RegistrationDataImportDatahub,
-    ImportedIndividual,
-    ImportedHousehold,
-    ImportedDocumentType,
-    ImportedDocument,
     ImportData,
-    ImportedIndividualRoleInHousehold,
+    ImportedDocument,
+    ImportedDocumentType,
+    ImportedHousehold,
+    ImportedIndividual,
     ImportedIndividualIdentity,
+    ImportedIndividualRoleInHousehold,
     KoboImportedSubmission,
+    RegistrationDataImportDatahub,
 )
 from hct_mis_api.apps.utils.admin import HOPEModelAdminBase
 
 
 @admin.register(RegistrationDataImportDatahub)
-class RegistrationDataImportDatahubAdmin(HOPEModelAdminBase):
-    pass
+class RegistrationDataImportDatahubAdmin(ExtraUrlMixin, HOPEModelAdminBase):
+    list_display = ("name", "import_date", "import_done", "business_area_slug")
+    list_filter = (
+        "import_done",
+        TextFieldFilter.factory("business_area_slug__istartswith"),
+    )
+    date_hierarchy = "import_date"
+
+    @button()
+    def inspect(self, request, pk):
+        context = self.get_common_context(request, pk)
+        obj: RegistrationDataImportDatahub = context["original"]
+        context["title"] = f"Import {obj.name} - {obj.import_done}"
+        context["data"] = {}
+        has_content = False
+        for model in [ImportedIndividual, ImportedHousehold]:
+            count = model.objects.filter(registration_data_import=obj).count()
+            has_content = has_content or count
+            context["data"][model] = {"count": count, "warnings": [], "errors": [], "meta": model._meta}
+
+        return TemplateResponse(request, "registration_datahub/admin/inspect.html", context)
 
 
 @admin.register(ImportedIndividual)
@@ -34,32 +57,44 @@ class ImportedIndividualAdmin(HOPEModelAdminBase):
 
 @admin.register(ImportedIndividualIdentity)
 class ImportedIndividualIdentityAdmin(HOPEModelAdminBase):
-    pass
+    raw_id_fields = ("individual",)
 
 
 @admin.register(ImportedHousehold)
 class ImportedHouseholdAdmin(HOPEModelAdminBase):
-    pass
+    list_display = ("registration_data_import", "registration_method", "name_enumerator", "country", "country_origin")
+    raw_id_fields = ("registration_data_import", "head_of_household")
+    date_hierarchy = "registration_data_import__import_date"
+    list_filter = (
+        ("country", ChoicesFieldComboFilter),
+        ("country_origin", ChoicesFieldComboFilter),
+        TextFieldFilter.factory("registration_data_import__name__istartswith"),
+        TextFieldFilter.factory("kobo_submission_uuid__istartswith"),
+    )
 
 
 @admin.register(ImportData)
 class ImportDataAdmin(HOPEModelAdminBase):
-    pass
-
-
-@admin.register(ImportedDocument)
-class ImportedDocumentAdmin(HOPEModelAdminBase):
-    list_display = ("document_number", "type", "individual")
+    list_filter = ("data_type",)
+    date_hierarchy = "created_at"
 
 
 @admin.register(ImportedDocumentType)
 class ImportedDocumentTypeAdmin(HOPEModelAdminBase):
     list_display = ("label", "country")
+    list_filter = (("country", ChoicesFieldComboFilter),)
+
+
+@admin.register(ImportedDocument)
+class ImportedDocumentAdmin(HOPEModelAdminBase):
+    list_display = ("document_number", "type", "individual")
+    raw_id_fields = ("individual", "type")
 
 
 @admin.register(ImportedIndividualRoleInHousehold)
 class ImportedIndividualRoleInHouseholdAdmin(HOPEModelAdminBase):
-    pass
+    raw_id_fields = ("individual", "household")
+    list_filter = ("role",)
 
 
 @admin.register(KoboImportedSubmission)
