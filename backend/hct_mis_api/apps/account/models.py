354379bd9feec4cff55e72b344c9f511a2a553cc
sax <s.apostolico@gmail.com>
2021-07-15 11:03:47 +0200

    updates reqs

--- backend/hct_mis_api/apps/account/models.py
+++ backend/hct_mis_api/apps/account/models.py
@@ -103,7 +103,7 @@ class User(AbstractUser, UUIDModel):
         permissions = (
             ("can_load_from_ad", "Can load users from ActiveDirectory"),
             ("can_sync_with_ad", "Can synchronise user with ActiveDirectory"),
-            ("can_upload_to_kobo", "Can upload users to Kobo"),
+            ("can_create_kobo_user", "Can create users in Kobo"),
             ("can_import_from_kobo", "Can import and sync users from Kobo"),
             ("can_debug", "Can access debug informations"),
             ("can_inspect", "Can inspect objects"),
