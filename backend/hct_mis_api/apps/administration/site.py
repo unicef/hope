dc7760ff5872e3ad971bd7ba92942e830ebdf99f
sax <s.apostolico@gmail.com>
2021-08-06 18:31:54 +0200

    fixes wrong logger rename

--- backend/hct_mis_api/apps/administration/site.py
+++ backend/hct_mis_api/apps/administration/site.py
@@ -7,12 +7,17 @@ from smart_admin.site import SmartAdminSite
 cache = caches["default"]
 
 
+def clean(v):
+    return v.replace(r"\n", "").strip()
+
+
 def get_bookmarks(request):
     quick_links = []
     for entry in config.QUICK_LINKS.split(";"):
-        if entry:
+        if clean(entry):
             try:
-                quick_links.append(entry.split(","))
+                label, url = entry.split(",")
+                quick_links.append([clean(label), clean(url)])
             except ValueError:
                 pass
     return quick_links
