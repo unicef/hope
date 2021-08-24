a3da06bb1e37bd926d4507545d65b1fa272f07c3
sax <s.apostolico@gmail.com>
2021-08-24 12:28:45 +0200

    log role/group permissions change

--- backend/hct_mis_api/apps/activity_log/admin.py
+++ backend/hct_mis_api/apps/activity_log/admin.py
@@ -5,4 +5,17 @@ from hct_mis_api.apps.activity_log.models import LogEntry
 
 @admin.register(LogEntry)
 class LogEntryAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "timestamp",
+        "user",
+        "action",
+        "business_area",
+        "content_type",
+    )
+    date_hierarchy = "timestamp"
+    search_fields = ("object_repr",)
+    raw_id_fields = ("business_area", "user")
+    list_filter = (
+        "business_area",
+        "content_type",
+    )
--- backend/hct_mis_api/apps/grievance/admin.py
+++ backend/hct_mis_api/apps/grievance/admin.py
@@ -5,6 +5,7 @@ from adminfilters.filters import (
     RelatedFieldComboFilter,
     TextFieldFilter,
 )
+from advanced_filters.admin import AdminAdvancedFiltersMixin
 
 from hct_mis_api.apps.grievance.models import (
     GrievanceTicket,
@@ -20,9 +21,10 @@ from hct_mis_api.apps.utils.admin import HOPEModelAdminBase
 
 
 @admin.register(GrievanceTicket)
-class GrievanceTicketAdmin(HOPEModelAdminBase):
+class GrievanceTicketAdmin(AdminAdvancedFiltersMixin, HOPEModelAdminBase):
+
     list_display = ("created_at", "created_by", "assigned_to", "status", "category")
-    raw_id_fields = ("created_by", "assigned_to")
+    raw_id_fields = ("created_by", "assigned_to", "admin2", "business_area", "registration_data_import")
     list_filter = (
         ("status", ChoicesFieldComboFilter),
         ("category", ChoicesFieldComboFilter),
@@ -31,12 +33,20 @@ class GrievanceTicketAdmin(HOPEModelAdminBase):
         TextFieldFilter.factory("assigned_to__username__istartswith"),
         "updated_at",
     )
+    advanced_filter_fields = (
+        "status",
+        "category",
+        ("created_by__username__istartswith", "created by"),
+        ("assigned_to__username__istartswith", "assigned to"),
+        ("business_area__name", "business area"),
+    )
+
     readonly_fields = ("unicef_id",)
 
 
 @admin.register(TicketNote)
 class TicketNoteAdmin(HOPEModelAdminBase):
-    pass
+    raw_id_fields = ("ticket", "created_by")
 
 
 @admin.register(TicketComplaintDetails)
