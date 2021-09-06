f0b1c70a7467c0ca4da834d255577a9bf6a8a6eb
Jan Romaniak <jan.romaniak@tivix.com>
2021-09-06 09:31:43 +0200

    fixes to url

--- backend/hct_mis_api/settings/base.py
+++ backend/hct_mis_api/settings/base.py
@@ -147,9 +147,9 @@ if "DATABASE_URL_HUB_REGISTRATION" not in os.environ:
         f'{os.getenv("POSTGRES_REGISTRATION_DATAHUB_DB")}'
     )
 os.environ["DATABASE_URL_BACKUP"] = (
-    f'postgis://{os.getenv("POSTGRES_USER")}'
+    f'postgis://hctappprdusr'
     f':{os.getenv("POSTGRES_PASSWORD")}'
-    f'@pgsql-10-reserved-hctmis-prd-restore090321:5432/'
+    f'@pgsql-10-reserved-hctmis-prd-restore090321.postgres.database.azure.com:5432/'
     f'{os.getenv("POSTGRES_DB")}'
 )
 
@@ -159,7 +159,7 @@ DATABASES = {
     "cash_assist_datahub_ca": env.db("DATABASE_URL_HUB_CA"),
     "cash_assist_datahub_erp": env.db("DATABASE_URL_HUB_ERP"),
     "registration_datahub": env.db("DATABASE_URL_HUB_REGISTRATION"),
-    # "backup_database": env.db("DATABASE_URL_BACKUP")
+    "backup_database": env.db("DATABASE_URL_BACKUP")
 }
 
 # If app is not specified here it will use default db
@@ -168,7 +168,7 @@ DATABASE_APPS_MAPPING = {
     "mis_datahub": "cash_assist_datahub_mis",
     "erp_datahub": "cash_assist_datahub_erp",
     "registration_datahub": "registration_datahub",
-    "grievance2": "registration_datahub",
+    "grievance2": "backup_database",
 }
 
 DATABASE_ROUTERS = ("hct_mis_api.apps.core.dbrouters.DbRouter",)
