11628eafcddca14efe1b235061585a6bacbac685
Jan Romaniak <jan.romaniak@tivix.com>
2021-09-06 10:54:21 +0200

    changes to restore backup

--- backend/hct_mis_api/apps/grievance2/celery_tasks.py
+++ backend/hct_mis_api/apps/grievance2/celery_tasks.py
@@ -36,22 +36,39 @@ def restore_backup():
             "TicketNegativeFeedbackDetails",
             "TicketReferralDetails",
         ]
+        ignored_ticket_ids = []
         for model_name in models:
             model_backup = getattr(models_backup, model_name)
             model_destination = getattr(models_destination, model_name)
-            for obj_backup in model_backup.objects.all():
+            for obj_backup in model_backup.objects.exclude(
+                pk__in=list(model_destination.objects.values_list("pk", flat=True))
+            ).all():
                 try:
+                    if (
+                        hasattr(obj_backup, "main_ticket")
+                        and (
+                            obj_backup.main_ticket.pk in ignored_ticket_ids
+                            or obj_backup.linked_ticket.pk in ignored_ticket_ids
+                        )
+                        or (hasattr(obj_backup, "ticket") and obj_backup.ticket.pk in ignored_ticket_ids)
+                    ):
+                        continue
                     obj_destination = copy_model_object(obj_backup, model_destination)
                     obj_destination.save()
                 except ValidationError:
                     pass
                 except Exception as e:
                     if 'is not present in table "household_individual"' in str(e):
+                        if model_name == "GrievanceTicket":
+                            ignored_ticket_ids.append(obj_backup.pk)
                         pass
                     elif 'is not present in table "household_household"' in str(e):
+                        if model_name == "GrievanceTicket":
+                            ignored_ticket_ids.append(obj_backup.pk)
                         pass
                     else:
                         raise
     except Exception as e:
         logger.exception(e)
         raise
+    return ignored_ticket_ids
