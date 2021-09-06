a66bc972c5224deb3f777e5994f0b44ecba7bfe6
Jan Romaniak <jan.romaniak@tivix.com>
2021-09-06 14:05:36 +0200

    reverse code for using backup grivances

--- backend/docker-test-entrypoint.sh
+++ backend/docker-test-entrypoint.sh
@@ -6,5 +6,5 @@ until pg_isready -h $POSTGRES_CASHASSIST_DATAHUB_HOST -p 5432;
   do echo "waiting for database"; sleep 1; done;
 until pg_isready -h $POSTGRES_REGISTRATION_DATAHUB_HOST -p 5432;
   do echo "waiting for database"; sleep 1; done;
-#python manage.py test  --settings hct_mis_api.settings.test
+python manage.py test  --settings hct_mis_api.settings.test
 
--- backend/hct_mis_api/apps/grievance2/mutations_extras/payment_verification.py
+++
@@ -1,9 +0,0 @@
-import graphene
-
-
-class PaymentVerificationTicketExtras(graphene.InputObjectType):
-    pass
-
-
-def save_payment_verification_extras(root, info, input, grievance_ticket, extras, **kwargs):
-    pass
