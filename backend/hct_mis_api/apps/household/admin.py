c9f57e4b20101a0985e781d875f5b035c84adda4
sax <s.apostolico@gmail.com>
2021-09-13 15:13:04 +0200

    some more admin tweaks

--- backend/hct_mis_api/apps/household/admin.py
+++ backend/hct_mis_api/apps/household/admin.py
@@ -333,6 +333,7 @@ class EntitlementCardAdmin(ExtraUrlMixin, HOPEModelAdminBase):
     list_display = ("id", "card_number", "status", "card_type", "service_provider")
     search_fields = ("card_number",)
     date_hierarchy = "created_at"
+    raw_id_fields = ("household",)
     list_filter = (
         "status",
         TextFieldFilter.factory("card_type"),
