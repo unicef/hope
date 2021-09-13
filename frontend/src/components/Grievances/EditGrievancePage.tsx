d567053e6d7e692fb01783c58288d619e8aa38d2
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-09-13 15:28:03 +0200

    Add formik file field to handle image type flexfields

--- frontend/src/components/Grievances/EditGrievancePage.tsx
+++ frontend/src/components/Grievances/EditGrievancePage.tsx
@@ -1,10 +1,4 @@
-import {
-  Box,
-  Button,
-  DialogActions,
-  FormHelperText,
-  Grid,
-} from '@material-ui/core';
+import { Box, Button, FormHelperText, Grid } from '@material-ui/core';
 import { Field, Formik } from 'formik';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -65,12 +59,6 @@ const BoxPadding = styled.div`
 `;
 const NewTicket = styled.div`
   padding: 20px;
-`;
-const DialogFooter = styled.div`
-  padding: 12px 16px;
-  margin: 0;
-  border-top: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
-  text-align: right;
 `;
 const BoxWithBorderBottom = styled.div`
   border-bottom: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
