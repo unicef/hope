82e6de6338314731f9812e5f0f38e37bb5581e85
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-09-13 15:28:03 +0200

    Add formik file field to handle image type flexfields

--- frontend/src/components/PageHeader.tsx
+++ frontend/src/components/PageHeader.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import { Typography } from '@material-ui/core';
-import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
 import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 import { BreadCrumbs, BreadCrumbsItem } from './BreadCrumbs';
 import { Flag } from './Flag';
 import { FlagTooltip } from './FlagTooltip';
-import { useTranslation } from 'react-i18next';
 
 const Wrapper = styled.div`
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
---
+++ frontend/src/shared/Formik/FormikFileField/FormikFileField.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import InputLabel from '../../InputLabel';
+
+export const FormikFileField = ({
+  field,
+  form,
+  ...otherProps
+}): React.ReactElement => {
+  return (
+    <>
+      <Box mb={1}>
+        <InputLabel>{otherProps.label}</InputLabel>
+      </Box>
+      <input
+        type='file'
+        onChange={(event) => {
+          form.setFieldValue(field.name, event.currentTarget.files[0]);
+        }}
+      />
+    </>
+  );
+};
