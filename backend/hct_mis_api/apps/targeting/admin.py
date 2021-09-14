32ff152750b3dbdb846fbfd4f9c67b349ad4d3b2
sax <s.apostolico@gmail.com>
2021-09-14 23:32:27 +0200

    add duplicates view and TP inspect

--- backend/hct_mis_api/apps/targeting/admin.py
+++ backend/hct_mis_api/apps/targeting/admin.py
@@ -13,6 +13,7 @@ from adminfilters.filters import (
     RelatedFieldComboFilter,
     TextFieldFilter,
 )
+from smart_admin.mixins import LinkedObjectsMixin
 
 from hct_mis_api.apps.steficon.models import Rule
 from hct_mis_api.apps.targeting.models import HouseholdSelection, TargetPopulation
@@ -25,7 +26,7 @@ class RuleTestForm(forms.Form):
 
 
 @admin.register(TargetPopulation)
-class TargetPopulationAdmin(SoftDeletableAdminMixin, ExtraUrlMixin, HOPEModelAdminBase):
+class TargetPopulationAdmin(SoftDeletableAdminMixin, LinkedObjectsMixin, ExtraUrlMixin, HOPEModelAdminBase):
     list_display = (
         "name",
         "status",
@@ -55,6 +56,12 @@ class TargetPopulationAdmin(SoftDeletableAdminMixin, ExtraUrlMixin, HOPEModelAdm
         obj = self.get_object(request, pk)
         url = reverse("admin:targeting_householdselection_changelist")
         return HttpResponseRedirect(f"{url}?target_population|id={obj.id}")
+
+    @button()
+    def inspect(self, request, pk):
+        context = self.get_common_context(request, pk, title="Inspect")
+
+        return TemplateResponse(request, "admin/targeting/inspect.html", context)
 
     @button()
     def test_steficon(self, request, pk):
