344c9a27af3439a5ea869c12bb6356178d267c68
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2021-09-15 11:57:11 +0200

    Fix mark submissions

--- backend/hct_mis_api/apps/registration_datahub/tasks/mark_submissions.py
+++ backend/hct_mis_api/apps/registration_datahub/tasks/mark_submissions.py
@@ -1,13 +1,8 @@
-import operator
-from functools import reduce
-
-from django.db.models import Q
-
 from hct_mis_api.apps.core.models import BusinessArea
+from hct_mis_api.apps.registration_data.models import RegistrationDataImport
 from hct_mis_api.apps.registration_datahub.models import (
     ImportedHousehold,
     KoboImportedSubmission,
-    RegistrationDataImportDatahub,
 )
 
 
@@ -17,12 +12,12 @@ class MarkSubmissions:
 
     def execute(self):
         # Filter rdi with status done and following business area slug
-        rdi_ids = self._get_rdi_ids()
-        if not rdi_ids:
+        datahub_ids = self._get_datahub_ids()
+        if not datahub_ids:
             return {"message": "No suitable RDI found", "submissions": 0}
 
         # Filter households submissions_id for following rdi id
-        submission_ids = self._get_submissions_ids(rdi_ids)
+        submission_ids = self._get_submissions_ids(datahub_ids)
         if not submission_ids:
             return {"message": "No suitable Submissions found", "submissions": 0}
 
@@ -35,21 +30,18 @@ class MarkSubmissions:
         rows = submissions.update(amended=True)
         return {"message": f"{rows} submissions successfully amended", "submissions": rows}
 
-
     def _get_submissions(self, submission_ids):
-        return KoboImportedSubmission.objects.exclude(
-            reduce(operator.or_, (Q(kobo_submission_uuid=submission_id) for submission_id in submission_ids))
-        )
+        return KoboImportedSubmission.objects.exclude(kobo_submission_uuid__in=list(submission_ids))
 
-    def _get_submissions_ids(self, rdi_ids):
+    def _get_submissions_ids(self, datahub_ids):
         return ImportedHousehold.objects.filter(
             kobo_submission_uuid__isnull=False,
-            registration_data_import__id__in=rdi_ids,
+            registration_data_import__id__in=list(datahub_ids),
         ).values_list("kobo_submission_uuid", flat=True)
 
-    def _get_rdi_ids(self):
+    def _get_datahub_ids(self):
         return (
-            RegistrationDataImportDatahub.objects.filter(import_done=RegistrationDataImportDatahub.DONE)
-            .filter(business_area_slug=self.business_area.slug)
-            .values_list("id", flat=True)
+            RegistrationDataImport.objects.filter(status=RegistrationDataImport.MERGED)
+            .filter(business_area=self.business_area)
+            .values_list("datahub_id", flat=True)
         )
