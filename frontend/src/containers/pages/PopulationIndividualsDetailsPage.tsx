5ea29a5ab7f990ea3f99ad75a127795f99c3d055
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-10-06 12:12:06 +0200

    Duplicate/Sanction match icons missing the tooltip

--- frontend/src/containers/pages/PopulationIndividualsDetailsPage.tsx
+++ frontend/src/containers/pages/PopulationIndividualsDetailsPage.tsx
@@ -1,8 +1,11 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { BreadCrumbsItem } from '../../components/BreadCrumbs';
+import { Flag } from '../../components/Flag';
+import { FlagTooltip } from '../../components/FlagTooltip';
 import { LoadingComponent } from '../../components/LoadingComponent';
 import { PageHeader } from '../../components/PageHeader';
 import { PermissionDenied } from '../../components/PermissionDenied';
@@ -66,13 +69,35 @@ export function PopulationIndividualsDetailsPage(): React.ReactElement {
             ? breadCrumbsItems
             : null
         }
-        possibleMatch={individual.sanctionListPossibleMatch}
-        confirmedMatch={individual.sanctionListConfirmedMatch}
-        withTriangle={individual.deduplicationGoldenRecordStatus !== 'UNIQUE'}
       >
-        {individual.photo ? (
-          <IndividualPhotoModal individual={individual as IndividualNode} />
-        ) : null}
+        <>
+          <Box mr={2}>
+            {individual.deduplicationGoldenRecordStatus !== 'UNIQUE' && (
+              <FlagTooltip message={t('Possible Duplicate')} />
+            )}
+          </Box>
+          <Box mr={2}>
+            {individual.sanctionListPossibleMatch && (
+              <Flag
+                message={t('Sanction List Possible Match')}
+                confirmed={individual.sanctionListConfirmedMatch}
+              />
+            )}
+          </Box>
+          <Box mr={2}>
+            {individual.sanctionListConfirmedMatch && (
+              <Flag
+                message={t('Sanction List Confirmed Match')}
+                confirmed={individual.sanctionListConfirmedMatch}
+              />
+            )}
+          </Box>
+          <Box mr={2}>
+            {individual.photo ? (
+              <IndividualPhotoModal individual={individual as IndividualNode} />
+            ) : null}
+          </Box>
+        </>
       </PageHeader>
       <Container>
         <IndividualsBioData individual={individual as IndividualNode} />
