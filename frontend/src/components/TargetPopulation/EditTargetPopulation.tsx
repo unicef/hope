fe0bbd38343b303afae8f1ee107117b5b0d5ea41
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-10-07 15:19:48 +0200

    add excluded ids inputs and render the ids on target population page

--- frontend/src/components/TargetPopulation/EditTargetPopulation.tsx
+++ frontend/src/components/TargetPopulation/EditTargetPopulation.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core';
+import { Box, Button, Grid, Typography } from '@material-ui/core';
 import { Field, Form, Formik } from 'formik';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import * as Yup from 'yup';
 import { TARGET_POPULATION_QUERY } from '../../apollo/queries/TargetPopulation';
+import { PaperContainer } from '../../containers/pages/CreateTargetPopulation';
 import { useBusinessArea } from '../../hooks/useBusinessArea';
 import { useSnackbar } from '../../hooks/useSnackBar';
 import { FormikTextField } from '../../shared/Formik/FormikTextField';
@@ -192,6 +193,26 @@ export function EditTargetPopulation({
             loading={loadingPrograms}
             program={values.program}
           />
+          <PaperContainer>
+            <Typography variant='h6'>
+              {t(
+                'Excluded Target Population Entries (Households or Individuals)',
+              )}
+            </Typography>
+            <Box mt={2}>
+              <Grid container>
+                <Grid xs={6}>
+                  <Field
+                    name='excludedIds'
+                    fullWidth
+                    variant='outlined'
+                    label={t('Excluded Ids')}
+                    component={FormikTextField}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+          </PaperContainer>
           <CandidateListTab
             values={values}
             selectedProgram={getFullNodeFromEdgesById(
