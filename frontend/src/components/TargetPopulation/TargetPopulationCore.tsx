04495438126f50f75bf667e79926e0dc87b3d1bc
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-10-12 12:17:26 +0200

    exclusions changes according to new requirements

--- frontend/src/components/TargetPopulation/TargetPopulationCore.tsx
+++ frontend/src/components/TargetPopulation/TargetPopulationCore.tsx
@@ -31,30 +31,39 @@ export function TargetPopulationCore({
   const { rules: candidateListRules } = candidateList;
   return (
     <>
-      <PaperContainer>
-        <Typography variant='h6'>
-          {t('Excluded Target Population Entries (Households or Individuals)')}
-        </Typography>
-        <Box mt={2}>
-          <Grid container>
-            <Grid xs={6}>{targetPopulation?.excludedIds}</Grid>
-          </Grid>
-        </Box>
-        <Box mt={2}>
-          <Grid container>
-            <Grid xs={6}>{targetPopulation?.exclusionReason}</Grid>
-          </Grid>
-        </Box>
-      </PaperContainer>
       <TargetingCriteria
         candidateListRules={candidateListRules}
         targetPopulation={targetPopulation}
       />
+      {targetPopulation?.excludedIds ? (
+        <PaperContainer>
+          <Typography variant='h6'>
+            {t(
+              'Excluded Target Population Entries (Households or Individuals)',
+            )}
+          </Typography>
+          <Box mt={2}>
+            <Grid container>
+              <Grid item xs={6}>
+                {targetPopulation?.excludedIds}
+              </Grid>
+            </Grid>
+          </Box>
+          <Box mt={2}>
+            <Grid container>
+              <Grid item xs={6}>
+                {targetPopulation?.exclusionReason}
+              </Grid>
+            </Grid>
+          </Box>
+        </PaperContainer>
+      ) : null}
       <Results
         resultsData={targetPopulation.candidateStats}
         totalNumOfHouseholds={targetPopulation.candidateStats.allHouseholds}
         totalNumOfIndividuals={targetPopulation.candidateStats.allIndividuals}
       />
+
       {candidateListRules.length ? (
         <TargetingHouseholds
           id={id}
