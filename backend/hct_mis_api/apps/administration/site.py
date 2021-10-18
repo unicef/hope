9e081e886250a9805ea5e43dbb16b41184382fa7
sax <s.apostolico@gmail.com>
2021-10-18 15:21:14 +0200

    admin enahcement

--- backend/hct_mis_api/apps/administration/site.py
+++ backend/hct_mis_api/apps/administration/site.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.core.cache import caches
+from django.http import HttpResponse
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from constance import config
-from smart_admin.site import SmartAdminSite
+from smart_admin.site import SmartAdminSite, page
 
 cache = caches["default"]
 
@@ -13,11 +16,23 @@ def clean(v):
 
 def get_bookmarks(request):
     quick_links = []
-    for entry in config.QUICK_LINKS.split(";"):
-        if clean(entry):
+    for entry in config.QUICK_LINKS.split("\n"):
+        if entry := clean(entry):
             try:
-                label, url = entry.split(",")
-                quick_links.append([clean(label), clean(url)])
+                if entry == "--":
+                    quick_links.append(mark_safe("<li><hr/></li>"))
+                elif parts := entry.split(","):
+                    args = None
+                    if len(parts) == 1:
+                        args = parts[0], "viewlink", parts[0], parts[0]
+                    elif len(parts) == 2:
+                        args = parts[0], "viewlink", parts[1], parts[0]
+                    elif len(parts) == 3:
+                        args = parts[0], "viewlink", parts[1], parts[0]
+                    elif len(parts) == 4:
+                        args = parts.reverse()
+                    if args:
+                        quick_links.append(format_html('<li><a target="{}" class="{}" href="{}">{}</a></li>', *args))
             except ValueError:
                 pass
     return quick_links
