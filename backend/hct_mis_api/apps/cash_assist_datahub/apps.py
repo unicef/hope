3c8392096edb0b5622c971cf6b4ed9bc6a0a79d6
sax <s.apostolico@gmail.com>
2021-10-18 15:21:14 +0200

    admin enahcement

--- backend/hct_mis_api/apps/cash_assist_datahub/apps.py
+++ backend/hct_mis_api/apps/cash_assist_datahub/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class CashAssistConfig(AppConfig):
+class Config(AppConfig):
     name = "hct_mis_api.apps.cash_assist_datahub"
+    verbose_name = "HUB CA (CA->Hope)"
--- backend/hct_mis_api/apps/erp_datahub/apps.py
+++ backend/hct_mis_api/apps/erp_datahub/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
 
-class CashAssistConfig(AppConfig):
+class Config(AppConfig):
     name = "hct_mis_api.apps.erp_datahub"
+    verbose_name = "HUB ERP (Vision->Hope)"
