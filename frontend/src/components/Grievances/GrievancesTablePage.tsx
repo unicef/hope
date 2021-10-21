0c1ad37bb4063c8eef35b7176ba2ddfbaab71f98
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-10-21 10:23:39 +0200

    add correct ids

--- frontend/src/components/Grievances/GrievancesTablePage.tsx
+++ frontend/src/components/Grievances/GrievancesTablePage.tsx
@@ -26,7 +26,7 @@ export function GrievancesTablePage(): React.ReactElement {
   const businessArea = useBusinessArea();
   const permissions = usePermissions();
 
-  const { id, cashPlanId } = useParams();
+  const { id, verificationId } = useParams();
   const [filter, setFilter] = useState({
     search: '',
     status: '',
@@ -34,7 +34,7 @@ export function GrievancesTablePage(): React.ReactElement {
     createdAtRange: '',
     admin: null,
     registrationDataImport: id,
-    cashPlanPaymentVerification: cashPlanId,
+    cashPlanPaymentVerification: verificationId,
   });
   const debouncedFilter = useDebounce(filter, 500);
   const {
