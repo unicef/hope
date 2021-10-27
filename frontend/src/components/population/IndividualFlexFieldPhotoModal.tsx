b6259b3aa809b86ccb28e586c56855e09ae01d4d
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-10-27 20:46:14 +0200

    add flex field photo modal for individual vulnerabilities

---
+++ frontend/src/components/population/IndividualFlexFieldPhotoModal.tsx
@@ -0,0 +1,75 @@
+import { Box, Button, DialogContent, DialogTitle } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { Dialog } from '../../containers/dialogs/Dialog';
+import { DialogActions } from '../../containers/dialogs/DialogActions';
+import { useIndividualFlexFieldsQuery } from '../../__generated__/graphql';
+
+const DialogTitleWrapper = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+`;
+
+const DialogFooter = styled.div`
+  padding: 12px 16px;
+  margin: 0;
+  border-top: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+  text-align: right;
+`;
+
+const StyledImage = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const MiniImage = styled.img`
+  height: 45px;
+  width: 45px;
+  cursor: pointer;
+`;
+
+export const StyledLink = styled(Link)`
+  color: #000;
+`;
+
+export const IndividualFlexFieldPhotoModal = ({
+  field,
+}): React.ReactElement => {
+  const { id } = useParams();
+  const { data } = useIndividualFlexFieldsQuery({
+    variables: { id },
+  });
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  if (!data) {
+    return null;
+  }
+
+  const { flexFields } = data.individual;
+  const picUrl = flexFields[field.name];
+
+  return (
+    <>
+      <MiniImage alt='photo' src={picUrl} onClick={() => setDialogOpen(true)} />
+      <Dialog
+        open={dialogOpen}
+        onClose={() => setDialogOpen(false)}
+        aria-labelledby='form-dialog-title'
+      >
+        <DialogTitleWrapper>
+          <DialogTitle id='scroll-dialog-title'>Photo</DialogTitle>
+        </DialogTitleWrapper>
+        <DialogContent>
+          <Box p={3}>
+            <StyledImage alt='photo' src={picUrl} />
+          </Box>
+        </DialogContent>
+        <DialogFooter>
+          <DialogActions>
+            <Button onClick={() => setDialogOpen(false)}>CANCEL</Button>
+          </DialogActions>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+};
