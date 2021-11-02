ad35be4b6041675acbc81fa21087cb233620726f
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2021-11-02 21:26:04 +0100

    fix modals to prevent save image as

--- frontend/src/components/Grievances/GrievanceFlexFieldPhotoModalEditable.tsx
+++ frontend/src/components/Grievances/GrievanceFlexFieldPhotoModalEditable.tsx
@@ -7,8 +7,7 @@ import {
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import React, { useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
 import { Dialog } from '../../containers/dialogs/Dialog';
 import { DialogActions } from '../../containers/dialogs/DialogActions';
 import { FormikFileField } from '../../shared/Formik/FormikFileField';
@@ -16,32 +15,12 @@ import {
   AllAddIndividualFieldsQuery,
   useGrievanceTicketFlexFieldsQuery,
 } from '../../__generated__/graphql';
-
-const DialogTitleWrapper = styled.div`
-  border-bottom: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
-`;
-
-const DialogFooter = styled.div`
-  padding: 12px 16px;
-  margin: 0;
-  border-top: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
-  text-align: right;
-`;
-
-const StyledImage = styled.img`
-  max-width: 100%;
-  max-height: 100%;
-`;
-
-const MiniImage = styled.img`
-  height: 45px;
-  width: 45px;
-  cursor: pointer;
-`;
-
-export const StyledLink = styled(Link)`
-  color: #000;
-`;
+import {
+  DialogFooter,
+  DialogTitleWrapper,
+  MiniImage,
+  StyledImage,
+} from '../population/IndividualFlexFieldPhotoModal';
 
 export interface GrievanceFlexFieldPhotoModalEditableProps {
   flexField: AllAddIndividualFieldsQuery['allAddIndividualsFieldsAttributes'][number];
