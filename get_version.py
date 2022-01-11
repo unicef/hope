5486e9b1868a6c4125dba5129bf5b643035d9e75
sax <s.apostolico@gmail.com>
2022-01-11 16:04:54 +0100

    updates import user

--- get_version.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import json
-
-with open("frontend/package.json", "r") as f:
-    json_dict = json.load(f)
-frontend_version = json_dict["version"]
-print(frontend_version)
-
