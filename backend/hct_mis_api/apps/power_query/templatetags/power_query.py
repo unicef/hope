07c75650435be8698a5c82ba957fdec21cdc8011
sax <s.apostolico@gmail.com>
2022-01-14 12:51:03 +0100

    add sentry link to power_query details

--- backend/hct_mis_api/apps/power_query/templatetags/power_query.py
+++ backend/hct_mis_api/apps/power_query/templatetags/power_query.py
@@ -1,4 +1,17 @@
 from django import template
 
+from adminactions.utils import get_attr
+
+from hct_mis_api.apps.power_query.utils import get_sentry_url
+
 register = template.Library()
 
+
+@register.filter()
+def field(obj, field_name):
+    return get_attr(obj, field_name)
+
+
+@register.filter()
+def link_to_sentry(event_id, href=False):
+    return get_sentry_url(event_id, href)
