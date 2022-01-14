aa3caa1c3cf427ae85577a4440e1353d15a57167
sax <s.apostolico@gmail.com>
2022-01-14 15:28:01 +0100

    updates steficon

---
+++ backend/hct_mis_api/apps/targeting/tasks.py
@@ -0,0 +1,35 @@
+import logging
+
+from django.db.transaction import atomic
+from django.utils import timezone
+
+from hct_mis_api.apps.core.celery import app
+
+from ..targeting.models import HouseholdSelection, TargetPopulation
+from .models import RuleCommit
+
+logger = logging.getLogger(__name__)
+
+
+@app.task()
+def target_population_apply_steficon(target_population_id):
+    try:
+        target_population = TargetPopulation.objects.get(pk=target_population_id)
+        rule = target_population.steficon_rule
+        try:
+            target_population.status = TargetPopulation.STATUS_STEFICON_RUN
+            target_population.steficon_applied_date = timezone.now()
+            target_population.save()
+            with atomic():
+                entry: HouseholdSelection
+                for entry in target_population.selections.all():
+                    result = rule.execute({"context": entry})
+                    entry.vulnerability_score = result.value
+                    entry.save()
+            target_population.status = TargetPopulation.STATUS_STEFICON_COMPLETED
+        except Exception as e:
+            target_population.status = TargetPopulation.STATUS_STEFICON_ERROR
+        finally:
+            target_population.save()
+    except Exception as e:
+        logger.exception(e)
