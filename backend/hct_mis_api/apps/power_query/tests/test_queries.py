b39ca42589dbc07d8b8b636cdd6e824c09c61f54
sax <s.apostolico@gmail.com>
2022-01-16 19:59:57 +0100

    updates tests

--- backend/hct_mis_api/apps/power_query/tests/test_queries.py
+++ backend/hct_mis_api/apps/power_query/tests/test_queries.py
@@ -1,21 +1,16 @@
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
-from hct_mis_api.apps.household.fixtures import HouseholdFactory
-
 from ...account.fixtures import UserFactory
-from ...household.models import Household
+from ...account.models import User
 from ..models import Query
 
 
 class TestBasicRule(TestCase):
     @classmethod
     def setUpTestData(self):
-        self.household = HouseholdFactory()
         self.user = UserFactory()
 
     def test_execution(self):
-        r = Query(
-            name="All HH", owner=self.user, target=ContentType.objects.get_for_model(Household), code="qs=conn.all()"
-        )
+        r = Query(name="All HH", owner=self.user, target=ContentType.objects.get_for_model(User), code="qs=conn.all()")
         r.execute()
