01a0a832ba0720f05aa6306415d36f4a58b97a16
sax <s.apostolico@gmail.com>
2022-01-19 10:43:17 +0100

    better codemirror support

--- backend/hct_mis_api/apps/steficon/widget.py
+++ backend/hct_mis_api/apps/steficon/widget.py
@@ -1,8 +1,11 @@
-from django.forms import Textarea
+from django import forms
+from django.contrib.contenttypes.models import ContentType
 from django.templatetags.static import static
 
 
-class PythonEditor(Textarea):
+class PythonEditor(forms.Textarea):
+    template_name = "steficon/widgets/codewidget.html"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.attrs["class"] = "python-editor"
@@ -11,6 +14,8 @@ class PythonEditor(Textarea):
         css = {
             "all": (
                 static("admin/steficon/codemirror/codemirror.css"),
+                static("admin/steficon/codemirror/fullscreen.css"),
+                static("admin/steficon/codemirror/midnight.css"),
                 static("admin/steficon/codemirror/foldgutter.css"),
             )
         }
@@ -21,4 +26,37 @@ class PythonEditor(Textarea):
             static("admin/steficon/codemirror/active-line.js"),
             static("admin/steficon/codemirror/foldcode.js"),
             static("admin/steficon/codemirror/foldgutter.js"),
+            static("admin/steficon/codemirror/indent-fold.js"),
         )
+
+
+class ContentTypeChoiceField(forms.ModelChoiceField):
+    def __init__(
+        self,
+        *,
+        empty_label="---------",
+        required=True,
+        widget=None,
+        label=None,
+        initial=None,
+        help_text="",
+        to_field_name=None,
+        limit_choices_to=None,
+        **kwargs,
+    ):
+        queryset = ContentType.objects.order_by("model", "app_label")
+        super().__init__(
+            queryset,
+            empty_label=empty_label,
+            required=required,
+            widget=widget,
+            label=label,
+            initial=initial,
+            help_text=help_text,
+            to_field_name=to_field_name,
+            limit_choices_to=limit_choices_to,
+            **kwargs,
+        )
+
+    def label_from_instance(self, obj):
+        return f"{obj.name.title()} ({obj.app_label})"
