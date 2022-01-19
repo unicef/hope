68e31707a3b3c6dec88be9c61afbb56ab44de2b0
sax <s.apostolico@gmail.com>
2022-01-19 12:52:22 +0100

    rebase

--- backend/hct_mis_api/apps/power_query/forms.py
+++ backend/hct_mis_api/apps/power_query/forms.py
@@ -1,46 +1,12 @@
 from django import forms
-from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ValidationError
 
 from ..account.models import User
-from .models import Formatter, Query, Report
-from .widget import CodeWidget
+from ..steficon.widget import ContentTypeChoiceField, PythonEditor
+from .models import Formatter, Query
 
 
 class ExportForm(forms.Form):
     formatter = forms.ModelChoiceField(queryset=Formatter.objects)
-
-
-class ContentTypeChoiceField(forms.ModelChoiceField):
-    def __init__(
-        self,
-        *,
-        empty_label="---------",
-        required=True,
-        widget=None,
-        label=None,
-        initial=None,
-        help_text="",
-        to_field_name=None,
-        limit_choices_to=None,
-        **kwargs,
-    ):
-        queryset = ContentType.objects.order_by("model", "app_label")
-        super().__init__(
-            queryset,
-            empty_label=empty_label,
-            required=required,
-            widget=widget,
-            label=label,
-            initial=initial,
-            help_text=help_text,
-            to_field_name=to_field_name,
-            limit_choices_to=limit_choices_to,
-            **kwargs,
-        )
-
-    def label_from_instance(self, obj):
-        return f"{obj.name.title()} ({obj.app_label})"
 
 
 class FormatterTestForm(forms.Form):
@@ -50,7 +16,7 @@ class FormatterTestForm(forms.Form):
 class QueryForm(forms.ModelForm):
     name = forms.CharField(required=True, widget=forms.TextInput(attrs={"style": "width:80%"}))
     target = ContentTypeChoiceField()
-    code = forms.CharField(widget=CodeWidget)
+    code = forms.CharField(widget=PythonEditor)
     owner = forms.ModelChoiceField(queryset=User.objects, widget=forms.HiddenInput)
     description = forms.CharField(required=False, widget=forms.Textarea(attrs={"rows": 2, "style": "width:80%"}))
 
