f0c3961592386c53307c82245d5ffb77981eb2d2
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2022-01-28 10:50:02 +0100

    Change doap mail

--- backend/hct_mis_api/apps/core/tests/test_doap.py
+++ backend/hct_mis_api/apps/core/tests/test_doap.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 
 from django_webtest import WebTest
 
-from hct_mis_api.apps.account.fixtures import RoleFactory, UserFactory, UserRoleFactory
+from hct_mis_api.apps.account.fixtures import UserFactory, UserRoleFactory
 from hct_mis_api.apps.core.models import BusinessArea
 
 
@@ -24,10 +24,19 @@ class TestDOAP(WebTest):
         adm = admin.site._registry[BusinessArea]
         matrix = adm._get_doap_matrix(self.business_area)
         self.assertEqual(len(matrix), 2)
-        self.assertEqual(matrix[0][0:5], ["org", "Last Name", "First Name", "Email", "Action"])
+        expected_headers = ["org", "Last Name", "First Name", "Email", "Business Unit", "Partner Instance ID", "Action"]
+        self.assertEqual(matrix[0][0:7], expected_headers)
         self.assertEqual(
-            list(matrix[1].values())[0:5],
-            ["UNICEF", self.officer.last_name, self.officer.first_name, self.officer.email, "ADD"],
+            list(matrix[1].values())[0:7],
+            [
+                "UNICEF",
+                self.officer.last_name,
+                self.officer.first_name,
+                self.officer.email,
+                f"UNICEF - {self.business_area.name}",
+                int(self.business_area.code),
+                "ADD",
+            ],
         )
 
     def test_show_doap(self):
@@ -37,16 +46,26 @@ class TestDOAP(WebTest):
 
     def test_send_doap(self):
         url = reverse("admin:core_businessarea_send_doap", args=[self.business_area.pk])
-        res = self.app.get(url, user=self.user)
+        self.app.get(url, user=self.user)
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, "DOAP updates for Afghanistan")
+        self.assertEqual(mail.outbox[0].subject, "CashAssist - UNICEF - Afghanistan user updates")
 
     def test_export_doap(self):
         url = reverse("admin:core_businessarea_export_doap", args=[self.business_area.pk])
         res = self.app.get(url, user=self.user)
         self.assertEqual(res.content_type, "text/csv")
         csv_content = list(csv.reader(StringIO(res.content.decode())))
-        self.assertEqual(csv_content[0][0:5], ["org", "Last Name", "First Name", "Email", "Action"])
+        expected_headers = ["org", "Last Name", "First Name", "Email", "Business Unit", "Partner Instance ID", "Action"]
+        self.assertEqual(csv_content[0][0:7], expected_headers)
         self.assertEqual(
-            csv_content[1][0:5], ["UNICEF", self.officer.last_name, self.officer.first_name, self.officer.email, "ADD"]
+            csv_content[1][0:7],
+            [
+                "UNICEF",
+                self.officer.last_name,
+                self.officer.first_name,
+                self.officer.email,
+                f"UNICEF - {self.business_area.name}",
+                str(int(self.business_area.code)),
+                "ADD",
+            ],
         )
