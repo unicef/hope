b6000191224c3af2e87fc65328c795385cefeeb0
Jan Romaniak <jan.romaniak@tivix.com>
2022-02-02 16:06:28 +0100

    refactor code

--- backend/hct_mis_api/apps/mis_datahub/celery_tasks.py
+++ backend/hct_mis_api/apps/mis_datahub/celery_tasks.py
@@ -6,15 +6,16 @@ logger = logging.getLogger(__name__)
 
 
 @app.task
-def send_target_population_task():
+def send_target_population_task(target_population_id):
     logger.info("send_target_population_task start")
 
     try:
         from hct_mis_api.apps.mis_datahub.tasks.send_tp_to_datahub import (
             SendTPToDatahubTask,
         )
-
-        SendTPToDatahubTask().execute()
+        from hct_mis_api.apps.targeting.models import TargetPopulation
+        target_population = TargetPopulation.objects.select_related("program").get(id=target_population_id)
+        SendTPToDatahubTask().execute(target_population)
     except Exception as e:
         logger.exception(e)
         raise
