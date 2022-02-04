e6d02aa98bf1b49a243d5d40dcb3d7cc57a928c1
Jan Romaniak <jan.romaniak@tivix.com>
2022-02-04 15:47:49 +0100

    fix

--- frontend/src/apollo/queries/Household.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from 'apollo-boost';
-
-export const HOUSEHOLD_QUERY = gql`
-  query Household($id: ID!) {
-    household(id: $id) {
-      ...householdDetailed
-    }
-  }
-`;
--- frontend/src/apollo/registration/ImportedIndividual.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from 'apollo-boost';
-
-export const IMPORTED_INDIVIDUAL_QUERY = gql`
-  query ImportedIndividual($id: ID!) {
-    importedIndividual(id: $id) {
-      ...importedIndividualDetailed
-    }
-  }
-`;
