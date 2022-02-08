3e2b52e51372bb222fecfd1f73097f15d2f49d19
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2022-02-08 14:28:31 +0100

    Add tests, move functionality to the service

---
+++ backend/hct_mis_api/apps/payment/tests/snapshots/snap_test_create_payment_verification_mutation.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['TestCreatePaymentVerificationMutation::test_create_cash_plan_payment_verification_0_with_permission 1'] = {
+    'data': {
+        'createCashPlanPaymentVerification': {
+            'cashPlan': {
+                'id': 'Q2FzaFBsYW5Ob2RlOjBlMjkyN2FmLWM4NGQtNDg1Mi1iYjBiLTc3M2VmZTA1OWUwNQ=='
+            }
+        }
+    }
+}
+
+snapshots['TestCreatePaymentVerificationMutation::test_create_cash_plan_payment_verification_1_without_permission 1'] = {
+    'data': {
+        'createCashPlanPaymentVerification': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 13,
+                    'line': 3
+                }
+            ],
+            'message': 'Permission Denied: User does not have correct permission.',
+            'path': [
+                'createCashPlanPaymentVerification'
+            ]
+        }
+    ]
+}
+
+snapshots['TestCreatePaymentVerificationMutation::test_create_cash_plan_payment_verification_when_invalid_arguments 1'] = {
+    'data': {
+        'createCashPlanPaymentVerification': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 13,
+                    'line': 3
+                }
+            ],
+            'message': 'You have to provide full_list_arguments in FULL_LIST',
+            'path': [
+                'createCashPlanPaymentVerification'
+            ]
+        }
+    ]
+}
+
+snapshots['TestCreatePaymentVerificationMutation::test_create_cash_plan_payment_verification_when_invalid_arguments 2'] = {
+    'data': {
+        'createCashPlanPaymentVerification': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 13,
+                    'line': 3
+                }
+            ],
+            'message': 'You have to provide random_sampling_arguments in RANDOM',
+            'path': [
+                'createCashPlanPaymentVerification'
+            ]
+        }
+    ]
+}
+
+snapshots['TestCreatePaymentVerificationMutation::test_create_cash_plan_payment_verification_when_invalid_arguments 3'] = {
+    'data': {
+        'createCashPlanPaymentVerification': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 13,
+                    'line': 3
+                }
+            ],
+            'message': "You can't provide full_list_arguments in RANDOM",
+            'path': [
+                'createCashPlanPaymentVerification'
+            ]
+        }
+    ]
+}
