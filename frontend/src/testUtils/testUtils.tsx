ab5b98329a4cb90fbd996e0df41d9b34107dc1f3
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-02-16 13:31:57 +0100

    mock router

--- frontend/src/testUtils/testUtils.tsx
+++ frontend/src/testUtils/testUtils.tsx
@@ -1,11 +1,16 @@
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render, RenderOptions } from '@testing-library/react';
 import { TestProviders } from './testProviders';
 
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'queries'>,
-) => render(ui, { wrapper: TestProviders, ...options });
+) =>
+  render(<BrowserRouter>{ui}</BrowserRouter>, {
+    wrapper: TestProviders,
+    ...options,
+  });
 
 export * from '@testing-library/react';
 export { customRender as render };
