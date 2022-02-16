9ab9c92bc4d53e6d1594e01c2f69ae9018a0566c
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2022-02-16 13:19:02 +0100

    Move create payment verification class to service directory

--- backend/hct_mis_api/apps/payment/services/payment_verification_create.py
+++ backend/hct_mis_api/apps/payment/services/payment_verification_create.py
@@ -8,52 +8,10 @@ from hct_mis_api.apps.payment.models import (
     PaymentRecord,
     PaymentVerification,
 )
+from hct_mis_api.apps.payment.services.verifiers import (
+    PaymentVerificationArgumentVerifier,
+)
 from hct_mis_api.apps.payment.utils import get_number_of_samples
-
-
-class PaymentVerificationVerifyArgument:
-    ARGUMENTS = {
-        "sampling": {
-            CashPlanPaymentVerification.SAMPLING_FULL_LIST: {
-                "required": ["full_list_arguments"],
-                "not_allowed": ["random_sampling_arguments"],
-            },
-            CashPlanPaymentVerification.SAMPLING_RANDOM: {
-                "required": ["random_sampling_arguments"],
-                "not_allowed": ["full_list_arguments"],
-            },
-        },
-        "verification_channel": {
-            CashPlanPaymentVerification.VERIFICATION_METHOD_RAPIDPRO: {
-                "required": ["rapid_pro_arguments"],
-                "not_allowed": ["xlsx_arguments", "manual_arguments"],
-            },
-            CashPlanPaymentVerification.VERIFICATION_METHOD_XLSX: {
-                "required": [],
-                "not_allowed": ["rapid_pro_arguments", "manual_arguments"],
-            },
-            CashPlanPaymentVerification.VERIFICATION_METHOD_MANUAL: {
-                "required": [],
-                "not_allowed": ["rapid_pro_arguments", "xlsx_arguments"],
-            },
-        },
-    }
-
-    def __init__(self, input):
-        self.input = input
-
-    def verify(self, field_name):
-        options = self.ARGUMENTS.get(field_name)
-
-        for key, value in options.items():
-            if key != self.input.get(field_name):
-                continue
-            for required in value.get("required"):
-                if self.input.get(required) is None:
-                    raise ValueError(f"You have to provide {required} in {key}")
-            for not_allowed in value.get("not_allowed"):
-                if self.input.get(not_allowed) is not None:
-                    raise ValueError(f"You can't provide {not_allowed} in {key}")
 
 
 class PaymentVerificationCreate:
@@ -62,7 +20,7 @@ class PaymentVerificationCreate:
         self.cash_plan = cash_plan
 
     def execute(self) -> CashPlanPaymentVerification:
-        verifier = PaymentVerificationVerifyArgument(self.input)
+        verifier = PaymentVerificationArgumentVerifier(self.input)
         verifier.verify("sampling")
         verifier.verify("verification_channel")
 
