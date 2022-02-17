8fdcb5c30ff96231f202868896d54ac79eb37339
Mikolaj Kosmal <mikolaj.kosmal@gmail.com>
2022-02-17 10:39:41 -0500

    Ops/Update: Kobotoolbox 2.020.25 -> 2.021.47

--- deployment/kobo/files/kobocat/wait_for_mongo.bash
+++ deployment/kobo/files/kobocat/wait_for_mongo.bash
@@ -2,5 +2,5 @@
 set -e
 
 echo 'Waiting for container `mongo`.'
-wait-for-it -t 40 -h ${KPI_MONGO_HOST} -p ${KPI_MONGO_PORT}
+wait-for-it -t 40 -h ${KOBOCAT_MONGO_HOST} -p ${KOBOCAT_MONGO_PORT}
 echo 'Container `mongo` up.'}
