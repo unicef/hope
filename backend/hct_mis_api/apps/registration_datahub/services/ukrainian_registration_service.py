bed05d8d45837c14dc1063f47d32a24b196b060e
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2022-04-06 16:23:36 +0200

    Add household and individual validation

--- backend/hct_mis_api/apps/registration_datahub/services/ukrainian_registration_service.py
+++ backend/hct_mis_api/apps/registration_datahub/services/ukrainian_registration_service.py
@@ -4,26 +4,27 @@ from typing import List
 from django.core.exceptions import ValidationError
 from django.db.models import QuerySet
 from django.db.transaction import atomic
+from django.forms import modelform_factory
 
 from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.core.utils import build_arg_dict_from_dict
 from hct_mis_api.apps.household.models import (
-    IDENTIFICATION_TYPE_TAX_ID,
-    IDENTIFICATION_TYPE_NATIONAL_ID,
     IDENTIFICATION_TYPE_BIRTH_CERTIFICATE,
-    IDENTIFICATION_TYPE_NATIONAL_PASSPORT,
     IDENTIFICATION_TYPE_DRIVERS_LICENSE,
+    IDENTIFICATION_TYPE_NATIONAL_ID,
+    IDENTIFICATION_TYPE_NATIONAL_PASSPORT,
     IDENTIFICATION_TYPE_RESIDENCE_PERMIT_NO,
+    IDENTIFICATION_TYPE_TAX_ID,
 )
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
 from hct_mis_api.apps.registration_datahub.models import (
+    ImportData,
+    ImportedDocument,
+    ImportedDocumentType,
     ImportedHousehold,
     ImportedIndividual,
-    ImportedDocument,
     Record,
-    ImportedDocumentType,
     RegistrationDataImportDatahub,
-    ImportData,
 )
 
 
@@ -36,11 +37,13 @@ class UkrainianRegistrationService:
         "gender_i_c": "gender",
         "relationship_i_c": "relationship",
         "disability_i_c": "disability",
-        "disabiliyt_recognize_i_c": "disability_recognize",
-        "disability_certificate_picture": "disability_certificate_picture",
+        # "disabiliyt_recognize_i_c": "disability_recognize",
+        # "disability_certificate_picture": "disability_certificate_picture",
         "phone_no_i_c": "phone_no",
-        "tax_id_no_i_c": "tax_id_no",
         "role_i_c": "role",
+    }
+
+    BANK_MAPPING_DICT = {
         "bank_account_h_f": "bank_account",
         "bank_name_h_f": "bank_name",
         "other_bank_name": "other_bank_name",
@@ -56,6 +59,7 @@ class UkrainianRegistrationService:
         "admin2_h_c": "admin2",
         "admin3_h_c": "admin3",
         "size_h_c": "size",
+        # "where_are_you_now": "",
     }
     DOCUMENT_MAPPING_TYPE_DICT = {
         "national_id_no_i_c_1": IDENTIFICATION_TYPE_NATIONAL_ID,
@@ -114,8 +118,8 @@ class UkrainianRegistrationService:
         rdi.number_of_individuals = number_of_individuals
         import_data.number_of_individuals = number_of_individuals
 
-        rdi.save()
-        import_data.save()
+        rdi.save(update_fields=("number_of_individuals",))
+        import_data.save(update_fields=("number_of_individuals",))
 
     def create_household_for_rdi_household(
         self, record: Record, registration_data_import: RegistrationDataImportDatahub
@@ -123,38 +127,60 @@ class UkrainianRegistrationService:
         individuals: List[ImportedIndividual] = []
         documents: List[ImportedDocument] = []
         record_data_dict = json.loads(record.storage.tobytes().decode("utf-8"))
-        household_dict = record_data_dict.get("household")
+        household_dict = record_data_dict.get("household", [])[0]
         individuals_array = record_data_dict.get("individuals", [])
+
         self.validate_household(individuals_array)
-        household = ImportedHousehold.objects.create(
+
+        household_data = self._prepare_household_data(household_dict, record, registration_data_import)
+        household = self._create_household(household_data)
+
+        for individual_dict in individuals_array:
+            individual_data = self._prepare_individual_data(individual_dict, household, registration_data_import)
+            individual = self._create_individual(individual_data)
+            individuals.append(individual)
+
+            documents.extend(self._prepare_documents(individual_dict, individual))
+
+        record.registration_data_import = registration_data_import
+        record.save(update_fields=("registration_data_import",))
+        ImportedDocument.objects.bulk_create(documents)
+        return len(individuals)
+
+    def _create_individual(self, individual_data) -> ImportedIndividual:
+        IndividualForm = modelform_factory(ImportedIndividual, fields=individual_data.keys())
+        form = IndividualForm(individual_data)
+        if not form.is_valid():
+            raise ValidationError(form.errors)
+        return form.save()
+
+    def _create_household(self, household_data) -> ImportedHousehold:
+        HouseholdForm = modelform_factory(ImportedHousehold, fields=household_data.keys())
+        form = HouseholdForm(household_data)
+        if not form.is_valid():
+            raise ValidationError(form.errors)
+        return form.save()
+
+    def _prepare_household_data(self, household_dict, record, registration_data_import) -> dict:
+        return dict(
             **build_arg_dict_from_dict(household_dict, UkrainianRegistrationService.HOUSEHOLD_MAPPING_DICT),
             flex_registrations_record=record,
             registration_data_import=registration_data_import
         )
-        for individual_dict in individuals_array:
-            imported_individual = self.prepare_individual(individual_dict, household, registration_data_import)
-            individuals.append(imported_individual)
-            documents.extend(self.prepare_documents(individual_dict, imported_individual))
 
-        record.registration_data_import = registration_data_import
-        record.save()
-        return len(individuals)
-
-    def prepare_individual(
+    def _prepare_individual_data(
         self,
         individual_dict: dict,
         household: ImportedHousehold,
         registration_data_import: RegistrationDataImportDatahub,
-    ) -> ImportedIndividual:
-        individual = ImportedIndividual.objects.create(
+    ) -> dict:
+        return dict(
             **build_arg_dict_from_dict(individual_dict, UkrainianRegistrationService.HOUSEHOLD_MAPPING_DICT),
             household=household,
             registration_data_import=registration_data_import
         )
-        self.prepare_documents(individual_dict, individual)
-        return individual
 
-    def prepare_documents(self, individual_dict: dict, individual: ImportedIndividual) -> List[ImportedDocument]:
+    def _prepare_documents(self, individual_dict: dict, individual: ImportedIndividual) -> List[ImportedDocument]:
         documents = []
         for field_name, document_type_string in UkrainianRegistrationService.DOCUMENT_MAPPING_TYPE_DICT:
             document_number = individual_dict.get(field_name)
@@ -162,85 +188,10 @@ class UkrainianRegistrationService:
                 continue
             document_type = ImportedDocumentType.objects.get(type=document_type_string, country="UA")
             documents.append(
-                ImportedDocument.objects.create(
-                    document_type=document_type, document_number=document_number, individual=individual
-                )
+                ImportedDocument(document_type=document_type, document_number=document_number, individual=individual)
             )
         return documents
 
     def validate_household(self, individuals_array):
         if not individuals_array:
             raise ValidationError("Household should has at least one individual")
-
-    def validate_individual(self):
-        raise
-
-
-# {
-# "household": {
-#     "residence_status_h_c":"non_host",
-#     "where_are_you_now":"",
-#     "admin1_h_c":"UA01",
-#     "admin2_h_c":"UA0104",
-#     "admin3_h_c":"UA0104001",
-#     "size_h_c":2,
-# }
-# "individuals":[
-#     {
-#         "given_name_i_c":"Jan",
-#         "family_name_i_c":"Romaniak",
-#         "middle_name_i_c":"Roman",
-#         "birth_date":"1991-04-13",
-#         "gender_i_c":"male",
-#         "relationship_i_c":"head",
-#         "disability_i_c":"y",
-#         "disabiliyt_recognize_i_c":"y",
-#         "disability_certificate_picture":null,
-#         "phone_no_i_c":"",
-#         "q1":"",
-#         "tax_id_no_i_c":"",
-#         "national_id_no_i_c_1":"",
-#         "international_passport_i_c":"ASH123123",
-#         "drivers_license_no_i_c":"",
-#         "birth_certificate_no_i_c":"",
-#         "residence_permit_no_i_c":"",
-#         "birth_certificate_picture":null,
-#         "role_i_c":"y",
-#         "bank_account_h_f":"y",
-#         "bank_name_h_f":"privatbank",
-#         "other_bank_name":"",
-#         "bank_account":"8327832823982398239",
-#         "bank_account_number":"8327832823982398239",
-#         "debit_card_number_h_f":"123123123",
-#         "debit_card_number":"123123123"
-#     },
-#     {
-#         "given_name_i_c":"Pavlo",
-#         "family_name_i_c":"Mokkichuk",
-#         "middle_name_i_c":"Viktor",
-#         "birth_date":"1991-11-15",
-#         "gender_i_c":"male",
-#         "relationship_i_c":"head",
-#         "disability_i_c":"y",
-#         "disabiliyt_recognize_i_c":"y",
-#         "disability_certificate_picture":null,
-#         "phone_no_i_c":"",
-#         "q1":"",
-#         "tax_id_no_i_c":"",
-#         "national_id_no_i_c_1":"",
-#         "international_passport_i_c":"ASH999999",
-#         "drivers_license_no_i_c":"",
-#         "birth_certificate_no_i_c":"",
-#         "residence_permit_no_i_c":"",
-#         "birth_certificate_picture":null,
-#         "role_i_c":"n",
-#         "bank_account_h_f":"y",
-#         "bank_name_h_f":"privatbank",
-#         "other_bank_name":"",
-#         "bank_account":"832783232398239",
-#         "bank_account_number":"832783232398239",
-#         "debit_card_number_h_f":null,
-#         "debit_card_number":""
-#     }
-# ]
-# }
