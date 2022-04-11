b030480e3f7c82a11c75b4ef8ebd5cef800e41f5
Pavlo Mokiichuk <pmk@android-97809c3c1431674f.local>
2022-04-11 09:41:54 +0200

    small fixes & refactoring

--- backend/hct_mis_api/apps/grievance/mutations_extras/payment_verification.py
+++ backend/hct_mis_api/apps/grievance/mutations_extras/payment_verification.py
@@ -2,6 +2,8 @@ import graphene
 
 from django.utils import timezone
 
+from hct_mis_api.apps.payment.models import PaymentVerification
+
 
 class PaymentVerificationTicketExtras(graphene.InputObjectType):
     pass
@@ -12,7 +14,17 @@ def save_payment_verification_extras(root, info, input, grievance_ticket, extras
     payment_verification = payment_verification_details.payment_verification
     if not (payment_verification_details.approve_status and payment_verification and not payment_verification_details.has_multiple_payment_verifications):
         return
-    payment_verification.status = payment_verification_details.new_status
+    # update PaymentVerification status
+    if payment_verification.payment_record and not payment_verification_details.new_status == PaymentVerification.STATUS_NOT_RECEIVED:
+        if payment_verification_details.new_received_amount >= payment_verification.payment_record.delivered_quantity:
+            status = PaymentVerification.STATUS_RECEIVED
+        else:
+            status = PaymentVerification.STATUS_RECEIVED_WITH_ISSUES
+    else:
+        # set 'NOT_RECEIVED' status
+        status = payment_verification_details.new_status
+
+    payment_verification.status = status
     payment_verification.status_date = timezone.now()
     payment_verification.received_amount = payment_verification_details.new_received_amount
 
