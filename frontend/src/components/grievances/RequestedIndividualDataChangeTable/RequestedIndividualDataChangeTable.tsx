bbc013318dfa21931c2fa5e3d359a0af4041d5d6
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-04-12 13:22:51 +0200

    fix G&F: Not possible to update a field (flex)

--- frontend/src/components/grievances/RequestedIndividualDataChangeTable/RequestedIndividualDataChangeTable.tsx
+++ frontend/src/components/grievances/RequestedIndividualDataChangeTable/RequestedIndividualDataChangeTable.tsx
@@ -79,7 +79,7 @@ export function RequestedIndividualDataChangeTable({
 
   return (
     <div>
-      {entries?.length ? (
+      {entries?.length || entriesFlexFields?.length ? (
         <EntriesTable
           values={values}
           isEdit={isEdit}
