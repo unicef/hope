82c15a0e63a57aab0753dd0e6f3973026e107cba
Jan Romaniak <jan.romaniak@tivix.com>
2022-04-15 14:34:19 +0200

    fixes

--- backend/hct_mis_api/apps/targeting/utils.py
+++ backend/hct_mis_api/apps/targeting/utils.py
@@ -5,11 +5,12 @@ from django.db.models import Count, Q
 
 
 def has_children_filter_in_criteria(targeting_criteria_dict):
-    return any(has(rule) for rule in targeting_criteria_dict.get("rules", []))
+    return any(has_number_of_children(rule) for rule in targeting_criteria_dict.get("rules", []))
 
 
-def has(rule):
+def has_number_of_children(rule):
     return any(filter_dict["field_name"] == "number_of_children" for filter_dict in rule.get("filters", []))
+
 
 def get_annotate_for_children_count(household_queryset):
     date_18_years_ago = (dt.datetime.now() - relativedelta(years=+18)).date()
