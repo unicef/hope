70cb1cd0439d663088e826f4d44857b95ee5b40d
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-05-06 13:57:54 +0200

    create fsp array

--- frontend/src/components/paymentmodule/CreateFspPlan/Fsp/Fsp.test.tsx
+++ frontend/src/components/paymentmodule/CreateFspPlan/Fsp/Fsp.test.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { PERMISSIONS } from '../../../../config/permissions';
 import { render } from '../../../../testUtils/testUtils';
 import { Fsp } from './Fsp';
 
 describe('components/paymentmodule/Fsp', () => {
   it('should render', () => {
-    const { container } = render(
-      <Fsp permissions={[PERMISSIONS.PAYMENT_MODULE_VIEW_LIST]} />,
-    );
+    const { container } = render(<Fsp index={0} baseName='wallet' />);
     expect(container).toMatchSnapshot();
   });
 });
