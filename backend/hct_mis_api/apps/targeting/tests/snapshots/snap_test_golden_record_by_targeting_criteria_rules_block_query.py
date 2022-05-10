5beebac32234361e5dafb3bd76a6327661f04a5a
Konrad Marzec <konradmarzec@Konrads-MacBook-Pro.local>
2022-05-10 21:09:45 +0200

    Add snapshot tests

--- backend/hct_mis_api/apps/targeting/tests/snapshots/snap_test_golden_record_by_targeting_criteria_rules_block_query.py
+++ backend/hct_mis_api/apps/targeting/tests/snapshots/snap_test_golden_record_by_targeting_criteria_rules_block_query.py
@@ -4,12 +4,34 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
+
 snapshots = Snapshot()
 
-snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_size 1'] = {
+snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_bank_account_info 1'] = {
     'data': {
         'goldenRecordByTargetingCriteria': {
             'edges': [
+                {
+                    'node': {
+                        'individuals': {
+                            'edges': [
+                                {
+                                    'node': {
+                                        'maritalStatus': 'SINGLE',
+                                        'sex': 'MALE'
+                                    }
+                                },
+                                {
+                                    'node': {
+                                        'maritalStatus': 'MARRIED',
+                                        'sex': 'FEMALE'
+                                    }
+                                }
+                            ]
+                        },
+                        'size': 2
+                    }
+                },
                 {
                     'node': {
                         'individuals': {
@@ -26,7 +48,7 @@ snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_gold
                     }
                 }
             ],
-            'totalCount': 1
+            'totalCount': 2
         }
     }
 }
@@ -59,7 +81,89 @@ snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_gold
 snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_observed_disability_with_invalid_argument 1'] = {
     'data': {
         'goldenRecordByTargetingCriteria': {
-            'edges': [],
+            'edges': [
+            ],
+            'totalCount': 0
+        }
+    }
+}
+
+snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_phone_number 1'] = {
+    'data': {
+        'goldenRecordByTargetingCriteria': {
+            'edges': [
+                {
+                    'node': {
+                        'individuals': {
+                            'edges': [
+                                {
+                                    'node': {
+                                        'maritalStatus': 'SINGLE',
+                                        'sex': 'MALE'
+                                    }
+                                },
+                                {
+                                    'node': {
+                                        'maritalStatus': 'MARRIED',
+                                        'sex': 'FEMALE'
+                                    }
+                                }
+                            ]
+                        },
+                        'size': 2
+                    }
+                },
+                {
+                    'node': {
+                        'individuals': {
+                            'edges': [
+                                {
+                                    'node': {
+                                        'maritalStatus': 'MARRIED',
+                                        'sex': 'MALE'
+                                    }
+                                }
+                            ]
+                        },
+                        'size': 1
+                    }
+                }
+            ],
+            'totalCount': 2
+        }
+    }
+}
+
+snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_size 1'] = {
+    'data': {
+        'goldenRecordByTargetingCriteria': {
+            'edges': [
+                {
+                    'node': {
+                        'individuals': {
+                            'edges': [
+                                {
+                                    'node': {
+                                        'maritalStatus': 'MARRIED',
+                                        'sex': 'MALE'
+                                    }
+                                }
+                            ]
+                        },
+                        'size': 1
+                    }
+                }
+            ],
+            'totalCount': 1
+        }
+    }
+}
+
+snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_tax_id 1'] = {
+    'data': {
+        'goldenRecordByTargetingCriteria': {
+            'edges': [
+            ],
             'totalCount': 0
         }
     }
