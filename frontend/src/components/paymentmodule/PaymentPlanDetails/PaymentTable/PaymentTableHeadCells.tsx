c1130718e591cf1eb04805aa96272b081e32176b
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-05-10 14:36:40 +0200

    payment table

---
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentTable/PaymentTableHeadCells.tsx
@@ -0,0 +1,49 @@
+import { AllGrievanceTicketQuery } from '../../../../__generated__/graphql';
+import { HeadCell } from '../../../core/Table/EnhancedTableHead';
+
+export const headCells: HeadCell<
+  AllGrievanceTicketQuery['allGrievanceTicket']['edges'][number]['node']
+>[] = [
+  {
+    disablePadding: false,
+    label: 'Payment Id',
+    id: 'id',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Household Id',
+    id: 'household__id',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Household Size',
+    id: 'household__size',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Administrative Level 2',
+    id: 'household__admin2',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Collector',
+    id: 'household__collector_id',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Payment Channel',
+    id: 'payment_channel',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Entitlement (USD)',
+    id: 'entitlement',
+    numeric: false,
+  },
+];
