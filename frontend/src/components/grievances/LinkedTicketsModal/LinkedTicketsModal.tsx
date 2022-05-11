0a1e820021b0d57d90c5231a7492ad2e97b323db
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-05-11 12:34:42 +0200

    add existing tickets too

--- frontend/src/components/grievances/LinkedTicketsModal/LinkedTicketsModal.tsx
+++ frontend/src/components/grievances/LinkedTicketsModal/LinkedTicketsModal.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   DialogActions,
   DialogContent,
@@ -8,6 +9,7 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Typography,
 } from '@material-ui/core';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -15,18 +17,21 @@ import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { Dialog } from '../../../containers/dialogs/Dialog';
 import {
+  decodeIdString,
   grievanceTicketStatusToColor,
-  renderUserName,
 } from '../../../utils/utils';
-import { AllGrievanceTicketQuery } from '../../../__generated__/graphql';
+import {
+  AllGrievanceTicketQuery,
+  useExistingGrievanceTicketsQuery,
+} from '../../../__generated__/graphql';
 import { BlackLink } from '../../core/BlackLink';
 import {
   DialogFooter,
   DialogTitleWrapper,
 } from '../../core/ConfirmationDialog/ConfirmationDialog';
+import { LoadingComponent } from '../../core/LoadingComponent';
 import { StatusBox } from '../../core/StatusBox';
 import { ClickableTableRow } from '../../core/Table/ClickableTableRow';
-import { UniversalMoment } from '../../core/UniversalMoment';
 
 export const StyledLink = styled.div`
   color: #000;
@@ -42,47 +47,118 @@ const StatusContainer = styled.div`
   min-width: 120px;
   max-width: 200px;
 `;
+const Bold = styled.span`
+  font-weight: bold;
+`;
 
 interface LinkedTicketsModalProps {
-  linkedTickets: AllGrievanceTicketQuery['allGrievanceTicket']['edges'][number]['node']['relatedTickets'];
+  ticket: AllGrievanceTicketQuery['allGrievanceTicket']['edges'][number]['node'];
   categoryChoices: { [id: number]: string };
   statusChoices: { [id: number]: string };
   canViewDetails: boolean;
   businessArea: string;
+  issueTypeChoicesData;
 }
 
 export const LinkedTicketsModal = ({
-  linkedTickets,
+  ticket,
   categoryChoices,
   statusChoices,
   canViewDetails,
   businessArea,
+  issueTypeChoicesData,
 }: LinkedTicketsModalProps): React.ReactElement => {
   const [dialogOpen, setDialogOpen] = useState(false);
+  const { data, loading } = useExistingGrievanceTicketsQuery({
+    variables: {
+      businessArea,
+      household:
+        decodeIdString(ticket.household?.id) ||
+        '294cfa7e-b16f-4331-8014-a22ffb2b8b3c',
+      //adding some random ID to get 0 results if there is no household id.
+    },
+  });
   const history = useHistory();
   const { t } = useTranslation();
+  const linkedTickets = ticket.relatedTickets;
+
+  if (loading) return <LoadingComponent />;
+  if (!data) return null;
+
+  const existingTickets = data.existingGrievanceTickets;
+
+  const renderRow = (row): React.ReactElement => {
+    const issueType = row.issueType
+      ? issueTypeChoicesData
+          .filter((el) => el.category === row.category.toString())[0]
+          .subCategories.filter(
+            (el) => el.value === row.issueType.toString(),
+          )[0].name
+      : '-';
+
+    return (
+      <ClickableTableRow
+        hover
+        onClick={
+          canViewDetails
+            ? () =>
+                history.push(
+                  `/${businessArea}/grievance-and-feedback/${row.id}`,
+                )
+            : undefined
+        }
+        key={row.id}
+      >
+        <TableCell align='left'>
+          {canViewDetails ? (
+            <BlackLink to={`/${businessArea}/grievance-and-feedback/${row.id}`}>
+              {row.unicefId}
+            </BlackLink>
+          ) : (
+            row.unicefId
+          )}
+        </TableCell>
+        <TableCell align='left'>{categoryChoices[row.category]}</TableCell>
+        <TableCell align='left'>{issueType || '-'}</TableCell>
+        <TableCell align='left'>
+          <StatusContainer>
+            <StatusBox
+              status={statusChoices[row.status]}
+              statusToColor={grievanceTicketStatusToColor}
+            />
+          </StatusContainer>
+        </TableCell>
+      </ClickableTableRow>
+    );
+  };
+
+  const renderLink = (): React.ReactElement => {
+    const ticketsCount = linkedTickets.length + existingTickets.totalCount;
+    if (ticketsCount === 0) {
+      return <span>-</span>;
+    }
+    return (
+      <StyledLink
+        onClick={(e) => {
+          e.stopPropagation();
+          setDialogOpen(true);
+        }}
+      >
+        {ticketsCount} linked ticket
+        {ticketsCount === 1 ? '' : 's'}
+      </StyledLink>
+    );
+  };
 
   return (
     <>
-      {linkedTickets.length ? (
-        <StyledLink
-          onClick={(e) => {
-            e.stopPropagation();
-            setDialogOpen(true);
-          }}
-        >
-          {linkedTickets.length} linked ticket
-          {linkedTickets.length === 1 ? '' : 's'}
-        </StyledLink>
-      ) : (
-        '-'
-      )}
+      {renderLink()}
       <Dialog
         open={dialogOpen}
         onClose={() => setDialogOpen(false)}
         scroll='paper'
         aria-labelledby='form-dialog-title'
-        maxWidth='md'
+        maxWidth='lg'
       >
         <DialogTitleWrapper>
           <DialogTitle id='scroll-dialog-title'>
@@ -90,54 +166,26 @@ export const LinkedTicketsModal = ({
           </DialogTitle>
         </DialogTitleWrapper>
         <DialogContent>
+          <Box mt={2} mb={6}>
+            <Typography>
+              <Bold>Ticket ID {ticket.unicefId} </Bold>
+              is linked to following related tickets.
+            </Typography>
+          </Box>
           <StyledTable>
             <TableHead>
               <TableRow>
-                <TableCell align='left'>{t('ID')}</TableCell>
-                <TableCell align='left'>{t('Status')}</TableCell>
+                <TableCell align='left'>{t('Ticket Id')}</TableCell>
                 <TableCell align='left'>{t('Category')}</TableCell>
                 <TableCell align='left'>{t('Issue Type')}</TableCell>
+                <TableCell align='left'>{t('Status')}</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {linkedTickets.map((ticket) => (
-                <ClickableTableRow
-                  hover
-                  onClick={
-                    canViewDetails
-                      ? history.push(
-                          `/${businessArea}/grievance-and-feedback/${ticket.id}`,
-                        )
-                      : undefined
-                  }
-                  key={ticket.id}
-                >
-                  <TableCell align='left'>
-                    {canViewDetails ? (
-                      <BlackLink
-                        to={`/${businessArea}/grievance-and-feedback/${ticket.id}`}
-                      >
-                        {ticket.unicefId}
-                      </BlackLink>
-                    ) : (
-                      ticket.unicefId
-                    )}
-                  </TableCell>
-                  <TableCell align='left'>
-                    <StatusContainer>
-                      <StatusBox
-                        status={statusChoices[ticket.status]}
-                        statusToColor={grievanceTicketStatusToColor}
-                      />
-                    </StatusContainer>
-                  </TableCell>
-
-                  <TableCell align='left'>
-                    {categoryChoices[ticket.category]}
-                  </TableCell>
-                  <TableCell align='left'>{ticket.issueType || '-'}</TableCell>
-                </ClickableTableRow>
-              ))}
+              {linkedTickets.map((linkedTicket) => renderRow(linkedTicket))}
+              {existingTickets.edges.map((existingTicket) =>
+                renderRow(existingTicket.node),
+              )}
             </TableBody>
           </StyledTable>
         </DialogContent>
