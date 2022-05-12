2267f8f7efb1d04ef21a11bd46613281b6c1a9ea
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-05-12 13:45:30 +0200

    Fsp plan details, total amount

---
+++ frontend/src/components/paymentmodule/FspPlanDetails/TotalAmount/TotalAmount.tsx
@@ -0,0 +1,31 @@
+import { Box, Typography } from '@material-ui/core';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { ContainerColumnWithBorder } from '../../../core/ContainerColumnWithBorder';
+import { Title } from '../../../core/Title';
+
+interface TotalAmountProps {
+  businessArea: string;
+  permissions: string[];
+}
+
+export function TotalAmount({
+  businessArea,
+  permissions,
+}: TotalAmountProps): React.ReactElement {
+  const { t } = useTranslation();
+
+  return (
+    <Box m={5}>
+      <ContainerColumnWithBorder>
+        <Box mt={4}>
+          <Title>
+            <Typography variant='h6'>
+              {t('Total Amount')}(PLN) {t('per FSP')}
+            </Typography>
+          </Title>
+        </Box>
+      </ContainerColumnWithBorder>
+    </Box>
+  );
+}
