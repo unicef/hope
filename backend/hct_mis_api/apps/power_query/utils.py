17a11f1d2a997a4e29584620b42d8907f259ae66
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2022-05-17 12:03:27 +0000

    Return a general excel file

--- backend/hct_mis_api/apps/power_query/utils.py
+++ backend/hct_mis_api/apps/power_query/utils.py
@@ -34,10 +34,13 @@ def fqn(o):
 def to_dataset(result):
     if isinstance(result, QuerySet):
         data = tablib.Dataset()
-        fields = [field.name for field in result.model._meta.fields]
+        fields = set().union(*(d.keys() for d in list(result)))
         data.headers = fields
-        for obj in result.all():
-            data.append([getattr(obj, f) for f in fields])
+        try :
+            for obj in result.all():
+                data.append([obj[f] for f in fields])
+        except Exception as e:
+            raise ValueError("Results can't be rendered as a tablib Dataset")
     elif isinstance(result, tablib.Dataset):
         data = result
     elif isinstance(result, dict):
