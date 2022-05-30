ba9018e80771eb325cae34db2d9367603bb89892
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-05-30 10:26:47 +0200

    add message dialog for acceptance process

---
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/AcceptanceProcess/MessageDialog.tsx
@@ -0,0 +1,89 @@
+import {
+  Box,
+  Button,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+} from '@material-ui/core';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import MessageIcon from '@material-ui/icons/Message';
+import styled from 'styled-components';
+import { Dialog } from '../../../../containers/dialogs/Dialog';
+import { DialogActions } from '../../../../containers/dialogs/DialogActions';
+
+const DialogTitleWrapper = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+`;
+
+const DialogFooter = styled.div`
+  padding: 12px 16px;
+  margin: 0;
+  border-top: 1px solid ${({ theme }) => theme.hctPalette.lighterGray};
+  text-align: right;
+`;
+
+const GreyText = styled.div`
+  color: #9e9e9e;
+`;
+
+const Message = styled(MessageIcon)`
+  color: #043f91;
+`;
+
+export interface MessageDialogProps {
+  message: string;
+  author: string;
+  date: string;
+}
+export function MessageDialog({
+  message,
+  author,
+  date,
+}: MessageDialogProps): React.ReactElement {
+  const { t } = useTranslation();
+  const [MessageDialogOpen, setMessageDialogOpen] = useState(false);
+  return (
+    <>
+      <Box p={2}>
+        <IconButton onClick={() => setMessageDialogOpen(true)}>
+          <Message />
+        </IconButton>
+      </Box>
+      <Dialog
+        open={MessageDialogOpen}
+        onClose={() => setMessageDialogOpen(false)}
+        scroll='paper'
+        aria-labelledby='form-dialog-title'
+        maxWidth='md'
+      >
+        <DialogTitleWrapper>
+          <DialogTitle id='scroll-dialog-title'>{t('Comment')}</DialogTitle>
+        </DialogTitleWrapper>
+        <DialogContent>
+          <Box display='flex' flexDirection='column'>
+            <Box display='flex'>
+              {author}{' '}
+              <GreyText>
+                <Box ml={1}>on {date}</Box>
+              </GreyText>
+            </Box>
+            {message}
+          </Box>
+        </DialogContent>
+        <DialogFooter>
+          <DialogActions>
+            <Button
+              type='submit'
+              color='primary'
+              variant='contained'
+              onClick={() => setMessageDialogOpen(false)}
+            >
+              {t('Close')}
+            </Button>
+          </DialogActions>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
