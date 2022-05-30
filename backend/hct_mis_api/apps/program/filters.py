e3ecc753418ce8377e58a737c0b081074d2530f9
KonradMarzec1991 <33575891+KonradMarzec1991@users.noreply.github.com>
2022-05-30 16:33:37 +0200

    Refactor filters (#1403)
    * Upgrade django_filter version

---
+++ backend/hct_mis_api/apps/program/filters.py
@@ -0,0 +1,133 @@
+from django.db.models import Count, Q
+from django.db.models.functions import Lower
+
+from django_filters import (
+    CharFilter,
+    DateFilter,
+    FilterSet,
+    MultipleChoiceFilter,
+    OrderingFilter,
+)
+
+from hct_mis_api.apps.core.filters import DecimalRangeFilter, IntegerRangeFilter
+from hct_mis_api.apps.core.utils import CustomOrderingFilter
+from hct_mis_api.apps.payment.models import PaymentRecord, CashPlanPaymentVerification
+from hct_mis_api.apps.program.models import Program, CashPlan
+
+
+class ProgramFilter(FilterSet):
+    business_area = CharFilter(field_name="business_area__slug", required=True)
+    search = CharFilter(method="search_filter")
+    status = MultipleChoiceFilter(field_name="status", choices=Program.STATUS_CHOICE)
+    sector = MultipleChoiceFilter(field_name="sector", choices=Program.SECTOR_CHOICE)
+    number_of_households = IntegerRangeFilter(field_name="total_hh_count")
+    budget = DecimalRangeFilter(field_name="budget")
+    start_date = DateFilter(field_name="start_date", lookup_expr="gte")
+    end_date = DateFilter(field_name="end_date", lookup_expr="lte")
+
+    class Meta:
+        fields = (
+            "business_area",
+            "search",
+            "status",
+            "sector",
+            "number_of_households",
+            "budget",
+            "start_date",
+            "end_date",
+        )
+        model = Program
+
+    order_by = CustomOrderingFilter(
+        fields=(Lower("name"), "status", "start_date", "end_date", "sector", "total_hh_count", "budget")
+    )
+
+    def filter_queryset(self, queryset):
+        queryset = queryset.annotate(
+            total_hh_count=Count(
+                "cash_plans__payment_records__household",
+                filter=Q(cash_plans__payment_records__delivered_quantity__gte=0),
+                distinct=True,
+            )
+        )
+        return super().filter_queryset(queryset)
+
+    def search_filter(self, qs, name, value):
+        values = value.split(" ")
+        q_obj = Q()
+        for value in values:
+            q_obj |= Q(name__istartswith=value)
+        return qs.filter(q_obj)
+
+
+class CashPlanFilter(FilterSet):
+    search = CharFilter(method="search_filter")
+    delivery_type = MultipleChoiceFilter(field_name="delivery_type", choices=PaymentRecord.DELIVERY_TYPE_CHOICE)
+    verification_status = MultipleChoiceFilter(
+        field_name="cash_plan_payment_verification_summary__status", choices=CashPlanPaymentVerification.STATUS_CHOICES
+    )
+    business_area = CharFilter(
+        field_name="business_area__slug",
+    )
+
+    class Meta:
+        fields = {
+            "program": ["exact"],
+            "assistance_through": ["exact", "startswith"],
+            "service_provider__full_name": ["exact", "startswith"],
+            "start_date": ["exact", "lte", "gte"],
+            "end_date": ["exact", "lte", "gte"],
+            "business_area": ["exact"],
+        }
+        model = CashPlan
+
+    order_by = OrderingFilter(
+        fields=(
+            "ca_id",
+            "status",
+            "total_number_of_hh",
+            "total_entitled_quantity",
+            ("cash_plan_payment_verification_summary__status", "verification_status"),
+            "total_persons_covered",
+            "total_delivered_quantity",
+            "total_undelivered_quantity",
+            "dispersion_date",
+            "assistance_measurement",
+            "assistance_through",
+            "delivery_type",
+            "start_date",
+            "end_date",
+            "program__name",
+            "id",
+            "updated_at",
+            "service_provider__full_name",
+        )
+    )
+
+    def filter_queryset(self, queryset):
+        queryset = queryset.annotate(total_number_of_hh=Count("payment_records"))
+        return super().filter_queryset(queryset)
+
+    def search_filter(self, qs, name, value):
+        values = value.split(" ")
+        q_obj = Q()
+        for value in values:
+            q_obj |= Q(ca_id__istartswith=value)
+        return qs.filter(q_obj)
+
+
+class ChartProgramFilter(FilterSet):
+    business_area = CharFilter(field_name="business_area__slug", required=True)
+
+    class Meta:
+        fields = ("business_area",)
+        model = Program
+
+    def search_filter(self, qs, name, value):
+        values = value.split(" ")
+        q_obj = Q()
+        for value in values:
+            q_obj |= Q(first_name__startswith=value)
+            q_obj |= Q(last_name__startswith=value)
+            q_obj |= Q(email__startswith=value)
+        return qs.filter(q_obj)
---
+++ backend/hct_mis_api/apps/targeting/filters.py
@@ -0,0 +1,121 @@
+from django.db.models import Q
+from django.db.models.functions import Lower
+from django_filters import CharFilter, DateTimeFilter, FilterSet, ModelMultipleChoiceFilter, NumericRangeFilter
+
+import hct_mis_api.apps.targeting.models as target_models
+from hct_mis_api.apps.core.filters import IntegerFilter
+from hct_mis_api.apps.core.utils import CustomOrderingFilter
+from hct_mis_api.apps.program.models import Program
+
+
+class HouseholdFilter(FilterSet):
+    order_by = CustomOrderingFilter(
+        fields=(
+            "id",
+            Lower("head_of_household__full_name"),
+            "size",
+            Lower("admin_area_new__name"),
+            "updated_at",
+            "unicef_id",
+        )
+    )
+    business_area = CharFilter(field_name="business_area__slug")
+
+
+class TargetPopulationFilter(FilterSet):
+    """Query target population records.
+
+    Loads associated entries for Households and TargetRules.
+    """
+
+    name = CharFilter(field_name="name", lookup_expr="startswith")
+    created_by_name = CharFilter(field_name="created_by", method="filter_created_by_name")
+    number_of_households_min = IntegerFilter(method="filter_number_of_households_min")
+    number_of_households_max = IntegerFilter(method="filter_number_of_households_max")
+    candidate_list_total_households_min = IntegerFilter(
+        field_name="candidate_list_total_households",
+        lookup_expr="gte",
+    )
+    candidate_list_total_households_max = IntegerFilter(
+        field_name="candidate_list_total_households",
+        lookup_expr="lte",
+    )
+    candidate_list_total_individuals_min = IntegerFilter(
+        field_name="candidate_list_total_individuals",
+        lookup_expr="gte",
+    )
+    candidate_list_total_individuals_max = IntegerFilter(
+        field_name="candidate_list_total_individuals",
+        lookup_expr="lte",
+    )
+
+    final_list_total_households_min = IntegerFilter(
+        field_name="final_list_total_households",
+        lookup_expr="gte",
+    )
+    final_list_total_households_max = IntegerFilter(
+        field_name="final_list_total_households",
+        lookup_expr="lte",
+    )
+    final_list_total_individuals_min = IntegerFilter(
+        field_name="final_list_total_individuals",
+        lookup_expr="gte",
+    )
+    final_list_total_individuals_max = IntegerFilter(
+        field_name="final_list_total_individuals",
+        lookup_expr="lte",
+    )
+    business_area = CharFilter(field_name="business_area__slug")
+    program = ModelMultipleChoiceFilter(field_name="program", to_field_name="id", queryset=Program.objects.all())
+
+    @staticmethod
+    def filter_created_by_name(queryset, model_field, value):
+        """Gets full name of the associated user from query."""
+        fname_query_key = f"{model_field}__given_name__icontains"
+        lname_query_key = f"{model_field}__family_name__icontains"
+        for name in value.strip().split():
+            queryset = queryset.filter(Q(**{fname_query_key: name}) | Q(**{lname_query_key: name}))
+        return queryset
+
+    def filter_number_of_households_min(self, queryset, model_field, value):
+        queryset = queryset.exclude(status=target_models.TargetPopulation.STATUS_DRAFT).filter(
+            number_of_households__gte=value
+        )
+        return queryset
+
+    def filter_number_of_households_max(self, queryset, model_field, value):
+        queryset = queryset.exclude(status=target_models.TargetPopulation.STATUS_DRAFT).filter(
+            number_of_households__lte=value
+        )
+        return queryset
+
+    class Meta:
+        model = target_models.TargetPopulation
+        fields = (
+            "name",
+            "created_by_name",
+            "created_at",
+            "updated_at",
+            "status",
+            "households",
+        )
+
+        filter_overrides = {
+            target_models.IntegerRangeField: {"filter_class": NumericRangeFilter},
+            target_models.models.DateTimeField: {"filter_class": DateTimeFilter},
+        }
+
+    order_by = CustomOrderingFilter(
+        fields=(
+            Lower("name"),
+            "created_at",
+            "created_by",
+            "updated_at",
+            "status",
+            "total_households",
+            "total_family_size",
+            "program__id",
+            "final_list_total_households",
+            "candidate_list_total_households",
+        )
+    )
