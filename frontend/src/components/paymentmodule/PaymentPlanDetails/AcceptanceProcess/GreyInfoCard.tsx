1ba8e450cf8c72df45c2be33418d26da72bfaaea
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-05-30 10:26:47 +0200

    add message dialog for acceptance process

--- frontend/src/components/paymentmodule/PaymentPlanDetails/AcceptanceProcess/GreyInfoCard.tsx
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/AcceptanceProcess/GreyInfoCard.tsx
@@ -2,9 +2,16 @@ import { Box } from '@material-ui/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
+import { MessageDialog } from './MessageDialog';
 
 const GreyText = styled.div`
   color: #9e9e9e;
+`;
+
+const GreyTitle = styled.div`
+  color: #7c8990;
+  text-transform: uppercase;
+  font-size: 12px;
 `;
 
 const GreyBox = styled(Box)`
@@ -22,8 +29,21 @@ export function GreyInfoCard({
   const { t } = useTranslation();
 
   return (
-    <GreyBox p={3}>
-      Approved by someone<GreyText>on 01/01/2022</GreyText>
-    </GreyBox>
+    <Box display='flex' flexDirection='column'>
+      <Box p={3}>
+        <GreyTitle>Sent for approval by martin scott on 01/01/2022</GreyTitle>
+      </Box>
+      <GreyBox display='flex' alignItems='center' ml={3} mr={3} p={3}>
+        Approved by someone
+        <Box ml={1}>
+          <GreyText>on 01/01/2022</GreyText>
+        </Box>
+        <MessageDialog
+          message='I rejected it because I felt it was wrong.'
+          author='Bob Ugar'
+          date='12/02/2022'
+        />
+      </GreyBox>
+    </Box>
   );
 }
