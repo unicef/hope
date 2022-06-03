7db6454a78aec12503b389704b829bd1ce710b89
Konrad Marzec <konradmarzec@Konrads-MacBook-Pro.local>
2022-06-03 16:23:03 +0200

    Add non-flex-fields serializer and view

--- backend/hct_mis_api/apps/core/schema.py
+++ backend/hct_mis_api/apps/core/schema.py
@@ -298,3 +298,93 @@ class Query(graphene.ObjectType):
 
     def resolve_all_groups_with_fields(self, info, **kwargs):
         return FlexibleAttributeGroup.objects.distinct().filter(flex_attributes__isnull=False)
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import serializers
+from django.core.cache import cache
+
+
+class LabelSerializer(serializers.Serializer):
+    label = serializers.CharField()
+    language = serializers.CharField()
+
+
+class CoreFieldChoiceSerializer(serializers.Serializer):
+    labels = serializers.SerializerMethodField()
+    label_en = serializers.SerializerMethodField()
+    value = serializers.CharField()
+    admin = serializers.CharField(default=None)
+    list_name = serializers.CharField(default=None)
+
+    def get_labels(self, obj):
+        labels = obj.get("labels")
+        if labels:
+            return obj["labels"]
+        else:
+            return [obj["label"]]
+
+    def get_label_en(self, obj):
+        labels = obj.get("labels")
+        if labels:
+           obj_list = obj["labels"]
+           my_key = [obj for obj in obj_list if obj["name"] == "English(EN)"]
+           return my_key[0]["English(EN)"]
+        else:
+            return obj["label"]["English(EN)"]
+
+
+class FieldAttributeSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    type = serializers.CharField()
+    name = serializers.CharField()
+    labels = serializers.SerializerMethodField()
+    label_en = serializers.SerializerMethodField()
+    hint = serializers.CharField()
+    required = serializers.BooleanField()
+    choices = CoreFieldChoiceSerializer(many=True)
+    associated_with = serializers.CharField()
+    is_flex_field = serializers.SerializerMethodField()
+
+    def get_labels(self, obj):
+        labels = obj.get("labels")
+        if labels:
+            return obj["labels"]
+        else:
+            return [obj["label"]]
+
+    def get_label_en(self, obj):
+        labels = obj.get("labels")
+        if labels:
+           obj_list = obj["labels"]
+           my_key = [obj for obj in obj_list if obj["name"] == "English(EN)"]
+           return my_key[0]["English(EN)"]
+        else:
+            return obj["label"]["English(EN)"]
+
+    def get_is_flex_field(self, obj):
+        if isinstance(obj, dict):
+            return False
+        return True
+
+
+@api_view()
+def all_fields_attributes(request):
+    flex_field = request.data.get("flex_field", False)
+    business_area_slug = request.data.get("business_area_slug")
+
+    logger.info("**********")
+    logger.info(cache.keys("*"))
+
+    records = cache.get(business_area_slug)
+    if records:
+        return Response(records)
+
+    records = sort_by_attr(get_fields_attr_generators(flex_field, business_area_slug), "label.English(EN)")
+    serializer = FieldAttributeSerializer(records, many=True)
+    data = serializer.data
+
+    cache.set(business_area_slug, data)
+
+    return Response(data)
