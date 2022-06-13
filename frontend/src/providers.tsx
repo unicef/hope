593ceadc870deb6038dbf65df44402caec14be77
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-06-13 13:36:10 +0200

    current progress

--- frontend/src/providers.tsx
+++ frontend/src/providers.tsx
@@ -1,9 +1,8 @@
 import { ApolloProvider } from '@apollo/react-hooks';
-import MomentUtils from '@date-io/moment';
 import { ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-import { MuiPickersUtilsProvider } from '@mui/pickers';
-import moment from 'moment';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import React, { useEffect, useState } from 'react';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import ApolloClient from 'apollo-client';
@@ -31,12 +30,12 @@ export const Providers: React.FC = ({
     <ApolloProvider client={apolloClient}>
       <ThemeProvider theme={theme}>
         <StyledThemeProvider theme={theme}>
-          <MuiPickersUtilsProvider libInstance={moment} utils={MomentUtils}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
             <ConfirmationDialogProvider>
               <CssBaseline />
               {children}
             </ConfirmationDialogProvider>
-          </MuiPickersUtilsProvider>
+          </LocalizationProvider>
         </StyledThemeProvider>
       </ThemeProvider>
     </ApolloProvider>
