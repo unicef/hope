d29302560e5c8a966ad67be0b7ac4fda6c8e52f2
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-06-14 13:24:16 +0200

    add new components for filters

--- frontend/src/containers/tables/paymentmodule/FspTable/FspFilters.tsx
+++ frontend/src/containers/tables/paymentmodule/FspTable/FspFilters.tsx
@@ -5,32 +5,21 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { ContainerWithBorder } from '../../../../components/core/ContainerWithBorder';
+import { SearchTextField } from '../../../../components/core/SearchTextField';
+import { SelectFilter } from '../../../../components/core/SelectFilter';
 import InputLabel from '../../../../shared/InputLabel';
 import Select from '../../../../shared/Select';
 import TextField from '../../../../shared/TextField';
 import { useCashPlanVerificationStatusChoicesQuery } from '../../../../__generated__/graphql';
 
-const StyledFormControl = styled(FormControl)`
-  width: 232px;
-  color: #5f6368;
-  border-bottom: 0;
-`;
-
-const SearchTextField = styled(TextField)`
-  flex: 1;
-  && {
-    min-width: 150px;
-  }
-`;
-
 interface FspFiltersProps {
   onFilterChange;
   filter;
 }
-export function FspFilters({
+export const FspFilters = ({
   onFilterChange,
   filter,
-}: FspFiltersProps): React.ReactElement {
+}: FspFiltersProps): React.ReactElement => {
   const { t } = useTranslation();
   const handleFilterChange = (e, name): void =>
     onFilterChange({ ...filter, [name]: e.target.value });
@@ -48,43 +37,28 @@ export function FspFilters({
         <Grid item>
           <SearchTextField
             label={t('Search')}
-            variant='outlined'
-            margin='dense'
             onChange={(e) => handleFilterChange(e, 'search')}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position='start'>
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
+            value={filter.search}
           />
         </Grid>
         <Grid item>
-          <StyledFormControl variant='outlined' margin='dense'>
-            <InputLabel>{t('Payment Channel')}</InputLabel>
-            <Select
-              /* eslint-disable-next-line @typescript-eslint/ban-ts-ignore */
-              // @ts-ignore
-              onChange={(e) => handleFilterChange(e, 'paymentChannel')}
-              variant='outlined'
-              label='Modality'
-              value={filter.paymentChannel || ''}
-            >
-              <MenuItem value=''>
-                <em>None</em>
+          <SelectFilter
+            onChange={(e) => handleFilterChange(e, 'paymentChannel')}
+            variant='outlined'
+            label={t('Payment Channel')}
+            value={filter.paymentChannel || ''}
+          >
+            <MenuItem value=''>
+              <em>None</em>
+            </MenuItem>
+            {statusChoicesData.paymentRecordDeliveryTypeChoices.map((item) => (
+              <MenuItem key={item.name} value={item.value}>
+                {item.name}
               </MenuItem>
-              {statusChoicesData.paymentRecordDeliveryTypeChoices.map(
-                (item) => (
-                  <MenuItem key={item.name} value={item.value}>
-                    {item.name}
-                  </MenuItem>
-                ),
-              )}
-            </Select>
-          </StyledFormControl>
+            ))}
+          </SelectFilter>
         </Grid>
       </Grid>
     </ContainerWithBorder>
   );
-}
+};
