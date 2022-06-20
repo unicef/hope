d0003ef0e0cd8c641758a34ecffa9b48b7bbb6de
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-06-20 14:18:34 +0200

    fix test

--- backend/hct_mis_api/apps/registration_datahub/tests/test_rdi_create.py
+++ backend/hct_mis_api/apps/registration_datahub/tests/test_rdi_create.py
@@ -827,7 +827,7 @@ class TestRdiDiiaCreateTask(BaseElasticSearchTestCase):
         )
         expected = {
             "relationship": "HEAD",
-            "sex": "M",
+            "sex": "MALE",
             "middle_name": "Mid",
             "marital_status": "MARRIED",
         }
