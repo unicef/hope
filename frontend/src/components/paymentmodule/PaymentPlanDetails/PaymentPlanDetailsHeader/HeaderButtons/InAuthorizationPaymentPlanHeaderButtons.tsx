aa5d984a9d9c0826100b39f1393b89e02c8e1800
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-06-28 12:23:30 +0200

    AuthorizePaymentPlan

--- frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentPlanDetailsHeader/HeaderButtons/InAuthorizationPaymentPlanHeaderButtons.tsx
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentPlanDetailsHeader/HeaderButtons/InAuthorizationPaymentPlanHeaderButtons.tsx
@@ -2,6 +2,7 @@ import { Box, Button } from '@material-ui/core';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ErrorButton } from '../../../../core/ErrorButton';
+import { AuthorizePaymentPlan } from '../AuthorizePaymentPlan';
 import { RejectPaymentPlan } from '../RejectPaymentPlan';
 
 export interface InAuthorizationPaymentPlanHeaderButtonsProps {
@@ -25,22 +26,8 @@ export function InAuthorizationPaymentPlanHeaderButtons({
   const [openDelete, setOpenDelete] = useState(false);
   return (
     <div>
-      {canLock && (
-        <Box m={2}>
-          <RejectPaymentPlan paymentPlanId='33333' />
-        </Box>
-      )}
-      {canLock && (
-        <Box m={2}>
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={() => setOpenApprove(true)}
-          >
-            {t('Authorize')}
-          </Button>
-        </Box>
-      )}
+      {canLock && <RejectPaymentPlan paymentPlanId='33333' />}
+      {canLock && <AuthorizePaymentPlan paymentPlanId='33333' />}
     </div>
   );
 }
