15d063989308ef60010d35e97065ccd3fd847525
Bartosz Woźniak <91740028+bwozniaktivix@users.noreply.github.com>
2022-07-06 11:18:42 +0200

    Extend IndividualNode interface with phone no validity info (#1540)

--- backend/hct_mis_api/apps/household/tests/test_individual_query.py
+++ backend/hct_mis_api/apps/household/tests/test_individual_query.py
@@ -25,6 +25,7 @@ class TestIndividualQuery(APITestCase):
             givenName
             familyName
             phoneNo
+            phoneNoValid
             birthDate
           }
         }
@@ -68,6 +69,7 @@ class TestIndividualQuery(APITestCase):
 
     @classmethod
     def setUpTestData(cls):
+        cls.maxDiff = None
         cached_business_areas_slug_id_dict.cache_clear()
         create_afghanistan()
         cls.user = UserFactory()
