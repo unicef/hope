a4d4cf426722019137c9bae6a21b10b20a70ee1c
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-07-08 13:04:09 +0200

    tests pass

--- backend/hct_mis_api/apps/household/tests/test_details_endpoint.py
+++ backend/hct_mis_api/apps/household/tests/test_details_endpoint.py
@@ -1,48 +1,41 @@
-from unittest import TestCase
-from hct_mis_api.apps.household.models import Household
-from hct_mis_api.apps.household.models import DocumentType
+from django.test import TestCase
 from rest_framework.test import APIClient
 
+from hct_mis_api.apps.account.models import BusinessArea
 from hct_mis_api.apps.account.fixtures import UserFactory, BusinessAreaFactory
 from hct_mis_api.apps.program.fixtures import ProgramFactory
-from hct_mis_api.apps.household.models import IDENTIFICATION_TYPE_TAX_ID
+from hct_mis_api.apps.household.models import IDENTIFICATION_TYPE_TAX_ID, Household, DocumentType
 from hct_mis_api.apps.household.fixtures import (
     DocumentFactory,
-    HouseholdFactory,
-    IndividualFactory,
     DocumentTypeFactory,
     create_household
 )
 
 
 class TestDetails(TestCase):
-    def setUp(self):
-        self.user = UserFactory()
-        self.business_area = BusinessAreaFactory(
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.business_area = BusinessAreaFactory(
             code="0060",
         )
-        self.program = ProgramFactory(business_area=self.business_area)
+        cls.program = ProgramFactory(business_area=cls.business_area)
         household, individuals = create_household(household_args={"size": 1})
-        self.household = household
-        self.individual = individuals[0]
-        self.document_type = DocumentType.objects.get(type=IDENTIFICATION_TYPE_TAX_ID)
-        self.document = DocumentFactory(
-            individual=self.individual,
-            type=self.document_type
+        cls.household = household
+        cls.individual = individuals[0]
+        cls.document_type = DocumentTypeFactory(type=IDENTIFICATION_TYPE_TAX_ID)
+        cls.document = DocumentFactory(
+            individual=cls.individual,
+            type=cls.document_type
         )
 
-        self.client = APIClient()
-        self.client.force_authenticate(user=self.user)
-
-    def tearDown(self):
-        # due to --keepdb, with each run, there are more households
-        # which causes the search to fail on finding multiple objects
-        Household.objects.all().delete()
+        cls.api_client = APIClient()
+        cls.api_client.force_authenticate(user=cls.user)
 
     def test_getting_individual(self):
         tax_id = self.document.document_number
-        self.assertEqual(self.client.get("/api/details?tax_id=non-existent").status_code, 400)
-        response = self.client.get(f"/api/details?tax_id={tax_id}")
+        self.assertEqual(self.api_client.get("/api/details?tax_id=non-existent").status_code, 400)
+        response = self.api_client.get(f"/api/details?tax_id={tax_id}")
         self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertIsNotNone(data["individual"])
@@ -54,8 +47,8 @@ class TestDetails(TestCase):
         registration_id = "HOPE-202253186077"
         self.household.kobo_asset_id = registration_id
         self.household.save()
-        self.assertEqual(self.client.get("/api/details?registration_id=non-existent").status_code, 400)
-        response = self.client.get(f"/api/details?registration_id={registration_id}")
+        self.assertEqual(self.api_client.get("/api/details?registration_id=non-existent").status_code, 400)
+        response = self.api_client.get(f"/api/details?registration_id={registration_id}")
         self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertIsNotNone(data["household"])
