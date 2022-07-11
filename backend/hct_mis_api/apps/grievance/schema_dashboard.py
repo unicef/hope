eb1574559d4c562dcb7e731b18794a88a8ff8308
Konrad Marzec <konradmarzec@Konrads-MacBook-Pro.local>
2022-07-11 15:31:54 +0200

    Add ticketByType query

--- backend/hct_mis_api/apps/grievance/schema_dashboard.py
+++ backend/hct_mis_api/apps/grievance/schema_dashboard.py
@@ -1,7 +1,7 @@
 import logging
 
 import graphene
-from django.db.models import Case, CharField, Count, When, Value, Q, Avg
+from django.db.models import Case, CharField, Count, When, Value, Q, Avg, F
 from django.utils.encoding import force_str
 
 from hct_mis_api.apps.grievance.models import GrievanceTicket
@@ -29,6 +29,15 @@ class BusinessAreaInput(graphene.InputObjectType):
     business_area = graphene.String()
 
 
+class TicketByType(graphene.ObjectType):
+    user_generated_count = graphene.Int()
+    system_generated_count = graphene.Int()
+    closed_user_generated_count = graphene.Int()
+    closed_system_generated_count = graphene.Int()
+    user_generated_avg_resolution = graphene.Float()
+    system_generated_avg_resolution = graphene.Float()
+
+
 class TicketByCategory(graphene.ObjectType):
     category_name = graphene.String()
     count = graphene.Int()
@@ -49,68 +58,12 @@ class TicketByLocationAndCategory(graphene.ObjectType):
 
 
 class Query(graphene.ObjectType):
+    tickets_by_type = graphene.Field(TicketByType)
     tickets_by_category = graphene.List(TicketByCategory, business_area=graphene.String(required=True))
     tickets_by_status = graphene.List(TicketByStatus, business_area=graphene.String(required=True))
     tickets_by_location_and_category = graphene.List(
         TicketByLocationAndCategory, business_area=graphene.String(required=True)
     )
-
-    def resolve_tickets_by_category(self, info, **kwargs):
-        logger.info(kwargs)
-        return (
-            GrievanceTicket.objects.filter(business_area=kwargs["business_area"])
-            .annotate(
-                category_name=Case(
-                    *[When(category=i, then=Value(force_str(j))) for (i, j) in GrievanceTicket.CATEGORY_CHOICES],
-                    default=Value("XXX"),
-                    output_field=CharField()
-                )
-            )
-            .values("category_name")
-            .annotate(count=Count("category"))
-            .order_by("-count")
-        )
-
-    def resolve_tickets_by_status(self, info, **kwargs):
-        return (
-            GrievanceTicket.objects.filter(business_area=kwargs["business_area"])
-            .annotate(
-                status_name=Case(
-                    *[When(status=i, then=Value(force_str(j))) for (i, j) in GrievanceTicket.STATUS_CHOICES],
-                    default=Value("XXX"),
-                    output_field=CharField()
-                )
-            )
-            .values("status_name")
-            .annotate(count=Count("status"))
-            .order_by("-count")
-        )
-
-    def resolve_tickets_by_location_and_category(self, info, **kwargs):
-        qs = (
-            GrievanceTicket.objects.select_related("admin2")
-            .filter(business_area=kwargs["business_area"])
-            .values_list("admin2__title", "category")
-            .annotate(
-                category_name=Case(
-                    *[When(category=i, then=Value(force_str(j))) for (i, j) in GrievanceTicket.CATEGORY_CHOICES],
-                    default=Value("XXX"),
-                    output_field=CharField()
-                )
-            )
-            .annotate(count=Count("category"))
-            .order_by("admin2__title", "-count")
-        )
-
-        results = []
-        for item in qs:
-            location, _, category_name, count = item
-            category_item = {"category_name": category_name, "count": count}
-            if results and results[-1]["location"] == location:
-                results[-1]["categories"].append(category_item)
-            else:
-                results.append({"location": location, "categories": [category_item]})
-        return results
 
     def resolve_tickets_by_type(self, info, **kwargs):
         user_generated, system_generated = create_little_query()
@@ -134,3 +87,60 @@ class Query(graphene.ObjectType):
                 system_generated_avg_resolution=Avg("days_diff", filter=system_generated),
             )
         )
+
+    def resolve_tickets_by_category(self, info, **kwargs):
+        logger.info(kwargs)
+        return (
+            GrievanceTicket.objects.filter(business_area=kwargs["business_area"])
+                .annotate(
+                category_name=Case(
+                    *[When(category=i, then=Value(force_str(j))) for (i, j) in GrievanceTicket.CATEGORY_CHOICES],
+                    default=Value("XXX"),
+                    output_field=CharField()
+                )
+            )
+                .values("category_name")
+                .annotate(count=Count("category"))
+                .order_by("-count")
+        )
+
+    def resolve_tickets_by_status(self, info, **kwargs):
+        return (
+            GrievanceTicket.objects.filter(business_area=kwargs["business_area"])
+                .annotate(
+                status_name=Case(
+                    *[When(status=i, then=Value(force_str(j))) for (i, j) in GrievanceTicket.STATUS_CHOICES],
+                    default=Value("XXX"),
+                    output_field=CharField()
+                )
+            )
+                .values("status_name")
+                .annotate(count=Count("status"))
+                .order_by("-count")
+        )
+
+    def resolve_tickets_by_location_and_category(self, info, **kwargs):
+        qs = (
+            GrievanceTicket.objects.select_related("admin2")
+                .filter(business_area=kwargs["business_area"])
+                .values_list("admin2__title", "category")
+                .annotate(
+                category_name=Case(
+                    *[When(category=i, then=Value(force_str(j))) for (i, j) in GrievanceTicket.CATEGORY_CHOICES],
+                    default=Value("XXX"),
+                    output_field=CharField()
+                )
+            )
+                .annotate(count=Count("category"))
+                .order_by("admin2__title", "-count")
+        )
+
+        results = []
+        for item in qs:
+            location, _, category_name, count = item
+            category_item = {"category_name": category_name, "count": count}
+            if results and results[-1]["location"] == location:
+                results[-1]["categories"].append(category_item)
+            else:
+                results.append({"location": location, "categories": [category_item]})
+        return results
