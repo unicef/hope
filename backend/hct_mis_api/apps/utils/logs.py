883e8f7a0bc23ea0b6d2b2aa304624aa53965ba1
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-07-18 13:21:29 +0200

    use logger from logging

--- backend/hct_mis_api/apps/utils/logs.py
+++ backend/hct_mis_api/apps/utils/logs.py
@@ -1,13 +1,15 @@
 import logging
 from functools import wraps
 
+logger = logging.getLogger(__name__)
+
 
 def log_start_and_end(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        logging.info(f"--- Starting: {func.__name__}")
+        logger.info(f"--- Starting: {func.__name__}")
         result = func(*args, **kwargs)
-        logging.info(f"--- Ending: {func.__name__}")
+        logger.info(f"--- Ending: {func.__name__}")
         return result
 
     return wrapper
