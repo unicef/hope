007f22aaab1049e54068bc108384449f7cb8fae1
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-07-18 12:35:48 +0200

    add wraps

--- backend/hct_mis_api/apps/utils/logs.py
+++ backend/hct_mis_api/apps/utils/logs.py
@@ -1,7 +1,9 @@
 import logging
+from functools import wraps
 
 
 def log_start_and_end(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         logging.info(f"--- Starting: {func.__name__}")
         result = func(*args, **kwargs)
