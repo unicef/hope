09fcd232bcbe31b1f799c8f19453f8cf8c180661
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-07-20 13:25:56 +0200

    smoke tests, login

--- cypress_testing_service/cypress/integration/smoke_testing/smoke_testing.spec.ts
+++ cypress_testing_service/cypress/integration/smoke_testing/smoke_testing.spec.ts
@@ -1,4 +1,4 @@
-import { When, Then, Before } from 'cypress-cucumber-preprocessor/steps';
+import { When, Then, And } from 'cypress-cucumber-preprocessor/steps';
 // import '../../support/before';
 
 // Before(() => {
@@ -30,4 +30,22 @@ import { When, Then, Before } from 'cypress-cucumber-preprocessor/steps';
 
 When('I visit /', () => {
   cy.visit('/');
+})
+
+Then("I should see the AD login page", () => {
+  cy.get('a').contains('Sign in');
+})
+
+When("I visit admin panel", () => {
+  cy.visit('/api/unicorn/');
+})
+
+And("I fill in the login form", () => {
+  cy.get('input[name="username"]').type('root');
+  cy.get('input[name="password"]').type('root1234');
+  cy.get('input').contains('Log in').click();
+})
+
+Then("I should see the admin panel contents", () => {
+  cy.get('a').contains('HOPE Administration');
 })
