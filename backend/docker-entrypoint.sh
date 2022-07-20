9097cf2c31dd05a8dd704ee4e4be4c05eb14f545
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-07-20 10:09:59 +0200

    improve script readability

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -8,18 +8,18 @@ if [ $# -eq 0 ]; then
 else
     case "$1" in
         "dev")
-        until pg_isready -h db -p 5432;
-          do echo "waiting for database"; sleep 2; done;
-        python manage.py collectstatic --no-input
-        python manage.py migratealldb
-        python manage.py runserver 0.0.0.0:8000
+            until pg_isready -h db -p 5432;
+            do echo "waiting for database"; sleep 2; done;
+            python manage.py collectstatic --no-input
+            python manage.py migratealldb
+            python manage.py runserver 0.0.0.0:8000
         ;;
         "celery-beat")
-        waitforit -host=backend -port=8000 --timeout 300 && \
-        celery -A hct_mis_api.apps.core.celery beat -l INFO --scheduler hct_mis_api.apps.core.models:CustomDatabaseScheduler
+            waitforit -host=backend -port=8000 --timeout 300 && \
+            celery -A hct_mis_api.apps.core.celery beat -l INFO --scheduler hct_mis_api.apps.core.models:CustomDatabaseScheduler
         ;;
-    *)
-    exec "$@"
-    ;;
+        *)
+            exec "$@"
+        ;;
     esac
 fi
