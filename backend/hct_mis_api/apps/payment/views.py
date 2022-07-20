47d325dbac7be97f103fda249782f8cfbec1cd74
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-07-20 09:55:14 +0200

    update unit tests & add new fields

--- backend/hct_mis_api/apps/payment/views.py
+++ backend/hct_mis_api/apps/payment/views.py
@@ -3,19 +3,15 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from graphql import GraphQLError
 
 from hct_mis_api.apps.account.permissions import Permissions
 from hct_mis_api.apps.core.utils import decode_id_string
 from hct_mis_api.apps.payment.models import CashPlanPaymentVerification
-from hct_mis_api.apps.payment.celery_tasks import create_cash_plan_payment_verification_xls
 
 logger = logging.getLogger(__name__)
 
 
-@api_view()
 @login_required
 def download_cash_plan_payment_verification(request, verification_id):
     cash_plan_payment_verification_id = decode_id_string(verification_id)
@@ -27,6 +23,8 @@ def download_cash_plan_payment_verification(request, verification_id):
     ):
         logger.error("Permission Denied: User does not have correct permission.")
         raise PermissionDenied("Permission Denied: User does not have correct permission.")
+    if cash_plan_payment_verification.verification_channel != CashPlanPaymentVerification.VERIFICATION_CHANNEL_XLSX:
+        raise GraphQLError("You can only download verification file when XLSX channel is selected")
 
     if cash_plan_payment_verification.has_xlsx_cash_plan_payment_verification_file:
         if not cash_plan_payment_verification.xlsx_cashplan_payment_verification_file.was_downloaded:
@@ -34,6 +32,5 @@ def download_cash_plan_payment_verification(request, verification_id):
             cash_plan_payment_verification.xlsx_cashplan_payment_verification_file.save()
         return redirect(cash_plan_payment_verification.xlsx_cash_plan_payment_verification_file_link)
     else:
-        create_cash_plan_payment_verification_xls.delay(cash_plan_payment_verification_id, request.user.pk)
-        msg = "Started creating xlsx file, link will send to user's email"
-        return Response({"message": msg})
+        logger.error(f"File not found. CashPlanPaymentVerification ID: {verification_id}")
+        raise GraphQLError("File not found")
