e608f061cc14700cb0729ad98015234ca1ec0b94
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-07-22 14:11:19 +0200

    done

--- backend/hct_mis_api/apps/cash_assist_datahub/management/commands/create_sessions.py
+++ backend/hct_mis_api/apps/cash_assist_datahub/management/commands/create_sessions.py
@@ -1,14 +1,14 @@
 from uuid import UUID
 from django.core.management import BaseCommand
 
+import hct_mis_api.apps.payment.fixtures as payment_fixtures
+import hct_mis_api.apps.cash_assist_datahub.fixtures as ca_fixtures
+
+from hct_mis_api.apps.household.models import Individual
 from hct_mis_api.apps.household.models import Household
-from hct_mis_api.apps.household.fixtures import IndividualFactory
-from hct_mis_api.apps.household.fixtures import HouseholdFactory
 from hct_mis_api.apps.program.fixtures import CashPlanFactory
 from hct_mis_api.apps.program.models import CashPlan
 from hct_mis_api.apps.payment.models import ServiceProvider
-from hct_mis_api.apps.payment.fixtures import ServiceProviderFactory
-from hct_mis_api.apps.cash_assist_datahub.fixtures import PaymentRecordFactory
 from hct_mis_api.apps.cash_assist_datahub.models import Session, PaymentRecord
 from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.household.fixtures import create_household
@@ -16,7 +16,7 @@ from hct_mis_api.apps.household.fixtures import create_household
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        business_areas = BusinessArea.objects.all()
+        business_areas = BusinessArea.objects.all()[:1]
         print(f"Business areas present: {len(business_areas)}")
 
         Session.objects.all().delete()
@@ -29,17 +29,37 @@ class Command(BaseCommand):
 
         session = Session.objects.order_by("?").first()
         ServiceProvider.objects.all().delete()
-        ServiceProviderFactory.create(ca_id=service_provider_ca_id)
+        payment_fixtures.ServiceProviderFactory.create(ca_id=service_provider_ca_id)
         print(f"Service providers: {ServiceProvider.objects.count()}")
 
         PaymentRecord.objects.all().delete()
+        Household.objects.all().delete()
+        Individual.objects.all().delete()
         for session in Session.objects.all():
-            PaymentRecordFactory.create(
+            household, _ = create_household(
+                {
+                    "size": 1,
+                    "residence_status": "HOST",
+                    "business_area": BusinessArea.objects.get(code=session.business_area),
+                    "total_cash_received": None,
+                    "total_cash_received_usd": None,
+                },
+            )
+            ca_fixtures.PaymentRecordFactory.create(
                 session=session,
                 service_provider_ca_id=service_provider_ca_id,
                 cash_plan_ca_id=cash_plan_ca_id,
+                household_mis_id=household.id,
             )
+            payment_fixtures.PaymentRecordFactory.create(
+                household=household,
+                delivered_quantity=1000,
+                delivered_quantity_usd=2000,
+            )
+
         print(f"Payment records: {PaymentRecord.objects.count()}")
+        print(f"Households: {Household.objects.count()}")
+        print(f"Individuals: {Individual.objects.count()}")
 
         CashPlan.objects.all().delete()
         CashPlanFactory.create(ca_id=cash_plan_ca_id)
