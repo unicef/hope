5293c87c11e79b98ff49092e3ac54b7b23b56382
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-07-25 10:55:54 +0200

    create target population step

--- backend/hct_mis_api/apps/payment/tests/test_recalculating_household_cash_received.py
+++ backend/hct_mis_api/apps/payment/tests/test_recalculating_household_cash_received.py
@@ -19,6 +19,7 @@ class TestRecalculatingCash(APITestCase):
     mutation CreateProgram($programData: CreateProgramInput!) {
       createProgram(programData: $programData) {
         program {
+          id
           name
           status
           startDate
@@ -37,6 +38,40 @@ class TestRecalculatingCash(APITestCase):
     }
     """
 
+    CREATE_TARGET_POPULATION_MUTATION = """
+    mutation CreateTargetPopulation($createTargetPopulationInput: CreateTargetPopulationInput!) {
+      createTargetPopulation(input: $createTargetPopulationInput) {
+        targetPopulation{
+          id
+          name
+          status
+          candidateListTotalHouseholds
+          candidateListTotalIndividuals
+            candidateListTargetingCriteria{
+            rules{
+              filters{
+                comparisionMethod
+                fieldName
+                arguments
+                isFlexField
+              }
+            }
+          }
+        }
+      }
+    }
+    """
+
+    UPDATE_PROGRAM_MUTATION = """
+    mutation UpdateProgram($programData: UpdateProgramInput) {
+      updateProgram(programData: $programData) {
+        program {
+          status
+        }
+      }
+    }
+    """
+
     @classmethod
     def setUpTestData(cls):
         create_afghanistan()
@@ -46,7 +81,7 @@ class TestRecalculatingCash(APITestCase):
         cls.create_program_mutation_variables = {
             "programData": {
                 "administrativeAreasOfImplementation": "",
-                "budget": "0.00",
+                "budget": "10000.00",
                 "businessAreaSlug": "afghanistan",
                 "cashPlus": True,
                 "description": "",
@@ -61,20 +96,70 @@ class TestRecalculatingCash(APITestCase):
             }
         }
 
+        cls.create_target_population_mutation_variables = lambda program_id: {
+            "createTargetPopulationInput": {
+                "programId": program_id,
+                "name": "asdasd",
+                "excludedIds": "",
+                "exclusionReason": "",
+                "businessAreaSlug": "afghanistan",
+                "targetingCriteria": {
+                    "rules": [
+                        {
+                            "filters": [
+                                {
+                                    "comparisionMethod": "LESS_THAN",
+                                    "arguments": [100],
+                                    "fieldName": "number_of_children",
+                                    "isFlexField": False,
+                                }
+                            ],
+                            "individualsFiltersBlocks": [],
+                        }
+                    ]
+                },
+            }
+        }
+
+        cls.update_program_mutation_variables = lambda program_id: {
+            "programData": {
+                "id": program_id,
+                "status": "ACTIVE",
+            }
+        }
+
+    def send_graphql_request(self, **kwargs):
+        response = self.graphql_request(**kwargs)
+        self.assertTrue("data" in response)
+        return response
+
     def create_program(self):
-        response = self.graphql_request(
+        return self.send_graphql_request(
             request_string=self.CREATE_PROGRAM_MUTATION,
             context={"user": self.user},
             variables=self.create_program_mutation_variables,
         )
-        self.assertTrue("data" in response)
-        return response
 
-    def create_targeting(self):
-        pass
+    def activate_program(self, program_id):
+        return self.send_graphql_request(
+            request_string=self.UPDATE_PROGRAM_MUTATION,
+            context={"user": self.user},
+            variables=self.update_program_mutation_variables(program_id),
+        )
+
+    def create_target_population(self, program_id):
+        return self.send_graphql_request(
+            request_string=self.CREATE_TARGET_POPULATION_MUTATION,
+            context={"user": self.user},
+            variables=self.create_target_population_mutation_variables(program_id),
+        )
 
     def test_household_cash_received_update(self):
-        self.create_user_role_with_permissions(self.user, [Permissions.PROGRAMME_CREATE], self.business_area)
+        self.create_user_role_with_permissions(
+            self.user,
+            [Permissions.PROGRAMME_CREATE, Permissions.TARGETING_CREATE, Permissions.PROGRAMME_ACTIVATE],
+            self.business_area,
+        )
         household, _ = create_household(
             {
                 "size": 1,
@@ -86,8 +171,11 @@ class TestRecalculatingCash(APITestCase):
         )
         Session.objects.create(business_area=self.business_area.code, status=Session.STATUS_READY)
 
-        self.create_program()
-        self.create_targeting()
+        program_response = self.create_program()
+        program_id = program_response["data"]["createProgram"]["program"]["id"]
+
+        self.activate_program(program_id)
+        self.create_target_population(program_id)
 
         self.assertIsNone(household.total_cash_received)
         self.assertIsNone(household.total_cash_received_usd)
