529fa5be0ce3ff7989af84fa95197239047d78f7
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-07-27 15:16:43 +0200

    fix create payment plan mutation

--- frontend/src/apollo/mutations/paymentmodule/CreatePaymentPlan.ts
+++ frontend/src/apollo/mutations/paymentmodule/CreatePaymentPlan.ts
@@ -1,28 +1,11 @@
 import { gql } from 'apollo-boost';
 
 export const CREATE_PAYMENT_PLAN = gql`
-  mutation CreatePaymentPlan(
-    $businessAreaSlug: String!
-    $targetingId: ID!
-    $startDate: Date!
-    $endDate: Date!
-    $dispersionStartDate: Date!
-    $dispersionEndDate: Date!
-    $currency: String!
-  ) {
-    createPaymentPlan(
-      businessAreaSlug: $businessAreaSlug
-      targetingId: $targetingId
-      startDate: $startDate
-      endDate: $endDate
-      dispersionStartDate: $dispersionStartDate
-      dispersionEndDate: $dispersionEndDate
-      currency: $currency
-    ) {
+  mutation CreatePaymentPlan($input: CreatePaymentPlanInput!) {
+    createPaymentPlan(input: $input) {
       paymentPlan {
         id
       }
-      validationErrors
     }
   }
 `;
