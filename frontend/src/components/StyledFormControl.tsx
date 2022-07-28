c5f6c9ed176e54ccd43444012d299ad42aa50045
Varun Kumar Gande <babluvarun@gmail.com>
2022-07-28 12:25:08 +0530

    ui enhancement and search changes

--- frontend/src/components/StyledFormControl.tsx
+++ frontend/src/components/StyledFormControl.tsx
@@ -5,4 +5,7 @@ export const StyledFormControl = styled(FormControl)`
   width: 260px;
   color: #5f6368;
   border-bottom: 0;
+  .MuiOutlinedInput-root {
+    border-radius: ${(props) => props.borderRadius || '4px'};
+  }
 `;
