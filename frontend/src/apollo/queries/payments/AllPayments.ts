42c818042895ad4daffcf21a4ad8d45344439902
Vinay Kumar Sharma <vinay.sharma@tivix.com>
2022-08-01 15:54:05 +0530

    query fix

--- frontend/src/apollo/queries/payments/AllPayments.ts
+++ frontend/src/apollo/queries/payments/AllPayments.ts
@@ -1,18 +1,22 @@
 import { gql } from 'apollo-boost';
 
 export const PAYMENT_LIST_QUERY = gql`
-  query AllPayments($paymentPlanId: String!) {
-    payment(id: $id) {
-      id
-      household {
-        id
-        size
-        admin2 {
-          name
+  query AllPayments ($paymentPlanId: String!) {
+    allPayments (paymentPlanId: $paymentPlanId) {
+      edges {
+        node {
+          id
+          household {
+            id
+            size
+            admin2 {
+              name
+            }
+          }
+          status
+          currency
         }
       }
-      status
-      currency
     }
   }
 `;
