b7dd57e1d3d6895069aaa4c4995abce9aa11db60
Bartosz Woźniak <91740028+bwozniaktivix@users.noreply.github.com>
2022-08-02 20:26:23 +0200

    Improve fixing grievance tickets script (#1636)

--- backend/hct_mis_api/apps/grievance/tests/test_fixing_grievance_tickets.py
+++ backend/hct_mis_api/apps/grievance/tests/test_fixing_grievance_tickets.py
@@ -116,3 +116,49 @@ class TestFixingGrievanceTickets(APITestCase):
             ticket.individual_data_update_ticket_details.individual_data["disability"]["value"],
             new_value,
         )
+
+    def test_skipping_when_ind_data_update_ticket_details_does_not_exist(self):
+        self.user = UserFactory.create()
+        self.business_area = BusinessAreaFactory(
+            code="0060",
+            name="Afghanistan",
+            long_name="THE ISLAMIC REPUBLIC OF AFGHANISTAN",
+            region_code="64",
+            region_name="SAR",
+            slug="afghanistan",
+            has_data_sharing_agreement=True,
+        )
+        self.registration_data_import = RegistrationDataImportFactory(business_area=self.business_area)
+
+        (_, individuals) = create_household_and_individuals(
+            household_data={
+                "registration_data_import": self.registration_data_import,
+                "business_area": self.business_area,
+            },
+            individuals_data=[
+                {
+                    "registration_data_import": self.registration_data_import,
+                    "given_name": "Test",
+                    "full_name": "Test Testowski",
+                    "middle_name": "",
+                    "family_name": "Testowski",
+                    "phone_no": "123-123-123",
+                    "phone_no_alternative": "",
+                    "relationship": HEAD,
+                    "sex": MALE,
+                    "birth_date": "1955-09-07",
+                },
+            ],
+        )
+        self.individual = individuals[0]
+
+        self.assertEqual(GrievanceTicket.objects.count(), 0)
+        GrievanceTicketFactory(
+            business_area=self.business_area,
+            category=GrievanceTicket.CATEGORY_DATA_CHANGE,
+            issue_type=GrievanceTicket.ISSUE_TYPE_DATA_CHANGE_ADD_INDIVIDUAL,
+        )
+        self.assertEqual(GrievanceTicket.objects.count(), 1)
+
+        call_command("fix_grievance_tickets")
+        # didn't throw, so it skipped ticket with not existing TicketIndividualDataUpdateDetailsFactory
