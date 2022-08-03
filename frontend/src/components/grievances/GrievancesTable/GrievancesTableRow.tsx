6155ec12deb2c917e81e94b0dfbe1db86c196db8
Varun Kumar Gande <babluvarun@gmail.com>
2022-08-03 14:37:33 +0530

    rounding to zero

--- frontend/src/components/grievances/GrievancesTable/GrievancesTableRow.tsx
+++ frontend/src/components/grievances/GrievancesTable/GrievancesTableRow.tsx
@@ -170,6 +170,7 @@ export function GrievancesTableRow({
             ticket.updatedAt
             ? ticket.updatedAt
             : null,
+          false
         )}
       </TableCell>
     </ClickableTableRow>
