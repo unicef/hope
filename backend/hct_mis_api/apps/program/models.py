073da5501b22f4218dec325f9f5737c0b5c298fa
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-08-09 14:56:29 +0200

    Add test

--- backend/hct_mis_api/apps/program/models.py
+++ backend/hct_mis_api/apps/program/models.py
@@ -17,9 +17,6 @@ from model_utils.models import SoftDeletableModel
 
 from hct_mis_api.apps.activity_log.utils import create_mapping_dict
 from hct_mis_api.apps.payment.models import CashPlanPaymentVerification, PaymentRecord
-from hct_mis_api.apps.payment.tasks.CheckRapidProVerificationTask import (
-    does_payment_record_have_right_hoh_phone_number,
-)
 from hct_mis_api.apps.utils.models import (
     AbstractSyncable,
     ConcurrencyModel,
