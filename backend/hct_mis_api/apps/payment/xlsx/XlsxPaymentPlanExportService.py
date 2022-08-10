a691dec67a47d3ae9281bf67875ede6ea990059f
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-08-10 10:37:29 +0200

    small fixes & add migrations

--- backend/hct_mis_api/apps/payment/xlsx/XlsxPaymentPlanExportService.py
+++ backend/hct_mis_api/apps/payment/xlsx/XlsxPaymentPlanExportService.py
@@ -1,24 +1,16 @@
-import logging
-import openpyxl
-
-from openpyxl.utils import get_column_letter
 from tempfile import NamedTemporaryFile
 
 from django.contrib.admin.options import get_content_type_for_model
 from django.core.files import File
-from django.core.mail import EmailMultiAlternatives
-from django.conf import settings
-from django.template.loader import render_to_string
 from django.urls import reverse
 
-from apps.core.utils import encode_id_base64
+from hct_mis_api.apps.payment.models import Payment
+from hct_mis_api.apps.payment.xlsx.BaseXlsxExportService import XlsxBaseExportService
 from hct_mis_api.apps.core.models import XLSXFileTemp
+from hct_mis_api.apps.core.utils import encode_id_base64
 
 
-logger = logging.getLogger(__name__)
-
-
-class XlsxPaymentPlanExportService:
+class XlsxPaymentPlanExportService(XlsxBaseExportService):
     TITLE = "Payment Plan - Payment List"
     HEADERS = (
         "payment_id",  # 0
@@ -29,39 +21,28 @@ class XlsxPaymentPlanExportService:
         "payment_channel",  # 5
         "fsp",
         "currency",
-        "entitlement",  # 7
+        "entitlement",  # 8
     )
 
     def __init__(self, payment_plan):
         self.payment_plan = payment_plan
         self.payment_list = payment_plan.all_active_payments
 
-    def _create_workbook(self) -> openpyxl.Workbook:
-        wb = openpyxl.Workbook()
-        ws_payments = wb.active
-        ws_payments.title = XlsxPaymentPlanExportService.TITLE
-        self.wb = wb
-        self.ws_payments = ws_payments
-        return wb
-
-    def _add_headers(self):
-        self.ws_payments.append(XlsxPaymentPlanExportService.HEADERS)
-
-    def _add_payment_row(self, payment):
+    def _add_payment_row(self, payment: Payment):
         household = payment.household
 
         payment_row = (
-            str(payment.id),
-            str(household.unicef_id) if household.unicef_id else "",
+            str(getattr(payment, "unicef_id", payment.id)),
+            str(getattr(household, "unicef_id", "")),
             household.size,
             str(household.admin2.title) if household.admin2 else "",
-            str(payment.head_of_household.full_name) if payment.head_of_household else "",  # TODO collector
-            "TODO payment_channel",  # str(payment.payment_channel)
+            str(payment.head_of_household.full_name) if payment.head_of_household else "",
+            "TODO payment_channel",  # str(collector.payment_channel)
             str(payment.financial_service_provider.name) if payment.financial_service_provider else "",
             payment.entitlement_quantity,
             payment.entitlement_quantity_usd,
         )
-        self.ws_payments.append(payment_row)
+        self.ws_export_list.append(payment_row)
 
     def _add_payment_list(self):
         for payment in self.payment_list:
@@ -71,34 +52,8 @@ class XlsxPaymentPlanExportService:
         self._create_workbook()
         self._add_headers()
         self._add_payment_list()
-        self._adjust_column_width_from_col(self.ws_payments, 2, 1, 0)
+        self._adjust_column_width_from_col(self.ws_export_list, 2, 1, 0)
         return self.wb
-
-    def generate_file(self, filename):
-        self.generate_workbook()
-        self.wb.save(filename=filename)
-
-    def _adjust_column_width_from_col(self, ws, min_row, min_col, max_col):
-        column_widths = []
-
-        for i, col in enumerate(ws.iter_cols(min_col=min_col, max_col=max_col, min_row=min_row)):
-
-            for cell in col:
-                value = cell.value
-                if value is not None:
-
-                    if isinstance(value, str) is False:
-                        value = str(value)
-
-                    try:
-                        column_widths[i] = max(column_widths[i], len(value))
-                    except IndexError:
-                        column_widths.append(len(value))
-
-        for i, width in enumerate(column_widths):
-            col_name = get_column_letter(min_col + i)
-            value = column_widths[i] + 2
-            ws.column_dimensions[col_name].width = value
 
     def save_xlsx_file(self, user):
         filename = f"payment_plan_payment_list_{self.payment_plan.unicef_id}.xlsx"
@@ -114,14 +69,12 @@ class XlsxPaymentPlanExportService:
             tmp.seek(0)
             xlsx_obj.file.save(filename, File(tmp))
 
-    @staticmethod
-    def send_email(user, cash_plan_payment_verification_id):
-        protocol = "http" if settings.IS_DEV else "https"
-        payment_verification_id = encode_id_base64(cash_plan_payment_verification_id, "PaymentPlan")
-        api = reverse("download-payment-plan-payment-list", args=[payment_verification_id])
-        link = f"{protocol}://{settings.FRONTEND_HOST}{api}"
+    def get_context(self, user):
+        payment_verification_id = encode_id_base64(self.payment_plan.id, "PaymentPlan")
+        link = self.get_link(reverse("download-payment-plan-payment-list", args=[payment_verification_id]))
 
-        msg = "Verification Plan xlsx file was generated and below You have the link to download this file."
+        msg = "Payment Plan Payment List xlsx file was generated and below You have the link to download this file."
+
         context = {
             "first_name": user.first_name,
             "last_name": user.last_name,
@@ -130,16 +83,5 @@ class XlsxPaymentPlanExportService:
             "link": link,
             "title": "Payment Plan Payment List XLSX file generated",
         }
-        text_body = render_to_string("payment/xlsx_file_generated_email.txt", context=context)
-        html_body = render_to_string("payment/xlsx_file_generated_email.html", context=context)
 
-        email = EmailMultiAlternatives(
-            subject="Verification Plan XLSX file generated",
-            from_email=settings.EMAIL_HOST_USER,
-            to=[context["email"]],
-            body=text_body,
-        )
-        email.attach_alternative(html_body, "text/html")
-        result = email.send()
-        if not result:
-            logger.error(f"Email couldn't be send to {context['email']}")
+        return context
--- backend/hct_mis_api/apps/steficon/filters.py
+++ backend/hct_mis_api/apps/steficon/filters.py
@@ -1,9 +1,11 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, CharFilter, ChoiceFilter
 
 from hct_mis_api.apps.steficon.models import Rule
 
 
 class SteficonRuleFilter(FilterSet):
+    type = ChoiceFilter(choices=Rule.TYPE_CHOICES, required=True, field_name="type")
+
     class Meta:
         fields = ("enabled", "deprecated", "type")
         model = Rule
