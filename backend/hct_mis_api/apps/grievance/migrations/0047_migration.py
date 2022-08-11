d95ac1d164ff95e2e8dee19e3d3df08156cf98ce
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-08-11 11:32:19 +0200

    Edit existing migration, so it takes only the necessary fields for the procedure

--- backend/hct_mis_api/apps/grievance/migrations/0047_migration.py
+++ backend/hct_mis_api/apps/grievance/migrations/0047_migration.py
@@ -5,11 +5,12 @@ from django.db import migrations
 
 def set_household_unicef_id(apps, schema_editor):
     from hct_mis_api.apps.grievance.models import GrievanceTicket
+
     start = 10_000
     grievance_tickets = []
-    i, count = 0, GrievanceTicket.objects.all().count() // start + 1
+    i, count = 0, GrievanceTicket.objects.count() // start + 1
     while i <= count:
-        batch = GrievanceTicket.objects.all().order_by("created_at")[start * i: start * (i + 1)]
+        batch = GrievanceTicket.objects.only("issue_type").order_by("created_at")[start * i : start * (i + 1)]
         for grv in batch:
             ticket_details = getattr(grv, "ticket_details", None)
             if ticket_details:
@@ -26,9 +27,9 @@ def delete_household_unicef_id(apps, schema_editor):
     GrievanceTicket = apps.get_model("grievance", "GrievanceTicket")
     start = 10_000
     grievance_tickets = []
-    i, count = 0, GrievanceTicket.objects.all().count() // start + 1
+    i, count = 0, GrievanceTicket.objects.count() // start + 1
     while i <= count:
-        batch = GrievanceTicket.objects.all().order_by("created_at")[start * i: start * (i + 1)]
+        batch = GrievanceTicket.objects.all().order_by("created_at")[start * i : start * (i + 1)]
         for grv in batch:
             grv.household_unicef_id = None
             grievance_tickets.append(grv)
@@ -40,10 +41,8 @@ def delete_household_unicef_id(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('grievance', '0046_migration'),
-        ('household', '0111_migration'),
+        ("grievance", "0046_migration"),
+        ("household", "0111_migration"),
     ]
 
-    operations = [
-        migrations.RunPython(set_household_unicef_id, delete_household_unicef_id)
-    ]
+    operations = [migrations.RunPython(set_household_unicef_id, delete_household_unicef_id)]
