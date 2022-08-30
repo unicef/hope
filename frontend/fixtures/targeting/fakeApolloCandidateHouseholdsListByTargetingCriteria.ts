ccb87e17a7b8a8034edd5d7ec95c91737b1992e7
Jan Romaniak <jan.romaniak@tivix.com>
2022-08-30 14:29:17 +0200

    fix frontend tests

--- frontend/fixtures/targeting/fakeApolloCandidateHouseholdsListByTargetingCriteria.ts
+++ frontend/fixtures/targeting/fakeApolloCandidateHouseholdsListByTargetingCriteria.ts
@@ -13,7 +13,7 @@ export const fakeApolloTargetPopulationHouseholds = [
     },
     result: {
       data: {
-        candidateHouseholdsListByTargetingCriteria: {
+        targetPopulationHouseholds: {
           edges: [
             {
               node: {
