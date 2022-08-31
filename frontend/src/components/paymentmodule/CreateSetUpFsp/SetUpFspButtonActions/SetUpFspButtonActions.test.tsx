db8a33b7bb3bca9990637549de59f7f2e4f24764
Vinay Kumar Sharma <vinay.sharma@tivix.com>
2022-08-31 16:16:06 +0530

    more tests added for payment module components

--- frontend/src/components/paymentmodule/CreateSetUpFsp/SetUpFspButtonActions/SetUpFspButtonActions.test.tsx
+++ frontend/src/components/paymentmodule/CreateSetUpFsp/SetUpFspButtonActions/SetUpFspButtonActions.test.tsx
@@ -1,15 +1,16 @@
-// import { act } from '@testing-library/react';
+import { act } from '@testing-library/react';
 import React from 'react';
-// import wait from 'waait';
+import wait from 'waait';
 import { render } from '../../../../testUtils/testUtils';
 import { SetUpFspButtonActions } from './SetUpFspButtonActions';
 
 
 describe('components/paymentmodule/CreateSetUpFsp/SetUpFspButtonActions', () => {
-  it('should render', () => {
+  it('should render', async () => {
     const step = 0;
     const setStep = jest.fn();
-    const submitForm = (_values) => Promise.resolve();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const submitForm = (_values): Promise<void> => Promise.resolve();
     const { container } = render(
       <SetUpFspButtonActions
         step={step}
@@ -18,7 +19,7 @@ describe('components/paymentmodule/CreateSetUpFsp/SetUpFspButtonActions', () => 
         paymentPlanId='asdjkfhsakdjfsd76asdf0sdf=='
         handleBackStep={setStep} />,
     );
-    // await act(() => wait(0)); // wait for response
+    await act(() => wait(0)); // wait for response
     expect(container).toMatchSnapshot();
   });
 });
