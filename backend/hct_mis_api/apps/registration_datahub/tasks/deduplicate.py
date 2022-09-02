b98fddf31605da8894729a09b0e65f9b4728e7f1
Jan Romaniak <jan.romaniak@tivix.com>
2022-09-02 11:49:36 +0200

    adjusting admin

--- backend/hct_mis_api/apps/registration_datahub/tasks/deduplicate.py
+++ backend/hct_mis_api/apps/registration_datahub/tasks/deduplicate.py
@@ -930,14 +930,12 @@ class DeduplicateTask:
                 return None
         household = main_individual.household
         admin_level_2 = household.admin2 if household else None
-        admin_level_2_new = household.admin2_new if household else None
         area = household.village if household else ""
 
         ticket = GrievanceTicket(
             category=GrievanceTicket.CATEGORY_NEEDS_ADJUDICATION,
             business_area=business_area,
             admin2=admin_level_2,
-            admin2_new=admin_level_2_new,
             area=area,
             registration_data_import=registration_data_import,
         )
