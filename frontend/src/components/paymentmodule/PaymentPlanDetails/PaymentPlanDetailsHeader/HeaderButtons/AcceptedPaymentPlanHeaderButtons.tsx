c1c874f74ff1e5c27549db17f07546bb062f4585
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-09-06 13:35:51 +0200

    add mutations

--- frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentPlanDetailsHeader/HeaderButtons/AcceptedPaymentPlanHeaderButtons.tsx
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentPlanDetailsHeader/HeaderButtons/AcceptedPaymentPlanHeaderButtons.tsx
@@ -1,9 +1,15 @@
 import { Box, Button } from '@material-ui/core';
 import { GetApp } from '@material-ui/icons';
 import styled from 'styled-components';
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { PaymentPlanQuery } from '../../../../../__generated__/graphql';
+import {
+  PaymentPlanQuery,
+  useExportXlsxPpListPerFspMutation,
+} from '../../../../../__generated__/graphql';
+import { LoadingButton } from '../../../../core/LoadingButton';
+import { useSnackbar } from '../../../../../hooks/useSnackBar';
+import { ImportXlsxPaymentPlanPaymentListPerFsp } from '../../ImportXlsxPaymentPlanPaymentListPerFsp';
 
 const DownloadIcon = styled(GetApp)`
   color: #043f91;
@@ -21,33 +27,58 @@ export const AcceptedPaymentPlanHeaderButtons = ({
   paymentPlan,
 }: AcceptedPaymentPlanHeaderButtonsProps): React.ReactElement => {
   const { t } = useTranslation();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [openApprove, setOpenApprove] = useState(false);
+  const { showMessage } = useSnackbar();
+  const [
+    mutateExport,
+    { loading: loadingExport },
+  ] = useExportXlsxPpListPerFspMutation();
 
   return (
     <Box display='flex' alignItems='center'>
       {canDownloadXlsx && (
-        <Box m={2}>
-          <Button
-            color='primary'
-            startIcon={<DownloadIcon />}
-            component='a'
-            download
-            href={`/api/download-payment-plan-payment-list-per-fsp/${paymentPlan.id}`}
-          >
-            {t('Download XLSX')}
-          </Button>
-        </Box>
+        <>
+          {!paymentPlan.hasPaymentListPerFspZipFile ? (
+            <Box p={2}>
+              <LoadingButton
+                loading={loadingExport}
+                disabled={loadingExport}
+                color='primary'
+                variant='outlined'
+                startIcon={<GetApp />}
+                onClick={async () => {
+                  try {
+                    await mutateExport({
+                      variables: {
+                        paymentPlanId: paymentPlan.id,
+                      },
+                    });
+                    showMessage(t('Exporting XLSX started'));
+                  } catch (e) {
+                    e.graphQLErrors.map((x) => showMessage(x.message));
+                  }
+                }}
+              >
+                {t('Export Xlsx')}
+              </LoadingButton>
+            </Box>
+          ) : (
+            <Box m={2}>
+              <Button
+                color='primary'
+                startIcon={<DownloadIcon />}
+                component='a'
+                download
+                href={`/api/download-payment-plan-payment-list-per-fsp/${paymentPlan.id}`}
+              >
+                {t('Download XLSX')}
+              </Button>
+            </Box>
+          )}
+        </>
       )}
       {canSendToFsp && (
         <Box m={2}>
-          <Button
-            variant='contained'
-            color='primary'
-            onClick={() => setOpenApprove(true)}
-          >
-            {t('Send to Fsp')}
-          </Button>
+          <ImportXlsxPaymentPlanPaymentListPerFsp paymentPlan={paymentPlan} />
         </Box>
       )}
     </Box>
