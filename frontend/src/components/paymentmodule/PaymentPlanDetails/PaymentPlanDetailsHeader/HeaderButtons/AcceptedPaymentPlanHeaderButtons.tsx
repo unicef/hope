c4cb6fc2ddefc3dee433a58e94e613fb774ba99e
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-09-06 10:27:30 +0200

    add import error, add download xlsx button link

--- frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentPlanDetailsHeader/HeaderButtons/AcceptedPaymentPlanHeaderButtons.tsx
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentPlanDetailsHeader/HeaderButtons/AcceptedPaymentPlanHeaderButtons.tsx
@@ -1,15 +1,24 @@
 import { Box, Button } from '@material-ui/core';
+import { GetApp } from '@material-ui/icons';
+import styled from 'styled-components';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { PaymentPlanQuery } from '../../../../../__generated__/graphql';
+
+const DownloadIcon = styled(GetApp)`
+  color: #043f91;
+`;
 
 export interface AcceptedPaymentPlanHeaderButtonsProps {
   canDownloadXlsx: boolean;
   canSendToFsp: boolean;
+  paymentPlan: PaymentPlanQuery['paymentPlan'];
 }
 
 export const AcceptedPaymentPlanHeaderButtons = ({
   canDownloadXlsx,
   canSendToFsp,
+  paymentPlan,
 }: AcceptedPaymentPlanHeaderButtonsProps): React.ReactElement => {
   const { t } = useTranslation();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -20,9 +29,11 @@ export const AcceptedPaymentPlanHeaderButtons = ({
       {canDownloadXlsx && (
         <Box m={2}>
           <Button
-            variant='contained'
             color='primary'
-            onClick={() => setOpenApprove(true)}
+            startIcon={<DownloadIcon />}
+            component='a'
+            download
+            href={`/api/download-payment-plan-payment-list-per-fsp/${paymentPlan.id}`}
           >
             {t('Download XLSX')}
           </Button>
