55dc24f949e155a588a1db7a77f451393139208e
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-09-08 09:25:58 +0200

    Fix refreshing tp stats

--- backend/hct_mis_api/apps/targeting/models.py
+++ backend/hct_mis_api/apps/targeting/models.py
@@ -284,9 +284,10 @@ class TargetPopulation(SoftDeletableModel, TimeStampedUUIDModel, ConcurrencyMode
         return queryset.distinct()
 
     def refresh_stats(self):
+        households = self.household_list.all()
         delta18 = relativedelta(years=+18)
         date18ago = timezone.now() - delta18
-        targeted_individuals = Individual.objects.filter(household__in=self.household_list).aggregate(
+        targeted_individuals = Individual.objects.filter(household__in=households).aggregate(
             child_male_count=Count("id", distinct=True, filter=Q(birth_date__gt=date18ago, sex=MALE)),
             child_female_count=Count("id", distinct=True, filter=Q(birth_date__gt=date18ago, sex=FEMALE)),
             adult_male_count=Count("id", distinct=True, filter=Q(birth_date__lte=date18ago, sex=MALE)),
@@ -296,7 +297,7 @@ class TargetPopulation(SoftDeletableModel, TimeStampedUUIDModel, ConcurrencyMode
         self.child_female_count = targeted_individuals.get("child_female_count")
         self.adult_male_count = targeted_individuals.get("adult_male_count")
         self.adult_female_count = targeted_individuals.get("adult_female_count")
-        self.total_households_count = self.household_list.count()
+        self.total_households_count = households.count()
         self.total_individuals_count = (
             targeted_individuals.get("child_male_count")
             + targeted_individuals.get("child_female_count")
