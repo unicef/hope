bb84191251de0c175d1c78202d29992b64b5cceb
Jan Romaniak <jan.romaniak@tivix.com>
2022-09-13 10:48:01 +0200

    fixed redirect

---
+++ backend/hct_mis_api/apps/core/permissions_views_mixins.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.core.exceptions import PermissionDenied
+
+from hct_mis_api.apps.account.permissions import Permissions
+
+
+class ViewPermissionsMixinBase(AccessMixin):
+    def has_permissions(self):
+        return NotImplemented
+
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        if not self.has_permissions():
+            raise PermissionDenied
+        return super(ViewPermissionsMixinBase, self).dispatch(request, *args, **kwargs)
+
+
+class UploadFilePermissionMixin(ViewPermissionsMixinBase):
+    def has_permissions(self):
+        roles = self.request.user.user_roles.all()
+
+        return any(
+            self.request.user.has_permission(
+                Permissions.UPLOAD_STORAGE_FILE.name, role.business_area
+            ) for role in roles
+        )
