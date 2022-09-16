f40f5a0b95a947712d91beba320e9708ce073eb3
Vinay Kumar Sharma <vinay.sharma@tivix.com>
2022-09-16 13:51:00 +0530

    optimization and fields update

--- backend/hct_mis_api/apps/communication/filters.py
+++ backend/hct_mis_api/apps/communication/filters.py
@@ -1,8 +1,8 @@
 from django.db.models.functions import Lower
 
-from django_filters import FilterSet, UUIDFilter
+from django_filters import CharFilter, FilterSet, UUIDFilter
 
-from hct_mis_api.apps.core.utils import CustomOrderingFilter
+from hct_mis_api.apps.core.utils import CustomOrderingFilter, decode_id_string
 from hct_mis_api.apps.household.models import Household
 
 from .models import Message
@@ -22,10 +22,16 @@ class MessagesFilter(FilterSet):
 
 
 class MessageRecipientsMapFilter(FilterSet):
-    message_id = UUIDFilter(field_name="messages__id", required=True)
-    recipient_id = UUIDFilter(field_name="id")
-    full_name = UUIDFilter(field_name="head_of_household__full_name", lookup_expr=["exact", "icontains", "istartswith"])
-    phone_no = UUIDFilter(field_name="head_of_household__phone_no", lookup_expr=["exact", "icontains", "istartswith"])
+    message_id = CharFilter(method="message_id_filter", required=True)
+    recipient_id = CharFilter(method="recipient_id_filter")
+    full_name = CharFilter(field_name="head_of_household__full_name", lookup_expr=["exact", "icontains", "istartswith"])
+    phone_no = CharFilter(field_name="head_of_household__phone_no", lookup_expr=["exact", "icontains", "istartswith"])
+
+    def message_id_filter(self, queryset, name, value):
+        return queryset.filter(message_id=decode_id_string(value))
+
+    def recipient_id_filter(self, queryset, name, value):
+        return queryset.filter(id=decode_id_string(value))
 
     class Meta:
         model = Household
@@ -33,12 +39,11 @@ class MessageRecipientsMapFilter(FilterSet):
 
     order_by = CustomOrderingFilter(
         fields=(
-            "residence_status",
             "id",
             Lower("head_of_household__first_name"),
             "size",
             Lower("admin2__name"),
             "residence_status",
-            "first_registration_date",
+            "head_of_household__first_registration_date",
         )
     )
