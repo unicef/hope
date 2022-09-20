99913ada31550ab988ff0409f33dd7ff1929b18e
saxix <s.apostolico@gmail.com>
2022-09-20 21:34:49 +0200

    API upload pictures

--- backend/hct_mis_api/api/tests.py
+++ backend/hct_mis_api/api/tests.py
@@ -1,6 +1,9 @@
+import base64
+from pathlib import Path
 from unittest import TestCase
 from unittest.mock import Mock
 
+from PIL.Image import Image
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -52,7 +55,6 @@ class UploadRDITests(APITestCase):
         super().setUpTestData()
         cls.business_area = BusinessAreaFactory(name="Afghanistan")
         ImportedDocumentType.objects.create(country="AF", type=IDENTIFICATION_TYPE_BIRTH_CERTIFICATE, label="--")
-
         cls.user: User = UserFactory()
         cls.role = RoleFactory(subsystem="API", permissions=[Permissions.API_UPLOAD_RDI])
         cls.user.user_roles.create(role=cls.role, business_area=cls.business_area)
@@ -206,3 +208,66 @@ class UploadRDITests(APITestCase):
         self.assertEqual(len(members), 1)
 
         self.assertTrue(hoh.documents.exists())
+
+    def test_upload_with_document_photo(self):
+        image = Path(__file__).parent / "logo.png"
+        base64_encoded_data = base64.b64encode(image.read_bytes())
+
+        data = {
+            "name": "aaaa",
+            "number_of_households": 1,
+            "number_of_individuals": 1,
+            "households": [
+                {
+                    "residence_status": "",
+                    "village": "village1",
+                    "country": "AF",
+                    "members": [
+                        {
+                            "relationship": HEAD,
+                            "full_name": "Jhon Doe",
+                            "birth_date": "2000-01-01",
+                            "sex": "MALE",
+                            "role": "",
+                            "documents": [
+                                {
+                                    "document_number": 10,
+                                    "image": base64_encoded_data,
+                                    "doc_date": "2010-01-01",
+                                    "country": "AF",
+                                    "type": IDENTIFICATION_TYPE_BIRTH_CERTIFICATE,
+                                }
+                            ],
+                        },
+                        {
+                            "relationship": NON_BENEFICIARY,
+                            "full_name": "Mary Doe",
+                            "birth_date": "2000-01-01",
+                            "role": ROLE_PRIMARY,
+                            "sex": "FEMALE",
+                        },
+                    ],
+                    "collect_individual_data": "FULL",
+                    "size": 1,
+                }
+            ],
+        }
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, str(response.json()))
+        hoh = ImportedIndividual.objects.filter(birth_date="2000-01-01", full_name="Jhon Doe", sex=MALE).first()
+
+        self.assertTrue(hoh)
+        hh: ImportedHousehold = hoh.household
+        self.assertEqual(hoh.household.village, "village1")
+
+        # check collectors
+        self.assertNotEqual(hoh.household.primary_collector, hoh)
+        self.assertIsInstance(hoh.household.primary_collector, ImportedIndividual)
+        self.assertIsNone(hoh.household.alternate_collector)
+        members = hh.individuals.all()
+        self.assertEqual(len(members), 1)
+
+        self.assertTrue(hoh.documents.exists())
+        self.assertTrue(hoh.documents.first().photo)
+        # Image()
+        # hoh.documents.exists()
