295c7a4be29e769fea35dd3820c8bc5929e2e6d8
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-09-20 15:09:57 +0200

    add feedback page (no backend yet)

---
+++ frontend/src/containers/tables/Feedback/FeedbackTableHeadCells.tsx
@@ -0,0 +1,41 @@
+import { HeadCell } from '../../../components/core/Table/EnhancedTableHead';
+import { CommunicationMessageNode } from '../../../__generated__/graphql';
+
+export const headCells: HeadCell<CommunicationMessageNode>[] = [
+  {
+    disablePadding: false,
+    label: 'Feedback ID',
+    id: 'id',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Issue Type',
+    id: 'issueType',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Household ID',
+    id: 'numberOfRecipients',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Linked Grievance',
+    id: 'linkedGrievance',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Created by',
+    id: 'created_by',
+    numeric: false,
+  },
+  {
+    disablePadding: false,
+    label: 'Creation date',
+    id: 'creation_date',
+    numeric: false,
+  },
+];
