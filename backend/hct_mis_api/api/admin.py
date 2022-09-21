1d1aef19925ea1c72eb4663dafb15a077ac762a3
saxix <s.apostolico@gmail.com>
2022-09-21 19:51:58 +0200

    add APIToken admin

---
+++ backend/hct_mis_api/api/admin.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib import admin, messages
+
+from adminfilters.autocomplete import AutoCompleteFilter
+from smart_admin.modeladmin import SmartModelAdmin
+
+from hct_mis_api.api.models import APIToken
+
+
+@admin.register(APIToken)
+class APITokenAdmin(SmartModelAdmin):
+    list_display = ("user", "valid_from", "valid_to")
+    list_filter = (("user", AutoCompleteFilter),)
+    filter_horizontal = ("valid_for",)
+    readonly_fields = ("key",)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("user")
