2d315df3f3221c5e4012fedd75114d94f81b14ee
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-09-21 15:59:54 +0200

    Extend test

--- backend/hct_mis_api/apps/payment/tests/test_verification_plan_status_change_services.py
+++ backend/hct_mis_api/apps/payment/tests/test_verification_plan_status_change_services.py
@@ -34,8 +34,9 @@ class TestPhoneNumberVerification(TestCase):
     def setUpTestData(cls):
         create_afghanistan()
         cls.payment_record_amount = 110
-
         user = UserFactory()
+
+        ###
 
         program = ProgramFactory(business_area=BusinessArea.objects.first())
         program.admin_areas.set(Area.objects.order_by("?")[:3])
@@ -89,6 +90,60 @@ class TestPhoneNumberVerification(TestCase):
         cls.cash_plan = cash_plan
         cls.verification = cash_plan.verifications.first()
 
+        ###
+
+        other_program = ProgramFactory(business_area=BusinessArea.objects.first())
+        other_program.admin_areas.set(Area.objects.order_by("?")[:3])
+        other_targeting_criteria = TargetingCriteriaFactory()
+
+        other_target_population = TargetPopulationFactory(
+            created_by=user,
+            targeting_criteria=other_targeting_criteria,
+            business_area=BusinessArea.objects.first(),
+        )
+        other_cash_plan = CashPlanFactory(
+            program=program,
+            business_area=BusinessArea.objects.first(),
+        )
+        other_cash_plan.save()
+        other_cash_plan_payment_verification = CashPlanPaymentVerificationFactory(
+            status=CashPlanPaymentVerification.STATUS_PENDING,
+            verification_channel=CashPlanPaymentVerification.VERIFICATION_CHANNEL_RAPIDPRO,
+            cash_plan=other_cash_plan,
+        )
+        cls.other_individuals = []
+        for _ in range(cls.payment_record_amount):
+            other_registration_data_import = RegistrationDataImportFactory(
+                imported_by=user, business_area=BusinessArea.objects.first()
+            )
+            other_household, other_individuals = create_household(
+                {
+                    "registration_data_import": other_registration_data_import,
+                    "admin_area": Area.objects.order_by("?").first(),
+                },
+                {"registration_data_import": other_registration_data_import},
+            )
+            cls.other_individuals.append(other_individuals[0])
+
+            other_household.programs.add(program)
+
+            other_payment_record = PaymentRecordFactory(
+                cash_plan=other_cash_plan,
+                household=other_household,
+                head_of_household=other_household.head_of_household,
+                target_population=other_target_population,
+                delivered_quantity_usd=200,
+            )
+
+            PaymentVerificationFactory(
+                cash_plan_payment_verification=other_cash_plan_payment_verification,
+                payment_record=other_payment_record,
+                status=PaymentVerification.STATUS_PENDING,
+            )
+            EntitlementCardFactory(household=other_household)
+        cls.other_cash_plan = other_cash_plan
+        cls.other_verification = other_cash_plan.verifications.first()
+
     def test_failing_rapid_pro_during_cash_plan_payment_verification(self):
         self.assertEqual(self.verification.status, PaymentVerification.STATUS_PENDING)
         self.assertIsNone(self.verification.error)
@@ -118,20 +173,48 @@ class TestPhoneNumberVerification(TestCase):
         self.assertIsNotNone(self.verification.error)
 
         self.assertEqual(
-            PaymentVerification.objects.filter(status=PaymentVerification.STATUS_PENDING).count(),
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.verification, status=PaymentVerification.STATUS_PENDING
+            ).count(),
             self.payment_record_amount,
         )
         self.assertEqual(
             PaymentVerification.objects.filter(
-                status=PaymentVerification.STATUS_PENDING, sent_to_rapid_pro=True
+                cash_plan_payment_verification=self.other_verification, status=PaymentVerification.STATUS_PENDING
+            ).count(),
+            self.payment_record_amount,
+        )
+        self.assertEqual(
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=True,
             ).count(),
             100,
         )
         self.assertEqual(
             PaymentVerification.objects.filter(
-                status=PaymentVerification.STATUS_PENDING, sent_to_rapid_pro=False
+                cash_plan_payment_verification=self.other_verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=True,
+            ).count(),
+            0,
+        )
+        self.assertEqual(
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=False,
             ).count(),
             10,
+        )
+        self.assertEqual(
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.other_verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=False,
+            ).count(),
+            self.payment_record_amount,
         )
 
         post_request_mock = MagicMock()
@@ -146,21 +229,46 @@ class TestPhoneNumberVerification(TestCase):
         self.assertIsNone(self.verification.error)
 
         self.assertEqual(
-            PaymentVerification.objects.filter(status=PaymentVerification.STATUS_PENDING).count(),
-            self.payment_record_amount,
-        )
-        self.assertEqual(
             PaymentVerification.objects.filter(
-                status=PaymentVerification.STATUS_PENDING, sent_to_rapid_pro=True
+                cash_plan_payment_verification=self.verification, status=PaymentVerification.STATUS_PENDING
             ).count(),
             self.payment_record_amount,
         )
         self.assertEqual(
             PaymentVerification.objects.filter(
-                status=PaymentVerification.STATUS_PENDING, sent_to_rapid_pro=False
+                cash_plan_payment_verification=self.other_verification, status=PaymentVerification.STATUS_PENDING
+            ).count(),
+            self.payment_record_amount,
+        )
+        self.assertEqual(
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=True,
+            ).count(),
+            self.payment_record_amount,
+        )
+        self.assertEqual(
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.other_verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=True,
             ).count(),
             0,
         )
-
-    def test_not_querying_individuals_from_other_cash_plan_payment_verification(self):
-        pass
+        self.assertEqual(
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=False,
+            ).count(),
+            0,
+        )
+        self.assertEqual(
+            PaymentVerification.objects.filter(
+                cash_plan_payment_verification=self.other_verification,
+                status=PaymentVerification.STATUS_PENDING,
+                sent_to_rapid_pro=False,
+            ).count(),
+            self.payment_record_amount,
+        )
