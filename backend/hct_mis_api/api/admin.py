6646facd22311e6c9e8a8af9be9a9d6ccebfbd9f
Domenico DiNicola <dom.dinicola@gmail.com>
2022-09-21 17:27:30 -0500

    missing migrations break HouseholdListSerializer save

--- backend/hct_mis_api/api/admin.py
+++ backend/hct_mis_api/api/admin.py
@@ -1,5 +1,4 @@
-from django import forms
-from django.contrib import admin, messages
+from django.contrib import admin
 
 from adminfilters.autocomplete import AutoCompleteFilter
 from smart_admin.modeladmin import SmartModelAdmin
