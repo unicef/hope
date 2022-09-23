012ef2d9e09efba608bfc54809a24e0f0cad011f
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-09-23 13:59:09 +0200

    connect allfeedbacks query

---
+++ frontend/src/apollo/queries/feedback/AllFeedbacks.ts
@@ -0,0 +1,39 @@
+import { gql } from 'apollo-boost';
+
+export const AllFeedbacks = gql`
+  query AllFeedbacks(
+    $offset: Int
+    $before: String
+    $after: String
+    $first: Int
+    $last: Int
+    $businessAreaSlug: String!
+  ) {
+    allFeedbacks(
+      offset: $offset
+      before: $before
+      after: $after
+      first: $first
+      last: $last
+      businessAreaSlug: $businessAreaSlug
+    ) {
+      totalCount
+      pageInfo {
+        startCursor
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          issueType
+          householdLookup {
+            id
+            unicefId
+          }
+          createdAt
+        }
+      }
+    }
+  }
+`;
