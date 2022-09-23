8a592bdfe0a82c08328d823219fa176fcc526671
saxix <s.apostolico@gmail.com>
2022-09-23 13:15:42 +0200

    update humanize

--- backend/hct_mis_api/api/utils.py
+++ backend/hct_mis_api/api/utils.py
@@ -1,19 +1,28 @@
 def humanize_errors(errors):
     """
+    >>> e = {"name":"Required", 'households': [{}, {'members': {'primary_collector': ['Required']}}]}
+    >>> humanize_errors(e)
+    {'name': 'Required', 'households': [{'Household #2': [{'primary_collector': ['Required']}]}]}
+
     >>> e = {'households': [{}, {'members': {'primary_collector': ['Required']}}]}
     >>> humanize_errors(e)
-    [{'Household #2': [{'primary_collector': ['Required']}]}]
+    {'households': [{'Household #2': [{'primary_collector': ['Required']}]}]}
 
     >>> e = {'households': [{'members': {'head_of_household': ['Only one HoH allowed']}}]}
     >>> humanize_errors(e)
-    [{'Household #1': [{'head_of_household': ['Only one HoH allowed']}]}]
+    {'households': [{'Household #1': [{'head_of_household': ['Only one HoH allowed']}]}]}
 
     >>> e = {'households': [{'members': [{'role': ['This field is required.']}, {}, {}, {}]}, {'members': [{'role': ['This field is required.']}, {}], 'country': ['This field is required.']}]}
     >>> humanize_errors(e)
-    [{'Household #1': [{'role': ['This field is required.']}]}, {'Household #2': [{'role': ['This field is required.']}]}]
+    {'households': [{'Household #1': [{'member #1': [{'role': ['This field is required.']}]}]}, {'Household #2': [{'member #1': [{'role': ['This field is required.']}]}]}]}
+
+    >>> e = {'households': [{'members': [ {}, {'role': ['This field is required.']}, {}, {}]}, {}, {'members': [{'role': ['This field is required.']}, {}], 'country': ['This field is required.']}]}
+    >>> humanize_errors(e)
+    {'households': [{'Household #1': [{'member #2': [{'role': ['This field is required.']}]}]}, {'Household #3': [{'member #1': [{'role': ['This field is required.']}]}]}]}
     """
     try:
-        errs = []
+        errs = dict(errors)
+        errs["households"] = []
         hh = errors.get("households", [])
         for h, curr_h_errs in enumerate(hh, 1):
             if curr_h_errs:
@@ -24,16 +33,17 @@ def humanize_errors(errors):
                     clean_h_errs.append(xxx)
                 else:
                     for i, curr_i_errs in enumerate(xxx, 1):
+                        key2 = f"member #{i}"
                         if curr_i_errs:
                             clean_i_errs = []
                             if isinstance(curr_i_errs, dict):
                                 clean_i_errs.append(curr_i_errs)
                             elif isinstance(curr_i_errs, (list, tuple)):
                                 clean_i_errs.extend(curr_i_errs)
-                            clean_h_errs.extend(clean_i_errs)
+                            clean_h_errs.append({key2: clean_i_errs})
 
                 if clean_h_errs:
-                    errs.append({key1: clean_h_errs})
+                    errs["households"].append({key1: clean_h_errs})
         return errs
     except (ValueError, AttributeError):
         return errors
