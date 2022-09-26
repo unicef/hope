1b173f117a06b9bb7feb6046195bb1252bc0b2d6
Konrad Marzec <konradmarzec@Konrads-MacBook-Pro.local>
2022-09-26 12:04:40 +0200

    Add test for multiple files

--- backend/hct_mis_api/apps/grievance/tests/test_grievance_documents_upload.py
+++ backend/hct_mis_api/apps/grievance/tests/test_grievance_documents_upload.py
@@ -1,4 +1,4 @@
-from io import BytesIO
+from django.test import override_settings
 
 from hct_mis_api.apps.account.fixtures import UserFactory
 from hct_mis_api.apps.core.base_test_case import BaseMultipleFilesUploadTestCase
@@ -6,10 +6,8 @@ from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.core.fixtures import create_afghanistan
 from hct_mis_api.apps.grievance.fixtures import GrievanceTicketFactory
 
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from django.test import override_settings
 
-
+@override_settings(MEDIA_ROOT=(BaseMultipleFilesUploadTestCase.TEST_DIR + '/media'))
 class SupportGrievanceTicketDocumentsUploadTestCase(BaseMultipleFilesUploadTestCase):
     UPLOAD_GRIEVANCE_DOCUMENTS = """
         mutation uploadDocuments(
@@ -37,21 +35,17 @@ class SupportGrievanceTicketDocumentsUploadTestCase(BaseMultipleFilesUploadTestC
             assigned_to=cls.user
         )
 
-    @override_settings(MEDIA_ROOT=(BaseMultipleFilesUploadTestCase.TEST_DIR + '/media'))
-    def test_some_mutation(self):
-
-        files = {
-            "files.0": self.create_fixture_file(
-                name="some_file.jpg",
-                size=100,
-                content_type="image/jpeg"
-            )
-        }
-
+    def test_user_can_send_one_document(self):
         response = self.file_query(
             query=self.UPLOAD_GRIEVANCE_DOCUMENTS,
             op_name="uploadDocuments",
-            files=files,
+            files={
+                "files.0": self.create_fixture_file(
+                    name="some_file.jpg",
+                    size=100,
+                    content_type="image/jpeg"
+                )
+            },
             variables={
                 "grievanceTicketId": self.id_to_base64(self.grievance_ticket.id, "GrievanceTicketNode"),
                 "files": [None]
@@ -62,10 +56,51 @@ class SupportGrievanceTicketDocumentsUploadTestCase(BaseMultipleFilesUploadTestC
         self.assertEqual(
             response.json(),
             {
-                "data": {
-                    "uploadDocuments": {
-                        "success": True
-                    }
+                'data': {
+                    'uploadDocuments': {
+                        'success': True
+                    },
+                }
+            }
+        )
+
+        self.assertMatchSnapshot(response.json())
+
+    def test_user_can_send_multiple_documents(self):
+        response = self.file_query(
+            query=self.UPLOAD_GRIEVANCE_DOCUMENTS,
+            op_name="uploadDocuments",
+            files={
+                "files.0": self.create_fixture_file(
+                    name="some_file.jpg",
+                    size=100,
+                    content_type="image/jpeg"
+                ),
+                "files.1": self.create_fixture_file(
+                    name="some_file.png",
+                    size=200,
+                    content_type="image/png"
+                ),
+                "files.2": self.create_fixture_file(
+                    name="some_file.pdf",
+                    size=300,
+                    content_type="application/pdf"
+                )
+            },
+            variables={
+                "grievanceTicketId": self.id_to_base64(self.grievance_ticket.id, "GrievanceTicketNode"),
+                "files": [None, None, None]
+            }
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                'data': {
+                    'uploadDocuments': {
+                        'success': True
+                    },
                 }
             }
         )
