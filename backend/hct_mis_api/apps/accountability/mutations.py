49b20580ee2fcf03d7f31b0ae43057e9e7615d4f
Bartosz Woźniak <bartosz.wozniak@tivix.com>
2022-09-26 10:41:31 +0200

    Updating

--- backend/hct_mis_api/apps/accountability/mutations.py
+++ backend/hct_mis_api/apps/accountability/mutations.py
@@ -1,12 +1,15 @@
 from django.db import transaction
+from django.shortcuts import get_object_or_404
 
 import graphene
 
 from hct_mis_api.apps.account.permissions import PermissionMutation, Permissions
 from hct_mis_api.apps.activity_log.models import log_create
 from hct_mis_api.apps.core.permissions import is_authenticated
+from hct_mis_api.apps.accountability.models import Feedback
+from hct_mis_api.apps.core.utils import decode_id_string
 
-from .inputs import CreateAccountabilityCommunicationMessageInput, CreateFeedbackInput
+from .inputs import CreateAccountabilityCommunicationMessageInput, CreateFeedbackInput, UpdateFeedbackInput
 from .models import Message
 from .schema import CommunicationMessageNode, FeedbackNode
 from .services.message_crud_services import MessageCrudServices
@@ -58,6 +61,30 @@ class CreateFeedbackMutation(PermissionMutation):
         return cls(feedback=new_feedback)
 
 
+class UpdateFeedbackMutation(PermissionMutation):
+    feedback = graphene.Field(FeedbackNode)
+
+    class Arguments:
+        input = UpdateFeedbackInput(required=True)
+
+    @classmethod
+    @is_authenticated
+    @transaction.atomic
+    def mutate(cls, root, info, input):
+        feedback = get_object_or_404(Feedback, id=decode_id_string(input["feedback_id"]))
+        cls.has_permission(info, Permissions.ACCOUNTABILITY_FEEDBACK_VIEW_UPDATE, feedback.business_area.slug)
+        updated_feedback = FeedbackCrudServices.update(feedback, input)
+        log_create(
+            Message.ACTIVITY_LOG_MAPPING,
+            "business_area",
+            info.context.user,
+            None,
+            updated_feedback,
+        )
+        return cls(feedback=updated_feedback)
+
+
 class Mutations(graphene.ObjectType):
     create_accountability_communication_message = CreateCommunicationMessageMutation.Field()
     create_feedback = CreateFeedbackMutation.Field()
+    update_feedback = UpdateFeedbackMutation.Field()
