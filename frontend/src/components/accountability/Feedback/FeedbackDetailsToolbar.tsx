69dd8088c4dd38da3176b6f0f856c8ba30f9c1b2
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-09-27 14:18:27 +0200

    connect feedback details

--- frontend/src/components/accountability/Feedback/FeedbackDetailsToolbar.tsx
+++ frontend/src/components/accountability/Feedback/FeedbackDetailsToolbar.tsx
@@ -2,35 +2,37 @@ import { Box, Button } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/EditRounded';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { useBusinessArea } from '../../../hooks/useBusinessArea';
-import { GrievanceTicketQuery } from '../../../__generated__/graphql';
+import { FeedbackQuery } from '../../../__generated__/graphql';
 import { BreadCrumbsItem } from '../../core/BreadCrumbs';
 import { PageHeader } from '../../core/PageHeader';
 
 interface FeedbackDetailsToolbarProps {
-  ticket: GrievanceTicketQuery['grievanceTicket'];
+  feedback: FeedbackQuery['feedback'];
   canEdit: boolean;
 }
 
 export const FeedbackDetailsToolbar = ({
-  ticket,
+  feedback,
   canEdit,
 }: FeedbackDetailsToolbarProps): React.ReactElement => {
   const { t } = useTranslation();
   const { id } = useParams();
   const businessArea = useBusinessArea();
+  const history = useHistory();
 
   const breadCrumbsItems: BreadCrumbsItem[] = [
     {
-      title: t('Grievance and Feedback'),
-      to: `/${businessArea}/grievance-and-feedback/tickets`,
+      title: t('Feedback'),
+      to: `/${businessArea}/accountability/feedback`,
     },
   ];
+  console.log('feedback', feedback);
 
   return (
     <PageHeader
-      title={`Feedback ID: ${ticket.unicefId}`}
+      title={`Feedback ID: ${feedback.unicefId}`}
       breadCrumbs={breadCrumbsItems}
     >
       <Box display='flex' alignItems='center'>
@@ -47,6 +49,24 @@ export const FeedbackDetailsToolbar = ({
             </Button>
           </Box>
         )}
+        <Box mr={3}>
+          <Button
+            onClick={() =>
+              history.push({
+                pathname: `/${businessArea}/grievance-and-feedback/new-ticket`,
+                state: {
+                  selectedHousehold: feedback.householdLookup,
+                  selectedIndividual: feedback.individualLookup,
+                  linkedFeedbackId: id,
+                },
+              })
+            }
+            variant='contained'
+            color='primary'
+          >
+            {t('Create Linked Ticket')}
+          </Button>
+        </Box>
       </Box>
     </PageHeader>
   );
