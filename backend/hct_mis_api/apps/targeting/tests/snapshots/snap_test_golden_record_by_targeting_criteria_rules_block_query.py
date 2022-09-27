ef75f3c07efcede833765554ae59c82176a5a884
Konrad Marzec <konradmarzec@Konrads-MacBook-Pro.local>
2022-09-27 17:19:53 +0200

    Fix tests

--- backend/hct_mis_api/apps/targeting/tests/snapshots/snap_test_golden_record_by_targeting_criteria_rules_block_query.py
+++ backend/hct_mis_api/apps/targeting/tests/snapshots/snap_test_golden_record_by_targeting_criteria_rules_block_query.py
@@ -161,31 +161,6 @@ snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_gold
     }
 }
 
-snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_observed_disability_with_invalid_argument 1'] = {
-    'data': {
-        'goldenRecordByTargetingCriteria': {
-            'edges': [
-                {
-                    'node': {
-                        'individuals': {
-                            'edges': [
-                                {
-                                    'node': {
-                                        'maritalStatus': 'MARRIED',
-                                        'sex': 'MALE'
-                                    }
-                                }
-                            ]
-                        },
-                        'size': 1
-                    }
-                }
-            ],
-            'totalCount': 1
-        }
-    }
-}
-
 snapshots['GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase::test_golden_record_by_targeting_criteria_observed_disability_with_valid_argument 1'] = {
     'data': {
         'goldenRecordByTargetingCriteria': {
