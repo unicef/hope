454d6226ecfbf971614b142f30fe31ea6efc07f3
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-10-03 12:31:25 +0200

    Don't set null values if provided form FE

--- backend/hct_mis_api/apps/accountability/services/feedback_crud_services.py
+++ backend/hct_mis_api/apps/accountability/services/feedback_crud_services.py
@@ -13,33 +13,35 @@ User = get_user_model()
 class FeedbackCrudServices:
     @classmethod
     def validate_lookup(cls, feedback):
-        # pass
         if feedback.household_lookup is not None and feedback.individual_lookup is not None:
             if feedback.household_lookup != feedback.individual_lookup.household:
                 raise Exception("Household lookup does not match individual lookup")
 
     @classmethod
     def create(cls, user: User, input_data: dict) -> Feedback:
+        def check(key):
+            return key in input_data and input_data[key] is not None
+
         obj = Feedback(
             business_area=BusinessArea.objects.get(slug=input_data["business_area_slug"]),
             issue_type=input_data["issue_type"],
             description=input_data["description"],
         )
-        if input_data.get("household_lookup"):
+        if check("household_lookup"):
             obj.household_lookup = get_object_or_404(Household, id=decode_id_string(input_data["household_lookup"]))
-        if input_data.get("individual_lookup"):
+        if check("individual_lookup"):
             obj.individual_lookup = get_object_or_404(Individual, id=decode_id_string(input_data["individual_lookup"]))
-        if input_data.get("comments"):
+        if check("comments"):
             obj.comments = input_data["comments"]
-        if input_data.get("admin2"):
+        if check("admin2"):
             obj.admin2 = get_object_or_404(Area, id=decode_id_string(input_data["admin2"]))
-        if input_data.get("area"):
+        if check("area"):
             obj.area = input_data["area"]
-        if input_data.get("language"):
+        if check("language"):
             obj.language = input_data["language"]
-        if input_data.get("consent"):
+        if check("consent"):
             obj.consent = input_data["consent"]
-        if input_data.get("program"):
+        if check("program"):
             obj.program = get_object_or_404(Program, id=decode_id_string(input_data["program"]))
         obj.created_by = user
         cls.validate_lookup(obj)
@@ -48,29 +50,32 @@ class FeedbackCrudServices:
 
     @classmethod
     def update(cls, feedback: Feedback, input_data: dict) -> Feedback:
-        if "issue_type" in input_data:
+        def check(key):
+            return key in input_data and input_data[key] is not None
+
+        if check("issue_type"):
             feedback.issue_type = input_data["issue_type"]
-        if "description" in input_data:
+        if check("description"):
             feedback.description = input_data["description"]
-        if "household_lookup" in input_data:
+        if check("household_lookup"):
             feedback.household_lookup = get_object_or_404(
                 Household, id=decode_id_string(input_data["household_lookup"])
             )
-        if "individual_lookup" in input_data:
+        if check("individual_lookup"):
             feedback.individual_lookup = get_object_or_404(
                 Individual, id=decode_id_string(input_data["individual_lookup"])
             )
-        if "comments" in input_data:
+        if check("comments"):
             feedback.comments = input_data["comments"]
-        if "admin2" in input_data:
+        if check("admin2"):
             feedback.admin2 = get_object_or_404(Area, id=decode_id_string(input_data["admin2"]))
-        if "area" in input_data:
+        if check("area"):
             feedback.area = input_data["area"]
-        if "language" in input_data:
+        if check("language"):
             feedback.language = input_data["language"]
-        if "consent" in input_data:
+        if check("consent"):
             feedback.consent = input_data["consent"]
-        if "program" in input_data:
+        if check("program"):
             feedback.program = get_object_or_404(Program, id=decode_id_string(input_data["program"]))
         cls.validate_lookup(feedback)
         feedback.save()
