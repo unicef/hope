92f3de0af5f95e6d305364556d7b7b68c2765c42
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-10-06 07:29:22 +0200

    init upd all_cash_plans_and_payment_plans query

--- backend/hct_mis_api/apps/core/utils.py
+++ backend/hct_mis_api/apps/core/utils.py
@@ -704,7 +704,7 @@ def timezone_datetime(value):
     return datetime_value
 
 
-def get_paginator(qs, page_size, page, paginated_type, **kwargs):
+def get_paginator(qs, page_size, page):
     p = Paginator(qs, page_size)
     try:
         page_obj = p.page(page)
@@ -712,13 +712,4 @@ def get_paginator(qs, page_size, page, paginated_type, **kwargs):
         page_obj = p.page(1)
     except EmptyPage:
         page_obj = p.page(p.num_pages)
-    return paginated_type(
-        page=page_obj.number,
-        pages=p.num_pages,
-        page_size=page_size,
-        total_count=qs.count(),
-        has_next=page_obj.has_next(),
-        has_prev=page_obj.has_previous(),
-        objects=page_obj.object_list,
-        **kwargs
-    )
+    return p, page_obj
