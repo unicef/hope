c78ab36729e4dbb941275aa91d06227fac942ece
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-10-12 09:54:31 +0200

    Cypress scenario preconditions generated on BE (#1854)

--- cypress_testing_service/cypress/integration/program/program.spec.ts
+++ cypress_testing_service/cypress/integration/program/program.spec.ts
@@ -1,5 +1,5 @@
 import { When, Then, And, Given } from 'cypress-cucumber-preprocessor/steps';
-import { fillProgramForm } from '../../procedures/procedures';
+import { uniqueSeed } from '../../procedures/procedures';
 
 Given('I am authenticated', () => {
   cy.visit('/api/unicorn/');
@@ -33,7 +33,26 @@ Then('I should see the Set-up a new Programme modal', () => {
 });
 
 When('I fill out all the form fields', () => {
-  fillProgramForm(cy);
+  const programName = `test program ${uniqueSeed}`;
+  cy.get('[data-cy="input-programme-name"]').type(programName);
+  cy.get('[data-cy="input-cash-assist-scope"]').first().click();
+  cy.get('[data-cy="select-option-Unicef"]').click();
+  cy.get('[data-cy="input-sector"]').first().click();
+  cy.get('[data-cy="select-option-Multi Purpose"]').click();
+  cy.get('[data-cy="input-start-date"]').click().type('2022-12-12');
+  cy.get('[data-cy="input-end-date"]').click().type('2022-12-23');
+  cy.get('[data-cy="input-description"]')
+    .first()
+    .click()
+    .type('test description');
+  cy.get('[data-cy="input-budget"]')
+    .first()
+    .click()
+    .type('{backspace}{backspace}{backspace}{backspace}9999');
+  cy.get('[data-cy="input-admin-area"]').click().type('Some Admin Area');
+  cy.get('[data-cy="input-population-goal"]')
+    .click()
+    .type('{backspace}{backspace}{backspace}{backspace}4000');
 });
 
 And('I click the save button', () => {
