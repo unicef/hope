669072919809fd083f4e29cb029b50c438a16706
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-10-20 23:17:45 +0200

    Apply mypy suggestions (#1890)

--- backend/hct_mis_api/apps/grievance/mutations.py
+++ backend/hct_mis_api/apps/grievance/mutations.py
@@ -1,7 +1,6 @@
 import logging
 from enum import Enum
-from types import FunctionType
-from typing import Dict, List, Union
+from typing import Callable, Dict, List, Union
 
 from django.contrib.auth import get_user_model
 from django.db import transaction
@@ -269,7 +268,7 @@ class CreateGrievanceTicketMutation(PermissionMutation):
             verify_required_arguments(input, "issue_type", cls.ISSUE_TYPE_OPTIONS)
         category = arg("category")
         grievance_ticket, extras = cls.save_basic_data(root, info, input, **kwargs)
-        save_extra_methods: Dict[int, FunctionType] = {
+        save_extra_methods: Dict[int, Callable] = {
             GrievanceTicket.CATEGORY_PAYMENT_VERIFICATION: save_payment_verification_extras,
             GrievanceTicket.CATEGORY_DATA_CHANGE: save_data_change_extras,
             GrievanceTicket.CATEGORY_GRIEVANCE_COMPLAINT: save_grievance_complaint_extras,
@@ -278,7 +277,7 @@ class CreateGrievanceTicketMutation(PermissionMutation):
             GrievanceTicket.CATEGORY_NEGATIVE_FEEDBACK: save_negative_feedback_extras,
             GrievanceTicket.CATEGORY_REFERRAL: save_referral_extras,
         }
-        save_extra_method: FunctionType = save_extra_methods.get(category)
+        save_extra_method: Callable = save_extra_methods.get(category)
         grievances = [grievance_ticket]
         if save_extra_method:
             grievances = save_extra_method(root, info, input, grievance_ticket, extras, **kwargs)
@@ -609,7 +608,7 @@ class GrievanceStatusChangeMutation(PermissionMutation):
         GrievanceTicket.CATEGORY_SYSTEM_FLAGGING: close_system_flagging_ticket,
     }
 
-    MOVE_TO_STATUS_PERMISSION_MAPPING: Dict[str, Dict[Union[str, int], List[Enum]]] = {
+    MOVE_TO_STATUS_PERMISSION_MAPPING: Dict[int, Dict[Union[str, int], List[Enum]]] = {
         GrievanceTicket.STATUS_ASSIGNED: {
             "any": [
                 Permissions.GRIEVANCES_UPDATE,
