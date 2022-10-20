67e1e2f32dde9fed6520eafd1dc6221f1035c8bc
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-10-20 13:50:58 +0200

    PaymentPlanNode add verification_plans

--- backend/hct_mis_api/apps/program/schema.py
+++ backend/hct_mis_api/apps/program/schema.py
@@ -97,10 +97,13 @@ class CashPlanNode(BaseNodePermissionMixin, DjangoObjectType):
     available_payment_records_count = graphene.Int()
     verification_plans = DjangoPermissionFilterConnectionField(
         PaymentVerificationPlanNode,
-        source="payment_verification_plans",
+        source="get_payment_verification_plans",
         filterset_class=PaymentVerificationPlanFilter,
     )
-    payment_verification_summary = graphene.Field(PaymentVerificationSummaryNode)
+    payment_verification_summary = graphene.Field(
+        PaymentVerificationSummaryNode,
+        source="get_payment_verification_summary",
+    )
 
     class Meta:
         model = CashPlan
@@ -117,9 +120,6 @@ class CashPlanNode(BaseNodePermissionMixin, DjangoObjectType):
         return self.payment_items.filter(
             status__in=PaymentRecord.ALLOW_CREATE_VERIFICATION, delivered_quantity__gt=0
         ).count()
-
-    def resolve_payment_verification_summary(self, info, **kwargs):
-        return self.get_payment_verification_summary
 
 
 class Query(graphene.ObjectType):
