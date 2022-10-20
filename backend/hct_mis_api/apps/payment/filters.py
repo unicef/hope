02a4f85db7f5359dbb66509d0c51114a51fdaff1
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-10-20 17:52:43 +0200

    update cash_plan_and_payment_plan_filter and fix ordering

--- backend/hct_mis_api/apps/payment/filters.py
+++ backend/hct_mis_api/apps/payment/filters.py
@@ -1,6 +1,6 @@
 from base64 import b64decode
 
-from django.db.models import Case, CharField, Count, F, Q, Value, When
+from django.db.models import Case, CharField, Count, Q, Value, When
 from django.db.models.functions import Lower
 from django.shortcuts import get_object_or_404
 
@@ -20,6 +20,7 @@ from hct_mis_api.apps.core.utils import (
     decode_id_string,
     is_valid_uuid,
 )
+from hct_mis_api.apps.core.querysets import ExtendedQuerySetSequence
 from hct_mis_api.apps.household.models import ROLE_NO_ROLE
 from hct_mis_api.apps.payment.models import (
     CashPlan,
@@ -34,6 +35,7 @@ from hct_mis_api.apps.payment.models import (
     PaymentVerificationPlan,
     PaymentVerificationSummary,
 )
+from hct_mis_api.apps.program.models import Program
 
 
 class PaymentRecordFilter(FilterSet):
@@ -355,12 +357,53 @@ class PaymentFilter(FilterSet):
         return super().filter_queryset(queryset)
 
 
-# TODO need update this one
-class CashPlanPaymentPlanFilter(FilterSet):
-    business_area = CharFilter(field_name="business_area__slug", required=True)
+def cash_plan_and_payment_plan_filter(queryset: ExtendedQuerySetSequence, **kwargs) -> ExtendedQuerySetSequence:
+    business_area = kwargs.get("business_area")
+    program = kwargs.get("program")
+    service_provider = kwargs.get("service_provider")
+    delivery_type = kwargs.get("delivery_type")
+    verification_status = kwargs.get("verification_status")
+    start_date_gte, end_date_lte = kwargs.get("start_date_gte"), kwargs.get("end_date_lte")
+    search = kwargs.get("search")
 
-    class Meta:
-        model = CashPlan
-        fields = tuple()
+    if business_area:
+        queryset = queryset.filter(business_area__slug=business_area)
 
-    order_by = OrderingFilter(fields=("unicef_id",))
+    if program:
+        program_obj = get_object_or_404(Program, id=decode_id_string(program))
+        queryset = queryset.filter(program=program_obj)
+
+    if start_date_gte:
+        queryset = queryset.filter(start_date__gte=start_date_gte)
+    if end_date_lte:
+        queryset = queryset.filter(end_date__lte=end_date_lte)
+
+    if verification_status:
+        queryset = queryset.filter(payment_verification_summary__status__in=verification_status)
+
+    # TODO: add filters below
+    if service_provider:
+        pass
+    if delivery_type:
+        # ["delivery_type",] PaymentRecord.DELIVERY_TYPE_CHOICE
+        pass
+
+    if search:
+        pass
+
+    return queryset
+
+
+def cash_plan_and_payment_plan_ordering(queryset: ExtendedQuerySetSequence, order_by) -> ExtendedQuerySetSequence:
+    reverse = "-" if order_by.startswith("-") else ""
+    order_by = order_by[1:] if reverse else order_by
+
+    if order_by == "verification_status":
+        queryset = queryset.order_by(reverse + "custom_order")
+
+    elif order_by == "unicef_id":
+        queryset = sorted(queryset, key=lambda o: o.get_unicef_id, reverse=bool(reverse))
+    else:
+        queryset = queryset.order_by(reverse + order_by)
+
+    return queryset
