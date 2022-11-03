e344fa3f31d7e76b57c4f04db78d7d1edfccc00e
Marek Biczysko <34810846+MarekBiczysko@users.noreply.github.com>
2022-11-03 10:56:56 +0100

    117364_deadlock_issue (#1907)

--- backend/hct_mis_api/apps/household/services/household_withdraw.py
+++ backend/hct_mis_api/apps/household/services/household_withdraw.py
@@ -1,25 +1,26 @@
-from django.db.models import QuerySet
+from django.db import transaction
 
 from hct_mis_api.apps.grievance.models import GrievanceTicket
-from hct_mis_api.apps.household.models import Household, Individual
+from hct_mis_api.apps.household.models import Household
 
 
 class HouseholdWithdraw:
     def __init__(self, household: Household) -> None:
         self.household: Household = household
-        self.individuals: QuerySet[Individual] = self.household.individuals.filter(duplicate=False)
         self.documents = None
 
+    @transaction.atomic
     def withdraw(self, tag=None) -> None:
         self.household.withdraw(tag)
 
-        for individual in self.individuals:
+        for individual in self.household.individuals.select_for_update().filter(duplicate=False):
             individual.withdraw()
 
+    @transaction.atomic
     def unwithdraw(self) -> None:
         self.household.unwithdraw()
 
-        for individual in self.individuals:
+        for individual in self.household.individuals.select_for_update().filter(duplicate=False):
             individual.unwithdraw()
 
     def change_tickets_status(self, tickets) -> None:
--- backend/hct_mis_api/apps/sanction_list/tasks/check_against_sanction_list_pre_merge.py
+++ backend/hct_mis_api/apps/sanction_list/tasks/check_against_sanction_list_pre_merge.py
@@ -2,6 +2,7 @@ import logging
 from typing import Dict
 
 from django.core.cache import cache
+from django.db import transaction
 from django.utils import timezone
 
 from constance import config
@@ -22,6 +23,7 @@ from hct_mis_api.apps.household.models import (
     Individual,
 )
 from hct_mis_api.apps.sanction_list.models import SanctionListIndividual
+from hct_mis_api.apps.utils.querysets import evaluate_qs
 
 log = logging.getLogger(__name__)
 
@@ -71,6 +73,7 @@ class CheckAgainstSanctionListPreMergeTask:
         return query_dict
 
     @classmethod
+    @transaction.atomic
     def execute(cls, individuals=None, registration_data_import=None) -> None:
         if individuals is None:
             individuals = SanctionListIndividual.objects.all()
@@ -128,12 +131,18 @@ class CheckAgainstSanctionListPreMergeTask:
                 )
                 log.debug([(r.full_name, r.meta.score) for r in results])
         cache.set("sanction_list_last_check", timezone.now(), None)
-        Individual.objects.filter(id__in=possible_matches, sanction_list_possible_match=False).update(
-            sanction_list_possible_match=True
+
+        possible_matches_individuals = evaluate_qs(
+            Individual.objects.filter(id__in=possible_matches, sanction_list_possible_match=False).select_for_update()
         )
-        Individual.objects.exclude(id__in=possible_matches).filter(sanction_list_possible_match=True).update(
-            sanction_list_possible_match=False
+        possible_matches_individuals.update(sanction_list_possible_match=True)
+
+        not_possible_matches_individuals = evaluate_qs(
+            Individual.objects.exclude(id__in=possible_matches)
+            .filter(sanction_list_possible_match=True)
+            .select_for_update()
         )
+        not_possible_matches_individuals.update(sanction_list_possible_match=False)
 
         GrievanceTicket.objects.bulk_create(tickets_to_create)
         for ticket in tickets_to_create:
