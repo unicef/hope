641dfaf3a82cc3a13d1f60738c745f323e418f00
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2022-11-03 16:27:06 +0100

    Fix tests

--- backend/hct_mis_api/apps/accountability/tests/snapshots/snap_test_list_query_messages.py
+++ backend/hct_mis_api/apps/accountability/tests/snapshots/snap_test_list_query_messages.py
@@ -7,7 +7,7 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_0_with_list_permission_full_sampling 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_0_with_list_permission_full_sampling 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -16,7 +16,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_0_with_li
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_1_with_list_permission_random_sampling 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_1_with_list_permission_random_sampling 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -25,126 +25,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_1_with_li
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_2_with_list_permission_title 1'] = {
-    'data': {
-        'allAccountabilityCommunicationMessages': {
-            'edges': [
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 10 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 10'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 9 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 9'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 8 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 8'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 7 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 7'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 6 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 6'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 5 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 5'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 4 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 4'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 3 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 3'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 2 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 2'
-                    }
-                },
-                {
-                    'node': {
-                        'body': 'Greetings, we have sent you USD 1 in your registered account on 2022-09-19 20:00:00 UTC',
-                        'createdBy': {
-                            'firstName': 'John',
-                            'lastName': 'Wick'
-                        },
-                        'numberOfRecipients': 14,
-                        'title': 'You got credit of USD 1'
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestActionMessageMutation::test_list_communication_messages_3_with_list_permission_title 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_2_with_list_permission_title 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -263,7 +144,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_3_with_li
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_4_with_list_permission_numberOfRecipients 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_3_with_list_permission_title 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -382,7 +263,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_4_with_li
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_5_with_list_permission_createdBy 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_4_with_list_permission_numberOfRecipients 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -501,7 +382,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_5_with_li
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_6_with_view_details_permission 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_5_with_list_permission_createdBy 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -620,7 +501,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_6_with_vi
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_7_with_view_details_as_creator_permission 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_6_with_view_details_permission 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -739,7 +620,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_7_with_vi
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_8_without_permission 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_7_with_view_details_as_creator_permission 1'] = {
     'data': {
         'allAccountabilityCommunicationMessages': {
             'edges': [
@@ -858,7 +739,126 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_8_without
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_recipients_0_with_view_details_permission 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_8_without_permission 1'] = {
+    'data': {
+        'allAccountabilityCommunicationMessages': {
+            'edges': [
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 10 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 10'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 9 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 9'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 8 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 8'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 7 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 7'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 6 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 6'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 5 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 5'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 4 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 4'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 3 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 3'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 2 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 2'
+                    }
+                },
+                {
+                    'node': {
+                        'body': 'Greetings, we have sent you USD 1 in your registered account on 2022-09-19 20:00:00 UTC',
+                        'createdBy': {
+                            'firstName': 'John',
+                            'lastName': 'Wick'
+                        },
+                        'numberOfRecipients': 14,
+                        'title': 'You got credit of USD 1'
+                    }
+                }
+            ]
+        }
+    }
+}
+
+snapshots['TestListQueryMessage::test_list_communication_messages_recipients_0_with_view_details_permission 1'] = {
     'data': {
         'allAccountabilityCommunicationMessageRecipients': {
             'totalCount': 14
@@ -866,7 +866,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_recipient
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_recipients_1_with_view_details_permission 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_recipients_1_with_view_details_permission 1'] = {
     'data': {
         'allAccountabilityCommunicationMessageRecipients': {
             'totalCount': 14
@@ -874,7 +874,7 @@ snapshots['TestActionMessageMutation::test_list_communication_messages_recipient
     }
 }
 
-snapshots['TestActionMessageMutation::test_list_communication_messages_recipients_2_without_permission 1'] = {
+snapshots['TestListQueryMessage::test_list_communication_messages_recipients_2_without_permission 1'] = {
     'data': {
         'allAccountabilityCommunicationMessageRecipients': {
             'totalCount': 14
--- backend/hct_mis_api/apps/accountability/tests/snapshots/snap_test_sample_size_query.py
+++ backend/hct_mis_api/apps/accountability/tests/snapshots/snap_test_sample_size_query.py
@@ -7,7 +7,7 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_households_0_with_permission_and_full_list_households 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_households_0_with_permission_and_full_list_households 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 4,
@@ -16,7 +16,7 @@ snapshots['TestActionMessageMutation::test_get_communication_message_sample_size
     }
 }
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_households_1_with_permission_and_random_households 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_households_1_with_permission_and_random_households 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 4,
@@ -25,7 +25,7 @@ snapshots['TestActionMessageMutation::test_get_communication_message_sample_size
     }
 }
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_rdi_0_with_permission_and_full_list_rdi 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_rdi_0_with_permission_and_full_list_rdi 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 0,
@@ -34,7 +34,7 @@ snapshots['TestActionMessageMutation::test_get_communication_message_sample_size
     }
 }
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_rdi_1_with_permission_and_random_rdi 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_rdi_1_with_permission_and_random_rdi 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 0,
@@ -43,7 +43,7 @@ snapshots['TestActionMessageMutation::test_get_communication_message_sample_size
     }
 }
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_target_population_0_with_permission_and_full_list_tp 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_target_population_0_with_permission_and_full_list_tp 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 4,
@@ -52,7 +52,7 @@ snapshots['TestActionMessageMutation::test_get_communication_message_sample_size
     }
 }
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_target_population_1_with_permission_and_random_tp 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_target_population_1_with_permission_and_random_tp 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 4,
@@ -61,7 +61,7 @@ snapshots['TestActionMessageMutation::test_get_communication_message_sample_size
     }
 }
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_target_population_2_without_permission_full_list_tp 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_target_population_2_without_permission_full_list_tp 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 4,
@@ -70,7 +70,7 @@ snapshots['TestActionMessageMutation::test_get_communication_message_sample_size
     }
 }
 
-snapshots['TestActionMessageMutation::test_get_communication_message_sample_size_for_target_population_3_without_permission_random_tp 1'] = {
+snapshots['TestSampleSizeQuery::test_get_communication_message_sample_size_for_target_population_3_without_permission_random_tp 1'] = {
     'data': {
         'accountabilityCommunicationMessageSampleSize': {
             'numberOfRecipients': 4,
