583345c3856bfa632abc4be8094868754f24cf30
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2022-11-10 01:48:55 -0500

    Switched to markdown

--- backend/hct_mis_api/apps/changelog/urls.py
+++ backend/hct_mis_api/apps/changelog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path
 
 from hct_mis_api.apps.changelog import views
 
--- backend/hct_mis_api/urls.py
+++ backend/hct_mis_api/urls.py
@@ -78,7 +78,7 @@ api_patterns = [
         include("hct_mis_api.apps.power_query.urls"),
     ),
     path(
-        f"b/changelog/",
+        "b/changelog/",
         include("hct_mis_api.apps.changelog.urls"),
     ),
     path(f"{settings.ADMIN_PANEL_URL}/", admin.site.urls),
