5b7b54858d9f683b6dc83620b54c81568078d397
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-11-10 18:28:57 +0100

    reason of withdrawal household frontend

--- frontend/src/components/grievances/DeleteHouseholdGrievanceDetails.tsx
+++ frontend/src/components/grievances/DeleteHouseholdGrievanceDetails.tsx
@@ -1,8 +1,11 @@
 import { Box, Button, Grid, Typography } from '@material-ui/core';
+import { Field, Formik } from 'formik';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useBusinessArea } from '../../hooks/useBusinessArea';
 import { useSnackbar } from '../../hooks/useSnackBar';
+import { FormikRadioGroup } from '../../shared/Formik/FormikRadioGroup';
+import { FormikTextField } from '../../shared/Formik/FormikTextField';
 import { GRIEVANCE_TICKET_STATES } from '../../utils/constants';
 import { choicesToDict } from '../../utils/utils';
 import {
@@ -37,15 +40,62 @@ export const DeleteHouseholdGrievanceDetails = ({
 
   const isForApproval = ticket.status === GRIEVANCE_TICKET_STATES.FOR_APPROVAL;
   const approveEnabled = isForApproval;
-
-  let dialogText = t(
+  const dialogText = t(
     'You did not approve the following household to be withdrawn. Are you sure you want to continue?',
   );
-  if (!ticket.deleteHouseholdTicketDetails.approveStatus) {
-    dialogText = t(
-      'You are approving the following household to be withdrawn. Are you sure you want to continue?',
-    );
-  }
+
+  const dialogContentWithdrawReason = (
+    <Formik
+      initialValues={{ withdrawReason: '', duplicateHouseholdId: null }}
+      enableReinitialize
+      onSubmit={(values) => {
+        console.log('VALUES', values);
+      }}
+    >
+      {({ submitForm }) => (
+        <Box display='flex' flexDirection='column'>
+          <Box mt={2}>
+            <Typography variant='body2'>
+              {t('Please provide the reason of withdrawal of this household.')}
+            </Typography>
+          </Box>
+          <Field
+            name='withdrawReason'
+            choices={[
+              {
+                value: 'duplicate',
+                name: 'This household is a duplicate of another household',
+              },
+            ]}
+            component={FormikRadioGroup}
+            noMargin
+          />
+          <Grid container>
+            <Grid item xs={6}>
+              <Field
+                name='duplicateHouseholdId'
+                fullWidth
+                variant='outlined'
+                label={t('Household Id')}
+                component={FormikTextField}
+                required
+              />
+            </Grid>
+          </Grid>
+          <Field
+            name='withdrawReason'
+            choices={[{ value: 'other', name: 'Other' }]}
+            component={FormikRadioGroup}
+            noMargin
+          />
+          <Button onClick={submitForm}>Submit</Button>
+        </Box>
+      )}
+    </Formik>
+  );
+
+  const { approveStatus } = ticket.deleteHouseholdTicketDetails;
+
   if (choicesLoading) return <LoadingComponent />;
 
   const residenceChoicesDict = choicesToDict(
@@ -61,8 +111,13 @@ export const DeleteHouseholdGrievanceDetails = ({
             <Button
               onClick={() =>
                 confirm({
-                  title: t('Warning'),
-                  content: dialogText,
+                  title: approveStatus
+                    ? t('Warning')
+                    : t('Reason of withdrawal'),
+                  content: approveStatus
+                    ? dialogText
+                    : dialogContentWithdrawReason,
+                  continueText: t('Confirm'),
                 }).then(async () => {
                   try {
                     await mutate({
@@ -78,10 +133,10 @@ export const DeleteHouseholdGrievanceDetails = ({
                         },
                       ],
                     });
-                    if (ticket.deleteHouseholdTicketDetails.approveStatus) {
+                    if (approveStatus) {
                       showMessage(t('Changes Disapproved'));
                     }
-                    if (!ticket.deleteHouseholdTicketDetails.approveStatus) {
+                    if (!approveStatus) {
                       showMessage(t('Changes Approved'));
                     }
                   } catch (e) {
@@ -89,17 +144,11 @@ export const DeleteHouseholdGrievanceDetails = ({
                   }
                 })
               }
-              variant={
-                ticket.deleteHouseholdTicketDetails?.approveStatus
-                  ? 'outlined'
-                  : 'contained'
-              }
+              variant={approveStatus ? 'outlined' : 'contained'}
               color='primary'
               disabled={!approveEnabled}
             >
-              {ticket.deleteHouseholdTicketDetails?.approveStatus
-                ? t('Disapprove')
-                : t('Approve')}
+              {approveStatus ? t('Disapprove') : t('Approve')}
             </Button>
           )}
         </Box>
