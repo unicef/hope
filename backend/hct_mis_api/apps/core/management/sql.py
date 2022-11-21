8880725031a6b3bc10e27e27628844f8391e702c
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-11-21 15:31:09 +0100

    fix sql_drop_tables

--- backend/hct_mis_api/apps/core/management/sql.py
+++ backend/hct_mis_api/apps/core/management/sql.py
@@ -1,8 +1,11 @@
 def sql_drop_tables(connection):
     tables = connection.introspection.django_table_names(only_existing=True, include_views=False)
-    tables.append("django_migrations")
-    if not tables:
+    nnn = connection.introspection.table_names(include_views=False)
+    nnn.append("django_migrations")
+
+    print("Get all names", nnn)
+    if not nnn:
         return ""
-    tables_sql = ", ".join(connection.ops.quote_name(table) for table in tables)
+    tables_sql = ", ".join(connection.ops.quote_name(table) for table in nnn)
     sql = f"DROP TABLE IF EXISTS {tables_sql} CASCADE;"
     return "\n".join(["BEGIN;", sql, "COMMIT;"])
