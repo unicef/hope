a499c52ecdf5dba2f311cd1d4eec25a8277630d2
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-11-21 15:44:57 +0100

    add only_existing False

--- backend/hct_mis_api/apps/core/management/sql.py
+++ backend/hct_mis_api/apps/core/management/sql.py
@@ -1,11 +1,15 @@
 def sql_drop_tables(connection):
     tables = connection.introspection.django_table_names(only_existing=True, include_views=False)
+    tables222 = connection.introspection.django_table_names(only_existing=False, include_views=False)
     nnn = connection.introspection.table_names(include_views=False)
-    nnn.append("django_migrations")
+    tables222.append("django_migrations")
+
+    print("django tables==> ", tables)
+    print("only_existing=False django tables==>: ", tables222)
 
     print("Get all names", nnn)
-    if not nnn:
+    if not tables222:
         return ""
-    tables_sql = ", ".join(connection.ops.quote_name(table) for table in nnn)
+    tables_sql = ", ".join(connection.ops.quote_name(table) for table in tables222)
     sql = f"DROP TABLE IF EXISTS {tables_sql} CASCADE;"
     return "\n".join(["BEGIN;", sql, "COMMIT;"])
