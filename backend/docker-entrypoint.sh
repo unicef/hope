7ccfb84342f823df2651f89209999e11e5f8085a
Jan Romaniak <jan.romaniak@tivix.com>
2022-11-21 13:50:50 +0100

    newrelic remove

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,9 +2,7 @@
 set -e
 
 if [ $# -eq 0 ]; then
-    export NEW_RELIC_CONFIG_FILE=/code/newrelic.ini
-    export NEW_RELIC_ENVIRONMENT=$ENV
-    exec newrelic-admin run-program gunicorn hct_mis_api.wsgi -c /code/gunicorn_config.py
+    exec gunicorn hct_mis_api.wsgi -c /code/gunicorn_config.py
 else
     case "$1" in
         "dev")
