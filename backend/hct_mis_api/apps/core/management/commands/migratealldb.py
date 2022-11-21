bebac487bb947ac61db78a229d8e97bab779b759
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-11-21 14:40:04 +0100

    test

--- backend/hct_mis_api/apps/core/management/commands/migratealldb.py
+++ backend/hct_mis_api/apps/core/management/commands/migratealldb.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     help = "Migrate all databases specified in settings"
 
     def handle(self, *args, **options):
+        print("migrate all DB =====")
         for db in settings.DATABASES:
             self.stdout.write(self.style.WARNING(f"Migrating {db} database"))
             if db == "read_only":
