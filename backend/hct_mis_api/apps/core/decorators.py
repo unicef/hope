4998e2e13131bc2b8bfad06b282f313b4ddddd2a
Jan Romaniak <jan.romaniak@tivix.com>
2022-11-21 16:51:40 +0100

    without else

--- backend/hct_mis_api/apps/core/decorators.py
+++ backend/hct_mis_api/apps/core/decorators.py
@@ -8,10 +8,9 @@ def cached_in_django_cache(timeout_in_hours):
             value = cache.get(key)
             if value is not None:
                 return value
-            else:
-                value = func(*args, **kwargs)
-                cache.set(key, value, timeout_in_hours * 60 * 60)
-                return value
+            value = func(*args, **kwargs)
+            cache.set(key, value, timeout_in_hours * 60 * 60)
+            return value
 
         return wrapper
 
