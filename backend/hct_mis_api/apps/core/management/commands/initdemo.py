27b8799aa4f43e1e02df2571f7979beaff88ae6d
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-11-21 14:40:04 +0100

    test

--- backend/hct_mis_api/apps/core/management/commands/initdemo.py
+++ backend/hct_mis_api/apps/core/management/commands/initdemo.py
@@ -25,34 +25,36 @@ class Command(BaseCommand):
             self._drop_databases()
             call_command("migratealldb")
 
-        call_command("flush", "--noinput")
-        call_command("flush", "--noinput", database="cash_assist_datahub_mis")
-        call_command("flush", "--noinput", database="cash_assist_datahub_ca")
-        call_command("flush", "--noinput", database="cash_assist_datahub_erp")
-        call_command("flush", "--noinput", database="registration_datahub")
-
-        call_command("loaddata", "hct_mis_api/apps/geo/fixtures/data.json")
-        call_command("loaddata", "hct_mis_api/apps/core/fixtures/data.json")
-        call_command("loaddata", "hct_mis_api/apps/account/fixtures/data.json")
-        call_command("loaddata", "hct_mis_api/apps/registration_data/fixtures/data.json")
-        call_command("loaddata", "hct_mis_api/apps/household/fixtures/data.json")
-        call_command("loaddata", "hct_mis_api/apps/grievance/fixtures/data.json")
-
-        call_command(
-            "loaddata", "hct_mis_api/apps/registration_datahub/fixtures/data.json", database="registration_datahub"
-        )
-        call_command(
-            "loaddata", "hct_mis_api/apps/registration_datahub/fixtures/diiadata.json", database="registration_datahub"
-        )
-        call_command("loaddata", "hct_mis_api/apps/steficon/fixtures/data.json")
-
-        call_command("search_index", "--rebuild", "-f")
-        update_mis_unicef_id_individual_and_household()
-        generate_payment_plan()
-        generate_real_cash_plans()
+        # call_command("flush", "--noinput")
+        # call_command("flush", "--noinput", database="cash_assist_datahub_mis")
+        # call_command("flush", "--noinput", database="cash_assist_datahub_ca")
+        # call_command("flush", "--noinput", database="cash_assist_datahub_erp")
+        # call_command("flush", "--noinput", database="registration_datahub")
+        #
+        # call_command("loaddata", "hct_mis_api/apps/geo/fixtures/data.json")
+        # call_command("loaddata", "hct_mis_api/apps/core/fixtures/data.json")
+        # call_command("loaddata", "hct_mis_api/apps/account/fixtures/data.json")
+        # call_command("loaddata", "hct_mis_api/apps/registration_data/fixtures/data.json")
+        # call_command("loaddata", "hct_mis_api/apps/household/fixtures/data.json")
+        # call_command("loaddata", "hct_mis_api/apps/grievance/fixtures/data.json")
+        #
+        # call_command(
+        #     "loaddata", "hct_mis_api/apps/registration_datahub/fixtures/data.json", database="registration_datahub"
+        # )
+        # call_command(
+        #     "loaddata", "hct_mis_api/apps/registration_datahub/fixtures/diiadata.json", database="registration_datahub"
+        # )
+        # call_command("loaddata", "hct_mis_api/apps/steficon/fixtures/data.json")
+        #
+        # call_command("search_index", "--rebuild", "-f")
+        # update_mis_unicef_id_individual_and_household()
+        # generate_payment_plan()
+        # generate_real_cash_plans()
 
     def _drop_databases(self):
+        print("Drop DB ====")
         for connection_name in connections:
+            print("con => >>>>>", connection_name)
             if connection_name == "read_only":
                 continue
             connection = connections[connection_name]
