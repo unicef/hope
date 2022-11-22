25cd1a6a530bb30cfd82956008ce72459abb622b
Jan Romaniak <jan.romaniak@tivix.com>
2022-11-22 19:33:49 +0100

    fixes to tests

---
+++ backend/hct_mis_api/apps/core/memcache.py
@@ -0,0 +1,12 @@
+import re
+
+from django.core.cache.backends.locmem import LocMemCache as DjangoLocMemCache
+
+
+class LocMemCache(DjangoLocMemCache):
+    def delete_pattern(self, pattern):
+        regex_pattern = re.escape(pattern).replace("\\*", "(.*)")
+        key_pattern = self.make_key(regex_pattern)
+        for key in self._cache.keys():
+            if re.match(key_pattern, key):
+                self.delete(key)
