a43331db42173ce6a7ac6e947d266a86c07017a5
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-11-25 11:48:02 +0100

    WIP

--- backend/hct_mis_api/apps/core/management/sql.py
+++ backend/hct_mis_api/apps/core/management/sql.py
@@ -6,3 +6,15 @@ def sql_drop_tables(connection):
     tables_sql = ", ".join(connection.ops.quote_name(table) for table in tables)
     sql = f"DROP TABLE IF EXISTS {tables_sql} CASCADE;"
     return "\n".join(["BEGIN;", sql, "COMMIT;"])
+
+
+def drop_databases() -> None:
+    for connection_name in connections:
+        if connection_name == "read_only":
+            continue
+        connection = connections[connection_name]
+        with connection.cursor() as cursor:
+            sql = sql_drop_tables(connection)
+            if not sql:
+                continue
+            cursor.execute(sql)
