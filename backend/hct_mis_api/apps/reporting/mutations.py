0b3dd89e9e132170d50ca2d4afb4841710294a45
Pavlo Mokiichuk <pavlo.mokiichuk@tivix.com>
2022-11-29 11:19:32 +0100

    review refactor

--- backend/hct_mis_api/apps/reporting/mutations.py
+++ backend/hct_mis_api/apps/reporting/mutations.py
@@ -1,6 +1,7 @@
 import datetime
 import logging
 
+from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 
@@ -78,10 +79,9 @@ class CreateReport(ReportValidator, PermissionMutation):
             admin_area_ids = [decode_id_string(admin_area_id) for admin_area_id in admin_area_2_ids]
 
         if admin_area_ids:
-            admin_areas = [
-                get_object_or_404(Area, id=admin_area_id, area_type__country__name=business_area.name)
-                for admin_area_id in admin_area_ids
-            ]
+            admin_areas = Area.objects.filter(id__in=admin_area_ids, area_type__country__name=business_area.name)
+            if len(admin_area_ids) != len(admin_areas):
+                raise Http404("No Area matches the given query.")
 
         report = Report.objects.create(**report_vars)
         if admin_areas:
