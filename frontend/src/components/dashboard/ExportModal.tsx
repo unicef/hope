d7de72f84b0e9ff5ab3cb8415b27ed4d6125c54f
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2022-11-30 15:39:01 +0100

    dashboard- uncheck Export Modal checkboxes upon cancelling

--- frontend/src/components/dashboard/ExportModal.tsx
+++ frontend/src/components/dashboard/ExportModal.tsx
@@ -176,7 +176,14 @@ export const ExportModal = ({ filter, year }): React.ReactElement => {
         </DialogContent>
         <DialogFooter>
           <DialogActions>
-            <Button onClick={() => setDialogOpen(false)}>{t('CANCEL')}</Button>
+            <Button
+              onClick={() => {
+                setDialogOpen(false);
+                setSelected([]);
+              }}
+            >
+              {t('CANCEL')}
+            </Button>
             <Button
               type='submit'
               color='primary'
