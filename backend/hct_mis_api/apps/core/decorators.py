b7a77309d126af44b8a99275e8c3ac6a0825d7fa
Jan Romaniak <jan.romaniak@tivix.com>
2022-11-30 15:02:15 +0100

    fixes to logs

--- backend/hct_mis_api/apps/core/decorators.py
+++ backend/hct_mis_api/apps/core/decorators.py
@@ -1,10 +1,14 @@
+import hashlib
+import json
+
 from django.core.cache import cache
 
 
 def cached_in_django_cache(timeout_in_hours):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            key = f"{func.__name__}_{kwargs}"
+            hashed_args = hashlib.sha1(json.dumps(kwargs).encode()).hexdigest()
+            key = f"{func.__name__}_{hashed_args}"
             value = cache.get(key)
             if value is None:
                 value = func(*args, **kwargs)
