87adbb91e9de9fccf2c55e2b1be92914693e22c4
Jan Romaniak <jan.romaniak@tivix.com>
2022-11-30 14:27:50 +0100

    test cache

--- backend/hct_mis_api/apps/core/tests/test_cache.py
+++ backend/hct_mis_api/apps/core/tests/test_cache.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.db import models
 from django.db.models import Count
 from django.db.models.functions import Extract
@@ -5,82 +6,29 @@ from django.db.models.functions import Extract
 from hct_mis_api.apps.core.base_test_case import APITestCase
 from hct_mis_api.apps.core.fixtures import create_afghanistan
 from hct_mis_api.apps.core.models import BusinessArea
+from hct_mis_api.apps.core.utils import save_data_in_cache
 from hct_mis_api.apps.household.fixtures import create_household_and_individuals
 from hct_mis_api.apps.household.models import Individual, Household, REFUGEE, DISABILITY_CHOICES, DISABLED, NOT_DISABLED
 
 
-class Age(models.Func):
-    function = "AGE"
+class TestCache(APITestCase):
+    def test_simple_cache(self):
+        cache.clear()
+        self.assertEqual(cache.get("FOO", "NOTHING"), "NOTHING")
+        result_from_function = save_data_in_cache("FOO", lambda: "BAR")
+        self.assertEqual(result_from_function, "BAR")
+        self.assertEqual(cache.get("FOO", "NOTHING"), "BAR")
 
+    def test_conditional_false_cache(self):
+        cache.clear()
+        self.assertEqual(cache.get("FOO", "NOTHING"), "NOTHING")
+        result_from_function = save_data_in_cache("FOO", lambda: "BAR", cache_condition=lambda x: x != "BAR")
+        self.assertEqual(result_from_function, "BAR")
+        self.assertEqual(cache.get("FOO", "NOTHING"), "NOTHING")
 
-def get_households_at_least_3():
-    individuals = (
-        Individual.objects.annotate(new_age=Extract(Age("first_registration_date", "birth_date"), "year"))
-        .filter(new_age__lt=18, withdrawn=False)
-        .values("household_id")
-        .annotate(count_household=Count("id"))
-        .filter(count_household__gte=3)
-        .distinct()
-    )
-    return Household.objects.filter(id__in=individuals.values("household_id"), payment_records__isnull=True).distinct()
-
-
-def get_households_disabled():
-    individuals = (
-        Individual.objects.annotate(new_age=Extract(Age("first_registration_date", "birth_date"), "year"))
-        .filter(new_age__lt=18, disability=DISABLED, withdrawn=False)
-        .distinct()
-    )
-    return Household.objects.filter(id__in=individuals.values("household_id"), payment_records__isnull=True).distinct()
-
-
-class TestOptionalRecalculationOfIndividuals(APITestCase):
-    def test_get_households_3(self):
-        create_afghanistan()
-        business_area = BusinessArea.objects.get(slug="afghanistan")
-        create_household_and_individuals(
-            {"business_area": business_area},
-            individuals_data=[
-                {"birth_date": "2004-11-07", "first_registration_date": "2022-05-07"},
-                {"birth_date": "2004-11-08", "first_registration_date": "2022-05-07"},
-                {"birth_date": "2004-11-18", "first_registration_date": "2022-05-07"},
-            ],
-        )
-        households = get_households_at_least_3()
-        self.assertEqual(households.count(), 1)
-
-    def test_get_zero_households_3(self):
-        create_afghanistan()
-        business_area = BusinessArea.objects.get(slug="afghanistan")
-        create_household_and_individuals(
-            {"business_area": business_area},
-            individuals_data=[
-                {"birth_date": "2004-05-06", "first_registration_date": "2022-05-07"},
-                {"birth_date": "2004-11-08", "first_registration_date": "2022-05-07"},
-                {"birth_date": "2004-11-18", "first_registration_date": "2022-05-07"},
-            ],
-        )
-        households = get_households_at_least_3()
-        self.assertEqual(households.count(), 0)
-
-    def test_get_households_disability(self):
-        create_afghanistan()
-        business_area = BusinessArea.objects.get(slug="afghanistan")
-        create_household_and_individuals(
-            {"business_area": business_area},
-            individuals_data=[
-                {"birth_date": "2004-11-07", "first_registration_date": "2022-05-07", "disability": NOT_DISABLED},
-                {"birth_date": "2004-11-08", "first_registration_date": "2022-05-07", "disability": DISABLED},
-                {"birth_date": "2004-11-18", "first_registration_date": "2022-05-07", "disability": NOT_DISABLED},
-            ],
-        )
-        create_household_and_individuals(
-            {"business_area": business_area},
-            individuals_data=[
-                {"birth_date": "2004-11-07", "first_registration_date": "2022-05-07", "disability": NOT_DISABLED},
-                {"birth_date": "2004-11-08", "first_registration_date": "2022-05-07", "disability": NOT_DISABLED},
-                {"birth_date": "2004-11-18", "first_registration_date": "2022-05-07", "disability": NOT_DISABLED},
-            ],
-        )
-        households = get_households_disabled()
-        self.assertEqual(households.count(), 1)
+    def test_conditional_cache(self):
+        cache.clear()
+        self.assertEqual(cache.get("FOO", "NOTHING"), "NOTHING")
+        result_from_function = save_data_in_cache("FOO", lambda: "BAR", cache_condition=lambda x: x == "BAR")
+        self.assertEqual(result_from_function, "BAR")
+        self.assertEqual(cache.get("FOO", "NOTHING"), "BAR")
