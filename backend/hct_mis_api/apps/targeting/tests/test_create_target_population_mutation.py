effee208457337c3463ebf5ecc009c7421cb77d2
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-12-01 13:41:52 +0100

    Store test execution time in file for debugging purposes

--- backend/hct_mis_api/apps/targeting/tests/test_create_target_population_mutation.py
+++ backend/hct_mis_api/apps/targeting/tests/test_create_target_population_mutation.py
@@ -95,7 +95,7 @@ class TestCreateTargetPopulationMutation(APITestCase):
             ("without_permission", []),
         ]
     )
-    def test_create_mutation_with_comparison_method_contains(self, _, permissions):
+    def test_create_mutation_with_comparison_method_contains(self, _, permissions) -> None:
         self.create_user_role_with_permissions(self.user, permissions, self.program.business_area)
 
         variables = {
