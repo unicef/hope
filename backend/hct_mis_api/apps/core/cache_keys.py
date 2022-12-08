866efe6f51939e4b0aeaff030c143cb9687ae39f
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2022-12-08 12:58:28 +0000

    Format fix

--- backend/hct_mis_api/apps/core/cache_keys.py
+++ backend/hct_mis_api/apps/core/cache_keys.py
@@ -1,1 +1,3 @@
-PROGRAM_TOTAL_NUMBER_OF_HOUSEHOLDS_CACHE_KEY = "program_total_number_of_households_{}_{}"  # + business_area_id + program_id
+PROGRAM_TOTAL_NUMBER_OF_HOUSEHOLDS_CACHE_KEY = (
+    "program_total_number_of_households_{}_{}"  # + business_area_id + program_id
+)
