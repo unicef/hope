197a75fdcbefc38619cde0c1971383c80beefb9a
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-12-12 16:24:50 +0100

    Elaborate in todo

--- backend/hct_mis_api/apps/targeting/graphql_types.py
+++ backend/hct_mis_api/apps/targeting/graphql_types.py
@@ -123,8 +123,9 @@ class TargetPopulationNode(BaseNodePermissionMixin, DjangoObjectType):
 
     total_family_size = graphene.Int(source="total_family_size")
     targeting_criteria = TargetingCriteriaRuleFilterNode()
-    # TODO: make is snake_case
     householdList = DjangoFilterConnectionField(HouseholdNode, filterset_class=HouseholdFilter)
+    # TODO: make it snake_case (some snapshots fail)
+    # household_list = DjangoFilterConnectionField(HouseholdNode, filterset_class=HouseholdFilter)
 
     class Meta:
         model = target_models.TargetPopulation
