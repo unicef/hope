8e91318492c24be2ca885ece254dd99388524548
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2022-12-12 15:28:18 +0100

    WIP

--- backend/hct_mis_api/apps/targeting/tests/test_golden_record_by_targeting_criteria_rules_block_query.py
+++ backend/hct_mis_api/apps/targeting/tests/test_golden_record_by_targeting_criteria_rules_block_query.py
@@ -35,7 +35,7 @@ class GoldenRecordTargetingCriteriaWithBlockFiltersQueryTestCase(APITestCase):
             edges {
               node {
                 size
-                individuals {
+                individuals(orderBy: "sex") {
                     edges{
                         node{
                             sex
