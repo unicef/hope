aeea11137a924dbb76de9858a7b08531960f6fad
marekbiczysko <marek.biczysko@tivix.com>
2022-12-12 12:46:49 +0100

    migration fix

--- backend/hct_mis_api/apps/household/migrations/0128_migration.py
+++ backend/hct_mis_api/apps/household/migrations/0128_migration.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('household', '0123_migration'),
+        ('household', '0127_migration'),
     ]
 
     operations = [
