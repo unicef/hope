de896a7159ff9fc2b10d361641e43ca76bfc7f64
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2022-12-14 10:30:50 +0100

    [140223] Fix convert program id (#2050)
    * Fix convert program id
    
    * fixes for merge
    
    * fixes for merge
    
    Co-authored-by: Jan Romaniak <jan.romaniak@tivix.com>

--- backend/hct_mis_api/apps/household/services/household_programs_with_delivered_quantity.py
+++ backend/hct_mis_api/apps/household/services/household_programs_with_delivered_quantity.py
@@ -1,12 +1,26 @@
-from typing import Any, Dict
+from collections import defaultdict
+from decimal import Decimal
+from typing import Any, Dict, List, TypedDict
 
 from django.db.models import DecimalField, F, Sum
 
+from hct_mis_api.apps.core.utils import encode_id_base64_required
 from hct_mis_api.apps.household.models import Household
 from hct_mis_api.apps.payment.models import PaymentRecord
 
 
-def programs_with_delivered_quantity(household: Household) -> Dict[Any, Dict[str, Any]]:
+class QuantityType(TypedDict):
+    total_delivered_quantity: Decimal
+    currency: str
+
+
+class ProgramType(TypedDict):
+    id: str
+    name: str
+    quantity: List[QuantityType]
+
+
+def programs_with_delivered_quantity(household: Household) -> List[Dict[str, Any]]:
     programs = (
         household.payment_records.exclude(status=PaymentRecord.STATUS_FORCE_FAILED)
         .annotate(program=F("cash_plan__program"))
@@ -21,20 +35,20 @@ def programs_with_delivered_quantity(household: Household) -> Dict[Any, Dict[str
         .order_by("cash_plan__program__created_at")
     )
 
-    programs_dict = {}
+    programs_dict: Dict[str, ProgramType] = defaultdict(dict)
 
     for program in programs:
-        if program["program_id"] not in programs_dict.keys():
-            programs_dict[program["program_id"]] = {
-                "id": program["program_id"],
-                "name": program["program_name"],
-                "quantity": [
-                    {
-                        "total_delivered_quantity": program["total_delivered_quantity_usd"],
-                        "currency": "USD",
-                    }
-                ],
+        programs_dict[program["program_id"]]["id"] = encode_id_base64_required(program["program_id"], "Program")
+        programs_dict[program["program_id"]]["name"] = program["program_name"]
+        programs_dict[program["program_id"]]["quantity"] = programs_dict[program["program_id"]].get("quantity", [])
+
+        programs_dict[program["program_id"]]["quantity"].append(
+            {
+                "total_delivered_quantity": program["total_delivered_quantity_usd"],
+                "currency": "USD",
             }
+        )
+
         if program["currency"] != "USD":
             programs_dict[program["program_id"]]["quantity"].append(
                 {
@@ -42,4 +56,4 @@ def programs_with_delivered_quantity(household: Household) -> Dict[Any, Dict[str
                     "currency": program["currency"],
                 }
             )
-    return programs_dict.values()  # type: ignore
+    return list(programs_dict.values())
