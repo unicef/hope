8f11ac4843fc01a511f18c3eb122876bb8f2e6a9
saxix <s.apostolico@gmail.com>
2022-12-20 09:03:18 +0100

    ignore mypy warn

--- backend/hct_mis_api/apps/account/tests/test_user_roles.py
+++ backend/hct_mis_api/apps/account/tests/test_user_roles.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, List, Union
 
 from django.forms.models import inlineformset_factory
 from django.forms.utils import ErrorList
@@ -76,6 +76,5 @@ class UserRolesTest(TestCase):
         self.assertFalse(formset.is_valid())
         self.assertEqual(len(formset.errors), 2)
 
-        errors: List[ErrorList] = formset.errors
-        # TODO: No overload variant of "__getitem__" of "UserList" matches argument type "str"
+        errors: List[Union[ErrorList, Any]] = formset.errors
         self.assertIn(f"{self.role_1.name} is incompatible with {self.role_2.name}.", errors[0]["role"])
