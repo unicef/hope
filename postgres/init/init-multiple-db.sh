59e3003feda256912c4e109f8667a1330e37d4c8
Jan Romaniak <jan.romaniak@tivix.com>
2023-01-13 13:25:33 +0100

    changed to postgres 14

--- postgres/init/init-multiple-db.sh
+++ postgres/init/init-multiple-db.sh
@@ -4,12 +4,12 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -h localhost <<-EOSQL
 	    CREATE USER $database;
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
-  psql --dbname="$database" -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  psql --dbname="$database" -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -h localhost <<-EOSQL
 		CREATE EXTENSION IF NOT EXISTS postgis;
 		CREATE EXTENSION IF NOT EXISTS postgis_topology;
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
