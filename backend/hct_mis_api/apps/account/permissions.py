c66f1634852d24d1fe75d3b4df2ce341de61ae03
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2023-01-24 11:05:23 +0100

    Revert force failed payment record

--- backend/hct_mis_api/apps/account/permissions.py
+++ backend/hct_mis_api/apps/account/permissions.py
@@ -2,7 +2,7 @@ import logging
 from collections import OrderedDict
 from enum import Enum, auto, unique
 from functools import partial
-from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Type
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Type, Union
 
 from django.core.exceptions import PermissionDenied
 from django.db.models import Model
@@ -360,7 +360,9 @@ class BaseMutationPermissionMixin:
         return True
 
     @classmethod
-    def has_permission(cls, info: Any, permission: Any, business_area_arg: str, raise_error: bool = True) -> bool:
+    def has_permission(
+        cls, info: Any, permission: Any, business_area_arg: Union[str, BusinessArea], raise_error: bool = True
+    ) -> bool:
         cls.is_authenticated(info)
         permissions: Iterable = (permission,) if not isinstance(permission, list) else permission
         if isinstance(business_area_arg, BusinessArea):
