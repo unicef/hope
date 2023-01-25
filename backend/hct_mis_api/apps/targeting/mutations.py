baf2e2b1c0bc2a0ded79cdc6420f020a1ef95a01
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-01-25 12:39:52 +0100

    revert typing

--- backend/hct_mis_api/apps/targeting/mutations.py
+++ backend/hct_mis_api/apps/targeting/mutations.py
@@ -227,8 +227,7 @@ class UpdateTargetPopulationMutation(PermissionMutation, ValidationErrorMutation
         if targeting_criteria_input:
             should_rebuild_list = True
             TargetingCriteriaInputValidator.validate(targeting_criteria_input)
-            tp_program: Optional[Program] = target_population.program
-            targeting_criteria = from_input_to_targeting_criteria(targeting_criteria_input, tp_program)
+            targeting_criteria = from_input_to_targeting_criteria(targeting_criteria_input, target_population.program)
             if target_population.status == TargetPopulation.STATUS_OPEN:
                 if target_population.targeting_criteria:
                     target_population.targeting_criteria.delete()
