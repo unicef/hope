12c4b53444fb8a81171265a61c943a01343504b7
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-01-30 14:57:16 +0100

    Stabilize tests

--- backend/hct_mis_api/apps/registration_datahub/tests/test_rdi_merge.py
+++ backend/hct_mis_api/apps/registration_datahub/tests/test_rdi_merge.py
@@ -136,6 +136,7 @@ class TestRdiMergeTask(BaseElasticSearchTestCase):
                 "family_name": "Franklin",
                 "relationship": BROTHER_SISTER,
                 "birth_date": "1996-11-29",  # age 25
+                "phone_no": "this-is-not-a-phone-number",
                 "sex": "FEMALE",
                 "registration_data_import": cls.rdi_hub,
                 "phone_no_valid": None,
