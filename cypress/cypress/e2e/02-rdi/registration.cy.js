0b796191e6bdbd243f7c374ebf70332badc2fa8f
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-01-31 15:01:03 +0100

    add initial rdi scenario

--- cypress/cypress/e2e/02-rdi/registration.cy.js
+++ cypress/cypress/e2e/02-rdi/registration.cy.js
@@ -1,5 +1,17 @@
 /// <reference types="cypress" />
 
 context("RDI", () => {
-  it("Registration Data Import", () => {});
+  beforeEach(() => {
+    cy.visit("/api/unicorn/");
+    cy.get('input[name="username"]').type(Cypress.env("username"));
+    cy.get('input[name="password"]').type(Cypress.env("password"));
+    cy.get("input").contains("Log in").click();
+  });
+  it("Registration Data Import", () => {
+    cy.visit("/");
+    cy.get("span").contains("Registration Data Import").click();
+    cy.get("h5").contains("Registration Data Import");
+    cy.get("button > span").contains("IMPORT").click({ force: true });
+    cy.get("h2").contains("Select File to Import").click();
+  });
 });
