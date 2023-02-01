9bc043683a63ce5119140cbd1637529a13d0f062
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-02-01 10:43:20 +0100

    rdi test works

--- cypress/cypress/e2e/02-rdi/registration.cy.js
+++ cypress/cypress/e2e/02-rdi/registration.cy.js
@@ -11,6 +11,9 @@ context("RDI", () => {
     cy.get("input").contains("Log in").click();
   });
   it("Registration Data Import", () => {
+    let householdId;
+    let individualId;
+
     cy.visit("/");
     cy.get("span").contains("Registration Data Import").click();
     cy.get("h5").contains("Registration Data Import");
@@ -29,14 +32,14 @@ context("RDI", () => {
           fileName,
           mimeType:
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-          encoding: "base64"
+          encoding: "base64",
         });
       });
     });
     cy.get('[data-cy="number-of-households"]').contains(
       "1 Household available to import",
       {
-        timeout: 10000
+        timeout: 10000,
       }
     );
     cy.get('[data-cy="number-of-individuals"]').contains(
@@ -44,6 +47,7 @@ context("RDI", () => {
     );
     cy.get("div").contains("Errors").should("not.exist");
     cy.get('[data-cy="button-import-rdi"').click();
+
     cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
     cy.reload();
     cy.wait(500); // eslint-disable-line cypress/no-unnecessary-waiting
@@ -51,5 +55,51 @@ context("RDI", () => {
 
     cy.get("div").contains("IMPORT ERROR").should("not.exist");
     cy.get("div").contains("IN REVIEW");
+
+    cy.get("span").contains("Merge").click({ force: true }); // top of page
+    cy.get("span").contains("MERGE").click({ force: true }); // inside modal
+
+    cy.get("div").contains("MERGING");
+
+    cy.reload();
+
+    cy.get("div").contains("MERGED");
+    cy.log("Looking for householdId");
+    cy.get('[data-cy="imported-households-row"]')
+      .find("td:nth-child(2)")
+      .then(($td) => {
+        householdId = $td.text().split(" (")[0];
+        cy.log(`Saved householdId: ${householdId}`);
+      })
+      .then(() => {
+        cy.get("button > span").contains("Individuals").click({ force: true });
+
+        cy.get('[data-cy="imported-individuals-table"]')
+          .find(`tbody > tr:nth-child(1) > td:nth-child(1)`)
+          .then(($td) => {
+            individualId = $td.text().split(" (")[0];
+            cy.log(`Saved individualId: ${individualId}`);
+          })
+          .then(() => {
+            cy.get("span").contains("Population").click();
+            cy.get("span").contains("Households").click();
+
+            cy.log(`looking for householdId: ${householdId}`);
+            cy.get('[data-cy="hh-filters-search"]')
+              .find("input")
+              .type(householdId, { force: true });
+            cy.get("td").should("contain", householdId);
+
+            cy.get("span").contains("Individuals").click({ force: true });
+
+            cy.log(`looking for individualId: + ${individualId}`);
+            cy.get('[data-cy="ind-filters-search"]').type(individualId);
+            cy.get("td").should("contain", individualId);
+
+            cy.get("td").contains(householdId).click({ force: true });
+
+            // cy.get('[data-cy="label-COLLECT TYPE"]').contains("Full"); // TODO
+          });
+      });
   });
 });
