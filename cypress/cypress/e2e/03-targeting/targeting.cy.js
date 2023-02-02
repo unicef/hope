138c18d784a3c03d237844c759aa301584d7bc52
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-02-02 09:56:32 +0100

    add a little bit of targeting scenario

--- cypress/cypress/e2e/03-targeting/targeting.cy.js
+++ cypress/cypress/e2e/03-targeting/targeting.cy.js
@@ -20,40 +20,47 @@ context("Targeting", () => {
     cy.get('[data-cy="button-target-population-create-new"]').click({
       force: true
     });
+    cy.uniqueSeed().then((seed) => {
+      const targetPopulationName = `test TP ${seed}`;
+      cy.get('[data-cy="input-name"]')
+        .eq(1)
+        .type(targetPopulationName, { force: true });
+      cy.get('[data-cy="input-program"]').first().click();
+      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.get(`[data-cy="select-option-${programName}-${seed}"]`).click();
+      cy.get('[data-cy="button-target-population-add-criteria"]').click();
 
-    const targetPopulationName = `test TP ${uniqueSeed}`;
-    cy.get('[data-cy="input-name"]').first().type(targetPopulationName);
-    cy.get('[data-cy="input-program"]').first().click();
-    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-    cy.get(`[data-cy="select-option-${programName}-${uniqueSeed}"]`).click();
-    cy.get('[data-cy="button-target-population-add-criteria"]').click();
-
-    cy.get('[data-cy="button-household-rule"]', {
-      timeout: 10000
-    }).click();
-    cy.get('[data-cy="autocomplete-target-criteria"]').click().type("address");
-    cy.contains("Address").click();
-    cy.get('[data-cy="input-filters[0].value"]')
-      .click()
-      .type(`TargetingVille-${uniqueSeed}`);
-    cy.get('[data-cy="button-target-population-add-criteria"]').eq(1).click();
-    cy.get("h6").contains("Households");
-    cy.get(
-      "[data-cy=button-target-population-create] > .MuiButton-label"
-    ).click();
-    cy.get("h6").contains("Targeting Criteria");
-    cy.get('[data-cy="status-container"]').contains("Open");
-    cy.get('[data-cy="button-target-population-lock"]').click({ force: true });
-    cy.get('[data-cy="button-target-population-modal-lock"]').click({
-      force: true
+      cy.get('[data-cy="button-household-rule"]', {
+        timeout: 10000
+      }).click();
+      cy.get('[data-cy="autocomplete-target-criteria"]')
+        .click()
+        .type("address");
+      cy.contains("Address").click();
+      cy.get('[data-cy="input-filters[0].value"]')
+        .click()
+        .type(`TargetingVille-${seed}`);
+      cy.get('[data-cy="button-target-population-add-criteria"]').eq(1).click();
+      cy.get("h6").contains("Households");
+      cy.get(
+        "[data-cy=button-target-population-create] > .MuiButton-label"
+      ).click();
+      cy.get("h6").contains("Targeting Criteria");
+      cy.get('[data-cy="status-container"]').contains("Open");
+      cy.get('[data-cy="button-target-population-lock"]').click({
+        force: true
+      });
+      cy.get('[data-cy="button-target-population-modal-lock"]').click({
+        force: true
+      });
+      cy.get("h6").contains("Targeting Criteria");
+      cy.get('[data-cy="status-container"]').contains("Locked");
+      cy.get('[data-cy="button-target-population-send-to-hope"]').click({
+        force: true
+      });
+      cy.get('[data-cy="button-target-population-modal-send-to-hope"]').click();
+      cy.get("h6").contains("Targeting Criteria");
+      cy.get('[data-cy="status-container"]').contains("Ready");
     });
-    cy.get("h6").contains("Targeting Criteria");
-    cy.get('[data-cy="status-container"]').contains("Locked");
-    cy.get('[data-cy="button-target-population-send-to-hope"]').click({
-      force: true
-    });
-    cy.get('[data-cy="button-target-population-modal-send-to-hope"]').click();
-    cy.get("h6").contains("Targeting Criteria");
-    cy.get('[data-cy="status-container"]').contains("Ready");
   });
 });
