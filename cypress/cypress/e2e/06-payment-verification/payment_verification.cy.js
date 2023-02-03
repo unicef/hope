be340a4f3a817e1199dc46827f59d0664d556c6b
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-02-03 09:20:11 +0100

    payment verification scenario

--- cypress/cypress/e2e/06-payment-verification/payment_verification.cy.js
+++ cypress/cypress/e2e/06-payment-verification/payment_verification.cy.js
@@ -1,5 +1,22 @@
 /// <reference types="cypress" />
 
 context("Payment Verification", () => {
-  it("Can create a verification plan", () => {});
+  beforeEach(() => {
+    cy.visit("/api/unicorn/");
+    cy.get('input[name="username"]').type(Cypress.env("username"));
+    cy.get('input[name="password"]').type(Cypress.env("password"));
+    cy.get("input").contains("Log in").click();
+  });
+
+  it("Can see the Cash Plan Details Page", () => {
+    cy.visit("/");
+    cy.get("span").contains("Payment Verification").click();
+    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+    cy.get("h6").contains("List of Cash Plans");
+    cy.get('[data-cy="cash-plan-table-row"]').first().click();
+    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+    cy.get('[data-cy="page-header-container"]').contains("Payment Plan");
+    cy.get("h6").contains("Cash Plan Details");
+    cy.get("h6").contains("Verification Plans Summary");
+  });
 });
