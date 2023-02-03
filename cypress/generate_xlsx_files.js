b25960988944420867146d98b905cb96fd5d93bb
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-02-03 13:40:14 +0100

    fix

--- cypress/generate_xlsx_files.js
+++ cypress/generate_xlsx_files.js
@@ -16,13 +16,5 @@ const seed = args[2];
 const command = `curl -X POST ${serverAddress}/api/cypress/ --data "command=generate-xlsx-files&size=${size}&seed=${seed}"`;
 exec(command);
 
-// const copy = require("copy");
-// copy("../backend/generated/*", "cypress/fixtures", function (err, files) {
-//   if (err) throw err;
-//   console.log("Files copied successfully");
-// });
-
-// move to cypress/fixtures and call curl -OJ http://localhost:8082/api/cypress/xlsx/<seed>/ there
-
-const copyCommand = `cd cypress/fixtures && curl -OJ http://localhost:8082/api/cypress/xlsx/${seed}/`;
+const copyCommand = `cd cypress/fixtures && curl -OJ ${serverAddress}/api/cypress/xlsx/${seed}/`;
 exec(copyCommand);
