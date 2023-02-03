d80e06ac4e17c119405767e26acbba7a8f7da237
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-02-03 13:22:49 +0100

    all tests should be working on local in container

--- cypress/generate_xlsx_files.js
+++ cypress/generate_xlsx_files.js
@@ -16,8 +16,13 @@ const seed = args[2];
 const command = `curl -X POST ${serverAddress}/api/cypress/ --data "command=generate-xlsx-files&size=${size}&seed=${seed}"`;
 exec(command);
 
-const copy = require("copy");
-copy("../backend/generated/*", "cypress/fixtures", function (err, files) {
-  if (err) throw err;
-  console.log("Files copied successfully");
-});
+// const copy = require("copy");
+// copy("../backend/generated/*", "cypress/fixtures", function (err, files) {
+//   if (err) throw err;
+//   console.log("Files copied successfully");
+// });
+
+// move to cypress/fixtures and call curl -OJ http://localhost:8082/api/cypress/xlsx/<seed>/ there
+
+const copyCommand = `cd cypress/fixtures && curl -OJ http://localhost:8082/api/cypress/xlsx/${seed}/`;
+exec(copyCommand);
