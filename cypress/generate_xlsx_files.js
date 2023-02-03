acee4087554a4786955e263d7f9396684dd1907d
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-02-03 10:52:21 +0100

    init scenario with curl instead of docker compose

--- cypress/generate_xlsx_files.js
+++ cypress/generate_xlsx_files.js
@@ -13,7 +13,6 @@ const size = args[0];
 const seed = args[1];
 
 const command = `curl -X POST http://localhost:8082/api/cypress/ --data "command=generate-xlsx-files&size=${size}&seed=${seed}"`;
-// const command = `cd ../ && docker-compose run --rm backend ./manage.py generate_rdi_xlsx_files ${size} --seed ${seed}`;
 exec(command);
 
 const copy = require("copy");
