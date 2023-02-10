4d9a538556f5b0b71f1d83aa21f6b1edf543ce03
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-02-10 11:11:29 +0100

    review

--- backend/hct_mis_api/apps/payment/services/payment_plan_services.py
+++ backend/hct_mis_api/apps/payment/services/payment_plan_services.py
@@ -126,9 +126,7 @@ class PaymentPlanService:
         self.payment_plan.status_unlock()
         self.payment_plan.update_population_count_fields()
         self.payment_plan.update_money_fields()
-        if self.payment_plan.has_export_file:
-            self.payment_plan.export_file.delete()
-            self.payment_plan.export_file = None
+        self.payment_plan.remove_export_file()
 
         self.payment_plan.save()
 
