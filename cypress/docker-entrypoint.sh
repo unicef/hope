194daf962f978625a9e08c788fcf016758f779ae
kamilswiec <kamilswiec@protonmail.com>
2023-02-13 15:17:02 +0200

    try with building services before cypress test

--- cypress/docker-entrypoint.sh
+++ cypress/docker-entrypoint.sh
@@ -10,11 +10,8 @@ case "$1" in
         waitforit -host=frontend -port=80 --timeout 300
         echo "Waiting for proxy to be ready"
         waitforit -host=proxy -port=80 --timeout 300
-
-        yarn cypress run --headless \
-            --config baseUrl=http://proxy \
-            --reporter junit \
-            --reporter-options "mochaFile=cypress/results/results-[hash].xml,toConsole=true"
+        echo "Staring cypress tests..."
+        exec yarn test
         ;;
     *)
         exec "$@"
