4d34dfba51bec41be399b4a4e32ee115909aaee1
kamilswiec <kamilswiec@protonmail.com>
2023-02-15 09:32:14 +0200

    fix:update dockerfiles

--- cypress/docker-entrypoint.sh
+++ cypress/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 case "$1" in
-    "test")
+    "ci-test")
         echo "Waiting for backend to be ready"
         waitforit -host=backend -port=8000 --timeout 300
         echo "Waiting for frontend to be ready"
@@ -11,7 +11,10 @@ case "$1" in
         echo "Waiting for proxy to be ready"
         waitforit -host=proxy -port=80 --timeout 300
         echo "Staring cypress tests..."
-        exec yarn test
+        exec yarn cypress run --headless --config \
+          baseUrl=http://proxy,video=false \
+          --reporter junit --reporter-options \
+          "mochaFile=/code/cypress/results/results-[hash].xml,toConsole=true"
         ;;
     *)
         exec "$@"
