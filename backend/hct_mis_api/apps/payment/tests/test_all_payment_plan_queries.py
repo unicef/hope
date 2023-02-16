b74d5323484e363fe6d6b6106f79d0004bf20e1a
marekbiczysko <marek.biczysko@tivix.com>
2023-02-16 16:34:45 +0100

    store historical acceptance process required numbers

--- backend/hct_mis_api/apps/payment/tests/test_all_payment_plan_queries.py
+++ backend/hct_mis_api/apps/payment/tests/test_all_payment_plan_queries.py
@@ -63,9 +63,6 @@ class TestPaymentPlanQueries(APITestCase):
             approvalProcess{
               totalCount
             }
-            approvalNumberRequired
-            authorizationNumberRequired
-            financeReleaseNumberRequired
             paymentsConflictsCount
           }
         }
