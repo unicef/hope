ea966fe046659662fa20932779f2c3772cffe159
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-02-20 10:03:21 +0100

    change options

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -115,11 +115,11 @@ export function paymentRecordStatusToColor(
   status: string,
 ): string {
   switch (status) {
-    case 'TRANSACTION_SUCCESSFUL':
+    case 'Transaction Successful':
       return theme.hctPalette.green;
-    case 'DISTRIBUTION_SUCCESSFUL':
+    case 'Distribution Successful':
       return theme.hctPalette.green;
-    case 'TRANSACTION_PENDING':
+    case 'Pending':
       return theme.hctPalette.orange;
     default:
       return theme.palette.error.main;
