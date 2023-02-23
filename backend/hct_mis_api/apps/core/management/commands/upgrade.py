f51e00fb3223f3405f0a62384262673e8de77ec4
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2023-02-23 11:56:48 +0100

    Fix upgrade command
    Run the command after the application is fully loaded

--- backend/hct_mis_api/apps/core/management/commands/upgrade.py
+++ backend/hct_mis_api/apps/core/management/commands/upgrade.py
@@ -1,22 +1,22 @@
+from time import sleep
 from typing import Any
 
+from django.apps import apps
 from django.core.management import BaseCommand, call_command
 
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> None:
+        while not apps.ready:
+            # Wait until the application started
+            sleep(1)
+
         call_command("collectstatic", interactive=False)
         call_command("migratealldb")
         call_command("generateroles")
         from adminactions.perms import create_extra_permissions
 
-        try:
-            create_extra_permissions()
-        except AttributeError:
-            # kinda hacky but this is the only way for now to make it work
-            # first time, we get AttributeError: 'NoneType' object has no attribute '_meta'
-            # but the second time, we don't
-            create_extra_permissions()
+        create_extra_permissions()
 
         from hct_mis_api.apps.power_query.defaults import create_defaults
 
