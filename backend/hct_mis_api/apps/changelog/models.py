395a025755f999513a260514dd571999d21508dc
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2023-02-27 13:47:31 +0200

    Default today

--- backend/hct_mis_api/apps/changelog/models.py
+++ backend/hct_mis_api/apps/changelog/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -9,7 +11,7 @@ class Changelog(models.Model):
     description = models.TextField(blank=True)
     version = models.CharField(max_length=30, help_text=_("HOPE version"))
     active = models.BooleanField(default=False)
-    date = models.DateField()
+    date = models.DateField(default=datetime.date.today)
 
     class Meta:
         ordering = ("-date",)
