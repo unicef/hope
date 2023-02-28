529450c3e4796be666953c93e1c4d16919f4f780
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-02-28 16:54:27 +0100

    create copy old tmp file

--- backend/hct_mis_api/apps/payment/mutations.py
+++ backend/hct_mis_api/apps/payment/mutations.py
@@ -684,6 +684,8 @@ class ActionPaymentPlanMutation(PermissionMutation):
         payment_plan_id = decode_id_string(input.get("payment_plan_id"))
         payment_plan = get_object_or_404(PaymentPlan, id=payment_plan_id)
         old_payment_plan = copy_model_object(payment_plan)
+        if old_payment_plan.imported_file:
+            old_payment_plan.imported_file = copy_model_object(payment_plan.imported_file)
 
         cls.check_permissions(info, payment_plan.business_area, input.get("action", ""), payment_plan.status)
 
