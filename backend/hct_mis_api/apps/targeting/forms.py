6ba13affd0f2bae1eb41fe21fdeab8b35e8741d9
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2023-03-10 12:34:42 +0200

    Criteria needed

--- backend/hct_mis_api/apps/targeting/forms.py
+++ backend/hct_mis_api/apps/targeting/forms.py
@@ -11,8 +11,11 @@ class TargetPopulationForm(forms.ModelForm):
 
     def clean_program(self) -> Program:
         program = self.cleaned_data["program"]
-        if program is None:
-            raise forms.ValidationError("Program must be set")
-        elif self.cleaned_data["program"].business_area != self.cleaned_data["business_area"]:
+        import ipdb
+
+        ipdb.set_trace()
+        if program is None or program.status == "FINISHED":
+            raise forms.ValidationError("Program must be set and active")
+        elif program.business_area != self.cleaned_data["business_area"]:
             raise forms.ValidationError("The program must be opened in the business_area")
         return program
