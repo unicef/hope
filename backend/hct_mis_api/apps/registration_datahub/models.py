5db49f31e71cb5ac056c5dab936c13c046987d79
Jan Romaniak <jan.romaniak@tivix.com>
2023-03-17 08:41:14 +0100

    fix xlsx import

--- backend/hct_mis_api/apps/registration_datahub/models.py
+++ backend/hct_mis_api/apps/registration_datahub/models.py
@@ -439,7 +439,7 @@ class DocumentValidator(TimeStampedUUIDModel):
 
 class ImportedDocumentType(TimeStampedUUIDModel):
     label = models.CharField(max_length=100)
-    type = models.CharField(max_length=50, choices=IDENTIFICATION_TYPE_CHOICE)
+    key = models.CharField(max_length=50, unique=True)
     is_identity_document = models.BooleanField(default=True)
 
     def __str__(self) -> str:
