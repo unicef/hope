bab207d1c43a0b6af970ad8c04fd79f6f0d873f8
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-03-21 10:54:06 +0100

    upd tests & move clear cache to quick links

--- backend/hct_mis_api/apps/payment/tests/snapshots/snap_test_financial_service_provider.py
+++ backend/hct_mis_api/apps/payment/tests/snapshots/snap_test_financial_service_provider.py
@@ -7,6 +7,14 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
+snapshots['TestAllFinancialServiceProviders::test_create_financial_service_provider 1'] = {
+    'errors': [
+        {
+            'message': 'Object of type set is not JSON serializable'
+        }
+    ]
+}
+
 snapshots['TestAllFinancialServiceProviders::test_fetch_all_financial_service_providers 1'] = {
     'data': {
         'allFinancialServiceProviders': {
@@ -103,4 +111,19 @@ snapshots['TestAllFinancialServiceProviders::test_fetch_count_financial_service_
             'totalCount': 10
         }
     }
+}
+
+snapshots['TestAllFinancialServiceProviders::test_update_financial_service_provider 1'] = {
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 9,
+                    'line': 5
+                }
+            ],
+            'message': '''Variable "$inputs" got invalid value {"communicationChannel": "XLSX", "deliveryMechanisms": ["Transfer", "Cash"], "distributionLimit": "100999", "name": "New Gen Bank", "visionVendorNumber": "XYZB-123456799"}.
+In field "fspXlsxTemplateId": Expected "ID!", found null.'''
+        }
+    ]
 }
