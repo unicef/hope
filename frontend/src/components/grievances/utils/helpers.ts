07d7f9562d361fe29dfc549ef965f205eb42f642
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-03-22 21:25:18 +0100

    fix issue for editing multiple documents in grievance ticket

--- frontend/src/components/grievances/utils/helpers.ts
+++ frontend/src/components/grievances/utils/helpers.ts
@@ -13,3 +13,23 @@ export const handleSelected = (
   }
   setFieldValue(arrayName, newSelected);
 };
+
+export const removeDocument = (
+  documents,
+  docId: string,
+  arrayHelpers,
+): void => {
+  const index = documents?.findIndex((item) => item.id === docId);
+  if (index !== -1) {
+    arrayHelpers.remove(index);
+  }
+};
+
+export const getIndexForId = (array, id?: string): number => {
+  if (!id) {
+    return -1;
+  }
+
+  const index = array.findIndex((item) => item.id === id);
+  return index !== -1 ? index : 0;
+};
