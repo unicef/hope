53b6c18d77d9e2ecd59ae027a8738cb84e31fce1
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-03-22 17:04:07 +0100

    Create BaseCeleryManager

--- backend/hct_mis_api/apps/registration_datahub/celery_manager.py
+++ backend/hct_mis_api/apps/registration_datahub/celery_manager.py
@@ -1,50 +1,98 @@
+import abc
 import logging
 
 from hct_mis_api.apps.core.celery import app
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
 from hct_mis_api.apps.registration_datahub.models import RegistrationDataImportDatahub
-from hct_mis_api.apps.registration_datahub.celery_tasks import registration_xlsx_import_task
+from hct_mis_api.apps.registration_datahub.celery_tasks import registration_xlsx_import_task, \
+    merge_registration_data_import_task
 
 logger = logging.getLogger(__name__)
 
 
-def check_rdi_imports() -> None:
-    i = app.control.inspect()
-    scheduled = i.scheduled()
-    active = i.active()
-    reserved = i.reserved()
+class BaseCeleryManager(abc.ABC):
+    model = ""
+    model_status = ""
+    task = ""
+    lookup = ""
 
-    importing_rdi_list = list(
-        RegistrationDataImport.objects.filter(status=RegistrationDataImport.IMPORTING).values_list("id", flat=True)
-    )
+    def create_obj_list(self):
+        return list(self.model.objects.filter(status=self.model_status).values_list("id", flat=True))
 
-    celery_rdi_ids_list = []
-    for group in (scheduled, active, reserved):
-        for _, task_list in group.items():
-            for task in task_list:
-                task_name = task["name"].split(".")[-1]
-                if task_name == "registration_xlsx_import_task":
-                    celery_rdi_ids_list.append(task["kwargs"].get("registration_data_import_id"))
+    @staticmethod
+    def get_celery_tasks():
+        i = app.control.inspect()
+        return i.scheduled(), i.active(), i.reserved()
 
-    rdi_to_run = []
-    for rdi_id in importing_rdi_list:
-        if rdi_id not in celery_rdi_ids_list:
-            rdi_to_run.append(rdi_id)
+    @abc.abstractmethod
+    def create_kwargs(self, rdi_ids_to_run):
+        pass
 
-    if not rdi_to_run:
-        logger.info("No RDI in status IMPORTING found")
+    def execute(self):
+        list_to_check = self.create_obj_list()
+        celery_ids_list = []
 
-    registration_xlsx_import_task_kwargs = []
-    for rdi_id in rdi_to_run:
-        rdi = RegistrationDataImport.objects.get(id=rdi_id)
-        rdi_datahub = RegistrationDataImportDatahub.objects.get(id=str(rdi.datahub_id))
-        kwargs = {
-            'registration_data_import_id': str(rdi_datahub.id),
-            'import_data_id': str(rdi_datahub.import_data_id),
-            'business_area_id': str(rdi.business_area.id)
-        }
-        registration_xlsx_import_task_kwargs.append(kwargs)
+        for group in [*self.get_celery_tasks()]:
+            for _, task_list in group.items():
+                for task in task_list:
+                    task_name = task["name"].split(".")[-1]
+                    if task_name == self.task.__name__:
+                        celery_ids_list.append(task["kwargs"].get(self.lookup))
 
-    for kwargs in registration_xlsx_import_task_kwargs:
-        registration_xlsx_import_task.delay(**kwargs)
-        logger.info(f"RDI import task run for id {kwargs['registration_data_import_id']} from periodic task scheduler")
+        rdi_ids_to_run = []
+        for rdi_id in list_to_check:
+            if rdi_id not in celery_ids_list:
+                rdi_ids_to_run.append(rdi_id)
+
+        if not rdi_ids_to_run:
+            logger.info(f"Found no {self.model.__name__} with status {self.model_status}")
+
+        task_kwargs = self.create_kwargs(rdi_ids_to_run)
+        for kwargs in task_kwargs:
+            self.task.delay(**kwargs)
+            logger.info(
+                f"Celery task run for {self.model.__name__} id: {kwargs[self.lookup]} from periodic task scheduler")
+
+
+class RdiImportCeleryManager(BaseCeleryManager):
+    model = RegistrationDataImport
+    model_status = RegistrationDataImport.IMPORTING
+    task = registration_xlsx_import_task
+    lookup = "registration_data_import_id"
+
+    def create_kwargs(self, rdi_ids_to_run):
+        registration_xlsx_import_task_kwargs = []
+        for rdi_id in rdi_ids_to_run:
+            rdi = RegistrationDataImport.objects.get(id=rdi_id)
+            rdi_datahub = RegistrationDataImportDatahub.objects.get(id=str(rdi.datahub_id))
+            kwargs = {
+                'registration_data_import_id': str(rdi_datahub.id),
+                'import_data_id': str(rdi_datahub.import_data_id),
+                'business_area_id': str(rdi.business_area.id)
+            }
+            registration_xlsx_import_task_kwargs.append(kwargs)
+        return registration_xlsx_import_task_kwargs
+
+
+class RdiMergeCeleryManager(BaseCeleryManager):
+    model = RegistrationDataImport
+    model_status = RegistrationDataImport.MERGING
+    task = merge_registration_data_import_task
+    lookup = "registration_data_import_id"
+
+    def create_kwargs(self, rdi_ids_to_run):
+        merge_registration_data_import_task_kwargs = []
+        for rdi_id in rdi_ids_to_run:
+            merge_registration_data_import_task_kwargs.append({'registration_data_import_id': str(rdi_id)})
+        return merge_registration_data_import_task_kwargs
+
+
+class SendTPCeleryManager(BaseCeleryManager):
+
+    def create_kwargs(self, rdi_ids_to_run):
+        pass
+
+
+rdi_import_celery_manager = RdiImportCeleryManager()
+rdi_merge_celery_manager = RdiMergeCeleryManager()
+send_tp_celery_manager = SendTPCeleryManager()
