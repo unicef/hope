4949b4d6b99cd43f99dcda5869502816ccee7604
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-03-24 15:26:59 +0100

    black

--- backend/hct_mis_api/apps/payment/schema.py
+++ backend/hct_mis_api/apps/payment/schema.py
@@ -1312,7 +1312,7 @@ class Query(graphene.ObjectType):
 
     def resolve_all_payment_records_and_payments(self, info: Any, **kwargs: Any) -> Dict[str, Any]:
         # query AllPaymentRecordsAndPayments
-        """ used in Household Page > Payment Records """
+        """used in Household Page > Payment Records"""
         qs = ExtendedQuerySetSequence(PaymentRecord.objects.all(), Payment.objects.exclude(excluded=True)).order_by(
             "-updated_at"
         )
