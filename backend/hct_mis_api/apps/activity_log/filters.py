a2d94ed78f01c0655a7f76d3ebaef813193532cc
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-03-28 12:26:14 +0200

    Filter by userId

--- backend/hct_mis_api/apps/activity_log/filters.py
+++ backend/hct_mis_api/apps/activity_log/filters.py
@@ -2,9 +2,10 @@ from typing import TYPE_CHECKING
 
 from django.db.models import Q
 
-from django_filters import CharFilter, FilterSet
+from django_filters import CharFilter, FilterSet, UUIDFilter
 
 from hct_mis_api.apps.activity_log.models import LogEntry
+from hct_mis_api.apps.core.utils import decode_id_string_required
 
 if TYPE_CHECKING:
     from django.db.models.query import QuerySet
@@ -14,11 +15,11 @@ class LogEntryFilter(FilterSet):
     business_area = CharFilter(field_name="business_area__slug", required=True)
     search = CharFilter(method="search_filter")
     module = CharFilter(field_name="content_type__model")
-    user_id = CharFilter(field_name="user__id")
+    user_id = CharFilter(method="search_user")
 
     class Meta:
         model = LogEntry
-        fields = ("object_id",)
+        fields = ("object_id", "user")
 
     def search_filter(self, qs: "QuerySet", name: str, value: str) -> "QuerySet[LogEntry]":
         values = value.split(" ")
@@ -32,3 +33,6 @@ class LogEntryFilter(FilterSet):
             q_obj |= Q(object_repr__startswith=value)
             q_obj |= Q(timestamp__startswith=value)
         return qs.filter(q_obj)
+
+    def search_user(self, qs: "QuerySet", name: str, value: str) -> "QuerySet[LogEntry]":
+        return qs.filter(user__id=decode_id_string_required(value))
