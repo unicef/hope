85cdf542ed36f7f9d95d0a6aa32af652192dfd27
Marek Biczysko <34810846+MarekBiczysko@users.noreply.github.com>
2023-04-03 15:13:24 +0200

    154612 accountabitly communication recipients list (#2457)

--- backend/hct_mis_api/apps/accountability/services/message_crud_services.py
+++ backend/hct_mis_api/apps/accountability/services/message_crud_services.py
@@ -60,12 +60,16 @@ class MessageCrudServices:
     @classmethod
     def _get_households(cls, input_data: dict) -> QuerySet[Household]:
         if household_ids := [decode_id_string(household) for household in input_data.get("households", [])]:
-            return Household.objects.filter(id__in=household_ids)
+            return Household.objects.filter(id__in=household_ids).exclude(
+                head_of_household__phone_no_valid=False, head_of_household__phone_no_alternative_valid=False
+            )
         elif target_population_id := input_data.get("target_population"):
-            return Household.objects.filter(selections__target_population__id=decode_id_string(target_population_id))
+            return Household.objects.filter(
+                selections__target_population__id=decode_id_string(target_population_id),
+            ).exclude(head_of_household__phone_no_valid=False, head_of_household__phone_no_alternative_valid=False)
         elif registration_data_import_id := input_data.get("registration_data_import"):
             return Household.objects.filter(
                 registration_data_import__status=RegistrationDataImport.MERGED,
                 registration_data_import_id=decode_id_string(registration_data_import_id),
-            )
+            ).exclude(head_of_household__phone_no_valid=False, head_of_household__phone_no_alternative_valid=False)
         return Household.objects.none()
