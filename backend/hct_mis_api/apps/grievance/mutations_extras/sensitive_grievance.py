77244c3d7ac026dd53bc62252fab37c1da0f1794
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-04-12 14:47:46 +0200

    Add generic key for sensitive grievances

--- backend/hct_mis_api/apps/grievance/mutations_extras/sensitive_grievance.py
+++ backend/hct_mis_api/apps/grievance/mutations_extras/sensitive_grievance.py
@@ -1,8 +1,13 @@
 from typing import TYPE_CHECKING, Any, Dict, List
 
+from django.contrib.admin.options import get_content_type_for_model
+
 import graphene
 
-from hct_mis_api.apps.core.utils import decode_and_get_object
+from hct_mis_api.apps.core.utils import (
+    decode_and_get_object,
+    decode_and_get_payment_object,
+)
 from hct_mis_api.apps.grievance.models import TicketSensitiveDetails
 from hct_mis_api.apps.household.models import Household, Individual
 from hct_mis_api.apps.household.schema import HouseholdNode, IndividualNode
@@ -37,12 +42,13 @@ def save_sensitive_grievance_extras(
     payment_record = None
     if payment_record_encoded_ids_list:
         payment_record_encoded_id = payment_record_encoded_ids_list.pop(0)
-        payment_record = decode_and_get_object(payment_record_encoded_id, PaymentRecord, False)
+        payment_record = decode_and_get_payment_object(payment_record_encoded_id, False)
 
     TicketSensitiveDetails.objects.create(
         individual=individual,
         household=household,
-        payment_record=payment_record,
+        payment_content_type=get_content_type_for_model(payment_record) if payment_record else None,
+        payment_object_id=getattr(payment_record, "pk", None),
         ticket=grievance_ticket,
     )
     grievance_ticket.refresh_from_db()
@@ -61,7 +67,8 @@ def save_sensitive_grievance_extras(
         TicketSensitiveDetails.objects.create(
             individual=individual,
             household=household,
-            payment_record=payment_record,
+            payment_content_type=get_content_type_for_model(payment_record),  # type: ignore
+            payment_object_id=payment_record.pk,
             ticket=ticket,
         )
 
