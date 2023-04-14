4ce8218005551f4f893f1b19fc2b0adb9467de1a
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-04-14 17:44:47 +0530

    Test cases RDI import

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
+const { verifyDownloadTasks } = require('cy-verify-downloads');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on('task', verifyDownloadTasks);
       // implement node event listeners here
     },
     projectId: "cypress",
--- cypress/cypress/e2e/02-rdi/registration.cy.js
+++ cypress/cypress/e2e/02-rdi/registration.cy.js
@@ -1,32 +1,157 @@
 /// <reference types="cypress" />
-
 context("RDI", () => {
   beforeEach(() => {
     cy.adminLogin()
-  });
+    cy.visit("/");
+    cy.get("span").contains("Registration Data Import").click();
+  })
 
-  it("Registration Data Import", () => {
+  it("RDI visibility", () => {
+    cy.get('[data-cy="button-import"]').should('be.visible')
+    cy.get('[data-cy="table-title"]').should('be.visible')
+    cy.get('.sc-keFjpB > .MuiInputBase-root > .MuiInputBase-input').should('be.visible')
+  })
+
+  it("Download Template", () => {
+    cy.get("button > span").contains("IMPORT").click({ force: true });
+    cy.window().document().then(function (doc) {
+      doc.addEventListener('click', () => {
+        setTimeout(function () { doc.location.reload() }, 5000)
+      })
+      cy.get('span.MuiButton-label').contains('DOWNLOAD TEMPLATE').click()
+    })
+    cy.verifyDownload('registration_data_import_template.xlsx');
+  })
+
+  it("Registration Data Import with excel and verfify", () => {
     uploadRDIFile();
+    verifyUpload()
+    return;
+  })
 
-    return; // TODO: make this work
+  it("Registration Data Import and merge the data and verify merge file", () => {
+    uploadRDIFile();
+    mergeRDIFile()
+    verifyMergedData()
+    return;
+  })
 
-    mergeRDIFile();
-    verifyMergedData();
-  });
+  it("Registration Data Import and Refuse the import", () => {
+    uploadRDIFile();
+    verifyUpload()
+    refuseImport()
+    verfifyRefuseImport()
+    return;
+  })
+
+  it('Merged View ticket', () => {
+    uploadRDIFile();
+    mergeRDIFile()
+ cy.get('div.sc-kEYyzF').click({ force: true })
+    cy.reload()
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').eq(1).each(($data) => {
+          if ($data.text() == 'MERGED')
+            cy.contains('MERGED').click({ force: true })
+        })
+      })
+    })
+    cy.get('a span.MuiButton-label').click({ force: true })
+    cy.get('h5').should('contain', 'Grievance and Feedback')
+  })
 });
+
+function uploadRDIFile() {
+  cy.createExcel()
+    cy.get("h5").contains("Registration Data Import");
+    cy.get("button > span").contains("IMPORT").click({ force: true });
+    cy.get("h2").contains("Select File to Import").click();
+    cy.get('[data-cy="import-type-select"]').click();
+    cy.get('[data-cy="excel-menu-item"]').click();
+    cy.get('[data-cy="input-name"]').type(
+      "Test import ".concat(new Date().toISOString())
+    );
+    cy.uniqueSeed().then((seed) => {
+      const fileName = `rdi_import_1_hh_1_ind_seed_${seed}.xlsx`;
+      cy.fixture(fileName, "base64").then((fileContent) => {
+        cy.get('[data-cy="file-input"]').attachFile({
+          fileContent,
+          fileName,
+          mimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+          encoding: "base64",
+        });
+      });
+    });
+    cy.wait(5000)
+    cy.get('[data-cy="button-import-rdi"] > .MuiButton-label').click({ force: true })
+    cy.get('div.sc-kEYyzF').click({ force: true })
+  }
+
+  function verifyUpload() {
+    cy.reload()
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').eq(1).each(($data) => {
+         if ($data.text() == 'IN REVIEW')
+            expect($data.text()).to.eq('IN REVIEW')
+        })
+      })
+    })
+  }
+
+  function mergeRDIFile() {
+    cy.reload()
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').eq(1).each(($data) => {
+          if ($data.text() == 'IN REVIEW')
+            cy.contains('IN REVIEW').click({ force: true })
+        })
+      })
+    })
+    cy.get('[data-cy="label-Total Number of Households"]').contains("1");
+    cy.get('[data-cy="label-Total Number of Individuals"]').contains("1");
+    cy.get("span").contains("Merge").click({ force: true });
+    cy.get('strong').should('contain', '1 households and 1 individuals will be merged.')
+    cy.get("span").contains("MERGE").click({ force: true })
+    cy.wait(2000)
+  }
+  function refuseImport() {
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').eq(1).each(($data) => {
+          if ($data.text() == 'IN REVIEW')
+            cy.contains('IN REVIEW').click({ force: true })
+        })
+      })
+    })
+    cy.get('span').contains('Refuse Import').click({ force: true })
+    cy.wait(2000)
+    cy.get('div.sc-kEYyzF').click({ force: true })
+  }
 
 function verifyMergedData() {
   let householdId;
   let individualId;
+  cy.get('div.sc-kEYyzF').click({ force: true })
   cy.log("Looking for householdId");
+  cy.get('tbody tr').eq(0).each(($tablerows) => {
+    cy.wrap($tablerows).within(() => {
+      cy.get('td').eq(1).each(($data) => {
+        cy.contains('MERGED').click({ force: true })
+      })
+    })
+  })
   cy.get('[data-cy="imported-households-row"]')
     .find("td:nth-child(2)")
     .then(($td) => {
       householdId = $td.text().split(" (")[0];
+      cy.log($td.text())
       cy.log(`Saved householdId: ${householdId}`);
     })
     .then(() => {
-      cy.get("button > span").contains("Individuals").click({ force: true });
+      cy.get("button> span").contains("Individuals").click({ force: true });
 
       cy.get('[data-cy="imported-individuals-table"]')
         .find(`tbody > tr:nth-child(1) > td:nth-child(1)`)
@@ -43,73 +168,24 @@ function verifyMergedData() {
             .find("input")
             .type(householdId, { force: true });
           cy.get("td").should("contain", householdId);
-
           cy.get("span").contains("Individuals").click({ force: true });
-
           cy.log(`looking for individualId: + ${individualId}`);
           cy.get('[data-cy="ind-filters-search"]').type(individualId);
           cy.get("td").should("contain", individualId);
-
-          cy.get("td").contains(householdId).click({ force: true });
-
-          cy.get('[data-cy="label-Household ID"]').contains(householdId);
+          // cy.get("td").contains(householdId).click({ force: true });
+          // cy.get('[data-cy="label-Household ID"]').contains(householdId);
         });
     });
 }
 
-function uploadRDIFile() {
-  cy.visit("/");
-  cy.get("span").contains("Registration Data Import").click();
-  cy.get("h5").contains("Registration Data Import");
-  cy.get("button > span").contains("IMPORT").click({ force: true });
-  cy.get("h2").contains("Select File to Import").click();
-  cy.get('[data-cy="import-type-select"]').click();
-  cy.get('[data-cy="excel-menu-item"]').click();
-  cy.get('[data-cy="input-name"]').type(
-    "Test import ".concat(new Date().toISOString())
-  );
-  cy.uniqueSeed().then((seed) => {
-    const fileName = `rdi_import_1_hh_1_ind_seed_${seed}.xlsx`;
-    cy.fixture(fileName, "base64").then((fileContent) => {
-      cy.get('[data-cy="file-input"]').attachFile({
-        fileContent,
-        fileName,
-        mimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        encoding: "base64",
-      });
-    });
-  });
-}
-
-
-function mergeRDIFile() {
-  cy.get('[data-cy="number-of-households"]').contains(
-    "1 Household available to import",
-    {
-      timeout: 10000,
-    }
-  );
-  cy.get('[data-cy="number-of-individuals"]').contains(
-    "1 Individual available to import"
-  );
-  cy.get("div").contains("Errors").should("not.exist");
-  cy.get('[data-cy="button-import-rdi"').click();
-
-  cy.wait(1000);
-  cy.reload();
-  cy.wait(500);
-  // it lets the browser load the status
-  
-  cy.get("div").contains("IMPORT ERROR").should("not.exist");
-  cy.get("div").contains("IN REVIEW");
-
-  cy.get("span").contains("Merge").click({ force: true }); // top of page
-  cy.get("span").contains("MERGE").click({ force: true }); // inside modal
-
-  cy.get("div").contains("MERGING");
-
-  cy.reload();
-
-  cy.get("div").contains("MERGED");
+function verfifyRefuseImport() {
+  cy.get('tbody tr').eq(0).each(($tablerows) => {
+    cy.wrap($tablerows).within(() => {
+      cy.get('td').eq(1).each(($data) => {
+        if ($data.text() == 'REFUSED')
+          expect($data.text()).to.eq('REFUSED')
+      })
+    })
+  })
 }
 
