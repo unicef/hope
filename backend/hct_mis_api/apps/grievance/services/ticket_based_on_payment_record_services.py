8d00c5e9d84c854f0fbac6ca77a9873291c262bb
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-04-19 15:36:42 +0200

    fix create_tickets_based_on_payment_records_service

--- backend/hct_mis_api/apps/grievance/services/ticket_based_on_payment_record_services.py
+++ backend/hct_mis_api/apps/grievance/services/ticket_based_on_payment_record_services.py
@@ -1,6 +1,9 @@
-from typing import Dict, List, Type
+from base64 import b64decode
+from typing import Dict, List, Type, Union
 
+from django.contrib.admin.options import get_content_type_for_model
 from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
 from hct_mis_api.apps.core.utils import (
     decode_and_get_object,
@@ -8,7 +11,7 @@ from hct_mis_api.apps.core.utils import (
 )
 from hct_mis_api.apps.grievance.models import GrievanceTicket
 from hct_mis_api.apps.household.models import Household, Individual
-from hct_mis_api.apps.payment.models import PaymentRecord
+from hct_mis_api.apps.payment.models import Payment, PaymentRecord
 
 
 def create_tickets_based_on_payment_records_service(
@@ -19,25 +22,33 @@ def create_tickets_based_on_payment_records_service(
 
     household_encoded_id = details.get("household")
     household = decode_and_get_object(household_encoded_id, Household, False)
-
+    # Payment or PaymentRecord ids
     payment_record_encoded_ids_list = details.get("payment_record") or []
 
     payment_record = None
     if payment_record_encoded_ids_list:
         payment_record_encoded_id = payment_record_encoded_ids_list.pop(0)
-        payment_record = decode_and_get_object(payment_record_encoded_id, PaymentRecord, False)
+        node_name, obj_id = b64decode(payment_record_encoded_id).decode().split(":")
+
+        payment_record: Union["Payment", "PaymentRecord"] = get_object_or_404(  # type: ignore
+            Payment if node_name == "PaymentNode" else PaymentRecord, id=obj_id
+        )
 
     model.objects.create(
         individual=individual,
         household=household,
-        payment_record=payment_record,
+        payment_content_type=get_content_type_for_model(payment_record) if payment_record else None,  # type: ignore
+        payment_object_id=payment_record.pk if payment_record else None,
         ticket=grievance_ticket,
     )
     grievance_ticket.refresh_from_db()
     grievance_tickets_to_return = [grievance_ticket]
-
     for payment_record_encoded_id in payment_record_encoded_ids_list:
-        payment_record = decode_and_get_object(payment_record_encoded_id, PaymentRecord, False)
+        node_name, obj_id = b64decode(payment_record_encoded_id).decode().split(":")
+
+        payment_record: Union["Payment", "PaymentRecord"] = get_object_or_404(  # type: ignore
+            Payment if node_name == "PaymentNode" else PaymentRecord, id=obj_id
+        )
 
         # copy GrievanceTicket object and assign linked tickets
         ticket = grievance_ticket
@@ -49,7 +60,8 @@ def create_tickets_based_on_payment_records_service(
         model.objects.create(
             individual=individual,
             household=household,
-            payment_record=payment_record,
+            payment_content_type=get_content_type_for_model(payment_record),  # type: ignore
+            payment_object_id=payment_record.pk,
             ticket=ticket,
         )
 
@@ -77,7 +89,12 @@ def update_ticket_based_on_payment_record_service(
         ticket_details.individual = individual
 
     if payment_record_id := input_data.get("payment_record"):
-        payment_record = decode_and_get_object_required(payment_record_id, PaymentRecord)
-        ticket_details.payment_record = payment_record
+        node_name, obj_id = b64decode(payment_record_id).decode().split(":")
+
+        payment_record: Union["Payment", "PaymentRecord"] = get_object_or_404(  # type: ignore
+            Payment if node_name == "PaymentNode" else PaymentRecord, id=obj_id
+        )
+        ticket_details.payment_content_type = (get_content_type_for_model(payment_record),)
+        ticket_details.payment_object_id = (payment_record.pk,)
     ticket_details.save()
     return grievance_ticket
