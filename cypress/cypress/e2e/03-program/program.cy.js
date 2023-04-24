ef7940e80c89b842776b077706f3ce900bb38654
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-04-24 20:17:17 +0530

    program test cases

--- cypress/cypress/e2e/03-program/program.cy.js
+++ cypress/cypress/e2e/03-program/program.cy.js
@@ -2,15 +2,12 @@
 
 context("Program", () => {
   beforeEach(() => {
-    cy.visit("/api/unicorn/");
-    cy.get('input[name="username"]').type(Cypress.env("username"));
-    cy.get('input[name="password"]').type(Cypress.env("password"));
-    cy.get("input").contains("Log in").click();
-  });
-  it("Can visit the Programs page and create a program", () => {
+    cy.adminLogin()
     cy.visit("/");
     cy.get("span").contains("Programme Management").click();
-    cy.get("h5").contains("Programme Management");
+    cy.get("h5").should('contain', "Programme Management");
+    });
+  it("visit the Programs page and create a program", () => {
     cy.get('[data-cy="button-new-program"]').click({ force: true });
     cy.get("h6").contains("Set-up a new Programme");
     cy.uniqueSeed().then((seed) => {
@@ -20,8 +17,8 @@ context("Program", () => {
       cy.get('[data-cy="select-option-Unicef"]').click();
       cy.get('[data-cy="input-sector"]').first().click();
       cy.get('[data-cy="select-option-Multi Purpose"]').click();
-      cy.get('[data-cy="input-start-date"]').click().type("2022-12-12");
-      cy.get('[data-cy="input-end-date"]').click().type("2022-12-23");
+      cy.get('[data-cy="input-start-date"]').click().type("2023-04-10");
+      cy.get('[data-cy="input-end-date"]').click().type("2025-04-30");
       cy.get('[data-cy="input-description"]')
         .first()
         .click()
@@ -35,14 +32,91 @@ context("Program", () => {
         .click()
         .type("{backspace}{backspace}{backspace}{backspace}4000");
       cy.get('[data-cy="button-save"]').click({ force: true });
-      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-      cy.get("h6").contains("Programme Details");
+      cy.get("h6").should('contain', "Programme Details");
       cy.get('[data-cy="button-activate-program"]').click({ force: true });
-      cy.get('[data-cy="button-activate-program-modal"]').click({
-        force: true
-      });
-      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-      cy.get('[data-cy="status-container"]').contains("ACTIVE");
+      cy.get('[data-cy="button-activate-program-modal"]').click({ force: true });
+      cy.get('[data-cy="status-container"]').should('contain', "ACTIVE");
     });
   });
-});
+
+  it("Edit Program", () => {
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="ACTIVE"]').click({ force: true })
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.contains('EDIT PROGRAMME').click({ force: true })
+    cy.get('[data-cy="input-programme-name"]').clear().type('Edited Name');
+    cy.get('[data-cy="input-cash-assist-scope"]').first().click();
+    cy.get('[data-cy="select-option-Unicef"]').click();
+    cy.get('[data-cy="input-sector"]').first().click();
+    cy.get('[data-cy="select-option-Multi Purpose"]').click();
+    cy.get('[data-cy="input-start-date"]').click().type("2023-05-1");
+    cy.get('[data-cy="input-end-date"]').click().type("2025-05-30");
+    cy.get('[data-cy="input-description"]')
+      .first().clear().type("Edit Test description");
+    cy.get('[data-cy="input-budget"]')
+      .first()
+      .click()
+      .type("{backspace}{backspace}{backspace}{backspace}8888");
+    cy.get('[data-cy="input-admin-area"]').clear().type("Some Admin Area");
+    cy.get('[data-cy="input-population-goal"]')
+      .click()
+      .type("{backspace}{backspace}{backspace}{backspace}2000");
+    cy.get('[data-cy="button-save"]').click({ force: true });
+    cy.get("h6").should('contain', "Programme Details");
+  })
+
+  it("Finish Program", () => {
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="ACTIVE"]').click({ force: true })
+    cy.reload()
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.contains('Finish Programme').click({ force: true })
+    cy.get('[data-cy="button-finish-program"]').eq(1).click({ force: true })
+    cy.get('[data-cy="status-container"]').should('contain', "FINISHED");
+  })
+
+  it("Reactivate Program", () => {
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="FINISHED"]').click({ force: true })
+    cy.get('[data-cy="status-container"]').should('contain', "FINISHED");
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.contains('Reactivate').eq(0).click({ force: true })
+    cy.get('.MuiDialogActions-root > .MuiButton-contained').click({ force: true })
+    cy.get('[data-cy="status-container"]').should('contain', "ACTIVE");
+  })
+
+  it("Delete Program", () => {
+    cy.get('[data-cy="button-new-program"]').click({ force: true });
+    cy.get("h6").contains("Set-up a new Programme");
+    cy.uniqueSeed().then((seed) => {
+      const programName = `program ${seed}`;
+      cy.get('[data-cy="input-programme-name"]').type(programName);
+      cy.get('[data-cy="input-cash-assist-scope"]').first().click();
+      cy.get('[data-cy="select-option-Unicef"]').click();
+      cy.get('[data-cy="input-sector"]').first().click();
+      cy.get('[data-cy="select-option-Multi Purpose"]').click();
+      cy.get('[data-cy="input-start-date"]').click().type("2023-04-13");
+      cy.get('[data-cy="input-end-date"]').click().type("2023-05-20");
+      cy.get('[data-cy="input-description"]')
+        .first()
+        .click()
+        .type("Draft description");
+      cy.get('[data-cy="input-budget"]')
+        .first()
+        .click()
+        .type("{backspace}{backspace}{backspace}{backspace}3533");
+      cy.get('[data-cy="input-admin-area"]').click().type("Admin");
+      cy.get('[data-cy="input-population-goal"]')
+        .click()
+        .type("{backspace}{backspace}{backspace}{backspace}3000");
+      cy.get('[data-cy="button-save"]').click({ force: true });
+      cy.get('[data-cy="status-container"]').should('contain', "DRAFT");
+    })
+    cy.get("span").contains("Programme Management").click();
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="DRAFT"]').click({ force: true })
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.get('[data-cy="button-remove-program"]').click({ force: true })
+    cy.get('[data-cy="button-remove-program"]').eq(1).click({ force: true })
+  })
+})
