261b01f3468cc40ae33d7e7b3615b5e3343cda01
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-04-25 16:00:30 +0530

    Created target population

--- cypress/cypress/e2e/04-targeting/targeting.cy.js
+++ cypress/cypress/e2e/04-targeting/targeting.cy.js
@@ -2,65 +2,35 @@
 
 context("Targeting", () => {
   let programName = "TargetingProgram";
-
-  beforeEach(() => {
-    cy.initScenario("targeting");
-    cy.visit("/api/unicorn/");
-    cy.get('input[name="username"]').type(Cypress.env("username"));
-    cy.get('input[name="password"]').type(Cypress.env("password"));
-    cy.get("input").contains("Log in").click();
-  });
-
-  it("Can visit the targeting page and create a target population", () => {
+   beforeEach(() => {
+    cy.adminLogin()
     cy.visit("/");
+    cy.initScenario("targeting");
     cy.get("span").contains("Targeting").click();
-    cy.get("h5").contains("Targeting");
-    cy.get('[data-cy="button-target-population-create-new"]').click({
-      force: true
-    });
+    cy.get("h5").should('contain', "Targeting");
+  });
+  it("Can visit the targeting page and create a target population", () => {
+    cy.get('[data-cy="button-target-population-create-new"]').click({ force: true });
     cy.uniqueSeed().then((seed) => {
       const targetPopulationName = `test TP ${seed}`;
-      cy.get('[data-cy="input-name"]')
-        .eq(1)
-        .type(targetPopulationName, { force: true });
+      cy.get('input[data-cy="input-name"]').type(targetPopulationName, { force: true });
       cy.get('[data-cy="input-program"]').first().click();
-      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
       cy.get(`[data-cy="select-option-${programName}-${seed}"]`).click();
       cy.get('[data-cy="button-target-population-add-criteria"]').click();
-
-      cy.get('[data-cy="button-household-rule"]', {
-        timeout: 10000
-      }).click();
-      cy.get('[data-cy="autocomplete-target-criteria"]')
-        .click()
-        .type("address");
-
-      // TODO
-      // cy.contains("Address").click();
-      // cy.get('[data-cy="input-filters[0].value"]')
-      //   .click()
-      //   .type(`TargetingVille-${seed}`);
-      // cy.get('[data-cy="button-target-population-add-criteria"]').eq(1).click();
-      // cy.get("h6").contains("Households");
-      // cy.get(
-      //   "[data-cy=button-target-population-create] > .MuiButton-label"
-      // ).click();
-      // cy.get("h6").contains("Targeting Criteria");
-      // cy.get('[data-cy="status-container"]').contains("OPEN");
-      // cy.get('[data-cy="button-target-population-lock"]').click({
-      //   force: true
-      // });
-      // cy.get('[data-cy="button-target-population-modal-lock"]').click({
-      //   force: true
-      // });
-      // cy.get("h6").contains("Targeting Criteria");
-      // cy.get('[data-cy="status-container"]').contains("LOCKED");
-      // cy.get('[data-cy="button-target-population-send-to-hope"]').click({
-      //   force: true
-      // });
-      // cy.get('[data-cy="button-target-population-modal-send-to-hope"]').click();
-      // cy.get("h6").contains("Targeting Criteria");
-      // cy.get('[data-cy="status-container"]').contains("READY");
+      cy.get('[data-cy="button-individual-rule"]').click({ force: true })
+      cy.get('[data-cy="autocomplete-target-criteria"]').should("be.visible")
+      cy.get('[data-cy="autocomplete-target-criteria-option-0"]').type('Age {downArrow}{enter}')
+      cy.get('[data-cy="input-individualsFiltersBlocks[0].individualBlockFilters[0].value.from"]').type('0')
+      cy.get('[data-cy="input-individualsFiltersBlocks[0].individualBlockFilters[0].value.to"]').type('100')
+      cy.get('[data-cy="button-target-population-add-criteria"]').eq(1).click({ force: true });
+      cy.get("[data-cy='button-target-population-create']").click({ force: true });
+      cy.get('[data-cy="status-container"]').should('contain', "OPEN");
+      cy.get('[data-cy="button-target-population-lock"]').click({ force: true });
+      cy.get('[data-cy="button-target-population-modal-lock"]').click({ force: true });
+      cy.get('[data-cy="status-container"]').should('contain', "LOCKED");
+      cy.get('[data-cy="button-target-population-send-to-hope"]').click({ force: true });
+      cy.get('[data-cy="button-target-population-modal-send-to-hope"]').click({ force: true });
+      cy.get('[data-cy="status-container"]').should('contain', "READY");
     });
   });
 });
