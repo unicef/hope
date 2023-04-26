939f0aa8a1fcf3cbc0c34edc0aae8a1013975528
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-04-26 19:46:01 +0530

    Initial commit

--- cypress/cypress/e2e/02-rdi/registration.cy.js
+++ cypress/cypress/e2e/02-rdi/registration.cy.js
@@ -5,131 +5,161 @@ context("RDI", () => {
     cy.visit("/");
     cy.get("span").contains("Registration Data Import").click();
   })
-
   it("RDI visibility", () => {
     cy.get('[data-cy="button-import"]').should('be.visible')
     cy.get('[data-cy="table-title"]').should('be.visible')
-    cy.get('.sc-keFjpB > .MuiInputBase-root > .MuiInputBase-input').should('be.visible')
+    cy.get('[data-cy="filter-search"]').should('be.visible')
+    cy.get('[data-cy="filter-import-date"]').should('be.visible')
+    cy.get('[data-cy="filter-status"]').should('be.visible')
   })
-
   it("Download Template", () => {
     cy.get("button > span").contains("IMPORT").click({ force: true });
     cy.window().document().then(function (doc) {
       doc.addEventListener('click', () => {
         setTimeout(function () { doc.location.reload() }, 5000)
       })
-      cy.get('span.MuiButton-label').contains('DOWNLOAD TEMPLATE').click()
+      cy.get('span').contains('DOWNLOAD TEMPLATE').click()
     })
     cy.verifyDownload('registration_data_import_template.xlsx');
   })
-
-  it("Registration Data Import with excel and verfify", () => {
-    uploadRDIFile();
-    verifyUpload()
-    return;
-  })
-
-  // it("Registration Data Import with Kobo and verfify", () => {
-  //   uploadRDIFileKobo();
-  //   verifyUploadKobo()
-  //   return;
-  // })
-  it("Registration Data Import and merge the data and verify merge file", () => {
+ it("Registration Data Import and merge the data and verify merge file and View Ticket", () => {
     uploadRDIFile();
     mergeRDIFile()
     verifyMergedData()
+    viewTicket()
     return;
   })
-
   it("Registration Data Import and Refuse the import", () => {
     uploadRDIFile();
-    verifyUpload()
     refuseImport()
-    verfifyRefuseImport()
     return;
   })
+it('RDI- Searches by import title', () => {
+    cy.createExcel()
+    cy.get("h5").contains("Registration Data Import");
+    cy.get("span").contains("IMPORT").click({ force: true });
+    cy.get("h2").contains("Select File to Import").click();
+    cy.get('[data-cy="import-type-select"]').click();
+    cy.get('[data-cy="excel-menu-item"]').click();
+    cy.get('[data-cy="input-name"]').type(
+      "Test import ".concat(new Date().toISOString())
+    );
+    const name = "Test import ".concat(new Date().toISOString())
+    cy.uniqueSeed().then((seed) => {
+      const fileName = `rdi_import_1_hh_1_ind_seed_${seed}.xlsx`;
+      cy.fixture(fileName, "base64").then((fileContent) => {
+        cy.get('[data-cy="file-input"]').attachFile({
+          fileContent,
+          fileName,
+          mimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+          encoding: "base64",
+        });
+      });
+    });
+    cy.wait(5000)
+    cy.get('[data-cy="button-import-rdi"]').click({ force: true })
+    cy.get("span").contains("Registration Data Import").click();
+    cy.reload()
+    cy.get('[data-cy="filter-search"]').type(name)
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').eq(0).each(($data) => {
+          expect($data.text()).to.contain(name)
+        })
+      })
+    })
+  })
+  it('RDI- Searches by Date', () => {
+    const d = new Date()
+    cy.get('[data-cy="filter-import-date"]').type(d.toLocaleDateString('en-CA') + '{enter}')
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').eq(2).each(($data) => {
+          expect($data.text()).to.contain(d.toLocaleDateString('en-US', { day: "2-digit" }) + " " + d.toLocaleDateString('en-US', { month: "short" }) + " " + d.toLocaleDateString('en-US', { year: "numeric" }))
+        })
+      })
+    })
+  })
 
-  
-  it('Merged View ticket', () => {
-    uploadRDIFile();
-    mergeRDIFile()
-   
-   cy.get('div.sc-kEYyzF').click({ force: true })
-   cy.reload()
+  it('RDI- Searches Imported by', () => {
+    cy.get('input[type="text"]').eq(2).type('cypress@cypress.com{downArrow}{enter}')
+    cy.reload()
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').eq(5).each(($data) => {
+          expect($data.text()).to.contain('cypress@cypress.com')
+        })
+      })
+    })
+  })
+  it('RDI- Searches by status', () => {
+    cy.get('[data-cy="filter-status"]').click()
+    cy.get('li[data-value="IN_REVIEW"]').click()
+    cy.reload()
     cy.get('tbody tr').eq(0).each(($tablerows) => {
       cy.wrap($tablerows).within(() => {
         cy.get('td').eq(1).each(($data) => {
-          if ($data.text() == 'MERGED')
-            cy.contains('MERGED').click({ force: true })
+          expect($data.text()).to.contain('IN REVIEW')
         })
       })
     })
-    cy.get('a span.MuiButton-label').click({ force: true })
-    cy.get('h5').should('contain', 'Grievance and Feedback')
+  })
+
+  it('RDI - Searches with all filters', () => {
+    cy.createExcel()
+    const d = new Date()
+    cy.get("h5").contains("Registration Data Import");
+    cy.get("span").contains("IMPORT").click({ force: true });
+    cy.get("h2").contains("Select File to Import").click();
+    cy.get('[data-cy="import-type-select"]').click();
+    cy.get('[data-cy="excel-menu-item"]').click();
+    cy.get('[data-cy="input-name"]').type(
+      "Test import ".concat(d.toISOString())
+    );
+    const name = "Test import ".concat(d.toISOString())
+    cy.uniqueSeed().then((seed) => {
+      const fileName = `rdi_import_1_hh_1_ind_seed_${seed}.xlsx`;
+      cy.fixture(fileName, "base64").then((fileContent) => {
+        cy.get('[data-cy="file-input"]').attachFile({
+          fileContent,
+          fileName,
+          mimeType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+          encoding: "base64",
+        });
+      });
+    });
+    cy.wait(5000)
+    cy.get('[data-cy="button-import-rdi"]').click({ force: true })
+    cy.get("span").contains("Registration Data Import").click();
+    cy.reload()
+    cy.get('[data-cy="filter-search"]').type(name)
+    cy.get('[data-cy="filter-import-date"]').type(d.toLocaleDateString('en-CA'))
+
+    cy.get('input[type="text"]').eq(2).type('cypress@cypress.com{downArrow}{enter}')
+    cy.get('[data-cy="filter-status"]').click()
+    cy.get('li[data-value="IN_REVIEW"]').click()
+
+    cy.get('tbody tr').eq(0).each(($tablerows) => {
+      cy.wrap($tablerows).within(() => {
+        cy.get('td').each(($data) => {
+          cy.log($data.text())
+        })
+      })
+    })
   })
 });
 
-function verifyMergedData() {
-  let householdId;
-  let individualId;
-  cy.get('div.sc-kEYyzF').click({ force: true })
-  
-  cy.log("Looking for householdId");
-  cy.get('tbody tr').eq(0).each(($tablerows) => {
-    cy.wrap($tablerows).within(() => {
-      cy.get('td').eq(1).each(($data) => {
-        cy.contains('MERGED').click({ force: true })
-      })
-    })
-  })
-  cy.get('[data-cy="imported-households-row"]')
-    .find("td:nth-child(2)")
-    .then(($td) => {
-      householdId = $td.text().split(" (")[0];
-      cy.log($td.text())
-      cy.log(`Saved householdId: ${householdId}`);
-    })
-    .then(() => {
-      cy.get("button> span").contains("Individuals").click({ force: true });
-
-      cy.get('[data-cy="imported-individuals-table"]')
-        .find(`tbody > tr:nth-child(1) > td:nth-child(1)`)
-        .then(($td) => {
-          individualId = $td.text().split(" (")[0];
-          cy.log(`Saved individualId: ${individualId}`);
-        })
-        .then(() => {
-          cy.get("span").contains("Population").click();
-          cy.get("span").contains("Households").click();
-
-          cy.log(`looking for householdId: ${householdId}`);
-          cy.get('[data-cy="hh-filters-search"]')
-            .find("input")
-            .type(householdId, { force: true });
-          cy.get("td").should("contain", householdId);
-         cy.get("span").contains("Individuals").click({ force: true });
-          cy.log(`looking for individualId: + ${individualId}`);
-          cy.get('[data-cy="ind-filters-search"]').type(individualId);
-          cy.get("td").should("contain", individualId);
-          // cy.get("td").contains(householdId).click({ force: true });
-          // cy.get('[data-cy="label-Household ID"]').contains(householdId);
-        });
-    });
-}
-
 function uploadRDIFile() {
-
-    
-cy.createExcel()
+  cy.createExcel()
   cy.get("h5").contains("Registration Data Import");
-  cy.get("button > span").contains("IMPORT").click({ force: true });
+  cy.get("span").contains("IMPORT").click({ force: true });
   cy.get("h2").contains("Select File to Import").click();
   cy.get('[data-cy="import-type-select"]').click();
   cy.get('[data-cy="excel-menu-item"]').click();
-
   cy.get('[data-cy="input-name"]').type(
     "Test import ".concat(new Date().toISOString())
   );
+  const name = "Test import ".concat(new Date().toISOString())
   cy.uniqueSeed().then((seed) => {
     const fileName = `rdi_import_1_hh_1_ind_seed_${seed}.xlsx`;
     cy.fixture(fileName, "base64").then((fileContent) => {
@@ -142,86 +172,73 @@ cy.createExcel()
     });
   });
   cy.wait(5000)
-  cy.get('[data-cy="button-import-rdi"] > .MuiButton-label').click({ force: true })
-
-  cy.get('div.sc-kEYyzF').click({ force: true })
-  
-}
-
-
-function uploadRDIFileKobo() {
-
-  cy.get("h5").contains("Registration Data Import");
-  cy.get("button > span").contains("IMPORT").click({ force: true });
-  cy.get("h2").contains("Select File to Import").click();
-  cy.get('[data-cy="import-type-select"]').click();
-  cy.get('[data-cy="kobo-menu-item"]').click();
-  cy.get('input.PrivateSwitchBase-input-208').eq(0).click()
-  cy.get('input.PrivateSwitchBase-input-208').eq(1).click()
-  //select project
-  cy.get('#mui-component-select-koboAssetId').select()
-  cy.get('#textField-name').type('')
-  cy.wait(2000)
-  cy.get('[data-cy="button-import-rdi"] > .MuiButton-label').click({ force: true })
-}
-function verifyUploadKobo() {
-
-
-
-
-}
-function verifyUpload() {
+  cy.get('[data-cy="button-import-rdi"]').click({ force: true })
+  cy.get("span").contains("Registration Data Import").click();
   cy.reload()
-  cy.get('tbody tr').eq(0).each(($tablerows) => {
-    cy.wrap($tablerows).within(() => {
-      cy.get('td').eq(1).each(($data) => {
-        // cy.log($data.text())
-        if ($data.text() == 'IN REVIEW')
-        expect($data.text()).to.eq('IN REVIEW')
-      })
-    })
-  })
+  cy.get('[data-cy="status-container"]').eq(0).should('contain', 'IN REVIEW')
 }
-
 function mergeRDIFile() {
-  cy.reload()
-  cy.get('tbody tr').eq(0).each(($tablerows) => {
-    cy.wrap($tablerows).within(() => {
-      cy.get('td').eq(1).each(($data) => {
-        if ($data.text() == 'IN REVIEW')
-          cy.contains('IN REVIEW').click({ force: true })
-      })
-    })
-  })
+  cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
   cy.get('[data-cy="label-Total Number of Households"]').contains("1");
   cy.get('[data-cy="label-Total Number of Individuals"]').contains("1");
   cy.get("span").contains("Merge").click({ force: true });
   cy.get('strong').should('contain', '1 households and 1 individuals will be merged.')
   cy.get("span").contains("MERGE").click({ force: true })
-  cy.wait(2000)
-}
-function refuseImport() {
-  cy.get('tbody tr').eq(0).each(($tablerows) => {
-    cy.wrap($tablerows).within(() => {
-      cy.get('td').eq(1).each(($data) => {
-        if ($data.text() == 'IN REVIEW')
-          cy.contains('IN REVIEW').click({ force: true })
-      })
-    })
-  })
-  cy.get('span').contains('Refuse Import').click({ force: true })
-  cy.wait(2000)
-  cy.get('div.sc-kEYyzF').click({ force: true })
+
+  cy.get("span").contains("Registration Data Import").click();
+  cy.reload()
+  cy.get('[data-cy="status-container"]').eq(0).should('contain', 'MERGED')
 }
 
-function verfifyRefuseImport() {
-  cy.get('tbody tr').eq(0).each(($tablerows) => {
-    cy.wrap($tablerows).within(() => {
-      cy.get('td').eq(1).each(($data) => {
-        if ($data.text() == 'REFUSED')
-          expect($data.text()).to.eq('REFUSED')
-          })
+function verifyMergedData() {
+  let householdId;
+  let individualId;
+  cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+  cy.log("Looking for householdId");
+  cy.get('[data-cy="imported-households-row"]')
+    .find("td:nth-child(2)")
+    .then(($td) => {
+      householdId = $td.text().split(" (")[0];
+      cy.log($td.text())
+      cy.log(`Saved householdId: ${householdId}`);
     })
-  })
+    .then(() => {
+      cy.get("button> span").contains("Individuals").click({ force: true });
+      cy.get('[data-cy="imported-individuals-table"]')
+        .find(`tbody > tr:nth-child(1) > td:nth-child(1)`)
+        .then(($td) => {
+          individualId = $td.text().split(" (")[0];
+          cy.log(`Saved individualId: ${individualId}`);
+        })
+        .then(() => {
+          cy.get("span").contains("Population").click();
+          cy.get("span").contains("Households").click();
+          cy.log(`looking for householdId: ${householdId}`);
+          cy.get('[data-cy="hh-filters-search"]')
+            .find("input")
+            .type(householdId, { force: true });
+          cy.get("td").should("contain", householdId);
+          cy.get("span").contains("Individuals").click({ force: true });
+          cy.log(`looking for individualId: + ${individualId}`);
+          cy.get('[data-cy="ind-filters-search"]').type(individualId);
+          cy.get("td").should("contain", individualId);
+        });
+    });
 }
+function refuseImport() {
+  cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+  cy.get('span').contains('Refuse Import').click({ force: true })
+  cy.get("span").contains("Registration Data Import").click();
+  cy.reload()
+  cy.get('[data-cy="status-container"]').eq(0).should('contain', 'REFUSED')
+}
+ function viewTicket()
+ {
+  cy.get("span").contains("Registration Data Import").click();
+  cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+  cy.get('span').contains('View Tickets').click({ force: true })
+  cy.get('h5').should('contain', 'Grievance and Feedback')
+ }
+
+
 
--- cypress/cypress/e2e/03-program/program.cy.js
+++ cypress/cypress/e2e/03-program/program.cy.js
@@ -5,11 +5,9 @@ context("Program", () => {
     cy.adminLogin()
     cy.visit("/");
     cy.get("span").contains("Programme Management").click();
-    cy.get("h5").contains("Programme Management");
-  
-  });
-  it("Can visit the Programs page and create a program", () => {
-    
+    cy.get("h5").should('contain', "Programme Management");
+    });
+  it("visit the Programs page and create a program", () => {
     cy.get('[data-cy="button-new-program"]').click({ force: true });
     cy.get("h6").contains("Set-up a new Programme");
     cy.uniqueSeed().then((seed) => {
@@ -20,7 +18,7 @@ context("Program", () => {
       cy.get('[data-cy="input-sector"]').first().click();
       cy.get('[data-cy="select-option-Multi Purpose"]').click();
       cy.get('[data-cy="input-start-date"]').click().type("2023-04-10");
-      cy.get('[data-cy="input-end-date"]').click().type("2023-04-30");
+      cy.get('[data-cy="input-end-date"]').click().type("2025-04-30");
       cy.get('[data-cy="input-description"]')
         .first()
         .click()
@@ -34,86 +32,61 @@ context("Program", () => {
         .click()
         .type("{backspace}{backspace}{backspace}{backspace}4000");
       cy.get('[data-cy="button-save"]').click({ force: true });
-cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-      cy.get("h6").contains("Programme Details");
+      cy.get("h6").should('contain', "Programme Details");
       cy.get('[data-cy="button-activate-program"]').click({ force: true });
-      cy.get('[data-cy="button-activate-program-modal"]').click({force: true});
-      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-     cy.get('[data-cy="status-container"]').should('contain',"ACTIVE");
+      cy.get('[data-cy="button-activate-program-modal"]').click({ force: true });
+      cy.get('[data-cy="status-container"]').should('contain', "ACTIVE");
     });
   });
 
   it("Edit Program", () => {
-    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({force:true})
-   cy.get('[data-value="ACTIVE"]').click({force:true})
-  
-  cy.get('tbody tr').eq(0).each(($tablerows) => {
-    cy.wrap($tablerows).within(() => {
-      cy.get('td').eq(1).each(($data) => {
-        if ($data.text() == 'ACTIVE')
-       cy.contains('ACTIVE').click({ force: true })
-      })
-    })
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="ACTIVE"]').click({ force: true })
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.contains('EDIT PROGRAMME').click({ force: true })
+    cy.get('[data-cy="input-programme-name"]').clear().type('Edited Name');
+    cy.get('[data-cy="input-cash-assist-scope"]').first().click();
+    cy.get('[data-cy="select-option-Unicef"]').click();
+    cy.get('[data-cy="input-sector"]').first().click();
+    cy.get('[data-cy="select-option-Multi Purpose"]').click();
+    cy.get('[data-cy="input-start-date"]').click().type("2023-05-1");
+    cy.get('[data-cy="input-end-date"]').click().type("2025-05-30");
+    cy.get('[data-cy="input-description"]')
+      .first().clear().type("Edit Test description");
+    cy.get('[data-cy="input-budget"]')
+      .first()
+      .click()
+      .type("{backspace}{backspace}{backspace}{backspace}8888");
+    cy.get('[data-cy="input-admin-area"]').clear().type("Some Admin Area");
+    cy.get('[data-cy="input-population-goal"]')
+      .click()
+      .type("{backspace}{backspace}{backspace}{backspace}2000");
+    cy.get('[data-cy="button-save"]').click({ force: true });
+    cy.get("h6").should('contain', "Programme Details");
   })
-  cy.contains('EDIT PROGRAMME').click({ force: true })
-  cy.get('[data-cy="input-programme-name"]').clear().type('Edited Name');
-  cy.get('[data-cy="input-cash-assist-scope"]').first().click();
-  cy.get('[data-cy="select-option-Unicef"]').click();
-  cy.get('[data-cy="input-sector"]').first().click();
-  cy.get('[data-cy="select-option-Multi Purpose"]').click();
-  cy.get('[data-cy="input-start-date"]').click().type("2023-05-1");
-  cy.get('[data-cy="input-end-date"]').click().type("2023-05-30");
-  cy.get('[data-cy="input-description"]')
-    .first().clear().type("Edit Test description");
-  cy.get('[data-cy="input-budget"]')
-    .first()
-    .click()
-    .type("{backspace}{backspace}{backspace}{backspace}8888");
-  cy.get('[data-cy="input-admin-area"]').clear().type("Some Admin Area");
-  cy.get('[data-cy="input-population-goal"]')
-    .click()
-    .type("{backspace}{backspace}{backspace}{backspace}2000");
-  cy.get('[data-cy="button-save"]').click({ force: true });
 
-})
-
-it("Finish Program", () => {
-  cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({force:true})
- cy.get('[data-value="ACTIVE"]').click({force:true})
-
-cy.get('tbody tr').eq(0).each(($tablerows) => {
-  cy.wrap($tablerows).within(() => {
-    cy.get('td').eq(1).each(($data) => {
-      if ($data.text() == 'ACTIVE')
-     cy.contains('ACTIVE').click({ force: true })
-    })
+  it("Finish Program", () => {
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="ACTIVE"]').click({ force: true })
+    cy.reload()
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.contains('Finish Programme').click({ force: true })
+    cy.get('[data-cy="button-finish-program"]').eq(1).click({ force: true })
+    cy.get('[data-cy="status-container"]').should('contain', "FINISHED");
   })
-})
-cy.contains('Finish Programme').click({ force: true })
-cy.get('[data-cy="button-finish-program"]').eq(1).click({force: true})
-cy.get('[data-cy="status-container"]').should('contain',"FINISHED");
-})
 
-it("Reactivate Program", () => {
-  cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({force:true})
- cy.get('[data-value="FINISHED"]').click({force:true})
-cy.wait(2000)
-cy.get('tbody tr').eq(0).each(($tablerows) => {
-  cy.wrap($tablerows).within(() => {
-    cy.get('td').eq(1).each(($data) => {
-      if ($data.text() == 'FINISHED')
-     cy.contains('FINISHED').click({ force: true })
-    })
+  it("Reactivate Program", () => {
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="FINISHED"]').click({ force: true })
+    cy.get('[data-cy="status-container"]').should('contain', "FINISHED");
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.contains('Reactivate').eq(0).click({ force: true })
+    cy.get('.MuiDialogActions-root > .MuiButton-contained').click({ force: true })
+    cy.get('[data-cy="status-container"]').should('contain', "ACTIVE");
   })
-})
-cy.contains('Reactivate').click({ force: true })
-cy.get('.MuiDialogActions-root > .MuiButton-contained').click({force: true})
-cy.get('[data-cy="status-container"]').should('contain',"ACTIVE");
-})
 
-it("Delete Program", () => {
-
-   cy.get('[data-cy="button-new-program"]').click({ force: true });
+  it("Delete Program", () => {
+    cy.get('[data-cy="button-new-program"]').click({ force: true });
     cy.get("h6").contains("Set-up a new Programme");
     cy.uniqueSeed().then((seed) => {
       const programName = `program ${seed}`;
@@ -137,25 +110,14 @@ it("Delete Program", () => {
         .click()
         .type("{backspace}{backspace}{backspace}{backspace}3000");
       cy.get('[data-cy="button-save"]').click({ force: true });
-      
-      cy.wait(2000);
-      cy.get('[data-cy="status-container"]').should('contain',"DRAFT");
-      cy.get('div.sc-kEYyzF').click({ force: true })
+      cy.get('[data-cy="status-container"]').should('contain', "DRAFT");
     })
-cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({force:true})
- cy.get('[data-value="DRAFT"]').click({force:true})
- cy.wait(2000)
-cy.get('tbody tr').eq(0).each(($tablerows) => {
-  cy.wrap($tablerows).within(() => {
-    cy.get('td').eq(1).each(($data) => {
-      if ($data.text() == 'DRAFT')
-     cy.contains('DRAFT').click({ force: true })
-    })
+    cy.get("span").contains("Programme Management").click();
+    cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
+    cy.get('[data-value="DRAFT"]').click({ force: true })
+    cy.get('[data-cy="status-container"]').eq(0).click({ force: true })
+    cy.get('[data-cy="button-remove-program"]').click({ force: true })
+    cy.get('[data-cy="button-remove-program"]').eq(1).click({ force: true })
   })
 })
-cy.get('[data-cy="button-remove-program"]').click({force: true})
-cy.get('[data-cy="button-remove-program"]').eq(1).click({force: true})
 
-
-})
-})
