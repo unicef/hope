fb57f1d5fd0c1ea4d624a99f428c68b0e34ee7c0
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-04-28 09:10:30 +0200

    add separate component for RelatedFollowUpPP

---
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/PaymentPlanDetails/RelatedFollowUpPaymentPlans.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import { useTranslation } from 'react-i18next';
+import { PaymentPlanQuery } from '../../../../__generated__/graphql';
+import { BlackLink } from '../../../core/BlackLink';
+import { LabelizedField } from '../../../core/LabelizedField';
+
+interface RelatedFollowUpPaymentPlansProps {
+  businessArea: string;
+  followUps: PaymentPlanQuery['paymentPlan']['followUps'];
+}
+
+const RelatedFollowUpPaymentPlans = ({
+  followUps = {},
+  businessArea,
+}: RelatedFollowUpPaymentPlansProps) => {
+  const { t } = useTranslation();
+  const [showAll, setShowAll] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowAll(!showAll);
+  };
+
+  let followUpLinks = null;
+  if (followUps?.edges?.length > 0) {
+    const truncatedFollowUps = showAll
+      ? followUps.edges
+      : followUps.edges.slice(0, 5);
+    followUpLinks = truncatedFollowUps.map((followUp) => (
+      <BlackLink
+        key={followUp?.node?.id}
+        to={`/${businessArea}/payment-module/payment-plans/${followUp?.node?.id}`}
+      >
+        {followUp?.node?.unicefId}
+      </BlackLink>
+    ));
+  }
+
+  return (
+    <LabelizedField label={t('Related Follow-Up Payment Plans')}>
+      {followUpLinks}
+      {followUps?.edges?.length > 5 && (
+        <Button variant='outlined' color='primary' onClick={handleButtonClick}>
+          {showAll ? t('Hide') : t('See all')}
+        </Button>
+      )}
+    </LabelizedField>
+  );
+};
+
+export default RelatedFollowUpPaymentPlans;
