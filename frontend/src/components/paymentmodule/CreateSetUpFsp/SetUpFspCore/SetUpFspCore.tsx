0e750483f29e8a8eb2a80a6a2ea3d9a79fd32502
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-05-09 16:02:21 +0200

    fix fsp freeze

--- frontend/src/components/paymentmodule/CreateSetUpFsp/SetUpFspCore/SetUpFspCore.tsx
+++ frontend/src/components/paymentmodule/CreateSetUpFsp/SetUpFspCore/SetUpFspCore.tsx
@@ -176,17 +176,6 @@ export const SetUpFspCore = ({
       enableReinitialize
     >
       {({ values, submitForm }) => {
-        const checkFspIsEmpty = (obj): boolean => {
-          for (let i = 0; i < obj.deliveryMechanisms.length; i + 1) {
-            if (obj.deliveryMechanisms[i].fsp === '') {
-              return true;
-            }
-          }
-          return false;
-        };
-
-        const isFspEmpty = checkFspIsEmpty(values);
-
         return (
           <Form>
             <AutoSubmitFormOnEnter />
@@ -277,7 +266,6 @@ export const SetUpFspCore = ({
                   businessArea={businessArea}
                   paymentPlanId={id}
                   handleBackStep={handleBackStep}
-                  isFspEmpty={isFspEmpty}
                 />
               </ContainerColumnWithBorder>
             </Box>
