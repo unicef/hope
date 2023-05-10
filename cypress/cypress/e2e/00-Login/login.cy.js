d512c0a758c041a6d6ae24c32dfb46062cfc682b
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-05-10 15:30:44 +0530

    some changes

--- cypress/cypress/e2e/00-Login/login.cy.js
+++ cypress/cypress/e2e/00-Login/login.cy.js
@@ -22,7 +22,7 @@ context("Login", () => {
     })
     it('Check the login with Invalid username and Invalid password', () => {
         cy.visit("/api/unicorn/");
-        cy.get('input[name="username"]').type(Cypress.env("username"));
+        cy.get('input[name="username"]').type("wrong-username");
         cy.get('input[name="password"]').type("wrong-password");
         cy.get("input").contains("Log in").click();
         cy.get('.errornote').should('contain', "Please enter the correct username and password for a staff account. Note that both fields may be case-sensitive.")
