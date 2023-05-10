118246fac3b1a1d17b78860229ba638e8c914f34
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-05-10 17:30:23 +0200

    update script

--- backend/hct_mis_api/one_time_scripts/update_payments_without_delivery_date.py
+++ backend/hct_mis_api/one_time_scripts/update_payments_without_delivery_date.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 
+from hct_mis_api.apps.core.models import FileTemp
 from hct_mis_api.apps.payment.models import Payment, PaymentPlan
 
 
@@ -7,18 +8,25 @@ def update_payments_with_empty_delivery_date() -> None:
     payments_to_update = []
     queryset = (
         Payment.objects.filter(delivery_date__isnull=True, parent__status=PaymentPlan.Status.FINISHED)
-        .only("parent", "delivery_date")
-        .select_related("parent__export_file_per_fsp")
+        .only("id", "parent")
+        .select_related("parent")
     )
-    paginator = Paginator(queryset, 500)
+    paginator = Paginator(queryset, 100)
     number_of_pages = paginator.num_pages
+
+    most_recent_files = (
+        FileTemp.objects.order_by("object_id", "-created").distinct("object_id").only("object_id", "created")
+    )
 
     for page in paginator.page_range:
         print(f"Processing page {page} of {number_of_pages}")
         for payment in paginator.page(page).object_list:
-            delivery_date = getattr(payment.parent.export_file_per_fsp, "created", None)
-            if delivery_date:
-                payment.delivery_date = delivery_date
+            file_temp = most_recent_files.filter(object_id=payment.parent_id).first()
+            if file_temp and file_temp.created:
+                payment.delivery_date = file_temp.created
                 payments_to_update.append(payment)
         Payment.objects.bulk_update(payments_to_update, ["delivery_date"])
         payments_to_update = []
+
+    print("Processing finished successfully")
+    return
