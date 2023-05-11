b27bbc3e1886dcb02fb0dc919483bf81715c8782
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-05-11 18:34:22 +0530

    changes in program test cases

--- cypress/cypress/e2e/03-program/program.cy.js
+++ cypress/cypress/e2e/03-program/program.cy.js
@@ -86,46 +86,5 @@ context("Program", () => {
     cy.get('.MuiDialogActions-root > .MuiButton-contained').click({ force: true })
     cy.get('[data-cy="status-container"]').should('contain', "ACTIVE");
   })
-  it("Delete Program", () => {
-    cy.get('[data-cy="button-new-program"]').click({ force: true });
-    cy.get("h6").should('contain', "Set-up a new Programme");
-    cy.uniqueSeed().then((seed) => {
-      const programName = `Draft program ${seed}`;
-      cy.get('[data-cy="input-programme-name"]').type(programName);
-      cy.get('[data-cy="input-cash-assist-scope"]').first().click();
-      cy.get('[data-cy="select-option-Unicef"]').click();
-      cy.get('[data-cy="input-sector"]').first().click();
-      cy.get('[data-cy="select-option-Multi Purpose"]').click();
-      cy.get('[data-cy="input-start-date"]').click().type("2023-04-13");
-      cy.get('[data-cy="input-end-date"]').click().type("2033-05-20");
-      cy.get('[data-cy="input-description"]')
-        .first()
-        .click()
-        .type("Draft description");
-      cy.get('[data-cy="input-budget"]')
-        .first()
-        .click()
-        .type("{backspace}{backspace}{backspace}{backspace}3533");
-      cy.get('[data-cy="input-admin-area"]').click().type("Admin");
-      cy.get('[data-cy="input-population-goal"]')
-        .click()
-        .type("{backspace}{backspace}{backspace}{backspace}3000");
-      cy.get('[data-cy="button-save"]').click({ force: true });
-      cy.get("h6").should('contain', "Programme Details");
-      cy.reload()
-      cy.get('[data-cy="status-container"]').should('contain', "DRAFT");
-      cy.get('[data-cy="button-remove-program"]').click({ force: true })
-      cy.get('[data-cy="button-remove-program"]').eq(1).click({ force: true })
-      cy.get('[data-mui-test="SelectDisplay"]').eq(0).click({ force: true })
-      cy.get('[data-value="DRAFT"]').click({ force: true })
-      cy.reload()
-      cy.get('[data-cy="status-container"]').should('contain', "DRAFT");
-      cy.get('tbody tr').each(($tablerows) => {
-        cy.wrap($tablerows).within(() => {
-          cy.get('td').eq(0).should('not.contain', programName)
-        })
-      })
-    })
-  })
 })
 
