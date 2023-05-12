293af7c921093eaa8f2e2d8c5e746185750aa837
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-05-12 15:41:59 +0530

    added waits

--- cypress/cypress/e2e/04-targeting/targeting.cy.js
+++ cypress/cypress/e2e/04-targeting/targeting.cy.js
@@ -16,7 +16,8 @@ context("Targeting", () => {
       cy.get('[data-cy="input-program"]').first().click();
       cy.get(`[data-cy="select-option-${programName}-${seed}"]`).click();
       cy.get('[data-cy="button-target-population-add-criteria"]').click();
-      cy.get('[data-cy="button-individual-rule"]', { timeout: 20000 }).click({ force: true })
+      cy.wait(1000)
+      cy.get('[data-cy="button-individual-rule"]').click({ force: true })
       cy.get('[data-cy="autocomplete-target-criteria"]').should("be.visible")
       cy.get('[data-cy="autocomplete-target-criteria-option-0"]').type('Age {downArrow}{enter}')
       cy.get('[data-cy="input-individualsFiltersBlocks[0].individualBlockFilters[0].value.from"]').type('0')
@@ -25,7 +26,8 @@ context("Targeting", () => {
       cy.get('[data-cy="criteria-container"]').should('to.visible')
       cy.get("[data-cy='button-target-population-create']").click({ force: true });
       cy.get('[data-cy="status-container"]').should('contain', "OPEN");
-      cy.get('[data-cy="button-target-population-lock"]', { timeout: 20000 }).should('to.visible');
+      cy.wait(1000)
+      cy.get('[data-cy="button-target-population-lock"]').should('to.visible');
       cy.get('[data-cy="label-Total Number of Households"]').invoke('text').should('not.contain', '0');
       cy.get('[data-cy="label-Targeted Individuals"]').invoke('text').should('not.contain', '0');
       cy.get('[data-cy="button-target-population-lock"]',).click({ force: true });
