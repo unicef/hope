d1ffd9c0ab0e84c94519a224375f0619a504374c
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-05-15 14:02:27 +0530

    fixing cypress pipeline

--- cypress/cypress/e2e/04-targeting/targeting.cy.js
+++ cypress/cypress/e2e/04-targeting/targeting.cy.js
@@ -16,7 +16,8 @@ context("Targeting", () => {
       cy.get('[data-cy="input-program"]').first().click();
       cy.get(`[data-cy="select-option-${programName}-${seed}"]`).click();
       cy.get('[data-cy="button-target-population-add-criteria"]').click();
-      cy.get('[data-cy="button-individual-rule"]').click({ force: true })
+      cy.get('[data-cy="button-individual-rule"]').as('addrule')
+      cy.get('@addrule').click({ force: true })
       cy.get('[data-cy="autocomplete-target-criteria"]').should("be.visible")
       cy.get('[data-cy="autocomplete-target-criteria-option-0"]').type('Age {downArrow}{enter}')
       cy.get('[data-cy="input-individualsFiltersBlocks[0].individualBlockFilters[0].value.from"]').type('0')
@@ -25,8 +26,10 @@ context("Targeting", () => {
       cy.get('[data-cy="criteria-container"]').should('to.visible')
       cy.get("[data-cy='button-target-population-create']").click({ force: true });
       cy.get('[data-cy="status-container"]').should('contain', "OPEN");
-      cy.get('[data-cy="label-Total Number of Households"]').invoke('text').should('not.contain', '0');
-      cy.get('[data-cy="label-Targeted Individuals"]').invoke('text').should('not.contain', '0');
+      cy.get('[data-cy="button-target-population-lock"]').as('totalTarget')
+      cy.get('@totalTarget').should('to.visible');
+      cy.get('[data-cy="label-Total Number of Households"]').invoke('text').should('not.contain', ' 0 ');
+      cy.get('[data-cy="label-Targeted Individuals"]').invoke('text').should('not.contain', ' 0 ');
       cy.get('[data-cy="button-target-population-lock"]',).click({ force: true });
       cy.get('[data-cy="button-target-population-modal-lock"]').click({ force: true });
       cy.get('[data-cy="status-container"]').should('contain', "LOCKED");
