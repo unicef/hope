50dca1794a4fb37145376ca5c05f356ab3c9cd8e
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-05-17 17:58:36 +0530

    Target population tc

--- cypress/cypress/e2e/04-targeting/targeting.cy.js
+++ cypress/cypress/e2e/04-targeting/targeting.cy.js
@@ -25,7 +25,6 @@ context("Targeting", () => {
       cy.get('[data-cy="criteria-container"]').should('to.visible')
       cy.get("[data-cy='button-target-population-create']").click({ force: true });
       cy.get('[data-cy="status-container"]').should('contain', "OPEN");
-      cy.get('[data-cy="button-target-population-lock"]').should('to.visible',{timeout:100000},{ force: true });
       cy.get('[data-cy="label-Total Number of Households"]').invoke('text').should('not.contain', ' 0 ');
       cy.get('[data-cy="label-Targeted Individuals"]').invoke('text').should('not.contain', ' 0 ');
       cy.get('[data-cy="button-target-population-lock"]').click({ force: true });
