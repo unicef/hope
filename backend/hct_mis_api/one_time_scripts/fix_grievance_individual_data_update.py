3dd0d0f4c6a2e5dfcfb24063c62c4a9a2c768746
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-05-18 08:41:35 +0200

    black

--- backend/hct_mis_api/one_time_scripts/fix_grievance_individual_data_update.py
+++ backend/hct_mis_api/one_time_scripts/fix_grievance_individual_data_update.py
@@ -1,4 +1,7 @@
-from hct_mis_api.apps.grievance.models import TicketIndividualDataUpdateDetails, GrievanceTicket
+from hct_mis_api.apps.grievance.models import (
+    GrievanceTicket,
+    TicketIndividualDataUpdateDetails,
+)
 
 
 def fix_individual_data_update_details_document_type() -> None:
@@ -22,16 +25,18 @@ def fix_individual_data_update_details_document_type() -> None:
     """
 
     tickets_qs = TicketIndividualDataUpdateDetails.objects.exclude(ticket__status=GrievanceTicket.STATUS_CLOSED)
-    print(f"Found {tickets_qs.count()} tickets. \n Start fixing...", )
+    print(
+        f"Found {tickets_qs.count()} tickets. \n Start fixing...",
+    )
 
     for ticket_details in tickets_qs:
         update = False
 
-        ind_data = ticket_details.individual_data # json
-        for documents_list in ("documents", "documents_to_edit"): # fix only 'documents' and 'documents_to_edit' lists
+        ind_data = ticket_details.individual_data  # json
+        for documents_list in ("documents", "documents_to_edit"):  # fix only 'documents' and 'documents_to_edit' lists
             if docs := ind_data.get(documents_list):
                 for doc in docs:
-                    for key in  ("value", "previous_value"):
+                    for key in ("value", "previous_value"):
                         if doc_value := doc.get(key):  # {"type": "drivers_license", "number": "23"}
                             if doc_type := doc_value.pop("type", None):
                                 doc_value.update({"key": doc_type.lower()})
@@ -40,4 +45,4 @@ def fix_individual_data_update_details_document_type() -> None:
             ticket_details.save(update_fields=["individual_data"])
             print(f"Fixed GrievanceTicket: {ticket_details.ticket.unicef_id}")
 
-    print(f"Finished fixing.")
+    print("Finished fixing.")
