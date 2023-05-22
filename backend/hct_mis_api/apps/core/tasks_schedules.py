d6a69c5d11ef4a0e3e67637fcb81e310bd9319f3
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-05-22 22:11:36 +0200

    Fix style

--- backend/hct_mis_api/apps/core/tasks_schedules.py
+++ backend/hct_mis_api/apps/core/tasks_schedules.py
@@ -55,6 +55,6 @@ TASKS_SCHEDULES = {
     },
     "remove_old_rdi_links_task": {
         "task": "hct_mis_api.apps.registration_datahub.celery_tasks.remove_old_rdi_links_task",
-        "schedule": crontab(0, 0, day_of_month='1,15'),
-    }
+        "schedule": crontab(0, 0, day_of_month="1,15"),
+    },
 }
