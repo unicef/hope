c3e8650cf382d212ca8b6ec62779aa9a2974c2ff
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-05-24 13:07:20 +0200

    Refactor organization of tests - part 1

--- cypress/cypress/e2e/06-payment-verification/payment_verification.cy.js
+++ cypress/cypress/e2e/06-payment-verification/payment_verification.cy.js
@@ -1,30 +1,80 @@
 import PaymentVerification from "../../page-objects/pages/payment_veryfication/payment_veryfication.po";
 
 let pv = new PaymentVerification()
-
-context("Payment Verification", () => {
+describe("Payment Verification", () => {
   beforeEach(() => {
     cy.adminLogin()
     cy.navigateToHomePage();
     pv.clickMenuButtonPaymentVerification()
   });
 
-  it("Check Payment Verification Page is loaded", () => {
-    pv.checkPaymentVerificationTitle()
-    pv.checkListOfCashPlansTitle()
+  context("Smoke Payment Verification", () => {
 
-    pv.checkAllSearchFieldsVisible()
-    pv.checkChachPlansTableVisible()
+    it("Check Payment Verification Page is loaded", () => {
+      pv.checkPaymentVerificationTitle()
+      pv.checkListOfCashPlansTitle()
+
+      pv.checkAllSearchFieldsVisible()
+      pv.checkChachPlansTableVisible()
+    });
   });
 
+  context("E2E Payment Verification", () => {
+    it.skip("Create Verification Plan", () => {
+      // ToDo
+    });
 
-  it("Can see the Cash Plan Details Page", () => {
+    it.skip("Verification Plan Settings", () => {
+      // ToDo
+    });
 
-    return; // TODO: must seed with some cash plan
-    cy.get('[data-cy="cash-plan-table-row"]').first().click();
-    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-    cy.get('[data-cy="page-header-container"]').contains("Payment Plan");
-    cy.get("h6").contains("Cash Plan Details");
-    cy.get("h6").contains("Verification Plans Summary");
+    it.skip("Edit Verification Plan", () => {
+      // ToDo
+    });
+
+    it.skip("Delete Verification Plan", () => {
+      // ToDo
+    });
+
+    it.skip("Activate Verification Plan", () => {
+      // ToDo
+    });
+
+    it.skip("Finish Verification Plan", () => {
+      // ToDo
+    });
+
+    it.skip("Grievance creation/preview", () => {
+      // ToDo
+    });
+
+    it.skip("Verify Payment Record - Verify Manually", () => {
+      // ToDo
+    });
+
+    it.skip("Verify Payment Record - Verify using RapidPro", () => {
+      // ToDo
+    });
+
+    it.skip("Verify Payment Record - Verify using XLSX", () => {
+      // ToDo
+    });
+
+    it.skip("Business Requirements", () => {
+      // ToDo
+    });
+
+    
+    it.skip("Can see the Cash Plan Details Page", () => {
+
+      return; // TODO: must seed with some cash plan
+      cy.get('[data-cy="cash-plan-table-row"]').first().click();
+      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.get('[data-cy="page-header-container"]').contains("Payment Plan");
+      cy.get("h6").contains("Cash Plan Details");
+      cy.get("h6").contains("Verification Plans Summary");
+    });
+  });
+  context("Regression Payment Verification", () => {
   });
 });
