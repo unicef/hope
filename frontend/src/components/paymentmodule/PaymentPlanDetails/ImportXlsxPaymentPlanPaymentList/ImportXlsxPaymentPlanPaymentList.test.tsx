89bde96b121d5d11de4f371ff7990c172b235bbf
Jan Romaniak <jan.romaniak@tivix.com>
2023-05-25 15:00:11 +0200

    fixed test

--- frontend/src/components/paymentmodule/PaymentPlanDetails/ImportXlsxPaymentPlanPaymentList/ImportXlsxPaymentPlanPaymentList.test.tsx
+++ frontend/src/components/paymentmodule/PaymentPlanDetails/ImportXlsxPaymentPlanPaymentList/ImportXlsxPaymentPlanPaymentList.test.tsx
@@ -1,15 +1,15 @@
 import { MockedProvider } from '@apollo/react-testing';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
+import wait from 'waait';
 import { fakeApolloPaymentPlan } from '../../../../../fixtures/paymentmodule/fakeApolloPaymentPlan';
 import { fakeImportXlsxPpListMutation } from '../../../../../fixtures/paymentmodule/fakeImportXlsxPpListMutation';
 import { render } from '../../../../testUtils/testUtils';
-import { PERMISSIONS } from '../../../../config/permissions';
 import { ImportXlsxPaymentPlanPaymentList } from './ImportXlsxPaymentPlanPaymentList';
+import { PERMISSIONS } from '../../../../config/permissions';
 
 describe('components/paymentmodule/PaymentPlanDetails/ImportXlsxPaymentPlanPaymentList', () => {
   it('should render', async () => {
-    //TODO Fix this test
-
     const { container } = render(
       <MockedProvider addTypename={false} mocks={fakeImportXlsxPpListMutation}>
         <ImportXlsxPaymentPlanPaymentList
@@ -18,50 +18,47 @@ describe('components/paymentmodule/PaymentPlanDetails/ImportXlsxPaymentPlanPayme
         />
       </MockedProvider>,
     );
-    expect(container).toMatchSnapshot();
-    // await act(() => wait(0)); // wait for response
-    //
-    // const buttonImport = container.querySelector('[data-cy="button-import"]');
-    // expect(buttonImport).toBeInTheDocument();
-    // fireEvent.click(buttonImport);
-    // //
-    // // act(() => {
-    // //   buttonImport.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-    // // });
-    // await act(() => wait(0));
-    // const dialog = container.querySelector('[data-cy="dialog-import"]');
-    // expect(dialog).toBeInTheDocument();
 
-    // const inputFile = document.querySelector('[data-cy="file-input"]');
-    // const buttonImportSubmit = document.querySelector(
-    //   '[data-cy="button-import-entitlement"]',
-    // );
-    // const buttonClose = document.querySelector('[data-cy="close-button"]');
-    //
-    // expect(inputFile).toBeInTheDocument();
-    // expect(buttonImportSubmit).toBeInTheDocument();
-    // expect(buttonClose).toBeInTheDocument();
-    //
-    // const file = new File([new ArrayBuffer(210715200)], 'sample.xlsx', {
-    //   type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    // });
-    // Object.defineProperty(inputFile, 'files', {
-    //   value: [file],
-    // });
-    //
-    // act(() => {
-    //   inputFile.dispatchEvent(new Event('change', { bubbles: true }));
-    // });
-    //
-    // act(() => {
-    //   buttonImportSubmit.dispatchEvent(
-    //     new MouseEvent('click', { bubbles: true }),
-    //   );
-    // });
-    //
-    // act(() => {
-    //   buttonClose.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-    // });
-    // expect(container).toMatchSnapshot();
+    const buttonImport = container.querySelector('[data-cy="button-import"]');
+    expect(buttonImport).toBeInTheDocument();
+
+    act(() => {
+      buttonImport.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    });
+
+    const inputFile = document.querySelector('[data-cy="file-input"]');
+    const buttonImportSubmit = document.querySelector(
+      '[data-cy="button-import-entitlement"]',
+    );
+    const buttonClose = document.querySelector('[data-cy="close-button"]');
+
+    expect(inputFile).toBeInTheDocument();
+    expect(buttonImportSubmit).toBeInTheDocument();
+    expect(buttonClose).toBeInTheDocument();
+
+    const file = new File([new ArrayBuffer(210715200)], 'sample.xlsx', {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+    Object.defineProperty(inputFile, 'files', {
+      value: [file],
+    });
+
+    act(() => {
+      inputFile.dispatchEvent(new Event('change', { bubbles: true }));
+    });
+
+    act(() => {
+      buttonImportSubmit.dispatchEvent(
+        new MouseEvent('click', { bubbles: true }),
+      );
+    });
+
+    act(() => {
+      buttonClose.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    });
+
+    await act(() => wait(0)); // wait for response
+
+    expect(container).toMatchSnapshot();
   });
 });
