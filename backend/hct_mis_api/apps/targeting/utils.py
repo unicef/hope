b821365a0efc361662ceffe9470967333e50c292
Paulina Kujawa <p.kujawa2f@gmail.com>
2023-05-26 14:20:46 +0200

    review fix

--- backend/hct_mis_api/apps/targeting/utils.py
+++ backend/hct_mis_api/apps/targeting/utils.py
@@ -13,43 +13,28 @@ def apply_targeting_criteria_flags(target_population: TargetPopulation, househol
 
 
 def apply_flag_exclude_if_active_adjudication_ticket(households: QuerySet) -> QuerySet:
-    households_to_be_excluded = households.annotate(
-        has_active_adjudication_tickets=Case(
-            When(
-                (
-                    Q(individuals__ticket_duplicates__isnull=False)
-                    & ~Q(individuals__ticket_duplicates__ticket__status=GrievanceTicket.STATUS_CLOSED)
-                )
-                | (
-                    Q(individuals__ticket_golden_records__isnull=False)
-                    & ~Q(individuals__ticket_golden_records__ticket__status=GrievanceTicket.STATUS_CLOSED)
-                )
-                | (
-                    Q(representatives__ticket_duplicates__isnull=False)
-                    & ~Q(representatives__ticket_duplicates__ticket__status=GrievanceTicket.STATUS_CLOSED)
-                )
-                | (
-                    Q(representatives__ticket_golden_records__isnull=False)
-                    & ~Q(representatives__ticket_golden_records__ticket__status=GrievanceTicket.STATUS_CLOSED)
-                ),
-                then=True,
-            ),
-            default=False,
+    return households.exclude(
+        (
+            Q(individuals__ticket_duplicates__isnull=False)
+            & ~Q(individuals__ticket_duplicates__ticket__status=GrievanceTicket.STATUS_CLOSED)
         )
-    ).filter(has_active_adjudication_tickets=True)
-
-    return households.exclude(id__in=households_to_be_excluded)
+        | (
+            Q(individuals__ticket_golden_records__isnull=False)
+            & ~Q(individuals__ticket_golden_records__ticket__status=GrievanceTicket.STATUS_CLOSED)
+        )
+        | (
+            Q(representatives__ticket_duplicates__isnull=False)
+            & ~Q(representatives__ticket_duplicates__ticket__status=GrievanceTicket.STATUS_CLOSED)
+        )
+        | (
+            Q(representatives__ticket_golden_records__isnull=False)
+            & ~Q(representatives__ticket_golden_records__ticket__status=GrievanceTicket.STATUS_CLOSED)
+        )
+    )
 
 
 def apply_flag_exclude_if_on_sanction_list(households: QuerySet) -> QuerySet:
-    households_to_be_excluded = households.annotate(
-        is_on_saction_list=Case(
-            When(
-                Q(individuals__sanction_list_confirmed_match=True)
-                | Q(representatives__sanction_list_confirmed_match=True),
-                then=True,
-            ),
-            default=False,
-        )
-    ).filter(is_on_saction_list=True)
-    return households.exclude(id__in=households_to_be_excluded)
+    return households.exclude(
+        Q(individuals__sanction_list_confirmed_match=True)
+        | Q(representatives__sanction_list_confirmed_match=True)
+    )
