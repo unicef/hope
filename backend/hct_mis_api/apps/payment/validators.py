13f6c57a81dc8dbdccbbfcdc615aded9ca0d0bf7
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-05-26 09:06:43 +0200

    review changes

--- backend/hct_mis_api/apps/payment/validators.py
+++ backend/hct_mis_api/apps/payment/validators.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 from django.core.exceptions import ValidationError
 
@@ -6,22 +7,20 @@ from django.core.exceptions import ValidationError
 def generate_numeric_token(digit_number: int = 3) -> int:
     while True:
         # Token and Order Number must not start with 0
-        token = random.choice("123456789") + "".join(random.choices("1234567890", k=digit_number - 1))
+        token = random.randint(10 ** (digit_number - 1), (10**digit_number) - 1)
         # Token and Order Number must not have the same digit more than 3 times in a row
-        if not has_repeated_digits(token):
-            return int(token)
+        if not has_repeated_digits_more_than_3_times(token):
+            return token
 
 
-def has_repeated_digits(token: str) -> bool:
+def has_repeated_digits_more_than_3_times(token: int) -> bool:
     """
     check if the token has the same digit repeated more than 3 times in a row (like 1111)
     """
-    for i in range(len(token) - 3):
-        if token[i] == token[i + 1] == token[i + 2] == token[i + 3]:
-            return True
-    return False
+    pattern = r"(\d)\1{3,}"
+    return bool(re.search(pattern, str(token)))
 
 
 def payment_token_and_order_number_validator(value: int) -> None:
-    if has_repeated_digits(str(value)):
+    if has_repeated_digits_more_than_3_times(value):
         raise ValidationError("Token and Order Number must not have the same digit more than 3 times in a row.")
