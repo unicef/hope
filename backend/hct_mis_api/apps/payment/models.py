59a2027c58349ec507a9e41ee0e575462a483255
Paulina Kujawa <p.kujawa2f@gmail.com>
2023-05-29 10:42:26 +0200

    fix

--- backend/hct_mis_api/apps/payment/models.py
+++ backend/hct_mis_api/apps/payment/models.py
@@ -741,7 +741,7 @@ class PaymentPlan(SoftDeletableModel, GenericPaymentPlan, UnicefIdentifiedModel)
             if self.status == PaymentPlan.Status.LOCKED and not self.is_follow_up:
                 return self.export_file_entitlement is not None
             elif self.status in (PaymentPlan.Status.ACCEPTED, PaymentPlan.Status.FINISHED):
-                return self.export_file_per_fsp is not None
+                return self.export_file_per_fsp is not None or FileTemp.objects.filter(object_id=self.id).exists()
             else:
                 return False
         except FileTemp.DoesNotExist:
@@ -752,7 +752,9 @@ class PaymentPlan(SoftDeletableModel, GenericPaymentPlan, UnicefIdentifiedModel)
         if self.status == PaymentPlan.Status.LOCKED and not self.is_follow_up:
             return self.export_file_entitlement.file.url
         elif self.status in (PaymentPlan.Status.ACCEPTED, PaymentPlan.Status.FINISHED):
-            return self.export_file_per_fsp.file.url
+            if self.export_file_per_fsp:
+                return self.export_file_per_fsp.file.url
+            return FileTemp.objects.filter(object_id=self.id).order_by("-created").first().file.url
         else:
             return None
 
