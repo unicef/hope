6e4da31c16bf1139dfafa1fe595f459195ffc75a
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-05-29 16:20:40 +0200

    Remove es data

--- backend/hct_mis_api/apps/utils/elasticsearch_utils.py
+++ backend/hct_mis_api/apps/utils/elasticsearch_utils.py
@@ -61,7 +61,10 @@ def populate_all_indexes() -> None:
 
 
 def remove_elasticsearch_documents_by_matching_ids(id_list: List[str], document: "Type[Document]") -> None:
-    query_dict = {"query": {"terms": {"id.keyword": [str(_id) for _id in id_list]}}}
+    logger.info("****")
+    logger.info(id_list)
+    logger.info(document)
+    query_dict = {"query": {"terms": {"_id": [str(_id) for _id in id_list]}}}
     search = Search(index=document.Index.name)
     search.update_from_dict(query_dict)
     search.delete()
