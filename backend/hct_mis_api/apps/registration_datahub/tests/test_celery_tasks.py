5edd097ed39b7fc89285535b336553ed5c82272a
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-05-29 19:51:55 +0200

    Fix style

--- backend/hct_mis_api/apps/registration_datahub/tests/test_celery_tasks.py
+++ backend/hct_mis_api/apps/registration_datahub/tests/test_celery_tasks.py
@@ -16,7 +16,6 @@ from django.utils import timezone
 from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.core.utils import IDENTIFICATION_TYPE_TO_KEY_MAPPING
 from hct_mis_api.apps.geo import models as geo_models
-from hct_mis_api.apps.household.fixtures import create_household
 from hct_mis_api.apps.household.models import (
     DISABLED,
     FEMALE,
