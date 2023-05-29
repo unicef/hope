272fb5c031bbe328c504b4fdc4c93736feaf9352
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-05-29 16:26:04 +0200

    Add periodic task

--- backend/hct_mis_api/apps/registration_datahub/celery_tasks.py
+++ backend/hct_mis_api/apps/registration_datahub/celery_tasks.py
@@ -12,7 +12,7 @@ from sentry_sdk import configure_scope
 from hct_mis_api.apps.core.celery import app
 from hct_mis_api.apps.household.models import Document
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
-from hct_mis_api.apps.registration_datahub.models import Record
+from hct_mis_api.apps.registration_datahub.models import Record, ImportedIndividual, ImportedHousehold
 from hct_mis_api.apps.registration_datahub.services.extract_record import extract
 from hct_mis_api.apps.registration_datahub.tasks.deduplicate import (
     HardDocumentDeduplication,
@@ -532,3 +532,53 @@ def check_rdi_merge_periodic_task() -> bool:
             return True
         rdi_merge_celery_manager.execute()
     return True
+
+
+@app.task
+@sentry_tags
+def remove_old_rdi_links_task() -> None:
+    """This task removes linked RDI Datahub objects for households which were created more than 2 weeks ago"""
+
+    from datetime import timedelta
+
+    from hct_mis_api.apps.household.models import Household, Individual
+
+    try:
+        # Get all HHs older than 2 weeks
+        old_household_ids = list(
+            Household.objects.filter(is_removed=False, created_at__lte=timezone.now() - timedelta(weeks=2)).values_list(
+                "id", flat=True
+            )
+        )
+
+        # Get all imported individuals linked to HHs
+        old_imported_individuals_ids = list(
+            Individual.objects.filter(household__id__in=old_household_ids).values_list(
+                "imported_individual_id", flat=True
+            )
+        )
+
+        # Get unique imported HHs ids
+        imported_household_ids_to_remove = list(
+            ImportedIndividual.objects.filter(id__in=old_imported_individuals_ids)
+            .values_list("household_id", flat=True)
+            .distinct()
+        )
+
+        page_count = 1000
+        if len(imported_household_ids_to_remove) <= page_count:
+            # removes with CASCADE individuals, documents, roles, bank_infos
+            ImportedHousehold.objects.filter(id__in=imported_household_ids_to_remove).delete()
+        else:
+            i, count = 0, len(imported_household_ids_to_remove) // page_count
+            while i <= count:
+                logger.info(f"Page {i}/{count} processing...")
+
+                ImportedHousehold.objects.filter(
+                    id__in=imported_household_ids_to_remove[i * page_count : (i + 1) * page_count]
+                ).delete()
+                i += 1
+
+    except Exception:
+        logger.error("Removing old RDI objects failed")
+        raise
