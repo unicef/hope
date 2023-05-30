c262612e9aa42f39b5855da1f1b1e50ebdb607d8
Paulina Kujawa <p.kujawa2f@gmail.com>
2023-05-30 19:11:19 +0200

    admin areas reassign - admin1_area as admin_area, admin2_area as admin1, admin3_area as admin2

--- backend/hct_mis_api/one_time_scripts/tests/test_add_missing_admin_area_for_ukr_households.py
+++ backend/hct_mis_api/one_time_scripts/tests/test_add_missing_admin_area_for_ukr_households.py
@@ -37,17 +37,15 @@ class TestUpdateAdminAreaFromFile(TestCase):
         area_type_level_1 = AreaTypeFactory(name="State1", area_level=1)
         area_type_level_2 = AreaTypeFactory(name="State2", area_level=2)
         area_type_level_3 = AreaTypeFactory(name="State3", area_level=3)
-        area_type_level_4 = AreaTypeFactory(name="State4", area_level=4)
-        cls.area1 = AreaFactory(name="City Test1", area_type=area_type_level_1, p_code="area1")
-        cls.area2 = AreaFactory(name="City Test2", area_type=area_type_level_2, p_code=admin1_area_id, parent=cls.area1)
-        cls.area3 = AreaFactory(name="City Test3", area_type=area_type_level_3, p_code=admin2_area_id, parent=cls.area2)
-        cls.area4 = AreaFactory(name="City Test4", area_type=area_type_level_4, p_code=admin3_area_id, parent=cls.area3)
+        cls.area1 = AreaFactory(name="City Test1", area_type=area_type_level_1, p_code=admin1_area_id)
+        cls.area2 = AreaFactory(name="City Test2", area_type=area_type_level_2, p_code=admin2_area_id, parent=cls.area1)
+        cls.area3 = AreaFactory(name="City Test3", area_type=area_type_level_3, p_code=admin3_area_id, parent=cls.area2)
 
     def test_update_household_admin_areas(self) -> None:
         with mock.patch("sys.stdout", new=StringIO()), mock.patch("os.path.join") as mock_join:
             mock_join.return_value = self.test_file_path
             update_admin_area_for_households()
         self.household.refresh_from_db()
+        self.assertEqual(self.household.admin_area, self.area1)
         self.assertEqual(self.household.admin1, self.area2)
         self.assertEqual(self.household.admin2, self.area3)
-        self.assertEqual(self.household.admin3, self.area4)
