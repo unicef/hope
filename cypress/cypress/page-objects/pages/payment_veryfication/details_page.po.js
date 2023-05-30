ccaac8cd697de2e578bd0cf8b53cdc9374967c65
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-05-30 10:05:22 +0200

    Added tests: Delete one Verification Plan; Activate Verification Plan

--- cypress/cypress/page-objects/pages/payment_veryfication/details_page.po.js
+++ cypress/cypress/page-objects/pages/payment_veryfication/details_page.po.js
@@ -11,6 +11,22 @@ export default class PVDetailsPage extends BaseComponent {
     divVerificationPlansSummary = 'div[class="sc-feJyhm GGSnz"]'
     tableTitle = 'h6[class="MuiTypography-root MuiTypography-h6"]'
     gridBankReconciliation = 'div[class="MuiGrid-root MuiGrid-container MuiGrid-direction-xs-column"]'
+    summaryStatus = 'div[data-cy="label-Status"]'
+    summaryActivationDate = 'div[data-cy="label-Activation Date"]'
+    summaryCompletionDate = 'div[data-cy="label-Completion Date"]'
+    summaryNumberOfPlans = 'div[data-cy="label-Number of Verification Plans"]'
+    deletePlan = 'button[data-cy="button-delete-plan"]'
+    deletePopUP = 'div[data-cy="dialog-actions-container"]'
+    snackbarMessage = 'div[class="MuiSnackbarContent-message"]'
+    activetePlan = 'button[data-cy="button-activate-plan"]'
+    discardPlan = 'button[data-cy="button-discard-plan"]'
+    // Create Verification Plan
+    cvp = 'div[aria-labelledby="form-dialog-title"]'
+    cvpTabList = 'div[role="tablist"]'
+    cvpTab = 'button[role="tab"]'
+    cvpTitle = 'h2[class="MuiTypography-root MuiTypography-h6"]'
+    cvpSubtitle = 'span[class="MuiTypography-root MuiTypography-caption"]'
+    cvpSubmit = 'button[data-cy="button-submit"]'
 
     // Texts
     textTitle = "Payment Verification"
@@ -24,6 +40,12 @@ export default class PVDetailsPage extends BaseComponent {
     textSuccessful = "SUCCESSFUL"
     textErroneus = "ERRONEOUS"
     textVerificationPlansSummary = "Verification Plans Summary"
+    textSummaryStatus = "Status"
+    textSummaryActivationDate = "Activation Date"
+    textSummaryCompletionDate = "Completion Date"
+    textSummaryNumberOfPlans = "Number of Verification Plans"
+    textCVPTitle = "Create Verification Plan"
+    textCVPConfidenceInterval = "Confidence Interval"
 
     // Elements
     getPaymentVerificationTitle = () => cy.get(this.paymentVerificationTitle)
@@ -37,6 +59,23 @@ export default class PVDetailsPage extends BaseComponent {
     getSuccessful = () => cy.get(this.divBankReconciliation).eq(0).get(this.gridBankReconciliation).get("div").eq(0)
     getErroneus = () => cy.get(this.divBankReconciliation).eq(0).get(this.gridBankReconciliation).get("div").eq(1)
     getVerificationPlansSummary = () => cy.get(this.divVerificationPlansSummary).get(this.tableTitle)
+    getStatus = () => cy.get(this.summaryStatus)
+    getActivationDate = () => cy.get(this.summaryActivationDate)
+    getCompletionDate = () => cy.get(this.summaryCompletionDate)
+    getNumberOfPlans = () => cy.get(this.summaryNumberOfPlans)
+    getDeletePlan = () => cy.get(this.deletePlan)
+    getDelete = () => cy.get(this.deletePopUP).get(this.cvpSubmit)
+    getSnackbarMessage = () => cy.get(this.snackbarMessage)
+    getActivatePlan = () => cy.get(this.activetePlan)
+    getActivate = () => cy.get(this.cvpSubmit)
+    getDiscardPlan = () => cy.get(this.discardPlan)
+    getDiscard = () => cy.get(this.cvpSubmit)
+    // Create Verification Plan
+    getCVPTitle = () => cy.get(this.cvp).get(this.cvpTitle)
+    getFullList = () => cy.get(this.cvp).get(this.cvpTab).eq(0)
+    getRandomSampling = () => cy.get(this.cvp).get(this.cvpTab).eq(1)
+    getCVPConfidenceInterval = () => cy.get(this.cvp).get(this.cvpSubtitle)
+    getCVPSave = () => cy.get(this.cvp).get(this.cvpSubmit)
 
     checkPaymentVerificationTitle(){
         this.getPaymentVerificationTitle().contains(this.textTitle)
@@ -65,6 +104,44 @@ export default class PVDetailsPage extends BaseComponent {
     }
 
     checkGridVerificationPlansSummary(){
+        this.getStatus().prev().contains(this.textSummaryStatus)
+        this.getActivationDate().prev().contains(this.textSummaryActivationDate)
+        this.getCompletionDate().prev().contains(this.textSummaryCompletionDate)
+        this.getNumberOfPlans().prev().contains(this.textSummaryNumberOfPlans)
+    }
 
+    checkCVPTitle(){
+        this.getCVPTitle().contains(this.textCVPTitle)
+    }
+    
+    checkVerificationPlan(){
+        this.checkVerificationPlansSummaryTitle()
+    }
+
+    deleteVerificationPlan(){
+        this.getDeletePlan().click()
+        this.getDelete().click()
+        this.getNumberOfPlans().contains(0)
+    }
+
+    discardVerificationPlan(){
+        this.getDiscardPlan().click()
+        this.getDiscard().click()
+        this.getDeletePlan()
+    }
+
+    createNewVerificationPlan(){
+
+        this.checkPaymentVerificationTitle()
+        this.getNumberOfPlans().then(($el) => {
+          if ($el.text() == "0") {
+            this.getCreateVerificationPlan().click()
+            this.checkCVPTitle()
+            this.getRandomSampling().click()
+            this.getCVPConfidenceInterval().contains(this.textCVPConfidenceInterval)
+            this.getCVPSave().click()
+            this.checkVerificationPlan()
+          }
+          });
     }
 }
