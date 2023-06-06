27fdd9e233849171c6642f1e0dfd5eebd41f4c40
Domenico DiNicola <dom.dinicola@gmail.com>
2023-06-06 14:40:59 +0200

    pq fix

--- backend/hct_mis_api/apps/power_query/models.py
+++ backend/hct_mis_api/apps/power_query/models.py
@@ -102,7 +102,8 @@ class Parametrizer(NaturalKeyModel, models.Model):
         blank=True,
     )
     value = models.JSONField(default=dict, blank=False, validators=[validate_queryargs])
-    system = models.BooleanField(blank=True, default=False, editable=False)
+    system = models.BooleanField(blank=True, default=False, editable=False
+    source = models.ForeignKey("Query", blank=True, null=True, on_delete=models.CASCADE, related_name="+")
 
     class Meta:
         verbose_name_plural = "Arguments"
