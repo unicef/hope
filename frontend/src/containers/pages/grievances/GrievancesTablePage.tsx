1dbe227e13d71b04f5e288f8df89788ffa80f386
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-06-13 19:35:09 +0200

    add separate urls for user-generated, system-generated tickets

--- frontend/src/containers/pages/grievances/GrievancesTablePage.tsx
+++ frontend/src/containers/pages/grievances/GrievancesTablePage.tsx
@@ -49,7 +49,9 @@ export const GrievancesTablePage = (): React.ReactElement => {
   };
 
   const [selectedTab, setSelectedTab] = useState(
-    GRIEVANCE_TICKETS_TYPES.userGenerated,
+    isUserGenerated
+      ? GRIEVANCE_TICKETS_TYPES.userGenerated
+      : GRIEVANCE_TICKETS_TYPES.systemGenerated,
   );
 
   const [filter, setFilter] = useState(
@@ -64,8 +66,8 @@ export const GrievancesTablePage = (): React.ReactElement => {
   } = useGrievancesChoiceDataQuery();
 
   const grievanceTicketsTypes = ['USER-GENERATED', 'SYSTEM-GENERATED'];
-  const userGeneratedPath = `/${businessArea}/grievance-and-feedback/user-generated`;
-  const systemGeneratedPath = `/${businessArea}/grievance-and-feedback/system-generated`;
+  const userGeneratedPath = `/${businessArea}/grievance-and-feedback/tickets/user-generated`;
+  const systemGeneratedPath = `/${businessArea}/grievance-and-feedback/tickets/system-generated`;
 
   const mappedTabs = grievanceTicketsTypes.map((el) => (
     <Tab key={el} label={el} />
