b9a6b447c906c23da03921444573b3f47b2e93c6
Domenico DiNicola <dom.dinicola@gmail.com>
2023-06-15 15:42:47 +0200

    163658 fix mas withdrawn

--- backend/hct_mis_api/apps/household/admin/mixins.py
+++ backend/hct_mis_api/apps/household/admin/mixins.py
@@ -19,7 +19,6 @@ from ..forms import (
     CreateTargetPopulationForm,
     MassRestoreForm,
     MassWithdrawForm,
-    RestoreForm,
     WithdrawForm,
 )
 from ..models import Household
@@ -143,7 +142,7 @@ class HouseholdWithDrawnMixin:
                 context["form"] = form
                 return TemplateResponse(request, "admin/household/household/mass_withdrawn.html", context)
         else:
-            context["form"] = RestoreForm(
+            context["form"] = MassRestoreForm(
                 initial={
                     "reopen_tickets": True,
                     "_selected_action": request.POST.getlist(ACTION_CHECKBOX_NAME),
