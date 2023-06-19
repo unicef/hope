57f2790872d7ef7ac51c2c0ad2ca91404c6e571e
Domenico <dom.dinicola@gmail.com>
2023-06-19 13:05:42 +0200

    rdi strategy (#2623)

--- backend/hct_mis_api/apps/registration_datahub/services/flex_registration_service.py
+++ backend/hct_mis_api/apps/registration_datahub/services/flex_registration_service.py
@@ -1,38 +1,11 @@
-from typing import TYPE_CHECKING, Any, Dict, List
-
-from hct_mis_api.apps.registration_datahub.services.czech_republic_flex_registration_service import (
-    CzechRepublicFlexRegistration,
-)
-from hct_mis_api.apps.registration_datahub.services.sri_lanka_flex_registration_service import (
-    SriLankaRegistrationService,
-)
-from hct_mis_api.apps.registration_datahub.services.ukraine_flex_registration_service import (
-    UkraineBaseRegistrationService,
-    UkraineRegistrationService,
-)
+from typing import TYPE_CHECKING, Any, List
 
 if TYPE_CHECKING:
     from uuid import UUID
 
 
-def get_registration_to_rdi_service_map() -> Dict[int, Any]:
-    return {
-        2: UkraineBaseRegistrationService,  # ukraine
-        3: UkraineBaseRegistrationService,  # ukraine
-        21: UkraineRegistrationService,  # ukraine new form
-        26: UkraineRegistrationService,  # ukraine new form
-        27: UkraineRegistrationService,  # ukraine new form
-        28: UkraineRegistrationService,  # ukraine new form
-        29: UkraineRegistrationService,  # ukraine new form
-        17: SriLankaRegistrationService,  # sri lanka
-        # 18: "czech republic",
-        # 19: "czech republic",
-        25: CzechRepublicFlexRegistration,  # czech republic
-    }
-
-
-def create_task_for_processing_records(service: Any, rdi_id: "UUID", records_ids: List) -> None:
+def create_task_for_processing_records(service: Any, reg_id: "UUID", rdi_id: "UUID", records_ids: List) -> None:
     if celery_task := service.PROCESS_FLEX_RECORDS_TASK:
-        celery_task.delay(rdi_id, records_ids, service.REGISTRATION_ID)
+        celery_task.delay(reg_id, rdi_id, records_ids)
     else:
         raise NotImplementedError
