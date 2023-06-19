23c517b5f477e481b481dc5c9180dd1b068f153c
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-06-19 17:32:28 +0200

    Add filters

--- frontend/src/apollo/queries/rdi/AllRegistrationDataImports.ts
+++ frontend/src/apollo/queries/rdi/AllRegistrationDataImports.ts
@@ -14,6 +14,7 @@ export const ALL_REGISTRATION_DATA_IMPORT_QUERY = gql`
     $businessArea: String
     $importDateRange: String
     $size: String
+    $programId: UUID
   ) {
     allRegistrationDataImports(
       after: $after
@@ -28,6 +29,7 @@ export const ALL_REGISTRATION_DATA_IMPORT_QUERY = gql`
       businessArea: $businessArea
       importDateRange: $importDateRange
       size: $size
+      programId: $programId
     ) {
       pageInfo {
         hasNextPage
