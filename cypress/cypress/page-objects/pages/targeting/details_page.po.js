f48d14c781da945fb97a2e74e1b9bb0790c9a96e
szymon-kellton <130459593+szymon-kellton@users.noreply.github.com>
2023-06-19 15:55:29 +0200

    Add targeting smoke test (#2700)
    * Added targeting smoke test
    
    * Added tests to targeting details and targeting create new (smoke)
    
    * Fixed naming
    
    * Fixed naming of targeting pages variables

--- cypress/cypress/page-objects/pages/targeting/details_page.po.js
+++ cypress/cypress/page-objects/pages/targeting/details_page.po.js
@@ -2,6 +2,15 @@ import BaseComponent from "../../base.component";
 
 export default class TDetailsPage extends BaseComponent {
   // Locators
+  titlePage = 'h5[data-cy="page-header-title"]';
+  status = 'div[data-cy="target-population-status"]';
   // Texts
   // Elements
+  getTitlePage = () => cy.get(this.titlePage);
+  getStatus = () => cy.get(this.status);
+
+  checkElementsOnPage(status) {
+    this.getTitlePage().should("be.visible");
+    this.getStatus().contains(status);
+  }
 }
