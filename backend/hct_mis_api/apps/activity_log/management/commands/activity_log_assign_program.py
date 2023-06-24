4fbb1401040563e5b46084ffb31d9186ba134537
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-06-24 14:59:33 +0200

    remove print

--- backend/hct_mis_api/apps/activity_log/management/commands/activity_log_assign_program.py
+++ backend/hct_mis_api/apps/activity_log/management/commands/activity_log_assign_program.py
@@ -89,9 +89,7 @@ def activity_log_assign_program() -> None:
         print(f"Loading page {page} of {number_of_pages}")
 
         for log in paginator.page(page).object_list:
-            program_id = GetProgramId(log.content_object).get_id
-            print(f"Program id for {log.content_object.__class__.__name__} is {str(program_id)}")
-            log.program_id = program_id
+            log.program_id = GetProgramId(log.content_object).get_id
             to_update.append(log)
 
         LogEntry.objects.bulk_update(to_update, ["program_id"])
