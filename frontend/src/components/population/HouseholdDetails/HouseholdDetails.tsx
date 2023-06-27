a08cbde5745a6fcc5d2accfc48a98af843ff6116
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-06-27 14:40:15 +0200

    remove Programme from benefits section of Household Details

--- frontend/src/components/population/HouseholdDetails/HouseholdDetails.tsx
+++ frontend/src/components/population/HouseholdDetails/HouseholdDetails.tsx
@@ -2,14 +2,13 @@ import { Box, Grid, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { MiśTheme } from '../../../theme';
-import { COLLECT_TYPES_MAPPING } from '../../../utils/constants';
-import { choicesToDict, formatCurrencyWithSymbol } from '../../../utils/utils';
 import {
   GrievancesChoiceDataQuery,
   HouseholdChoiceDataQuery,
   HouseholdNode,
 } from '../../../__generated__/graphql';
+import { COLLECT_TYPES_MAPPING } from '../../../utils/constants';
+import { choicesToDict, formatCurrencyWithSymbol } from '../../../utils/utils';
 import { ContentLink } from '../../core/ContentLink';
 import { LabelizedField } from '../../core/LabelizedField';
 import { Title } from '../../core/Title';
@@ -45,10 +44,6 @@ const Overview = styled.div`
 const OverviewPaper = styled(Paper)`
   margin: 20px 20px 0 20px;
   padding: 20px ${({ theme }) => theme.spacing(11)}px;
-`;
-
-const Label = styled.span`
-  ${({ theme }: { theme: MiśTheme }) => theme.styledMixins.label}
 `;
 
 interface HouseholdDetailsProps {
@@ -192,60 +187,40 @@ export const HouseholdDetails = ({
           <Typography variant='h6'>{t('Benefits')}</Typography>
         </Title>
         <Grid container>
-          <Grid item xs={4}>
-            <Grid container>
-              <Grid item xs={6}>
-                <Label color='textSecondary'>
-                  {t('PrOgRAmmE(S) ENROLLED')}
-                </Label>
-              </Grid>
-              <Grid item xs={6}>
-                <Label color='textSecondary'>{t('Cash received')}</Label>
-              </Grid>
-            </Grid>
-            {household?.programsWithDeliveredQuantity?.length ? (
-              household?.programsWithDeliveredQuantity?.map((item) => (
-                <Box key={item.id} mb={2}>
-                  <Grid container key={item.id}>
-                    <Grid item xs={6}>
-                      <ContentLink href={`/${baseUrl}/details/${item.id}`}>
-                        {item.name}
-                      </ContentLink>
+          <Grid item xs={3}>
+            <LabelizedField label={t('Cash received')}>
+              {household?.programsWithDeliveredQuantity?.length ? (
+                household?.programsWithDeliveredQuantity?.map((item) => (
+                  <Box key={item.id} mb={2}>
+                    <Grid container key={item.id}>
+                      <Grid item xs={6}>
+                        <Box display='flex' flexDirection='column'>
+                          {item.quantity.map((qty) => (
+                            <Box
+                              key={`${item.id}-${qty.currency}-${qty.totalDeliveredQuantity}`}
+                            >
+                              {qty.currency === 'USD'
+                                ? formatCurrencyWithSymbol(
+                                    qty.totalDeliveredQuantity,
+                                    qty.currency,
+                                  )
+                                : `(${formatCurrencyWithSymbol(
+                                    qty.totalDeliveredQuantity,
+                                    qty.currency,
+                                  )})`}
+                            </Box>
+                          ))}
+                        </Box>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                      <Box display='flex' flexDirection='column'>
-                        {item.quantity.map((qty) => (
-                          <Box
-                            key={`${item.id}-${qty.currency}-${qty.totalDeliveredQuantity}`}
-                          >
-                            {qty.currency === 'USD'
-                              ? formatCurrencyWithSymbol(
-                                  qty.totalDeliveredQuantity,
-                                  qty.currency,
-                                )
-                              : `(${formatCurrencyWithSymbol(
-                                  qty.totalDeliveredQuantity,
-                                  qty.currency,
-                                )})`}
-                          </Box>
-                        ))}
-                      </Box>
-                    </Grid>
-                  </Grid>
-                </Box>
-              ))
-            ) : (
-              <Grid container>
-                <Grid item xs={6}>
-                  -
-                </Grid>
-                <Grid item xs={6}>
-                  -
-                </Grid>
-              </Grid>
-            )}
+                  </Box>
+                ))
+              ) : (
+                <>-</>
+              )}
+            </LabelizedField>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={3}>
             <BigValueContainer>
               <LabelizedField label={t('Total Cash Received')}>
                 <BigValue>
