8b4ce5719294a851a89c84443ddd2b00cffdce42
Szymon <szymon.wyderka@kellton.com>
2023-06-28 14:18:25 +0200

    Fixed cypress.config.js

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -4,31 +4,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on("task", verifyDownloadTasks);
-      on("after:run", (results) => {
-        if (results) {
-          const fs = require("fs");
-          fs.writeFile(
-            "cypress/reports/test.txt",
-            results.totalPassed.toString(),
-            (err) => {
-              if (err) {
-                console.error(err);
-              }
-              // file written successfully
-            }
-          );
-          // if (results.totalPassed !== results.totalTests) {
-          //   throw "Tests fails: " + results.totalFailed.toString();
-          // }
-          // results will be undefined in interactive mode
-          // console.log(
-          //   results.totalPassed,
-          //   "out of",
-          //   results.totalTests,
-          //   "passed"
-          // );
-        }
-      });
     },
     projectId: "cypress",
     baseUrl: "http://localhost:8082",
