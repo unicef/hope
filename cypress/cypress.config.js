a0f109fc88ddea1dd7f2d295c700c80053f44883
Szymon <szymon.wyderka@kellton.com>
2023-07-03 17:55:19 +0200

    Test of Slack_Report

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -4,6 +4,33 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on("task", verifyDownloadTasks);
+      on("after:run", (results) => {
+        //curl -d "text=Hi I am a bot that can post messages to any public channel." -d "channel=C05EKHETMT9" -H "Authorization: Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ" -X POST https://slack.com/api/chat.postMessage
+        console.log("Gagagaggaga");
+        // request("POST", "https://slack.com/api/chat.postMessage", {
+        //   auth: "Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ",
+        //   body: {
+        //     text: "Hi I am a bot that can post messages to any public channel.",
+        //     channel: "C05EKHETMT9",
+        //   },
+        // }).then((res) => {
+        //   console.log("Gugugugugu");
+        //   console.log(res);
+        fetch("https://slack.com/api/chat.postMessage", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+            Authorization:
+              "Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ",
+          },
+          data: JSON.stringify({
+            text: "Hi I am a bot that can post messages to any public channel.",
+            channel: "C05EKHETMT9",
+          }),
+        }).then((r) => {
+          console.log(r.status);
+        });
+      });
     },
     projectId: "cypress",
     baseUrl: "http://localhost:8082",
