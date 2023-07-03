5ee2070712846b3e509397d59c3b8f075cad230a
Szymon <szymon.wyderka@kellton.com>
2023-07-03 18:02:55 +0200

    Slack_Report test: ci-test

--- cypress/docker-entrypoint.sh
+++ cypress/docker-entrypoint.sh
@@ -12,6 +12,7 @@ case "$1" in
         waitforit -host=proxy -port=80 --timeout 300
         echo "Staring cypress tests..."
         exec npm test
+        exec curl -d "text=Inny test CI z ci-test" -d "channel=C05EKHETMT9" -H "Authorization: Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ" -X POST https://slack.com/api/chat.postMessage
         ;;
     *)
         exec "$@"
