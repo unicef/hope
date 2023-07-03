a610e7ccb67a2cd5931b6264545ed23ac649ddde
Szymon <szymon.wyderka@kellton.com>
2023-07-03 19:38:29 +0200

    Slack_Report test: ci-test

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,35 +1,19 @@
 const { defineConfig } = require("cypress");
 const { verifyDownloadTasks } = require("cy-verify-downloads");
+const {execSync} = require("child_process");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on("task", verifyDownloadTasks);
       on("after:run", (results) => {
-        //curl -d "text=Hi I am a bot that can post messages to any public channel." -d "channel=C05EKHETMT9" -H "Authorization: Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ" -X POST https://slack.com/api/chat.postMessage
         console.log("Gagagaggaga");
-        // request("POST", "https://slack.com/api/chat.postMessage", {
-        //   auth: "Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ",
-        //   body: {
-        //     text: "Hi I am a bot that can post messages to any public channel.",
-        //     channel: "C05EKHETMT9",
-        //   },
-        // }).then((res) => {
-        //   console.log("Gugugugugu");
-        //   console.log(res);
-        fetch("https://slack.com/api/chat.postMessage", {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-            Authorization:
-              "Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ",
-          },
-          data: JSON.stringify({
-            text: "Hi I am a bot that can post messages to any public channel.",
-            channel: "C05EKHETMT9",
-          }),
-        }).then((r) => {
-          console.log(r.status);
-        });
+        const { execSync } = require("child_process");
+        const exec = (command) => {
+          execSync(command, {
+            stdio: "inherit",
+          });
+        };
+        exec('curl -d "text=Hi I am a bot that can post messages to any public channel." -d "channel=C05EKHETMT9" -H "Authorization: Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ" -X POST https://slack.com/api/chat.postMessage');
       });
     },
     projectId: "cypress",
---
+++ cypress/cypress/scripts/slackReport.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const { execSync } = require("child_process");
+
+const exec = (command) => {
+    execSync(command, {
+        stdio: "inherit",
+    });
+};
+
+const fs = require("fs");
+fs.readFile("/Users/szymonwyderka/Projects/hct-mis/cypress/cypress/reports/mochareports/report.json", "utf8", (err, jsonString) => {
+    if (err) {
+        console.log("File read failed:", err);
+        return;
+    }
+    try {
+        const report = JSON.parse(jsonString);
+        const command = `curl -d "text=
+        Tests Passed: ${report.stats.passes} \n
+        Tests Failed: ${report.stats.failures}\n
+        Tests ToDo: ${report.stats.pending}\n" -d "channel=C05EKHETMT9" -H "Authorization: Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ" -X POST https://slack.com/api/chat.postMessage`;
+        exec(command);
+    } catch (err) {
+        console.log("Error parsing JSON string:", err);
+    }});
