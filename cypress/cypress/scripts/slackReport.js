043f691fed6a679b2b0d2e1e0ece0501402db28b
Szymon <szymon.wyderka@kellton.com>
2023-07-06 10:51:30 +0200

    SlackBot Tests

--- cypress/cypress/scripts/slackReport.js
+++ cypress/cypress/scripts/slackReport.js
@@ -7,10 +7,40 @@ const exec = (command) => {
   });
 };
 
-exec(
-  'curl -d "text=E2E Tests Report: " -d "channel=C05EKHETMT9" -H "Authorization: Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ" -X POST https://slack.com/api/chat.postMessage'
-);
+const request = require("request");
+const SLACK_BOT_USER_TOKEN =
+"xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ";
+
+function sendMessage(data) {
+  request(
+    {
+      url: "https://slack.com/api/chat.postMessage",
+      method: "POST",
+      json: data,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        Authorization: `Bearer ${SLACK_BOT_USER_TOKEN}`,
+      },
+    },
+    function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+        console.error("Error sending slack response:", error);
+      } else if (!response.body.ok) {
+        console.error("Slack responded with error:", response.body);
+      } else {
+        // All good!
+      }
+    }
+  );
+}
+
+sendMessage({
+  text: "E2E Tests Report: ",
+  channel: "C05EKHETMT9",
+});
+
 const fs = require("fs");
+const { log } = require("console");
 fs.readFile(
   "./cypress/reports/mochareports/report.json",
   "utf8",
@@ -21,90 +51,68 @@ fs.readFile(
     }
     try {
       const report = JSON.parse(jsonString);
-      const command = `curl -d "text=Tests Passed: ${report.stats.passes} 
-        \nTests Failed: ${report.stats.failures}
-        \nTests ToDo: ${report.stats.pending}\n" -d "channel=C05EKHETMT9" -H "Authorization: Bearer xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ" -X POST https://slack.com/api/chat.postMessage`;
-      exec(command);
-      exec("pwd")
-      exec("ls")
-      exec("zip -r report.zip ./cypress/reports/mochareports");
+      const text=`Tests Passed: ${report.stats.passes}
+                \nTests Failed: ${report.stats.failures}
+                \nTests ToDo: ${report.stats.pending}\n`
+        sendMessage({
+          text: text,
+          channel: "C05EKHETMT9",
+        });
 
       let coverage =
         ((report.stats.tests - report.stats.pending) / report.stats.tests) *
         100;
-      const chart = {
-        type: "doughnut",
-        data: {
-          labels: ["Passed", "Failed", "ToDo"],
-          labelColors: "#000",
-          datasets: [
-            {
-              data: [
-                report.stats.passes,
-                report.stats.failures,
-                report.stats.pending,
-              ],
-              borderColor: "rgba(255,255,255,0.8)",
-              backgroundColor: [
-                "rgb(72,159,52)",
-                "rgb(215,64,95)",
-                "rgb(36,113,164)",
-              ],
-              hoverOffset: 40,
-            },
-          ],
+
+const QuickChart = require('quickchart-js');
+const chart = new QuickChart();
+
+chart.setWidth(500)
+chart.setHeight(300);
+chart.setVersion('2.9.4');
+
+chart.setConfig({
+  type: 'doughnut',
+  data: {
+    datasets: [
+      {
+        data: [
+          report.stats.passes,
+          report.stats.failures,
+          report.stats.pending,
+        ],
+        backgroundColor: [
+          'rgb(75, 192, 192)',
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+        ],
+      },
+    ],
+    labels: ['Pass', 'Failed', 'ToDo'],
+  },
+  options: {
+    plugins: {
+      datalabels: {
+        color: "#000",
+        formatter: (value) => {
+          if (value < 1) return '';
+          return value;
         },
-        options: {
-          plugins: {
-            datalabels: {
-              color: ["#000", "#000", "#000"],
-              anchor: "end",
-              align: "end",
-            },
-            doughnutlabel: {
-              labels: [
-                {
-                  text: coverage.toFixed(0).toString() + "%",
-                  color: "#000",
-                  font: { size: 30 },
-                },
-                { text: "Coverage", color: "#000" },
-              ],
-            },
-          },
-        },
-      };
-      const encodedChart = encodeURIComponent(JSON.stringify(chart));
-      const chartUrl = `https://quickchart.io/chart?format=jpg&bkg=white&c=${encodedChart}`;
-      console.log("\n\n" + chartUrl + "\n\n");
-
-      const request = require("request");
-
-      const SLACK_BOT_USER_TOKEN =
-        "xoxb-5509997426931-5523162721089-IlVaqxdRKRyKftvRAZojd7yZ";
-
-      function sendMessage(data) {
-        request(
+      },
+      doughnutlabel: {
+        labels: [
           {
-            url: "https://slack.com/api/chat.postMessage",
-            method: "POST",
-            json: data,
-            headers: {
-              "Content-Type": "application/json; charset=utf-8",
-              Authorization: `Bearer ${SLACK_BOT_USER_TOKEN}`,
-            },
+            text: coverage.toFixed(0).toString() + "%",
+            color: "#000",
+            font: { size: 30 },
           },
-          function (error, response, body) {
-            if (error || response.statusCode !== 200) {
-              console.error("Error sending slack response:", error);
-            } else if (!response.body.ok) {
-              console.error("Slack responded with error:", response.body);
-            } else {
-              // All good!
-            }
-          }
-        );
-      }
+          { text: "Coverage", color: "#000" },
+        ],
+      },
+    },
+  },
+});
+const chartUrl = chart.getUrl()
+console.log("\n\n" + chartUrl + "\n\n");
 
       sendMessage({
         text: "Chart data update",
