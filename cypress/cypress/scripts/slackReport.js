063f84b5e6bf0927c8add9d1bbbd296b6b2a4d87
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-07-11 19:10:28 +0200

    Added branch name to Slack report

--- cypress/cypress/scripts/slackReport.js
+++ cypress/cypress/scripts/slackReport.js
@@ -150,22 +150,23 @@ fs.readFile(
     }
     let email2 = exec(`echo $MAILDWA`)
     let branch = exec(`echo $BRANCH_NAMEDWA`)
-    let prbra = exec(`echo $PRBRANCHDWA`)
+    let branchName = exec(`echo $PRBRANCHDWA`)
     let bver = exec(`echo $BUILDVERSIONDWA`)
     let rep = exec(`echo $REPONAMEDWA`)
     console.log(`\n MAILDWA = ${email2}`)
-    console.log(`\n BRANCH_NAME = ${branch} \n`)
-    console.log(`\n PRBRANCH = ${prbra}`)
-    console.log(`\n BUILDVERSION = ${bver} \n`)
-    console.log(`\n REPONAME = ${rep} \n`)
+    console.log(`\n BRANCH_NAME = ${branch}`)
+    console.log(`\n PRBRANCH = ${branchName}`)
+    console.log(`\n BUILDVERSION = ${bver}`)
+    console.log(`\n REPONAME = ${rep}`)
 
     // let slackUserData = exec(`curl  -d "channel=C05EKHETMT9" -d "email=${email}" -H "Authorization: Bearer ${SLACK_BOT_USER_TOKEN}" -X POST https://slack.com/api/users.lookupByEmail`);
     // parseJJ = JSON.parse(slackUserData)
     // let branchName = exec(`echo $BRANCH_NAME`)
-    // sendMessage({
-    //   text: `Hello <@${parseJJ.user.id.toString()}> \nIt is report from your branch: ${branchName}`,
-    //   channel: CHANNEL,
-    // });
+    sendMessage({
+      // text: `Hello <@${parseJJ.user.id.toString()}> \nIt is report from your branch: ${branchName}`,
+      text: `It is report from your branch: ${branchName}`,
+      channel: CHANNEL,
+    });
     sendFile('report.zip')
     // exec(`git show HEAD | grep Author`).then((info) => {email = info.split('<')[1].split('>')[0];})
     // let slackUserData = exec(`curl  -d "channel=C05EKHETMT9" -d "email=${email}" -H "Authorization: Bearer ${SLACK_BOT_USER_TOKEN}" -X POST https://slack.com/api/users.lookupByEmail`);
