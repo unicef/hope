05fe48c097a30538ef9eb836d1ab6c42d5d34029
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-07-20 17:07:52 +0200

    Fix test

--- backend/hct_mis_api/apps/household/tests/snapshots/snap_test_individual_query.py
+++ backend/hct_mis_api/apps/household/tests/snapshots/snap_test_individual_query.py
@@ -7,92 +7,6 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestIndividualQuery::test_individual_programme_filter_0_with_permission 1'] = {
-    'data': {
-        'allIndividuals': {
-            'edges': [
-                {
-                    'node': {
-                        'birthDate': '1943-07-30',
-                        'familyName': 'Butler',
-                        'givenName': 'Benjamin',
-                        'household': {
-                            'programs': {
-                                'edges': [
-                                    {
-                                        'node': {
-                                            'name': 'Test program TWO'
-                                        }
-                                    }
-                                ]
-                            }
-                        },
-                        'phoneNo': '(953)682-4596'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1969-11-29',
-                        'familyName': 'Franklin',
-                        'givenName': 'Jenna',
-                        'household': {
-                            'programs': {
-                                'edges': [
-                                    {
-                                        'node': {
-                                            'name': 'Test program TWO'
-                                        }
-                                    }
-                                ]
-                            }
-                        },
-                        'phoneNo': '001-296-358-5428-607'
-                    }
-                },
-                {
-                    'node': {
-                        'birthDate': '1983-12-21',
-                        'familyName': 'Perry',
-                        'givenName': 'Timothy',
-                        'household': {
-                            'programs': {
-                                'edges': [
-                                    {
-                                        'node': {
-                                            'name': 'Test program TWO'
-                                        }
-                                    }
-                                ]
-                            }
-                        },
-                        'phoneNo': '(548)313-1700-902'
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestIndividualQuery::test_individual_programme_filter_1_without_permission 1'] = {
-    'data': {
-        'allIndividuals': None
-    },
-    'errors': [
-        {
-            'locations': [
-                {
-                    'column': 7,
-                    'line': 3
-                }
-            ],
-            'message': 'Permission Denied',
-            'path': [
-                'allIndividuals'
-            ]
-        }
-    ]
-}
-
 snapshots['TestIndividualQuery::test_individual_query_all_0_with_permission 1'] = {
     'data': {
         'allIndividuals': {
@@ -173,29 +87,33 @@ snapshots['TestIndividualQuery::test_individual_query_all_1_without_permission 1
 }
 
 snapshots['TestIndividualQuery::test_individual_query_single_0_with_permission 1'] = {
-    'errors': [
-        {
-            'locations': [
-                {
-                    'column': 27,
-                    'line': 3
-                }
-            ],
-            'message': 'Unknown argument "orderBy" on field "individual" of type "Query".'
+    'data': {
+        'individual': {
+            'birthDate': '1943-07-30',
+            'familyName': 'Butler',
+            'fullName': 'Benjamin Butler',
+            'givenName': 'Benjamin',
+            'phoneNo': '(953)682-4596'
         }
-    ]
+    }
 }
 
 snapshots['TestIndividualQuery::test_individual_query_single_1_without_permission 1'] = {
+    'data': {
+        'individual': None
+    },
     'errors': [
         {
             'locations': [
                 {
-                    'column': 27,
+                    'column': 7,
                     'line': 3
                 }
             ],
-            'message': 'Unknown argument "orderBy" on field "individual" of type "Query".'
+            'message': 'Permission Denied',
+            'path': [
+                'individual'
+            ]
         }
     ]
 }
