ef0666508d5cd4b9c56844a17aadc3c003d5eba4
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-07-26 15:45:43 +0200

    linter

--- backend/hct_mis_api/apps/household/tests/test_individual_query.py
+++ backend/hct_mis_api/apps/household/tests/test_individual_query.py
@@ -8,7 +8,6 @@ from hct_mis_api.apps.core.base_test_case import APITestCase
 from hct_mis_api.apps.core.fixtures import create_afghanistan
 from hct_mis_api.apps.household.fixtures import HouseholdFactory, IndividualFactory
 from hct_mis_api.apps.program.fixtures import ProgramFactory
-from hct_mis_api.conftest import disabled_locally_test
 
 
 class TestIndividualQuery(APITestCase):
