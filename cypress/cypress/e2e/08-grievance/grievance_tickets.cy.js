c1f306fdb7e334fc9e3ba6417a05a6f57aa17b95
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-07-31 14:34:27 +0200

    Updated cypress; Deleted session mechanism for logging; Turned on new memory management

--- cypress/cypress/e2e/08-grievance/grievance_tickets.cy.js
+++ cypress/cypress/e2e/08-grievance/grievance_tickets.cy.js
@@ -71,7 +71,7 @@ describe("Grievance", () => {
     context("Export", () => {
       it.skip("Export", () => {});
     });
-    context.skip("Grievance Filters", () => {
+    context("Grievance Filters", () => {
       [
         ["USER-GENERATED", "GRV-0000001", 1, "Ticket ID: GRV-0000001"],
         ["SYSTEM-GENERATED", "GRV-0000003", 1, "Ticket ID: GRV-0000003"],
