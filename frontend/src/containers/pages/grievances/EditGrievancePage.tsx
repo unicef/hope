33f0e23caa7fc5fc7ab9640a203a5d57e26e957e
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-08-01 09:22:43 +0200

    enable urgency priority change

--- frontend/src/containers/pages/grievances/EditGrievancePage.tsx
+++ frontend/src/containers/pages/grievances/EditGrievancePage.tsx
@@ -499,7 +499,6 @@ export const EditGrievancePage = (): React.ReactElement => {
                             label={t('Priority')}
                             choices={choicesData.grievanceTicketPriorityChoices}
                             component={FormikSelectField}
-                            disabled={ticket.priority !== 0}
                           />
                         </Grid>
                         <Grid item xs={3}>
@@ -511,7 +510,6 @@ export const EditGrievancePage = (): React.ReactElement => {
                             label={t('Urgency')}
                             choices={choicesData.grievanceTicketUrgencyChoices}
                             component={FormikSelectField}
-                            disabled={ticket.urgency !== 0}
                           />
                         </Grid>
                         {ticket.issueType?.toString() !==
