db5cc51e879571e0d24d0cbafbd6a0294babcc30
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-08-21 13:39:13 +0200

    Added Locators
    Added Page Objects for GPF

---
+++ cypress/cypress/e2e/09-global-program-filter/global_program_filter.cy.js
@@ -0,0 +1,70 @@
+import AllProgrammes from "../../page-objects/pages/all_programmes/all_programmes.po";
+
+let programmesPage = new AllProgrammes();
+
+describe("Country Dashboard", () => {
+  beforeEach(() => {
+    programmesPage.navigateToProgrammePage();
+  });
+
+  describe("Smoke tests Global Program Filter", () => {
+    it("Check All Programmes - Programme Management page", () => {
+      cy.scenario([
+        "1. Go to main page (All programmes set)",
+        "2. Check if all elements on page exist",
+      ]);
+      programmesPage.getMenuButtonProgrammeManagement().should("be.visible");
+      programmesPage.getMenuButtonFeedback().should("not.exist");
+      // programmesPage.getMenuButtonPaymentVerification().should("not.exist");
+      programmesPage.getMenuButtonGrievanceTickets().should("not.exist");
+      programmesPage.getButtonNewProgram().should("be.visible");
+      programmesPage.getFiltersSearch().should("be.visible");
+      programmesPage.getFiltersStartDate().should("be.visible");
+      programmesPage.getFiltersEndDate().should("be.visible");
+      programmesPage.getFiltersSector().should("be.visible");
+      programmesPage.getFiltersNumberOfHouseholdsMin().should("be.visible");
+      programmesPage.getFiltersNumberOfHouseholdsMax().should("be.visible");
+      programmesPage.getFiltersBudgetMin().should("be.visible");
+      programmesPage.getFiltersBudgetMax().should("be.visible");
+      programmesPage
+        .getPageHeaderTitle()
+        .contains(programmesPage.textPageHeaderTitle);
+      programmesPage
+        .getGlobalProgramFilter()
+        .contains(programmesPage.textAllProgrammes);
+      programmesPage.getButtonApply().should("be.visible");
+      programmesPage.getButtonClear().should("be.visible");
+      programmesPage.getTabTitle().contains(programmesPage.textProgrammes);
+      programmesPage.getName().should("be.visible");
+      programmesPage.getStatus().should("be.visible");
+      programmesPage.getTimeframe().should("be.visible");
+      programmesPage.getSelector().should("be.visible");
+      programmesPage.getNumOfouseholds().should("be.visible");
+      programmesPage.getBudget().should("be.visible");
+      programmesPage.getProgrammesRows().should("have.length", 2);
+    });
+    it.skip("Choose program", () => {});
+    it.skip("Come back to All Programmes", () => {});
+  });
+
+  describe("Component tests All Programmes - Global Program Filter", () => {
+    context("Filters", () => {
+      it.skip("Search filter", () => {});
+      it.skip("Status filter", () => {});
+      it.skip("Start Date filter", () => {});
+      it.skip("End Date filter", () => {});
+      it.skip("Sector filter", () => {});
+      it.skip("Num. of Households filter From", () => {});
+      it.skip("Num. of Households filter To", () => {});
+      it.skip("Budget From filter", () => {});
+      it.skip("Budget To filter", () => {});
+    });
+    context("Add New Programme", () => {
+      it.skip("Add New Programme", () => {});
+      it.skip("Cancel New Programme", () => {});
+    });
+  });
+  describe.skip("E2E tests Country Dashboard", () => {});
+
+  describe.skip("Regression tests Country Dashboard", () => {});
+});
--- cypress/cypress/page-objects/base.component.js
+++ cypress/cypress/page-objects/base.component.js
@@ -1,14 +1,34 @@
 export default class BaseComponent {
   // Menu Locators
+  navRegistrationDataImport = 'a[data-cy="nav-Registration Data Import"]';
+  navProgrammePopulation = 'div[data-cy="nav-Programme Population"]';
+  navHouseholds = 'a[data-cy="nav-Households"]';
+  navIndividuals = 'a[data-cy="nav-Individuals"]';
+  navProgrammeManagement = 'a[data-cy="nav-Programme Management"]';
+  navProgrammeDetails = 'a[data-cy="nav-Programme Details"]';
+  navTargeting = 'a[data-cy="nav-Targeting"]';
+  navCashAssist = 'a[data-cy="nav-Cash Assist"]';
+  navPaymentModule = 'a[data-cy="nav-Payment Module"]';
+  navPaymentVerification = 'a[data-cy="nav-Payment Verification"]';
+  navGrievance = 'div[data-cy="nav-Grievance"]';
+  navGrievanceTickets = 'a[data-cy="nav-Grievance Tickets"]';
+  navGrievanceDashboard = 'a[data-cy="nav-Grievance Dashboard"]';
+  navFeedback = 'a[data-cy="nav-Feedback"]';
+  navReporting = 'a[data-cy="nav-Reporting"]';
+  navProgrammeUsers = 'a[data-cy="nav-Programme Users"]';
+  navActivityLog = 'a[data-cy="nav-Activity Log"]';
+  navResourcesKnowledgeBase = 'a[data-cy="nav-resources-Knowledge Base"]';
+  navResourcesConversations = 'a[data-cy="nav-resources-Conversations"]';
+  navResourcesToolsAndMaterials =
+    'a[data-cy="nav-resources-Tools and Materials"]';
+  navResourcesReleaseNote = 'a[data-cy="nav-resources-Release Note"]';
   buttonPaymentVerification =
     'span[class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock"]';
   buttonTargeting =
     'span[class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock"]';
-  buttonGrievance = 'div[data-cy="nav-Grievance"]';
-  buttonGrievanceTickets = 'a[data-cy="nav-Grievance Tickets"]';
-  buttonGrievanceDashboard = 'a[data-cy="nav-Grievance Dashboard"]';
-  buttonFeedback = 'a[data-cy="nav-Feedback"]';
   headerTitle = 'h5[data-cy="page-header-title"]';
+  buttonProgrammeManagement = 'a[data-cy="nav-Programme Management"]';
+  globalProgramFilter = 'div[data-cy="global-program-filter"]';
 
   // Texts
   buttonPaymentVerificationText = "Payment Verification";
@@ -22,10 +42,13 @@ export default class BaseComponent {
   getMenuButtonPaymentVerification = () =>
     cy.get(this.buttonPaymentVerification);
   getMenuButtonTargeting = () => cy.get(this.buttonTargeting);
-  getMenuButtonGrievance = () => cy.get(this.buttonGrievance);
-  getMenuButtonGrievanceTickets = () => cy.get(this.buttonGrievanceTickets);
-  getMenuButtonGrievanceDashboard = () => cy.get(this.buttonGrievanceDashboard);
-  getMenuButtonFeedback = () => cy.get(this.buttonFeedback);
+  getMenuButtonGrievance = () => cy.get(this.navGrievance);
+  getMenuButtonGrievanceTickets = () => cy.get(this.navGrievanceTickets);
+  getMenuButtonGrievanceDashboard = () => cy.get(this.navGrievanceDashboard);
+  getMenuButtonProgrammeManagement = () =>
+    cy.get(this.buttonProgrammeManagement);
+  getGlobalProgramFilter = () => cy.get(this.globalProgramFilter);
+  getMenuButtonFeedback = () => cy.get(this.navFeedback);
   getHeaderTitle = () => cy.get(this.headerTitle);
 
   checkGrievanceMenu() {
