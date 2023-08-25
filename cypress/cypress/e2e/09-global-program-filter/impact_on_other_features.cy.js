71ff64200d81ace5bd2298ce5c6b0f06203f38d4
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-08-25 14:11:24 +0200

    Added test GPF - Registration data import Verification

--- cypress/cypress/e2e/09-global-program-filter/impact_on_other_features.cy.js
+++ cypress/cypress/e2e/09-global-program-filter/impact_on_other_features.cy.js
@@ -1,6 +1,8 @@
 import AllProgrammes from "../../page-objects/pages/all_programmes/all_programmes.po";
+import Grievance from "../../page-objects/pages/grievance/grievance_tickets.po";
 
 let programmesPage = new AllProgrammes();
+let grievancePage = new Grievance();
 
 describe("Global Program Filter - Impacts", () => {
   beforeEach(() => {
@@ -11,7 +13,16 @@ describe("Global Program Filter - Impacts", () => {
       programmesPage.getGlobalProgramFilter().click();
       programmesPage
         .getProgrammesOptions()
-        .contains(programmesPage.textTestProgramm);
+        .contains(programmesPage.textTestProgramm)
+        .click();
+      grievancePage.clickMenuButtonGrievance();
+      grievancePage.clickMenuButtonGrievanceTickets();
+      grievancePage.getTicketListRow().should("have.length", 4);
+      programmesPage.getGlobalProgramFilter().click();
+      programmesPage.getProgrammesOptions().contains("Draft Program").click();
+      grievancePage.clickMenuButtonGrievance();
+      grievancePage.clickMenuButtonGrievanceTickets();
+      grievancePage.getTicketListRow().should("have.length", 0);
     });
     it.skip("GPF - Program management", () => {});
     it.skip("GPF - Targeting", () => {});
