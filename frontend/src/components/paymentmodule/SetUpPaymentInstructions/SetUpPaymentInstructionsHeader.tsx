9d7e2cb709259132c0e1c61708d3cbfef3234899
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-08-29 22:48:47 +0200

    drag and drop info component

--- frontend/src/components/paymentmodule/SetUpPaymentInstructions/SetUpPaymentInstructionsHeader.tsx
+++ frontend/src/components/paymentmodule/SetUpPaymentInstructions/SetUpPaymentInstructionsHeader.tsx
@@ -1,8 +1,10 @@
-import React, { ReactElement } from 'react';
+import { Box, Button, Checkbox, FormControlLabel } from '@material-ui/core';
+import React, { ReactElement, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useParams, useLocation } from 'react-router-dom';
-import { hasPermissions, PERMISSIONS } from '../../../config/permissions';
+import { useLocation, useParams } from 'react-router-dom';
+import { PERMISSIONS, hasPermissions } from '../../../config/permissions';
 import { BreadCrumbsItem } from '../../core/BreadCrumbs';
+import { GreyText } from '../../core/GreyText';
 import { PageHeader } from '../../core/PageHeader';
 
 interface SetUpPaymentInstructionsHeaderProps {
@@ -19,6 +21,10 @@ export const SetUpPaymentInstructionsHeader = ({
   const location = useLocation();
   const { t } = useTranslation();
   const { id } = useParams();
+  const [isChecked, setChecked] = useState(false);
+  const [isClosed, setClosed] = useState(
+    localStorage.getItem('showDragAndDropInfo') === 'false',
+  );
   const isFollowUp = location.pathname.indexOf('followup') !== -1;
 
   const breadCrumbsItems: BreadCrumbsItem[] = [
@@ -30,6 +36,41 @@ export const SetUpPaymentInstructionsHeader = ({
     },
   ];
 
+  const handleDoNotShowAgain = (): void => {
+    localStorage.setItem('showDragAndDropInfo', 'false');
+  };
+
+  const dragAndDropShowInfoComponent = !isClosed ? (
+    <Box
+      display='flex'
+      alignItems='center'
+      justifyContent='space-between'
+      pl={8}
+      pr={8}
+      pb={2}
+    >
+      <GreyText>{t('Drag and drop Payment Instructions to reorder')}</GreyText>
+      <Box>
+        <FormControlLabel
+          label={<GreyText>{t("Don't show again")}</GreyText>}
+          control={
+            <Checkbox
+              color='primary'
+              data-cy='checkbox-dont-show-again'
+              onChange={(): void => {
+                handleDoNotShowAgain();
+                setChecked(!isChecked);
+              }}
+              checked={isChecked}
+              inputProps={{ 'aria-labelledby': 'selected' }}
+            />
+          }
+        />
+        <Button onClick={() => setClosed(true)}>{t('Close')}</Button>
+      </Box>
+    </Box>
+  ) : null;
+
   return (
     <PageHeader
       title={t('Set up Payment Instruction')}
@@ -38,6 +79,7 @@ export const SetUpPaymentInstructionsHeader = ({
           ? breadCrumbsItems
           : null
       }
+      tabs={dragAndDropShowInfoComponent}
     >
       {buttons}
     </PageHeader>
