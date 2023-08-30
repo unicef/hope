42b267390562ff90e089d6927ca02dce171f7e24
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-08-30 10:19:38 +0200

    payment instruction section

--- frontend/src/components/paymentmodule/SetUpPaymentInstructions/PaymentInstructionsSection.tsx
+++ frontend/src/components/paymentmodule/SetUpPaymentInstructions/PaymentInstructionsSection.tsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import { PaymentPlanQuery } from '../../../__generated__/graphql';
 import { useBaseUrl } from '../../../hooks/useBaseUrl';
 import { BaseSection } from '../../core/BaseSection';
-import { Missing } from '../../core/Missing';
 
 interface PaymentInstructionsSectionProps {
   paymentPlan: PaymentPlanQuery['paymentPlan'];
@@ -21,17 +20,11 @@ export const PaymentInstructionsSection = ({
     <Button
       component={Link}
       to={`/${baseUrl}/payment-module/payment-plans/${paymentPlan.id}/setup-payment-instructions/create`}
-      variant='outlined'
+      variant='contained'
       color='primary'
     >
       {t('Set up Payment Instructions')}
     </Button>
   );
-  return (
-    <BaseSection title='Set up Payment Instructions' buttons={buttons}>
-      <>
-        <Missing />
-      </>
-    </BaseSection>
-  );
+  return <BaseSection title='Payment Instructions' buttons={buttons} />;
 };
