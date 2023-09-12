d02829b6d71b3892bc1ed6a6685aa0c912927f4b
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2023-09-12 16:17:48 +0200

    Fix tests

--- backend/hct_mis_api/apps/grievance/tests/test_close_data_change_tickets.py
+++ backend/hct_mis_api/apps/grievance/tests/test_close_data_change_tickets.py
@@ -8,7 +8,7 @@ from parameterized import parameterized
 
 from hct_mis_api.apps.account.fixtures import UserFactory
 from hct_mis_api.apps.account.permissions import Permissions
-from hct_mis_api.apps.core.base_test_case import APITestCase
+from hct_mis_api.apps.core.base_test_case import APITestCase, BaseElasticSearchTestCase
 from hct_mis_api.apps.core.fixtures import create_afghanistan
 from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.core.utils import IDENTIFICATION_TYPE_TO_KEY_MAPPING
@@ -44,7 +44,9 @@ from hct_mis_api.apps.household.models import (
 from hct_mis_api.apps.program.fixtures import ProgramFactory
 
 
-class TestCloseDataChangeTickets(APITestCase):
+class TestCloseDataChangeTickets(BaseElasticSearchTestCase, APITestCase):
+    databases = {"default", "registration_datahub"}
+
     STATUS_CHANGE_MUTATION = """
     mutation GrievanceStatusChange($grievanceTicketId: ID!, $status: Int) {
       grievanceStatusChange(grievanceTicketId: $grievanceTicketId, status: $status) {
@@ -290,6 +292,8 @@ class TestCloseDataChangeTickets(APITestCase):
             },
             approve_status=True,
         )
+
+        super().setUpTestData()
 
     @parameterized.expand(
         [
