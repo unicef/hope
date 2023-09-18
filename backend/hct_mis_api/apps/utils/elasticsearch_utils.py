6a42151b9f935f7c48aaf54f473bc0b162e7636f
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-09-18 13:21:30 +0200

    Add bulk_upsert

--- backend/hct_mis_api/apps/utils/elasticsearch_utils.py
+++ backend/hct_mis_api/apps/utils/elasticsearch_utils.py
@@ -3,10 +3,14 @@ import logging
 from time import sleep
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Type
 
+from django.conf import settings
 from django.db.models import Model
 
 from django_elasticsearch_dsl.registries import registry
 from elasticsearch_dsl import Search, connections
+from elasticsearch import Elasticsearch, helpers
+
+from hct_mis_api.apps.household.models import Household, Individual
 
 logger = logging.getLogger(__name__)
 
@@ -96,3 +100,45 @@ def wait_until_es_healthy() -> None:
             f"Max Check ES attempts reached - status: {health.get('status')} timeout: {health.get('timed_out')} "
             f"number of pending tasks:{health.get('number_of_pending_tasks')}"
         )
+
+
+def bulk_upsert(ids: List[str], model_name: str, business_area_slug: str) -> None:
+    from hct_mis_api.apps.household.documents import (
+        HouseholdDocument,
+        IndividualDocumentAfghanistan,
+        IndividualDocumentUkraine,
+        IndividualDocumentOthers
+    )
+
+    es = Elasticsearch(settings.ELASTICSEARCH_HOST)
+
+    document = HouseholdDocument
+    if model_name == "Household":
+        model = Household
+    else:
+        if business_area_slug == "afghanistan":
+            document = IndividualDocumentAfghanistan
+        elif business_area_slug == "ukraine":
+            document = IndividualDocumentUkraine
+        else:
+            document = IndividualDocumentOthers
+        model = Individual
+
+    documents_to_update = []
+    for obj in model.objects.filter(id__in=ids):
+        data = document().prepare(obj)
+        doc = {
+            "_op_type": 'update',
+            "_index": document.Index.name,
+            "_id": str(obj.id),
+            "doc": data,
+            "doc_as_upsert": True
+        }
+        documents_to_update.append(doc)
+
+    try:
+        helpers.bulk(es, documents_to_update)
+    except Exception:
+        logger.error(f"Updating {','.join([str(_id) for _id in ids])} of {document.__name__} failed")
+
+    logger.info(f"{document.__name__} with {','.join([str(_id) for _id in ids])} have been updated.")
