5b3d2f44ec6114e1e9c0ba2af4d9298e5bea71c6
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-09-19 17:10:58 +0200

    add filter total_delivered_quantity_usd

--- backend/hct_mis_api/apps/program/models.py
+++ backend/hct_mis_api/apps/program/models.py
@@ -234,7 +234,7 @@ class ProgramCycle(SoftDeletableModel, TimeStampedUUIDModel, AbstractSyncable, C
         return f"{self.program.name} - cycle start date {self.start_date}"
 
     @property
-    def total_entitled_quantity(self) -> Decimal:
+    def total_entitled_quantity_usd(self) -> Decimal:
         result = Decimal(0.0)
         for payment_plan in self.paymentplan_set.all():
             if payment_plan.total_entitled_quantity_usd:
@@ -242,7 +242,7 @@ class ProgramCycle(SoftDeletableModel, TimeStampedUUIDModel, AbstractSyncable, C
         return result
 
     @property
-    def total_undelivered_quantity(self) -> Decimal:
+    def total_undelivered_quantity_usd(self) -> Decimal:
         result = Decimal(0.0)
         for payment_plan in self.paymentplan_set.all():
             if payment_plan.total_undelivered_quantity_usd:
@@ -250,7 +250,7 @@ class ProgramCycle(SoftDeletableModel, TimeStampedUUIDModel, AbstractSyncable, C
         return result
 
     @property
-    def total_delivered_quantity(self) -> Decimal:
+    def total_delivered_quantity_usd(self) -> Decimal:
         result = Decimal(0.0)
         for payment_plan in self.paymentplan_set.all():
             if payment_plan.total_delivered_quantity_usd:
