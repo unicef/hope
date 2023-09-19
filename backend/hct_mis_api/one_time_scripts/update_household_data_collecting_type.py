fee17eeb0a33bc70ca9a27bb6b0d93a85e86fe55
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-09-19 09:40:37 +0200

    add arg business_area_slug

--- backend/hct_mis_api/one_time_scripts/update_household_data_collecting_type.py
+++ backend/hct_mis_api/one_time_scripts/update_household_data_collecting_type.py
@@ -1,16 +1,19 @@
+from typing import Optional
+
+from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.household.models import Household
 from hct_mis_api.apps.program.models import Program
 from hct_mis_api.apps.targeting.models import TargetPopulation
-from hct_mis_api.apps.core.models import BusinessArea
 
 
-
-def update_household_data_collecting_type():
-    for business_area in BusinessArea.objects.all():
+def update_household_data_collecting_type(business_area_slug: Optional[str] = None) -> None:
+    ba_qs = BusinessArea.objects.filter(slug=business_area_slug) if business_area_slug else BusinessArea.objects.all()
+    for business_area in ba_qs:
         print(f"Updating Households data_collecting_type for BA: {business_area.name}")
 
         for program in Program.objects.filter(business_area=business_area, data_collecting_type__isnull=False):
             data_collecting_type = program.data_collecting_type
             for target_population in TargetPopulation.objects.filter(program=program):
-                Household.objects.filter(target_populations=target_population, data_collecting_type__isnull=True).update(data_collecting_type=data_collecting_type)
-
+                Household.objects.filter(
+                    target_populations=target_population, data_collecting_type__isnull=True
+                ).update(data_collecting_type=data_collecting_type)
