df912e6cc52ba3005264bf2c6ceadeb2891737ca
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2023-09-21 19:37:32 +0200

    Format fix

--- backend/hct_mis_api/apps/household/admin/household.py
+++ backend/hct_mis_api/apps/household/admin/household.py
@@ -130,7 +130,7 @@ class HouseholdAdmin(
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
         qs = self.model.all_objects.get_queryset().select_related(
-            "head_of_household", "country", "country_origin", "admin_area", "admin1", "admin2", "admin3", "admin4" 
+            "head_of_household", "country", "country_origin", "admin_area", "admin1", "admin2", "admin3", "admin4"
         )
         ordering = self.get_ordering(request)
         if ordering:
