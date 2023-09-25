fd44f6558f8e816a6ce7342d93ffdd3ad1d162b2
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-09-25 20:08:14 +0200

    Dump fixtures

--- backend/hct_mis_api/apps/core/management/commands/initcypress.py
+++ backend/hct_mis_api/apps/core/management/commands/initcypress.py
@@ -4,7 +4,6 @@ from typing import Any
 from django.core.management import BaseCommand, call_command
 
 from hct_mis_api.apps.account.models import Role, User, UserRole
-from hct_mis_api.apps.core.fixtures import generate_data_collecting_types
 from hct_mis_api.apps.core.management.commands.reset_business_area_sequences import (
     reset_business_area_sequences,
 )
@@ -26,7 +25,6 @@ class Command(BaseCommand):
             call_command("migratealldb")
 
         reset_business_area_sequences()
-        generate_data_collecting_types()
         call_command("flush", "--noinput")
         call_command("flush", "--noinput", database="cash_assist_datahub_mis")
         call_command("flush", "--noinput", database="cash_assist_datahub_ca")
