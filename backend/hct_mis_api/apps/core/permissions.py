54871745b3424fec3de46ad6876c104bd4df54b7
Patryk Dabrowski <patryk.dabrowski@kellton.com>
2023-09-27 11:44:46 +0200

    Remove logger (#3094)

--- backend/hct_mis_api/apps/core/permissions.py
+++ backend/hct_mis_api/apps/core/permissions.py
@@ -1,9 +1,6 @@
-import logging
 from typing import Any, Callable
 
 from django.core.exceptions import PermissionDenied
-
-logger = logging.getLogger(__name__)
 
 
 def is_authenticated(func: Callable) -> Any:
@@ -14,7 +11,6 @@ def is_authenticated(func: Callable) -> Any:
 
     def wrapper(cls: Any, root: Any, info: Any, *args: Any, **kwargs: Any) -> Any:
         if not info.context.user.is_authenticated:
-            logger.error(f"Permission Denied for {info.context.user}, User is not authenticated.")
             raise PermissionDenied("Permission Denied: User is not authenticated.")
         return func(cls, root, info, *args, **kwargs)
 
