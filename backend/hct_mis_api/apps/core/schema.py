cde780ab3ab0b47c97626dd0946fa438ae704ee0
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-09-27 22:30:49 +0200

    reformat

--- backend/hct_mis_api/apps/core/schema.py
+++ backend/hct_mis_api/apps/core/schema.py
@@ -393,7 +393,8 @@ class Query(graphene.ObjectType):
 
     def resolve_data_collection_type_choices(self, info: Any, **kwargs: Any) -> List[Dict[str, Any]]:
         data_collecting_types = (
-            DataCollectingType.objects.filter(active=True).only("code", "label")
+            DataCollectingType.objects.filter(active=True)
+            .only("code", "label")
             .values("code", "label")
             .order_by("label")
         )
