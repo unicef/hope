d573ee9a1dc01f747a5226b8d5ea6f94b6dae5c9
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-09-27 14:50:58 +0200

    fix ProgramNode, ProgramQuery issue

--- frontend/src/containers/tables/Surveys/LookUpProgrammesTableSurveys/LookUpProgrammesTableSurveys.tsx
+++ frontend/src/containers/tables/Surveys/LookUpProgrammesTableSurveys/LookUpProgrammesTableSurveys.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
-import { TableWrapper } from '../../../../components/core/TableWrapper';
 import {
   AllActiveProgramsQueryVariables,
+  AllProgramsQuery,
   ProgrammeChoiceDataQuery,
-  ProgramNode,
   useAllActiveProgramsQuery,
 } from '../../../../__generated__/graphql';
+import { TableWrapper } from '../../../../components/core/TableWrapper';
 import { UniversalTable } from '../../UniversalTable';
 import { headCells } from './LookUpProgrammesHeadCellsSurveys';
 import { LookUpProgrammesTableRowSurveys } from './LookUpProgrammesTableRowSurveys';
@@ -57,7 +57,10 @@ export const LookUpProgrammesTableSurveys = ({
   return (
     <NoTableStyling>
       <TableWrapper>
-        <UniversalTable<ProgramNode, AllActiveProgramsQueryVariables>
+        <UniversalTable<
+          AllProgramsQuery['allPrograms']['edges'][number]['node'],
+          AllActiveProgramsQueryVariables
+        >
           headCells={headCells}
           query={useAllActiveProgramsQuery}
           queriedObjectName='allActivePrograms'
