67c7d0856193d278d9db07006cdc977939cfe711
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-09-28 09:31:14 +0200

    change description to label

--- frontend/src/components/programs/ProgramDetails/ProgramDetails.tsx
+++ frontend/src/components/programs/ProgramDetails/ProgramDetails.tsx
@@ -93,7 +93,7 @@ export function ProgramDetails({
           <Grid item xs={4}>
             <LabelizedField
               label={t('Data Collecting Type')}
-              value={program.dataCollectingType?.description}
+              value={program.dataCollectingType?.label}
             />
           </Grid>
           <Grid item xs={4}>
