e77ad7c842a4d90b889d0f19ff92eeff6ed2f7e6
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-10-02 16:38:05 +0200

    upd initdemo fixtures

--- backend/hct_mis_api/apps/core/fixtures.py
+++ backend/hct_mis_api/apps/core/fixtures.py
@@ -66,16 +66,3 @@ class StorageFileFactory(DjangoModelFactory):
         model = StorageFile
 
     business_area = factory.LazyAttribute(lambda _: BusinessArea.objects.first())
-
-
-def generate_data_collecting_types() -> None:
-    data_collecting_types = [
-        {"label": "Partial", "code": "partial", "description": "Partial individuals collected"},
-        {"label": "Full", "code": "full", "description": "Full individual collected"},
-        {"label": "Size only", "code": "size_only", "description": "Size only collected"},
-        {"label": "No individual data", "code": "no_ind_data", "description": "No individual data"},
-        {"label": "Unknown", "code": "unknown", "description": "Unknown"},
-    ]
-
-    for data_dict in data_collecting_types:
-        DataCollectingType.objects.update_or_create(**data_dict)
