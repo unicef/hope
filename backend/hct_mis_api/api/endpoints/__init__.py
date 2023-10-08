08b29a6a99c333fba8a1edc41b2f568f7c81f229
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-10-08 21:37:41 +0200

    Add test

--- backend/hct_mis_api/api/endpoints/__init__.py
+++ backend/hct_mis_api/api/endpoints/__init__.py
@@ -1,6 +1,6 @@
 from hct_mis_api.api.endpoints.area import AreaList, AreaTypeList  # noqa: F401
 from hct_mis_api.api.endpoints.base import ConstanceSettingsAPIView  # noqa: F401
-from hct_mis_api.api.endpoints.data_collecting_type import (  # noqa: F401
+from hct_mis_api.api.endpoints.data_collecting_types import (  # noqa: F401
     DataCollectingTypeViewSet,
 )
 from hct_mis_api.api.endpoints.lookups import (  # noqa: F401
