374b4eb785676c000e7adf107183405b5dec20c2
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-10-10 22:48:48 +0200

    Reverse celery task timer

--- backend/hct_mis_api/apps/core/tasks_schedules.py
+++ backend/hct_mis_api/apps/core/tasks_schedules.py
@@ -39,7 +39,7 @@ TASKS_SCHEDULES = {
     },
     "check_rdi_import_periodic_task": {
         "task": "hct_mis_api.apps.registration_datahub.celery_tasks.check_rdi_import_periodic_task",
-        "schedule": crontab(minute="*/1"),
+        "schedule": crontab(minute="*/15"),
     },
     # "remove_old_rdi_links_task": {
     #     "task": "hct_mis_api.apps.registration_datahub.celery_tasks.remove_old_rdi_links_task",
