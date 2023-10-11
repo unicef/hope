d73940a2739628bae3db7aed066a23a49ee64e82
Jan Romaniak <jan.romaniak@kellton.com>
2023-10-11 17:20:38 +0200

    fix mypy again

--- backend/hct_mis_api/apps/household/models.py
+++ backend/hct_mis_api/apps/household/models.py
@@ -1059,7 +1059,7 @@ class Individual(
 
     def mark_as_duplicate(self, original_individual: Optional["Individual"] = None) -> None:
         if original_individual is not None:
-            self.unicef_id: str = str(original_individual.unicef_id)
+            self.unicef_id = str(original_individual.unicef_id)
         self.documents.update(status=Document.STATUS_INVALID)
         self.duplicate = True
         self.duplicate_date = timezone.now()
