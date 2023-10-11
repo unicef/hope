eb4b8ea359bbea51d645669252e479a3ccc0efa8
Paulina Kujawa <42150286+pkujawa@users.noreply.github.com>
2023-10-11 03:22:28 +0200

    Fix ticket creation and update if subimtted for pcode with multiple areas (#3122)

--- backend/hct_mis_api/apps/grievance/tests/test_grievance_create_complaint_ticket.py
+++ backend/hct_mis_api/apps/grievance/tests/test_grievance_create_complaint_ticket.py
@@ -9,6 +9,7 @@ from hct_mis_api.apps.account.permissions import Permissions
 from hct_mis_api.apps.core.base_test_case import APITestCase
 from hct_mis_api.apps.core.fixtures import create_afghanistan
 from hct_mis_api.apps.core.models import BusinessArea
+from hct_mis_api.apps.core.utils import encode_id_base64
 from hct_mis_api.apps.geo import models as geo_models
 from hct_mis_api.apps.geo.fixtures import AreaFactory, AreaTypeFactory
 from hct_mis_api.apps.grievance.models import GrievanceTicket
@@ -248,7 +249,7 @@ class TestGrievanceCreateComplaintTicketQuery(APITestCase):
                 "assignedTo": self.id_to_base64(self.user.id, "UserNode"),
                 "category": GrievanceTicket.CATEGORY_GRIEVANCE_COMPLAINT,
                 "issueType": GrievanceTicket.ISSUE_TYPE_FSP_COMPLAINT,
-                "admin": self.admin_area.p_code,
+                "admin": encode_id_base64(str(self.admin_area.id), "Area"),
                 "language": "Polish, English",
                 "consent": True,
                 "businessArea": "afghanistan",
