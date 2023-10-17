f7215b453048502e40aa6b6ed23559d3fc5ad77c
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-10-17 10:28:15 +0200

    Fix cypress tests

--- cypress/cypress/page-objects/pages/population_module/population_households.po.js
+++ cypress/cypress/page-objects/pages/population_module/population_households.po.js
@@ -8,13 +8,10 @@ export default class PopulationHouseholds extends BaseComponent {
   textTitle = "Households";
 
   // Elements
-<<<<<<< HEAD
   clickNavHouseholds() {
     this.getMenuButtonProgrammePopulation().click();
     this.getMenuButtonHouseholds().should("be.visible");
     this.getMenuButtonHouseholds().click();
   }
-=======
   getTitle = () => cy.get(this.titlePage);
->>>>>>> 1320d7b3c06f4b8cc0506fbb6d09aaac676921bd
 }
