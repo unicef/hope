03ead21ca7cbc684393b74c3f3c346db4875faf2
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-10-19 09:42:26 +0200

    Fix cypress tests

--- cypress/cypress/page-objects/pages/program_management/details_page.po.js
+++ cypress/cypress/page-objects/pages/program_management/details_page.po.js
@@ -2,8 +2,6 @@ import BaseComponent from "../../base.component";
 
 export default class PMDetailsPage extends BaseComponent {
   // Locators
-  pageHeaderTitle = 'h5[data-cy="page-header-title"]';
   // Texts
   // Elements
-  getTitle = () => cy.get(this.pageHeaderTitle);
 }
