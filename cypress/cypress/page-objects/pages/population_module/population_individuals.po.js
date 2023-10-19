2c96ac6a4e7e3980ec6b5d6ba631ea0dedaa2c12
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-10-19 09:42:26 +0200

    Fix cypress tests

--- cypress/cypress/page-objects/pages/population_module/population_individuals.po.js
+++ cypress/cypress/page-objects/pages/population_module/population_individuals.po.js
@@ -2,13 +2,8 @@ import BaseComponent from "../../base.component";
 
 export default class PopulationIndividuals extends BaseComponent {
   // Locators
-  titlePage = 'h5[data-cy="page-header-title"]';
-
   // Texts
-  textTitle = "Individuals";
-
   // Elements
-  getTitle = () => cy.get(this.titlePage);
 
   clickNavIndividuals() {
     this.getMenuButtonProgrammePopulation().click();
