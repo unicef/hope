08f4ad799164ac5f5321db7d82d6965ad5cbf0e2
Jan Romaniak <jan.romaniak@tivix.com>
2023-10-27 14:01:26 +0200

    fixes

--- backend/compare_coverage.py
+++ backend/compare_coverage.py
@@ -1,5 +1,6 @@
 import sys
 import xml.etree.ElementTree as ET
+from typing import Optional
 
 new_file_argument = sys.argv[1]
 old_file_argument = sys.argv[2] if len(sys.argv) == 3 else None
@@ -8,24 +9,27 @@ if not new_file_argument:
     exit(0)
 
 
-def get_coverage_from_report(file_path):
+def get_coverage_from_report(file_path: str) -> Optional[float]:
     try:
         tree = ET.parse(file_path)
         root = tree.getroot()
-        coverage = float(root.get("line-rate"))
+        coverage = float(root.get("line-rate"))  # type: ignore
         return coverage
-    except:
+    except FileNotFoundError:
         return None
 
 
 current_coverage = get_coverage_from_report(new_file_argument)
-previous_coverage = 0
+previous_coverage: Optional[float] = 0.0
 if old_file_argument:
     previous_coverage = get_coverage_from_report(old_file_argument)
 if previous_coverage is None:
     print("could not get previous coverage, current coverage is", current_coverage)
     exit(0)
-elif current_coverage - previous_coverage < 0:
+if current_coverage is None:
+    print("could not get current coverage, previous coverage was", previous_coverage)
+    exit(0)
+elif current_coverage - previous_coverage < 0.0:
     print(f"coverage decreased from {previous_coverage} to {current_coverage}")
     exit(1)
 else:
