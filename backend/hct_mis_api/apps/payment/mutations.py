92918cc4fca6e849aede0292894145d155d77f9b
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-10-31 07:44:50 +0100

    style

--- backend/hct_mis_api/apps/payment/mutations.py
+++ backend/hct_mis_api/apps/payment/mutations.py
@@ -88,6 +88,7 @@ from hct_mis_api.apps.utils.mutations import ValidationErrorMutationMixin
 
 if TYPE_CHECKING:  # pragma: no cover
     from uuid import UUID
+
     from hct_mis_api.apps.account.models import User
     from hct_mis_api.apps.core.models import BusinessArea
 
