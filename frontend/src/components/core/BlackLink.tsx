b8a48e198a17b4cce394ee7629b1b39e8dfa19bb
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-11-02 11:30:17 +0100

    fix frontend conflicts

--- frontend/src/components/core/BlackLink.tsx
+++ frontend/src/components/core/BlackLink.tsx
@@ -5,14 +5,7 @@ import { Link } from 'react-router-dom';
 export const StyledLink = styled(Link)`
   color: #000;
   max-width: ${(props) => (props.fullWidth ? '100%' : '200px')}
-<<<<<<< HEAD
-  white-space: wrap;
-  word-wrap: break-word;
-  word-break: break-all;
-  display: inline-block;
-=======
   overflow-wrap: break-word;
->>>>>>> origin
 `;
 
 export function BlackLink(props): React.ReactElement {
