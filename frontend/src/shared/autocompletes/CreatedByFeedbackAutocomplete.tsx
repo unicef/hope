0cd19804ff57719ce9c1dd5824bb8bdc85eb013b
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2023-11-02 16:48:45 +0100

    missing data-cy attributes

--- frontend/src/shared/autocompletes/CreatedByFeedbackAutocomplete.tsx
+++ frontend/src/shared/autocompletes/CreatedByFeedbackAutocomplete.tsx
@@ -29,7 +29,6 @@ export const CreatedByFeedbackAutocomplete = ({
   appliedFilter,
   setAppliedFilter,
   setFilter,
-  dataCy,
 }: {
   disabled?: boolean;
   fullWidth?: boolean;
@@ -41,7 +40,6 @@ export const CreatedByFeedbackAutocomplete = ({
   appliedFilter;
   setAppliedFilter: (filter) => void;
   setFilter: (filter) => void;
-  dataCy?: string;
 }): React.ReactElement => {
   const { t } = useTranslation();
   const history = useHistory();
@@ -85,7 +83,7 @@ export const CreatedByFeedbackAutocomplete = ({
   return (
     <StyledAutocomplete
       value={value}
-      data-cy={dataCy}
+      data-cy='created-by-autocomplete'
       fullWidth={fullWidth}
       open={open}
       filterOptions={(options1) => options1}
@@ -106,11 +104,15 @@ export const CreatedByFeedbackAutocomplete = ({
       getOptionLabel={(option) => {
         let optionLabel;
         if (option.node) {
-          const {firstName, lastName} = option.node;
+          const { firstName, lastName } = option.node;
           optionLabel = `${firstName} ${lastName}`;
         } else {
-          const foundUser = data?.allUsers?.edges?.find((el) => el.node.id === option)?.node;
-          optionLabel = foundUser ? `${foundUser.firstName} ${foundUser.lastName}` : '';
+          const foundUser = data?.allUsers?.edges?.find(
+            (el) => el.node.id === option,
+          )?.node;
+          optionLabel = foundUser
+            ? `${foundUser.firstName} ${foundUser.lastName}`
+            : '';
         }
         return `${optionLabel}`;
       }}
@@ -121,6 +123,7 @@ export const CreatedByFeedbackAutocomplete = ({
         <TextField
           {...params}
           label={label || t('Created By')}
+          data-cy='created-by-input'
           variant='outlined'
           margin='dense'
           value={inputValue}
