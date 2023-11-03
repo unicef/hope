6f6a7450946b0df927ed3e8e06a49bc93ff4b165
Paulina Kujawa <p.kujawa2f@gmail.com>
2023-11-03 13:08:14 +0100

    add filter

--- backend/hct_mis_api/one_time_scripts/soft_delete_original_objects.py
+++ backend/hct_mis_api/one_time_scripts/soft_delete_original_objects.py
@@ -1,12 +1,12 @@
 from hct_mis_api.apps.household.models import Household, Individual, IndividualRoleInHousehold, IndividualIdentity, \
     Document, BankAccountInfo
 
-Household.objects.delete()
-Individual.objects.delete()
-IndividualRoleInHousehold.objects.delete()
-Document.objects.delete()
-IndividualIdentity.objects.delete()
-BankAccountInfo.objects.delete()
+Household.objects(program__isnull=True).delete()
+Individual(program__isnull=True).objects.delete()
+IndividualRoleInHousehold.objects(household__program__isnull=True).delete()
+Document.objects(individual__program__isnull=True).delete()
+IndividualIdentity(individual__program__isnull=True).objects.delete()
+BankAccountInfo(individual__program__isnull=True).objects.delete()
 # objects that cannot be deleted because they do not support soft-delete:
 # Feedback, Message, GrievanceTicket, HouseholdSelection
 
