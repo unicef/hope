c6620ce03950cb18a856a7a9968225b91ed19b99
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-11-03 11:44:01 +0100

    Fix cypress tests

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -4,7 +4,7 @@ const { verifyDownloadTasks } = require("cy-verify-downloads");
 module.exports = defineConfig({
   experimentalMemoryManagement: true,
   numTestsKeptInMemory: 20,
-  retries: 0,
+  retries: 1,
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
