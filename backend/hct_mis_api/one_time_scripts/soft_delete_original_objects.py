ecf01d65d45f0aa2c23f3fc3eee2b2d84d79d989
Paulina Kujawa <p.kujawa2f@gmail.com>
2023-11-05 21:46:23 +0100

    add tests

--- backend/hct_mis_api/one_time_scripts/soft_delete_original_objects.py
+++ backend/hct_mis_api/one_time_scripts/soft_delete_original_objects.py
@@ -1,12 +1,19 @@
-from hct_mis_api.apps.household.models import Household, Individual, IndividualRoleInHousehold, IndividualIdentity, \
-    Document, BankAccountInfo
+from hct_mis_api.apps.household.models import (
+    BankAccountInfo,
+    Document,
+    Household,
+    Individual,
+    IndividualIdentity,
+    IndividualRoleInHousehold,
+)
 
-Household.objects(program__isnull=True).delete()
-Individual(program__isnull=True).objects.delete()
-IndividualRoleInHousehold.objects(household__program__isnull=True).delete()
-Document.objects(individual__program__isnull=True).delete()
-IndividualIdentity(individual__program__isnull=True).objects.delete()
-BankAccountInfo(individual__program__isnull=True).objects.delete()
-# objects that cannot be deleted because they do not support soft-delete:
-# Feedback, Message, GrievanceTicket, HouseholdSelection
 
+def soft_delete_original_objects() -> None:
+    Household.objects.filter(program__isnull=True).delete()
+    Individual.objects.filter(program__isnull=True).delete()
+    IndividualRoleInHousehold.objects.filter(household__program__isnull=True).delete()
+    Document.objects.filter(individual__program__isnull=True).delete()
+    IndividualIdentity.objects.filter(individual__program__isnull=True).delete()
+    BankAccountInfo.objects.filter(individual__program__isnull=True).delete()
+    # objects that cannot be deleted because they do not support soft-delete:
+    # Feedback, Message, GrievanceTicket, HouseholdSelection
