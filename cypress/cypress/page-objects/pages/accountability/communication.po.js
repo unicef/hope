5508caa66593be15e5998fdb1ee6ebe958e81e45
szymon-kellton <130459593+szymon-kellton@users.noreply.github.com>
2023-11-06 22:36:32 +0100

    Cypress GPF (#3202)
    * Added test for 171253
    
    * Added cypress tests for 176265
    
    * Enabled RDI test
    
    * Added cypress test for logout (176667)
    
    * Added page objects for Accountability
    
    * Fix cypress tests
    
    * Fix data-cy="created-by-input"
    
    * Fix data-cy="created-by-input"
    
    * Added skip to RDI - Merge Data: ToDo: 178234
    
    * Fix filter created by in feedback in cypress
    
    * Added page objects for Accountability
    
    * Fix cypress tests
    
    * Fix RDI test
    
    ---------
    
    Co-authored-by: Maciej Szewczyk <34482854+mmaciekk@users.noreply.github.com>

---
+++ cypress/cypress/page-objects/pages/accountability/communication.po.js
@@ -0,0 +1,28 @@
+import BaseComponent from "../../base.component";
+
+export default class CommunicationPage extends BaseComponent {
+  // Locators
+  titlePage = 'h5[data-cy="page-header-title"]';
+  rows = 'tr[role="checkbox"]';
+
+  // Texts
+
+  textTitlePage = "Communication";
+  textNewMessage = "New Message";
+  textTargetPopulationFilter = "Target Population";
+  textTabCreatedBy = "Created by";
+  buttonApply = 'button[data-cy="button-filters-apply"]';
+  buttonClear = 'button[data-cy="button-filters-clear"]';
+
+  // Elements
+
+  getTitlePage = () => cy.get(this.titlePage);
+  getMessageID = () => cy.get(this.tabColumnLabel).eq(0);
+  getApply = () => cy.get(this.buttonApply);
+  getClear = () => cy.get(this.buttonClear);
+  getTargetPopulationsRows = () => cy.get(this.rows);
+
+  checkElementsOnPage() {
+    this.getTitlePage().should("be.visible").contains(this.textTitlePage);
+  }
+}
