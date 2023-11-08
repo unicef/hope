0398bf94235b3dffc012932c8725478ceb156ccf
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-11-08 12:39:07 +0100

    split pipeline

--- cypress/docker-entrypoint.sh
+++ cypress/docker-entrypoint.sh
@@ -2,16 +2,24 @@
 
 set -e
 
+wait_for_stuff() {
+    echo "Waiting for backend to be ready"
+    waitforit -host=backend -port=8000 --timeout 300
+    echo "Waiting for frontend to be ready"
+    waitforit -host=frontend -port=80 --timeout 300
+    echo "Waiting for proxy to be ready"
+    waitforit -host=proxy -port=80 --timeout 300
+    echo "Staring cypress tests..."
+}
+
 case "$1" in
-    "ci-test")
-        echo "Waiting for backend to be ready"
-        waitforit -host=backend -port=8000 --timeout 300
-        echo "Waiting for frontend to be ready"
-        waitforit -host=frontend -port=80 --timeout 300
-        echo "Waiting for proxy to be ready"
-        waitforit -host=proxy -port=80 --timeout 300
-        echo "Staring cypress tests..."
-        exec npm run test
+    "ci-test-sequential")
+        wait_for_stuff
+        exec npm run sequential || npm run posttest
+        ;;
+    "ci-test-parallel")
+        wait_for_stuff
+        exec npm run parallel || npm run posttest
         ;;
     *)
         exec "$@"
