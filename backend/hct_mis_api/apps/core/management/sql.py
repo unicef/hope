8aa94f447a158794db7a4959e5b9ad8674f08f26
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-11-13 15:46:59 +0100

    test with post_gis_tables list

--- backend/hct_mis_api/apps/core/management/sql.py
+++ backend/hct_mis_api/apps/core/management/sql.py
@@ -1,15 +1,52 @@
 from typing import Any
 
 from django.db import connections
-from django.contrib.gis.db.backends import postgis
 
 
 def sql_drop_tables(connection: Any, connection_name: str = "") -> str:
     tables = connection.introspection.table_names(include_views=False)
-    tables.append("django_migrations")
-    gis_tables = ["addr", "addrfeat"]
-    tables = list(set(tables) - set(gis_tables))
-    print("=>>> TABLES:", tables)
+    if "django_migrations" not in tables:
+        tables.append("django_migrations")
+    # fix 'cannot drop table addr because extension postgis_tiger_geocoder requires it'
+    post_gis_tables = [
+        "addr",
+        "addrfeat",
+        "bg",
+        "county",
+        "county_lookup",
+        "countysub_lookup",
+        "cousub",
+        "direction_lookup",
+        "edges",
+        "faces",
+        "featnames",
+        "geocode_settings",
+        "geocode_settings_default",
+        "layer",
+        "loader_lookuptables",
+        "loader_platform",
+        "loader_variables",
+        "pagc_gaz",
+        "pagc_lex",
+        "pagc_rules",
+        "place",
+        "place_lookup",
+        "secondary_unit_lookup",
+        "state",
+        "state_lookup",
+        "street_type_lookup",
+        "tabblock",
+        "tabblock20",
+        "topology",
+        "tract",
+        "zcta5",
+        "zip_lookup",
+        "zip_lookup_all",
+        "zip_lookup_base",
+        "zip_state",
+        "zip_state_loc",
+    ]
+    tables = list(set(tables) - set(post_gis_tables))
     if not tables:
         return ""
     tables_sql = ", ".join(connection.ops.quote_name(table) for table in tables)
