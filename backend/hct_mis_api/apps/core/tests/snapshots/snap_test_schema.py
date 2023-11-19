ec8194584585ee54aa925014393b8b9ff094dd7d
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-11-19 14:33:04 +0100

    Disable creation of a program with unknown DCT

---
+++ backend/hct_mis_api/apps/core/tests/snapshots/snap_test_schema.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestDataCollectionTypeSchema::test_dct_with_unknown_code_is_not_in_list 1'] = {
+    'data': {
+        'dataCollectionTypeChoices': [
+            {
+                'name': 'Full',
+                'value': 'full'
+            },
+            {
+                'name': 'Partial',
+                'value': 'partial'
+            },
+            {
+                'name': 'Size Only',
+                'value': 'size_only'
+            }
+        ]
+    }
+}
