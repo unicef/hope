a1f6f0eb7c5d936ce93beadec0e8eabefb882910
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2023-11-21 11:25:49 +0100

    Add weight to test

--- backend/hct_mis_api/apps/core/tests/test_schema.py
+++ backend/hct_mis_api/apps/core/tests/test_schema.py
@@ -19,23 +19,20 @@ class TestDataCollectionTypeSchema(APITestCase):
         cls.user = UserFactory()
 
         cls.data_collecting_type_full = DataCollectingTypeFactory(
-            label="Full", code="full", business_areas=[cls.business_area]
+            label="Full", code="full", weight=1, business_areas=[cls.business_area]
         )
         cls.data_collecting_type_partial = DataCollectingTypeFactory(
-            label="Partial", code="partial", business_areas=[cls.business_area]
+            label="Partial", code="partial", weight=2, business_areas=[cls.business_area]
         )
         cls.data_collecting_type_size_only = DataCollectingTypeFactory(
-            label="Size Only", code="size_only", business_areas=[cls.business_area]
+            label="Size Only", code="size_only", weight=3, business_areas=[cls.business_area]
         )
         cls.data_collecting_type_unknown = DataCollectingTypeFactory(
-            label="Unknown", code="unknown", business_areas=[cls.business_area]
+            label="Unknown", code="unknown", weight=4, business_areas=[cls.business_area]
         )
 
-    def test_dct_with_unknown_code_is_not_in_list(self) -> None:
+    def test_dct_with_unknown_code_is_not_in_list(self) -> None:  # also checks weight sorting
         self.snapshot_graphql_request(
             request_string=self.QUERY_DATA_COLLECTING_TYPE_CHOICES,
             context={"user": self.user, "headers": {"Business-Area": self.business_area.slug}},
         )
-
-    def test_dct_is_ordered_with_weight_attr(self) -> None:  # TODO: add it when weight attr merged
-        pass
