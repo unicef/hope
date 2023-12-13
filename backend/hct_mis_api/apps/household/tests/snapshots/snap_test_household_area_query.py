1b2249bf169a61ae79764867ed56891c87444d7b
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2023-12-13 13:28:40 +0100

    add order_by size

--- backend/hct_mis_api/apps/household/tests/snapshots/snap_test_household_area_query.py
+++ backend/hct_mis_api/apps/household/tests/snapshots/snap_test_household_area_query.py
@@ -22,35 +22,35 @@ snapshots['TestHouseholdAreaQuery::test_household_admin_area_1_is_filtered_0_wit
                     'node': {
                         'address': 'address_2',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 2
                     }
                 },
                 {
                     'node': {
                         'address': 'address_3',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 3
                     }
                 },
                 {
                     'node': {
                         'address': 'address_4',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 4
                     }
                 },
                 {
                     'node': {
                         'address': 'address_5',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 5
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
@@ -93,21 +93,21 @@ snapshots['TestHouseholdAreaQuery::test_household_admin_area_2_and_admin_area_3_
                     'node': {
                         'address': 'address_4',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 4
                     }
                 },
                 {
                     'node': {
                         'address': 'address_5',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 5
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
@@ -143,28 +143,28 @@ snapshots['TestHouseholdAreaQuery::test_household_admin_area_2_is_filtered_0_wit
                     'node': {
                         'address': 'address_3',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 3
                     }
                 },
                 {
                     'node': {
                         'address': 'address_4',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 4
                     }
                 },
                 {
                     'node': {
                         'address': 'address_5',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 5
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
@@ -200,14 +200,14 @@ snapshots['TestHouseholdAreaQuery::test_household_admin_area_3_is_filtered_0_wit
                     'node': {
                         'address': 'address_5',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 5
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
@@ -250,14 +250,14 @@ snapshots['TestHouseholdAreaQuery::test_household_many_admin_area_2_is_filtered_
                     'node': {
                         'address': 'address_2',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 2
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
@@ -293,21 +293,21 @@ snapshots['TestHouseholdAreaQuery::test_household_many_admin_area_3_is_filtered_
                     'node': {
                         'address': 'address_4',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 4
                     }
                 },
                 {
                     'node': {
                         'address': 'address_5',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 5
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
@@ -316,148 +316,6 @@ snapshots['TestHouseholdAreaQuery::test_household_many_admin_area_3_is_filtered_
 }
 
 snapshots['TestHouseholdAreaQuery::test_household_many_admin_area_3_is_filtered_1_without_permission 1'] = {
-    'data': {
-        'allHouseholds': None
-    },
-    'errors': [
-        {
-            'locations': [
-                {
-                    'column': 5,
-                    'line': 3
-                }
-            ],
-            'message': 'Permission Denied',
-            'path': [
-                'allHouseholds'
-            ]
-        }
-    ]
-}
-
-snapshots['TestHouseholdAreaQuery::test_households_are_filtered_when_partner_has_business_area_access_0_with_permission 1'] = {
-    'data': {
-        'allHouseholds': {
-            'edges': [
-                {
-                    'node': {
-                        'address': 'address_1',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_2',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_3',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_4',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_5',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_6',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestHouseholdAreaQuery::test_households_are_filtered_when_partner_has_business_area_access_1_without_permission 1'] = {
-    'data': {
-        'allHouseholds': None
-    },
-    'errors': [
-        {
-            'locations': [
-                {
-                    'column': 5,
-                    'line': 3
-                }
-            ],
-            'message': 'Permission Denied',
-            'path': [
-                'allHouseholds'
-            ]
-        }
-    ]
-}
-
-snapshots['TestHouseholdAreaQuery::test_households_are_filtered_when_partner_is_unicef_0_with_permission 1'] = {
-    'data': {
-        'allHouseholds': {
-            'edges': [
-                {
-                    'node': {
-                        'address': 'address_1',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_2',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_3',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_4',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_5',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_6',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestHouseholdAreaQuery::test_households_are_filtered_when_partner_is_unicef_1_without_permission 1'] = {
     'data': {
         'allHouseholds': None
     },
@@ -492,35 +350,35 @@ snapshots['TestHouseholdAreaQuery::test_households_area_filtered_when_partner_ha
                     'node': {
                         'address': 'address_2',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 2
                     }
                 },
                 {
                     'node': {
                         'address': 'address_3',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 3
                     }
                 },
                 {
                     'node': {
                         'address': 'address_4',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 4
                     }
                 },
                 {
                     'node': {
                         'address': 'address_5',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 5
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
@@ -548,57 +406,6 @@ snapshots['TestHouseholdAreaQuery::test_households_area_filtered_when_partner_ha
     ]
 }
 
-snapshots['TestHouseholdAreaQuery::test_households_area_filtered_when_partner_is_unicef_0_with_permission 1'] = {
-    'data': {
-        'allHouseholds': {
-            'edges': [
-                {
-                    'node': {
-                        'address': 'address_1',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_2',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_3',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_4',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_5',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_6',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                }
-            ]
-        }
-    }
-}
-
 snapshots['TestHouseholdAreaQuery::test_households_area_filtered_when_partner_is_unicef_0_without_user_role_permission 1'] = {
     'data': {
         'allHouseholds': {
@@ -614,86 +421,35 @@ snapshots['TestHouseholdAreaQuery::test_households_area_filtered_when_partner_is
                     'node': {
                         'address': 'address_2',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 2
                     }
                 },
                 {
                     'node': {
                         'address': 'address_3',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 3
                     }
                 },
                 {
                     'node': {
                         'address': 'address_4',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 4
                     }
                 },
                 {
                     'node': {
                         'address': 'address_5',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 5
                     }
                 },
                 {
                     'node': {
                         'address': 'address_6',
                         'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                }
-            ]
-        }
-    }
-}
-
-snapshots['TestHouseholdAreaQuery::test_households_area_filtered_when_partner_is_unicef_1_without_permission 1'] = {
-    'data': {
-        'allHouseholds': {
-            'edges': [
-                {
-                    'node': {
-                        'address': 'address_1',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_2',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_3',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_4',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_5',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
-                    }
-                },
-                {
-                    'node': {
-                        'address': 'address_6',
-                        'countryOrigin': 'Afghanistan',
-                        'size': 1
+                        'size': 6
                     }
                 }
             ]
