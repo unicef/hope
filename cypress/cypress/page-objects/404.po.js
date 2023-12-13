0e5f5bea5aba8621e484503d546294de2b25b7b2
Szymon Wyderka <szymon.wyderka@kellton.com>
2023-12-13 09:15:07 +0100

    Fix cypress tests

--- cypress/cypress/page-objects/404.po.js
+++ cypress/cypress/page-objects/404.po.js
@@ -4,12 +4,12 @@ export default class ErrorPage extends BaseComponent {
   // Locators
   pageNotFound = "h1";
   buttonRefresh = "button";
-  buttonCountryDashboard = "a";
+  buttonCountryDashboard = '[data-cy="button-go-to-programme-management"]';
 
   // Texts
-  text404Error = "Oops! Page Not Found";
+  text404Error = "Access Denied";
   textRefresh = "REFRESH PAGE";
-  textGoTo = "GO TO COUNTRY DASHBOARD";
+  textGoTo = "GO TO PROGRAMME MANAGEMENT";
   // Elements
   getPageNoFound = () => cy.get(this.pageNotFound).contains(this.text404Error);
   getButtonRefresh = () =>
