f7a76dd6d9a03aac7ca9381b1d75cc6940ba0f2d
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2024-01-08 09:02:42 +0100

    mv cy step to entrypoint

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -18,6 +18,13 @@ else
       python manage.py migratealldb
       python manage.py runserver 0.0.0.0:8000
       ;;
+    "cy")
+      wait_for_db db
+      python manage.py collectstatic --no-input
+      python manage.py migratealldb
+      python manage.py initcypress --skip-drop
+      python manage.py runserver 0.0.0.0:8000
+      ;;
     "celery-beat")
       waitforit -host=backend -port=8000 --timeout 300 && \
       celery -A hct_mis_api.apps.core.celery beat -l INFO --scheduler hct_mis_api.apps.core.models:CustomDatabaseScheduler
