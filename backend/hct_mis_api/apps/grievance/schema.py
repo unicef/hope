97e08b9839736477b874c9f6f0436d93d4a0bfd8
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-01-15 21:40:58 +0100

    update test

--- backend/hct_mis_api/apps/grievance/schema.py
+++ backend/hct_mis_api/apps/grievance/schema.py
@@ -569,7 +569,9 @@ class Query(graphene.ObjectType):
 
         queryset = queryset.prefetch_related(*to_prefetch)
 
-        if not user.partner.is_unicef:  # Full access to all AdminAreas if is_unicef
+        # Full access to all AdminAreas if is_unicef
+        # and ignore filtering for Cross Area tickets
+        if not user.partner.is_unicef and not kwargs.get("is_cross_area", False):
             queryset = filter_grievance_tickets_based_on_partner_areas_2(
                 queryset, user.partner, business_area_id, program_id
             )
