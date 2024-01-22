b3d7e909166c6847b4e07596c1524c91d0d16a1b
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2024-01-22 21:47:06 +0100

    Filter_rdi_template_choices_by_ba

--- backend/hct_mis_api/apps/registration_datahub/views.py
+++ backend/hct_mis_api/apps/registration_datahub/views.py
@@ -1,4 +1,5 @@
 from tempfile import NamedTemporaryFile
+from urllib.parse import urlparse
 
 from django.http import HttpRequest, HttpResponse
 
@@ -8,15 +9,18 @@ from hct_mis_api.apps.registration_datahub.template_generator import (
 
 
 def download_template(request: HttpRequest) -> HttpResponse:
+    parse_result = urlparse(request.headers["Referer"])
+    business_area_slug = parse_result.path[1:].split("/")[0]
+
     mimetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     filename = "registration_data_import_template.xlsx"
     response = HttpResponse(content_type=mimetype)
     response["Content-Disposition"] = f"attachment; filename={filename}"
-    wb = TemplateFileGenerator.get_template_file()
+
+    wb = TemplateFileGenerator.get_template_file(business_area_slug)
     with NamedTemporaryFile() as tmp:
         wb.save(tmp.name)
         file = bytes(tmp.read())
 
     response.write(file)
-
     return response
