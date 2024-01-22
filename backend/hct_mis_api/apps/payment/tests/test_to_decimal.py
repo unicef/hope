88f2b6865365e40a4a258213d2b64cb90bc4eec7
Patryk Dabrowski <patryk.dabrowski@kellton.com>
2024-01-22 09:25:27 +0100

    Fix decimal conversion (#3526)

---
+++ backend/hct_mis_api/apps/payment/tests/test_to_decimal.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+
+from django.test import TestCase
+
+from hct_mis_api.apps.payment.utils import to_decimal
+
+
+class TestToDecimal(TestCase):
+    def test_to_decimal(self) -> None:
+        self.assertEqual(to_decimal(" "), None)
+        self.assertEqual(to_decimal(""), None)
+        self.assertEqual(to_decimal(None), None)
+        self.assertEqual(to_decimal(3), Decimal(f"{round(3.00, 2):.2f}"))
+        self.assertEqual(to_decimal("3"), Decimal(f"{round(3.00, 2):.2f}"))
+        self.assertEqual(to_decimal(3.1), Decimal(f"{round(3.10, 2):.2f}"))
+        self.assertEqual(to_decimal(3.14), Decimal(f"{round(3.14, 2):.2f}"))
+        self.assertEqual(to_decimal("3.1"), Decimal(f"{round(3.10, 2):.2f}"))
+        self.assertEqual(to_decimal("3.14"), Decimal(f"{round(3.14, 2):.2f}"))
+        self.assertEqual(to_decimal(Decimal(3.14)), Decimal(f"{round(3.14, 2):.2f}"))
