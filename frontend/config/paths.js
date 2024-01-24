cab9be8146243c0f43d859b5b081474630305970
Jan Romaniak <jan.romaniak@kellton.com>
2024-01-24 13:20:31 +0100

    fixed production build

--- frontend/config/paths.js
+++ frontend/config/paths.js
@@ -69,7 +69,10 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
+  appBuild: path.resolve(
+        __dirname,
+        '../../backend/hct_mis_api/apps/web/static/web/',
+      ),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
