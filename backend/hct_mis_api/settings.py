ee6904d7f65af7b840938bcca048036e80d6ea82
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2024-01-24 09:40:22 +0100

    Allow to set cache's location through env (#3541)

--- backend/hct_mis_api/settings.py
+++ backend/hct_mis_api/settings.py
@@ -489,7 +489,7 @@ if CACHE_ENABLED:
     CACHES = {
         "default": {
             "BACKEND": "django_redis.cache.RedisCache",
-            "LOCATION": f"redis://{REDIS_INSTANCE}/1",
+            "LOCATION": env("CACHE_LOCATION", default=f"redis://{REDIS_INSTANCE}/1"),
             "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         }
     }
