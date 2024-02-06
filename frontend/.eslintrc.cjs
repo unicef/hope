f56dda32c4ec10bedc9a04cf0f464f4b589d6665
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2024-02-06 20:58:21 +0100

    fix eslint config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,7 +1,19 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['airbnb', 'airbnb-typescript'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  scripts: {
+    lint: 'eslint src --ext .ts,.tsx',
   },
 };
