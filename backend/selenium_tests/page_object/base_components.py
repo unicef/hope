7052c79a6a6ab9f61c0150dcec3edffa87b0d222
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-02-08 11:29:36 +0100

    Selenium single docker almost done

--- backend/selenium_tests/page_object/base_components.py
+++
@@ -1,9 +0,0 @@
-from helpers.helper import Common
-from selenium.webdriver.remote.webelement import WebElement
-
-
-class BaseComponents(Common):
-    navProgrammeManagement = 'a[data-cy="nav-Programme Management"]'
-
-    def getNavProgrammeManagement(self) -> WebElement:
-        return self.wait_for(self.navProgrammeManagement)
