fe0abcc611305fb94a64a3d76ea40da5b9f18928
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2024-02-23 12:57:11 +0100

    Split build to vendor and index

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.png'],
-  plugins: [tsconfigPaths(), react()],
+  base: '',
+  plugins: [tsconfigPaths(), react(), splitVendorChunkPlugin()],
   resolve: {
     mainFields: [],
   },
