1c701e543ccbbfdced1bb9b6b326a84bd8d1333f
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-03-01 14:35:21 +0100

    generate new template based on program DCT

--- backend/hct_mis_api/apps/registration_datahub/views.py
+++ backend/hct_mis_api/apps/registration_datahub/views.py
@@ -9,15 +9,19 @@ from hct_mis_api.apps.registration_datahub.template_generator import (
 
 
 def download_template(request: HttpRequest) -> HttpResponse:
+    program = request.headers["Program"]
     parse_result = urlparse(request.headers["Referer"])
     business_area_slug = parse_result.path[1:].split("/")[0]
 
+    is_program_for_social_worker = False
+
     mimetype = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    filename = "registration_data_import_template.xlsx"
+    filename = "registration_data_import_template_social_worker.xlsx" if is_program_for_social_worker else "registration_data_import_template.xlsx"
     response = HttpResponse(content_type=mimetype)
     response["Content-Disposition"] = f"attachment; filename={filename}"
 
-    wb = TemplateFileGenerator.get_template_file(business_area_slug)
+    wb = TemplateFileGenerator.get_template_file(business_area_slug, is_program_for_social_worker)
+
     with NamedTemporaryFile() as tmp:
         wb.save(tmp.name)
         file = bytes(tmp.read())
