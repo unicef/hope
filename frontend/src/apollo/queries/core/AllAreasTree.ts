dc80e8eeb4d905d2b078601f9e9d9849d1fe1a33
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2024-03-18 16:24:46 +0100

    make codegen use graphql schema

--- frontend/src/apollo/queries/core/AllAreasTree.ts
+++ frontend/src/apollo/queries/core/AllAreasTree.ts
@@ -4,24 +4,22 @@ export const ALL_AREAS_TREE_QUERY = gql`
   query AllAreasTree($businessArea: String!) {
     allAreasTree(businessArea: $businessArea) {
       id
-      name
       pCode
+      name
       level
       areas {
         id
         name
-        pCode
         level
+        pCode
         areas {
           id
           name
           pCode
-          level
           areas {
             id
             name
             pCode
-            level
           }
         }
       }
