3c7bd6b7928d53da4e33fb6265c070dfd69362dc
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2024-03-20 18:29:43 +0100

    Reformat urls

--- backend/hct_mis_api/urls.py
+++ backend/hct_mis_api/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.admin import site
@@ -127,6 +126,6 @@ urlpatterns = (
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [
-        url(r"^__debug__/", include(debug_toolbar.urls)),
-    ]
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
