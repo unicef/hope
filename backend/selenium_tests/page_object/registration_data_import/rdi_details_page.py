01e8eed7484cf7892c346ba272cd9703bdc9a417
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-03-22 16:44:49 +0100

    Added RDI details page object

--- backend/selenium_tests/page_object/registration_data_import/rdi_details_page.py
+++ backend/selenium_tests/page_object/registration_data_import/rdi_details_page.py
@@ -1,8 +1,69 @@
 from page_object.base_components import BaseComponents
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class RDIDetailsPage(BaseComponents):
-    pass
-    # Locators
-    # Texts
-    # Elements
+    mainContent = 'div[data-cy="main-content"]'
+    pageHeaderContainer = 'div[data-cy="page-header-container"]'
+    pageHeaderTitle = 'h5[data-cy="page-header-title"]'
+    labelStatus = 'div[data-cy="label-status"]'
+    statusContainer = 'div[data-cy="status-container"]'
+    labelSourceOfData = 'div[data-cy="label-Source of Data"]'
+    labelImportDate = 'div[data-cy="label-Import Date"]'
+    labelImportedBy = 'div[data-cy="label-Imported by"]'
+    labelizedFieldContainerHouseholds = 'div[data-cy="labelized-field-container-households"]'
+    labelTotalNumberOfHouseholds = 'div[data-cy="label-Total Number of Households"]'
+    labelizedFieldContainerIndividuals = 'div[data-cy="labelized-field-container-individuals"]'
+    labelTotalNumberOfIndividuals = 'div[data-cy="label-Total Number of Individuals"]'
+    tableLabel = 'span[data-cy="table-label"]'
+    tablePagination = 'div[data-cy="table-pagination"]'
+    importedIndividualsTable = 'div[data-cy="imported-individuals-table"]'
+    tableRow = 'tr[data-cy="table-row"]'
+
+    def getMainContent(self) -> WebElement:
+        return self.wait_for(self.mainContent)
+
+    def getPageHeaderContainer(self) -> WebElement:
+        return self.wait_for(self.pageHeaderContainer)
+
+    def getPageHeaderTitle(self) -> WebElement:
+        return self.wait_for(self.pageHeaderTitle)
+
+    def getLabelStatus(self) -> WebElement:
+        return self.wait_for(self.labelStatus)
+
+    def getStatusContainer(self) -> WebElement:
+        return self.wait_for(self.statusContainer)
+
+    def getLabelSourceOfData(self) -> WebElement:
+        return self.wait_for(self.labelSourceOfData)
+
+    def getLabelImportDate(self) -> WebElement:
+        return self.wait_for(self.labelImportDate)
+
+    def getLabelImportedBy(self) -> WebElement:
+        return self.wait_for(self.labelImportedBy)
+
+    def getLabelizedFieldContainerHouseholds(self) -> WebElement:
+        return self.wait_for(self.labelizedFieldContainerHouseholds)
+
+    def getLabelTotalNumberOfHouseholds(self) -> WebElement:
+        return self.wait_for(self.labelTotalNumberOfHouseholds)
+
+    def getLabelizedFieldContainerIndividuals(self) -> WebElement:
+        return self.wait_for(self.labelizedFieldContainerIndividuals)
+
+    def getLabelTotalNumberOfIndividuals(self) -> WebElement:
+        return self.wait_for(self.labelTotalNumberOfIndividuals)
+
+    def getTableLabel(self) -> WebElement:
+        return self.wait_for(self.tableLabel)
+
+    def getTableRow(self) -> WebElement:
+        return self.wait_for(self.tableRow)
+
+    def getTablePagination(self) -> WebElement:
+        return self.wait_for(self.tablePagination)
+
+    def getImportedIndividualsTable(self) -> WebElement:
+        return self.wait_for(self.importedIndividualsTable)
--- backend/selenium_tests/page_object/registration_data_import/registration_data_import.py
+++ backend/selenium_tests/page_object/registration_data_import/registration_data_import.py
@@ -25,5 +25,86 @@ class RegistrationDataImport(BaseComponents):
 
     # Elements
 
-    def getButtonImport(self) -> WebElement:
-        return self.wait_for(self.buttonImport)
+    def MaingetContent(self) -> WebElement:
+        return self.wait_for(self.mainContent)
+
+    def PagegetHeadergetContainer(self) -> WebElement:
+        return self.wait_for(self.pageHeaderContainer)
+
+    def PagegetHeadergetTitle(self) -> WebElement:
+        return self.wait_for(self.pageHeaderTitle)
+
+    def ButtongetNewgetProgram(self) -> WebElement:
+        return self.wait_for(self.buttonNewProgram)
+
+    def FiltersgetSearch(self) -> WebElement:
+        return self.wait_for(self.filtersSearch)
+
+    def FiltersgetStatus(self) -> WebElement:
+        return self.wait_for(self.filtersStatus)
+
+    def DategetPickergetFilter(self) -> WebElement:
+        return self.wait_for(self.datePickerFilter)
+
+    def DategetPickergetFilter(self) -> WebElement:
+        return self.wait_for(self.datePickerFilter)
+
+    def FiltersgetSector(self) -> WebElement:
+        return self.wait_for(self.filtersSector)
+
+    def FiltersgetNumbergetOfgetHouseholdsgetMin(self) -> WebElement:
+        return self.wait_for(self.filtersNumberOfHouseholdsMin)
+
+    def FiltersgetNumbergetOfgetHouseholdsgetMax(self) -> WebElement:
+        return self.wait_for(self.filtersNumberOfHouseholdsMax)
+
+    def FiltersgetBudgetgetMin(self) -> WebElement:
+        return self.wait_for(self.filtersBudgetMin)
+
+    def FiltersgetBudgetgetMax(self) -> WebElement:
+        return self.wait_for(self.filtersBudgetMax)
+
+    def FiltersgetDatagetCollectinggetType(self) -> WebElement:
+        return self.wait_for(self.filtersDataCollectingType)
+
+    def ButtongetFiltersgetClear(self) -> WebElement:
+        return self.wait_for(self.buttonFiltersClear)
+
+    def ButtongetFiltersgetApply(self) -> WebElement:
+        return self.wait_for(self.buttonFiltersApply)
+
+    def TablegetTitle(self) -> WebElement:
+        return self.wait_for(self.tableTitle)
+
+    def TablegetLabel(self) -> WebElement:
+        return self.wait_for(self.tableLabel)
+
+    def TablegetLabel(self) -> WebElement:
+        return self.wait_for(self.tableLabel)
+
+    def TablegetLabel(self) -> WebElement:
+        return self.wait_for(self.tableLabel)
+
+    def TablegetLabel(self) -> WebElement:
+        return self.wait_for(self.tableLabel)
+
+    def TablegetLabel(self) -> WebElement:
+        return self.wait_for(self.tableLabel)
+
+    def TablegetLabel(self) -> WebElement:
+        return self.wait_for(self.tableLabel)
+
+    def TablegetRowgetDraftgetProgram(self) -> WebElement:
+        return self.wait_for(self.tableRowDraftProgram)
+
+    def StatusgetContainer(self) -> WebElement:
+        return self.wait_for(self.statusContainer)
+
+    def TablegetRowgetTestgetProgramm(self) -> WebElement:
+        return self.wait_for(self.tableRowTestProgramm)
+
+    def StatusgetContainer(self) -> WebElement:
+        return self.wait_for(self.statusContainer)
+
+    def TablegetPagination(self) -> WebElement:
+        return self.wait_for(self.tablePagination)
