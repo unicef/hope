0ef7d04628721716c92763b991bd18e460edd978
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2024-03-25 14:06:48 +0100

    Integrate Matomo

---
+++ backend/hct_mis_api/apps/core/context_processors.py
@@ -0,0 +1,16 @@
+from typing import TypedDict
+
+from django.conf import settings
+from django.http import HttpRequest
+
+
+class MatomoConfig(TypedDict):
+    MATOMO_SITE_ID: str
+    MATOMO_TRACKER_URL: str
+
+
+def matomo(request: HttpRequest) -> MatomoConfig:
+    return {
+        "MATOMO_SITE_ID": settings.MATOMO_SITE_ID,
+        "MATOMO_TRACKER_URL": settings.MATOMO_TRACKER_URL,
+    }
