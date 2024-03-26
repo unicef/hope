b15057694b47335b3cddbae07f6c637422192bc3
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-03-26 13:43:28 +0100

    small fix

--- backend/hct_mis_api/middlewares/sentry.py
+++ backend/hct_mis_api/middlewares/sentry.py
@@ -7,6 +7,14 @@ import pycountry
 from sentry_sdk import configure_scope
 
 
+def is_country_name(country_name: str) -> bool:
+    try:
+        pycountry.countries.lookup(country_name)
+        return True
+    except LookupError:
+        return False
+
+
 class SentryScopeMiddleware:
     def __init__(self, get_response: Callable) -> None:
         self.get_response = get_response
@@ -19,10 +27,11 @@ class SentryScopeMiddleware:
             business_area = request.headers.get("Business-Area")
             if not business_area:
                 business_area = "NO_BA"
+                # example: api/rest/ukraine/rdi/upload/; api/admin/account/role/;
                 country_from_path = request.path.split("/")
                 country_name = country_from_path[3] if len(country_from_path) > 3 else None
                 if country_name:
-                    business_area = country_name if pycountry.countries.lookup(country_name) else "NO_BA"
+                    business_area = country_name if is_country_name(country_name) else "NO_BA"
             scope.set_tag("username", request.user.username)
             scope.set_tag("business_area", business_area)
             response = self.get_response(request)
