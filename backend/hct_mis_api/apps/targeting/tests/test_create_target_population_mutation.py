73eca31e34b9a3ee98e9d36407733ece9dc4629a
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-03-28 17:35:49 +0100

    upd test create tp

--- backend/hct_mis_api/apps/targeting/tests/test_create_target_population_mutation.py
+++ backend/hct_mis_api/apps/targeting/tests/test_create_target_population_mutation.py
@@ -2,6 +2,7 @@ from typing import Any, List
 
 from parameterized import parameterized
 
+from hct_mis_api.apps.household.models import Household
 from hct_mis_api.apps.account.fixtures import UserFactory
 from hct_mis_api.apps.account.permissions import Permissions
 from hct_mis_api.apps.core.base_test_case import APITestCase
@@ -22,7 +23,11 @@ class TestCreateTargetPopulationMutation(APITestCase):
           status
           totalHouseholdsCount
           totalIndividualsCount
+          hasEmptyCriteria
+          hasEmptyIdsCriteria
             targetingCriteria{
+            householdIds
+            individualIds
             rules{
               filters{
                 comparisonMethod
@@ -41,17 +46,17 @@ class TestCreateTargetPopulationMutation(APITestCase):
     def setUpTestData(cls) -> None:
         cls.user = UserFactory.create()
         create_afghanistan()
-        create_household(
-            {"size": 2, "residence_status": "HOST"},
-        )
-        create_household(
-            {"size": 3, "residence_status": "HOST"},
-        )
-        create_household(
-            {"size": 3, "residence_status": "HOST"},
-        )
         business_area = BusinessArea.objects.get(slug="afghanistan")
         cls.program = ProgramFactory.create(name="program1", status=Program.ACTIVE, business_area=business_area)
+        create_household(
+            {"size": 2, "residence_status": "HOST", "program": cls.program},
+        )
+        create_household(
+            {"size": 3, "residence_status": "HOST", "program": cls.program},
+        )
+        create_household(
+            {"size": 4, "residence_status": "HOST", "program": cls.program},
+        )
 
     @parameterized.expand(
         [
@@ -190,3 +195,41 @@ class TestCreateTargetPopulationMutation(APITestCase):
         )
         assert "errors" not in response_ok
         self.assertEqual(TargetPopulation.objects.count(), 1)
+
+    def test_create_mutation_target_by_id(self) -> None:
+        self.create_user_role_with_permissions(self.user, [Permissions.TARGETING_CREATE], self.program.business_area)
+        hh_1 = Household.objects.filter(program_id=self.program.id, size=2).first()
+        hh_2 = Household.objects.filter(program_id=self.program.id, size=3).first()
+        hh_3 = Household.objects.filter(program_id=self.program.id, size=4).first()
+        hh_1.unicef_id = "HH-1"
+        hh_2.unicef_id = "HH-2"
+        hh_3.unicef_id = "HH-3"
+        hh_1.save()
+        hh_2.save()
+        hh_3.save()
+        ind_hh_3 = hh_3.individuals.first()
+        ind_hh_3.unicef_id = "IND-33"
+        ind_hh_3.save()
+
+        targeting_criteria_list = [
+            {"householdIds": " HH-1,", "individualIds": "", "rules": []},
+            {"householdIds": "HH-1, HH-2, HH-3, ", "individualIds": "IND-33, IND-33, ", "rules": []},
+            {"householdIds": "HH-1", "individualIds": "IND-33", "rules": []},
+            {"householdIds": "", "individualIds": "IND-33", "rules": []},
+        ]
+
+        for num, targeting_criteria in enumerate(targeting_criteria_list, 1):
+            variables = {
+                "createTargetPopulationInput": {
+                    "name": f"Test name {num}",
+                    "businessAreaSlug": "afghanistan",
+                    "programId": self.id_to_base64(self.program.id, "ProgramNode"),
+                    "excludedIds": "",
+                    "targetingCriteria": targeting_criteria,
+                }
+            }
+            self.snapshot_graphql_request(
+                request_string=TestCreateTargetPopulationMutation.MUTATION_QUERY,
+                context={"user": self.user},
+                variables=variables,
+            )
