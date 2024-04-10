011e5cc43a0359218c92fab0235f07ec0772e189
Konrad Marzec <konradmarzec@Konrad-Marzec-Kellton-MacBook-Pro-XYFVVL617K.local>
2024-04-10 11:00:38 +0200

    Add rdi_merge_status field

--- backend/hct_mis_api/apps/core/mixins.py
+++ backend/hct_mis_api/apps/core/mixins.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.query import QuerySet
-
+from django.utils.translation import gettext_lazy as _
 from hct_mis_api.apps.core.models import BusinessArea
 
 
@@ -17,6 +17,33 @@ class LimitBusinessAreaModelMixin(models.Model):
     allowed_business_areas = models.ManyToManyField(to=BusinessArea)
 
     objects = LimitBusinessAreaModelManager()
+
+    class Meta:
+        abstract = True
+
+
+class RdiMergedModelManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(rdi_merge_status=RdiMergeStatusMixin.MERGED)
+
+
+class RdiImportedModelManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(rdi_merge_status=RdiMergeStatusMixin.PENDING)
+
+
+class RdiMergeStatusMixin(models.Model):
+    PENDING = "PENDING"
+    MERGED = "MERGED"
+    STATUS_CHOICE = (
+        (PENDING, _("Pending")),
+        (MERGED, _("Merged")),
+    )
+
+    rdi_merge_status = models.CharField(max_length=10, choices=STATUS_CHOICE, null=True)
+
+    objects = RdiMergedModelManager()
+    imported = RdiImportedModelManager()
 
     class Meta:
         abstract = True
