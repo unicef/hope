bb4215417a51174a66c7792bbd041c0435ae3b88
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-04-11 09:31:46 +0200

    add test get_version

---
+++ backend/hct_mis_api/apps/utils/tests/test_get_version.py
@@ -0,0 +1,22 @@
+from typing import Any
+from unittest.mock import mock_open, patch
+
+from django.test import TestCase
+
+from hct_mis_api import get_full_version
+
+
+class TestUtils(TestCase):
+    @patch("builtins.open", new_callable=mock_open, read_data='[project]\n version = "2.3.4"')
+    def test_get_full_version(self, mock_file_open: Any) -> None:
+        self.assertEqual(get_full_version(), "2.3.4")
+
+    @patch("builtins.open", side_effect=FileNotFoundError)
+    def test_file_not_found(self, mock_file_open: Any) -> None:
+        with self.assertRaises(FileNotFoundError):
+            get_full_version()
+
+    @patch("builtins.open", new_callable=mock_open, read_data='[project]\n wrong_key = "empty_version_XD"')
+    def test_key_not_found(self, mock_file_open: Any) -> None:
+        with self.assertRaises(KeyError):
+            get_full_version()
