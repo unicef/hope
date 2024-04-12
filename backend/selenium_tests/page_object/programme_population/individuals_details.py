478af6d53919ab56ee8f2d90227a3f5ff59e125a
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-04-12 08:23:55 +0200

    Add locators

--- backend/selenium_tests/page_object/programme_population/individuals_details.py
+++ backend/selenium_tests/page_object/programme_population/individuals_details.py
@@ -144,14 +144,8 @@ class IndividualsDetails(BaseComponents):
     def getLabelUnhcrId(self) -> WebElement:
         return self.wait_for(self.labelUnhcrId)
 
-    def getLabelIssued(self) -> WebElement:
-        return self.wait_for(self.labelIssued)
-
     def getLabelWfpId(self) -> WebElement:
         return self.wait_for(self.labelWfpId)
-
-    def getLabelIssued(self) -> WebElement:
-        return self.wait_for(self.labelIssued)
 
     def getLabelEmail(self) -> WebElement:
         return self.wait_for(self.labelEmail)
