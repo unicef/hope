085bbb26639c8e7976fb2fc0344c2e36c7d97454
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-04-17 15:11:09 +0200

    add typing

--- backend/tests/apps/api/test_cache.py
+++ backend/tests/apps/api/test_cache.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -6,31 +8,31 @@ from hct_mis_api.api.caches import etag_decorator
 
 class TestEtagDecorator:
     class DummyEtagConstructorClass:
-        def __init__(self, *args, **kwargs):
+        def __init__(self, *args: Any, **kwargs: Any) -> None:
             pass
 
-        def __call__(self, *args, **kwargs):
+        def __call__(self, *args: Any, **kwargs: Any) -> str:
             return "etag"
 
     class DummyRequest:
-        def __init__(self, headers: dict):
+        def __init__(self, headers: dict) -> None:
             self.headers = headers
 
     @etag_decorator(DummyEtagConstructorClass)
-    def dummy_view(self, request, *args, **kwargs):
+    def dummy_view(self, request: Any, *args: Any, **kwargs: Any) -> Response:
         return Response()
 
-    def test_etag_decorator_etag_exists(self):
+    def test_etag_decorator_etag_exists(self) -> None:
         response = self.dummy_view(self.DummyRequest({"ETAG": "etag"}), (1, 2, 3), {})
         assert response.status_code == status.HTTP_304_NOT_MODIFIED
         assert response.headers["ETAG"] == "etag"
 
-    def test_etag_decorator_etag_not_exists(self):
+    def test_etag_decorator_etag_not_exists(self) -> None:
         response = self.dummy_view(self.DummyRequest({}), (1, 2, 3), {})
         assert response.status_code == status.HTTP_200_OK
         assert response.headers["ETAG"] == "etag"
 
-    def test_etag_decorator_old_etag(self):
+    def test_etag_decorator_old_etag(self) -> None:
         response = self.dummy_view(self.DummyRequest({"ETAG": "old"}), (1, 2, 3), {})
         assert response.status_code == status.HTTP_200_OK
         assert response.headers["ETAG"] == "etag"
