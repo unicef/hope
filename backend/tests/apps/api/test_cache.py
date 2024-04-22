a572c677ddf1655824d3838a791e92378a8ef0bc
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-04-22 16:59:43 +0200

    add test

--- backend/tests/apps/api/test_cache.py
+++ backend/tests/apps/api/test_cache.py
@@ -1,9 +1,11 @@
 from typing import Any
 
+from django.core.cache import cache
+
 from rest_framework import status
 from rest_framework.response import Response
 
-from hct_mis_api.api.caches import etag_decorator
+from hct_mis_api.api.caches import ModelVersionKeyBitMixin, etag_decorator
 
 
 class TestEtagDecorator:
@@ -36,3 +38,13 @@ class TestEtagDecorator:
         response = self.dummy_view(self.DummyRequest({"If-None-Match": "old"}), (1, 2, 3), {})
         assert response.status_code == status.HTTP_200_OK
         assert response.headers["ETAG"] == "etag"
+
+
+class TestModelVersionKeyBitMixin:
+    def test_model_version_key_bit_mixin_key_not_in_the_cache(self) -> None:
+        assert ModelVersionKeyBitMixin.get_value_for_key("some-key") == "1"
+
+    def test_model_version_key_bit_mixin_key_in_the_cache(self) -> None:
+        key = "some-key"
+        cache.set(key, 3)
+        assert ModelVersionKeyBitMixin.get_value_for_key(key) == "3"
