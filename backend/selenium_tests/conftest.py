fc70d523edfe36153928ad797d65183184fa7e07
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-04-22 13:47:26 +0200

    Test CI

--- backend/selenium_tests/conftest.py
+++ backend/selenium_tests/conftest.py
@@ -41,74 +41,73 @@ def pytest_addoption(parser) -> None:  # type: ignore
 
 
 def pytest_configure() -> None:
-    if not request.node.get_closest_marker("local"):
-        from django.conf import settings
+    from django.conf import settings
 
-        settings.DEBUG = True
-        settings.ALLOWED_HOSTS = ["localhost", "127.0.0.1", "10.0.2.2", os.getenv("DOMAIN", "")]
-        settings.CELERY_TASK_ALWAYS_EAGER = True
+    settings.DEBUG = True
+    settings.ALLOWED_HOSTS = ["localhost", "127.0.0.1", "10.0.2.2", os.getenv("DOMAIN", "")]
+    settings.CELERY_TASK_ALWAYS_EAGER = True
 
-        settings.ELASTICSEARCH_INDEX_PREFIX = "test_"
-        settings.EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    settings.ELASTICSEARCH_INDEX_PREFIX = "test_"
+    settings.EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-        settings.EXCHANGE_RATE_CACHE_EXPIRY = 0
-        settings.USE_DUMMY_EXCHANGE_RATES = True
+    settings.EXCHANGE_RATE_CACHE_EXPIRY = 0
+    settings.USE_DUMMY_EXCHANGE_RATES = True
 
-        settings.SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
-        settings.CSRF_COOKIE_SECURE = False
-        settings.CSRF_COOKIE_HTTPONLY = False
-        settings.SESSION_COOKIE_SECURE = False
-        settings.SESSION_COOKIE_HTTPONLY = True
-        settings.SECURE_HSTS_SECONDS = False
-        settings.SECURE_CONTENT_TYPE_NOSNIFF = True
-        settings.SECURE_REFERRER_POLICY = "same-origin"
+    settings.SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
+    settings.CSRF_COOKIE_SECURE = False
+    settings.CSRF_COOKIE_HTTPONLY = False
+    settings.SESSION_COOKIE_SECURE = False
+    settings.SESSION_COOKIE_HTTPONLY = True
+    settings.SECURE_HSTS_SECONDS = False
+    settings.SECURE_CONTENT_TYPE_NOSNIFF = True
+    settings.SECURE_REFERRER_POLICY = "same-origin"
 
-        settings.CACHE_ENABLED = False
-        settings.CACHES = {
-            "default": {
-                "BACKEND": "hct_mis_api.apps.core.memcache.LocMemCache",
-                "TIMEOUT": 1800,
-            }
+    settings.CACHE_ENABLED = False
+    settings.CACHES = {
+        "default": {
+            "BACKEND": "hct_mis_api.apps.core.memcache.LocMemCache",
+            "TIMEOUT": 1800,
         }
+    }
 
-        settings.LOGGING["loggers"].update(
-            {
-                "": {"handlers": ["default"], "level": "DEBUG", "propagate": True},
-                "registration_datahub.tasks.deduplicate": {
-                    "handlers": ["default"],
-                    "level": "INFO",
-                    "propagate": True,
-                },
-                "sanction_list.tasks.check_against_sanction_list_pre_merge": {
-                    "handlers": ["default"],
-                    "level": "INFO",
-                    "propagate": True,
-                },
-                "graphql": {"handlers": ["default"], "level": "CRITICAL", "propagate": True},
-                "elasticsearch": {
-                    "handlers": ["default"],
-                    "level": "CRITICAL",
-                    "propagate": True,
-                },
-                "elasticsearch-dsl-django": {
-                    "handlers": ["default"],
-                    "level": "CRITICAL",
-                    "propagate": True,
-                },
-                "hct_mis_api.apps.registration_datahub.tasks.deduplicate": {
-                    "handlers": ["default"],
-                    "level": "CRITICAL",
-                    "propagate": True,
-                },
-                "hct_mis_api.apps.core.tasks.upload_new_template_and_update_flex_fields": {
-                    "handlers": ["default"],
-                    "level": "CRITICAL",
-                    "propagate": True,
-                },
-            }
-        )
+    settings.LOGGING["loggers"].update(
+        {
+            "": {"handlers": ["default"], "level": "DEBUG", "propagate": True},
+            "registration_datahub.tasks.deduplicate": {
+                "handlers": ["default"],
+                "level": "INFO",
+                "propagate": True,
+            },
+            "sanction_list.tasks.check_against_sanction_list_pre_merge": {
+                "handlers": ["default"],
+                "level": "INFO",
+                "propagate": True,
+            },
+            "graphql": {"handlers": ["default"], "level": "CRITICAL", "propagate": True},
+            "elasticsearch": {
+                "handlers": ["default"],
+                "level": "CRITICAL",
+                "propagate": True,
+            },
+            "elasticsearch-dsl-django": {
+                "handlers": ["default"],
+                "level": "CRITICAL",
+                "propagate": True,
+            },
+            "hct_mis_api.apps.registration_datahub.tasks.deduplicate": {
+                "handlers": ["default"],
+                "level": "CRITICAL",
+                "propagate": True,
+            },
+            "hct_mis_api.apps.core.tasks.upload_new_template_and_update_flex_fields": {
+                "handlers": ["default"],
+                "level": "CRITICAL",
+                "propagate": True,
+            },
+        }
+    )
 
-        logging.disable(logging.CRITICAL)
+    logging.disable(logging.CRITICAL)
     pytest.SELENIUM_PATH = os.path.dirname(__file__)
     pytest.CSRF = ""
     pytest.SESSION_ID = ""
