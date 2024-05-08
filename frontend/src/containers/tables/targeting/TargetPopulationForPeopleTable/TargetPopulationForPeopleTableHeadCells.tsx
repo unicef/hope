0758264c01c6129cfcc8198db69b5227453fa2b8
Jan Romaniak <jan.romaniak@kellton.com>
2024-05-08 21:17:15 +0200

    added changes on Targeting page

---
+++ frontend/src/containers/tables/targeting/TargetPopulationForPeopleTable/TargetPopulationForPeopleTableHeadCells.tsx
@@ -0,0 +1,54 @@
+import { HeadCell } from '@components/core/Table/EnhancedTableHead';
+import { TargetPopulationNode } from '@generated/graphql';
+
+export const headCells: HeadCell<TargetPopulationNode>[] = [
+  {
+    disablePadding: false,
+    label: '',
+    id: 'radio',
+    numeric: false,
+    dataCy: 'radio-id',
+  },
+  {
+    disablePadding: false,
+    label: 'Name',
+    id: 'name',
+    numeric: false,
+    dataCy: 'name',
+  },
+  {
+    disablePadding: false,
+    label: 'Status',
+    id: 'status',
+    numeric: false,
+    dataCy: 'status',
+  },
+  {
+    disablePadding: false,
+    label: 'Num. of People',
+    id: 'total_households_count',
+    numeric: false,
+    dataCy: 'num-of-households',
+  },
+  {
+    disablePadding: false,
+    label: 'Date Created',
+    id: 'created_at',
+    numeric: false,
+    dataCy: 'date-created',
+  },
+  {
+    disablePadding: false,
+    label: 'Last Edited',
+    id: 'updated_at',
+    numeric: false,
+    dataCy: 'last-edited',
+  },
+  {
+    disablePadding: false,
+    label: 'Created by',
+    id: 'created_by',
+    numeric: false,
+    dataCy: 'created-by',
+  },
+];
