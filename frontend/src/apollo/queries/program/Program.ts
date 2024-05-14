6a4a2b0e87e83ef3a11a25f2216a2d12a8d60250
Patryk Dabrowski <patryk.dabrowski@kellton.com>
2024-05-14 10:11:33 +0200

    Not possible to finish active program (#3846)

--- frontend/src/apollo/queries/program/Program.ts
+++ frontend/src/apollo/queries/program/Program.ts
@@ -3,47 +3,7 @@ import { gql } from '@apollo/client';
 export const PROGRAM_QUERY = gql`
   query Program($id: ID!) {
     program(id: $id) {
-      id
-      name
-      programmeCode
-      startDate
-      endDate
-      status
-      caId
-      caHashId
-      description
-      budget
-      frequencyOfPayments
-      cashPlus
-      populationGoal
-      scope
-      sector
-      totalNumberOfHouseholds
-      totalNumberOfHouseholdsWithTpInProgram
-      administrativeAreasOfImplementation
-      isSocialWorkerProgram
-      version
-      adminUrl
-      dataCollectingType {
-        id
-        code
-        label
-        active
-        individualFiltersAvailable
-        householdFiltersAvailable
-        description
-        type
-      }
-      partnerAccess
-      partners {
-        id
-        name
-        areaAccess
-        areas {
-          id
-          level
-        }
-      }
+      ...programDetails
     }
   }
 `;
