1c00a3e4a74c6da068bb5e5eaf2e938d8a81bc50
szymon-kellton <130459593+szymon-kellton@users.noreply.github.com>
2024-05-14 13:04:06 +0200

    Selenium: Targeting smoke tests (#3822)
    * Init
    
    * Added test_smoke_targeting_details_page
    
    * Added creation page smoke tests
    
    * Edited ci.yml
    
    * Black
    
    * Fixes after conflicts
    
    * Update ci.yml
    
    * Upgrade helper.py
    
    * mypy fix

--- backend/selenium_tests/page_object/targeting/create_new.py
+++ backend/selenium_tests/page_object/targeting/create_new.py
@@ -2,20 +2,7 @@ from page_object.base_components import BaseComponents
 from selenium.webdriver.remote.webelement import WebElement
 
 
-class TargetingCreate(BaseComponents):
-    # Locators
-    targetingCriteria = 'h6[data-cy="title-targeting-criteria"]'
-    addCriteriaButton = 'div[data-cy="button-target-population-add-criteria"]'
-    addHouseholdRuleButton = '[data-cy="button-household-rule"]'
-    addIndividualRuleButton = '[data-cy="button-individual-rule"]'
-    addPeopleRuleButton = '[data-cy="button-household-rule"]'
-    titlePage = 'h5[data-cy="page-header-title"]'
-    fieldName = 'input[data-cy="input-name"]'
-    targetingCriteriaAutoComplete = 'input[data-cy="autocomplete-target-criteria-option-{}"]'
-    targetingCriteriaValue = '[data-cy="select-filters[{}].value"]'
-    targetingCriteriaAddDialogSaveButton = 'button[data-cy="button-target-population-add-criteria"]'
-    criteriaContainer = 'div[data-cy="criteria-container"]'
-    targetPopulationSaveButton = 'button[data-cy="button-target-population-create"]'
+class CreateNew(BaseComponents):
     pageHeaderContainer = 'div[data-cy="page-header-container"]'
     pageHeaderTitle = 'h5[data-cy="page-header-title"]'
     buttonTargetPopulationCreate = 'button[data-cy="button-target-population-create"]'
@@ -26,7 +13,12 @@ class TargetingCreate(BaseComponents):
     inputIndividualids = 'input[data-cy="input-individualIds"]'
     inputFlagexcludeifactiveadjudicationticket = 'span[data-cy="input-flagExcludeIfActiveAdjudicationTicket"]'
     inputName = 'input[data-cy="input-name"]'
+
+    pageHeaderContainer = 'div[data-cy="page-header-container"]'
+    pageHeaderTitle = 'h5[data-cy="page-header-title"]'
+    buttonTargetPopulationCreate = 'button[data-cy="button-target-population-create"]'
     divTargetPopulationAddCriteria = 'div[data-cy="button-target-population-add-criteria"]'
+    inputFlagexcludeifactiveadjudicationticket = 'span[data-cy="input-flagExcludeIfActiveAdjudicationTicket"]'
     titleExcludedEntries = 'h6[data-cy="title-excluded-entries"]'
     buttonShowHideExclusions = 'button[data-cy="button-show-hide-exclusions"]'
     inputExcludedIds = 'div[data-cy="input-excluded-ids"]'
@@ -39,8 +31,6 @@ class TargetingCreate(BaseComponents):
     buttonHouseholdRule = 'button[data-cy="button-household-rule"]'
     buttonIndividualRule = 'button[data-cy="button-individual-rule"]'
     buttonTargetPopulationAddCriteria = 'button[data-cy="button-target-population-add-criteria"]'
-    # Texts
-    textTargetingCriteria = "Targeting Criteria"
 
     def getPageHeaderTitle(self) -> WebElement:
         return self.wait_for(self.pageHeaderTitle)
@@ -95,39 +85,3 @@ class TargetingCreate(BaseComponents):
 
     def getAutocompleteTargetCriteriaOption(self) -> WebElement:
         return self.wait_for(self.autocompleteTargetCriteriaOption)
-
-    def getTargetingCriteria(self) -> WebElement:
-        return self.wait_for(self.targetingCriteria)
-
-    def getTitlePage(self) -> WebElement:
-        return self.wait_for(self.titlePage)
-
-    def getAddCriteriaButton(self) -> WebElement:
-        return self.wait_for(self.addCriteriaButton)
-
-    def getAddHouseholdRuleButton(self) -> WebElement:
-        return self.wait_for(self.addHouseholdRuleButton)
-
-    def getAddIndividualRuleButton(self) -> WebElement:
-        return self.wait_for(self.addIndividualRuleButton)
-
-    def getAddPeopleRuleButton(self) -> WebElement:
-        return self.wait_for(self.addPeopleRuleButton)
-
-    def getTargetingCriteriaAutoComplete(self, index: int = 0) -> WebElement:
-        return self.wait_for(self.targetingCriteriaAutoComplete.format(index))
-
-    def getTargetingCriteriaValue(self, index: int = 0) -> WebElement:
-        return self.wait_for(self.targetingCriteriaValue.format(index))
-
-    def getTargetingCriteriaAddDialogSaveButton(self) -> WebElement:
-        return self.wait_for(self.targetingCriteriaAddDialogSaveButton)
-
-    def getCriteriaContainer(self) -> WebElement:
-        return self.wait_for(self.criteriaContainer)
-
-    def getFieldName(self) -> WebElement:
-        return self.wait_for(self.fieldName)
-
-    def getTargetPopulationSaveButton(self) -> WebElement:
-        return self.wait_for(self.targetPopulationSaveButton)
