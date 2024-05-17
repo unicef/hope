104f06e6f9a910bf1f26e3e9eee359b805ebadc9
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-05-17 07:07:25 +0200

    Fix for drawer tests and feedback tests

--- backend/selenium_tests/filters/test_filters.py
+++ backend/selenium_tests/filters/test_filters.py
@@ -1,11 +1,9 @@
-from time import sleep
-
 from django.conf import settings
 from django.core.management import call_command
 
 import pytest
 from page_object.filters import Filters
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 pytestmark = pytest.mark.django_db(transaction=True)
 
@@ -18,7 +16,7 @@ def create_programs() -> None:
 
 
 class TestSmokeFilters:
-    @pytest.mark.skip()
+
     def test_filters_all_programs(self, login, create_programs, filters: Filters):
         all_programs = {
             "Country Dashboard": [filters.globalProgramFilter, filters.globalProgramFilterContainer],
@@ -90,15 +88,11 @@ class TestSmokeFilters:
             if nav_menu == "Feedback":
                 filters.wait_for(f'[data-cy="nav-Grievance"]').click()
             filters.wait_for(f'[data-cy="nav-{nav_menu}"]').click()
-            filters.wait_for(all_programs[nav_menu][0])
-            ids = filters.driver.find_elements(By.XPATH, f"//*[@data-cy]")
-            list_locators = []
-            for i in ids:
-                if "button-filters-apply" == i.get_attribute("data-cy"):
-                    break
-                list_locators.append(f'{i.tag_name}[data-cy="{i.get_attribute("data-cy")}"]')
             for locator in all_programs[nav_menu]:
-                assert locator in list_locators
+                try:
+                    filters.wait_for(locator, timeout=20)
+                except BaseException:
+                    raise Exception(f"Element {locator} not found on the {nav_menu} page.")
 
     def test_filters_selected_program(self, login, create_programs, filters: Filters):
         filters.selectGlobalProgramFilter("Test Programm").click()
@@ -205,7 +199,6 @@ class TestSmokeFilters:
             "Accountability": [
                 filters.filtersTargetPopulationAutocomplete,
                 filters.targetPopulationInput,
-                filters.filtersCreatedByAutocomplete,
                 filters.createdByInput,
                 filters.filtersCreationDateFrom,
                 filters.filtersCreationDateTo
@@ -214,12 +207,11 @@ class TestSmokeFilters:
                 filters.filtersSearch,
                 filters.filtersTargetPopulationAutocomplete,
                 filters.targetPopulationInput,
-                filters.filtersCreatedByAutocomplete,
                 filters.createdByInput,
                 filters.filtersCreationDateFrom,
                 filters.filtersCreationDateTo,
             ],
-            # "Programme Users": [],
+            "Programme Users": [],
             "Program Log": [
                 filters.filtersSearch,
                 filters.selectFilter,
@@ -236,20 +228,9 @@ class TestSmokeFilters:
             if nav_menu == "Surveys":
                 filters.wait_for(f'[data-cy="nav-Accountability"]').click()
             filters.wait_for(f'[data-cy="nav-{nav_menu}"]').click()
-            # filters.wait_for(programs[nav_menu][0], timeout=20)
-            sleep(10)
-            ids = filters.driver.find_elements(By.XPATH, f"//*[@data-cy]")
-            print(f"---------------{nav_menu}---------------")
-            list_locators = []
-            for i in ids:
-                if "button-filters-clear" == i.get_attribute("data-cy"):
-                    break
-                data_cy_attribute = i.get_attribute("data-cy")  # type: ignore
-                var_name = [i.capitalize() for i in data_cy_attribute.lower().replace("-", " ").split(" ")]
-                var_name[0] = var_name[0].lower()
-                var_name = "".join(var_name)  # type: ignore
-                print(f"filters.{var_name}, ")
-                list_locators.append(f'{i.tag_name}[data-cy="{i.get_attribute("data-cy")}"]')
-            print(list_locators)
             for locator in programs[nav_menu]:
-                assert locator in list_locators
+                try:
+                    filters.wait_for(locator, timeout=20)
+                except BaseException:
+                    raise Exception(f"Element {locator} not found on the {nav_menu} page.")
+
