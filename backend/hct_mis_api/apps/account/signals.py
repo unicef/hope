6995ba00edaeef98c3481b9211465b777d9014bb
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-05-20 23:38:04 +0200

    fix imports

--- backend/hct_mis_api/apps/account/signals.py
+++ backend/hct_mis_api/apps/account/signals.py
@@ -1,9 +1,9 @@
-from datetime import timezone
 from typing import Any
 
 from django.contrib.auth import get_user_model
 from django.db.models.signals import m2m_changed, post_save, pre_delete, pre_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 from hct_mis_api.apps.account.models import Partner, Role, User, UserRole
 from hct_mis_api.apps.core.models import BusinessArea
