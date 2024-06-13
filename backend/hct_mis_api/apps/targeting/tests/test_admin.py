cdfcea8dc40b208e96c2a4584f22f623cc2b326e
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-06-13 12:05:12 +0200

    review

--- backend/hct_mis_api/apps/targeting/tests/test_admin.py
+++ backend/hct_mis_api/apps/targeting/tests/test_admin.py
@@ -13,14 +13,14 @@ class HouseholdSelectionAdminTest(BaseTest):
 
         # removed tp
         url = f"{base_url}?&target_population__exact={str(targeting_removed.id)}"
-        res = self.app.get(url, user=self.superuser)  # noqa: F841
+        res = self.app.get(url, user=self.superuser)
         self.assertEqual(res.status_code, 200)
 
         url = f"{base_url}?&target_population__exact={str(targeting_1.id)}"
-        res = self.app.get(url, user=self.superuser)  # noqa: F841
+        res = self.app.get(url, user=self.superuser)
         self.assertEqual(res.status_code, 200)
 
         # invalid id
         url = f"{base_url}?&target_population__exact=00000000-1111-0000-1111-faceb00c1111"
-        res = self.app.get(url, user=self.superuser)  # noqa: F841
+        res = self.app.get(url, user=self.superuser)
         self.assertEqual(res.status_code, 200)
