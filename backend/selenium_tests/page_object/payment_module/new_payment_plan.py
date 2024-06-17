779586a3a31e9906a668df18693663558a9e7062
szymon-kellton <130459593+szymon-kellton@users.noreply.github.com>
2024-06-17 10:08:47 +0200

    Selenium: payment module - smoke test (#3964)
    * Init
    
    * Init
    
    * Added test_smoke_payment_plan
    
    * add dataCy payment plan parameters
    
    * Test
    
    * Added test_smoke_details_payment_plan
    
    * Added test_smoke_details_payment_plan
    
    * Added test_smoke_details_payment_plan
    
    * Update backend/selenium_tests/payment_module/test_payment_module.py
    
    Co-authored-by: Patryk Dabrowski <patryk.dabrowski@kellton.com>
    
    * Fix after review
    
    ---------
    
    Co-authored-by: Maciej Szewczyk <maciej.szewczyk@tivix.com>
    Co-authored-by: Patryk Dabrowski <patryk.dabrowski@kellton.com>

--- backend/selenium_tests/page_object/payment_module/new_payment_plan.py
+++ backend/selenium_tests/page_object/payment_module/new_payment_plan.py
@@ -1,8 +1,45 @@
 from page_object.base_components import BaseComponents
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class NewPaymentPlan(BaseComponents):
-    pass
-    # Locators
-    # Texts
-    # Elements
+    pageHeaderContainer = 'div[data-cy="page-header-container"]'
+    pageHeaderTitle = 'h5[data-cy="page-header-title"]'
+    buttonSavePaymentPlan = 'button[data-cy="button-save-payment-plan"]'
+    inputTargetPopulation = 'div[data-cy="input-target-population"]'
+    selectTargetingid = 'div[data-cy="select-targetingId"]'
+    inputStartDate = 'div[data-cy="input-start-date"]'
+    inputEndDate = 'div[data-cy="input-end-date"]'
+    inputCurrency = 'div[data-cy="input-currency"]'
+    inputDispersionStartDate = 'div[data-cy="input-dispersion-start-date"]'
+    inputDispersionEndDate = 'div[data-cy="input-dispersion-end-date"]'
+
+    def getPageHeaderContainer(self) -> WebElement:
+        return self.wait_for(self.pageHeaderContainer)
+
+    def getPageHeaderTitle(self) -> WebElement:
+        return self.wait_for(self.pageHeaderTitle)
+
+    def getButtonSavePaymentPlan(self) -> WebElement:
+        return self.wait_for(self.buttonSavePaymentPlan)
+
+    def getInputTargetPopulation(self) -> WebElement:
+        return self.wait_for(self.inputTargetPopulation)
+
+    def getSelectTargetingid(self) -> WebElement:
+        return self.wait_for(self.selectTargetingid)
+
+    def getInputStartDate(self) -> WebElement:
+        return self.wait_for(self.inputStartDate)
+
+    def getInputEndDate(self) -> WebElement:
+        return self.wait_for(self.inputEndDate)
+
+    def getInputCurrency(self) -> WebElement:
+        return self.wait_for(self.inputCurrency)
+
+    def getInputDispersionStartDate(self) -> WebElement:
+        return self.wait_for(self.inputDispersionStartDate)
+
+    def getInputDispersionEndDate(self) -> WebElement:
+        return self.wait_for(self.inputDispersionEndDate)
