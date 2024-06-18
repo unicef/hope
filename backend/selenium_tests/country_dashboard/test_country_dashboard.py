70e1acec4efb68d35941dc9ab0e2601714f5ffab
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-06-18 14:47:53 +0200

    Init

--- backend/selenium_tests/country_dashboard/test_country_dashboard.py
+++ backend/selenium_tests/country_dashboard/test_country_dashboard.py
@@ -1,1 +1,14 @@
 import pytest
+from page_object.filters import Filters
+
+pytestmark = pytest.mark.django_db(transaction=True)
+
+
+@pytest.mark.usefixtures("login")
+class TestSmokeCountryDashboard:
+    def test_smoke_country_dashboard(self, filters: Filters) -> None:
+        filters.getNavCountryDashboard().click()
+        from selenium_tests.tools.tag_name_finder import printing
+        printing("Mapping", filters.driver)
+        printing("Methods", filters.driver)
+        printing("Mapping", filters.driver)
