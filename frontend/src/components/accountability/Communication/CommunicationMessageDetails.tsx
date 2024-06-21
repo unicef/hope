0ffd469bb6bbfb69794a6e374d121b328c527cce
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2024-06-21 13:47:41 +0200

    add data-cy to communication details + minor layout fixes

--- frontend/src/components/accountability/Communication/CommunicationMessageDetails.tsx
+++ frontend/src/components/accountability/Communication/CommunicationMessageDetails.tsx
@@ -19,34 +19,36 @@ interface CommunicationMessageDetailsProps {
   message: AccountabilityCommunicationMessageQuery['accountabilityCommunicationMessage'];
 }
 
-export function CommunicationMessageDetails({
+export const CommunicationMessageDetails = ({
   message,
-}: CommunicationMessageDetailsProps): React.ReactElement {
+}: CommunicationMessageDetailsProps): React.ReactElement => {
   const { t } = useTranslation();
   return (
-    <Grid item xs={8}>
+    <Grid item xs={8} data-cy="communication-message-details">
       <Box p={5}>
         <StyledBox>
           <Title>
-            <Typography variant="h6">{t('Message')}</Typography>
+            <Typography variant="h6" data-cy="message-title">
+              {t('Message')}
+            </Typography>
           </Title>
           <OverviewContainer>
             <Grid container spacing={6}>
               <Grid item container justifyContent="space-between" xs={12}>
-                <Typography variant="subtitle2">
-                  {renderUserName(message.createdBy)}
-                </Typography>
-                <Typography color="textSecondary">
+                <Box ml={2}>
+                  <Typography variant="subtitle2" data-cy="message-created-by">
+                    {renderUserName(message.createdBy)}
+                  </Typography>
+                </Box>
+                <Typography color="textSecondary" data-cy="message-created-at">
                   <UniversalMoment withTime>
                     {message.createdAt}
                   </UniversalMoment>
                 </Typography>
               </Grid>
-              <Box px={3}>
-                <div>{message.title}</div>
-                <Box py={3}>
-                  <div>{message.body}</div>
-                </Box>
+              <Box ml={6} p={3}>
+                <Box data-cy="message-title-content">{message.title}</Box>
+                <Box data-cy="message-body-content">{message.body}</Box>
               </Box>
             </Grid>
           </OverviewContainer>
@@ -54,4 +56,4 @@ export function CommunicationMessageDetails({
       </Box>
     </Grid>
   );
-}
+};
