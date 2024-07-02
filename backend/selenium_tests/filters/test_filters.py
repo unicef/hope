d5dbfa35f7df32cdbf1a578ce258f69c779c725b
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-07-02 08:21:56 +0200

    Black & Isort

--- backend/selenium_tests/filters/test_filters.py
+++ backend/selenium_tests/filters/test_filters.py
@@ -1,7 +1,7 @@
+from datetime import datetime
+
 from django.conf import settings
 from django.core.management import call_command
-
-from datetime import datetime
 
 import pytest
 from dateutil.relativedelta import relativedelta
@@ -10,13 +10,15 @@ from page_object.filters import Filters
 from hct_mis_api.apps.account.models import User
 from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.geo.models import Area
+from hct_mis_api.apps.grievance.fixtures import GrievanceTicketFactory
+from hct_mis_api.apps.grievance.models import GrievanceTicket
 from hct_mis_api.apps.household.fixtures import create_household
 from hct_mis_api.apps.payment.fixtures import (
     CashPlanFactory,
     PaymentRecordFactory,
     PaymentVerificationPlanFactory,
 )
-from hct_mis_api.apps.payment.models import PaymentVerificationPlan
+from hct_mis_api.apps.payment.models import PaymentPlan, PaymentVerificationPlan
 from hct_mis_api.apps.program.models import Program
 from hct_mis_api.apps.registration_data.fixtures import RegistrationDataImportFactory
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
@@ -29,29 +31,16 @@ from hct_mis_api.apps.targeting.fixtures import (
     TargetingCriteriaFactory,
     TargetPopulationFactory,
 )
+from hct_mis_api.apps.targeting.models import TargetPopulation
 from selenium_tests.page_object.programme_details.programme_details import (
     ProgrammeDetails,
 )
-from hct_mis_api.apps.targeting.models import TargetPopulation
-from hct_mis_api.apps.payment.models import PaymentPlan
-
 
 pytestmark = pytest.mark.django_db(transaction=True, databases=["registration_datahub", "default"])
 
 
 @pytest.fixture
 def create_payment_plan() -> None:
-    TargetPopulationFactory(
-        program=Program.objects.filter(name="Test Programm").first(),
-        status=TargetPopulation.STATUS_OPEN,
-        targeting_criteria=TargetingCriteriaFactory(),
-    )
-    TargetPopulationFactory(
-        program=Program.objects.filter(name="Test Programm").first(),
-        status=TargetPopulation.STATUS_OPEN,
-        targeting_criteria=TargetingCriteriaFactory(),
-    )
-
     tp = TargetPopulation.objects.all()[0]
     tp2 = TargetPopulation.objects.all()[1]
 
@@ -96,6 +85,79 @@ def create_payment_plan() -> None:
 
 
 @pytest.fixture
+def add_grievance_tickets() -> None:
+    create_grievance("GRV-0000123")
+    create_grievance("GRV-0000666")
+
+
+def create_grievance(name: str, program: str = "Test Programm", business_area_slug: str = "afghanistan") -> None:
+    business_area = BusinessArea.objects.get(slug=business_area_slug)
+    grievance = GrievanceTicketFactory(
+        category=GrievanceTicket.CATEGORY_DATA_CHANGE,
+        issue_type=GrievanceTicket.ISSUE_TYPE_INDIVIDUAL_DATA_CHANGE_DATA_UPDATE,
+        business_area=business_area,
+        status=GrievanceTicket.STATUS_FOR_APPROVAL,
+    )
+    grievance.programs.add(Program.objects.filter(name=program).first())
+    grievance.unicef_id = name
+    grievance.save()
+
+
+def generate_grievance(
+    unicef_id: str,
+    status: int = GrievanceTicket.STATUS_NEW,
+    category: int = GrievanceTicket.CATEGORY_REFERRAL,
+    created_by: User | None = None,
+    assigned_to: User | None = None,
+    business_area: BusinessArea | None = None,
+    priority: int = 1,
+    urgency: int = 1,
+    household_unicef_id: str = "HH-20-0000.0001",
+    updated_at: str = "2023-09-27T11:26:33.846Z",
+    created_at: str = "2022-04-30T09:54:07.827000",
+) -> None:
+    created_by = User.objects.first() if created_by is None else created_by
+    assigned_to = User.objects.first() if assigned_to is None else assigned_to
+    business_area = BusinessArea.objects.filter(slug="afghanistan").first() if business_area is None else business_area
+    GrievanceTicket.objects.create(
+        **{
+            "business_area": business_area,
+            "unicef_id": unicef_id,
+            "language": "Polish",
+            "consent": True,
+            "description": "grievance_ticket_1",
+            "category": category,
+            "status": status,
+            "created_by": created_by,
+            "assigned_to": assigned_to,
+            "created_at": created_at,
+            "updated_at": updated_at,
+            "household_unicef_id": household_unicef_id,
+            "priority": priority,
+            "urgency": urgency,
+        }
+    )
+
+
+@pytest.fixture
+def add_household() -> None:
+    registration_data_import = RegistrationDataImportFactory(
+        imported_by=User.objects.first(), business_area=BusinessArea.objects.first()
+    )
+    household, _ = create_household(
+        {
+            "registration_data_import": registration_data_import,
+            "admin_area": Area.objects.order_by("?").first(),
+            "program": Program.objects.filter(name="Test Programm").first(),
+        },
+        {"registration_data_import": registration_data_import},
+    )
+
+    household.unicef_id = "HH-00-0000.1380"
+    household.save()
+
+
+@pytest.fixture
 def add_payment_verification() -> None:
     registration_data_import = RegistrationDataImportFactory(
         imported_by=User.objects.first(), business_area=BusinessArea.objects.first()
@@ -122,13 +184,8 @@ def add_payment_verification() -> None:
         program=program,
         business_area=BusinessArea.objects.first(),
     )
-    targeting_criteria = TargetingCriteriaFactory()
 
-    target_population = TargetPopulationFactory(
-        created_by=User.objects.first(),
-        targeting_criteria=targeting_criteria,
-        business_area=BusinessArea.objects.first(),
-    )
+    target_population = TargetPopulation.objects.first()
 
     PaymentRecordFactory(
         parent=cash_plan,
@@ -225,83 +282,6 @@ def create_programs() -> None:
 
 @pytest.mark.usefixtures("login")
 class TestSmokeFilters:
-    def test_filters_all_programs(self, create_programs: None, filters: Filters) -> None:
-        all_programs = {
-            "Country Dashboard": [filters.globalProgramFilter, filters.globalProgramFilterContainer],
-            "Programs": [
-                filters.filtersDataCollectingType,
-                filters.filtersBudgetMax,
-                filters.filtersBudgetMin,
-                filters.filtersNumberOfHouseholdsMin,
-                filters.filtersNumberOfHouseholdsMax,
-                filters.filtersSector,
-                filters.filtersStartDate,
-                filters.filtersEndDate,
-                filters.filtersStatus,
-                filters.filtersSearch,
-            ],
-            "Grievance": [
-                filters.filtersSearch,
-                filters.selectFilter,
-                filters.filtersDocumentType,
-                filters.filtersDocumentNumber,
-                filters.filtersProgram,
-                filters.programmeInput,
-                filters.selectFilter,
-                filters.filtersStatus,
-                filters.filtersFsp,
-                filters.filtersCreationDateFrom,
-                filters.filtersCreationDateTo,
-                filters.selectFilter,
-                filters.filtersCategory,
-                filters.filtersAdminLevel,
-                filters.filtersAssignee,
-                filters.assignedToInput,
-                filters.filtersCreatedByAutocomplete,
-                filters.filtersRegistrationDataImport,
-                filters.filtersPreferredLanguage,
-                filters.filtersPriority,
-                filters.filtersUrgency,
-                filters.filtersActiveTickets,
-                filters.filtersProgramState,
-            ],
-            "Feedback": [
-                filters.filtersSearch,
-                filters.filtersProgram,
-                filters.programmeInput,
-                filters.selectFilter,
-                filters.filtersIssueType,
-                filters.filtersCreatedByAutocomplete,
-                filters.filtersCreationDateFrom,
-                filters.filtersCreationDateTo,
-                filters.selectFilter,
-                filters.filtersProgramState,
-            ],
-            "Reporting": [
-                filters.reportOnlyMyFilter,
-                filters.reportStatusFilter,
-                filters.reportCreatedToFilter,
-                filters.reportCreatedFromFilter,
-                filters.reportTypeFilter,
-            ],
-            "Activity Log": [
-                filters.filtersResidenceStatus,
-                filters.filtersSearch,
-                filters.userInput,
-                filters.selectFilter,
-            ],
-        }
-
-        for nav_menu in all_programs:
-            if nav_menu == "Feedback":
-                filters.wait_for('[data-cy="nav-Grievance"]').click()
-            filters.wait_for(f'[data-cy="nav-{nav_menu}"]').click()
-            for locator in all_programs[nav_menu]:
-                try:
-                    filters.wait_for(locator, timeout=20)
-                except BaseException:
-                    raise Exception(f"Element {locator} not found on the {nav_menu} page.")
-
     def test_filters_selected_program(self, create_programs: None, filters: Filters) -> None:
         filters.selectGlobalProgramFilter("Test Programm").click()
 
@@ -442,25 +422,105 @@ class TestSmokeFilters:
                 except BaseException:
                     raise Exception(f"Element {locator} not found on the {nav_menu} page.")
 
+    def test_filters_all_programs(self, create_programs: None, filters: Filters) -> None:
+        all_programs = {
+            "Country Dashboard": [filters.globalProgramFilter, filters.globalProgramFilterContainer],
+            "Programs": [
+                filters.filtersDataCollectingType,
+                filters.filtersBudgetMax,
+                filters.filtersBudgetMin,
+                filters.filtersNumberOfHouseholdsMin,
+                filters.filtersNumberOfHouseholdsMax,
+                filters.filtersSector,
+                filters.filtersStartDate,
+                filters.filtersEndDate,
+                filters.filtersStatus,
+                filters.filtersSearch,
+            ],
+            "Grievance": [
+                filters.filtersSearch,
+                filters.selectFilter,
+                filters.filtersDocumentType,
+                filters.filtersDocumentNumber,
+                filters.filtersProgram,
+                filters.programmeInput,
+                filters.selectFilter,
+                filters.filtersStatus,
+                filters.filtersFsp,
+                filters.filtersCreationDateFrom,
+                filters.filtersCreationDateTo,
+                filters.selectFilter,
+                filters.filtersCategory,
+                filters.filtersAdminLevel,
+                filters.filtersAssignee,
+                filters.assignedToInput,
+                filters.filtersCreatedByAutocomplete,
+                filters.filtersRegistrationDataImport,
+                filters.filtersPreferredLanguage,
+                filters.filtersPriority,
+                filters.filtersUrgency,
+                filters.filtersActiveTickets,
+                filters.filtersProgramState,
+            ],
+            "Feedback": [
+                filters.filtersSearch,
+                filters.filtersProgram,
+                filters.programmeInput,
+                filters.selectFilter,
+                filters.filtersIssueType,
+                filters.filtersCreatedByAutocomplete,
+                filters.filtersCreationDateFrom,
+                filters.filtersCreationDateTo,
+                filters.selectFilter,
+                filters.filtersProgramState,
+            ],
+            "Reporting": [
+                filters.reportOnlyMyFilter,
+                filters.reportStatusFilter,
+                filters.reportCreatedToFilter,
+                filters.reportCreatedFromFilter,
+                filters.reportTypeFilter,
+            ],
+            "Activity Log": [
+                filters.filtersResidenceStatus,
+                filters.filtersSearch,
+                filters.userInput,
+                filters.selectFilter,
+            ],
+        }
+
+        for nav_menu in all_programs:
+            if nav_menu == "Feedback":
+                filters.wait_for('[data-cy="nav-Grievance"]').click()
+            filters.wait_for(f'[data-cy="nav-{nav_menu}"]').click()
+            for locator in all_programs[nav_menu]:
+                try:
+                    filters.wait_for(locator, timeout=20)
+                except BaseException:
+                    raise Exception(f"Element {locator} not found on the {nav_menu} page.")
+
     @pytest.mark.parametrize(
         "module",
         [
             pytest.param(["Registration Data Import", "filter-search", "Test"], id="Registration Data Import"),
-            # pytest.param(["Program Population", "hh-filters-search", "search"], id="Program Population"),
             pytest.param(["Targeting", "filters-search", "Test"], id="Targeting"),
             pytest.param(["Payment Module", "filter-search", "PP-0060-22-11223344"], id="Payment Module"),
             pytest.param(["Payment Verification", "filter-search", "PP-0000-00-11223344"], id="Payment Verification"),
-            # pytest.param(["Grievance", "filters-search", "search"], id="Grievance"),
+            pytest.param(["Grievance", "filters-search", "GRV-0000123"], id="Grievance"),
+            # ToDo: uncomment after fix bug: 206395
+            # pytest.param(["Program Population", "hh-filters-search", "HH-00-0000.1380"], id="Program Population"),
         ],
     )
     def test_filters_happy_path_search_filter(
         self,
         module: list,
         create_programs: None,
-        create_rdi: None,
         create_targeting: None,
+        create_rdi: None,
         add_payment_verification: None,
         create_payment_plan: None,
+        add_household: None,
+        add_grievance_tickets: None,
         filters: Filters,
         pageProgrammeDetails: ProgrammeDetails,
     ) -> None:
