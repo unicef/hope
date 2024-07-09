2d57cf1f3fdf628808170de0a4d3ebb228f28309
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-07-09 21:12:54 +0200

    change files structure and import statemets

--- backend/hct_mis_api/api/endpoints/core/serializers.py
+++ backend/hct_mis_api/api/endpoints/core/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from rest_framework.generics import ListAPIView
 
-from hct_mis_api.api.endpoints.base import HOPEAPIView
 from hct_mis_api.apps.core.models import BusinessArea
 
 
@@ -18,8 +16,3 @@ class BusinessAreaSerializer(serializers.ModelSerializer):
             "is_split",
             "active",
         )
-
-
-class BusinessAreaListView(HOPEAPIView, ListAPIView):
-    serializer_class = BusinessAreaSerializer
-    queryset = BusinessArea.objects.all()
--- backend/hct_mis_api/api/endpoints/program/__init__.py
+++
@@ -1,3 +0,0 @@
-from hct_mis_api.api.endpoints.program.program import (  # noqa: F401
-    ProgramGlobalListView,
-)
