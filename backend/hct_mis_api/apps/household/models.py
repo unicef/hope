75aaf243253046c482c8828ab87874411201bb08
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-07-11 19:38:54 +0200

    small fixes

--- backend/hct_mis_api/apps/household/models.py
+++ backend/hct_mis_api/apps/household/models.py
@@ -1164,6 +1164,14 @@ class Individual(
         self.duplicate_date = timezone.now()
         self.save()
 
+    def mark_as_distinct(self, original_individual: Optional["Individual"] = None) -> None:
+        if original_individual is not None:
+            self.unicef_id = str(original_individual.unicef_id)
+        self.documents.update(status=Document.STATUS_VALID)
+        self.duplicate = False
+        self.duplicate_date = timezone.now()
+        self.save()
+
     def set_relationship_confirmed_flag(self, confirmed: bool) -> None:
         self.relationship_confirmed = confirmed
         self.save(update_fields=["relationship_confirmed"])
