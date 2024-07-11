d47025a15133c147bbb1e415d522ae0c0de1c146
Jan Romaniak <jan.romaniak@kellton.com>
2024-07-11 10:09:41 +0200

    generating workbook

--- backend/hct_mis_api/apps/periodic_data_update/service/PeriodicDataUpdateTemplateService.py
+++ backend/hct_mis_api/apps/periodic_data_update/service/PeriodicDataUpdateTemplateService.py
@@ -1,3 +1,4 @@
+import openpyxl
 from django.db.models import Q
 
 from hct_mis_api.apps.core.attributes_qet_queries import age_to_birth_date_query
@@ -19,8 +20,12 @@ from hct_mis_api.apps.periodic_data_update.models import PeriodicDataUpdateTempl
 
 
 class PeriodicDataUpdateExportTemplateService:
+    PDU_SHEET = "Timeseries Export List"
+    META_SHEET = "Meta"
+
     def __init__(self, periodic_data_update_template: PeriodicDataUpdateTemplate):
         self.periodic_data_update_template = periodic_data_update_template
+        self.round_data = periodic_data_update_template.rounds_data
         self.program = periodic_data_update_template.program
         self.registration_data_import_id_filter = periodic_data_update_template.filters.get(
             "registration_data_import_id"
@@ -34,45 +39,63 @@ class PeriodicDataUpdateExportTemplateService:
         self.admin2_filter = periodic_data_update_template.filters.get("admin2")
         self.received_assistance_filter = periodic_data_update_template.filters.get("received_assistance")
 
-    """
-    one with data
-    one with metadata
-    id of template
-    id of template saved also in xlsx metadata https://stackoverflow.com/questions/72888685/add-custom-properties-to-excel-workbook-with-openpyxl
-    individual__uuid
-    individual_unicef_id
-    first_name
-    last_name
-    {pdu_field_name}__round_number
-    {pdu_field_name}__round_name
-    {pdu_field_name}__value
-    {pdu_field_name}__collection_date
-    
-     {
-                "field": "Vaccination Records Update",
-                "round": 2,
-                "round_name": "February vaccination",
-                "number_of_records": 100,
-            },
-    """
-    def generate_row(self, individual: Individual):
+    def generate_workbook(self) -> openpyxl.Workbook:
+        self._create_workbook()
+        self._add_meta()
+        self._generate_header()
+        for individual in self._get_individuals_queryset():
+            row = self._generate_row(individual)
+            if row:
+                self.ws_export_list.append(row)
+        return self.wb
 
+
+    def _create_workbook(self) -> openpyxl.Workbook:
+        wb = openpyxl.Workbook()
+        ws_pdu = wb.active
+        ws_pdu.title = PeriodicDataUpdateExportTemplateService.PDU_SHEET
+        self.wb = wb
+        self.ws_export_list = ws_pdu
+        self.ws_meta = wb.create_sheet(PeriodicDataUpdateExportTemplateService.META_SHEET)
+        return wb
+
+    def _add_meta(self) -> None:
+        self.ws_meta["A1"] = "Periodic Data Update Template ID"
+        self.ws_meta["B1"] = self.periodic_data_update_template.pk
+        self.wb.set_custom_property("pdu_template_id", self.periodic_data_update_template.pk)
+        self.wb.close()
+
+    def _generate_header(self):
+        header = [
+            "individual__uuid",
+            "individual_unicef_id",
+            "first_name",
+            "last_name",
+        ]
+        for round_info_data in self.round_data:
+            header.extend([f"{round_info_data['field']}__round_number", f"{round_info_data['field']}__round_name"])
+        return header
+
+    def _generate_row(self, individual: Individual):
         individual__uuid = individual.pk
         individual_unicef_id = individual.unicef_id
         first_name = individual.given_name
         last_name = individual.family_name
-        row =[individual__uuid, individual_unicef_id, first_name, last_name]
+        row = [individual__uuid, individual_unicef_id, first_name, last_name]
         is_individual_not_allowed = True
-        used_in_rounds = dict()
-        for round_info_data in self.periodic_data_update_template.rounds_data:
+        for round_info_data in self.rounds_data:
             pdu_field_name = round_info_data["field"]
             round_number = round_info_data["round"]
             round_name = round_info_data["round_name"]
             round_value = self._get_round_value(individual, pdu_field_name, round_number)
-            used_in_rounds[pdu_field_name] = used_in_rounds.get(pdu_field_name, 0) + 1
+            if round_value is None:
+                round_info_data["number_of_records"] = round_info_data["number_of_records"] + 1
+                row.extend([round_number, round_name, "", ""])
+            else:
+                row.extend([round_number, round_name, "-", "-"])
             is_individual_not_allowed = is_individual_not_allowed or (round_value is None)
-            row.extend([round_number, round_name])
-
+        if is_individual_not_allowed:
+            return None
 
     def _get_round_value(self, individual, pdu_field_name, round_number):
         flex_fields_data = individual.flex_fields
@@ -82,6 +105,7 @@ class PeriodicDataUpdateExportTemplateService:
             if round_data:
                 return round_data.get("value")
         return None
+
     def _get_individuals_queryset(self):
         queryset = Individual.objects
         queryset = queryset.filter(program=self.program)
@@ -107,6 +131,8 @@ class PeriodicDataUpdateExportTemplateService:
             queryset = queryset.filter(admin2__in=self.admin2_filter)
         if self.has_grievance_ticket_filter:
             queryset = self._get_grievance_ticket_filter(queryset)
+
+        return queryset
 
     def _get_grievance_ticket_filter(self, queryset):
         ticket_details_individual_field = [
