c1ea37611b7d57fd4a2f6ae68f68f8cb434b3f80
szymon-kellton <130459593+szymon-kellton@users.noreply.github.com>
2024-07-12 13:26:36 +0200

    Selenium: Payment Module - Happy path (#4016)
    * Init
    
    * In progress test_payment_plan_happy_path
    
    * In progress test_payment_plan_happy_path
    
    * In progress test_payment_plan_happy_path
    
    * In progress test_payment_plan_happy_path
    
    * Added test_payment_plan_happy_path
    
    * Fix for test_payment_plan_happy_path
    
    * Fix for test_smoke_new_payment_plan
    
    * Fixed checkStatus

--- backend/selenium_tests/helpers/date_time_format.py
+++ backend/selenium_tests/helpers/date_time_format.py
@@ -2,20 +2,30 @@ from datetime import datetime
 
 
 class FormatTime:
-    def __init__(self, day: int, month: int, year: int, hour: int = 0, minute: int = 0):
+    def __init__(
+        self,
+        day: int = 0,
+        month: int = 0,
+        year: int = 0,
+        hour: int = 0,
+        minute: int = 0,
+        time: datetime | None = None,
+    ):
+        self._datetime = time
         self.day = day
         self.month = month
         self.year = year
         self.hour = hour
         self.minute = minute
-        self.datetime = datetime(year, month, day, hour, minute)
+        if self.year != 0:
+            self._datetime = datetime(self.year, self.month, self.day, self.hour, self.minute)
 
     @property
     def numerically_formatted_date(self) -> str:
         # date format YYYY-MM-DD
-        return str(self.datetime.strftime("%Y-%m-%d"))
+        return str(self._datetime.strftime("%Y-%m-%d"))
 
     @property
     def date_in_text_format(self) -> str:
         # date format -d Mon YYYY
-        return str(self.datetime.strftime("%-d %b %Y"))
+        return str(self._datetime.strftime("%-d %b %Y"))
