77c7316fac02f853e189288ae4d832d6db5e8ada
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-07-12 14:43:17 +0200

    Black

--- backend/selenium_tests/targeting/test_targeting.py
+++ backend/selenium_tests/targeting/test_targeting.py
@@ -134,6 +134,7 @@ def add_targeting() -> None:
     call_command("loaddata", f"{settings.PROJECT_ROOT}/apps/targeting/fixtures/data-cypress.json")
     yield
 
+
 @pytest.mark.usefixtures("login")
 class TestSmokeTargeting:
     def test_smoke_targeting_page(self, create_programs: None, add_targeting: None, pageTargeting: Targeting) -> None:
