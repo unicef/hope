901e05e782835c83117d4766c8f41adf0e71fdc7
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-07-16 23:45:10 +0200

    ammend button and documents manager

--- backend/hct_mis_api/apps/registration_data/admin.py
+++ backend/hct_mis_api/apps/registration_data/admin.py
@@ -23,15 +23,12 @@ from hct_mis_api.apps.grievance.models import GrievanceTicket
 from hct_mis_api.apps.household.celery_tasks import enroll_households_to_program_task
 from hct_mis_api.apps.household.documents import get_individual_doc
 from hct_mis_api.apps.household.forms import MassEnrollForm
-from hct_mis_api.apps.household.models import Individual
+from hct_mis_api.apps.household.models import Individual, PendingIndividual
 from hct_mis_api.apps.payment.models import PaymentRecord
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
-from hct_mis_api.apps.registration_datahub import models as datahub_models
 from hct_mis_api.apps.registration_datahub.celery_tasks import (
     merge_registration_data_import_task,
 )
-from hct_mis_api.apps.registration_datahub.documents import get_imported_individual_doc
-from hct_mis_api.apps.targeting.models import HouseholdSelection
 from hct_mis_api.apps.utils.admin import HOPEModelAdminBase
 from hct_mis_api.apps.utils.elasticsearch_utils import (
     remove_elasticsearch_documents_by_matching_ids,
@@ -133,21 +130,17 @@ class RegistrationDataImportAdmin(AdminAutoCompleteSearchMixin, HOPEModelAdminBa
     def delete_rdi(self, request: HttpRequest, pk: UUID) -> Any:  # TODO: typing
         try:
             if request.method == "POST":
-                with transaction.atomic(using="default"), transaction.atomic(using="registration_datahub"):
+                with transaction.atomic:
                     rdi = RegistrationDataImport.objects.get(pk=pk)
                     rdi_name = rdi.name
-                    rdi_datahub = datahub_models.RegistrationDataImportDatahub.objects.get(id=rdi.datahub_id)
-                    datahub_individuals_ids = list(
-                        datahub_models.ImportedIndividual.objects.filter(
-                            registration_data_import=rdi_datahub
-                        ).values_list("id", flat=True)
+                    pending_individuals_ids = list(
+                        PendingIndividual.objects.filter(registration_data_import=rdi).values_list("id", flat=True)
                     )
-                    rdi_datahub.delete()
                     rdi.delete()
                     # remove elastic search records linked to individuals
-                    business_area_slug = rdi_datahub.business_area_slug
+                    business_area_slug = rdi.business_area.slug
                     remove_elasticsearch_documents_by_matching_ids(
-                        datahub_individuals_ids, get_imported_individual_doc(business_area_slug)
+                        pending_individuals_ids, get_individual_doc(business_area_slug)
                     )
                     self.message_user(request, "RDI Deleted")
                     LogEntry.objects.log_action(
@@ -178,13 +171,10 @@ class RegistrationDataImportAdmin(AdminAutoCompleteSearchMixin, HOPEModelAdminBa
     @staticmethod
     def delete_merged_rdi_visible(rdi: RegistrationDataImport) -> bool:
         is_correct_status = rdi.status == RegistrationDataImport.MERGED
-        is_not_used_in_targeting = (
-            HouseholdSelection.objects.filter(household__registration_data_import=rdi).count() == 0
-        )
         is_not_used_by_payment_record = (
             PaymentRecord.objects.filter(household__registration_data_import=rdi).count() == 0
         )
-        return is_correct_status and is_not_used_in_targeting and is_not_used_by_payment_record
+        return is_correct_status and is_not_used_by_payment_record
 
     @staticmethod
     def generate_query_for_all_grievances_tickets(rdi: RegistrationDataImport) -> Q:
@@ -217,28 +207,18 @@ class RegistrationDataImportAdmin(AdminAutoCompleteSearchMixin, HOPEModelAdminBa
     def delete_merged_rdi(self, request: HttpRequest, pk: UUID) -> Optional[HttpResponse]:
         try:
             if request.method == "POST":
-                with transaction.atomic(using="default"), transaction.atomic(using="registration_datahub"):
+                with transaction.atomic:
                     rdi = RegistrationDataImport.objects.get(pk=pk)
                     rdi_name = rdi.name
-                    rdi_datahub = datahub_models.RegistrationDataImportDatahub.objects.get(id=rdi.datahub_id)
-                    datahub_individuals_ids = list(
-                        datahub_models.ImportedIndividual.objects.filter(
-                            registration_data_import=rdi_datahub
-                        ).values_list("id", flat=True)
-                    )
                     individuals_ids = list(
                         Individual.objects.filter(registration_data_import=rdi).values_list("id", flat=True)
                     )
-                    rdi_datahub.delete()
                     GrievanceTicket.objects.filter(
                         RegistrationDataImportAdmin.generate_query_for_all_grievances_tickets(rdi)
                     ).filter(business_area=rdi.business_area).delete()
                     rdi.delete()
                     # remove elastic search records linked to individuals
                     business_area_slug = rdi.business_area.slug
-                    remove_elasticsearch_documents_by_matching_ids(
-                        datahub_individuals_ids, get_imported_individual_doc(business_area_slug)
-                    )
                     remove_elasticsearch_documents_by_matching_ids(
                         individuals_ids, get_individual_doc(business_area_slug)
                     )
