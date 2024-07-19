f0e172ddcdede91f5a404473cfcbf5336b41678a
szymon-kellton <130459593+szymon-kellton@users.noreply.github.com>
2024-07-19 13:36:50 +0200

    Selenium: Programme Details - happy path (#4047)
    * Init
    
    * Done test_program_details_happy_path
    
    * Fixed test_payment_gateway_service.py
    
    * Fixed test_payment_gateway_service.py

--- backend/selenium_tests/page_object/programme_details/programme_details.py
+++ backend/selenium_tests/page_object/programme_details/programme_details.py
@@ -23,8 +23,10 @@ class ProgrammeDetails(BaseComponents):
     buttonRemoveProgram = 'button[data-cy="button-remove-program"]'
     buttonEditProgram = 'a[data-cy="button-edit-program"]'
     buttonActivateProgram = 'button[data-cy="button-activate-program"]'
+    buttonActivateProgramModal = 'button[data-cy="button-activate-program-modal"]'
     labelProgrammeCode = 'div[data-cy="label-Programme Code"]'
     buttonFinishProgram = 'button[data-cy="button-finish-program"]'
+    cashPlanTableRow = 'tr[data-cy="cash-plan-table-row"]'
 
     def getLabelPartnerName(self) -> WebElement:
         return self.wait_for(self.labelPartnerName)
@@ -86,11 +88,18 @@ class ProgrammeDetails(BaseComponents):
     def getButtonActivateProgram(self) -> WebElement:
         return self.wait_for(self.buttonActivateProgram)
 
+    def getButtonActivateProgramModal(self) -> WebElement:
+        return self.wait_for(self.buttonActivateProgramModal)
+
     def getLabelProgrammeCode(self) -> WebElement:
         return self.wait_for(self.labelProgrammeCode)
 
     def getButtonFinishProgram(self) -> WebElement:
         return self.wait_for(self.buttonFinishProgram)
+
+    def getCashPlanTableRow(self) -> [WebElement]:
+        self.wait_for(self.cashPlanTableRow)
+        return self.get_elements(self.cashPlanTableRow)
 
     def clickButtonFinishProgramPopup(self) -> None:
         self.wait_for('[data-cy="dialog-actions-container"]')
