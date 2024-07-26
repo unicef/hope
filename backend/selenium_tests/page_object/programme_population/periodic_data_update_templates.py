1c61e79762fbec0a9eb5149953d26c31d86418d9
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-07-26 05:49:19 +0200

    add pdu template and uploads page objects

--- backend/selenium_tests/page_object/programme_population/periodic_data_update_templates.py
+++ backend/selenium_tests/page_object/programme_population/periodic_data_update_templates.py
@@ -4,35 +4,9 @@ from page_object.base_components import BaseComponents
 
 
 class PeriodicDatUpdateTemplates(BaseComponents):
-    businessAreaContainer = 'div[data-cy="business-area-container"]'
-    globalProgramFilterContainer = 'div[data-cy="global-program-filter-container"]'
-    globalProgramFilter = 'button[data-cy="global-program-filter"]'
-    menuUserProfile = 'button[data-cy="menu-user-profile"]'
-    sideNav = 'div[data-cy="side-nav"]'
-    drawerItems = 'div[data-cy="drawer-items"]'
-    navCountryDashboard = 'a[data-cy="nav-Country Dashboard"]'
-    navRegistrationDataImport = 'a[data-cy="nav-Registration Data Import"]'
     navProgramPopulation = 'a[data-cy="nav-Program Population"]'
-    navHouseholds = 'a[data-cy="nav-Households"]'
     navHouseholdMembers = 'a[data-cy="nav-Household Members"]'
     navProgramDetails = 'a[data-cy="nav-Program Details"]'
-    navTargeting = 'a[data-cy="nav-Targeting"]'
-    navPaymentModule = 'a[data-cy="nav-Payment Module"]'
-    navPaymentVerification = 'a[data-cy="nav-Payment Verification"]'
-    navGrievance = 'a[data-cy="nav-Grievance"]'
-    navGrievanceTickets = 'a[data-cy="nav-Grievance Tickets"]'
-    navGrievanceDashboard = 'a[data-cy="nav-Grievance Dashboard"]'
-    navFeedback = 'a[data-cy="nav-Feedback"]'
-    navAccountability = 'a[data-cy="nav-Accountability"]'
-    navCommunication = 'a[data-cy="nav-Communication"]'
-    navSurveys = 'a[data-cy="nav-Surveys"]'
-    navProgrammeUsers = 'a[data-cy="nav-Programme Users"]'
-    navProgramLog = 'a[data-cy="nav-Program Log"]'
-    navResourcesKnowledgeBase = 'a[data-cy="nav-resources-Knowledge Base"]'
-    navResourcesConversations = 'a[data-cy="nav-resources-Conversations"]'
-    navResourcesToolsAndMaterials = 'a[data-cy="nav-resources-Tools and Materials"]'
-    navResourcesReleaseNote = 'a[data-cy="nav-resources-Release Note"]'
-    mainContent = 'div[data-cy="main-content"]'
     pageHeaderContainer = 'div[data-cy="page-header-container"]'
     pageHeaderTitle = 'h5[data-cy="page-header-title"]'
     tabIndividuals = 'button[data-cy="tab-individuals"]'
@@ -41,7 +15,7 @@ class PeriodicDatUpdateTemplates(BaseComponents):
     newTemplateButton = 'a[data-cy="new-template-button"]'
     buttonImport = 'button[data-cy="button-import"]'
     pduTemplatesBtn = 'button[data-cy="pdu-templates"]'
-    pduUpdates = 'button[data-cy="pdu-updates"]'
+    pduUpdatesBtn = 'button[data-cy="pdu-updates"]'
     headCellTemplateId = 'th[data-cy="head-cell-template-id"]'
     tableLabel = 'span[data-cy="table-label"]'
     headCellNumberOfRecords = 'th[data-cy="head-cell-number-of-records"]'
@@ -60,93 +34,21 @@ class PeriodicDatUpdateTemplates(BaseComponents):
     statusContainer = 'div[data-cy="status-container"]'
     templateAction = 'td[data-cy="template-action-{}"]'
     tablePagination = 'div[data-cy="table-pagination"]'
-
-    def getBusinessAreaContainer(self) -> WebElement:
-        return self.wait_for(self.businessAreaContainer)
-
-    def getGlobalProgramFilterContainer(self) -> WebElement:
-        return self.wait_for(self.globalProgramFilterContainer)
-
-    def getGlobalProgramFilter(self) -> WebElement:
-        return self.wait_for(self.globalProgramFilter)
-
-    def getMenuUserProfile(self) -> WebElement:
-        return self.wait_for(self.menuUserProfile)
-
-    def getSideNav(self) -> WebElement:
-        return self.wait_for(self.sideNav)
-
-    def getDrawerItems(self) -> WebElement:
-        return self.wait_for(self.drawerItems)
-
-    def getNavCountryDashboard(self) -> WebElement:
-        return self.wait_for(self.navCountryDashboard)
-
-    def getNavRegistrationDataImport(self) -> WebElement:
-        return self.wait_for(self.navRegistrationDataImport)
+    # details
+    detailModal = 'h2[data-cy="periodic-data-update-detail"]'
+    templateField = 'td[data-cy="template-field-{}"]'
+    templateRoundNumber = 'td[data-cy="template-round-number-{}"]'
+    templateRoundName = 'td[data-cy="template-round-name-{}"]'
+    templateNumberOfIndividuals = 'td[data-cy="template-number-of-individuals-{}"]'
 
     def getNavProgramPopulation(self) -> WebElement:
         return self.wait_for(self.navProgramPopulation)
-
-    def getNavHouseholds(self) -> WebElement:
-        return self.wait_for(self.navHouseholds)
 
     def getNavHouseholdMembers(self) -> WebElement:
         return self.wait_for(self.navHouseholdMembers)
 
     def getNavProgramDetails(self) -> WebElement:
         return self.wait_for(self.navProgramDetails)
-
-    def getNavTargeting(self) -> WebElement:
-        return self.wait_for(self.navTargeting)
-
-    def getNavPaymentModule(self) -> WebElement:
-        return self.wait_for(self.navPaymentModule)
-
-    def getNavPaymentVerification(self) -> WebElement:
-        return self.wait_for(self.navPaymentVerification)
-
-    def getNavGrievance(self) -> WebElement:
-        return self.wait_for(self.navGrievance)
-
-    def getNavGrievanceTickets(self) -> WebElement:
-        return self.wait_for(self.navGrievanceTickets)
-
-    def getNavGrievanceDashboard(self) -> WebElement:
-        return self.wait_for(self.navGrievanceDashboard)
-
-    def getNavFeedback(self) -> WebElement:
-        return self.wait_for(self.navFeedback)
-
-    def getNavAccountability(self) -> WebElement:
-        return self.wait_for(self.navAccountability)
-
-    def getNavCommunication(self) -> WebElement:
-        return self.wait_for(self.navCommunication)
-
-    def getNavSurveys(self) -> WebElement:
-        return self.wait_for(self.navSurveys)
-
-    def getNavProgrammeUsers(self) -> WebElement:
-        return self.wait_for(self.navProgrammeUsers)
-
-    def getNavProgramLog(self) -> WebElement:
-        return self.wait_for(self.navProgramLog)
-
-    def getNavResourcesKnowledgeBase(self) -> WebElement:
-        return self.wait_for(self.navResourcesKnowledgeBase)
-
-    def getNavResourcesConversations(self) -> WebElement:
-        return self.wait_for(self.navResourcesConversations)
-
-    def getNavResourcesToolsAndMaterials(self) -> WebElement:
-        return self.wait_for(self.navResourcesToolsAndMaterials)
-
-    def getNavResourcesReleaseNote(self) -> WebElement:
-        return self.wait_for(self.navResourcesReleaseNote)
-
-    def getMainContent(self) -> WebElement:
-        return self.wait_for(self.mainContent)
 
     def getPageHeaderContainer(self) -> WebElement:
         return self.wait_for(self.pageHeaderContainer)
@@ -172,8 +74,8 @@ class PeriodicDatUpdateTemplates(BaseComponents):
     def getPduTemplatesBtn(self) -> WebElement:
         return self.wait_for(self.pduTemplatesBtn)
 
-    def getPduUpdates(self) -> WebElement:
-        return self.wait_for(self.pduUpdates)
+    def getPduUpdatesBtn(self) -> WebElement:
+        return self.wait_for(self.pduUpdatesBtn)
 
     def getHeadCellTemplateId(self) -> WebElement:
         return self.wait_for(self.headCellTemplateId)
@@ -234,3 +136,22 @@ class PeriodicDatUpdateTemplates(BaseComponents):
 
     def getTablePagination(self) -> WebElement:
         return self.wait_for(self.tablePagination)
+
+    def getTemplateField(self, index: int) -> WebElement:
+        locator = self.templateField.format(index)
+        return self.wait_for(locator)
+
+    def getTemplateRoundNumber(self, index: int) -> WebElement:
+        locator = self.templateRoundNumber.format(index)
+        return self.wait_for(locator)
+
+    def getTemplateRoundName(self, index: int) -> WebElement:
+        locator = self.templateRoundName.format(index)
+        return self.wait_for(locator)
+
+    def getTemplateNumberOfIndividuals(self, index: int) -> WebElement:
+        locator = self.templateNumberOfIndividuals.format(index)
+        return self.wait_for(locator)
+
+    def getDetailModal(self) -> WebElement:
+        return self.wait_for(self.detailModal)
