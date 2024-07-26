941888aa585f34478203bb88322b9c566aedcccc
Jan Romaniak <jan.romaniak@kellton.com>
2024-07-26 09:45:30 +0200

    change back default timeout

--- backend/selenium_tests/helpers/helper.py
+++ backend/selenium_tests/helpers/helper.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Common:
-    DEFAULT_TIMEOUT = 5
+    DEFAULT_TIMEOUT = 20
 
     def __init__(self, driver: Chrome):
         self.driver = driver
