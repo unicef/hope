d3f3aa73edad0be2e0344df14e771a2f2aeaca08
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2024-07-30 00:10:22 +0200

    connect new template page

--- frontend/src/containers/pages/population/HouseholdMembersPage.tsx
+++ frontend/src/containers/pages/population/HouseholdMembersPage.tsx
@@ -23,6 +23,9 @@ export const HouseholdMembersPage = (): React.ReactElement => {
   const { t } = useTranslation();
   const location = useLocation();
   const { businessArea } = useBaseUrl();
+  const isNewTemplateJustCreated =
+    location.state?.isNewTemplateJustCreated || false;
+
   const permissions = usePermissions();
   const { data: householdChoicesData, loading: householdChoicesLoading } =
     useHouseholdChoiceDataQuery();
@@ -52,8 +55,9 @@ export const HouseholdMembersPage = (): React.ReactElement => {
     getFilterFromQueryParams(location, initialFilter),
   );
 
-  // Local state for managing tabs
-  const [currentTab, setCurrentTab] = useState(0);
+  const [currentTab, setCurrentTab] = useState(
+    isNewTemplateJustCreated ? 1 : 0,
+  );
 
   if (householdChoicesLoading || individualChoicesLoading)
     return <LoadingComponent />;
