16bd99c6fa43e6453039c9a12e71e5cd50010bd4
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2024-08-01 14:16:49 +0200

    Fix cache

--- backend/hct_mis_api/apps/program/api/caches.py
+++ backend/hct_mis_api/apps/program/api/caches.py
@@ -13,7 +13,7 @@ class ProgramCycleListVersionsKeyBit(KeyBitBase):
             self, params: Any, view_instance: Any, view_method: Any, request: Any, args: tuple, kwargs: dict
     ) -> str:
         program_id = decode_id_string(kwargs.get("program_id"))
-        program_cycle_updated_at = ProgramCycle.objects.filter(program_id=program_id).latest("updated_at").updated_at
+        program_cycle_updated_at = ProgramCycle.all_objects.filter(program_id=program_id).latest("updated_at").updated_at
 
         version_key = f"{program_id}:{program_cycle_updated_at}"
         return str(version_key)
