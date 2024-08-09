9859410a6de587221dce4a0de91b6977601c415f
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-08-09 17:06:50 +0200

    add tests for unique name validation cases for flex fields

--- backend/hct_mis_api/apps/core/models.py
+++ backend/hct_mis_api/apps/core/models.py
@@ -252,9 +252,15 @@ class FlexibleAttribute(SoftDeletableModel, NaturalKeyModel, TimeStampedUUIDMode
         ]
 
     def clean(self) -> None:
-        if FlexibleAttribute.objects.filter(name=self.name, program__isnull=True).exclude(id=self.id).exists():
+        if (
+            self.program
+            and FlexibleAttribute.objects.filter(name=self.name, program__isnull=True).exclude(id=self.id).exists()
+        ):
             raise ValidationError(f'Flex field with name "{self.name}" already exists without a program.')
-        if self.program is None and FlexibleAttribute.objects.filter(name=self.name).exclude(id=self.id).exists():
+        elif (
+            not self.program
+            and FlexibleAttribute.objects.filter(name=self.name, program__isnull=False).exclude(id=self.id).exists()
+        ):
             raise ValidationError(f'Flex field with name "{self.name}" already exists inside a program.')
 
     def save(self, *args: Any, **kwargs: Any) -> None:
