f07380afe94c312d5594d84ccb47e45fb6b10ec4
Maciej Szewczyk <34482854+mmaciekk@users.noreply.github.com>
2024-08-16 12:59:51 +0200

    change imports order

--- frontend/src/containers/pages/population/HouseholdMembersPage.tsx
+++ frontend/src/containers/pages/population/HouseholdMembersPage.tsx
@@ -1,5 +1,5 @@
-import { Box, Fade, Tooltip } from '@mui/material';
 import * as React from 'react';
+import { Box, Fade, Tooltip } from '@mui/material';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
