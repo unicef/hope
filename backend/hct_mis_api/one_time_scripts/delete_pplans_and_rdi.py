7fa52e7d14e1565a1ecdf4ed532872c0aec5a2ba
Patryk Dabrowski <patryk.dabrowski@tivix.com>
2024-08-16 13:18:20 +0200

    Make mandatory data_collecting_type in the program

--- backend/hct_mis_api/one_time_scripts/delete_pplans_and_rdi.py
+++ backend/hct_mis_api/one_time_scripts/delete_pplans_and_rdi.py
@@ -7,8 +7,6 @@ from hct_mis_api.apps.payment.models import PaymentPlan
 from hct_mis_api.apps.program.models import Program
 from hct_mis_api.apps.registration_data.admin import RegistrationDataImportAdmin
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
-from hct_mis_api.apps.registration_datahub import models as datahub_models
-from hct_mis_api.apps.registration_datahub.documents import get_imported_individual_doc
 from hct_mis_api.apps.utils.elasticsearch_utils import (
     remove_elasticsearch_documents_by_matching_ids,
 )
@@ -47,20 +45,10 @@ def delete_rdi_for_palestine() -> None:
 
 
 def _delete_rdi(rdi: RegistrationDataImport) -> None:
-    rdi_datahub = datahub_models.RegistrationDataImportDatahub.objects.get(id=rdi.datahub_id)
-    datahub_individuals_ids = list(
-        datahub_models.ImportedIndividual.objects.filter(registration_data_import=rdi_datahub).values_list(
-            "id", flat=True
-        )
-    )
     individuals_ids = list(Individual.objects.filter(registration_data_import=rdi).values_list("id", flat=True))
-    rdi_datahub.delete()
     GrievanceTicket.objects.filter(RegistrationDataImportAdmin.generate_query_for_all_grievances_tickets(rdi)).delete()
     rdi.delete()
     # remove elastic search records linked to individuals
     business_area_slug = rdi.business_area.slug
-    remove_elasticsearch_documents_by_matching_ids(
-        datahub_individuals_ids, get_imported_individual_doc(business_area_slug)
-    )
     remove_elasticsearch_documents_by_matching_ids(individuals_ids, get_individual_doc(business_area_slug))
     logger.info(f"Deleted RDI and related data for {rdi.name}")
