cd5c9f14fcbc38fcd1e365bc48e29d8769e33605
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2024-08-21 11:30:38 +0200

    fix bool display

--- frontend/src/components/targeting/TargetingCriteriaDisplay/Criteria.tsx
+++ frontend/src/components/targeting/TargetingCriteriaDisplay/Criteria.tsx
@@ -113,13 +113,16 @@ const CriteriaField = ({ field, choicesDict }): React.ReactElement => {
       fieldElement = (
         <p>
           {field.fieldAttribute.labelEn || field.fieldName}:{' '}
-          {field.fieldAttribute.type === 'BOOL' ? (
-            <span>{field.arguments?.[0] === 'True' ? t('Yes') : t('No')}</span>
+          {field.fieldAttribute.type === 'BOOL' ||
+          field.fieldAttribute?.pduData?.subtype ? (
+            field.isNull ? (
+              <span>{t('Empty')}</span>
+            ) : (
+              <span>{field.arguments?.[0] ? t('Yes') : t('No')}</span>
+            )
           ) : (
             <span>
-              {displayValueOrEmpty(
-                extractChoiceLabel(field, field.arguments?.[0]),
-              )}
+              {field.arguments?.[0] != null ? field.arguments[0] : t('Empty')}
             </span>
           )}
         </p>
