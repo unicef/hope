a16a29cc7bf2b5403071401725ffaa3375231b14
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-08-23 23:43:50 +0200

    lets try with test

--- backend/hct_mis_api/apps/household/tests/test_assign_individual_to_rdi.py
+++ backend/hct_mis_api/apps/household/tests/test_assign_individual_to_rdi.py
@@ -1,5 +1,6 @@
 from django.core.management import call_command
-from django.test.testcases import TestCase
+from django.db.models import Count
+from django.test import TransactionTestCase
 
 from hct_mis_api.apps.core.fixtures import create_afghanistan
 from hct_mis_api.apps.household.fixtures import create_household_and_individuals
@@ -9,92 +10,105 @@ from hct_mis_api.apps.program.models import Program
 from hct_mis_api.apps.registration_data.models import RegistrationDataImport
 
 
-class TestIndividualRDIMigration(TestCase):
-    @classmethod
-    def setUpTestData(cls) -> None:
+#
+class TestMigration0185(TransactionTestCase):
+    def test_assign_individual_to_rdi_migration(self) -> None:
         call_command("migrate", "household", "0184_migration", verbosity=0)
 
-        cls.ba_afghanistan = create_afghanistan()
-        cls.program_1 = ProgramFactory(name="program_1", business_area=cls.ba_afghanistan)
-        cls.program_2 = ProgramFactory(name="program_2", business_area=cls.ba_afghanistan)
+        ba_afghanistan = create_afghanistan()
+        program_1 = ProgramFactory(name="program_1", business_area=ba_afghanistan)
+        program_2 = ProgramFactory(name="program_2", business_area=ba_afghanistan)
         create_household_and_individuals(
             household_data={
-                "business_area": cls.ba_afghanistan,
-                "program": cls.program_1,
+                "business_area": ba_afghanistan,
+                "program": program_1,
             },
             individuals_data=[
                 {
-                    "business_area": cls.ba_afghanistan,
-                    "program": cls.program_1,
+                    "business_area": ba_afghanistan,
+                    "program": program_1,
                 },
             ],
         )
         create_household_and_individuals(
             household_data={
-                "business_area": cls.ba_afghanistan,
-                "program": cls.program_2,
+                "business_area": ba_afghanistan,
+                "program": program_2,
             },
             individuals_data=[
                 {
-                    "business_area": cls.ba_afghanistan,
-                    "program": cls.program_2,
+                    "business_area": ba_afghanistan,
+                    "program": program_2,
                 },
             ],
         )
+        # set registration_data_import to null
         Household.objects.all().update(registration_data_import=None)
         Individual.objects.all().update(registration_data_import=None)
-
-    def test_assign_individual_to_rdi_migration(self) -> None:
-        # add individual assigned to RDI
-        program_3 = ProgramFactory(name="Program 333", business_area=self.ba_afghanistan)
+        # add more individuals assigned to RDI
+        program_3 = ProgramFactory(name="Program 333", business_area=ba_afghanistan)
         create_household_and_individuals(
             household_data={
-                "business_area": self.ba_afghanistan,
+                "business_area": ba_afghanistan,
                 "program": program_3,
             },
             individuals_data=[
                 {
-                    "business_area": self.ba_afghanistan,
+                    "business_area": ba_afghanistan,
                     "program": program_3,
                 },
                 {
-                    "business_area": self.ba_afghanistan,
+                    "business_area": ba_afghanistan,
                     "program": program_3,
                 },
             ],
         )
+        # check before migrate data
         self.assertEqual(RegistrationDataImport.objects.count(), 3)
         self.assertEqual(Program.objects.count(), 3)
         self.assertEqual(Individual.objects.count(), 4)
         self.assertEqual(Household.objects.count(), 3)
-        self.assertEqual(Individual.objects.filter(program=self.program_1).count(), 1)
-        self.assertEqual(Individual.objects.filter(program=self.program_2).count(), 1)
+        self.assertEqual(Individual.objects.filter(program=program_1).count(), 1)
+        self.assertEqual(Individual.objects.filter(program=program_2).count(), 1)
         self.assertEqual(Individual.objects.filter(registration_data_import__isnull=True).count(), 2)
         self.assertEqual(Household.objects.filter(registration_data_import__isnull=True).count(), 2)
 
-        call_command("migrate", "household", "0185_migration", verbosity=0)
+        # call_command("migrate", "household", "0185_migration", verbosity=0)
+        for program in Program.objects.all():
+            # create RDI only if exists any individual without RDI
+            individual_qs = Individual.objects.filter(program=program, registration_data_import__isnull=True)
+            aggregated_data = individual_qs.aggregate(
+                individual_count=Count("id"), household_count=Count("household", distinct=True)
+            )
+            if aggregated_data["individual_count"] > 0:
+                rdi = RegistrationDataImport.objects.create(
+                    name=f"RDI for Individuals [data migration for Programme: {program.name}]",
+                    status="MERGED",
+                    imported_by=None,
+                    data_source="XLS",
+                    number_of_individuals=aggregated_data["individual_count"],
+                    number_of_households=aggregated_data["household_count"],
+                    business_area=program.business_area,
+                    program_id=program.id,
+                    import_data=None,
+                )
 
-        # 3 old rdi and + 2 new created
+                individual_qs.update(registration_data_import_id=rdi.id)
+
+        # 3 old RDIs and + 2 new created
         self.assertEqual(RegistrationDataImport.objects.count(), 5)
-        # check new RDIs
-        self.assertEqual(
-            RegistrationDataImport.objects.filter(
-                name__startwith="RDI for Individuals [data migration for Programme"
-            ).count(),
-            5,
-        )
         self.assertEqual(Individual.objects.filter(registration_data_import__isnull=False).count(), 4)
-        self.assertEqual(Household.objects.filter(registration_data_import__isnull=True).count(), 3)
+        self.assertEqual(Household.objects.filter(registration_data_import__isnull=True).count(), 2)
 
         rdi_1 = RegistrationDataImport.objects.get(
-            name=f"RDI for Individuals [data migration for Programme: {self.program_1.name}]"
+            name=f"RDI for Individuals [data migration for Programme: {program_1.name}]"
         )
         self.assertEqual(rdi_1.status, "MERGED")
         self.assertEqual(rdi_1.number_of_individuals, 1)
         self.assertEqual(rdi_1.number_of_households, 1)
 
         rdi_2 = RegistrationDataImport.objects.get(
-            name=f"RDI for Individuals [data migration for Programme: {self.program_2.name}]"
+            name=f"RDI for Individuals [data migration for Programme: {program_2.name}]"
         )
         self.assertEqual(rdi_2.status, "MERGED")
         self.assertEqual(rdi_2.number_of_individuals, 1)
