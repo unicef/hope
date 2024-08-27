f555ce47034342c7877b4cc8ea2ea55695394b09
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-08-27 14:10:53 +0200

    In progress PDU tests to people

--- backend/selenium_tests/people/test_people_periodic_data_update.py
+++ backend/selenium_tests/people/test_people_periodic_data_update.py
@@ -1,37 +1,22 @@
 import os
 from datetime import datetime
-from tempfile import NamedTemporaryFile, _TemporaryFileWrapper
-from typing import Any, List
+from time import sleep
+from typing import List
 
-import openpyxl
 import pytest
 from dateutil.relativedelta import relativedelta
 from django.db import transaction
-from page_object.programme_population.periodic_data_update_templates import (
-    PeriodicDatUpdateTemplates,
-)
-from page_object.programme_population.periodic_data_update_uploads import (
-    PeriodicDataUpdateUploads,
-)
 
 from hct_mis_api.apps.account.models import User
-from hct_mis_api.apps.core.fixtures import create_afghanistan
-from hct_mis_api.apps.core.models import FlexibleAttribute, PeriodicFieldData, BusinessArea
-from hct_mis_api.apps.household.fixtures import create_household_and_individuals, create_individual_document, \
+from hct_mis_api.apps.core.fixtures import DataCollectingTypeFactory
+from hct_mis_api.apps.core.models import FlexibleAttribute, PeriodicFieldData, BusinessArea, DataCollectingType
+from hct_mis_api.apps.household.fixtures import create_individual_document, \
     create_household
 from hct_mis_api.apps.household.models import Individual, SEEING, HOST
 from hct_mis_api.apps.payment.fixtures import CashPlanFactory, PaymentRecordFactory
-from hct_mis_api.apps.payment.models import PaymentRecord, GenericPayment
-from hct_mis_api.apps.periodic_data_update.fixtures import (
-    PeriodicDataUpdateTemplateFactory,
-    PeriodicDataUpdateUploadFactory,
-)
+from hct_mis_api.apps.payment.models import GenericPayment
 from hct_mis_api.apps.periodic_data_update.models import (
-    PeriodicDataUpdateTemplate,
     PeriodicDataUpdateUpload,
-)
-from hct_mis_api.apps.periodic_data_update.service.periodic_data_update_export_template_service import (
-    PeriodicDataUpdateExportTemplateService,
 )
 from hct_mis_api.apps.periodic_data_update.utils import (
     field_label_to_field_name,
@@ -39,7 +24,6 @@ from hct_mis_api.apps.periodic_data_update.utils import (
 )
 from hct_mis_api.apps.program.fixtures import ProgramFactory
 from hct_mis_api.apps.program.models import Program
-from hct_mis_api.apps.registration_data.fixtures import RegistrationDataImportFactory
 from hct_mis_api.apps.targeting.fixtures import TargetingCriteriaFactory, TargetPopulationFactory
 from selenium_tests.page_object.people.people import People
 from selenium_tests.page_object.people.people_details import PeopleDetails
@@ -57,11 +41,32 @@ def clear_downloaded_files() -> None:
 
 
 @pytest.fixture
-def add_people(social_worker_program: Program) -> List:
-    ba = social_worker_program.business_area
+def program() -> Program:
+    return get_program_with_dct_type_and_name("Test Program", "WORK", DataCollectingType.Type.SOCIAL, Program.ACTIVE)
+
+
+def get_program_with_dct_type_and_name(
+    name: str, programme_code: str, dct_type: str = DataCollectingType.Type.STANDARD, status: str = Program.DRAFT
+) -> Program:
+    BusinessArea.objects.filter(slug="afghanistan").update(is_payment_plan_applicable=True)
+    dct = DataCollectingTypeFactory(type=dct_type)
+    program = ProgramFactory(
+        name=name,
+        programme_code=programme_code,
+        start_date=datetime.now() - relativedelta(months=1),
+        end_date=datetime.now() + relativedelta(months=1),
+        data_collecting_type=dct,
+        status=status,
+    )
+    return program
+
+
+@pytest.fixture
+def add_people(program: Program) -> List:
+    ba = program.business_area
     with transaction.atomic():
         household, individuals = create_household(
-            household_args={"business_area": ba, "program": social_worker_program, "residence_status": HOST},
+            household_args={"business_area": ba, "program": program, "residence_status": HOST},
             individual_args={
                 "full_name": "Stacey Freeman",
                 "given_name": "Stacey",
@@ -77,8 +82,8 @@ def add_people(social_worker_program: Program) -> List:
 
 
 @pytest.fixture
-def add_program(add_people: List) -> Program:
-    program = Program.objects.filter(name="Worker Program").first()
+def individual(add_people: List) -> Individual:
+    program = Program.objects.filter(name="Test Program").first()
 
     cash_plan = CashPlanFactory(
         name="TEST",
@@ -106,21 +111,51 @@ def add_program(add_people: List) -> Program:
     )
     add_people[1].total_cash_received_usd = "21.36"
     add_people[1].save()
-    return program
+    return add_people[1]
+
+
+def create_flexible_attribute(
+    label: str, subtype: str, number_of_rounds: int, rounds_names: list[str], program: Program
+) -> FlexibleAttribute:
+    name = field_label_to_field_name(label)
+    flexible_attribute = FlexibleAttribute.objects.create(
+        label={"English(EN)": label},
+        name=name,
+        type=FlexibleAttribute.PDU,
+        associated_with=FlexibleAttribute.ASSOCIATED_WITH_INDIVIDUAL,
+        program=program,
+    )
+    flexible_attribute.pdu_data = PeriodicFieldData.objects.create(
+        subtype=subtype, number_of_rounds=number_of_rounds, rounds_names=rounds_names
+    )
+    flexible_attribute.save()
+    return flexible_attribute
+
+
+@pytest.fixture
+def string_attribute() -> FlexibleAttribute:
+    program = Program.objects.filter(name="Test Program").first()
+    return create_flexible_attribute(
+        label="Test String Attribute",
+        subtype=FlexibleAttribute.STRING,
+        number_of_rounds=1,
+        rounds_names=["Test Round"],
+        program=program,
+    )
 
 
 @pytest.mark.usefixtures("login")
-class TestPeriodicDataUpdateUpload:
-    def test_periodic_data_update_upload_success(
+class TestPeoplePeriodicDataUpdateUpload:
+    def test_people_periodic_data_update_upload_success(
         self,
         clear_downloaded_files: None,
-            pagePeople: People,
-            pagePeopleDetails: PeopleDetails,
-        program: Program,
+        pagePeople: People,
+        pagePeopleDetails: PeopleDetails,
         individual: Individual,
         string_attribute: FlexibleAttribute,
         pageIndividuals: Individuals,
     ) -> None:
+        program = Program.objects.filter(name="Test Program").first()
         populate_pdu_with_null_values(program, individual.flex_fields)
         individual.save()
         flexible_attribute = string_attribute
@@ -136,9 +171,8 @@ class TestPeriodicDataUpdateUpload:
             [["Test Value", "2021-05-02"]],
             program,
         )
-        pageIndividuals.selectGlobalProgramFilter(program.name)
-        pageIndividuals.getNavProgrammePopulation().click()
-        pageIndividuals.getNavIndividuals().click()
+        pagePeople.selectGlobalProgramFilter(program.name)
+        pagePeople.getNavPeople().click()
         pageIndividuals.getTabPeriodicDataUpdates().click()
         pageIndividuals.getButtonImport().click()
         pageIndividuals.getDialogImport()
@@ -146,8 +180,14 @@ class TestPeriodicDataUpdateUpload:
         pageIndividuals.upload_file(tmp_file.name)
         pageIndividuals.getButtonImportSubmit().click()
         pageIndividuals.getPduUpdates().click()
-        periodic_data_update_upload = PeriodicDataUpdateUpload.objects.first()
-        assert periodic_data_update_upload.status == PeriodicDataUpdateUpload.Status.SUCCESSFUL
+        for i in range(15):
+            periodic_data_update_upload = PeriodicDataUpdateUpload.objects.first()
+            if periodic_data_update_upload.status == PeriodicDataUpdateUpload.Status.SUCCESSFUL:
+                break
+            pageIndividuals.screenshot(i)
+            sleep(1)
+        else:
+            assert periodic_data_update_upload.status == PeriodicDataUpdateUpload.Status.SUCCESSFUL
         assert periodic_data_update_upload.error_message is None
         individual.refresh_from_db()
         assert individual.flex_fields[flexible_attribute.name]["1"]["value"] == "Test Value"
