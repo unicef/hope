3167cc014257592fe52d32086ceae21314341468
Jan Romaniak <jan.romaniak@kellton.com>
2024-08-27 11:56:41 +0200

    siwtch to pending objects

--- backend/hct_mis_api/apps/household/views.py
+++ backend/hct_mis_api/apps/household/views.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
@@ -13,20 +13,21 @@ from hct_mis_api.apps.household.models import (
     IDENTIFICATION_TYPE_TAX_ID,
     Document,
     Household,
+    Individual,
+    PendingDocument,
+    PendingIndividual,
 )
 from hct_mis_api.apps.household.serializers import (
     serialize_by_household,
     serialize_by_individual,
 )
-from hct_mis_api.apps.registration_datahub.models import (
-    ImportedDocument,
-    ImportedHousehold,
-)
+from hct_mis_api.apps.registration_datahub.models import PendingHousehold
 from hct_mis_api.apps.utils.profiling import profiling
 
 logger = logging.getLogger(__name__)
 
-def get_individual(tax_id: str, business_area_code: Optional[str]) -> Document:
+
+def get_individual(tax_id: str, business_area_code: Optional[str]) -> Union[Individual, PendingIndividual]:
     documents = (
         Document.objects.all()
         if not business_area_code
@@ -37,21 +38,21 @@ def get_individual(tax_id: str, business_area_code: Optional[str]) -> Document:
     if documents.count() == 1:
         return documents.first().individual
 
-    imported_documents = (
-        ImportedDocument.objects.all()
+    pending_documents = (
+        PendingDocument.objects.all()
         if not business_area_code
-        else ImportedDocument.objects.filter(
+        else PendingDocument.objects.filter(
             individual__household__registration_data_import__business_area__code=business_area_code
         )
     ).filter(type__key=IDENTIFICATION_TYPE_TO_KEY_MAPPING[IDENTIFICATION_TYPE_TAX_ID], document_number=tax_id)
-    if imported_documents.count() > 1:
-        raise Exception(f"Multiple imported documents ({imported_documents.count()}) with given tax_id found")
-    if imported_documents.count() == 1:
-        return imported_documents.first().individual
+    if pending_documents.count() > 1:
+        raise Exception(f"Multiple imported documents ({pending_documents.count()}) with given tax_id found")
+    if pending_documents.count() == 1:
+        return pending_documents.first().individual
     raise Exception("Document with given tax_id not found")
 
 
-def get_household(registration_id: str, business_area_code: Optional[str]) -> ImportedHousehold:
+def get_household(registration_id: str, business_area_code: Optional[str]) -> Union[PendingHousehold, Household]:
     kobo_asset_value = _prepare_kobo_asset_id_value(registration_id)
     households = (
         Household.objects.all()
@@ -64,17 +65,17 @@ def get_household(registration_id: str, business_area_code: Optional[str]) -> Im
         return households.first()  # type: ignore
 
     if business_area_code is None:
-        imported_households_by_business_area = ImportedHousehold.objects.all()
+        pending_households_by_business_area = PendingHousehold.objects.all()
     else:
         business_areas = BusinessArea.objects.filter(code=business_area_code)
         if not business_areas:
             raise Exception(f"Business area with code {business_area_code} not found")
         business_area = business_areas.first()  # code is unique, so no need to worry here
-        imported_households_by_business_area = ImportedHousehold.objects.filter(
+        pending_households_by_business_area = PendingHousehold.objects.filter(
             registration_data_import__business_area_slug=business_area.slug
         )
 
-    imported_households = imported_households_by_business_area.filter(detail_id__endswith=kobo_asset_value)
+    imported_households = pending_households_by_business_area.filter(detail_id__endswith=kobo_asset_value)
     if imported_households.count() > 1:
         raise Exception(
             f"Multiple imported households ({imported_households.count()}) with given registration_id found"
