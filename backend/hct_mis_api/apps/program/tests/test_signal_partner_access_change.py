f5e78cc2cf698d4568fd9fee95bea832fc9e93d5
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-08-30 11:24:20 +0200

    DEV -> STG (#4179)
    * fix potentially unstable tests
    
    * disable e2e temporarily
    
    ---------
    
    Co-authored-by: Paulina Kujawa <p.kujawa2f@gmail.com>
    Co-authored-by: Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
    Co-authored-by: Paulina Kujawa <42150286+pkujawa@users.noreply.github.com>

--- backend/hct_mis_api/apps/program/tests/test_signal_partner_access_change.py
+++ backend/hct_mis_api/apps/program/tests/test_signal_partner_access_change.py
@@ -34,9 +34,11 @@ class TestPartnerAccessChangeSignal(TestCase):
         )
         cls.area_type_other = AreaTypeFactory(name="Area Type Other", country=country_other)
 
-        cls.area_in_afg_1 = AreaFactory(name="Area in AFG 1", area_type=area_type_afg)
-        cls.area_in_afg_2 = AreaFactory(name="Area in AFG 2", area_type=area_type_afg)
-        cls.area_not_in_afg = AreaFactory(name="Area not in AFG", area_type=cls.area_type_other)
+        cls.area_in_afg_1 = AreaFactory(name="Area in AFG 1", area_type=area_type_afg, p_code="AREA-IN-AFG1")
+        cls.area_in_afg_2 = AreaFactory(name="Area in AFG 2", area_type=area_type_afg, p_code="AREA-IN-AFG2")
+        cls.area_not_in_afg = AreaFactory(
+            name="Area not in AFG", area_type=cls.area_type_other, p_code="AREA-NOT-IN-AFG"
+        )
 
         cls.program = ProgramFactory.create(
             status=Program.DRAFT, business_area=cls.business_area, partner_access=Program.NONE_PARTNERS_ACCESS
