1c9394a92959b1e8d2809dad043e39f905cc4873
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-09-05 12:26:37 +0200

    add more validation

--- backend/hct_mis_api/apps/program/api/views.py
+++ backend/hct_mis_api/apps/program/api/views.py
@@ -87,10 +87,12 @@ class ProgramCycleViewSet(
         if program_cycle.program.cycles.count() == 1:
             raise ValidationError("Don’t allow to delete last Cycle.")
 
-        if program_cycle.target_populations.exclude(status=TargetPopulation.STATUS_OPEN).exists():
+        if program_cycle.target_populations.exists():
             raise ValidationError("Don’t allow to delete Cycle with assigned Target Population")
 
-        program_cycle.target_populations.all().delete()
+        if program_cycle.payment_plans.exists():
+            raise ValidationError("Don’t allow to delete Cycle with assigned Payment Plan")
+
         program_cycle.delete()
 
     @action(detail=True, methods=["post"], permission_classes=[ProgramCycleUpdatePermission])
