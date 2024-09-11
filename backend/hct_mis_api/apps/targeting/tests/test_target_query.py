1b54987fd42bb0abb63afa59b8d33e8a75fc3f51
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-09-11 17:04:27 +0200

    add test coverage

--- backend/hct_mis_api/apps/targeting/tests/test_target_query.py
+++ backend/hct_mis_api/apps/targeting/tests/test_target_query.py
@@ -10,7 +10,11 @@ from hct_mis_api.apps.core.fixtures import (
     PeriodicFieldDataFactory,
     create_afghanistan,
 )
-from hct_mis_api.apps.core.models import BusinessArea, PeriodicFieldData
+from hct_mis_api.apps.core.models import (
+    BusinessArea,
+    DataCollectingType,
+    PeriodicFieldData,
+)
 from hct_mis_api.apps.household.fixtures import create_household
 from hct_mis_api.apps.household.models import DISABLED
 from hct_mis_api.apps.periodic_data_update.utils import populate_pdu_with_null_values
@@ -253,6 +257,44 @@ class TestTargetPopulationQuery(APITestCase):
         cls.target_population_with_individual_filter = full_rebuild(cls.target_population_with_individual_filter)
         cls.target_population_with_individual_filter.save()
 
+        # SW Program
+        cls.program_sw = ProgramFactory(
+            name="test_program_sw",
+            status=Program.ACTIVE,
+            business_area=cls.business_area,
+            data_collecting_type__type=DataCollectingType.Type.SOCIAL,
+        )
+        pdu_data_string_sw = PeriodicFieldDataFactory(
+            subtype=PeriodicFieldData.STRING,
+            number_of_rounds=2,
+            rounds_names=["Round 1", "Round 2"],
+        )
+        cls.pdu_field_string_for_sw = FlexibleAttributeForPDUFactory(
+            program=cls.program_sw,
+            label="PDU Field STRING for SW",
+            pdu_data=pdu_data_string_sw,
+        )
+        targeting_criteria = cls.get_targeting_criteria_for_rule(
+            {
+                "field_name": cls.pdu_field_string.name,
+                "arguments": ["Test"],
+                "comparison_method": "EQUALS",
+                "flex_field_classification": "FLEX_FIELD_PDU",
+                "round_number": 1,
+            }
+        )
+        cls.target_population_with_pdu_filter_for_sw = TargetPopulation(
+            name="target_population_with_pdu_filter_for_sw",
+            created_by=user_second,
+            targeting_criteria=targeting_criteria,
+            status=TargetPopulation.STATUS_LOCKED,
+            business_area=cls.business_area,
+            program=cls.program_sw,
+        )
+        cls.target_population_with_pdu_filter_for_sw.save()
+        cls.target_population_with_pdu_filter_for_sw = full_rebuild(cls.target_population_with_pdu_filter_for_sw)
+        cls.target_population_with_pdu_filter_for_sw.save()
+
     @staticmethod
     def get_targeting_criteria_for_rule(rule_filter: Dict) -> TargetingCriteria:
         targeting_criteria = TargetingCriteria()
@@ -311,7 +353,6 @@ class TestTargetPopulationQuery(APITestCase):
     )
     def test_simple_target_query(self, _: Any, permissions: List[Permissions]) -> None:
         self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
-
         self.snapshot_graphql_request(
             request_string=TestTargetPopulationQuery.TARGET_POPULATION_QUERY,
             context={"user": self.user},
@@ -375,6 +416,38 @@ class TestTargetPopulationQuery(APITestCase):
             variables={
                 "id": self.id_to_base64(
                     self.target_population_with_pdu_filter.id,
+                    "TargetPopulationNode",
+                )
+            },
+        )
+
+    @parameterized.expand(
+        [
+            (
+                "with_permission",
+                [Permissions.TARGETING_VIEW_DETAILS],
+            ),
+            (
+                "without_permission",
+                [],
+            ),
+        ]
+    )
+    def test_simple_target_query_pdu_for_sw_program(self, _: Any, permissions: List[Permissions]) -> None:
+        self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program_sw)
+        print("pduduuu")
+        self.snapshot_graphql_request(
+            request_string=TestTargetPopulationQuery.TARGET_POPULATION_QUERY,
+            context={
+                "user": self.user,
+                "headers": {
+                    "Business-Area": self.business_area.slug,
+                    "Program": self.id_to_base64(self.program_sw.id, "ProgramNode"),
+                },
+            },
+            variables={
+                "id": self.id_to_base64(
+                    self.target_population_with_pdu_filter_for_sw.id,
                     "TargetPopulationNode",
                 )
             },
