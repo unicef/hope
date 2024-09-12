1bcd7db54e10fa8f732f66793d7eb7d4976aea80
Szymon Wyderka <szymon.wyderka@kellton.com>
2024-09-12 09:44:48 +0200

    Init

--- backend/selenium_tests/payment_module/test_payment_plans.py
+++ backend/selenium_tests/payment_module/test_payment_plans.py
@@ -253,7 +253,6 @@ class TestSmokePaymentModule:
         assert "FSP Auth Code" in pagePaymentModuleDetails.getTableLabel()[9].text
         assert "Reconciliation" in pagePaymentModuleDetails.getTableLabel()[10].text
 
-    @pytest.mark.skip(reason="Test fails in CI")
     def test_payment_plan_happy_path(
         self,
         clear_downloaded_files: None,
