27f6106f7f56ee9a372b72f67da491e14367a4d6
Domenico DiNicola <dom.dinicola@gmail.com>
2024-09-20 20:44:49 +0200

    structure

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+SRC = os.path.abspath("src")
+sys.path.insert(0, SRC)
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hct_mis_api.config.settings")
 
