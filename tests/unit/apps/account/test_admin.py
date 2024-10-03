9ba990aa2fe3869ebf89d348a590a35cb85c7bbe
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-10-03 22:25:43 +0200

    contrib refactoring

--- tests/unit/apps/account/test_admin.py
+++ tests/unit/apps/account/test_admin.py
@@ -3,29 +3,33 @@ from urllib.parse import unquote
 
 from django.urls import reverse
 
-from django_webtest import WebTest
+import pytest
 
-from hct_mis_api.apps.account.fixtures import UserFactory
-from hct_mis_api.apps.account.models import User
+from hct_mis_api.apps.account.fixtures import RoleFactory, UserFactory
 
 
-class RoleTest(WebTest):
-    @classmethod
-    def setUpTestData(cls) -> None:
-        super().setUpTestData()
-        cls.superuser: User = UserFactory(is_superuser=True, is_staff=True)
+@pytest.fixture()
+def superuser(request, db):
+    return UserFactory(is_superuser=True, is_staff=True)
 
-    def test_role_perm_matrix(self) -> None:
-        url = reverse("admin:account_role_matrix")
-        res = self.app.get(url, user=self.superuser)
-        assert res.status_code == 200
 
-    def test_role_sync(self) -> None:
-        url = reverse("admin:account_role_dumpdata_qs")
-        res = self.app.get(url, user=self.superuser)
-        assert res.status_code == 200
-        print(res.json)
-        jres = json.loads(unquote(res.json["data"]))
-        models = set([item["model"] for item in jres])
-        assert len(models) == 1
-        assert models == {"account.role"}
+@pytest.fixture()
+def role(request, db):
+    return RoleFactory(name="Role")
+
+
+def test_role_perm_matrix(django_app, superuser) -> None:
+    url = reverse("admin:account_role_matrix")
+    res = django_app.get(url, user=superuser)
+    assert res.status_code == 200
+
+
+def test_role_sync(django_app, superuser, role) -> None:
+    url = reverse("admin:account_role_dumpdata_qs")
+    res = django_app.get(url, user=superuser)
+    assert res.status_code == 200
+    print(res.json)
+    jres = json.loads(unquote(res.json["data"]))
+    models = set([item["model"] for item in jres])
+    assert len(models) == 1
+    assert models == {"account.role"}
