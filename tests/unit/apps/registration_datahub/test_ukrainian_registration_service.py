e0a799b087b0918bd2ec0601368440d175dea9a7
Domenico <dom.dinicola@gmail.com>
2024-10-04 18:54:58 +0200

    add vision app (#4274)
    * contrib refactoring
    
    * contrib refactoring
    
    ---------
    
    Co-authored-by: Pavlo Mokiichuk <pv.pasha.pv@gmail.com>

--- tests/unit/apps/registration_datahub/test_ukrainian_registration_service.py
+++ tests/unit/apps/registration_datahub/test_ukrainian_registration_service.py
@@ -18,13 +18,13 @@ from hct_mis_api.apps.household.models import (
     PendingIndividual,
 )
 from hct_mis_api.apps.program.fixtures import ProgramFactory
-from hct_mis_api.aurora.fixtures import (
+from hct_mis_api.contrib.aurora.fixtures import (
     OrganizationFactory,
     ProjectFactory,
     RegistrationFactory,
 )
-from hct_mis_api.aurora.models import Record
-from hct_mis_api.aurora.services.ukraine_flex_registration_service import (
+from hct_mis_api.contrib.aurora.models import Record
+from hct_mis_api.contrib.aurora.services.ukraine_flex_registration_service import (
     Registration2024,
     UkraineBaseRegistrationService,
 )
