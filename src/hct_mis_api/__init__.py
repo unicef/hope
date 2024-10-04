745b8f835998d2739ab6e535e9eda98a3eb66d72
Jan Romaniak <jan.romaniak@kellton.com>
2024-10-04 16:45:06 +0200

    fixed retrieving version

--- src/hct_mis_api/__init__.py
+++ src/hct_mis_api/__init__.py
@@ -1,7 +1,18 @@
+import importlib.metadata
+import os
+
+from django.conf import settings
+
 import tomli
 
 
 def get_full_version() -> str:
-    with open("pyproject.toml", mode="rb") as fp:
-        config = tomli.load(fp)
-    return config["project"]["version"]
+    try:
+        # works in dist image
+        version = importlib.metadata.version("hope")
+        return version
+    except importlib.metadata.PackageNotFoundError:
+        # works in local and dev image
+        with open(os.path.join(settings.PROJECT_ROOT, "../../pyproject.toml"), mode="rb") as fp:
+            config = tomli.load(fp)
+        return config["project"]["version"]
