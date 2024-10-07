5f8690ebdd5a4423e8b68071b951191313726d97
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2024-10-07 14:23:29 +0200

    change imports

--- src/frontend/src/api/paymentModuleApi.ts
+++ src/frontend/src/api/paymentModuleApi.ts
@@ -1,6 +1,5 @@
 import { api } from './api';
-import { PaymentPlan } from 'restgenerated/models/PaymentPlan';
-
+import { PaymentPlan } from '../../../frontend/restgenerated/models/PaymentPlan';
 export const fetchPaymentPlansManagerial = async (
   businessAreaSlug,
   params = {},
