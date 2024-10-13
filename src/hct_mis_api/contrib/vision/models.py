4bc9223443c9236b10795236681450992b8a34f4
Domenico DiNicola <dom.dinicola@gmail.com>
2024-10-13 21:41:22 +0200

    pg-adapt

--- src/hct_mis_api/contrib/vision/models.py
+++ src/hct_mis_api/contrib/vision/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
+from model_utils.models import TimeStampedModel
+
 from hct_mis_api.apps.core.models import BusinessArea
 
 
-class FundsCommitment(models.Model):
-    rec_serial_number = models.CharField(max_length=10, unique=True)
+class FundsCommitment(TimeStampedModel):
+    rec_serial_number = models.IntegerField(primary_key=True)
     business_area = models.CharField(max_length=4, blank=True, null=True)
     funds_commitment_number = models.CharField(max_length=10, blank=True, null=True)
     document_type = models.CharField(max_length=2, blank=True, null=True)
     document_text = models.CharField(max_length=50, blank=True, null=True)
     currency_code = models.CharField(max_length=5, blank=True, null=True)
-    gl_account = models.CharField(null=True, blank=True, max_length=10)
+    gl_account = models.CharField(max_length=10, null=True, blank=True)
     commitment_amount_local = models.DecimalField(
         decimal_places=2,
         max_digits=15,
@@ -41,21 +43,27 @@ class FundsCommitment(models.Model):
     document_reference = models.CharField(max_length=16, null=True)
     fc_status = models.CharField(max_length=1, blank=True, null=True)
     create_date = models.DateTimeField(null=True, blank=True, auto_now_add=True)
+    funds_commitment_item = models.CharField(max_length=3, null=True, blank=True, default="")
+    wbs_element = models.CharField(max_length=24, null=True, blank=True, default="")
+    grant_number = models.CharField(max_length=20, null=True, blank=True, default="")
+
     created_by = models.CharField(max_length=20, null=True, blank=True, default="")
     update_date = models.DateTimeField(null=True, blank=True)
     updated_by = models.CharField(max_length=20, blank=True, null=True, default="")
 
-    grant_number = models.CharField(max_length=10, null=True, blank=True, default="")
     sponsor = models.CharField(max_length=10, null=True, blank=True, default="")
     sponsor_name = models.CharField(max_length=100, null=True, blank=True, default="")
-    wbs_element = models.CharField(max_length=24, null=True, blank=True, default="")
     fund = models.CharField(max_length=10, null=True, blank=True, default="")
     funds_center = models.CharField(max_length=16, null=True, blank=True, default="")
-    funds_commitment_item = models.CharField(max_length=3, null=True, blank=True, default="")
     percentage = models.DecimalField(decimal_places=2, max_digits=5, null=True, blank=True)
 
-    business_office_code = models.CharField(max_length=4, blank=True, null=True)
-    business_area_obj = models.ForeignKey(BusinessArea, on_delete=models.SET_NULL, null=True, blank=True)
+    office = models.ForeignKey(
+        BusinessArea,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        help_text="This can be a business office or a business area",
+    )
 
     def __str__(self) -> str:
         return self.funds_commitment_number
@@ -64,8 +72,8 @@ class FundsCommitment(models.Model):
         unique_together = ("funds_commitment_number", "funds_commitment_item")
 
 
-class DownPayment(models.Model):
-    rec_serial_number = models.CharField(max_length=10, blank=True, null=True)
+class DownPayment(TimeStampedModel):
+    rec_serial_number = models.CharField(max_length=10, primary_key=True)
     business_area = models.CharField(max_length=4)
     down_payment_reference = models.CharField(max_length=20)
     document_type = models.CharField(max_length=10)
@@ -90,5 +98,10 @@ class DownPayment(models.Model):
     update_date = models.DateTimeField(blank=True, null=True)
     updated_by = models.CharField(max_length=20, blank=True, null=True, default="")
 
-    business_area_obj = models.ForeignKey(BusinessArea, on_delete=models.SET_NULL, null=True, blank=True)
-    business_office_code = models.CharField(max_length=4, blank=True, null=True)
+    office = models.ForeignKey(
+        BusinessArea,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        help_text="This can be a business office or a business area",
+    )
