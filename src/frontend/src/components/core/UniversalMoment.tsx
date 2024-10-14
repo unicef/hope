b8b48934b5d1d31554b60a793b3e6f4685c98bda
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2024-10-14 16:35:19 +0200

    fix moment and jest

--- src/frontend/src/components/core/UniversalMoment.tsx
+++ src/frontend/src/components/core/UniversalMoment.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Moment from 'react-moment';
+import { format } from 'date-fns';
 import { DATE_FORMAT, DATE_FORMAT_LONG } from '../../config';
 
 export interface Props {
@@ -11,11 +11,14 @@ export function UniversalMoment({
   children,
   withTime,
 }: Props): React.ReactElement {
-  return children ? (
-    <Moment format={withTime ? DATE_FORMAT_LONG : DATE_FORMAT}>
-      {children}
-    </Moment>
+  const dateFormat = withTime ? DATE_FORMAT_LONG : DATE_FORMAT;
+  const date = children ? new Date(children) : null;
+  const formattedDate = date ? format(date, dateFormat) : '-';
+  const dateTime = date ? date.getTime().toString() : '';
+
+  return date ? (
+    <time dateTime={dateTime}>{formattedDate}</time>
   ) : (
-    <>-</>
+    <>{formattedDate}</>
   );
 }
