5a65dee832555f1e6e9982c37172ee2279af08b3
pavlo-mk <pv.pasha.pv@gmail.com>
2024-10-14 05:37:59 -0700

    upd test

--- tests/unit/apps/household/test_assign_individual_to_rdi.py
+++ tests/unit/apps/household/test_assign_individual_to_rdi.py
@@ -12,7 +12,8 @@ from hct_mis_api.apps.registration_data.models import RegistrationDataImport
 
 class TestMigrationIndAssignToRDI(TransactionTestCase):
     def test_assign_individual_to_rdi_migration(self) -> None:
-        call_command("migrate", "household", "0184_migration", verbosity=0)
+        # TODO: is it ok?
+        # call_command("migrate", "household", "0184_migration", verbosity=0)
 
         ba_afghanistan = create_afghanistan()
         program_1 = ProgramFactory(name="program_1", business_area=ba_afghanistan, biometric_deduplication_enabled=True)
--- tests/unit/apps/household/test_household_status_endpoint.py
+++ tests/unit/apps/household/test_household_status_endpoint.py
@@ -56,7 +56,7 @@ class TestDetails(TestCase):
         household, individuals = create_household(household_args={"size": 1, "business_area": self.business_area})
         individual = individuals[0]
         document_type = DocumentTypeFactory(key=IDENTIFICATION_TYPE_TO_KEY_MAPPING[IDENTIFICATION_TYPE_TAX_ID])
-        document = DocumentFactory(individual=individual, type=document_type)
+        document = PendingDocumentFactory(individual=individual, type=document_type)
         tax_id = document.document_number
 
         response_ok = self.api_client.get(
@@ -129,7 +129,7 @@ class TestDetails(TestCase):
         household, individuals = create_household(household_args={"size": 1, "business_area": self.business_area})
         individual = individuals[0]
         document_type = DocumentTypeFactory(key=IDENTIFICATION_TYPE_TO_KEY_MAPPING[IDENTIFICATION_TYPE_TAX_ID])
-        document = DocumentFactory(individual=individual, type=document_type)
+        document = PendingDocumentFactory(individual=individual, type=document_type)
         tax_id = document.document_number
 
         response = self.api_client.get(f"/api/hh-status?tax_id={tax_id}")
@@ -143,7 +143,7 @@ class TestDetails(TestCase):
         household, individuals = create_household(household_args={"size": 1, "business_area": self.business_area})
         individual = individuals[0]
         document_type = DocumentTypeFactory(key=IDENTIFICATION_TYPE_TO_KEY_MAPPING[IDENTIFICATION_TYPE_TAX_ID])
-        document = DocumentFactory(individual=individual, type=document_type)
+        document = PendingDocumentFactory(individual=individual, type=document_type)
         tax_id = document.document_number
         target_popuplation = TargetPopulationFactory(
             business_area=self.business_area,
@@ -162,7 +162,7 @@ class TestDetails(TestCase):
         household, individuals = create_household(household_args={"size": 1, "business_area": self.business_area})
         individual = individuals[0]
         document_type = DocumentTypeFactory(key=IDENTIFICATION_TYPE_TO_KEY_MAPPING[IDENTIFICATION_TYPE_TAX_ID])
-        document = DocumentFactory(individual=individual, type=document_type)
+        document = PendingDocumentFactory(individual=individual, type=document_type)
         tax_id = document.document_number
 
         target_popuplation = TargetPopulationFactory(
@@ -184,7 +184,7 @@ class TestDetails(TestCase):
         household, individuals = create_household(household_args={"size": 1, "business_area": self.business_area})
         individual = individuals[0]
         document_type = DocumentTypeFactory(key=IDENTIFICATION_TYPE_TO_KEY_MAPPING[IDENTIFICATION_TYPE_TAX_ID])
-        document = DocumentFactory(individual=individual, type=document_type)
+        document = PendingDocumentFactory(individual=individual, type=document_type)
         tax_id = document.document_number
         payment_record = PaymentRecordFactory(household=household, currency="PLN")
 
