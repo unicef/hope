ecbc2c10713a76f849337008a10bc89ef61fdbeb
pavlo-mk <pv.pasha.pv@gmail.com>
2024-10-15 13:51:39 +0200

    add more info and hard delete

--- src/hct_mis_api/one_time_scripts/remove_migrated_data_is_original.py
+++ src/hct_mis_api/one_time_scripts/remove_migrated_data_is_original.py
@@ -6,15 +6,26 @@ def remove_migrated_data_is_original() -> None:
 
     for model in all_models:
         if hasattr(model, "is_original"):
-            print(f"Removing objects from {model.__name__} that have the 'is_original' field.")
             if model.__name__ == "GrievanceTicket":
-                # or maybe try except AttributeError:
                 # 'GrievanceTicket' has no attribute 'all_objects'
-                queryset = model.default_for_migrations_fix.filter(is_original=True)
+                queryset_all = model.default_for_migrations_fix.all()
+                queryset_is_original = queryset_all.filter(is_original=True)
             elif model.__name__ in ["HouseholdSelection", "EntitlementCard", "Feedback", "Message"]:
-                queryset = model.original_and_repr_objects.filter(is_original=True)
+                queryset_all = model.original_and_repr_objects.all()
+                queryset_is_original = queryset_all.filter(is_original=True)
             else:
-                queryset = model.all_objects.filter(is_original=True)
+                queryset_all = model.all_objects.all()
+                queryset_is_original = queryset_all.filter(is_original=True)
 
-            count, _ = queryset.delete()
-            print(f"Deleted {count} {model.__name__} objects.\n")
+            print(
+                f"*** {model.__name__} All objects: {queryset_all.count()}. "
+                f"Removing objects with 'is_original=True': {queryset_is_original.count()}"
+            )
+
+            try:
+                count, _ = queryset_is_original.delete(soft=False)
+            #  TypeError: QuerySet.delete() got an unexpected keyword argument 'soft'
+            except TypeError:
+                count, _ = queryset_is_original.delete()
+
+            print(f"Deleted {model.__name__} and related objects {count}.\n")
