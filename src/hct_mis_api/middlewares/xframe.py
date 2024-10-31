26e5b49bafc399a3c0051e8f91c3ae151e279fe1
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2024-10-31 05:47:56 +0100

    Some other tests

--- src/hct_mis_api/middlewares/xframe.py
+++ src/hct_mis_api/middlewares/xframe.py
@@ -1,9 +1,11 @@
-from django.conf import settings
+from typing import List, Optional
+
+from django.http import HttpRequest, HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
 
 class AllowSpecificIframeDomainsMiddleware(MiddlewareMixin):
-    ALLOWED_IFRAME_DOMAINS = [
+    ALLOWED_IFRAME_DOMAINS: List[str] = [
         "https://localhost:3000",
         "https://dev-hope.unitst.org",
         "https://trn-hope.unitst.org",
@@ -11,12 +13,13 @@ class AllowSpecificIframeDomainsMiddleware(MiddlewareMixin):
         "https://hope.unitst.org",
     ]
 
-    def process_response(self, request, response):
-        origin = request.META.get("HTTP_ORIGIN")
-        referer = request.META.get("HTTP_REFERER")
+    def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
+        origin: Optional[str] = request.META.get("HTTP_ORIGIN")
+        referer: Optional[str] = request.META.get("HTTP_REFERER")
 
         if origin in self.ALLOWED_IFRAME_DOMAINS or (
             referer and any(domain in referer for domain in self.ALLOWED_IFRAME_DOMAINS)
         ):
-            response["X-Frame-Options"] = "ALLOW-FROM " + origin if origin else referer
+            response["X-Frame-Options"] = "ALLOW-FROM " + (origin or referer or "")
+
         return response
