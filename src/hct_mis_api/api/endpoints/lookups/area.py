f1d4caf9bcd3a25174419f9d86ff40b7172ce6b6
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-10-31 01:02:00 +0100

    add additional filters and search into country, area type and area endpoints

--- src/hct_mis_api/api/endpoints/lookups/area.py
+++ src/hct_mis_api/api/endpoints/lookups/area.py
@@ -1,6 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import serializers
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import ListAPIView
 
+from hct_mis_api.api.filters import AreaFilter, AreaTypeFilter
 from hct_mis_api.apps.geo.models import Area, AreaType
 
 
@@ -13,6 +16,9 @@ class AreaSerializer(serializers.ModelSerializer):
 class AreaList(ListAPIView):
     queryset = Area.objects.all()
     serializer_class = AreaSerializer
+    filter_backends = (OrderingFilter, DjangoFilterBackend, SearchFilter)
+    filterset_class = AreaFilter
+    search_fields = ("name", "p_code")
 
 
 class AreaTypeSerializer(serializers.ModelSerializer):
@@ -24,3 +30,6 @@ class AreaTypeSerializer(serializers.ModelSerializer):
 class AreaTypeList(ListAPIView):
     queryset = AreaType.objects.all()
     serializer_class = AreaTypeSerializer
+    filter_backends = (OrderingFilter, DjangoFilterBackend, SearchFilter)
+    filterset_class = AreaTypeFilter
+    search_fields = ("name",)
