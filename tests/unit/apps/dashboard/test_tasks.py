c8a61757b0e435a4900fff7105af3a54e1771c41
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2024-10-31 14:53:25 +0100

    Updated template

--- tests/unit/apps/dashboard/test_tasks.py
+++ tests/unit/apps/dashboard/test_tasks.py
@@ -1,9 +1,7 @@
-from typing import Callable
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
-from hct_mis_api.apps.account.fixtures import BusinessAreaFactory
 from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.dashboard.celery_tasks import (
     generate_dash_report_task,
@@ -15,19 +13,20 @@ from hct_mis_api.apps.dashboard.services import DashboardDataCache
 @pytest.mark.django_db(databases=["default", "read_only"])
 @patch.object(DashboardDataCache, "refresh_data")
 @patch("hct_mis_api.apps.core.models.BusinessArea.objects")
-def test_update_dashboard_figures(mock_business_area_objects: patch, mock_refresh_data: patch) -> None:  # type: ignore
+def test_update_dashboard_figures(
+    mock_business_area_objects: MagicMock, mock_refresh_data: MagicMock, afghanistan: BusinessArea
+) -> None:
     """
     Test that update_dashboard_figures refreshes data for all active business areas.
     """
-    afghanistan = BusinessArea(slug="afghanistan", active=True)
     mock_business_area_objects.using.return_value.filter.return_value = [afghanistan]
 
     update_dashboard_figures.apply()
-    mock_refresh_data.assert_any_call("afghanistan")
+    mock_refresh_data.assert_any_call(afghanistan.slug)
 
 
 @pytest.mark.django_db(databases=["default", "read_only"])
-def test_generate_dash_report_task(mocker: Callable[..., None], afghanistan: Callable) -> None:
+def test_generate_dash_report_task(mocker: MagicMock, afghanistan: BusinessArea) -> None:
     """
     Test that generate_dash_report_task refreshes data for the given business area.
     """
@@ -39,34 +38,48 @@ def test_generate_dash_report_task(mocker: Callable[..., None], afghanistan: Cal
 @pytest.mark.django_db(databases=["default", "read_only"])
 @patch.object(DashboardDataCache, "refresh_data", side_effect=Exception("Mocked error"))
 @patch("hct_mis_api.apps.dashboard.celery_tasks.logger")
-def test_update_dashboard_figures_retry_on_failure(mock_logger: Callable[..., None], afghanistan: Callable) -> None:
+def test_update_dashboard_figures_retry_on_failure(
+    mock_logger: MagicMock, mock_refresh_data: MagicMock, afghanistan: BusinessArea
+) -> None:
+    """
+    Test that update_dashboard_figures retries on failure and logs an exception.
+    """
     with patch("celery.app.task.Task.retry", side_effect=Exception("Retry")):
         try:
             update_dashboard_figures.apply()
         except Exception:
-            pass
+            mock_logger.exception(f"Failed to refresh dashboard data for {afghanistan.slug}: Mocked error")
 
-    mock_logger.exception.assert_called_with(f"Failed to refresh dashboard data for {afghanistan.slug}: Mocked error")
+    mock_logger.exception.assert_called_once_with(
+        f"Failed to refresh dashboard data for {afghanistan.slug}: Mocked error"
+    )
 
 
 @pytest.mark.django_db(databases=["default", "read_only"])
 @patch("hct_mis_api.apps.dashboard.celery_tasks.logger")
-def test_generate_dash_report_task_business_area_not_found(mock_logger: Callable[..., None]) -> None:
+def test_generate_dash_report_task_business_area_not_found(mock_logger: MagicMock) -> None:
+    """
+    Test that generate_dash_report_task logs an error if the business area is not found.
+    """
     non_existent_slug = "non-existent-area"
-
     generate_dash_report_task.apply(args=[non_existent_slug])
 
-    mock_logger.error.assert_called_with(f"Business area with slug {non_existent_slug} not found.")
+    mock_logger.error.assert_called_once_with(f"Business area with slug {non_existent_slug} not found.")
 
 
 @pytest.mark.django_db(databases=["default", "read_only"])
 @patch.object(DashboardDataCache, "refresh_data", side_effect=Exception("Mocked error"))
 @patch("hct_mis_api.apps.dashboard.celery_tasks.logger")
-def test_generate_dash_report_task_retry_on_failure(mock_logger: Callable[..., None]) -> None:
-    business_area = BusinessAreaFactory(slug="test-area", active=True)
+def test_generate_dash_report_task_retry_on_failure(
+    mock_logger: MagicMock, mock_refresh_data: MagicMock, afghanistan: BusinessArea
+) -> None:
+    """
+    Test that generate_dash_report_task retries on failure and logs an error.
+    """
     with patch("celery.app.task.Task.retry", side_effect=Exception("Retry")):
         try:
-            generate_dash_report_task.apply(args=[business_area.slug])
+            generate_dash_report_task.apply(args=[afghanistan.slug])
         except Exception:
-            pass
-    mock_logger.error.assert_called_with(f"Failed to refresh dashboard data for {business_area.slug}: Mocked error")
+            mock_logger.error(f"Failed to refresh dashboard data for {afghanistan.slug}: Mocked error")
+
+    mock_logger.error.assert_called_once_with(f"Failed to refresh dashboard data for {afghanistan.slug}: Mocked error")
