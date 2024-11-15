b887c178b094a744bd960176a1b027ef763b9453
Jan Romaniak <jan.romaniak@kellton.com>
2024-11-15 08:32:55 +0100

    some test

---
+++ tests/unit/one_time_scripts/test_south_sudan_update_script.py
@@ -0,0 +1,24 @@
+# import pytest
+#
+# from hct_mis_api.apps.household.fixtures import create_household_and_individuals
+#
+#
+# @pytest.fixture
+# def household():
+#     create
+#     household, individuals = create_household_and_individuals(
+#         household_data={
+#             "business_area": cls.business_area,
+#             "program_id": cls.program_for_household.pk,
+#         },
+#         individuals_data=[
+#             {
+#                 "business_area": cls.business_area,
+#                 "program_id": cls.program_for_household.pk,
+#             },
+#         ],
+#     )
+#
+# class TestSouthSudanUpdateScript:
+#
+#     def test_south_sudan_update_script(self):
