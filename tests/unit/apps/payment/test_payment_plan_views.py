42c30d808e56fb5e6d1a899b4e32068580b568c0
Jan Romaniak <jan.romaniak@tivix.com>
2024-11-20 15:27:58 +0100

    E2E speed improvement  (#4288)
    * chenged tests dir
    
    * fixed all linter problems
    
    * fix  precomit
    
    * fix compose
    
    * fixed  tests root dir
    
    * init speed improvements
    
    * fixes
    
    * changes to running tests
    
    * increased max connections in postgress compose
    
    * changes to running tests
    
    * changes to running tests
    
    * global program select fix
    
    * changes to running tests
    
    * fixes to reassigning
    
    * changes to running tests
    
    * changes to running tests
    
    * changes to running tests
    
    * changes to running tests fixed - test_grievance_tickets_edit_tickets_from_main_grievance_pag
    
    * changes to running tests fixed - test_create_feedback_mandatory_fields
    
    * changes to running tests
    
    * fixed file
    
    * Fix countries
    
    * linter fixes
    
    * fixing import
    
    * Tests fixes
    
    * black fixes
    
    * clean fronted
    
    * console logs removed
    
    * frontend fix build
    
    * mark unstable tests
    
    * change if to try except to avoid concurency problem
    
    * xfail more test
    
    * test fixes
    
    * fixes
    
    * xfail unstable tests
    
    * Fix for Upload Artifact
    
    * Fix for test_smoke_country_dashboard
    
    * added more tests
    
    * added more tests
    
    * Fix e2e tests
    
    * Black
    
    * more fixes more tests
    
    * added more as unstable
    
    * E2E Test stabilisation
    
    * Test without parallel execution
    
    * Test without extra_options
    
    * Fixed stability of test_managerial_console_smoke_test
    
    * Fixed stability of test_managerial_console_smoke_test
    
    * Delete transaction=True from pytest.mark.django_db; Fix test_payment_verification_delete
    
    * Fix test_edit_programme
    
    * Fix test_edit_programme
    
    * Fix test_edit_programme
    
    * Fix for User fixture
    
    * Fix test_create_targeting_with_pdu_date_criteria
    
    * Fix test_edit_programme
    
    * Fix test_edit_programme
    
    * Xfaile flaky tests
    
    * Xfaile flaky tests
    
    * Xfaile flaky tests
    
    * Xfaile flaky tests
    
    * add print :printer:
    
    * cleanup & upd utests :star2:
    
    * imports :star:
    
    ---------
    
    Co-authored-by: Szymon Wyderka <szymon.wyderka@kellton.com>
    Co-authored-by: szymon-kellton <130459593+szymon-kellton@users.noreply.github.com>
    Co-authored-by: Pavlo Mokiichuk <pv.pasha.pv@gmail.com>

--- tests/unit/apps/payment/test_payment_plan_views.py
+++ tests/unit/apps/payment/test_payment_plan_views.py
@@ -132,6 +132,7 @@ class TestPaymentPlanManagerialList(PaymentPlanTestMixin):
             response = _test_list()
             print("=====> All Headers:", response.headers)
             etag = response.headers["etag"]
+
             print("E-TAG>", etag)
 
             assert json.loads(cache.get(etag)[0].decode("utf8")) == response.json()
