d5691fee3ad5b2f3482cc3f9c151c20303996316
pavlo-mk <pv.pasha.pv@gmail.com>
2024-11-25 14:19:10 +0100

    init script

--- src/hct_mis_api/apps/payment/models.py
+++ src/hct_mis_api/apps/payment/models.py
@@ -452,36 +452,6 @@ class PaymentPlan(
     AdminUrlMixin,
     InternalDataFieldModel,
 ):
-    TP_MIGRATION_MAPPING = {
-        # tp.field: payment_plan.field
-        # if value has internal_data__ will story into json
-        "name": "name",
-        "ca_id": "internal_data__ca_id",
-        "ca_hash_id": "internal_data__ca_hash_id",
-        "created_by": "created_by",
-        "change_date": "status_date",
-        "business_area": "business_area",
-        "status": "status",
-        "build_status": "build_status",
-        "built_at": "built_at",
-        "program": "program",
-        "program_cycle": "program_cycle",
-        "targeting_criteria": "targeting_criteria",
-        "sent_to_datahub": "internal_data__sent_to_datahub",
-        "steficon_rule": "steficon_rule_targeting",
-        "steficon_applied_date": "steficon_targeting_applied_date",
-        "vulnerability_score_min": "vulnerability_score_min",
-        "vulnerability_score_max": "vulnerability_score_max",
-        "excluded_ids": "excluded_ids",
-        "exclusion_reason": "exclusion_reason",
-        "total_households_count": "total_households_count",
-        "total_individuals_count": "total_individuals_count",
-        "child_male_count": "male_children_count",
-        "child_female_count": "female_children_count",
-        "adult_male_count": "male_adults_count",
-        "adult_female_count": "female_adults_count",
-        "storage_file": "storage_file",
-    }
 
     ACTIVITY_LOG_MAPPING = create_mapping_dict(
         [
---
+++ src/hct_mis_api/one_time_scripts/migrate_tp_into_pp.py
@@ -0,0 +1,43 @@
+from hct_mis_api.apps.targeting.models import TargetPopulation
+
+
+def migrate_tp_into_pp()-> None:
+    TP_MIGRATION_MAPPING = {
+        # tp.field: payment_plan.field
+        # if value has internal_data__ will story into json
+        "name": "name",
+        "ca_id": "internal_data__ca_id",
+        "ca_hash_id": "internal_data__ca_hash_id",
+        "created_by": "created_by",
+        "change_date": "status_date",
+        "business_area": "business_area",
+        "status": "status",
+        "build_status": "build_status",
+        "built_at": "built_at",
+        "program": "program",
+        "program_cycle": "program_cycle",
+        "targeting_criteria": "targeting_criteria",
+        "sent_to_datahub": "internal_data__sent_to_datahub",
+        "steficon_rule": "steficon_rule_targeting",
+        "steficon_applied_date": "steficon_targeting_applied_date",
+        "vulnerability_score_min": "vulnerability_score_min",
+        "vulnerability_score_max": "vulnerability_score_max",
+        "excluded_ids": "excluded_ids",
+        "exclusion_reason": "exclusion_reason",
+        "total_households_count": "total_households_count",
+        "total_individuals_count": "total_individuals_count",
+        "child_male_count": "male_children_count",
+        "child_female_count": "female_children_count",
+        "adult_male_count": "male_adults_count",
+        "adult_female_count": "female_adults_count",
+        "storage_file": "storage_file",
+    }
+
+    # TODO: migrate hh_ids, ind_ids into rule hh_ids, ind_ids!
+
+    # per BA;
+    for tp in TargetPopulation.objects.all():
+        for pp in tp.payment_plans.all():
+
+            for tp_field, pp_field in TP_MIGRATION_MAPPING.items():
+                pass
