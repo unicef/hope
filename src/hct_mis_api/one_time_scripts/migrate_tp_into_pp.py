3c7fb210fe8dcc6b54682d6d1d77f627e85648e3
pavlo-mk <pv.pasha.pv@gmail.com>
2024-11-25 21:09:10 +0100

    script optimization

--- src/hct_mis_api/one_time_scripts/migrate_tp_into_pp.py
+++ src/hct_mis_api/one_time_scripts/migrate_tp_into_pp.py
@@ -1,4 +1,7 @@
 from collections import defaultdict
+from typing import Any, Dict
+
+from django.db import transaction
 
 from hct_mis_api.apps.payment.models import PaymentPlan
 from hct_mis_api.apps.targeting.models import TargetPopulation
@@ -9,8 +12,6 @@ def migrate_tp_into_pp() -> None:
         # tp.field: payment_plan.field
         # if value has internal_data__ will story into json
         "name": "name",
-        "ca_id": "internal_data__ca_id",
-        "ca_hash_id": "internal_data__ca_hash_id",
         "created_by": "created_by",
         "change_date": "status_date",
         "business_area": "business_area",
@@ -20,7 +21,6 @@ def migrate_tp_into_pp() -> None:
         "program": "program",
         "program_cycle": "program_cycle",
         "targeting_criteria": "targeting_criteria",
-        "sent_to_datahub": "internal_data__sent_to_datahub",
         "steficon_rule": "steficon_rule_targeting",
         "steficon_applied_date": "steficon_targeting_applied_date",
         "vulnerability_score_min": "vulnerability_score_min",
@@ -35,40 +35,54 @@ def migrate_tp_into_pp() -> None:
         "adult_female_count": "female_adults_count",
         "storage_file": "storage_file",
     }
+    INTERNAL_DATA_FIELDS = {
+        "ca_id": "internal_data__ca_id",
+        "ca_hash_id": "internal_data__ca_hash_id",
+        "sent_to_datahub": "internal_data__sent_to_datahub",
+    }
 
-    # TODO: migrate hh_ids, ind_ids into rule hh_ids, ind_ids!
+    ALL_TP_MIGRATION_MAPPING = TP_MIGRATION_MAPPING.update(INTERNAL_DATA_FIELDS)
 
-    # per BA;
-    for tp in TargetPopulation.objects.all():
-        for pp in tp.payment_plans.all():
-            internal_data = defaultdict(dict)
-
-            for tp_field, pp_field in TP_MIGRATION_MAPPING.items():
-                tp_value = getattr(tp, tp_field, None)
-
-                if tp_value:
-                    pp_field_list = pp_field.split("__")
-                    if len(pp_field_list) == 1:
-                        setattr(pp, pp_field, tp_value)
-                    if len(pp_field_list) == 2 and pp_field_list[0] == internal_data:
-                        internal_data[pp_field_list[1]] = str(tp_value)
-
-                    setattr(pp, "internal_data", internal_data)
-
-            pp.save()
-
-        # if no PP create new PaymentPlan
-        payment_plan = PaymentPlan()
-        for tp_field, pp_field in TP_MIGRATION_MAPPING.items():
+    def map_tp_to_pp(tp: TargetPopulation) -> Dict[str, Any]:
+        """helper function to map TargetPopulation to PaymentPlan fields."""
+        payment_plan_data = defaultdict(dict)
+        internal_data = defaultdict(dict)
+        for tp_field, pp_field in ALL_TP_MIGRATION_MAPPING.items():
             tp_value = getattr(tp, tp_field, None)
-
             if tp_value:
                 pp_field_list = pp_field.split("__")
                 if len(pp_field_list) == 1:
-                    setattr(payment_plan, pp_field, tp_value)
-                if len(pp_field_list) == 2 and pp_field_list[0] == internal_data:
+                    payment_plan_data[pp_field] = tp_value
+                elif len(pp_field_list) == 2 and pp_field_list[0] == "internal_data":
                     internal_data[pp_field_list[1]] = str(tp_value)
+        payment_plan_data["internal_data"] = internal_data
+        return payment_plan_data
 
-                setattr(payment_plan, "internal_data", internal_data)
+    # TODO: migrate hh_ids, ind_ids into rule hh_ids, ind_ids???
 
-        payment_plan.save()
+    with transaction.atomic():
+        tp_qs = TargetPopulation.objects.prefetch_related("payment_plans")
+        new_payment_plans = []
+        updated_payment_plans = []
+
+        for tp in tp_qs:
+            # update existing PaymentPlans
+            # TODO: double check if any TP with more that one PP ???
+            existing_payment_plans = list(tp.payment_plans.all())
+            if existing_payment_plans:
+                for pp in existing_payment_plans:
+                    payment_plan_data = map_tp_to_pp(tp)
+                    for field, value in payment_plan_data.items():
+                        setattr(pp, field, value)
+                    updated_payment_plans.append(pp)
+            else:
+                # create new PaymentPlan if none exist
+                payment_plan_data = map_tp_to_pp(tp)
+                new_payment_plans.append(PaymentPlan(**payment_plan_data))
+
+        if updated_payment_plans:
+            PaymentPlan.objects.bulk_update(
+                updated_payment_plans, TP_MIGRATION_MAPPING.values() + ["internal_data"], 1000
+            )
+        if new_payment_plans:
+            PaymentPlan.objects.bulk_create(new_payment_plans, 1000)
