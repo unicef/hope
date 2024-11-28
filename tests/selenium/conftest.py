5c07c894228583fcf6285536698bc72b2402654c
pavlo-mk <pv.pasha.pv@gmail.com>
2024-11-28 11:39:57 +0100

    selenium // fix create superuser

--- tests/selenium/conftest.py
+++ tests/selenium/conftest.py
@@ -560,14 +560,16 @@ def create_super_user(business_area: BusinessArea) -> User:
     call_command("loaddata", f"{settings.PROJECT_ROOT}/apps/geo/fixtures/data_small.json", verbosity=0)
     country = Country.objects.get(name="Afghanistan")
     business_area.countries.add(country)
-    user = UserFactory.create(
-        is_superuser=True,
-        is_staff=True,
-        username="superuser",
-        password="testtest2",
-        email="test@example.com",
-        partner=partner,
-    )
+    if not (user := User.objects.filter(pk="4196c2c5-c2dd-48d2-887f-3a9d39e78916").first()):
+        user = UserFactory(
+            pk="4196c2c5-c2dd-48d2-887f-3a9d39e78916",
+            is_superuser=True,
+            is_staff=True,
+            username="superuser",
+            password="testtest2",
+            email="test@example.com",
+            partner=partner,
+        )
     UserRole.objects.create(
         user=user,
         role=Role.objects.get(name="Role"),
