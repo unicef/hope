c569868b16e14c95f581c1860480bfd9f059b1f5
Paulina Kujawa <p.kujawa2f@gmail.com>
2024-12-03 14:56:38 +0100

    move migrations to new structure

--- src/hct_mis_api/apps/payment/migrations/0005_migration.py
+++ src/hct_mis_api/apps/payment/migrations/0005_migration.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('payment', '0147_migration'),
+        ('payment', '0004_migration'),
     ]
 
     operations = [
