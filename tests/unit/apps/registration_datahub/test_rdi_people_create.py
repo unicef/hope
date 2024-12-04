5e0f7a199ceb35110af0e1e17f6b3c86eefd3857
pavlo-mk <pv.pasha.pv@gmail.com>
2024-12-04 12:41:24 +0100

    fix test

--- tests/unit/apps/registration_datahub/test_rdi_people_create.py
+++ tests/unit/apps/registration_datahub/test_rdi_people_create.py
@@ -130,11 +130,11 @@ class TestRdiXlsxPeople(TestCase):
         self.assertEqual(dmd2.rdi_merge_status, MergeStatusModel.PENDING)
         self.assertEqual(dmd3.rdi_merge_status, MergeStatusModel.PENDING)
         self.assertEqual(
-            json.loads(dmd1.data),
+            dmd1.data,
             {"card_number__atm_card": "164260858", "card_expiry_date__atm_card": "1995-06-03T00:00:00"},
         )
         self.assertEqual(
-            json.loads(dmd2.data),
+            dmd2.data,
             {
                 "card_number__atm_card": "1975549730",
                 "card_expiry_date__atm_card": "2022-02-17T00:00:00",
@@ -142,7 +142,7 @@ class TestRdiXlsxPeople(TestCase):
             },
         )
         self.assertEqual(
-            json.loads(dmd3.data),
+            dmd3.data,
             {
                 "card_number__atm_card": "870567340",
                 "card_expiry_date__atm_card": "2016-06-27T00:00:00",
