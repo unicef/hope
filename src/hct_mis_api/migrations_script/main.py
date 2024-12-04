83f6baf74b753c79a3932792098dd5471d692030
Pavlo Mokiichuk <pv.pasha.pv@gmail.com>
2024-12-04 15:32:03 +0100

    STG //  Fix RDI import dmd json field (#4504)
    * fix rdi import dmd data json
    
    * add migration
    
    * fix test
    
    * imports
    
    * more fixes

--- src/hct_mis_api/migrations_script/main.py
+++ src/hct_mis_api/migrations_script/main.py
@@ -54,6 +54,7 @@ if __name__ == "__main__":
         ("payment", "0003_migration"),
         ("payment", "0004_migration"),
         ("payment", "0005_migration"),
+        ("payment", "0006_migration"),
         ("aurora", "0003_migration"),
     ]
     fake_migrations(excluded_migrations)
