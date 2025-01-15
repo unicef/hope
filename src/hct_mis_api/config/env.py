254b4698c58b131851f0ca83980fce1ca29f67c5
Jan Romaniak <jan.romaniak@kellton.com>
2025-01-15 14:33:25 +0100

    fixes for selenium test localy

--- src/hct_mis_api/config/env.py
+++ src/hct_mis_api/config/env.py
@@ -49,6 +49,7 @@ DEFAULTS = {
     "SANCTION_LIST_CC_MAIL": (str, "dfam-cashassistance@unicef.org"),
     "ELASTICSEARCH_HOST": (str, "http://elasticsearch:9200"),
     "ELASTICSEARCH_INDEX_PREFIX": (str, ""),
+    "ELASTICSEARCH_SYNONYMS_FILE": (str, "/app/data/synonyms.txt"),
     "RAPID_PRO_URL": (str, "https://rapidpro.io"),
     "DATAMART_USER": (str, ""),
     "DATAMART_URL": (str, "https://datamart-dev.unicef.io"),
