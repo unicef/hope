3871e70ed464270f78630180c586a52cb28407f4
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2025-01-17 05:27:29 +0100

    Global dasboard

--- src/hct_mis_api/apps/dashboard/serializers.py
+++ src/hct_mis_api/apps/dashboard/serializers.py
@@ -1,23 +1,30 @@
 from rest_framework import serializers
 
 
-class DashboardHouseholdSerializer(serializers.Serializer):
-    currency = serializers.CharField()
+class DashboardBaseSerializer(serializers.Serializer):
     total_delivered_quantity_usd = serializers.DecimalField(max_digits=12, decimal_places=2)
-    total_delivered_quantity = serializers.DecimalField(max_digits=12, decimal_places=2)
     payments = serializers.IntegerField()
     individuals = serializers.IntegerField()
     households = serializers.IntegerField()
     children_counts = serializers.IntegerField()
-    month = serializers.CharField()
     year = serializers.IntegerField()
     program = serializers.CharField()
     sector = serializers.CharField()
     status = serializers.CharField()
-    admin1 = serializers.CharField()
     fsp = serializers.CharField()
     delivery_types = serializers.CharField()
     pwd_counts = serializers.IntegerField()
     reconciled = serializers.IntegerField()
     finished_payment_plans = serializers.IntegerField()
     total_payment_plans = serializers.IntegerField()
+
+
+class DashboardHouseholdSerializer(DashboardBaseSerializer):
+    month = serializers.CharField()
+    admin1 = serializers.CharField()
+    currency = serializers.CharField()
+    total_delivered_quantity = serializers.DecimalField(max_digits=12, decimal_places=2)
+
+
+class DashboardGlobalSerializer(DashboardBaseSerializer):
+    country = serializers.CharField()
