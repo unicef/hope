718e759d32ae44ce5902a852451c43bd542454ad
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-01-23 04:52:44 +0100

    add signal for creating new UNICEF subpartner when creating new BA

--- src/hct_mis_api/apps/core/signals.py
+++ src/hct_mis_api/apps/core/signals.py
@@ -1,10 +1,11 @@
 from typing import Any
 
 from django.core.exceptions import ValidationError
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
 
-from hct_mis_api.apps.core.models import DataCollectingType
+from hct_mis_api.apps.account.models import Partner, RoleAssignment, Role
+from hct_mis_api.apps.core.models import DataCollectingType, BusinessArea
 
 
 @receiver(m2m_changed, sender=DataCollectingType.compatible_types.through)
@@ -15,3 +16,22 @@ def validate_compatible_types(
         incompatible_dcts = DataCollectingType.objects.filter(pk__in=pk_set).exclude(type=instance.type)
         if incompatible_dcts.exists():
             raise ValidationError("DCTs of different types cannot be compatible with each other.")
+
+
+@receiver(post_save, sender=BusinessArea)
+def business_area_created(sender: Any, instance: BusinessArea, created: bool, **kwargs: Any) -> None:
+    """
+    Create new UNICEF subpartners for the new business area
+    """
+    if created:
+        unicef_partner = Partner.objects.get(name="UNICEF")
+        unicef_subpartner = Partner.objects.create(name=f"UNICEF Partner for {instance.slug}", parent=unicef_partner)
+        role_for_unicef_subpartners = Role.objects.get(name="Role for UNICEF Partners")
+        unicef_subpartner.allowed_business_areas.add(instance)
+        RoleAssignment.objects.create(
+            user=None,
+            partner=unicef_subpartner,
+            role=role_for_unicef_subpartners,
+            business_area=instance,
+            program=None
+        )
