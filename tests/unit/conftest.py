7d6ac7de162abe21469c9b0e7a76ae37b9bc8959
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-01-30 01:57:20 +0100

    include fixtures for transaction and non transaction TestCase

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -25,9 +25,22 @@ def create_unicef_partner(db: Any) -> None:
     Partner.objects.get_or_create(name=settings.UNICEF_HQ_PARTNER, parent=unicef)
 
 
+@pytest.fixture(scope="session", autouse=True)
+def create_unicef_partner_session(django_db_setup: Any, django_db_blocker: Any) -> None:
+    with django_db_blocker.unblock():
+        unicef, _ = Partner.objects.get_or_create(name="UNICEF")
+        Partner.objects.get_or_create(name=settings.UNICEF_HQ_PARTNER, parent=unicef)
+
+
 @pytest.fixture(autouse=True)
 def create_role_with_all_permissions(db: Any) -> None:
     Role.objects.get_or_create(name="Role with all permissions")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_role_with_all_permissions_session(django_db_setup: Any, django_db_blocker: Any) -> None:
+    with django_db_blocker.unblock():
+        Role.objects.get_or_create(name="Role with all permissions")
 
 
 def pytest_addoption(parser: Parser) -> None:
