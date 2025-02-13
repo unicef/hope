adb63947fe49636cd2374f02e33d3a61dc5003c8
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2025-02-13 10:52:29 +0100

    generate rest types

---
+++ src/frontend/generated/models/BiometricDeduplicationBatchStatusEnum.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * * `DUPLICATE_IN_BATCH` - Duplicate in batch
+ * * `NOT_PROCESSED` - Not Processed
+ * * `SIMILAR_IN_BATCH` - Similar in batch
+ * * `UNIQUE_IN_BATCH` - Unique in batch
+ */
+export enum BiometricDeduplicationBatchStatusEnum {
+    DUPLICATE_IN_BATCH = 'DUPLICATE_IN_BATCH',
+    NOT_PROCESSED = 'NOT_PROCESSED',
+    SIMILAR_IN_BATCH = 'SIMILAR_IN_BATCH',
+    UNIQUE_IN_BATCH = 'UNIQUE_IN_BATCH',
+}
