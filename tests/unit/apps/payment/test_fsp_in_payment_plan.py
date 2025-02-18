9cfebaa1f62edbd96bf62d005908a6b5e66cffe5
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-02-18 15:43:17 +0100

    remove prints

--- tests/unit/apps/payment/test_fsp_in_payment_plan.py
+++ tests/unit/apps/payment/test_fsp_in_payment_plan.py
@@ -1063,11 +1063,9 @@ class TestVolumeByDeliveryMechanism(APITestCase):
 class TestValidateFSPPerDeliveryMechanism(APITestCase):
     @classmethod
     def setUpTestData(cls) -> None:
-        print("Before Setup")
         super().setUpTestData()
         base_setup(cls)
         payment_plan_setup(cls)
-        print("After Setup")
 
     def test_chosen_delivery_mechanism_not_supported_by_fsp(self) -> None:
         dm1 = DeliveryMechanismPerPaymentPlanFactory(
