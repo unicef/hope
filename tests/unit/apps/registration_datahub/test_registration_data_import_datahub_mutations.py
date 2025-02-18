7d834d1031e241dad6047f26320369de60a31b03
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-02-18 03:06:47 +0100

    test_registration_data_import_datahub_mutations

--- tests/unit/apps/registration_datahub/test_registration_data_import_datahub_mutations.py
+++ tests/unit/apps/registration_datahub/test_registration_data_import_datahub_mutations.py
@@ -206,7 +206,7 @@ class TestRegistrationDataImportDatahubMutations(APITestCase):
             number_of_households=3,
             number_of_individuals=6,
         )
-        self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
+        self.create_user_role_with_permissions(self.user, permissions, self.business_area, program)
         self.create_partner_role_with_permissions(self.user.partner, [], program.business_area, program)
         self.snapshot_graphql_request(
             request_string=self.CREATE_REGISTRATION_DATA_IMPORT,
