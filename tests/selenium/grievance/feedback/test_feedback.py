197be34caca61c434975c9af3c136654561ea759
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-02-18 07:54:13 +0100

    format

--- tests/selenium/grievance/feedback/test_feedback.py
+++ tests/selenium/grievance/feedback/test_feedback.py
@@ -439,7 +439,6 @@ class TestFeedback:
         pageFeedback.getRow(0).click()
         assert "-" in pageFeedbackDetails.getProgramme().text
         pageFeedbackDetails.getButtonEdit().click()
-        from hct_mis_api.apps.program.models import Program
 
         pageNewFeedback.selectProgramme("Draft Program")
         pageNewFeedback.getDescription().click()
