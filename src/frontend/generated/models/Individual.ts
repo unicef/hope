3ef184d9d462955220923dc1d5f883a70278b778
Domenico DiNicola <dom.dinicola@gmail.com>
2025-02-21 11:13:51 +0100

    removes

--- src/frontend/generated/models/Individual.ts
+++ src/frontend/generated/models/Individual.ts
@@ -111,9 +111,6 @@ export type Individual = {
     blockchain_name?: string;
     wallet_address?: string;
     origin_unicef_id?: string | null;
-    is_migration_handled?: boolean;
-    migrated_at?: string | null;
-    mis_unicef_id?: string | null;
     individual_collection?: number | null;
     program?: string | null;
     /**
