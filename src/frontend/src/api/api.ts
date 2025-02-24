a12d76993baf990f514042398c1d0a93c2a24c73
Maciej Szewczyk <maciej.szewczyk@tivix.com>
2025-02-24 10:01:31 +0100

    fix build

--- src/frontend/src/api/api.ts
+++ src/frontend/src/api/api.ts
@@ -159,11 +159,11 @@ export const handleMutationError = (error: any, action: string): never => {
   throw new Error(`Failed to ${action}: ${errorMessage}`);
 };
 
-export const postRequest = async <T>(
+export const postRequest = async (
   url: string,
   body: any,
   errorMessage: string,
-): Promise<T> => {
+) => {
   try {
     const response = await api.post(url, body);
     return response.data as T;
