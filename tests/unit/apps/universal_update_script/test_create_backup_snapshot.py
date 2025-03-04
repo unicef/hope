ed5fda2be8f763778cc8eb0dc6f9eb840bd6a1f8
Jan Romaniak <jan.romaniak@kellton.com>
2025-03-04 13:57:13 +0100

    Test for generate snapshot error handling

--- tests/unit/apps/universal_update_script/test_create_backup_snapshot.py
+++ tests/unit/apps/universal_update_script/test_create_backup_snapshot.py
@@ -138,3 +138,39 @@ def test_snapshot_json_generation_with_mocking(monkeypatch: MonkeyPatch, program
     assert captured_unicef_ids == ["IND-100"]
     backup_content = uu.backup_snapshot.read()
     assert backup_content.decode("utf-8") == '{"dummy": "snapshot"}'
+
+
+def test_snapshot_json_too_many_unicef_ids(program: Program, individuals: Tuple[Individual]) -> None:
+    log_message: Callable[[str], None] = lambda message_log: None
+    with pytest.raises(Exception) as exc_info:
+        create_snapshot_content(
+            log_message, str(program.id), ["IND-00-0000.0022", "IND-00-0000.0033", "IND-00-0000.0044"]
+        )
+    assert "Some unicef ids are not in the program" in str(exc_info.value)
+
+
+def test_snapshot_json_generation_no_unicef_id(monkeypatch: MonkeyPatch, program: Program) -> None:
+    uu = UniversalUpdate.objects.create(program=program, unicef_ids="IND-100")
+
+    wb = Workbook()
+    excel_bytes = BytesIO()
+    wb.save(excel_bytes)
+    excel_bytes.seek(0)
+
+    uu.update_file.save("update.xlsx", ContentFile(excel_bytes.read()))
+    uu.save()
+
+    captured_unicef_ids = []
+
+    def dummy_create_snapshot_content(log_message: Callable[[str], None], program_id: str, unicef_ids: [str]) -> str:
+        captured_unicef_ids.extend(unicef_ids)
+        return '{"dummy": "snapshot"}'
+
+    monkeypatch.setattr(
+        "hct_mis_api.apps.universal_update_script.universal_individual_update_service.create_backup_snapshot.create_snapshot_content",
+        dummy_create_snapshot_content,
+    )
+
+    with pytest.raises(Exception) as exc_info:
+        create_and_save_snapshot_chunked(uu)
+    assert "The column 'unicef_id' was not found in the header row." in str(exc_info.value)
