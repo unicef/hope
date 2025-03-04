f470e8e8b0368b557430d361b2a1700b3bb0f40c
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-03-04 13:08:00 +0100

    additional fields

--- src/hct_mis_api/apps/core/api/serializers.py
+++ src/hct_mis_api/apps/core/api/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from hct_mis_api.apps.account.api.fields import Base64ModelField
 from hct_mis_api.apps.core.models import BusinessArea
 
 
 class BusinessAreaSerializer(serializers.ModelSerializer):
+    id = Base64ModelField(model_name="BusinessArea")
+
     class Meta:
         model = BusinessArea
         fields = (
