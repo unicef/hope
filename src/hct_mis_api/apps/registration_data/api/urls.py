1f3e47665d4faede6291fd5d4e64bb0ca57e74ad
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-03-12 19:11:32 +0100

    use ProgramBaseNestedRouter class instead of get_program_nested_router function

--- src/hct_mis_api/apps/registration_data/api/urls.py
+++ src/hct_mis_api/apps/registration_data/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from hct_mis_api.apps.program.api.urls import get_program_nested_router
+from hct_mis_api.apps.program.api.urls import program_base_router
 from hct_mis_api.apps.registration_data.api.views import (
     RegistrationDataImportViewSet,
     WebhookDeduplicationView,
@@ -8,7 +8,7 @@ from hct_mis_api.apps.registration_data.api.views import (
 
 app_name = "registration_data"
 
-program_nested_router = get_program_nested_router()
+program_nested_router = program_base_router.program_nested_router
 program_nested_router.register(
     "registration-data-imports",
     RegistrationDataImportViewSet,
--- src/hct_mis_api/apps/targeting/api/urls.py
+++ src/hct_mis_api/apps/targeting/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 
-from hct_mis_api.apps.program.api.urls import get_program_nested_router
+from hct_mis_api.apps.program.api.urls import program_base_router
 from hct_mis_api.apps.targeting.api.views import TargetPopulationViewSet
 
 app_name = "targeting"
 
-program_nested_router = get_program_nested_router()
+program_nested_router = program_base_router.program_nested_router
 
 program_nested_router.register(
     r"target-populations",
