1dba1a3ddfd744768ea7fd1ac0ee8faada04f068
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-03-13 03:20:59 +0100

    fix payment plan managerial view tests

--- src/hct_mis_api/apps/payment/api/views.py
+++ src/hct_mis_api/apps/payment/api/views.py
@@ -44,7 +44,6 @@ logger = logging.getLogger(__name__)
 
 class PaymentPlanMixin:
     serializer_class = PaymentPlanSerializer
-    permission_classes = []
     filter_backends = (
         filters.DjangoFilterBackend,
         SearchFilter,
