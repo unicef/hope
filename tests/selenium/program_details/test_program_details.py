38a5821739c1fa62c8cda5f6ebafaba922256a82
pavlo-mk <pv.pasha.pv@gmail.com>
2025-03-18 15:23:10 +0100

    update e2e

--- tests/selenium/program_details/test_program_details.py
+++ tests/selenium/program_details/test_program_details.py
@@ -700,10 +700,10 @@ class TestProgrammeDetails:
         )
         pageProgrammeDetails.getButtonSave().click()
         for _ in range(50):
-            if "Start Date cannot be before Programme Start Date" in pageProgrammeDetails.getStartDateCycleDiv().text:
+            if "Start Date is required" in pageProgrammeDetails.getStartDateCycleDiv().text:
                 break
             sleep(0.1)
-        assert "Start Date cannot be before Programme Start Date" in pageProgrammeDetails.getStartDateCycleDiv().text
+        assert "Start Date is required" in pageProgrammeDetails.getStartDateCycleDiv().text
 
         pageProgrammeDetails.getStartDateCycle().click()
         pageProgrammeDetails.getStartDateCycle().send_keys(
