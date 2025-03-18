721be0232036edc199fe0e6d78cdb5f37e7521a7
marekbiczysko <marek.biczysko@tivix.com>
2025-03-18 11:52:46 +0100

    migration fix

--- src/hct_mis_api/apps/registration_data/migrations/0007_migration.py
+++ src/hct_mis_api/apps/registration_data/migrations/0007_migration.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration_data', '0004_migration'),
+        ('registration_data', '0006_migration'),
     ]
 
     operations = [
