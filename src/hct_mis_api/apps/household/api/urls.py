110019e1f2de366a75535255faec16ca9de4b726
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-03-31 17:07:45 +0200

    register individual urls

--- src/hct_mis_api/apps/household/api/urls.py
+++ src/hct_mis_api/apps/household/api/urls.py
@@ -4,10 +4,12 @@ from hct_mis_api.apps.core.api.urls import get_business_area_nested_router
 from hct_mis_api.apps.household.api.views import (
     HouseholdGlobalViewSet,
     HouseholdViewSet,
+    IndividualViewSet,
+    IndividualGlobalViewSet,
 )
 from hct_mis_api.apps.program.api.urls import program_base_router
 
-app_name = "payment"
+app_name = "household"
 
 program_nested_router = program_base_router.program_nested_router
 program_nested_router.register(
@@ -15,8 +17,15 @@ program_nested_router.register(
     HouseholdViewSet,
     basename="households",
 )
+program_nested_router.register(
+    "individuals",
+    IndividualViewSet,
+    basename="individuals",
+)
 business_area_nested_router = get_business_area_nested_router()
 business_area_nested_router.register("households", HouseholdGlobalViewSet, basename="households-global")
+business_area_nested_router.register("individuals", IndividualGlobalViewSet, basename="individuals-global")
+
 
 urlpatterns = [
     path("", include(program_nested_router.urls)),
