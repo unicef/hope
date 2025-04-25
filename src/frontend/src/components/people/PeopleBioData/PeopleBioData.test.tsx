2500065938d486dbbd7f5e014205cfc1481fb67c
Jan Romaniak <jan.romaniak@kellton.com>
2025-04-25 13:16:28 +0200

    rdi missing tests

---
+++ src/frontend/src/components/people/PeopleBioData/PeopleBioData.test.tsx
@@ -0,0 +1,16 @@
+import { act } from 'react';
+import wait from 'waait';
+// import { MockedProvider } from '@apollo/react-testing';
+// import { render } from '../../../testUtils/testUtils';
+// import { fakeHouseholdChoices } from '../../../../fixtures/population/fakeHouseholdChoices';
+// import { fakeIndividual } from '../../../../fixtures/population/fakeIndividual';
+// import { fakeApolloAllGrievances } from '../../../../fixtures/grievances/fakeApolloAllGrievances';
+// import { fakeGrievancesChoices } from '../../../../fixtures/grievances/fakeGrievancesChoices';
+// import { PeopleBioData } from './PeopleBioData';
+// import { fakeBaseUrl } from '../../../../fixtures/core/fakeBaseUrl';
+
+describe('components/population/IndividualBioData', () => {
+  it('should render', async () => {
+    assert(true);
+  });
+});
