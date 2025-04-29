14980598a5045f4d1f347e40c2f9f133e208b9f6
Jan Romaniak <jan.romaniak@kellton.com>
2025-04-29 16:02:29 +0200

    remove openapi

--- src/frontend/src/restgenerated/models/Admin1Enum.ts
+++ src/frontend/src/restgenerated/models/Admin1Enum.ts
@@ -2,25 +2,4 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-/**
- * * `AF16` - Christinehaven
- * * `HR47` - Kellerville
- * * `UA45` - Lake Daniel
- * * `HR90` - New Victoria
- * * `UA19` - Port Christopher
- * * `HR49` - Port Philipside
- * * `UA79` - Scotthaven
- * * `AF76` - South Rebecca
- * * `AF60` - West Joseshire
- */
-export enum Admin1Enum {
-    AF16 = 'AF16',
-    HR47 = 'HR47',
-    UA45 = 'UA45',
-    HR90 = 'HR90',
-    UA19 = 'UA19',
-    HR49 = 'HR49',
-    UA79 = 'UA79',
-    AF76 = 'AF76',
-    AF60 = 'AF60',
-}
+export type Admin1Enum = string;
