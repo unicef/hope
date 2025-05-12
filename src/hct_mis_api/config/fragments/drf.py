48acfc723ce732156d03dd55abda487cc885a0f7
Domenico DiNicola <dom.dinicola@gmail.com>
2025-05-12 14:22:37 +0200

    settings cleaning

--- src/hct_mis_api/config/fragments/drf.py
+++ src/hct_mis_api/config/fragments/drf.py
@@ -1,3 +1,5 @@
+from django.urls import reverse_lazy
+
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 50,
@@ -7,4 +9,20 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "SECURE_SSL_REDIRECT": True,
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "HOPE API",
+    "DESCRIPTION": "HOPE REST AOI Swagger Documentation",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+    "SWAGGER_UI_DIST": "SIDECAR",
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+    "REDOC_DIST": "SIDECAR",
+}
+
+SWAGGER_SETTINGS = {
+    "LOGOUT_URL": reverse_lazy("logout"),
+    "LOGIN_URL": "/",
+    "SECURITY_DEFINITIONS": {"DRF Token": {"type": "apiKey", "name": "Authorization", "in": "header"}},
 }
--- src/hct_mis_api/middlewares/xframe.py
+++ src/hct_mis_api/middlewares/xframe.py
@@ -1,24 +1,17 @@
-from typing import List, Optional
+from typing import Optional
 
+from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
 
 class AllowSpecificIframeDomainsMiddleware(MiddlewareMixin):
-    ALLOWED_IFRAME_DOMAINS: List[str] = [
-        "https://localhost:3000",
-        "https://dev-hope.unitst.org",
-        "https://trn-hope.unitst.org",
-        "https://stg-hope.unitst.org",
-        "https://hope.unitst.org",
-    ]
-
     def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
         origin: Optional[str] = request.META.get("HTTP_ORIGIN")
         referer: Optional[str] = request.META.get("HTTP_REFERER")
 
-        if origin in self.ALLOWED_IFRAME_DOMAINS or (
-            referer and any(domain in referer for domain in self.ALLOWED_IFRAME_DOMAINS)
+        if origin in settings.ALLOWED_IFRAME_DOMAINS or (
+            referer and any(domain in referer for domain in settings.ALLOWED_IFRAME_DOMAINS)
         ):
             response["X-Frame-Options"] = "ALLOW-FROM " + (origin or referer or "")
 
