b24b13d3fcbc58b816d2e61159712c7a11f6f314
pavlo-mk <pv.pasha.pv@gmail.com>
2025-05-15 09:00:10 +0200

    api types

--- src/frontend/src/restgenerated/models/Admin1Enum.ts
+++ src/frontend/src/restgenerated/models/Admin1Enum.ts
@@ -2,23 +2,4 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-/**
- * * `AF17` - Bryanburgh
- * * `UA25` - East Dustin
- * * `UA65` - Lewishaven
- * * `HR31` - New Casey
- * * `AF85` - New Juan
- * * `UA47` - North Jeffrey
- * * `HR72` - Port Christopherton
- * * `AF96` - South Jeremiahview
- */
-export enum Admin1Enum {
-    AF17 = 'AF17',
-    UA25 = 'UA25',
-    UA65 = 'UA65',
-    HR31 = 'HR31',
-    AF85 = 'AF85',
-    UA47 = 'UA47',
-    HR72 = 'HR72',
-    AF96 = 'AF96',
-}
+export type Admin1Enum = string;
