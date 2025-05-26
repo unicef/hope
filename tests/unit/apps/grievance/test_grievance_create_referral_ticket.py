0ea6fa93e2b43a3f20d3d0efb8cb853c9cedffe6
pavlo-mk <pv.pasha.pv@gmail.com>
2025-05-26 15:22:31 +0200

    tests

--- tests/unit/apps/grievance/test_grievance_create_referral_ticket.py
+++ tests/unit/apps/grievance/test_grievance_create_referral_ticket.py
@@ -1,176 +1,176 @@
-from typing import Any, Dict, List, Optional
-
-from django.core.management import call_command
-
-from parameterized import parameterized
-
-from hct_mis_api.apps.account.fixtures import PartnerFactory, UserFactory
-from hct_mis_api.apps.account.permissions import Permissions
-from hct_mis_api.apps.core.base_test_case import APITestCase
-from hct_mis_api.apps.core.fixtures import create_afghanistan
-from hct_mis_api.apps.core.models import BusinessArea
-from hct_mis_api.apps.core.utils import encode_id_base64
-from hct_mis_api.apps.geo import models as geo_models
-from hct_mis_api.apps.geo.fixtures import AreaFactory, AreaTypeFactory
-from hct_mis_api.apps.grievance.models import GrievanceTicket
-from hct_mis_api.apps.household.fixtures import create_household
-from hct_mis_api.apps.program.fixtures import ProgramFactory
-from hct_mis_api.apps.program.models import Program
-
-
-class TestGrievanceCreateReferralTicketQuery(APITestCase):
-    CREATE_GRIEVANCE_MUTATION = """
-    mutation CreateGrievanceTicket($input: CreateGrievanceTicketInput!) {
-      createGrievanceTicket(input: $input) {
-        grievanceTickets{
-          category
-          admin
-          language
-          description
-          consent
-          referralTicketDetails {
-            household {
-              size
-            }
-            individual {
-              fullName
-            }
-          }
-        }
-      }
-    }
-    """
-
-    @classmethod
-    def setUpTestData(cls) -> None:
-        super().setUpTestData()
-        create_afghanistan()
-        call_command("loadcountries")
-        partner = PartnerFactory(name="Partner")
-        cls.user = UserFactory.create(partner=partner)
-        cls.business_area = BusinessArea.objects.get(slug="afghanistan")
-        cls.program = ProgramFactory(status=Program.ACTIVE, business_area=cls.business_area)
-        cls.create_partner_role_with_permissions(partner, [], cls.business_area, cls.program)
-
-        country = geo_models.Country.objects.get(name="Afghanistan")
-        area_type = AreaTypeFactory(
-            name="Admin type one",
-            country=country,
-            area_level=2,
-        )
-        cls.admin_area = AreaFactory(name="City Test", area_type=area_type, p_code="asdfgfhghkjltr")
-
-        cls.household, cls.individuals = create_household(
-            {"size": 1, "business_area": cls.business_area},
-            {"given_name": "John", "family_name": "Doe", "middle_name": "", "full_name": "John Doe"},
-        )
-
-    @parameterized.expand(
-        [
-            (
-                "with_permission",
-                [Permissions.GRIEVANCES_CREATE],
-            ),
-            ("without_permission", []),
-        ]
-    )
-    def test_create_referral_ticket_without_extras(self, _: Any, permissions: List[Permissions]) -> None:
-        self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
-
-        input_data = self._prepare_input()
-
-        self.snapshot_graphql_request(
-            request_string=self.CREATE_GRIEVANCE_MUTATION,
-            context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
-            variables=input_data,
-        )
-
-    @parameterized.expand(
-        [
-            (
-                "with_permission",
-                [Permissions.GRIEVANCES_CREATE],
-            ),
-            ("without_permission", []),
-        ]
-    )
-    def test_create_referral_ticket_with_household_extras(self, _: Any, permissions: List[Permissions]) -> None:
-        self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
-
-        extras = {
-            "household": self.id_to_base64(self.household.id, "HouseholdNode"),
-        }
-        input_data = self._prepare_input(extras)
-
-        self.snapshot_graphql_request(
-            request_string=self.CREATE_GRIEVANCE_MUTATION,
-            context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
-            variables=input_data,
-        )
-
-    @parameterized.expand(
-        [
-            (
-                "with_permission",
-                [Permissions.GRIEVANCES_CREATE],
-            ),
-            ("without_permission", []),
-        ]
-    )
-    def test_create_referral_ticket_with_individual_extras(self, _: Any, permissions: List[Permissions]) -> None:
-        self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
-
-        extras = {
-            "individual": self.id_to_base64(self.individuals[0].id, "IndividualNode"),
-        }
-        input_data = self._prepare_input(extras)
-
-        self.snapshot_graphql_request(
-            request_string=self.CREATE_GRIEVANCE_MUTATION,
-            context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
-            variables=input_data,
-        )
-
-    @parameterized.expand(
-        [
-            (
-                "with_permission",
-                [Permissions.GRIEVANCES_CREATE],
-            ),
-            ("without_permission", []),
-        ]
-    )
-    def test_create_referral_ticket_with_household_and_individual_extras(
-        self, _: Any, permissions: List[Permissions]
-    ) -> None:
-        self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
-
-        extras = {
-            "household": self.id_to_base64(self.household.id, "HouseholdNode"),
-            "individual": self.id_to_base64(self.individuals[0].id, "IndividualNode"),
-        }
-        input_data = self._prepare_input(extras)
-
-        self.snapshot_graphql_request(
-            request_string=self.CREATE_GRIEVANCE_MUTATION,
-            context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
-            variables=input_data,
-        )
-
-    def _prepare_input(self, extras: Optional[Dict] = None) -> Dict:
-        input_data = {
-            "input": {
-                "description": "Test Feedback",
-                "assignedTo": self.id_to_base64(self.user.id, "UserNode"),
-                "category": GrievanceTicket.CATEGORY_REFERRAL,
-                "admin": encode_id_base64(str(self.admin_area.id), "Area"),
-                "language": "Polish, English",
-                "consent": True,
-                "businessArea": "afghanistan",
-            }
-        }
-
-        if extras:
-            input_data["input"]["extras"] = {"category": {"referralTicketExtras": extras}}
-
-        return input_data
+# from typing import Any, Dict, List, Optional
+#
+# from django.core.management import call_command
+#
+# from parameterized import parameterized
+#
+# from hct_mis_api.apps.account.fixtures import PartnerFactory, UserFactory
+# from hct_mis_api.apps.account.permissions import Permissions
+# from hct_mis_api.apps.core.base_test_case import APITestCase
+# from hct_mis_api.apps.core.fixtures import create_afghanistan
+# from hct_mis_api.apps.core.models import BusinessArea
+# from hct_mis_api.apps.core.utils import encode_id_base64
+# from hct_mis_api.apps.geo import models as geo_models
+# from hct_mis_api.apps.geo.fixtures import AreaFactory, AreaTypeFactory
+# from hct_mis_api.apps.grievance.models import GrievanceTicket
+# from hct_mis_api.apps.household.fixtures import create_household
+# from hct_mis_api.apps.program.fixtures import ProgramFactory
+# from hct_mis_api.apps.program.models import Program
+#
+#
+# class TestGrievanceCreateReferralTicketQuery(APITestCase):
+#     CREATE_GRIEVANCE_MUTATION = """
+#     mutation CreateGrievanceTicket($input: CreateGrievanceTicketInput!) {
+#       createGrievanceTicket(input: $input) {
+#         grievanceTickets{
+#           category
+#           admin
+#           language
+#           description
+#           consent
+#           referralTicketDetails {
+#             household {
+#               size
+#             }
+#             individual {
+#               fullName
+#             }
+#           }
+#         }
+#       }
+#     }
+#     """
+#
+#     @classmethod
+#     def setUpTestData(cls) -> None:
+#         super().setUpTestData()
+#         create_afghanistan()
+#         call_command("loadcountries")
+#         partner = PartnerFactory(name="Partner")
+#         cls.user = UserFactory.create(partner=partner)
+#         cls.business_area = BusinessArea.objects.get(slug="afghanistan")
+#         cls.program = ProgramFactory(status=Program.ACTIVE, business_area=cls.business_area)
+#         cls.create_partner_role_with_permissions(partner, [], cls.business_area, cls.program)
+#
+#         country = geo_models.Country.objects.get(name="Afghanistan")
+#         area_type = AreaTypeFactory(
+#             name="Admin type one",
+#             country=country,
+#             area_level=2,
+#         )
+#         cls.admin_area = AreaFactory(name="City Test", area_type=area_type, p_code="asdfgfhghkjltr")
+#
+#         cls.household, cls.individuals = create_household(
+#             {"size": 1, "business_area": cls.business_area},
+#             {"given_name": "John", "family_name": "Doe", "middle_name": "", "full_name": "John Doe"},
+#         )
+#
+#     @parameterized.expand(
+#         [
+#             (
+#                 "with_permission",
+#                 [Permissions.GRIEVANCES_CREATE],
+#             ),
+#             ("without_permission", []),
+#         ]
+#     )
+#     def test_create_referral_ticket_without_extras(self, _: Any, permissions: List[Permissions]) -> None:
+#         self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
+#
+#         input_data = self._prepare_input()
+#
+#         self.snapshot_graphql_request(
+#             request_string=self.CREATE_GRIEVANCE_MUTATION,
+#             context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
+#             variables=input_data,
+#         )
+#
+#     @parameterized.expand(
+#         [
+#             (
+#                 "with_permission",
+#                 [Permissions.GRIEVANCES_CREATE],
+#             ),
+#             ("without_permission", []),
+#         ]
+#     )
+#     def test_create_referral_ticket_with_household_extras(self, _: Any, permissions: List[Permissions]) -> None:
+#         self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
+#
+#         extras = {
+#             "household": self.id_to_base64(self.household.id, "HouseholdNode"),
+#         }
+#         input_data = self._prepare_input(extras)
+#
+#         self.snapshot_graphql_request(
+#             request_string=self.CREATE_GRIEVANCE_MUTATION,
+#             context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
+#             variables=input_data,
+#         )
+#
+#     @parameterized.expand(
+#         [
+#             (
+#                 "with_permission",
+#                 [Permissions.GRIEVANCES_CREATE],
+#             ),
+#             ("without_permission", []),
+#         ]
+#     )
+#     def test_create_referral_ticket_with_individual_extras(self, _: Any, permissions: List[Permissions]) -> None:
+#         self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
+#
+#         extras = {
+#             "individual": self.id_to_base64(self.individuals[0].id, "IndividualNode"),
+#         }
+#         input_data = self._prepare_input(extras)
+#
+#         self.snapshot_graphql_request(
+#             request_string=self.CREATE_GRIEVANCE_MUTATION,
+#             context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
+#             variables=input_data,
+#         )
+#
+#     @parameterized.expand(
+#         [
+#             (
+#                 "with_permission",
+#                 [Permissions.GRIEVANCES_CREATE],
+#             ),
+#             ("without_permission", []),
+#         ]
+#     )
+#     def test_create_referral_ticket_with_household_and_individual_extras(
+#         self, _: Any, permissions: List[Permissions]
+#     ) -> None:
+#         self.create_user_role_with_permissions(self.user, permissions, self.business_area, self.program)
+#
+#         extras = {
+#             "household": self.id_to_base64(self.household.id, "HouseholdNode"),
+#             "individual": self.id_to_base64(self.individuals[0].id, "IndividualNode"),
+#         }
+#         input_data = self._prepare_input(extras)
+#
+#         self.snapshot_graphql_request(
+#             request_string=self.CREATE_GRIEVANCE_MUTATION,
+#             context={"user": self.user, "headers": {"Program": self.id_to_base64(self.program.id, "ProgramNode")}},
+#             variables=input_data,
+#         )
+#
+#     def _prepare_input(self, extras: Optional[Dict] = None) -> Dict:
+#         input_data = {
+#             "input": {
+#                 "description": "Test Feedback",
+#                 "assignedTo": self.id_to_base64(self.user.id, "UserNode"),
+#                 "category": GrievanceTicket.CATEGORY_REFERRAL,
+#                 "admin": encode_id_base64(str(self.admin_area.id), "Area"),
+#                 "language": "Polish, English",
+#                 "consent": True,
+#                 "businessArea": "afghanistan",
+#             }
+#         }
+#
+#         if extras:
+#             input_data["input"]["extras"] = {"category": {"referralTicketExtras": extras}}
+#
+#         return input_data
