f336f1e94beb3007715aaea79bfe1a8d9750a30e
pavlo-mk <pv.pasha.pv@gmail.com>
2025-06-02 14:29:02 +0200

    uni tests

--- src/hct_mis_api/apps/grievance/utils.py
+++ src/hct_mis_api/apps/grievance/utils.py
@@ -6,10 +6,12 @@ from django.contrib.auth.models import AbstractUser
 from django.core.cache import cache
 from django.core.exceptions import PermissionDenied, ValidationError
 from django.db.models import Q, QuerySet
+from django.shortcuts import get_object_or_404
 
 from hct_mis_api.apps.account.models import Partner, User
 from hct_mis_api.apps.account.permissions import Permissions
 from hct_mis_api.apps.accountability.models import Feedback
+from hct_mis_api.apps.core.utils import decode_id_string
 from hct_mis_api.apps.grievance.models import (
     GrievanceDocument,
     GrievanceTicket,
@@ -24,6 +26,12 @@ from hct_mis_api.apps.grievance.validators import validate_file
 from hct_mis_api.apps.household.models import Individual
 
 logger = logging.getLogger(__name__)
+
+
+def get_individual(individual_id: str) -> Individual:
+    decoded_selected_individual_id = decode_id_string(individual_id)
+    individual = get_object_or_404(Individual.objects.select_related("household"), id=decoded_selected_individual_id)
+    return individual
 
 
 def traverse_sibling_tickets(grievance_ticket: GrievanceTicket, selected_individuals: QuerySet[Individual]) -> None:
