9edb91c1aed808e82fd9a752f520eed0cd0d7446
Domenico DiNicola <dom.dinicola@gmail.com>
2025-06-16 14:05:22 +0200

    pg remove extra

--- src/hct_mis_api/apps/payment/services/payment_gateway.py
+++ src/hct_mis_api/apps/payment/services/payment_gateway.py
@@ -60,21 +60,16 @@ class PaymentInstructionFromSplitSerializer(ReadOnlyModelSerializer):
     external_code = serializers.SerializerMethodField()
     fsp = serializers.SerializerMethodField()
     payload = serializers.SerializerMethodField()
-    extra = serializers.SerializerMethodField()
 
     def get_fsp(self, obj: Any) -> str:
         return obj.financial_service_provider.payment_gateway_id
 
-    def get_extra(self, obj: Any) -> Dict:
-        return {
-            "user": self.context["user_email"],
-            "config_key": obj.payment_plan.business_area.code,
-            "delivery_mechanism": obj.delivery_mechanism.code,
-        }
-
     def get_payload(self, obj: Any) -> Dict:
         return {
             "destination_currency": obj.payment_plan.currency,
+            "user": self.context["user_email"],
+            "config_key": obj.payment_plan.business_area.code,
+            "delivery_mechanism": obj.delivery_mechanism.code,
         }
 
     def get_external_code(self, obj: Any) -> str:
@@ -87,7 +82,6 @@ class PaymentInstructionFromSplitSerializer(ReadOnlyModelSerializer):
             "external_code",
             "fsp",
             "payload",
-            "extra",
         ]
 
 
@@ -108,10 +102,6 @@ class PaymentSerializer(ReadOnlyModelSerializer):
     remote_id = serializers.CharField(source="id")
     record_code = serializers.CharField(source="unicef_id")
     payload = serializers.SerializerMethodField()
-    extra_data = serializers.SerializerMethodField()
-
-    def get_extra_data(self, obj: Payment) -> Dict:
-        return {}
 
     def get_payload(self, obj: Payment) -> Dict:
         snapshot = getattr(obj, "household_snapshot", None)
@@ -186,7 +176,6 @@ class PaymentSerializer(ReadOnlyModelSerializer):
             "remote_id",
             "record_code",
             "payload",
-            "extra_data",
         ]
 
 
@@ -241,7 +230,6 @@ class PaymentInstructionData(FlexibleArgumentsDataclassMixin):
     fsp: str
     system: int
     payload: dict
-    extra: dict
     id: Optional[int] = None
 
 
