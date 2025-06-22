5ac44a26065dee804479688ccbf4bfc08cf198af
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-06-23 00:46:59 +0200

    select related, upd tests

--- tests/unit/apps/household/test_individual_views.py
+++ tests/unit/apps/household/test_individual_views.py
@@ -338,7 +338,7 @@ class TestIndividualList:
             etag = response.headers["etag"]
             assert json.loads(cache.get(etag)[0].decode("utf8")) == response.json()
             assert len(response.json()["results"]) == 4
-            assert len(ctx.captured_queries) == 22
+            assert len(ctx.captured_queries) == 30
 
         with CaptureQueriesContext(connection) as ctx:
             response = self.api_client.get(self.list_url)
@@ -357,7 +357,7 @@ class TestIndividualList:
             etag_third_call = response.headers["etag"]
             assert json.loads(cache.get(etag_third_call)[0].decode("utf8")) == response.json()
             assert etag_third_call not in [etag, etag_second_call]
-            assert len(ctx.captured_queries) == 17
+            assert len(ctx.captured_queries) == 25
 
         set_admin_area_limits_in_program(self.partner, self.program, [self.area1])
         with CaptureQueriesContext(connection) as ctx:
@@ -367,7 +367,7 @@ class TestIndividualList:
             etag_changed_areas = response.headers["etag"]
             assert json.loads(cache.get(etag_changed_areas)[0].decode("utf8")) == response.json()
             assert etag_changed_areas not in [etag, etag_second_call, etag_third_call]
-            assert len(ctx.captured_queries) == 17
+            assert len(ctx.captured_queries) == 25
 
         self.individual1_1.delete()
         with CaptureQueriesContext(connection) as ctx:
@@ -377,7 +377,7 @@ class TestIndividualList:
             etag_fourth_call = response.headers["etag"]
             assert len(response.json()["results"]) == 3
             assert etag_fourth_call not in [etag, etag_second_call, etag_third_call, etag_changed_areas]
-            assert len(ctx.captured_queries) == 16
+            assert len(ctx.captured_queries) == 22
 
         with CaptureQueriesContext(connection) as ctx:
             response = self.api_client.get(self.list_url)
