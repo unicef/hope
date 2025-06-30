fde64a411c18ccbbb1159fee8075ee800d21169c
pavlo-mk <pv.pasha.pv@gmail.com>
2025-06-30 15:02:41 +0200

    rm report model

--- src/frontend/src/restgenerated/models/Admin1Enum.ts
+++ src/frontend/src/restgenerated/models/Admin1Enum.ts
@@ -2,25 +2,4 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-/**
- * * `HR85` - Dorseyburgh
- * * `UA59` - East Meghanside
- * * `AF13` - East Ronnieland
- * * `HR59` - Lake Heather
- * * `UA64` - Paultown
- * * `UA36` - Port Leviview
- * * `HR95` - Rebeccatown
- * * `AF10` - South Josephport
- * * `AF43` - West Chad
- */
-export enum Admin1Enum {
-    HR85 = 'HR85',
-    UA59 = 'UA59',
-    AF13 = 'AF13',
-    HR59 = 'HR59',
-    UA64 = 'UA64',
-    UA36 = 'UA36',
-    HR95 = 'HR95',
-    AF10 = 'AF10',
-    AF43 = 'AF43',
-}
+export type Admin1Enum = string;
