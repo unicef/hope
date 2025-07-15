2503bfb0f589b3e252a27218b63518252ec62e4a
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-07-15 12:49:36 +0200

    fix grievance updates with allowing null and blank

--- src/hct_mis_api/apps/core/api/parsers.py
+++ src/hct_mis_api/apps/core/api/parsers.py
@@ -1,12 +1,14 @@
-from typing import IO
+from typing import IO, Any, Mapping, Optional
 
 from nested_multipart_parser.drf import NestedParser
 from rest_framework.exceptions import ParseError
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import DataAndFiles, MultiPartParser
 
 
 class DictDrfNestedParser(MultiPartParser):
-    def parse(self, stream: IO, media_type: str | None = None, parser_context: dict | None = None) -> dict:
+    def parse(
+        self, stream: IO, media_type: str | None = None, parser_context: Optional[Mapping[str, Any]] = None
+    ) -> DataAndFiles:
         clsDataAndFile = super().parse(stream, media_type, parser_context)
 
         data = clsDataAndFile.data.dict()
