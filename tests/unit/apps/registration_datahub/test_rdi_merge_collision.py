78ee26d46b4e1439389b3b44e84e1a2a877a09d7
Jan Romaniak <jan.romaniak@kellton.com>
2025-07-17 09:01:08 +0200

    fixed some tests

---
+++ tests/unit/apps/registration_datahub/test_rdi_merge_collision.py
@@ -0,0 +1,32 @@
+from contextlib import contextmanager
+from typing import Callable, Generator
+
+from django.db import DEFAULT_DB_ALIAS, connections
+
+import pytest
+
+pytestmark = pytest.mark.usefixtures("django_elasticsearch_setup")
+
+
+@contextmanager
+def capture_on_commit_callbacks(
+    *, using: str = DEFAULT_DB_ALIAS, execute: bool = False
+) -> Generator[list[Callable[[], None]], None, None]:
+    callbacks: list[Callable[[], None]] = []
+    start_count = len(connections[using].run_on_commit)
+    try:
+        yield callbacks
+    finally:
+        while True:
+            callback_count = len(connections[using].run_on_commit)
+            for _, callback in connections[using].run_on_commit[start_count:]:
+                callbacks.append(callback)
+                if execute:
+                    callback()
+
+            if callback_count == len(connections[using].run_on_commit):
+                break
+            start_count = callback_count
+
+
+pytest.mark.elasticsearch
