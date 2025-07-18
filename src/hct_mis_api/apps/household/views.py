7a085076c36c378f491e659eb2717bdda00548ef
Marek Biczysko <34810846+MarekBiczysko@users.noreply.github.com>
2025-07-18 15:38:20 +0200

    225156_Fix_too_broad_Exception_handling (#4960)

--- src/hct_mis_api/apps/household/views.py
+++ src/hct_mis_api/apps/household/views.py
@@ -1,12 +1,12 @@
 import logging
 from typing import Dict, Optional, Union
 
+from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from hct_mis_api.apps.core.models import BusinessArea
 from hct_mis_api.apps.core.utils import IDENTIFICATION_TYPE_TO_KEY_MAPPING
 from hct_mis_api.apps.household.filters import _prepare_kobo_asset_id_value
 from hct_mis_api.apps.household.models import (
@@ -34,48 +34,41 @@ def get_individual(tax_id: str, business_area_code: Optional[str]) -> PendingInd
         )
     ).filter(type__key=IDENTIFICATION_TYPE_TO_KEY_MAPPING[IDENTIFICATION_TYPE_TAX_ID], document_number=tax_id)
     if pending_documents.count() > 1:
-        raise Exception(f"Multiple imported documents ({pending_documents.count()}) with given tax_id found")
+        raise ValidationError(f"Multiple imported documents ({pending_documents.count()}) with given tax_id found")
     if pending_documents.count() == 1:
         return pending_documents.first().individual
-    raise Exception("Document with given tax_id not found")
+    raise NotFound(f"Document with given tax_id: {tax_id} not found")
 
 
 def get_household(detail_id: str, business_area_code: Optional[str]) -> Union[PendingHousehold, Household]:
     kobo_asset_value = _prepare_kobo_asset_id_value(detail_id)
-    households = (
-        Household.objects.all()
-        if not business_area_code
-        else Household.objects.filter(business_area__code=business_area_code)
-    ).filter(detail_id__endswith=kobo_asset_value)
+
+    households = Household.objects.filter(detail_id__endswith=kobo_asset_value)
+    if business_area_code:
+        households = households.filter(business_area__code=business_area_code)
     if households.count() > 1:
-        raise Exception(f"Multiple households ({households.count()}) with given detail_id found")
+        raise ValidationError(f"Multiple households ({households.count()}) with given detail_id found")
     if households.count() == 1:
         return households.first()  # type: ignore
 
-    if business_area_code is None:
-        pending_households_by_business_area = PendingHousehold.objects.all()
-    else:
-        business_areas = BusinessArea.objects.filter(code=business_area_code)
-        if not business_areas:
-            raise Exception(f"Business area with code {business_area_code} not found")
-        business_area = business_areas.first()  # code is unique, so no need to worry here
-        pending_households_by_business_area = PendingHousehold.objects.filter(
-            registration_data_import__business_area__slug=business_area.slug
-        )
-
-    imported_households = pending_households_by_business_area.filter(detail_id__endswith=kobo_asset_value)
+    imported_households = PendingHousehold.objects.filter(detail_id__endswith=kobo_asset_value)
+    if business_area_code:
+        imported_households = imported_households.filter(business_area__code=business_area_code)
     if imported_households.count() > 1:
-        raise Exception(f"Multiple imported households ({imported_households.count()}) with given detail_id found")
+        raise ValidationError(
+            f"Multiple imported households ({imported_households.count()}) with given detail_id found"
+        )
     if imported_households.count() == 1:
         return imported_households.first()
-    raise Exception("Household with given detail_id not found")
+
+    raise NotFound(f"Household with given detail_id: {detail_id} not found")
 
 
 def get_household_or_individual(
     tax_id: Optional[str], detail_id: Optional[str], business_area_code: Optional[str]
 ) -> Dict:
     if tax_id and detail_id:
-        raise Exception("tax_id and detail_id are mutually exclusive")
+        raise ValidationError("Tax id and detail_id are mutually exclusive")
 
     if tax_id:
         individual = get_individual(tax_id, business_area_code)
@@ -85,7 +78,7 @@ def get_household_or_individual(
         household = get_household(detail_id, business_area_code)
         return serialize_by_household(household)
 
-    raise Exception("tax_id or detail_id is required")
+    raise ValidationError("tax_id or detail_id is required")
 
 
 class HouseholdStatusView(APIView):
@@ -99,10 +92,6 @@ class HouseholdStatusView(APIView):
         detail_id = query_params.get("detail_id", None)
         business_area_code: Optional[str] = query_params.get("business_area_code")
 
-        try:
-            data = get_household_or_individual(tax_id, detail_id, business_area_code)
-        except Exception as e:  # pragma: no cover
-            logger.warning(e)
-            return Response({"status": "not found", "error_message": "Household not Found"}, status=404)
+        data = get_household_or_individual(tax_id, detail_id, business_area_code)
 
         return Response(data, status=200)
