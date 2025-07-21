a3c3269ee699d2289db1797f8189f0fbe67ccc71
pavlo-mk <pv.pasha.pv@gmail.com>
2025-07-21 16:26:57 +0200

    lines

--- src/hct_mis_api/apps/grievance/mutations.py
+++ src/hct_mis_api/apps/grievance/mutations.py
@@ -930,7 +930,7 @@ class HouseholdDataChangeApproveMutation(DataChangeValidator, PermissionMutation
             if field_name == "flex_fields":
                 for flex_field_name in item.keys():
                     flex_field = household_data["flex_fields"][flex_field_name]
-                    if isinstance(flex_field, dict):
+                    if isinstance(flex_field, dict):  # pragma no cover
                         flex_field["approve_status"] = flex_fields_approve_data.get(flex_field_name)
             if field_name == "roles":
                 role_lookup = {
