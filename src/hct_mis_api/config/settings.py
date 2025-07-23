be24d30557dcc7c1565fd6cf71efe7e8b1715ec0
Domenico DiNicola <dom.dinicola@gmail.com>
2025-07-23 10:54:54 +0200

    hope-app

--- src/hct_mis_api/config/settings.py
+++ src/hct_mis_api/config/settings.py
@@ -166,6 +166,7 @@ TEMPLATES: List[Dict[str, Any]] = [
     },
 ]
 PROJECT_APPS = [
+    "hct_mis_api",
     "hct_mis_api.api",
     "hct_mis_api.apps.geo.apps.Config",
     "hct_mis_api.apps.account.apps.AccountConfig",
