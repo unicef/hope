5caa8375d95801b386275230094fc3f349273606
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-08-01 16:12:08 +0200

    skip pdu upload again - upload file fixed in diff PR

--- tests/e2e/programme_population/test_periodic_data_update_upload.py
+++ tests/e2e/programme_population/test_periodic_data_update_upload.py
@@ -160,6 +160,7 @@ def prepare_xlsx_file(rounds_data: list, rows: list, program: Program) -> _Tempo
 
 @pytest.mark.usefixtures("login")
 class TestPeriodicDataUpdateUpload:
+    @pytest.mark.skip(reason="Unskip after REST refactoring is complete")
     def test_periodic_data_update_upload_success(
         self,
         clear_downloaded_files: None,
@@ -202,6 +203,7 @@ class TestPeriodicDataUpdateUpload:
         assert pageIndividuals.getUpdateStatus(periodic_data_update_upload.pk).text == "SUCCESSFUL"
 
     @pytest.mark.night
+    @pytest.mark.skip(reason="Unskip after REST refactoring is complete")
     def test_periodic_data_update_upload_form_error(
         self,
         clear_downloaded_files: None,
