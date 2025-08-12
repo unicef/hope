43b9e126ca18a9972c9951c165f8c82856efd664
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-08-12 20:42:27 +0200

    mixin for checking if user is authorized for specific pdu edits

---
+++ src/hope/apps/periodic_data_update/api/mixins.py
@@ -0,0 +1,35 @@
+
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.request import Request
+
+from hope.apps.periodic_data_update.models import PeriodicDataUpdateOnlineEdit
+
+
+class PeriodicDataUpdateOnlineEditAuthorizedUserMixin:
+    """
+    This mixin provides an authorization check to ensure that the requesting user
+    is in the `authorized_users` list of the PeriodicDataUpdateOnlineEdit instance.
+    If the action is a 'detail=False', it checks all instances specified in the 'ids' field of the request data.
+    """
+
+    def check_user_authorization(self, request: Request) -> None:
+        user = request.user
+        if not user or not user.is_authenticated:
+            return
+
+        if self.detail:
+            instance = self.get_object()
+            if not instance.authorized_users.filter(pk=user.pk).exists():
+                raise PermissionDenied("You are not an authorized user for this PDU online edit.")
+        else:
+            ids = request.data.get("ids")
+            if ids:
+                queryset = PeriodicDataUpdateOnlineEdit.objects.filter(pk__in=ids)
+                if queryset.count() != len(ids):
+                    raise PermissionDenied("One or more PDU online edits not found.")
+
+                queryset_unauthorized = queryset.exclude(authorized_users__pk=user.pk)
+                if queryset_unauthorized.exists():
+                    raise PermissionDenied(
+                        f"You are not an authorized user for PDU Online Edit: {queryset_unauthorized.values_list('id', flat=True)}"
+                    )
