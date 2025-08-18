c122934e5d24f3f057cca61d16b28eb687029547
pavlo-mk <pv.pasha.pv@gmail.com>
2025-08-18 19:12:15 +0200

    role approve

--- src/hope/apps/grievance/api/views.py
+++ src/hope/apps/grievance/api/views.py
@@ -860,10 +860,12 @@ class GrievanceTicketGlobalViewSet(
             elif field_name == "roles":
                 for role in household_data["roles"]:
                     individual_id = role["individual_id"]
-                    if household_approve_data.get(individual_id):
-                        role["approve_status"] = True
-                    else:
-                        role["approve_status"] = False
+                    approve_lookup = {
+                        individual_id: str(role.get("approve_status", "")).lower() == "true"
+                        for role in household_approve_data.get("roles", [])
+                    }
+                    for role in household_data["roles"]:
+                        role["approve_status"] = approve_lookup.get(individual_id, False)
             elif household_approve_data.get(field_name):
                 household_data[field_name]["approve_status"] = True
             else:
