88badb79aed715670068150c513581699d89835a
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-08-18 15:24:38 +0200

    fix generate_slug

--- src/hope/apps/program/migrations/0006_migration.py
+++ src/hope/apps/program/migrations/0006_migration.py
@@ -7,7 +7,7 @@ def generate_slug(apps, schema_editor):
     Program = apps.get_model("program", "Program")
     for program in Program.objects.all():
         if not program.programme_code:
-            program.programme_code = program.generate_code()
+            program.programme_code = program.generate_programme_code()
         program.slug = program.programme_code.lower().replace("/", "_").replace(".", "*")
         program.save()
 
