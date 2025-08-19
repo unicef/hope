845c43f7160cd69fec3bcdefb6b54a3e49b4d415
pavlo-mk <pv.pasha.pv@gmail.com>
2025-08-19 06:31:24 +0200

    fixes

--- src/hct_mis_api/apps/payment/xlsx/xlsx_payment_plan_per_fsp_import_service.py
+++ src/hct_mis_api/apps/payment/xlsx/xlsx_payment_plan_per_fsp_import_service.py
@@ -159,12 +159,14 @@ class XlsxPaymentPlanImportPerFspService(XlsxImportBaseService):
 
             if delivery_date != payment.delivery_date:
                 self.is_updated = True
+            # convert to date
+            delivery_date = delivery_date.date() if isinstance(delivery_date, datetime.datetime) else delivery_date
             if delivery_date > datetime.date.today() or delivery_date < self.payment_plan.program.start_date:
                 self.errors.append(
                     XlsxError(
                         self.sheetname,
                         cell.coordinate,
-                        f"Payment {payment_id}: Delivery date cannot be greater than today's date, and cannot be before Programme's start date",
+                        f"Payment {payment_id}: Delivery date ({delivery_date}) cannot be greater than today's date, and cannot be before Programme's start date",
                     )
                 )
         except Exception:
@@ -316,9 +318,17 @@ class XlsxPaymentPlanImportPerFspService(XlsxImportBaseService):
         if payment_delivery_date := payment.delivery_date:
             payment_delivery_date = payment.delivery_date.replace(tzinfo=None)
 
-        if delivery_date > datetime.date.today() or delivery_date < self.payment_plan.program.start_date:
+        # convert to date
+        delivery_date = delivery_date.date() if isinstance(delivery_date, datetime.datetime) else delivery_date
+
+        if (
+            delivery_date
+            and delivery_date > datetime.date.today()
+            or delivery_date
+            and delivery_date < self.payment_plan.program.start_date
+        ):
             # validate and skip update the date
-            delivered_quantity = payment_delivery_date
+            delivery_date = payment_delivery_date
 
         if delivered_quantity is not None and str(delivered_quantity).strip() != "":
             status, delivered_quantity = self._get_delivered_quantity_status_and_value(
