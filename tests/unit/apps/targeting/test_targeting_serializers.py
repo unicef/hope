a08a4d789091bf12f518d9ad494b8e956c597601
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-08-21 17:34:31 +0200

    upd unit tests failing after rounds_covered added

--- tests/unit/apps/targeting/test_targeting_serializers.py
+++ tests/unit/apps/targeting/test_targeting_serializers.py
@@ -265,6 +265,7 @@ class TargetingCriteriaSerializerTest(TestCase):
                 "subtype": pdu_data.subtype,
                 "number_of_rounds": pdu_data.number_of_rounds,
                 "rounds_names": pdu_data.rounds_names,
+                "rounds_covered": pdu_data.rounds_covered,
             },
         }
         assert field_attribute_data == expected_field_attribute_data
