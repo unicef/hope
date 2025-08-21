8941fa0435b6e7f463fbc96835fda2d4dee9b977
marekbiczysko <marek.biczysko@tivix.com>
2025-08-21 16:08:42 +0200

    QCFReportsService fixes

--- src/hct_mis_api/apps/payment/services/qcf_reports_service.py
+++ src/hct_mis_api/apps/payment/services/qcf_reports_service.py
@@ -1,12 +1,15 @@
 import csv
 import dataclasses
 import io
+import logging
 import zipfile
 from collections import defaultdict
 from datetime import datetime
-from typing import List, Tuple
+from tempfile import NamedTemporaryFile
+from typing import IO, List, Tuple
 
 from django.contrib.admin.options import get_content_type_for_model
+from django.core.files import File
 from django.core.files.base import ContentFile
 from django.db import transaction
 from django.template.loader import render_to_string
@@ -24,6 +27,8 @@ from hct_mis_api.apps.payment.models.payment import (
     WesternUnionQCFFileReport,
 )
 from hct_mis_api.apps.payment.services.western_union_ftp import WesternUnionFTPClient
+
+logger = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
@@ -46,7 +51,7 @@ class QCFReportPaymentPlanData:
     refunds_total_negative: float = dataclasses.field(init=False)
     payments_data: List[QCFReportPaymentRowData]
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.principal_total = sum(p.principal_amount for p in self.payments_data)
         self.charges_total = sum(p.charges_amount for p in self.payments_data)
         self.refunds_total_positive = sum(p.refund_amount for p in self.payments_data if p.refund_amount >= 0)
@@ -57,45 +62,54 @@ class QCFReportsService:
     class QCFReportsServiceException(Exception):
         pass
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.ftp_client = WesternUnionFTPClient()
 
-    def process_zip_file(self, filename, file_like) -> None:
-        with zipfile.ZipFile(io.BytesIO(file_like.read())) as z:
+    def process_zip_file(self, file_content: io.BytesIO, wu_qcf_file: WesternUnionQCFFile) -> None:
+        with zipfile.ZipFile(file_content) as z:
             for zip_info in z.infolist():
                 if zip_info.filename.endswith(".cf") or zip_info.filename.endswith(".CF"):
                     with z.open(zip_info) as txt_file:
-                        self.process_report_file(filename, txt_file)
+                        self.process_qcf_report_file(zip_info.filename, txt_file, wu_qcf_file)
 
     def process_files_since(self, date_from: datetime) -> None:
-        files = self.ftp_client.get_files_since(date_from)
+        files: List[Tuple[str, io.BytesIO]] = self.ftp_client.get_files_since(date_from)
         for filename, file_like in files:
             if not WesternUnionQCFFile.objects.filter(name=filename).exists():
-                self.process_zip_file(filename, file_like)
+                wu_qcf_file = self.store_qcf_file(filename, file_like)
+                self.process_zip_file(file_like, wu_qcf_file)
 
-    def store_qu_qcf_file(self, filename: str, file_content: str) -> WesternUnionQCFFile:
-        content_file = ContentFile(file_content, name=filename)
-        wu_qcf_file = WesternUnionQCFFile.objects.create(
+    def test_process_file(self, filename: str, file_like: io.BytesIO) -> None:
+        # TODO remove after testing
+        wu_qcf_file = self.store_qcf_file(filename, file_like)
+        self.process_zip_file(file_like, wu_qcf_file)
+
+    def store_qcf_file(self, filename: str, file_like: io.BytesIO) -> WesternUnionQCFFile:
+        content_file = ContentFile(file_like.read(), name=filename)
+        wu_qcf_file, created = WesternUnionQCFFile.objects.get_or_create(
             name=filename,
         )
-        file_temp = FileTemp.objects.create(
-            object_id=wu_qcf_file.pk,
-            content_type=get_content_type_for_model(wu_qcf_file),
-            file=content_file,
-        )
-        file_temp.file.save(filename, content_file)
-        wu_qcf_file.file = file_temp
-        wu_qcf_file.save()
+        if created:
+            file_temp = FileTemp.objects.create(
+                object_id=wu_qcf_file.pk,
+                content_type=get_content_type_for_model(wu_qcf_file),
+                file=content_file,
+            )
+            file_temp.file.save(filename, content_file)
+            wu_qcf_file.file = file_temp
+            wu_qcf_file.save()
 
         return wu_qcf_file
 
-    def process_report_file(self, filename: str, txt_file) -> None:
+    def process_qcf_report_file(self, filename: str, txt_file: IO[bytes], wu_qcf_file: WesternUnionQCFFile) -> None:
         file_content = txt_file.read()
-        wu_qcf_file = self.store_qu_qcf_file(filename, file_content)
-
         payment_plan_id_payments_lines_map = defaultdict(list)
 
-        decoded_content = file_content.decode("utf-8")
+        try:
+            decoded_content = file_content.decode("utf-8")
+        except UnicodeDecodeError:
+            raise self.QCFReportsServiceException(f"Could not decode QCF report file {filename}")
+
         lines = list(csv.reader(io.StringIO(decoded_content)))
         for fields in lines[1:-1]:
             record_type = fields[0]
@@ -106,28 +120,30 @@ class QCFReportsService:
                     payment = Payment.objects.get(unicef_id=payment_unicef_id)
                     payment_plan_id_payments_lines_map[payment.parent_id].append((payment, fields))
                 except Payment.DoesNotExist:
-                    print(f"Payment {payment_unicef_id} does not exist")
+                    logger.error(f"{wu_qcf_file}: File:{filename}, Payment {payment_unicef_id} does not exist")
                     continue
 
         for payment_plan_id, payment_raw_data in payment_plan_id_payments_lines_map.items():
             payment_plan = PaymentPlan.objects.get(id=payment_plan_id)
 
             with transaction.atomic():
-                report = self.generate_report(payment_plan, payment_raw_data)
+                report_name, report = self.generate_report(payment_plan, payment_raw_data)
 
                 wu_qcf_report = WesternUnionQCFFileReport.objects.create(
-                    name=report.name,
                     qcf_file=wu_qcf_file,
                     payment_plan=payment_plan,
                 )
-                file_temp = FileTemp.objects.create(
-                    object_id=wu_qcf_report.pk,
-                    content_type=get_content_type_for_model(wu_qcf_report),
-                    file=report,
-                )
-                file_temp.file.save(filename, report)
-                wu_qcf_file.report_file = file_temp
-                wu_qcf_file.save()
+
+                with NamedTemporaryFile() as tmp:
+                    file_temp = FileTemp.objects.create(
+                        object_id=str(wu_qcf_report.pk),
+                        content_type=get_content_type_for_model(wu_qcf_report),
+                    )
+                    report.save(tmp.name)
+                    tmp.seek(0)
+                    file_temp.file.save(report_name, File(tmp))
+                    wu_qcf_report.report_file = file_temp
+                    wu_qcf_report.save()
 
                 transaction.on_commit(
                     lambda wu_qcf_report_id=wu_qcf_report.id: send_qcf_report_email_notifications.delay(
@@ -135,7 +151,9 @@ class QCFReportsService:
                     )
                 )
 
-    def generate_report(self, payment_plan: PaymentPlan, payment_raw_data: List[Tuple[Payment, dict]]) -> ContentFile:
+    def generate_report(
+        self, payment_plan: PaymentPlan, payment_raw_data: List[Tuple[Payment, list]]
+    ) -> Tuple[str, openpyxl.Workbook]:
         no_of_qcf_reports_for_pp = WesternUnionQCFFileReport.objects.filter(payment_plan=payment_plan).count()
         report_filename = (
             f"QCF_{payment_plan.business_area.slug}_{payment_plan.unicef_id}_{no_of_qcf_reports_for_pp + 1}.xlsx"
@@ -143,16 +161,16 @@ class QCFReportsService:
 
         funds_commitments = (
             payment_plan.funds_commitments.all().values_list("rec_serial_number", flat=True) or []
-        )  # TODO rec_serial_number?
+        )  # TODO show FC rec_serial_number?
         funds_commitments_str = ", ".join(str(v) for v in funds_commitments) if funds_commitments else "No FC assigned"
 
-        payments_data = []
+        payments_data: List[QCFReportPaymentRowData] = []
         for payment, fields in payment_raw_data:
             mtcn = fields[1]
             if mtcn != payment.fsp_auth_code:
                 # TODO should we raise it and expose mtcn?
                 raise self.QCFReportsServiceException(
-                    f"MTCN {mtcn} does not match payment fsp_auth_code for payment {payment.id}"
+                    f"MTCN {mtcn} does not match payment fsp_auth_code for payment {payment.unicef_id}: {payment.fsp_auth_code}"
                 )
 
             payments_data.append(
@@ -161,9 +179,9 @@ class QCFReportsService:
                     payment_plan_unicef_id=payment_plan.unicef_id,
                     programme=payment_plan.program_cycle.program.name,
                     fc=funds_commitments_str,
-                    principal_amount=fields[10],
-                    charges_amount=fields[11],
-                    refund_amount=fields[0],
+                    principal_amount=float(fields[10]),
+                    charges_amount=float(fields[11]),
+                    refund_amount=float(fields[0]),
                 )
             )
 
@@ -171,9 +189,9 @@ class QCFReportsService:
             payment_plan_unicef_id=payment_plan.unicef_id, payments_data=payments_data
         )
 
-        return self.generate_report_xlsx(report_filename, report_data)
+        return report_filename, self.generate_report_xlsx(report_filename, report_data)
 
-    def generate_report_xlsx(self, filename: str, report_data: QCFReportPaymentPlanData) -> ContentFile:
+    def generate_report_xlsx(self, filename: str, report_data: QCFReportPaymentPlanData) -> openpyxl.Workbook:
         wb = openpyxl.Workbook()
         ws = wb.active
         ws.title = f"QCF Report - {report_data.payment_plan_unicef_id}"
@@ -208,12 +226,7 @@ class QCFReportsService:
         for row in range(last_row - 3, last_row + 1):
             ws[f"A{row}"].font = Font(bold=True)
 
-        output = io.BytesIO()
-        wb.save(output)
-        output.seek(0)
-        content_file = ContentFile(output.read(), name=filename)
-
-        return content_file
+        return wb
 
     def send_notification_emails(self, report: WesternUnionQCFFileReport) -> None:
         """
