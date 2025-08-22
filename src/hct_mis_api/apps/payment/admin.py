d9bb0080c63e49939c72a62d254a44ec7cb82307
marekbiczysko <marek.biczysko@tivix.com>
2025-08-22 16:48:17 +0200

    ut

--- src/hct_mis_api/apps/payment/admin.py
+++ src/hct_mis_api/apps/payment/admin.py
@@ -872,7 +872,7 @@ class WesternUnionQCFFileAdmin(admin.ModelAdmin):
 
     readonly_fields = ["download_link"]
 
-    def download_link(self, obj: WesternUnionQCFFile) -> str:
+    def download_link(self, obj: WesternUnionQCFFile) -> str:  # pragma: no cover
         if not obj.file:
             return "-"
         return format_html('<a href="{}" target="_blank">Download</a>', obj.file.file.url)
@@ -891,7 +891,7 @@ class WesternUnionQCFFileReportAdmin(admin.ModelAdmin):
     list_display = ["id", "qcf_file", "payment_plan"]
     readonly_fields = ["download_link"]
 
-    def download_link(self, obj: WesternUnionQCFFileReport) -> str:
+    def download_link(self, obj: WesternUnionQCFFileReport) -> str:  # pragma: no cover
         if not obj.report_file:
             return "-"
         return format_html('<a href="{}" target="_blank">Download</a>', obj.report_file.file.url)
