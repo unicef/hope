9cdc18d3041f3d06bd0a805e26a611dd6d9cc934
pavlo-mk <pv.pasha.pv@gmail.com>
2025-08-25 14:27:24 +0200

    failed amount

--- src/hct_mis_api/apps/payment/pdf/payment_plan_export_pdf_service.py
+++ src/hct_mis_api/apps/payment/pdf/payment_plan_export_pdf_service.py
@@ -80,8 +80,8 @@ class PaymentPlanPDFExportService:
             reconciled_usd=Sum("delivered_quantity_usd", filter=Q(status__in=Payment.DELIVERED_STATUSES)),
             reconciled_local=Sum("delivered_quantity", filter=Q(status__in=Payment.DELIVERED_STATUSES)),
             failed=Count("id", filter=Q(status__in=Payment.FAILED_STATUSES)),  # Unredeemed
-            failed_usd=Sum("delivered_quantity_usd", filter=Q(status__in=Payment.FAILED_STATUSES)),
-            failed_local=Sum("delivered_quantity", filter=Q(status__in=Payment.FAILED_STATUSES)),
+            failed_usd=Sum("entitlement_quantity_usd", filter=Q(status__in=Payment.FAILED_STATUSES)),
+            failed_local=Sum("entitlement_quantity", filter=Q(status__in=Payment.FAILED_STATUSES)),
         )
 
         pdf_context_data = {
