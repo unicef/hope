a8618934643eba0e8e2fc4673c94dc437abf0000
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-08-26 12:16:32 +0200

    add search for available_users

--- src/hope/apps/periodic_data_update/api/filters.py
+++ src/hope/apps/periodic_data_update/api/filters.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as filters
+from django.db.models import Q
 
 from hope.apps.core.api.filters import UpdatedAtFilter
 from hope.apps.periodic_data_update.models import PDUOnlineEdit
@@ -10,3 +11,18 @@ class PDUOnlineEditFilter(UpdatedAtFilter):
     class Meta:
         model = PDUOnlineEdit
         fields = ("status",)
+
+
+class UserAvailableFilter(filters.FilterSet):
+    search = filters.CharFilter(
+        method="search_filter", help_text="Search users by first name, last name, username, or email."
+    )
+
+    def search_filter(self, qs: "QuerySet", name: str, value: str) -> "QuerySet[User]":
+        values = value.split()
+        q_obj = Q()
+        for v in values:
+            q_obj |= Q(first_name__icontains=v)
+            q_obj |= Q(last_name__icontains=v)
+            q_obj |= Q(email__icontains=v)
+        return qs.filter(q_obj)
