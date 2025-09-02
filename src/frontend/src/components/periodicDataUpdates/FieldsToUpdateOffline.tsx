dafbc10ebdafb54724febacf7d23f1d2319a380a
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2025-09-02 21:27:34 +0200

    dont show fields with no rounds to update + template name null

--- src/frontend/src/components/periodicDataUpdates/FieldsToUpdateOffline.tsx
+++ src/frontend/src/components/periodicDataUpdates/FieldsToUpdateOffline.tsx
@@ -89,67 +89,74 @@ export const FieldsToUpdateOffline: FC<FieldsToUpdateOfflineProps> = ({
               </TableRow>
             </TableHead>
             <TableBody>
-              {values.roundsData.map((field) => (
-                <TableRow
-                  key={`${field.field}-${field.round}`}
-                  data-cy={`table-row-${field.field}`}
-                >
-                  <TableCell>
-                    <Checkbox
-                      data-cy={`checkbox-${field.field}`}
-                      checked={checkedFields[field.field] || false}
-                      onChange={(event) => handleCheckboxChange(event, field)}
-                    />
-                  </TableCell>
-                  <TableCell data-cy={`table-cell-field-${field.field}`}>
-                    {typeof field.label === 'string'
-                      ? field.label
-                      : //@ts-ignore
-                        field.label?.englishEn ||
-                        Object.values(field.label)[0] ||
-                        ''}
-                  </TableCell>
-                  <TableCell data-cy={`table-cell-roundNumber-${field.field}`}>
-                    <Select
-                      data-cy={`select-roundNumber-${field.field}`}
-                      value={field.roundNumber}
-                      onChange={(event) => {
-                        const selectedIndex = values.roundsData.findIndex(
-                          (selectedField) =>
-                            selectedField.field === field.field,
-                        );
-                        const newRoundNumber = Number(event.target.value);
-                        const newRoundName =
-                          field.rounds[newRoundNumber - 1].round_name;
-
-                        setFieldValue(
-                          `roundsData[${selectedIndex}].roundNumber`,
-                          newRoundNumber,
-                        );
-                        setFieldValue(
-                          `roundsData[${selectedIndex}].roundName`,
-                          newRoundName,
-                        );
-                      }}
+              {values.roundsData
+                .filter(
+                  (field) =>
+                    field.numberOfRounds - (field.roundsCovered || 0) > 0,
+                )
+                .map((field) => (
+                  <TableRow
+                    key={`${field.field}-${field.round}`}
+                    data-cy={`table-row-${field.field}`}
+                  >
+                    <TableCell>
+                      <Checkbox
+                        data-cy={`checkbox-${field.field}`}
+                        checked={checkedFields[field.field] || false}
+                        onChange={(event) => handleCheckboxChange(event, field)}
+                      />
+                    </TableCell>
+                    <TableCell data-cy={`table-cell-field-${field.field}`}>
+                      {typeof field.label === 'string'
+                        ? field.label
+                        : //@ts-ignore
+                          field.label?.englishEn ||
+                          Object.values(field.label)[0] ||
+                          ''}
+                    </TableCell>
+                    <TableCell
+                      data-cy={`table-cell-roundNumber-${field.field}`}
                     >
-                      {Array.from(
-                        {
-                          length:
-                            field.numberOfRounds - (field.roundsCovered || 0),
-                        },
-                        (_, num) => (
-                          <MenuItem
-                            key={num + 1 + (field.roundsCovered || 0)}
-                            value={num + 1 + (field.roundsCovered || 0)}
-                          >
-                            {num + 1 + (field.roundsCovered || 0)}
-                          </MenuItem>
-                        ),
-                      )}
-                    </Select>
-                  </TableCell>
-                </TableRow>
-              ))}
+                      <Select
+                        data-cy={`select-roundNumber-${field.field}`}
+                        value={field.roundNumber}
+                        onChange={(event) => {
+                          const selectedIndex = values.roundsData.findIndex(
+                            (selectedField) =>
+                              selectedField.field === field.field,
+                          );
+                          const newRoundNumber = Number(event.target.value);
+                          const newRoundName =
+                            field.rounds[newRoundNumber - 1].round_name;
+
+                          setFieldValue(
+                            `roundsData[${selectedIndex}].roundNumber`,
+                            newRoundNumber,
+                          );
+                          setFieldValue(
+                            `roundsData[${selectedIndex}].roundName`,
+                            newRoundName,
+                          );
+                        }}
+                      >
+                        {Array.from(
+                          {
+                            length:
+                              field.numberOfRounds - (field.roundsCovered || 0),
+                          },
+                          (_, num) => (
+                            <MenuItem
+                              key={num + 1 + (field.roundsCovered || 0)}
+                              value={num + 1 + (field.roundsCovered || 0)}
+                            >
+                              {num + 1 + (field.roundsCovered || 0)}
+                            </MenuItem>
+                          ),
+                        )}
+                      </Select>
+                    </TableCell>
+                  </TableRow>
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
