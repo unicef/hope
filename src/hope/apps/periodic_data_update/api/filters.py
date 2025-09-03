051e55cba649eb7dc7a87ca03514b08ec38a146e
Paulina Kujawa <p.kujawa2f@gmail.com>
2025-09-03 14:33:06 +0200

    lint

--- src/hope/apps/periodic_data_update/api/filters.py
+++ src/hope/apps/periodic_data_update/api/filters.py
@@ -15,9 +15,7 @@ class PDUOnlineEditFilter(UpdatedAtFilter):
 
 
 class UserAvailableFilter(filters.FilterSet):
-    search = filters.CharFilter(
-        method="search_filter", help_text="Search users by first name, last name or email."
-    )
+    search = filters.CharFilter(method="search_filter", help_text="Search users by first name, last name or email.")
 
     def search_filter(self, qs: QuerySet, name: str, value: str) -> QuerySet[User]:
         values = value.split()
