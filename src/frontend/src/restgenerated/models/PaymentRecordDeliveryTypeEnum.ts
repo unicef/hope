df9a1528f4016cb33f1c7870b3b1f4e3ebff447e
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2025-09-08 15:33:42 +0200

    fix admin2 in grievances

--- src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
+++ src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
@@ -4,45 +4,45 @@
 /* eslint-disable */
 /**
  * * `atm_card` - ATM Card
- * * `dc21d525-08cd-4081-9c68-efbc2a6dba2d` - Author question play story.
- * * `423271e0-a19c-429b-9ef9-108684509126` - Beat bank impact.
  * * `cardless_cash_withdrawal` - Cardless cash withdrawal
  * * `cash` - Cash
  * * `cash_by_fsp` - Cash by FSP
  * * `cash_over_the_counter` - Cash over the counter
  * * `cheque` - Cheque
+ * * `062a4d89-cda0-44ed-ae4c-25e6020c955d` - Concern century decade.
  * * `deposit_to_card` - Deposit to Card
+ * * `7ff023f6-a513-4dcf-a258-722a64759359` - Fund including floor thousand firm.
+ * * `e71ac428-dfc7-4761-ac13-162ca78c7009` - Join else significant.
  * * `mobile_money` - Mobile Money
- * * `e00bc270-e065-4574-8b9b-25ce2038981e` - Now feeling join partner field.
+ * * `04fec503-1e1b-490d-9059-05445742d1f9` - News treatment ten here.
  * * `pre-paid_card` - Pre-paid card
  * * `referral` - Referral
- * * `d0d3fcdd-25a5-43a5-bcaf-ed4480b9dd9b` - Region thing.
- * * `1d7aa060-3ab3-49f2-9905-814269882b1d` - Soldier trade price exactly.
+ * * `07b7e8eb-901c-40c5-9781-3fa4a6ecb5f7` - Table order behind article.
  * * `transfer` - Transfer
  * * `transfer_to_account` - Transfer to Account
  * * `transfer_to_digital_wallet` - Transfer to Digital Wallet
+ * * `7c5c5f01-04e1-464e-86c9-955a2b92fbd1` - View rest.
  * * `voucher` - Voucher
- * * `280f7fd6-ee87-4cfd-953c-e6442266e4db` - Your offer top knowledge.
  */
 export enum PaymentRecordDeliveryTypeEnum {
     ATM_CARD = 'atm_card',
-    DC21D525_08CD_4081_9C68_EFBC2A6DBA2D = 'dc21d525-08cd-4081-9c68-efbc2a6dba2d',
-    _423271E0_A19C_429B_9EF9_108684509126 = '423271e0-a19c-429b-9ef9-108684509126',
     CARDLESS_CASH_WITHDRAWAL = 'cardless_cash_withdrawal',
     CASH = 'cash',
     CASH_BY_FSP = 'cash_by_fsp',
     CASH_OVER_THE_COUNTER = 'cash_over_the_counter',
     CHEQUE = 'cheque',
+    _062A4D89_CDA0_44ED_AE4C_25E6020C955D = '062a4d89-cda0-44ed-ae4c-25e6020c955d',
     DEPOSIT_TO_CARD = 'deposit_to_card',
+    _7FF023F6_A513_4DCF_A258_722A64759359 = '7ff023f6-a513-4dcf-a258-722a64759359',
+    E71AC428_DFC7_4761_AC13_162CA78C7009 = 'e71ac428-dfc7-4761-ac13-162ca78c7009',
     MOBILE_MONEY = 'mobile_money',
-    E00BC270_E065_4574_8B9B_25CE2038981E = 'e00bc270-e065-4574-8b9b-25ce2038981e',
+    _04FEC503_1E1B_490D_9059_05445742D1F9 = '04fec503-1e1b-490d-9059-05445742d1f9',
     PRE_PAID_CARD = 'pre-paid_card',
     REFERRAL = 'referral',
-    D0D3FCDD_25A5_43A5_BCAF_ED4480B9DD9B = 'd0d3fcdd-25a5-43a5-bcaf-ed4480b9dd9b',
-    _1D7AA060_3AB3_49F2_9905_814269882B1D = '1d7aa060-3ab3-49f2-9905-814269882b1d',
+    _07B7E8EB_901C_40C5_9781_3FA4A6ECB5F7 = '07b7e8eb-901c-40c5-9781-3fa4a6ecb5f7',
     TRANSFER = 'transfer',
     TRANSFER_TO_ACCOUNT = 'transfer_to_account',
     TRANSFER_TO_DIGITAL_WALLET = 'transfer_to_digital_wallet',
+    _7C5C5F01_04E1_464E_86C9_955A2B92FBD1 = '7c5c5f01-04e1-464e-86c9-955a2b92fbd1',
     VOUCHER = 'voucher',
-    _280F7FD6_EE87_4CFD_953C_E6442266E4DB = '280f7fd6-ee87-4cfd-953c-e6442266e4db',
 }
