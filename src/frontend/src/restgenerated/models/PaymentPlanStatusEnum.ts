0407a659bd7c39b1dbf1dfd721045ac15f7e130f
pavlo-mk <pv.pasha.pv@gmail.com>
2025-09-09 07:26:20 -0500

    fe types

--- src/frontend/src/restgenerated/models/PaymentPlanStatusEnum.ts
+++ src/frontend/src/restgenerated/models/PaymentPlanStatusEnum.ts
@@ -3,40 +3,40 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
- * * `TP_OPEN` - Open
- * * `TP_LOCKED` - Locked
- * * `PROCESSING` - Processing
- * * `STEFICON_WAIT` - Steficon Wait
- * * `STEFICON_RUN` - Steficon Run
- * * `STEFICON_COMPLETED` - Steficon Completed
- * * `STEFICON_ERROR` - Steficon Error
+ * * `ACCEPTED` - Accepted
  * * `DRAFT` - Draft
- * * `PREPARING` - Preparing
- * * `OPEN` - Open
- * * `LOCKED` - Locked
- * * `LOCKED_FSP` - Locked FSP
+ * * `FINISHED` - Finished
  * * `IN_APPROVAL` - In Approval
  * * `IN_AUTHORIZATION` - In Authorization
  * * `IN_REVIEW` - In Review
- * * `ACCEPTED` - Accepted
- * * `FINISHED` - Finished
+ * * `TP_LOCKED` - Locked
+ * * `LOCKED` - Locked
+ * * `LOCKED_FSP` - Locked FSP
+ * * `TP_OPEN` - Open
+ * * `OPEN` - Open
+ * * `PREPARING` - Preparing
+ * * `PROCESSING` - Processing
+ * * `STEFICON_COMPLETED` - Steficon Completed
+ * * `STEFICON_ERROR` - Steficon Error
+ * * `STEFICON_RUN` - Steficon Run
+ * * `STEFICON_WAIT` - Steficon Wait
  */
 export enum PaymentPlanStatusEnum {
-    TP_OPEN = 'TP_OPEN',
-    TP_LOCKED = 'TP_LOCKED',
-    PROCESSING = 'PROCESSING',
-    STEFICON_WAIT = 'STEFICON_WAIT',
-    STEFICON_RUN = 'STEFICON_RUN',
-    STEFICON_COMPLETED = 'STEFICON_COMPLETED',
-    STEFICON_ERROR = 'STEFICON_ERROR',
+    ACCEPTED = 'ACCEPTED',
     DRAFT = 'DRAFT',
-    PREPARING = 'PREPARING',
-    OPEN = 'OPEN',
-    LOCKED = 'LOCKED',
-    LOCKED_FSP = 'LOCKED_FSP',
+    FINISHED = 'FINISHED',
     IN_APPROVAL = 'IN_APPROVAL',
     IN_AUTHORIZATION = 'IN_AUTHORIZATION',
     IN_REVIEW = 'IN_REVIEW',
-    ACCEPTED = 'ACCEPTED',
-    FINISHED = 'FINISHED',
+    TP_LOCKED = 'TP_LOCKED',
+    LOCKED = 'LOCKED',
+    LOCKED_FSP = 'LOCKED_FSP',
+    TP_OPEN = 'TP_OPEN',
+    OPEN = 'OPEN',
+    PREPARING = 'PREPARING',
+    PROCESSING = 'PROCESSING',
+    STEFICON_COMPLETED = 'STEFICON_COMPLETED',
+    STEFICON_ERROR = 'STEFICON_ERROR',
+    STEFICON_RUN = 'STEFICON_RUN',
+    STEFICON_WAIT = 'STEFICON_WAIT',
 }
