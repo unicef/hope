dd0c1f5adaf9be2808008f856d94ca6f2f6b2d24
Jan Romaniak <jan.romaniak@kellton.com>
2025-09-26 14:18:20 +0200

    household cache

--- src/hope/apps/core/signals.py
+++ src/hope/apps/core/signals.py
@@ -3,12 +3,14 @@ from typing import Any
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db.models.signals import m2m_changed, post_save
-from django.dispatch import receiver
+from django.dispatch import receiver, Signal
 
 from hope.apps.account.models import Partner, Role, RoleAssignment
 from hope.apps.account.permissions import DEFAULT_PERMISSIONS_LIST_FOR_IS_UNICEF_PARTNER
 from hope.apps.core.models import BusinessArea, DataCollectingType
 
+post_bulk_update = Signal()
+post_bulk_create = Signal()
 
 @receiver(m2m_changed, sender=DataCollectingType.compatible_types.through)
 def validate_compatible_types(
--- src/hope/apps/household/api/caches.py
+++ src/hope/apps/household/api/caches.py
@@ -1,28 +1,31 @@
 from typing import Any
 
 from django.db.models import QuerySet
+from rest_framework_extensions.key_constructor.bits import KeyBitBase
 
 from hope.api.caches import (
     AreaLimitKeyBit,
     BusinessAreaAndProgramLastUpdatedKeyBit,
     KeyConstructorMixin,
+    get_or_create_cache_key,
+    increment_cache_key,
 )
-from hope.apps.household.models import Household, Individual
+from hope.apps.household.models import Individual
+
+HOUSEHOLD_LIST_PROGRAM_KEY = "{program_id}:households_list:version"
 
 
-class HouseholdListKeyBit(BusinessAreaAndProgramLastUpdatedKeyBit):
-    specific_view_cache_key = "household_list"
+def get_household_list_program_key(program_id):
+    return get_or_create_cache_key(HOUSEHOLD_LIST_PROGRAM_KEY.format(program_id), 0)
 
-    def _get_queryset(
-        self,
-        business_area_slug: Any | None,
-        program_slug: Any | None,
-        view_instance: Any | None,
-    ) -> QuerySet:
-        return Household.all_merge_status_objects.filter(
-            program__slug=program_slug,
-            business_area__slug=business_area_slug,
-        )
+
+def increment_household_list_program_key(program_id):
+    return increment_cache_key(HOUSEHOLD_LIST_PROGRAM_KEY.format(program_id))
+
+
+class HouseholdListKeyBit(KeyBitBase):
+    def get_data(self, params, view_instance, view_method, request, args, kwargs):
+        return str(get_household_list_program_key(view_instance.program.id))
 
 
 class IndividualListKeyBit(BusinessAreaAndProgramLastUpdatedKeyBit):
