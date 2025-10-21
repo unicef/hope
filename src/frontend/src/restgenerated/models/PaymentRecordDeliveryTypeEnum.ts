afc78f957046044458e1e02660a87a2d86d5a101
Paulina Kujawa <42150286+pkujawa@users.noreply.github.com>
2025-10-21 12:19:00 +0200

    REST - payments on household fix (#5275)
    * add count to payments
    
    * take count from payments query
    
    * different
    
    * payments count
    
    * fix
    
    * nested parser
    
    * fix xlsx import pp, payments hh count, collectors table
    
    ---------
    
    Co-authored-by: Maciej Szewczyk <maciej.szewczyk@kellton.com>
    Co-authored-by: Maciej Szewczyk <34482854+mmaciekk@users.noreply.github.com>

--- src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
+++ src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
@@ -4,21 +4,21 @@
 /* eslint-disable */
 /**
  * * `atm_card` - ATM Card
+ * * `b8a4880b-8d28-4c81-99fd-6086e45cbe28` - Anything across leave from.
  * * `cardless_cash_withdrawal` - Cardless cash withdrawal
  * * `cash` - Cash
  * * `cash_by_fsp` - Cash by FSP
  * * `cash_over_the_counter` - Cash over the counter
  * * `cheque` - Cheque
  * * `deposit_to_card` - Deposit to Card
- * * `889ef715-0f4a-44fb-986b-a921086d3d26` - Enter Mrs.
- * * `ef8983d2-1b3e-476b-930a-f6d390378894` - Guy accept onto ahead.
- * * `c60e5b3e-3a21-4a21-af74-6cb4f7d0ec1a` - International drug across very.
  * * `mobile_money` - Mobile Money
+ * * `75c9ad14-a9e3-4995-abc6-013b085f1569` - Particular attention.
  * * `pre-paid_card` - Pre-paid card
- * * `2a0a89b0-402d-4c65-a5fb-d24a4c71db72` - Raise large long address.
+ * * `aa337cf4-ab13-4cdc-aa10-eee045f6b18f` - Prepare medical how.
  * * `referral` - Referral
- * * `c4875563-bb3a-4743-b45a-dcb34bbee25a` - Serious certainly step yes eye.
- * * `b648a840-4152-48d8-9e39-f1cb12d892d5` - Station leg safe.
+ * * `fa20daff-64f0-47e1-a504-58d8dc7b7cbe` - Statement enjoy final project pretty.
+ * * `753727f9-1037-4488-9572-8d1df6e8d235` - Student close class.
+ * * `2700969f-2a1e-40e7-a31d-ac8f3abf9db3` - Thousand until investment.
  * * `transfer` - Transfer
  * * `transfer_to_account` - Transfer to Account
  * * `transfer_to_digital_wallet` - Transfer to Digital Wallet
@@ -26,21 +26,21 @@
  */
 export enum PaymentRecordDeliveryTypeEnum {
     ATM_CARD = 'atm_card',
+    B8A4880B_8D28_4C81_99FD_6086E45CBE28 = 'b8a4880b-8d28-4c81-99fd-6086e45cbe28',
     CARDLESS_CASH_WITHDRAWAL = 'cardless_cash_withdrawal',
     CASH = 'cash',
     CASH_BY_FSP = 'cash_by_fsp',
     CASH_OVER_THE_COUNTER = 'cash_over_the_counter',
     CHEQUE = 'cheque',
     DEPOSIT_TO_CARD = 'deposit_to_card',
-    _889EF715_0F4A_44FB_986B_A921086D3D26 = '889ef715-0f4a-44fb-986b-a921086d3d26',
-    EF8983D2_1B3E_476B_930A_F6D390378894 = 'ef8983d2-1b3e-476b-930a-f6d390378894',
-    C60E5B3E_3A21_4A21_AF74_6CB4F7D0EC1A = 'c60e5b3e-3a21-4a21-af74-6cb4f7d0ec1a',
     MOBILE_MONEY = 'mobile_money',
+    _75C9AD14_A9E3_4995_ABC6_013B085F1569 = '75c9ad14-a9e3-4995-abc6-013b085f1569',
     PRE_PAID_CARD = 'pre-paid_card',
-    _2A0A89B0_402D_4C65_A5FB_D24A4C71DB72 = '2a0a89b0-402d-4c65-a5fb-d24a4c71db72',
+    AA337CF4_AB13_4CDC_AA10_EEE045F6B18F = 'aa337cf4-ab13-4cdc-aa10-eee045f6b18f',
     REFERRAL = 'referral',
-    C4875563_BB3A_4743_B45A_DCB34BBEE25A = 'c4875563-bb3a-4743-b45a-dcb34bbee25a',
-    B648A840_4152_48D8_9E39_F1CB12D892D5 = 'b648a840-4152-48d8-9e39-f1cb12d892d5',
+    FA20DAFF_64F0_47E1_A504_58D8DC7B7CBE = 'fa20daff-64f0-47e1-a504-58d8dc7b7cbe',
+    _753727F9_1037_4488_9572_8D1DF6E8D235 = '753727f9-1037-4488-9572-8d1df6e8d235',
+    _2700969F_2A1E_40E7_A31D_AC8F3ABF9DB3 = '2700969f-2a1e-40e7-a31d-ac8f3abf9db3',
     TRANSFER = 'transfer',
     TRANSFER_TO_ACCOUNT = 'transfer_to_account',
     TRANSFER_TO_DIGITAL_WALLET = 'transfer_to_digital_wallet',
