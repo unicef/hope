bf77683d5f404ac7930209646cdaa5f4e0479b89
Maciej Szewczyk <34482854+mmaciekk@users.noreply.github.com>
2025-10-24 17:24:12 +0200

    add missing pagination RDI (#5301)

--- src/frontend/src/containers/tables/rdi/ImportedHouseholdsTable/ImportedHouseholdTable.tsx
+++ src/frontend/src/containers/tables/rdi/ImportedHouseholdsTable/ImportedHouseholdTable.tsx
@@ -9,10 +9,15 @@ import { headCells as mergedHeadCells } from './MergedHouseholdTableHeadCells';
 import { useBaseUrl } from '@hooks/useBaseUrl';
 import { HouseholdDetail } from '@restgenerated/models/HouseholdDetail';
 import { UniversalRestQueryTable } from '@components/rest/UniversalRestQueryTable/UniversalRestQueryTable';
+import { CountResponse } from '@restgenerated/models/CountResponse';
+import { useQuery } from '@tanstack/react-query';
+import { createApiParams } from '@utils/apiUtils';
+import { usePersistedCount } from '@hooks/usePersistedCount';
 
 function ImportedHouseholdTable({ rdi, businessArea, isMerged }): ReactElement {
   const { selectedProgram } = useProgramContext();
   const { programId } = useBaseUrl();
+  const [page, setPage] = useState(0);
 
   const initialQueryVariables = useMemo(
     () => ({
@@ -29,6 +34,23 @@ function ImportedHouseholdTable({ rdi, businessArea, isMerged }): ReactElement {
     setQueryVariables(initialQueryVariables);
   }, [initialQueryVariables]);
 
+  const { data: countData } = useQuery<CountResponse>({
+    queryKey: [
+      'businessAreasProgramsHouseholdsCount',
+      programId,
+      businessArea,
+      queryVariables,
+    ],
+    queryFn: () =>
+      RestService.restBusinessAreasProgramsHouseholdsCountRetrieve(
+        createApiParams(
+          { businessAreaSlug: businessArea, programSlug: programId },
+          queryVariables,
+        ),
+      ),
+    enabled: page === 0,
+  });
+
   const beneficiaryGroup = selectedProgram?.beneficiaryGroup;
 
   const mergedReplacements = {
@@ -43,6 +65,8 @@ function ImportedHouseholdTable({ rdi, businessArea, isMerged }): ReactElement {
     beneficiaryGroup,
     mergedReplacements,
   );
+
+  const itemsCount = usePersistedCount(page, countData);
 
   const importedReplacements = {
     unicefId: (_beneficiaryGroup) => `${_beneficiaryGroup?.groupLabel} ID`,
@@ -67,6 +91,9 @@ function ImportedHouseholdTable({ rdi, businessArea, isMerged }): ReactElement {
         headCells={adjustedMergedHeadCells}
         queryVariables={queryVariables}
         setQueryVariables={setQueryVariables}
+        page={page}
+        setPage={setPage}
+        itemsCount={itemsCount}
       />
     );
   }
@@ -84,6 +111,9 @@ function ImportedHouseholdTable({ rdi, businessArea, isMerged }): ReactElement {
       queryVariables={queryVariables}
       setQueryVariables={setQueryVariables}
       query={RestService.restBusinessAreasProgramsHouseholdsList}
+      page={page}
+      setPage={setPage}
+      itemsCount={itemsCount}
     />
   );
 }
