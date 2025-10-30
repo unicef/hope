4e2cd06e332cb1ff5c09bb3ef88aeac9a1f4dbdf
pavlo-mk <pv.pasha.pv@gmail.com>
2025-10-30 10:50:57 -0400

    test

--- tests/unit/apps/sanction_list/conftest.py
+++ tests/unit/apps/sanction_list/conftest.py
@@ -1,11 +1,15 @@
 from pathlib import Path
-from typing import Any, Generator
+from typing import TYPE_CHECKING, Any, Generator
 
 import pytest
 from responses import RequestsMock
+from strategy_field.utils import fqn
 
 from hope.apps.core.celery import app
-from hope.models.sanction_list import SanctionList
+from hope.apps.sanction_list.strategies.eu import EUSanctionList
+
+if TYPE_CHECKING:
+    from hope.models.sanction_list import SanctionList
 
 
 @pytest.fixture
@@ -27,13 +31,20 @@ def always_eager() -> Generator[Any, None, None]:
     app.conf.task_always_eager = status
 
 
+# @pytest.fixture
+# def sanction_list(db: Any) -> SanctionList:
+#     obj, _ = SanctionList.objects.get_or_create(
+#         name="EU",
+#         strategy="hope.apps.sanction_list.strategies.eu.EUSanctionList",
+#         config={
+#             "url": "http://example.com/sl.xml",
+#         },
+#     )
+#     return obj
+
+
 @pytest.fixture
-def sanction_list(db: Any) -> SanctionList:
-    obj, _ = SanctionList.objects.get_or_create(
-        name="EU",
-        strategy="hope.apps.sanction_list.strategies.eu.EUSanctionList",
-        config={
-            "url": "http://example.com/sl.xml",
-        },
-    )
-    return obj
+def sanction_list(db: Any) -> "SanctionList":
+    from extras.test_utils.factories.sanction_list import SanctionListFactory
+
+    return SanctionListFactory(name="EU", strategy=fqn(EUSanctionList), config={"url": "http://example.com/sl.xml"})
--- tests/unit/apps/sanction_list/test_sl_views.py
+++ tests/unit/apps/sanction_list/test_sl_views.py
@@ -5,12 +5,11 @@ from django.urls import reverse
 import pytest
 from webtest import Upload
 
-from hope.models.sanction_list import UploadedXLSXFile
+from hope.models.sanction_list import SanctionList, UploadedXLSXFile
 
 if TYPE_CHECKING:
     from django_webtest import DjangoTestApp
 
-    from hope.models.sanction_list import SanctionList
     from hope.models.user import User
 
 
@@ -23,8 +22,9 @@ def test_upload(
     django_app: "DjangoTestApp",
     admin_user: "User",
     sample_file: bytes,
-    sanction_list: "SanctionList",
 ) -> None:
+    sanction_list: SanctionList = SanctionList.objects.all().first()
+    assert sanction_list is not None
     url = reverse("sanction:upload")
     res = django_app.get(url, user=admin_user)
     res.forms["upload-form"]["file"] = Upload("sample.xls", sample_file, content_type="application/vnd.ms-excel")
