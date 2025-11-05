c68998f5358fff6c5563798890193fb1c52d7554
pavlo-mk <pv.pasha.pv@gmail.com>
2025-11-05 06:54:04 -0500

    conflicts

---
+++ src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
@@ -0,0 +1,48 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * * `atm_card` - ATM Card
+ * * `ce0b1572-ad48-41f0-9020-8dd6f52508a4` - Argue upon out.
+ * * `b9d52b99-eff9-4791-9e5a-14917717723a` - Available tough goal movie.
+ * * `cardless_cash_withdrawal` - Cardless cash withdrawal
+ * * `cash` - Cash
+ * * `cash_by_fsp` - Cash by FSP
+ * * `cash_over_the_counter` - Cash over the counter
+ * * `cheque` - Cheque
+ * * `deposit_to_card` - Deposit to Card
+ * * `f5a2e6ee-49c2-417a-ad8a-0ec92d6f4e74` - General view family.
+ * * `84f2e317-ea71-4644-aa02-d349c5fa79e7` - Miss benefit.
+ * * `mobile_money` - Mobile Money
+ * * `e5d240fc-9f2f-45b2-84e5-d2e00e9c33d4` - National knowledge one.
+ * * `pre-paid_card` - Pre-paid card
+ * * `referral` - Referral
+ * * `a82cbfff-7b7e-47a7-94c8-ca6a048061b6` - So word respond either film.
+ * * `transfer` - Transfer
+ * * `transfer_to_account` - Transfer to Account
+ * * `transfer_to_digital_wallet` - Transfer to Digital Wallet
+ * * `voucher` - Voucher
+ */
+export enum PaymentRecordDeliveryTypeEnum {
+    ATM_CARD = 'atm_card',
+    CE0B1572_AD48_41F0_9020_8DD6F52508A4 = 'ce0b1572-ad48-41f0-9020-8dd6f52508a4',
+    B9D52B99_EFF9_4791_9E5A_14917717723A = 'b9d52b99-eff9-4791-9e5a-14917717723a',
+    CARDLESS_CASH_WITHDRAWAL = 'cardless_cash_withdrawal',
+    CASH = 'cash',
+    CASH_BY_FSP = 'cash_by_fsp',
+    CASH_OVER_THE_COUNTER = 'cash_over_the_counter',
+    CHEQUE = 'cheque',
+    DEPOSIT_TO_CARD = 'deposit_to_card',
+    F5A2E6EE_49C2_417A_AD8A_0EC92D6F4E74 = 'f5a2e6ee-49c2-417a-ad8a-0ec92d6f4e74',
+    _84F2E317_EA71_4644_AA02_D349C5FA79E7 = '84f2e317-ea71-4644-aa02-d349c5fa79e7',
+    MOBILE_MONEY = 'mobile_money',
+    E5D240FC_9F2F_45B2_84E5_D2E00E9C33D4 = 'e5d240fc-9f2f-45b2-84e5-d2e00e9c33d4',
+    PRE_PAID_CARD = 'pre-paid_card',
+    REFERRAL = 'referral',
+    A82CBFFF_7B7E_47A7_94C8_CA6A048061B6 = 'a82cbfff-7b7e-47a7-94c8-ca6a048061b6',
+    TRANSFER = 'transfer',
+    TRANSFER_TO_ACCOUNT = 'transfer_to_account',
+    TRANSFER_TO_DIGITAL_WALLET = 'transfer_to_digital_wallet',
+    VOUCHER = 'voucher',
+}
