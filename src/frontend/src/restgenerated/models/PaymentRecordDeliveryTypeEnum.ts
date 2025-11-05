265cf3c3f60e5fbcd22c1c2a524632ac8c93e684
pavlo-mk <pv.pasha.pv@gmail.com>
2025-11-05 14:40:39 -0500

    fe schema

---
+++ src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
@@ -0,0 +1,48 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * * `atm_card` - ATM Card
+ * * `3afce0d9-15b4-4958-89f6-499ae9d808d9` - Activity box behavior.
+ * * `cardless_cash_withdrawal` - Cardless cash withdrawal
+ * * `cash` - Cash
+ * * `cash_by_fsp` - Cash by FSP
+ * * `cash_over_the_counter` - Cash over the counter
+ * * `cheque` - Cheque
+ * * `ab6728f7-bf98-4a7d-a067-2bf7092c611c` - Dark sign.
+ * * `deposit_to_card` - Deposit to Card
+ * * `3b22183a-962e-4b0f-869d-c33edd2fa744` - Easy since better option guy.
+ * * `03431eb8-7615-4307-8f78-dd28642034a4` - Leave traditional development old.
+ * * `mobile_money` - Mobile Money
+ * * `pre-paid_card` - Pre-paid card
+ * * `a8554829-b4ce-4dc1-9c53-c2b2e987f0c4` - Receive music.
+ * * `referral` - Referral
+ * * `a5c0f097-3b8a-4b0e-89b2-8785f22bcdb1` - Scientist not site market.
+ * * `transfer` - Transfer
+ * * `transfer_to_account` - Transfer to Account
+ * * `transfer_to_digital_wallet` - Transfer to Digital Wallet
+ * * `voucher` - Voucher
+ */
+export enum PaymentRecordDeliveryTypeEnum {
+    ATM_CARD = 'atm_card',
+    _3AFCE0D9_15B4_4958_89F6_499AE9D808D9 = '3afce0d9-15b4-4958-89f6-499ae9d808d9',
+    CARDLESS_CASH_WITHDRAWAL = 'cardless_cash_withdrawal',
+    CASH = 'cash',
+    CASH_BY_FSP = 'cash_by_fsp',
+    CASH_OVER_THE_COUNTER = 'cash_over_the_counter',
+    CHEQUE = 'cheque',
+    AB6728F7_BF98_4A7D_A067_2BF7092C611C = 'ab6728f7-bf98-4a7d-a067-2bf7092c611c',
+    DEPOSIT_TO_CARD = 'deposit_to_card',
+    _3B22183A_962E_4B0F_869D_C33EDD2FA744 = '3b22183a-962e-4b0f-869d-c33edd2fa744',
+    _03431EB8_7615_4307_8F78_DD28642034A4 = '03431eb8-7615-4307-8f78-dd28642034a4',
+    MOBILE_MONEY = 'mobile_money',
+    PRE_PAID_CARD = 'pre-paid_card',
+    A8554829_B4CE_4DC1_9C53_C2B2E987F0C4 = 'a8554829-b4ce-4dc1-9c53-c2b2e987f0c4',
+    REFERRAL = 'referral',
+    A5C0F097_3B8A_4B0E_89B2_8785F22BCDB1 = 'a5c0f097-3b8a-4b0e-89b2-8785f22bcdb1',
+    TRANSFER = 'transfer',
+    TRANSFER_TO_ACCOUNT = 'transfer_to_account',
+    TRANSFER_TO_DIGITAL_WALLET = 'transfer_to_digital_wallet',
+    VOUCHER = 'voucher',
+}
