a4e3507687abc1ef278570b64c465eb02491e706
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2025-11-20 10:01:29 +0100

    formatting

--- src/hope/apps/account/permissions.py
+++ src/hope/apps/account/permissions.py
@@ -19,9 +19,7 @@ logger = logging.getLogger(__name__)
 @unique
 class Permissions(Enum):
     @staticmethod
-    def _generate_next_value_(
-        name: str, start: int, count: int, last_values: list[Any]
-    ) -> Any:
+    def _generate_next_value_(name: str, start: int, count: int, last_values: list[Any]) -> Any:
         return name
 
     # RDI
@@ -306,14 +304,10 @@ DEFAULT_PERMISSIONS_IS_UNICEF_PARTNER = (
     Permissions.ACTIVITY_LOG_VIEW,
 )
 
-DEFAULT_PERMISSIONS_LIST_FOR_IS_UNICEF_PARTNER = [
-    str(perm.value) for perm in DEFAULT_PERMISSIONS_IS_UNICEF_PARTNER
-]
+DEFAULT_PERMISSIONS_LIST_FOR_IS_UNICEF_PARTNER = [str(perm.value) for perm in DEFAULT_PERMISSIONS_IS_UNICEF_PARTNER]
 
 
-def check_permissions(
-    user: Any, permissions: Iterable[Permissions], **kwargs: Any
-) -> bool:
+def check_permissions(user: Any, permissions: Iterable[Permissions], **kwargs: Any) -> bool:
     from hope.apps.program.models import Program
 
     if not user.is_authenticated:
@@ -331,9 +325,7 @@ def check_permissions(
         return False
     program = None
     if program_slug := kwargs.get("program"):
-        program = Program.objects.filter(
-            slug=program_slug, business_area=business_area
-        ).first()
+        program = Program.objects.filter(slug=program_slug, business_area=business_area).first()
     obj = program or business_area
 
     return any(user.has_perm(permission.name, obj) for permission in permissions)
