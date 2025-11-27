f7ff75d05d410522c7e701990319601d732d35db
Jan Romaniak <jan.romaniak@kellton.com>
2025-11-27 14:36:38 +0100

    fixes to ticket structure for accounts

--- src/frontend/src/components/grievances/utils/createGrievanceUtils.ts
+++ src/frontend/src/components/grievances/utils/createGrievanceUtils.ts
@@ -476,18 +476,9 @@ export function prepareExistingAccountValues(
     return value;
   }
   return individualDataUpdateAccountsToEdit.map((item) => {
-    const dataFieldsArray = popKey(item, 'dataFields') || [];
-
-    const dataFields = {};
-    for (const field of dataFieldsArray) {
-      dataFields[field.key] = field.value;
-    }
-    return {
-      number: popKey(item, 'number'),
-      financialInstitution: popKey(item, 'financialInstitution'),
-      id: popKey(item, 'id'),
-      dataFields: { ...item, ...dataFields },
-    };
+    const preparedItem = { ...item };
+    delete preparedItem.accountType;
+    return preparedItem;
   });
 }
 
