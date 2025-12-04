3541df3d3d3bd896d3b0679d7f7e35fd06513964
pavlo-mk <pv.pasha.pv@gmail.com>
2025-12-04 13:31:41 -0500

    fe types

--- src/frontend/src/restgenerated/services/RestService.ts
+++ src/frontend/src/restgenerated/services/RestService.ts
@@ -13285,6 +13285,7 @@ export class RestService {
         programSlug,
         limit,
         offset,
+        ordering,
     }: {
         businessAreaSlug: string,
         /**
@@ -13300,6 +13301,7 @@ export class RestService {
          * The initial index from which to return the results.
          */
         offset?: number,
+        ordering?: string,
     }): CancelablePromise<PaginatedPendingPaymentList> {
         return __request(OpenAPI, {
             method: 'GET',
@@ -13312,6 +13314,33 @@ export class RestService {
             query: {
                 'limit': limit,
                 'offset': offset,
+                'ordering': ordering,
+            },
+        });
+    }
+    /**
+     * @returns CountResponse
+     * @throws ApiError
+     */
+    public static restBusinessAreasProgramsTargetPopulationsPendingPaymentsCountRetrieve({
+        businessAreaSlug,
+        id,
+        programSlug,
+    }: {
+        businessAreaSlug: string,
+        /**
+         * A UUID string identifying this Payment Plan.
+         */
+        id: string,
+        programSlug: string,
+    }): CancelablePromise<CountResponse> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/rest/business-areas/{business_area_slug}/programs/{program_slug}/target-populations/{id}/pending-payments/count/',
+            path: {
+                'business_area_slug': businessAreaSlug,
+                'id': id,
+                'program_slug': programSlug,
             },
         });
     }
