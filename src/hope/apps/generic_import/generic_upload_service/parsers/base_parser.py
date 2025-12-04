75cb869abec8f2b27ec63b56d956e8dcd59f71de
Jan Romaniak <jan.romaniak@kellton.com>
2025-12-04 16:41:39 +0100

    pragma no cover on abstract base class

--- src/hope/apps/generic_import/generic_upload_service/parsers/base_parser.py
+++ src/hope/apps/generic_import/generic_upload_service/parsers/base_parser.py
@@ -1,37 +1,37 @@
 class BaseParser:
     def parse(self, file_path):
-        raise NotImplementedError("Subclasses must implement this method")
+        raise NotImplementedError("Subclasses must implement this method")  # pragma: no cover
 
     def validate_file_structure(self) -> bool:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     def errors(self) -> [str]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     @property
     def supported_file_types(self) -> [str]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     @property
     def households_data(self) -> [dict]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     @property
     def individuals_data(self) -> [dict]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     @property
     def individual_roles_in_households_data(self) -> [dict]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     @property
     def accounts_data(self) -> [dict]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     @property
     def documents_data(self) -> [dict]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
 
     @property
     def identities_data(self) -> [dict]:
-        raise NotImplementedError("Subclasses must implement this property")
+        raise NotImplementedError("Subclasses must implement this property")  # pragma: no cover
