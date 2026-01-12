5fdb94938e186f0487b4db01064c1effdadc9db5
Marek Biczysko <34810846+MarekBiczysko@users.noreply.github.com>
2026-01-12 09:33:19 +0100

    291755_remove_delete_cascade_from_Payment (#5554)

--- tests/unit/apps/program/test_program_cycle_rest_api.py
+++ tests/unit/apps/program/test_program_cycle_rest_api.py
@@ -279,7 +279,7 @@ class TestProgramCycleAPI:
         if permissions and Permissions.PM_PROGRAMME_CYCLE_DELETE in permissions:
             assert bad_response.status_code == status.HTTP_400_BAD_REQUEST
             assert "Don't allow to delete Cycle with assigned Target Population" in bad_response.data
-            pp.delete()
+            pp.delete(soft=False)
 
             response = self.api_client.delete(url)
             assert response.status_code == status.HTTP_204_NO_CONTENT
