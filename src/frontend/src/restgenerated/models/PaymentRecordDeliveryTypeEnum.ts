23acbd9025e42e3e014ddcffaf204badde07589a
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2026-01-19 16:32:42 +0100

    build for backend

--- src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
+++ src/frontend/src/restgenerated/models/PaymentRecordDeliveryTypeEnum.ts
@@ -4,45 +4,45 @@
 /* eslint-disable */
 /**
  * * `atm_card` - ATM Card
- * * `6ec4ef87-aa46-4030-b1bd-bf310f117430` - And total reach west.
  * * `cardless_cash_withdrawal` - Cardless cash withdrawal
  * * `cash` - Cash
  * * `cash_by_fsp` - Cash by FSP
  * * `cash_over_the_counter` - Cash over the counter
  * * `cheque` - Cheque
+ * * `7195b0af-ae48-4d10-b296-53eb6421b126` - Color it surface those.
  * * `deposit_to_card` - Deposit to Card
+ * * `b62da004-d638-4c5b-a717-4572c04dbd94` - Make cell president.
+ * * `1807e1d1-98d6-46ae-9b45-726c9657616a` - Mention live teacher reveal.
  * * `mobile_money` - Mobile Money
- * * `2d2f6662-1c18-4256-8234-54e30fd0e0bd` - Often foreign boy.
+ * * `73d7ad7b-3ed2-4f0f-a8ae-d56312569eac` - Other by product.
  * * `pre-paid_card` - Pre-paid card
- * * `23a6d906-b6b9-4daa-8416-b84488434205` - President song popular.
  * * `referral` - Referral
- * * `f35515f5-f411-42b0-99ae-e1980eb4a57e` - Soldier Democrat ago.
+ * * `a3342fb1-b911-478b-a126-3c387336dacf` - Report dream case.
  * * `transfer` - Transfer
  * * `transfer_to_account` - Transfer to Account
  * * `transfer_to_digital_wallet` - Transfer to Digital Wallet
- * * `919adb84-788c-4095-8ba6-e13d607041b3` - Us scene century scene more.
  * * `voucher` - Voucher
- * * `42bd0481-d59b-4a76-be19-b630cb96b718` - Western break Democrat certainly former.
+ * * `a85f761b-adc0-426b-9227-cbc5c1c5c16c` - While religious.
  */
 export enum PaymentRecordDeliveryTypeEnum {
     ATM_CARD = 'atm_card',
-    _6EC4EF87_AA46_4030_B1BD_BF310F117430 = '6ec4ef87-aa46-4030-b1bd-bf310f117430',
     CARDLESS_CASH_WITHDRAWAL = 'cardless_cash_withdrawal',
     CASH = 'cash',
     CASH_BY_FSP = 'cash_by_fsp',
     CASH_OVER_THE_COUNTER = 'cash_over_the_counter',
     CHEQUE = 'cheque',
+    _7195B0AF_AE48_4D10_B296_53EB6421B126 = '7195b0af-ae48-4d10-b296-53eb6421b126',
     DEPOSIT_TO_CARD = 'deposit_to_card',
+    B62DA004_D638_4C5B_A717_4572C04DBD94 = 'b62da004-d638-4c5b-a717-4572c04dbd94',
+    _1807E1D1_98D6_46AE_9B45_726C9657616A = '1807e1d1-98d6-46ae-9b45-726c9657616a',
     MOBILE_MONEY = 'mobile_money',
-    _2D2F6662_1C18_4256_8234_54E30FD0E0BD = '2d2f6662-1c18-4256-8234-54e30fd0e0bd',
+    _73D7AD7B_3ED2_4F0F_A8AE_D56312569EAC = '73d7ad7b-3ed2-4f0f-a8ae-d56312569eac',
     PRE_PAID_CARD = 'pre-paid_card',
-    _23A6D906_B6B9_4DAA_8416_B84488434205 = '23a6d906-b6b9-4daa-8416-b84488434205',
     REFERRAL = 'referral',
-    F35515F5_F411_42B0_99AE_E1980EB4A57E = 'f35515f5-f411-42b0-99ae-e1980eb4a57e',
+    A3342FB1_B911_478B_A126_3C387336DACF = 'a3342fb1-b911-478b-a126-3c387336dacf',
     TRANSFER = 'transfer',
     TRANSFER_TO_ACCOUNT = 'transfer_to_account',
     TRANSFER_TO_DIGITAL_WALLET = 'transfer_to_digital_wallet',
-    _919ADB84_788C_4095_8BA6_E13D607041B3 = '919adb84-788c-4095-8ba6-e13d607041b3',
     VOUCHER = 'voucher',
-    _42BD0481_D59B_4A76_BE19_B630CB96B718 = '42bd0481-d59b-4a76-be19-b630cb96b718',
+    A85F761B_ADC0_426B_9227_CBC5C1C5C16C = 'a85f761b-adc0-426b-9227-cbc5c1c5c16c',
 }
