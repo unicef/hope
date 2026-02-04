b6bc12a283dc3e6d3e2e0b61839afeaf94db9082
marekbiczysko <marek.biczysko@tivix.com>
2026-02-04 16:08:08 +0100

    fix/tests/payment/test_payment_tests

--- tests/unit/apps/payment/test_to_decimal.py
+++ tests/unit/apps/payment/test_to_decimal.py
@@ -1,25 +1,23 @@
 from decimal import Decimal
 
-from django.test import TestCase
-
 from hope.apps.payment.utils import normalize_score, to_decimal
 
 
-class TestToDecimal(TestCase):
-    def test_to_decimal(self) -> None:
-        assert to_decimal(" ") is None
-        assert to_decimal("") is None
-        assert to_decimal(None) is None
-        assert to_decimal(3) == Decimal(f"{round(3.00, 2):.2f}")
-        assert to_decimal("3") == Decimal(f"{round(3.00, 2):.2f}")
-        assert to_decimal(3.1) == Decimal(f"{round(3.10, 2):.2f}")
-        assert to_decimal(3.14) == Decimal(f"{round(3.14, 2):.2f}")
-        assert to_decimal("3.1") == Decimal(f"{round(3.10, 2):.2f}")
-        assert to_decimal("3.14") == Decimal(f"{round(3.14, 2):.2f}")
-        assert to_decimal(Decimal(3.14)) == Decimal(f"{round(3.14, 2):.2f}")
+def test_to_decimal() -> None:
+    assert to_decimal(" ") is None
+    assert to_decimal("") is None
+    assert to_decimal(None) is None
+    assert to_decimal(3) == Decimal(f"{round(3.00, 2):.2f}")
+    assert to_decimal("3") == Decimal(f"{round(3.00, 2):.2f}")
+    assert to_decimal(3.1) == Decimal(f"{round(3.10, 2):.2f}")
+    assert to_decimal(3.14) == Decimal(f"{round(3.14, 2):.2f}")
+    assert to_decimal("3.1") == Decimal(f"{round(3.10, 2):.2f}")
+    assert to_decimal("3.14") == Decimal(f"{round(3.14, 2):.2f}")
+    assert to_decimal(Decimal(3.14)) == Decimal(f"{round(3.14, 2):.2f}")
 
-    def test_normalize_score(self) -> None:
-        assert normalize_score(None) is None
-        assert normalize_score("123.444111") == Decimal("123.444")
-        assert normalize_score("222.111") == Decimal("222.111")
-        assert normalize_score("111.2") == Decimal("111.2")
+
+def test_normalize_score() -> None:
+    assert normalize_score(None) is None
+    assert normalize_score("123.444111") == Decimal("123.444")
+    assert normalize_score("222.111") == Decimal("222.111")
+    assert normalize_score("111.2") == Decimal("111.2")
