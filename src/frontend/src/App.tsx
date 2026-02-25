6ca2066af1f40bc4ba990155343d7c0792e70761
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2026-02-25 13:10:01 +0100

    handle 404 all cases

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { AllProgramsRoutesSwitch } from '@containers/routers/AllProgramsRoutesSw
 import { BaseHomeRouter } from '@containers/routers/BaseHomeRouter';
 import { SelectedProgramRoutesSwitch } from '@containers/routers/SelectedProgramRoutesSwitch';
 import { AutoLogout } from '@core/AutoLogout';
+import withErrorBoundary from '@components/core/withErrorBoundary';
 import * as Sentry from '@sentry/react';
 import { FC, useEffect } from 'react';
 import {
@@ -99,9 +100,12 @@ const router = sentryCreateBrowserRouter([
   },
 ]);
 
-export const App: FC = () => (
-  <Providers>
-    <AutoLogout />
-    <RouterProvider router={router} />
-  </Providers>
+export const App: FC = withErrorBoundary(
+  () => (
+    <Providers>
+      <AutoLogout />
+      <RouterProvider router={router} />
+    </Providers>
+  ),
+  'App',
 );
