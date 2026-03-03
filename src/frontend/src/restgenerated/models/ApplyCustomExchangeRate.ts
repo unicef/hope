81636151fc935eab8c767491fcafab220a960d9b
marekbiczysko <marek.biczysko@tivix.com>
2026-03-03 19:44:09 +0100

    fe

---
+++ src/frontend/src/restgenerated/models/ApplyCustomExchangeRate.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ApplyCustomExchangeRate = {
+    unoreExchangeRate?: string | null;
+    customExchangeRate?: string | null;
+    version?: number;
+};
+
