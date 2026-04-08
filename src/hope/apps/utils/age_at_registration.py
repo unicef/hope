2169175bbf74934c9629677ac9f7bdd9ba5c7434
pavlo-mk <pv.pasha.pv@gmail.com>
2026-04-08 10:21:02 -0400

    fix calculate_age_at_registration

--- src/hope/apps/utils/age_at_registration.py
+++ src/hope/apps/utils/age_at_registration.py
@@ -1,14 +1,19 @@
-from typing import Any
+from datetime import datetime
 
 from dateutil import parser
 from dateutil.relativedelta import relativedelta
 
 
 def calculate_age_at_registration(
-    created_at: Any,
+    created_at: datetime,
     birth_date: str,
 ) -> int | None:
     try:
-        return relativedelta(created_at.date(), parser.parse(birth_date).date()).years
+        birth_date_parsed = parser.parse(birth_date).date()
+        created_at_date = created_at.date()
+        # return None if birth_date is future date
+        if birth_date_parsed > created_at_date:
+            return None
+        return relativedelta(created_at_date, birth_date_parsed).years
     except ValueError:
         return None
