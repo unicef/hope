be24d4cbc0e769bb7aa1e0002eaa76195d9273c9
Jan Romaniak <jan.romaniak@kellton.com>
2026-04-08 16:02:54 +0200

    choices typing

--- src/hope/api/endpoints/rdi/common.py
+++ src/hope/api/endpoints/rdi/common.py
@@ -1,19 +1,17 @@
-from typing import Any
-
 from rest_framework import serializers
 
 from hope.models import NOT_DISABLED
 
 
 class NullableChoiceField(serializers.ChoiceField):
-    def to_internal_value(self, data: Any) -> Any:
+    def to_internal_value(self, data: str) -> str | None:  # type: ignore[override]
         if data == "":
             return None
         return super().to_internal_value(data)
 
 
 class DisabilityChoiceField(serializers.ChoiceField):
-    def to_internal_value(self, data: Any) -> Any:
+    def to_internal_value(self, data: str) -> str:  # type: ignore[override]
         if data == "":
             return NOT_DISABLED
         return super().to_internal_value(data)
