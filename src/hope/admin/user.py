2d89cfb5146eb8b435627c8394be03af62e4d037
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2026-04-14 14:37:24 +0200

    fix display partner in list (column width)

--- src/hope/admin/user.py
+++ src/hope/admin/user.py
@@ -18,6 +18,7 @@ from django.forms.forms import Form
 from django.http import Http404, HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template.response import TemplateResponse
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 from jsoneditor.forms import JSONEditor
 from requests import HTTPError
@@ -235,7 +236,7 @@ class UserAdmin(HopeModelAdminMixin, UnfoldModelAdmin, UserAdminPlus, ADUSerMixi
         ("partner", AutoCompleteFilter),
         BusinessAreaFilter,
     ]
-    list_display = UserAdminPlus.list_display[:3] + ["partner"] + UserAdminPlus.list_display[3:]
+    list_display = UserAdminPlus.list_display[:3] + ["get_partner"] + UserAdminPlus.list_display[3:]
     inlines = (RoleAssignmentInline,)
     actions = [
         "add_business_area_role",
@@ -301,6 +302,12 @@ class UserAdmin(HopeModelAdminMixin, UnfoldModelAdmin, UserAdminPlus, ADUSerMixi
     @property
     def media(self) -> Any:
         return super().media + forms.Media(js=["hijack/hijack.js"])
+
+    @admin.display(description="Partner", ordering="partner__name")
+    def get_partner(self, obj: Any) -> str:
+        if obj.partner_id is None:
+            return "—"
+        return format_html('<span style="white-space: nowrap">{}</span>', obj.partner.name)
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
         return (
