fe8594699ca583fee2258b9a2d220d09956374ea
marekbiczysko <marek.biczysko@tivix.com>
2026-04-23 15:39:25 +0200

    migrations conflict

--- src/hope/apps/payment/migrations/0062_migration.py
+++ src/hope/apps/payment/migrations/0062_migration.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("payment", "0060_migration"),
+        ("payment", "0061_migration"),
     ]
 
     operations = [
