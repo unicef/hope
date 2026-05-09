e8659de1882b28c21cb979394a01bc064e362cec
pavlo-mk <pv.pasha.pv@gmail.com>
2026-05-08 23:48:44 -0400

    e2e :star:

--- tests/e2e/drawer/test_drawer.py
+++ tests/e2e/drawer/test_drawer.py
@@ -8,7 +8,7 @@ from e2e.page_object.programme_details.programme_details import ProgrammeDetails
 from e2e.page_object.programme_management.programme_management import (
     ProgrammeManagement,
 )
-from extras.test_utils.factories import DataCollectingTypeFactory, ProgramFactory
+from extras.test_utils.factories import DataCollectingTypeFactory, ProgramCycleFactory, ProgramFactory
 from hope.models import BeneficiaryGroup, BusinessArea, DataCollectingType, Program
 
 pytestmark = pytest.mark.django_db()
@@ -48,7 +48,7 @@ def get_program_with_dct_type_and_name(
 ) -> Program:
     dct = DataCollectingTypeFactory(type=dct_type)
     beneficiary_group = BeneficiaryGroup.objects.filter(name=beneficiary_group_name).first()
-    return ProgramFactory(
+    program = ProgramFactory(
         name=name,
         code=code,
         start_date=datetime.now() - relativedelta(months=1),
@@ -58,6 +58,8 @@ def get_program_with_dct_type_and_name(
         beneficiary_group=beneficiary_group,
         business_area=BusinessArea.objects.get(slug="afghanistan"),
     )
+    ProgramCycleFactory(program=program)
+    return program
 
 
 def get_social_program_with_dct_type_and_name(
