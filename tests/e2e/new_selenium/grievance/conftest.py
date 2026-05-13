dc6b4e86e5bd9f42625580085f593f92de76418c
pavlo-mk <pv.pasha.pv@gmail.com>
2026-05-13 11:47:58 -0400

    create cycle for social_worker_program

--- tests/e2e/new_selenium/grievance/conftest.py
+++ tests/e2e/new_selenium/grievance/conftest.py
@@ -3,22 +3,21 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import pytest
 
-from extras.test_utils.factories.core import (
-    DataCollectingTypeFactory,
-)
-from extras.test_utils.factories.program import ProgramFactory
+from extras.test_utils.factories.program import ProgramCycleFactory, ProgramFactory
 from hope.models import BeneficiaryGroup, BusinessArea, DataCollectingType, Program
 
 
 @pytest.fixture
 def social_worker_program(business_area: BusinessArea) -> Program:
     beneficiary_group, _ = BeneficiaryGroup.objects.get_or_create(name="People")
-    return ProgramFactory(
+    program = ProgramFactory(
         name="Social Program",
         status=Program.ACTIVE,
         business_area=business_area,
-        data_collecting_type=DataCollectingTypeFactory(type=DataCollectingType.Type.SOCIAL),
+        data_collecting_type=DataCollectingType.objects.filter(type=DataCollectingType.Type.SOCIAL).first(),
         beneficiary_group=beneficiary_group,
         start_date=datetime.now() - relativedelta(months=1),
         end_date=datetime.now() + relativedelta(months=1),
     )
+    ProgramCycleFactory(program=program)
+    return program
