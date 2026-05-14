0c88968882c1ad481fc7406d79dbd902fe21119b
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2026-05-14 11:36:28 +0200

    add selenium test for edit group name

--- tests/e2e/new_selenium/payment_module/conftest.py
+++ tests/e2e/new_selenium/payment_module/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
+from extras.test_utils.factories import PaymentPlanGroupFactory
 from extras.test_utils.factories.program import ProgramCycleFactory, ProgramFactory
-from hope.models import BusinessArea, Program, ProgramCycle
+from hope.models import BusinessArea, PaymentPlanGroup, Program, ProgramCycle
 
 
 @pytest.fixture
@@ -16,3 +17,8 @@ def payment_module_program(business_area: BusinessArea) -> Program:
 @pytest.fixture
 def program_cycle(payment_module_program: Program) -> ProgramCycle:
     return ProgramCycleFactory(program=payment_module_program)
+
+
+@pytest.fixture
+def payment_plan_group(program_cycle: ProgramCycle) -> PaymentPlanGroup:
+    return PaymentPlanGroupFactory(cycle=program_cycle, name="Original Group Name")
--- tests/e2e/new_selenium/payment_module/test_create_payment_plan_group.py
+++ tests/e2e/new_selenium/payment_module/test_create_payment_plan_group.py
@@ -3,7 +3,7 @@ import pytest
 from e2e.new_selenium.conftest import grant_permission
 from extras.test_utils.selenium import HopeTestBrowser
 from hope.apps.account.permissions import Permissions
-from hope.models import BusinessArea, ProgramCycle, User
+from hope.models import BusinessArea, PaymentPlanGroup, ProgramCycle, User
 
 pytestmark = pytest.mark.django_db()
 
@@ -45,3 +45,36 @@ def test_create_payment_plan_group(
         browser.click('a[data-cy="nav-Groups"]')
 
         browser.wait_for_text(group_name)
+
+
+def test_edit_payment_plan_group(
+    browser: HopeTestBrowser,
+    user_with_no_permissions: User,
+    business_area: BusinessArea,
+    payment_plan_group: PaymentPlanGroup,
+) -> None:
+    program = payment_plan_group.cycle.program
+    new_name = "Updated Group Name"
+
+    with grant_permission(
+        user_with_no_permissions,
+        business_area,
+        Permissions.PROGRAMME_VIEW_LIST_AND_DETAILS,
+        Permissions.PM_VIEW_LIST,
+        Permissions.PM_PAYMENT_PLAN_GROUP_VIEW_LIST,
+        Permissions.PM_PAYMENT_PLAN_GROUP_VIEW_DETAIL,
+        Permissions.PM_PAYMENT_PLAN_GROUP_UPDATE,
+    ):
+        browser.login(username="noperm_user", password="testtest2")
+        browser.open(f"/{business_area.slug}/programs/{program.code}/payment-module/groups/{payment_plan_group.id}")
+
+        browser.wait_for_element_clickable('[data-cy="button-edit-group-name"]')
+        browser.click('[data-cy="button-edit-group-name"]')
+
+        browser.wait_for_element_visible('input[name="name"]')
+        browser.clear('input[name="name"]')
+        browser.type('input[name="name"]', new_name)
+        browser.click('[data-cy="button-submit"]')
+
+        browser.wait_for_text("Group name updated")
+        browser.assert_text(new_name)
