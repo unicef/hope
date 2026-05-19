81641159e42611aa657fee17f272a8aafd70d039
marekbiczysko <marek.biczysko@tivix.com>
2026-05-19 13:11:05 +0200

    309534_WesterUnion_invoice_log_data_matching_parse_fix

--- src/hope/admin/western_union_payment_plan_report.py
+++ src/hope/admin/western_union_payment_plan_report.py
@@ -1,17 +1,32 @@
 from adminfilters.autocomplete import AutoCompleteFilter
 from adminfilters.mixin import AdminFiltersMixin
 from django.contrib import admin
+from django.forms import ModelForm
 from django.utils.html import format_html
 
 from hope.admin.utils import AutocompleteForeignKeyMixin
 from hope.models import WesternUnionPaymentPlanReport
 
 
+class WesternUnionPaymentPlanReportAdminForm(ModelForm):
+    class Meta:
+        model = WesternUnionPaymentPlanReport
+        fields = ["qcf_file", "report_file", "payment_plan", "sent"]
+        labels = {"qcf_file": "Invoice"}
+
+
 @admin.register(WesternUnionPaymentPlanReport)
 class WesternUnionPaymentPlanReportAdmin(AutocompleteForeignKeyMixin, AdminFiltersMixin, admin.ModelAdmin):
-    list_display = ["id", "qcf_file", "payment_plan"]
+    form = WesternUnionPaymentPlanReportAdminForm
+    list_display = ["id", "invoice", "payment_plan"]
     list_filter = [("payment_plan", AutoCompleteFilter)]
     readonly_fields = ["download_link"]
+
+    def invoice(self, obj: WesternUnionPaymentPlanReport) -> str:
+        return str(obj.qcf_file)
+
+    invoice.short_description = "Invoice"
+    invoice.admin_order_field = "qcf_file"
 
     def download_link(self, obj: WesternUnionPaymentPlanReport) -> str:  # pragma: no cover
         if not obj.report_file:
