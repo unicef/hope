5547983ac356a5a7b91a729a1314c528777e5a1f
marekbiczysko <marek.biczysko@tivix.com>
2026-05-28 10:33:05 +0200

    migration fix

--- src/hope/apps/payment/migrations/0065_migration.py
+++ src/hope/apps/payment/migrations/0065_migration.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("payment", "0063_migration"),
+        ("payment", "0064_migration"),
     ]
 
     operations = [
