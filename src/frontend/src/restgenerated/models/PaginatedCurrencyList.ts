f9a44e3c4836c50bca26e587596123dafffb7087
Maciej Szewczyk <maciej.szewczyk@kellton.com>
2026-06-02 11:37:29 +0200

    generate types

---
+++ src/frontend/src/restgenerated/models/PaginatedCurrencyList.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { Currency } from './Currency';
+export type PaginatedCurrencyList = {
+    next?: string | null;
+    previous?: string | null;
+    results: Array<Currency>;
+};
+
