83737073fdc1a537f3da44327d788b69e274a2f2
Paulina Kujawa <p.kujawa2f@gmail.com>
2026-06-29 11:11:39 +0200

    format

--- tests/unit/apps/payment/test_payment_plan_actions_views.py
+++ tests/unit/apps/payment/test_payment_plan_actions_views.py
@@ -2160,4 +2160,3 @@ def test_send_to_vision_missing_creds_returns_400(
         payment_plan_actions_context["url_send_to_vision"],
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-
