9f255d13cd9ee2ccec17cdabcb91f929c187f499
Paulina Kujawa <p.kujawa2f@gmail.com>
2026-07-29 16:27:43 +0200

    clean

--- src/hope/apps/utils/phone.py
+++ src/hope/apps/utils/phone.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, cast
 
 import phonenumbers
 
@@ -27,8 +27,7 @@ def recalculate_phone_numbers_validity(obj: Any) -> Any:
     if obj._state.adding is True:
         # create
         obj = calculate_phone_numbers_validity(obj)
-    current: Any = Individual.all_merge_status_objects.filter(pk=obj.pk).first()
-    if current:
+    elif current := cast("Individual | None", Individual.all_merge_status_objects.filter(pk=obj.pk).first()):
         # update
         if current.phone_no_valid is None or current.phone_no != obj.phone_no:
             obj.phone_no_valid = is_valid_phone_number(str(obj.phone_no))
