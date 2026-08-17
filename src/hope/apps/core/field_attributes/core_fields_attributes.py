dc9dd9fd4c6ac95dca38d7a1ad4011e507381b67
nerjan <jan.belkner@gmail.com>
2026-08-18 01:02:29 +0200

    not snapshotable fields TODO deleted

--- src/hope/apps/core/field_attributes/core_fields_attributes.py
+++ src/hope/apps/core/field_attributes/core_fields_attributes.py
@@ -970,7 +970,6 @@ CORE_FIELDS_ATTRIBUTES = [
         "associated_with": _INDIVIDUAL,
         "xlsx_field": "unhcr_id_photo_i_c",
         "scope": [Scope.GLOBAL, Scope.TARGETING, Scope.KOBO_IMPORT, Scope.XLSX_PEOPLE],
-        # "snapshot_field":  # TODO: this one is not added in snapshot
     },
     {
         "id": "4e836832-2cf2-4073-80eb-21316eaf7277",
@@ -1111,7 +1110,6 @@ CORE_FIELDS_ATTRIBUTES = [
         "associated_with": _INDIVIDUAL,
         "xlsx_field": "scope_id_photo_i_c",
         "scope": [Scope.GLOBAL, Scope.TARGETING, Scope.KOBO_IMPORT, Scope.XLSX_PEOPLE],
-        # "snapshot_field":  # TODO: this one is not added in snapshot
     },
     {
         "id": "3bf6105f-87d0-479b-bf92-7f90af4d8462",
@@ -1125,7 +1123,6 @@ CORE_FIELDS_ATTRIBUTES = [
         "associated_with": _INDIVIDUAL,
         "xlsx_field": "other_id_type_i_c",
         "scope": [Scope.GLOBAL, Scope.TARGETING, Scope.KOBO_IMPORT, Scope.XLSX_PEOPLE],
-        # "snapshot_field":  # TODO: this one is not added in snapshot
     },
     {
         "id": "556e14af-9901-47f3-bf2c-20b4c721e8f7",
@@ -1990,7 +1987,6 @@ CORE_FIELDS_ATTRIBUTES = [
         "associated_with": _INDIVIDUAL,
         "xlsx_field": "has_phone_number",
         "scope": [Scope.TARGETING],
-        # "snapshot_field":  # TODO: this one is not added in snapshot
     },
     {
         "id": "7c2b9a14-9f3d-4b6e-8a21-3d5f0c1e7b93",
@@ -2019,7 +2015,6 @@ CORE_FIELDS_ATTRIBUTES = [
         "associated_with": _INDIVIDUAL,
         "xlsx_field": "has_tax_ID_number",
         "scope": [Scope.TARGETING],
-        # "snapshot_field":  # TODO: this one is not added in snapshot
     },
     {
         "id": "6b97e9a3-38bb-49a3-9637-65f05d5b8ea4",
@@ -2034,7 +2029,6 @@ CORE_FIELDS_ATTRIBUTES = [
         "associated_with": _INDIVIDUAL,
         "xlsx_field": "has_the_bank_account_number",
         "scope": [Scope.TARGETING],
-        # "snapshot_field":  # TODO: this one is not added in snapshot
     },
     {
         "id": "0bf5fad3-8f4f-4528-85f7-57e8a84a2a43",
@@ -2149,7 +2143,6 @@ CORE_FIELDS_ATTRIBUTES = [
         "associated_with": _HOUSEHOLD,
         "xlsx_field": "end",
         "scope": [Scope.KOBO_IMPORT],
-        # "snapshot_field":  # TODO: this one is not added in snapshot
     },
     {
         "id": "16e68909-54f4-47cb-ab06-066d13e85134",
