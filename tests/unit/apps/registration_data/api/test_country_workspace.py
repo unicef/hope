2384da38bca49e979bfc2b4ce03d99cd80764582
Kamil Jeziorny <kamij98@gmail.com>
2026-08-24 09:52:57 +0200

    Remove redundant tests

--- tests/unit/apps/registration_data/api/test_country_workspace.py
+++ tests/unit/apps/registration_data/api/test_country_workspace.py
@@ -19,6 +19,7 @@ def test_notify_posts_signed_token_in_body() -> None:
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url == CALLBACK_URL
     assert json.loads(responses.calls[0].request.body) == {"signed_token": SIGNED_TOKEN}
+    assert "Authorization" not in responses.calls[0].request.headers
 
 
 @responses.activate
@@ -27,24 +28,6 @@ def test_notify_raises_on_non_2xx() -> None:
 
     with pytest.raises(CountryWorkspaceAPI.CountryWorkspaceAPIError):
         CountryWorkspaceAPI(api_url=CALLBACK_URL).notify_rdi_deleted(SIGNED_TOKEN)
-
-
-@responses.activate
-def test_notify_sends_no_auth_header() -> None:
-    responses.add(responses.POST, CALLBACK_URL, status=200)
-
-    CountryWorkspaceAPI(api_url=CALLBACK_URL).notify_rdi_deleted(SIGNED_TOKEN)
-
-    assert "Authorization" not in responses.calls[0].request.headers
-
-
-@responses.activate
-def test_notify_reads_no_body() -> None:
-    responses.add(responses.POST, CALLBACK_URL, status=200)
-
-    CountryWorkspaceAPI(api_url=CALLBACK_URL).notify_rdi_deleted(SIGNED_TOKEN)
-
-    assert len(responses.calls) == 1
 
 
 @responses.activate
