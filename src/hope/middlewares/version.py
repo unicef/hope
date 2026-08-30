d9e52432e250ac4a2ff2f7e5672163b718b180ce
Domenico DiNicola <dom.dinicola@gmail.com>
2026-08-30 10:00:10 +0300

    refactor: address review comments on Any annotations

--- src/hope/middlewares/version.py
+++ src/hope/middlewares/version.py
@@ -1,14 +1,15 @@
-from typing import Any, Callable
+from collections.abc import Callable
 
 from django.conf import settings
+from django.http import HttpRequest, HttpResponse
 
 
 class VersionMiddleware:
-    def __init__(self, get_response: Callable) -> None:
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
         self.get_response = get_response
         super().__init__()
 
-    def __call__(self, request: Any) -> Any:
+    def __call__(self, request: HttpRequest) -> HttpResponse:
         response = self.get_response(request)
         response["X-Hope-Backend-Version"] = settings.VERSION
         return response
