42c6a9d998b9ceecc422d4d226a8481a76dbcc5b
Domenico DiNicola <dom.dinicola@gmail.com>
2026-09-09 14:12:06 +0300

    refactor: tighten formfield types in HorizontalChoiceArrayField and ChoiceArrayField

--- src/hope/apps/account/fields.py
+++ src/hope/apps/account/fields.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from django import forms
 from django.contrib.postgres.fields import ArrayField
+from django.forms.fields import Field as FormField
 
 
 class ChoiceArrayField(ArrayField):
@@ -10,7 +11,7 @@ class ChoiceArrayField(ArrayField):
         form_class: type[forms.Field] | None = None,
         choices_form_class: type[forms.ChoiceField] | None = None,
         **kwargs: Any,
-    ) -> Any:
+    ) -> FormField | None:
         defaults = {
             "form_class": forms.MultipleChoiceField,
             "choices": self.base_field.choices,
