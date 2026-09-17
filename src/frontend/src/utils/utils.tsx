5153a045e69ac998a34172b97cf74e63fc26bbda
Kamil Jeziorny <40832304+Jeeziorny@users.noreply.github.com>
2026-09-17 14:30:19 +0200

    Implementation (#6256)
    317775 nullify cw and originating ids on copy (#6374)
    
    * Implementation
    
    * Biometric deduplication off in copied programme
    
    * for loop removal
    
    * Review fixes vol 1
    
    * Fixtures and other stuff Jan asked me about
    
    * dereduntify
    
    Rebase finished?
    
    Review fixes
    
    Fix FE
    
    Clean up
    
    mypy
    
    FE lint
    
    Disable deduplication tresholds for CW flow (#6392)
    
    * Implementation
    
    * COMMENT
    
    * num of queries

--- src/frontend/src/utils/utils.tsx
+++ src/frontend/src/utils/utils.tsx
@@ -2,7 +2,6 @@ import type { HeadCell } from '@core/Table/EnhancedTableHead';
 import { Box, Typography } from '@mui/material';
 import { PaymentPlanBackgroundActionStatusEnum } from '@restgenerated/models/PaymentPlanBackgroundActionStatusEnum';
 import { BuildStatusEnum } from '@restgenerated/models/BuildStatusEnum';
-import { DeduplicationEngineStatusEnum } from '@restgenerated/models/DeduplicationEngineStatusEnum';
 import { PaymentPlanStatusEnum as PaymentPlanStatus } from '@restgenerated/models/PaymentPlanStatusEnum';
 import { PaymentStatusEnum } from '@restgenerated/models/PaymentStatusEnum';
 import { PaymentVerificationStatusEnum } from '@restgenerated/models/PaymentVerificationStatusEnum';
@@ -283,27 +282,6 @@ export function registrationDataImportStatusToColor(
     case RegistrationDataImportStatusEnum.IMPORT_ERROR:
     case RegistrationDataImportStatusEnum.MERGE_ERROR:
     case RegistrationDataImportStatusEnum.DEDUPLICATION_FAILED:
-      return theme.palette.error.main;
-    default:
-      return theme.hctPalette.orange;
-  }
-}
-
-export function registrationDataImportDeduplicationEngineStatusToColor(
-  theme: typeof themeObj,
-  status: string,
-): string {
-  switch (status) {
-    case DeduplicationEngineStatusEnum.PENDING:
-      return theme.hctPalette.gray;
-    case DeduplicationEngineStatusEnum.UPLOADED:
-      return theme.hctPalette.orange;
-    case DeduplicationEngineStatusEnum.IN_PROGRESS:
-      return theme.hctPalette.orange;
-    case DeduplicationEngineStatusEnum.FINISHED:
-      return theme.hctPalette.green;
-    case DeduplicationEngineStatusEnum.UPLOAD_ERROR:
-    case DeduplicationEngineStatusEnum.ERROR:
       return theme.palette.error.main;
     default:
       return theme.hctPalette.orange;
